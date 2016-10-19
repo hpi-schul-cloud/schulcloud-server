f86f6f7ce09c9357eebed8fa3b4a23b3e573aeb8
Nico Knoll <mail@nico.is>
2016-10-19 19:09:04 +0200

    added code quality linter

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,13 +1,15 @@
 'use strict';
 
 // Add any common hooks you want to share across services in here.
-// 
+//
 // Below is an example of how a hook is written and exported. Please
 // see http://docs.feathersjs.com/hooks/readme.html for more details
 // on hooks.
 
+const winston = require('winston');
+
 exports.myHook = function(options) {
   return function(hook) {
-    console.log('My custom global hook ran. Feathers is awesome!');
+    //console.log('My custom global hook ran. Feathers is awesome!');
   };
 };
