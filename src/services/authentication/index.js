c895ce7b6af382a7017d37c072e43da952166b83
Carl Gödecken <carl.goedecken@student.hpi.de>
2016-10-21 12:35:11 +0200

    Refactor authentication/index.js promises, add bcryptjs library to package.json

--- src/services/authentication/index.js
+++ src/services/authentication/index.js
@@ -1,73 +1,68 @@
 'use strict';
 
 const authentication = require('feathers-authentication');
+const crypto = require('bcryptjs');
 
-var _bcryptjs = require('bcryptjs');
-var _bcryptjs2 = _interopRequireDefault(_bcryptjs);
-function _interopRequireDefault(obj) {
-	return obj && obj.__esModule ? obj : {default: obj};
-}
+module.exports = function() {
+    const app = this;
 
-module.exports = function () {
-	const app = this;
+    let config = app.get('auth');
 
-	let config = app.get('auth');
+    const authenticationService = {
+        // POST /auth/account
+        create({email, password}, params) {
 
-	app.use('/auth/account', {
-		create({email, password}, params) {
-			return new Promise((resolve, reject) => {
+            const accountService = app.service('/accounts');
+            const userService = app.service('/users');
+            const jwtService = app.service('/auth/token');
 
-				const accountService = app.service('/accounts');
-				const userService = app.service('/users');
-				const jwtService = app.service('/auth/token');
+            return accountService.find({'email': email})
+                .then((accounts) => {
+                    // 1. Find a fitting account
+                    var account = accounts[0] || accounts.data && accounts.data[0];
 
-				accountService.find({'email': email}).then((accounts) => {
-					// 1. Find a fitting account
-					var account = accounts[0] || accounts.data && accounts.data[0];
+                    if (!account) {
+                        throw new Error('No fitting account found');
+                    }
 
-					if (!account) {
-						return reject(null, false);
-					}
+                    return account;
+                })
+                .then((account) => {
+                    // 2. Check if credentials of account are correct
+                    var hash = account['password'];
 
-					return account;
-				}).then((account) => {
-					// 2. Check if credentials of account are correct
-					var crypto = _bcryptjs2.default;
-					var hash = account['password'];
+                    if (!hash) {
+                        throw new Error('Account record in the database is missing a password');
+                    }
 
-					if (!hash) {
-						return reject(new Error('Acccount record in the database is missing a password'));
-					}
+                    return new Promise((resolve, reject) => {
+                        crypto.compare(password, hash, (error, result) => {
+                            // Handle 500 server error.
+                            if (error) {
+                                return reject(error);
+                            }
 
-					return new Promise((done) => {
-						crypto.compare(password, hash, (error, result) => {
-							// Handle 500 server error.
-							if (error) {
-								return reject(error);
-							}
+                            if (!result) {
+                                return reject(new Error('Passwords not matching'));
+                            }
 
-							if (!result) {
-								return reject(new Error('Passwords not matching'));
-							}
+                            return resolve(account.userId);
+                        });
+                    });
+                }).then((userId) => {
+                    // 3. account credentials are correct - get user
+                    return userService.get(userId);
+                }).then((user) => {
+                    // 4. if user found: generate jwt
+                    return jwtService.create(user);
+                })/*.then((jwt) => {
+             // 5. return generated JWT
+             resolve(jwt);
+             })*/;
 
-							return done(account.userId);
-						});
-					});
-				}).then((userId) => {
-					// 3. account credentials are correct - get user
-					return userService.get(userId);
-				}).then((user) => {
-					// 4. if user found: generate jwt
-					return jwtService.create(user);
-				}).then((jwt) => {
-					// 5. return generated JWT
-					resolve(jwt);
-				}).catch((error) => {
-					reject(error);
-				});
-			});
-		}
-	});
+        }
+    };
 
-	app.configure(authentication(config));
+    app.use('/auth/account', authenticationService);
+    app.configure(authentication(config));
 };
