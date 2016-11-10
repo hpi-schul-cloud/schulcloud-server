fa6db4c3df27b71b1337fe0bfb5e6f2de7f7e423
Carl Gödecken <carl.goedecken@student.hpi.de>
2016-11-10 17:11:52 +0100

    Change itslearning behaviour on wrong credentials, refactor itslearning and lernsax modules, expand tests

--- test/services/authentication/lernsax.test.js
+++ test/services/authentication/lernsax.test.js
@@ -4,7 +4,8 @@ const chai = require('chai');
 const assert = require('assert');
 const promisify = require("es6-promisify");
 const LernsaxLoginStrategy = require('../../../src/services/authentication/strategies/lernsax.js');
-var should = chai.should();
+let should = chai.should();
+let expect = chai.expect;
 
 describe('Lernsax single-sign-on', function() {
 	const testLernSaxUserFail = {
@@ -22,17 +23,21 @@ describe('Lernsax single-sign-on', function() {
 		var loginService = new LernsaxLoginStrategy();
 		return loginService.login(testLernSaxUser).then((response) => {
 			var _res = response;
-			chai.expect(_res).to.be.not.undefined;
-			chai.expect(_res.success).to.be.true;
-			chai.expect(_res.username).to.equal(testLernSaxUser.username);
+			expect(_res).to.be.not.undefined;
+			expect(_res.success).to.be.true;
+			expect(_res.username).to.equal(testLernSaxUser.username);
 		});
 	});
 
 	it('should fail when input wrong user credentials', function() {
 		this.timeout(5000);
 		var loginService = new LernsaxLoginStrategy();
-		return loginService.login(testLernSaxUserFail).catch((err) => {
-			chai.expect(err).to.equal('NotAuthenticated: wrong password');
+		return loginService.login(testLernSaxUserFail)
+			.then(result => {
+				expect(result).to.be.nil;
+			})
+			.catch((err) => {
+			expect(err).to.equal('NotAuthenticated: wrong password');
 		});
 	});
 });
