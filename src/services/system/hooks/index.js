56ba9efbe352b875873363fb1c8e28b0226bdbfd
Carl Gödecken <carl.goedecken@student.hpi.de>
2016-11-10 17:13:55 +0100

    Relax authentication requirements on system service

--- src/services/system/hooks/index.js
+++ src/services/system/hooks/index.js
@@ -5,25 +5,37 @@ const hooks = require('feathers-hooks');
 const auth = require('feathers-authentication').hooks;
 
 exports.before = {
-  all: [
-    auth.verifyToken(),
-    auth.populateUser(),
-    auth.restrictToAuthenticated()
-  ],
-  find: [],
-  get: [],
-  create: [ifNotLocal(isAdmin())],
-  update: [ifNotLocal(isAdmin())],
-  patch: [ifNotLocal(isAdmin())],
-  remove: [ifNotLocal(isAdmin())]
+	all: [],
+	find: [],
+	get: [],
+	create: [
+		auth.verifyToken(),
+		auth.populateUser(),
+		auth.restrictToAuthenticated(),
+		ifNotLocal(isAdmin())],
+	update: [
+		auth.verifyToken(),
+		auth.populateUser(),
+		auth.restrictToAuthenticated(),
+		ifNotLocal(isAdmin())],
+	patch: [
+		auth.verifyToken(),
+		auth.populateUser(),
+		auth.restrictToAuthenticated(),
+		ifNotLocal(isAdmin())],
+	remove: [
+		auth.verifyToken(),
+		auth.populateUser(),
+		auth.restrictToAuthenticated(),
+		ifNotLocal(isAdmin())]
 };
 
 exports.after = {
-  all: [],
-  find: [],
-  get: [],
-  create: [],
-  update: [],
-  patch: [],
-  remove: []
+	all: [],
+	find: [],
+	get: [],
+	create: [],
+	update: [],
+	patch: [],
+	remove: []
 };
