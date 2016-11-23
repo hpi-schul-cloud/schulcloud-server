5eb4233fd308da101b859e85e77e0a60a1ea3613
Carl Gödecken <carl.goedecken@student.hpi.de>
2016-11-23 12:04:00 +0100

    Update feathers-swagger integration
    Swagger UI is now served by feathers-swagger

--- src/swagger.js
+++ src/swagger.js
@@ -2,26 +2,20 @@ const feathersSwagger = require('feathers-swagger');
 const serveStatic = require('feathers').static;
 const path = require('path');
 
-module.exports = function(app) {
-  app.configure(feathersSwagger({
-    /* example configuration */
-    docsPath:'docs',
-    version: '0.0.1',
-    basePath: '',
-    info: {
-      'title': 'Schul-Cloud API',
-      'description': 'This is the Schul-Cloud API.',
-      'termsOfServiceUrl': 'https://github.com/schulcloud/schulcloud-server/blob/master/LICENSE',
-      'contact': 'team@schul.tech',
-      'license': 'GPL-3.0',
-      'licenseUrl': 'https://github.com/schulcloud/schulcloud-server/blob/master/LICENSE'
-    }
-  }));
-
-  app.get('/swagger-ui', (req, res) => {
-    res.redirect('index.html?url=../../docs');
-  });
-
-  const swaggerUiPath = path.dirname(require.resolve('swagger-ui'));
-  app.use('/swagger-ui', serveStatic(swaggerUiPath));
+module.exports = function (app) {
+	app.configure(feathersSwagger({
+		/* example configuration */
+		docsPath: '/docs',
+		version: '0.0.1',
+		basePath: '',
+		uiIndex: path.join(__dirname, 'swagger.html'),
+		info: {
+			'title': 'Schul-Cloud API',
+			'description': 'This is the Schul-Cloud API.',
+			'termsOfServiceUrl': 'https://github.com/schulcloud/schulcloud-server/blob/master/LICENSE',
+			'contact': 'team@schul.tech',
+			'license': 'GPL-3.0',
+			'licenseUrl': 'https://github.com/schulcloud/schulcloud-server/blob/master/LICENSE'
+		}
+	}));
 };
