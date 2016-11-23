7ea7885e569d98f3fc42c438539ce4e003a1002f
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2016-11-23 16:16:51 +0100

    update default headers

--- src/middleware/defaultHeaders.js
+++ src/middleware/defaultHeaders.js
@@ -2,6 +2,6 @@
 
 module.exports = (req, res, next) => {
   req.headers['content-type'] = req.headers['content-type'] || 'application/json';
-  req.headers['accept'] = req.headers['accept'] || 'application/json';
+  req.headers['accept'] = (req.headers['accept'] === '*/*' || !req.headers['accept']) ? 'application/json' : req.headers['accept'];
   next();
 };
