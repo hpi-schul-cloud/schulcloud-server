ad80760bf1b82c73c5e5a366a0bbcc73d7d17d8d
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2017-01-19 16:10:01 +0100

    fix userId hook

--- src/services/fileStorage/hooks/index.js
+++ src/services/fileStorage/hooks/index.js
@@ -5,12 +5,8 @@ const hooks = require('feathers-hooks');
 const auth = require('feathers-authentication');
 
 const resolveUserId = (hook) => {
-	return hook.app.passport.verifyJWT(hook.params.headers.authorization, hook.app.get("auth")).then(res => {
-		if (res.userId) {
-			hook.params.payload.userId = res.userId;
-		}
-		return hook;
-	});
+	hook.params.payload.userId = hook.params.account.userId ? hook.params.account.userId : '';
+	return hook;
 };
 
 exports.before = {
