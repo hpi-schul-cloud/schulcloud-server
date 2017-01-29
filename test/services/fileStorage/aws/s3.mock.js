b16ef319cbb61a2b6f764d7384efbd94fd31e418
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2017-01-29 11:29:31 +0100

    improve test coverage for fileStorage

--- test/services/fileStorage/aws/s3.mock.js
+++ test/services/fileStorage/aws/s3.mock.js
@@ -1,3 +1,16 @@
+const testListObjectsReturn = {
+	Contents: [
+		{key: "testFile"}
+	]
+};
+
+const testHeadObjectReturn = {
+	Metadata: { name: "testName", thumbnail: "testThumbnail" },
+	LastModified: "test",
+	ContentLength: 5,
+	ContentType: "mime/image"
+};
+
 var mockAws = {
 	S3: function(){
 		return {
@@ -5,13 +18,13 @@ var mockAws = {
 				callback(null, "test");
 			},
 			listObjectsV2: function (params, callback) {
-				callback(null, "test");
+				callback(null, testListObjectsReturn);
 			},
 			getSignedUrl: function (params, callback) {
 				callback(null, "test");
 			},
 			headObject: function (params, callback) {
-				callback(null, "test");
+				callback(null, testHeadObjectReturn);
 			},
 			deleteFile: function (params, callback) {
 				callback(null, "test");
