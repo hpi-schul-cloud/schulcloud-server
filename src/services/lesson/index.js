546de0ec6aed40ac3bfb0aba8fbe041cb243efc0
Nico Knoll <mail@nico.is>
2017-03-14 12:45:39 +0100

    Implemented lessons basic service and some fixes like lean models

--- src/services/lesson/index.js
+++ src/services/lesson/index.js
@@ -4,26 +4,27 @@ const service = require('feathers-mongoose');
 const lesson = require('./model');
 const hooks = require('./hooks');
 
-module.exports = function() {
-  const app = this;
+module.exports = function () {
+	const app = this;
 
-  const options = {
-    Model: lesson,
-    paginate: {
-      default: 5,
-      max: 25
-    }
-  };
+	const options = {
+		Model: lesson,
+		paginate: {
+			default: 5,
+			max: 25
+		},
+		lean: true
+	};
 
-  // Initialize our service with any options it requires
-  app.use('/lessons', service(options));
+	// Initialize our service with any options it requires
+	app.use('/lessons', service(options));
 
-  // Get our initialize service to that we can bind hooks
-  const systemService = app.service('/lessons');
+	// Get our initialize service to that we can bind hooks
+	const systemService = app.service('/lessons');
 
-  // Set up our before hooks
-  systemService.before(hooks.before);
+	// Set up our before hooks
+	systemService.before(hooks.before);
 
-  // Set up our after hooks
-  systemService.after(hooks.after);
+	// Set up our after hooks
+	systemService.after(hooks.after);
 };
