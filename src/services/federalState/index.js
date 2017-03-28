54bb72146e4ac3176516e9bb3892d4988abfa010
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2017-03-28 12:21:48 +0200

    WIP

---
+++ src/services/federalState/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const service = require('feathers-mongoose');
+const federalState = require('./model');
+const hooks = require('./hooks');
+
+module.exports = function() {
+  const app = this;
+
+  const options = {
+    Model: federalState,
+    paginate: {
+      default: 16,
+      max: 25
+    },
+  	lean: true
+  };
+
+  // Initialize our service with any options it requires
+  app.use('/federalStates', service(options));
+
+  // Get our initialize service to that we can bind hooks
+  const federalStateService = app.service('/federalStates');
+
+  // Set up our before hooks
+  federalStateService.before(hooks.before);
+
+  // Set up our after hooks
+  federalStateService.after(hooks.after);
+};
