5ed1d3a4553876809d92caf05ccf34f987253d22
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2017-03-28 14:00:33 +0200

    fix linter

--- src/services/federalState/index.js
+++ src/services/federalState/index.js
@@ -4,27 +4,27 @@ const service = require('feathers-mongoose');
 const federalState = require('./model');
 const hooks = require('./hooks');
 
-module.exports = function() {
-  const app = this;
+module.exports = function () {
+	const app = this;
 
-  const options = {
-    Model: federalState,
-    paginate: {
-      default: 16,
-      max: 25
-    },
-  	lean: true
-  };
+	const options = {
+		Model: federalState,
+		paginate: {
+			default: 16,
+			max: 25
+		},
+		lean: true
+	};
 
-  // Initialize our service with any options it requires
-  app.use('/federalStates', service(options));
+	// Initialize our service with any options it requires
+	app.use('/federalStates', service(options));
 
-  // Get our initialize service to that we can bind hooks
-  const federalStateService = app.service('/federalStates');
+	// Get our initialize service to that we can bind hooks
+	const federalStateService = app.service('/federalStates');
 
-  // Set up our before hooks
-  federalStateService.before(hooks.before);
+	// Set up our before hooks
+	federalStateService.before(hooks.before);
 
-  // Set up our after hooks
-  federalStateService.after(hooks.after);
+	// Set up our after hooks
+	federalStateService.after(hooks.after);
 };
