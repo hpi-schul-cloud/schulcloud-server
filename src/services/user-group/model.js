8de28ac077437856274e9cac35845d9e478bb091
adrianjost <adrianjost@hackedit.de>
2017-04-04 11:36:36 +0200

    color default value fix

--- src/services/user-group/model.js
+++ src/services/user-group/model.js
@@ -22,7 +22,7 @@ const courseModel = mongoose.model('course', getUserGroupSchema({
 	classIds: [{type: Schema.Types.ObjectId, required: true, ref: 'class'}],
 	teacherIds: [{type: Schema.Types.ObjectId, required: true, ref: 'user'}],
 	ltiToolIds: [{type: Schema.Types.ObjectId, required: true, ref: 'ltiTool'}],
-	color: {type: String, required: true},
+	color: {type: String, required: true, 'default': '#1DE9B6'},
 	gradeSystem: {type: Boolean}
 }));
 const classModel =  mongoose.model('class', getUserGroupSchema({
