d7320d1fbb267eb27a87696f81a174a98cbeb08c
Pit Wegner <Pit Wegner>
2017-04-21 16:30:41 +0200

    modified news model

--- src/services/news/model.js
+++ src/services/news/model.js
@@ -7,8 +7,9 @@ const newsSchema = new Schema({
 	schoolId: {type: Schema.Types.ObjectId, required: true},
 	title: {type: String, required: true},
 	content: {type: String, required: true},
-	url: {type: String, required: true},
-	createdAt: {type: Date, 'default': Date.now}
+	createdAt: {type: Date, 'default': Date.now},
+	updatedAt: {type: Date, 'default': Date.now},
+	displayAt: {type: Date, 'default': Date.now}
 },{
 	timestamps: true
 });
