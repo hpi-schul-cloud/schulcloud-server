d7a104b5d9106d13657e1922563f48d45fb473e8
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2017-05-10 09:19:25 +0200

    create update-function for calendar-service, testing

--- test/services/calendar/index.test.js
+++ test/services/calendar/index.test.js
@@ -1,11 +1,96 @@
 'use strict';
 
 const assert = require('assert');
-const app = require('../../../src/app');
+const chai = require('chai');
+const expect = chai.expect;
+const mockery = require('mockery');
+const promisify = require("es6-promisify");
+const fs = require("fs");
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+const path = require("path");
+const requestMock = require('./mock/mockResponses');
 
 describe('calendar service', function () {
-	const service = app.service('calendar');
-	it('registered the calendar service', () => {
-		assert.ok(service);
+	this.timeout(10000);	// for slow require(app) call
+
+	let app = null;
+	let calendarService = null;
+
+	before(done => {
+		mockery.enable({
+			warnOnReplace: false,
+			warnOnUnregistered: false,
+			useCleanCache: true
+		});
+		mockery.registerMock('request-promise-native', requestMock);
+		app = require('../../../src/app');
+		app.setup();
+		calendarService = app.service('calendar');
+		done();
 	});
+
+	after(done => {
+		mockery.deregisterAll();
+		mockery.disable();
+		done();
+	});
+
+	it('registered the calendar service', () => {
+		assert.ok(calendarService);
+	});
+
+	it('GET /calendar', () => {
+		return calendarService.find({
+			query: {all: true},
+			payload: {userId: '0000d231816abba584714c9e'}}).then(result => {
+				expect(result.length).to.be.above(0);
+				expect(result[0].title).to.not.be.undefined;
+				expect(result[0].title).to.equal('tttttt');
+				expect(result[0].start).to.not.be.undefined;
+				expect(result[0].start).to.equal(1495224000000);
+				expect(result[0].end).to.not.be.undefined;
+				expect(result[0].end).to.equal(1495224000000);
+			});
+	});
+
+	it('POST /calendar', () => {
+		let postBody = {
+			"summary": "ttt",
+			"location": "Paul-Gerhardt-Gymnasium",
+			"description": "",
+			"startDate": "2017-05-19T20:00:00.000Z",
+			"endDate": "2017-05-19T20:00:00.000Z",
+			"scopeId": "0000d231816abba584714c9e"
+		};
+
+		return calendarService.create(postBody, {payload: {userId: '0000d231816abba584714c9e'}})
+			.then(result => {
+				expect(result.length).to.be.above(0);
+		});
+	});
+
+	it('PUT /calendar', () => {
+		let putBody = {
+			"summary": "ttt",
+			"location": "Paul-Gerhardt-Gymnasium",
+			"description": "",
+			"startDate": "2017-05-19T20:00:00.000Z",
+			"endDate": "2017-05-19T20:00:00.000Z",
+			"scopeId": "0000d231816abba584714c9e"
+		};
+
+		return calendarService.update('exampleId', putBody, {payload: {userId: '0000d231816abba584714c9e'}})
+			.then(result => {
+				expect(result.length).to.be.above(0);
+			});
+	});
+
+	it('DELETE /calendar', () => {
+		return calendarService.remove('exampleId', {payload: {userId: '0000d231816abba584714c9e'}})
+			.then(result => {
+				expect(result).to.not.be.undefined;
+			});
+	});
+
 });
