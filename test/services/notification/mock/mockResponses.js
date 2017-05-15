3059ca484c2270e7b8d7c486051c3b21a0559c26
Langleu <lars.lange@student.hpi.de>
2017-05-15 15:00:33 +0200

    add notification mock tests

---
+++ test/services/notification/mock/mockResponses.js
@@ -0,0 +1,17 @@
+const exampleResponse = {
+	"data": {
+		"type": "messages",
+		"id": "59199dbe8d4be221143cc866",
+		"attributes": {
+			"title": "New Notification from Teacher1_1",
+			"body": "You have a new Notification",
+			"priority": "medium",
+			"created-at": "2017-05-15T12:23:26.017Z"
+		}
+	}
+};
+
+
+module.exports = (options) => {
+	return Promise.resolve(exampleResponse);
+};
