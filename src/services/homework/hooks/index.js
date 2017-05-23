124cea75ebdc8b949e16807bca80bafa7bfe08a2
Langleu <lars.lange@student.hpi.de>
2017-05-23 11:29:21 +0200

    add limit filter and top up limit to 100k

--- src/services/homework/hooks/index.js
+++ src/services/homework/hooks/index.js
@@ -6,18 +6,25 @@ const auth = require('feathers-authentication');
 
 const filterApplicableHomework = hook => {
 	let uId = hook.params.account.userId;
-	hook.result.data = hook.result.data.filter(function(c){
+	let data = hook.result.data || hook.result;
+	data = data.filter(function(c){
 		return (new Date(c.availableDate).getTime() < Date.now()
 			&& c.courseId != null
 			&& (c.courseId.userIds || []).indexOf(uId) != -1)
 			|| JSON.stringify(c.teacherId)==JSON.stringify(uId);
 	});
+
+	if (hook.result.data)
+		hook.result.data = data;
+	else
+		hook.result = data;
+
 	return Promise.resolve(hook);
 };
 
 exports.before = {
   all: [auth.hooks.authenticate('jwt')],
-  find: [],
+  find: [globalHooks.mapPaginationQuery.bind(this)],
   get: [],
   create: [],
   update: [],
