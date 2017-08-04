c5dd05c4e70d18627482707ed82315d626fe1eb7
Langleu <lars.lange@student.hpi.de>
2017-08-04 13:44:23 +0200

    restrict access to accounts

--- src/services/lesson/hooks/index.js
+++ src/services/lesson/hooks/index.js
@@ -5,7 +5,7 @@ const hooks = require('feathers-hooks');
 const auth = require('feathers-authentication');
 
 exports.before = {
-	all: [(hook) => {
+	all: [auth.hooks.authenticate('jwt'), (hook) => {
 		if(hook.data) {
 			hook.data.contents = (hook.data.contents || []).map((item) => {
 				switch (item.component) {
