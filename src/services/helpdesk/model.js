02b57c3b14cf7782ada9d61d24ee68862dc1c130
Langleu <lars.lange@student.hpi.de>
2017-08-22 15:06:43 +0200

    initial restructure

---
+++ src/services/helpdesk/model.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// helpdesk-model.js - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const categories = ['dashboard', 'courses', 'classes', 'calendar', 'homework', 'files', 'content', 'administration', 'login_registration', 'other'];
+const states = ['open', 'closed', 'submitted'];
+
+const problemSchema = new Schema({
+	subject: {type: String, required: true},
+	category: {type: String, enum: categories},
+	currentState : {type: String},
+	targetState: {type: String},
+	state: {type: String, enum: states},
+	notes: {type: String},
+	userId: {type: Schema.Types.ObjectId, ref: 'user'},
+	schoolId: {type: Schema.Types.ObjectId, ref: 'school'},
+	forwardedAt: {type: Date},
+	createdAt: {type: Date, 'default': Date.now},
+	updatedAt: {type: Date, 'default': Date.now}
+});
+
+const problemModel = mongoose.model('problem', problemSchema);
+
+module.exports = problemModel;
