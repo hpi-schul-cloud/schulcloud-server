d7a9dd1f8388fb526e5c221141989f337e185481
Langleu <langleu90@gmail.com>
2017-11-17 11:04:30 +0100

    add test :shipit:

---
+++ test/services/statistic/index.test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const assert = require('assert');
+const app = require('../../../src/app');
+
+describe('statistic service', function() {
+	it('registered the statistics service', () => {
+		assert.ok(app.service('statistics'));
+	});
+
+	it('should be able to trigger fetch route', () => {
+		assert.ok(app.service('statistics/fetch').find());
+	});
+});
