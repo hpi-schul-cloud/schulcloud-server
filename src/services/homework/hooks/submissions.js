29f55bee683371de8fbe2e03a6dc0b7e3b0f1b96
adrianjost <homepage@hackedit.de>
2017-11-18 17:38:45 +0100

    code cleanup, bugfixing, commenting everything

--- src/services/homework/hooks/submissions.js
+++ src/services/homework/hooks/submissions.js
@@ -15,7 +15,7 @@ const filterApplicableSubmissions = hook => {
             }
             return     c.homeworkId.publicSubmissions                                               // publicSubmissions allowes (everyone can see)
                     || c.homeworkId.teacherId.toString() == hook.params.account.userId.toString()   // or user is teacher
-                    || c.studentId.toString() == hook.params.account.userId.toString()              // or is student (may not needed because all students should be in coWorkers)
+                    || c.studentId.toString() == hook.params.account.userId.toString()              // or is student (only needed for old tasks, in new tasks all users shoudl be in coWorkers)
                     || c.coWorkers.includes(hook.params.account.userId.toString());                 // or in the team                    
         });
         (hook.result.data)?(hook.result.data = data):(hook.result = data);
@@ -23,23 +23,39 @@ const filterApplicableSubmissions = hook => {
 	return Promise.resolve(hook);
 };
 
-const noSubmissionBefore = hook => {
-    if(hook.data.coWorkers){
+const normalizeCoWorkers = hook => {
+    if(hook.data.coWorkers && !(hook.data.grade || hook.data.gradeComment)){  // if student (noGrading) is going to modify coWorkers
+        // make coWorkers an Array if it isn't already
         if(!Array.isArray(hook.data.coWorkers)){
-            hook.data.coWorkers = [hook.data.coWorkers];
+            hook.data.coWorkers = (hook.data.coWorkers)?([hook.data.coWorkers]):[];
         }
+        // add current User if he isn't included already
         if(!hook.data.coWorkers.includes(hook.params.account.userId.toString())){
             hook.data.coWorkers.push(hook.params.account.userId.toString());
         }
-    }else if(!(hook.data.grade || hook.data.gradeComment)){
+        // set current User as studentId (allways contains the user who did the latest changes)
+        if(hook.data.studentId){
+            hook.data.studentId = hook.params.account.userId.toString();
+        }
+    }
+};
+const setCoWorkers = hook => {
+    if(!(hook.data.coWorkers || hook.data.grade || hook.data.gradeComment)){  // if student (no grading) is going to submit without coWorkers set
+        // set current User as coWorker if no coWorker Data submitted
         hook.data.coWorkers = [hook.params.account.userId.toString()];
     }
-    //console.log("studentId");
-    const submissionService = hook.app.service('/submissions');
-    return submissionService.find({query: {
-            homeworkId: hook.data.homeworkId,
-            $populate: ['studentId']
+};
+
+const preventMultipleSubmissions = hook => {
+    if(!(hook.data.grade || hook.data.gradeComment)){ // if student wan't to submit anything
+        // get all the submissions for the homework
+        const submissionService = hook.app.service('/submissions');
+        return submissionService.find({
+            query: {
+                homeworkId: hook.data.homeworkId,
+                $populate: ['studentId']
         }}).then((submissions) => {
+            // check that no one has already submitted for the current User
             let submissionsForMe = submissions.data.filter(raw => { 
                 let e = JSON.parse(JSON.stringify(raw));
                 return (e.coWorkers.includes(hook.params.account.userId.toString()))
@@ -50,23 +66,31 @@ const noSubmissionBefore = hook => {
                   "message": submissionsForMe[0].studentId.firstName + " " + submissionsForMe[0].studentId.lastName + " hat bereits für dich abgegeben!"
                 }));
             }
-
-            let submissionsForCoWorkers = submissions.data.filter(raw => { 
-                let e = JSON.parse(JSON.stringify(raw));
-                (hook.data.coWorkers || [hook.params.account.userId]).forEach(coWorker => {
-                    if((e.coWorkers.includes(coWorker.toString())) 
-                    || (e.studentId._id.toString() == coWorker.toString())){
-                        return true;
+            // check if a coWorker submitted a solution on his own => display names
+            if(hook.data.coWorkers){
+                let toRemove = '';
+                let submissionsForCoWorkers = submissions.data.filter(raw => { 
+                    let e = JSON.parse(JSON.stringify(raw));
+                    for (var i = 0; i < hook.data.coWorkers.length; i++) {
+                        const coWorker = hook.data.coWorkers[i].toString();
+                        if((e.coWorkers.includes(coWorker)
+                            || (e.studentId._id.toString() == coWorker))
+                            && (e.studentId._id.toString() != hook.params.account.userId.toString())){
+                            toRemove += (toRemove == '')?'':', ';
+                            toRemove += e.studentId.firstName + ' ' + e.studentId.lastName;
+                            return true;
+                        }
                     }
+                    return false;
                 });
-                return false;
-            });
-            if(submissionsForCoWorkers.length > 0){
-                return Promise.reject(new errors.Conflict({
-                  "message": "Einer deiner Teammitglieder hat bereits selbst eine Lösung abgegeben! Entferne dieses Mitglied!"
-                }));
+                if(submissionsForCoWorkers.length > 0){
+                    return Promise.reject(new errors.Conflict({
+                      "message": toRemove + ((submissionsForCoWorkers.length == 1)?' hat':' haben') + ' bereits eine Lösung abgegeben! Entferne diese' + ((submissionsForCoWorkers.length == 1)?'s Mitglied!':' Mitglieder!')
+                    }));
+                }
             }
         });
+    }
 };
 
 
@@ -110,14 +134,29 @@ const canGrade = hook => {
     }
 };
 
+const isCoWorker = hook => {
+    // only allow deletion for team Members
+    const submissionService = hook.app.service('/submissions');
+    return submissionService.get(hook.id).then((submission) => {
+        if(JSON.parse(JSON.stringify(submission.coWorkers)).includes(hook.params.account.userId.toString())
+            || submission.studentId == hook.params.account.userId.toString()){
+            return Promise.resolve(hook);
+        }
+        return Promise.reject(new errors.Forbidden());
+    })
+};
+
+/*
+Note: always set the coWorkers
+*/
 exports.before = {
   all: [auth.hooks.authenticate('jwt')],
   find: [globalHooks.mapPaginationQuery.bind(this)],
   get: [],
-  create: [noSubmissionBefore, maxCoWorkers, canGrade],
-  update: [maxCoWorkers, canGrade],
-  patch:  [maxCoWorkers, canGrade],
-  remove: []
+  create: [setCoWorkers, normalizeCoWorkers, preventMultipleSubmissions, maxCoWorkers, canGrade],
+  update: [normalizeCoWorkers, preventMultipleSubmissions, maxCoWorkers, canGrade],
+  patch:  [normalizeCoWorkers, preventMultipleSubmissions, maxCoWorkers, canGrade],
+  remove: [isCoWorker]
 };
 
 exports.after = {
