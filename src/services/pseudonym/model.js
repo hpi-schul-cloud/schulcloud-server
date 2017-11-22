9cb279f67cbe07c63104381697e62a78b798ae06
Raoul Baron <raoul.baron@student.hpi.de>
2017-11-22 15:28:43 +0100

    pseudoservice basis

---
+++ src/services/pseudonym/model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose"),
+	uuid	   = require("uuid/v4"),
+	Schema	   = mongoose.Schema;
+
+//const conn = mongoose.createConnection("mongodb://localhost:27017/schulcloud");
+
+const Pseudonym = new Schema({
+	userId: {type: Schema.Types.ObjectId, required: true},
+	toolId: {type: Schema.Types.ObjectId, required: true},
+	token : {type: String, required: true, default: uuid},
+}, {
+	timestamps: true
+});
+
+module.exports = mongoose.model("Pseudonym", Pseudonym);
