05529e2bbebf12fdb4e0c09d1d0c0ba9e9cad6a2
Raoul Baron <raoul.baron@student.hpi.de>
2017-12-19 11:41:25 +0100

    add missing service

--- src/services/lrs/index.js
+++ src/services/lrs/index.js
@@ -1,7 +1,13 @@
 const hooks = require("./hooks");
+const service = require("./service");
 
 module.exports = function() {
 	const app = this;
 
-	app.use
+	app.use("/lrs", service());
+
+	const lrs = app.service("/lrs");
+
+	lrs.before(hooks.before);
+	lrs.after(hooks.after);
 }
