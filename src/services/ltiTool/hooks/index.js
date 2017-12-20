976811ca154420477ffc0b2f2eb67d3f120cd1aa
dominikglandorf <do.gl@gmx.de>
2017-12-20 19:38:35 +0100

    ltiTools: add pseudonymized url

--- src/services/ltiTool/hooks/index.js
+++ src/services/ltiTool/hooks/index.js
@@ -14,10 +14,33 @@ exports.before = {
   remove: [globalHooks.hasPermission('TOOL_CREATE')]
 };
 
+const replacePseudonym = (hook) => {
+	let userId = hook.params.account.userId;
+	let pseudoService = hook.app.service('pseudonym');
+	if (Array.isArray(hook.result)) { // FIND
+		for (let tool in hook.result) {
+			// TODO: replace placeholder
+		}
+		return hook;
+	} else { // GET
+		let toolId = hook.result._id
+
+		return pseudoService.find({
+			query: {
+				userId: userId,
+				toolId: toolId
+			}
+		}).then((pseudonym) => {
+			hook.result.pseudonymizedUrl = hook.result.url.replace('{PSEUDONYM}', pseudonym.data[0].token);
+			return hook;
+		});
+	}
+}
+
 exports.after = {
   all: [],
   find: [],
-  get: [],
+  get: [replacePseudonym],
   create: [],
   update: [],
   patch: [],
