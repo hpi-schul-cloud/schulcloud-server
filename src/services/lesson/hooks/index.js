046e5d340f7743a6b29d48f2c30479b14bd857ba
Fabien Charlé <fabien.charle@student.hpi.de>
2017-12-21 15:01:49 +0100

    Del Console.logs

--- src/services/lesson/hooks/index.js
+++ src/services/lesson/hooks/index.js
@@ -8,11 +8,8 @@ const lesson = require('../model');
 exports.before = {
 	all: [auth.hooks.authenticate('jwt'), (hook) => {
 		if(hook.data && hook.data.contents) {
-			console.log(hook.data);
 			hook.data.contents = (hook.data.contents || []).map((item) =>{
-				console.log("BEFORE : " , item.user );
 				item.user = item.user || hook.params.account.userId;
-				console.log("AFTER  : " , item.user );
 				switch (item.component) {
 					case 'text':
 						if (item.content && item.content.text) {
