f51bd55a33eb40b77958fd036883b2cde42697d2
Fabien Charlé <fabien.charle@student.hpi.de>
2017-12-21 14:18:20 +0100

    Add User for Content

--- src/services/lesson/hooks/index.js
+++ src/services/lesson/hooks/index.js
@@ -3,11 +3,16 @@
 const stripJs = require('strip-js');
 const hooks = require('feathers-hooks');
 const auth = require('feathers-authentication');
+const lesson = require('../model');
 
 exports.before = {
 	all: [auth.hooks.authenticate('jwt'), (hook) => {
 		if(hook.data && hook.data.contents) {
-			hook.data.contents = (hook.data.contents || []).map((item) => {
+			console.log(hook.data);
+			hook.data.contents = (hook.data.contents || []).map((item) =>{
+				console.log("BEFORE : " , item.user );
+				item.user = item.user || hook.params.account.userId;
+				console.log("AFTER  : " , item.user );
 				switch (item.component) {
 					case 'text':
 						if (item.content && item.content.text) {
@@ -15,11 +20,9 @@ exports.before = {
 						}
 						break;
 				}
-
 				return item;
 			});
 		}
-
 		return hook;
 	}],
 	find: [],
