63a0d2c23822be13b5841e5e076aa6e614e42683
dominikglandorf <do.gl@gmx.de>
2018-01-30 12:14:42 +0100

    add user groups endpoint

--- src/services/provider/index.js
+++ src/services/provider/index.js
@@ -3,9 +3,8 @@
 module.exports = function() {
 	const app = this;
 
-	app.use('/provider/:pId/users/:token/metadata', {
+	app.use('/provider/users/:token/metadata', {
 		find(params) {
-			console.log(params);
 			const pseudoService = app.service("pseudonym");
 			return pseudoService.find({
 				query: {
@@ -34,6 +33,37 @@ module.exports = function() {
 						'type': user.roles[0].name,
 						'school_id': user.schoolId
 					});
+				});
+			});
+		}
+	});
+
+	app.use('/provider/users/:token/groups', {
+		find(params) {
+			console.log(params);
+			const pseudoService = app.service("pseudonym");
+			return pseudoService.find({
+				query: {
+					token: params.token
+				}
+			}).then(pseudonym => {
+				console.log(pseudonym);
+				if (!pseudonym.data[0]) {
+					return Promise.reject("User not found by token");
+				}
+				const toolId = (pseudonym.data[0].toolId);
+				const courseService = app.service("courses");
+				return courseService.find({
+					query: {
+						ltiToolIds: toolId
+					}
+				}).then(courses => {
+					console.log();
+					return Promise.resolve(courses.data.map(course => ({
+						id: course._id,
+						name: course.name,
+						studentCount: course.userIds.length
+					})));
 				});
 			});
 		}
