b67c6f34c9b4da6f41abcae6188b2a611c803054
Jan Renz <jan.renz@gmail.com>
2018-03-20 18:03:10 +0100

    Added tests

--- test/services/helpdesk/index.test.js
+++ test/services/helpdesk/index.test.js
@@ -36,5 +36,39 @@ describe('helpdesk service', function() {
 	it('registered the helpdesk service', () => {
 		assert.ok(helpdeskService);
 	});
+
+	it('POST /helpdesk with valid data', () => {
+
+		let postBody = {
+			_id: '5836bb5664582c35df3bc215',
+			subject: 'Dies ist ein Titel',
+			currentTarget: 'Dies ist der CurrentState',
+			targetState: 'Dies ist der TargetState',
+			category: 'dashboard',
+			schoolId: '5836bb5664582c35df3bc000'
+		};
+
+		return helpdeskService.create(postBody, { payload: {userId: '0000d213816abba584714c0a'}})
+			.then(result => {
+				expect(result.statusCode).to.equal(200);
+				expect(result.data.id).to.equal('59199dbe8d4be221143cc866');
+			});
+	});
+
+	it('POST /helpdesk with invalid data', () => {
+
+		let postBody = {
+			_id: '5836bb5664582c35df3bc215',
+			subject: 'Dies ist ein Titel',
+			currentTarget: 'Dies ist der CurrentState',
+			targetState: 'Dies ist der TargetState',
+			category: 'dashboard'
+		};
+
+		return helpdeskService.create(postBody, { payload: {userId: '0000d213816abba584714c0a'}})
+			.then(result => {
+				expect(result.statusCode).to.equal(500);
+			});
+	});
 });
 
