eced855fbebf1516580d7673fcfba36f0453804f
Thomas <thomas.feldtkeller@student.hpi.de>
2018-04-11 10:56:37 +0200

    force user mailadress to be unique

--- src/services/user/model.js
+++ src/services/user/model.js
@@ -21,7 +21,7 @@ const userSchema = new Schema({
 	timestamps: true
 });
 
-userSchema.index({email: 1}, {unique: true})
+userSchema.index({email: 1}, {unique: true});
 
 userSchema.methods.getPermissions = function() {
 	const roleModel = require('../role/model');
