4a0da3b49d5e0b8dd5349782e7fa995ff19fa7fc
adrianjost <homepage@hackedit.de>
2018-04-18 20:17:03 +0200

    first try of implementing requested changes
    todo: logic in copy-service not working :cry:

--- src/services/homework/index.js
+++ src/services/homework/index.js
@@ -1,60 +1,11 @@
 'use strict';
+const copyService = require('./copy-service');
+const modelService = require('./model-service');
 
-const service = require('feathers-mongoose');
-const {homeworkModel, submissionModel, commentModel} = require('./model');
-const hooks = require('./hooks');
-const homeworkCopyHooks = require('./hooks/homework-copy');
-const submissionHooks = require('./hooks/submissions');
-const commentHooks = require('./hooks/comments');
-
-module.exports = function() {
+module.exports = function () {
 	const app = this;
-
-	/* Homework model */
-	app.use('/homework', service({
-		Model: homeworkModel,
-		paginate: {
-			default: 100000,
-			max: 100000
-		}
-	}));
-	const hwService = app.service('/homework');
-	hwService.before(hooks.before);
-	hwService.after(hooks.after);
-
-	/* Homework model */
-	app.use('/homework/copy', service({
-		Model: homeworkModel,
-		paginate: {
-			default: 100000,
-			max: 100000
-		}
-	}));
-	const hwCopyService = app.service('/homework/copy');
-	hwCopyService.before(homeworkCopyHooks.before);
-	hwCopyService.after(homeworkCopyHooks.after);
-
-	/* Submission model */
-	app.use('/submissions', service({
-		Model: submissionModel,
-		paginate: {
-			default: 500,
-			max: 5000
-		}
-	}));
-	const submissionService = app.service('/submissions');
-	submissionService.before(submissionHooks.before);
-	submissionService.after(submissionHooks.after);
-
-	/* Comment model */
-	app.use('/comments', service({
-		Model: commentModel,
-		paginate: {
-			default: 500,
-			max: 5000
-		}
-	}));
-	const commentService = app.service('/comments');
-	commentService.before(commentHooks.before);
-	commentService.after(commentHooks.after);
+	// Setup copy services
+	app.configure(copyService);
+	// Setup model services
+	app.configure(modelService);
 };
