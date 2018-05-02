8a1fea9530b0d32b11ced7bb94f11bca5bac2af5
Katrin Klein <katrin_klein@icloud.com>
2018-05-02 16:26:40 +0200

    belongs to last commit

--- src/services/lesson/index.js
+++ src/services/lesson/index.js
@@ -3,6 +3,7 @@
 const service = require('feathers-mongoose');
 const lesson = require('./model');
 const hooks = require('./hooks');
+const swaggerDocs = require("./docs/");
 
 module.exports = function () {
 	const app = this;
@@ -15,12 +16,20 @@ module.exports = function () {
 		},
 		lean: true
 	};
+	
+	var lessonService =  service(options);
+	
+	lessonService.docs 	 = swaggerDocs.lessonService;
 
 	// Initialize our service with any options it requires
-	app.use('/lessons', service(options));
+	app.use('/lessons', lessonService);
 
 	// Return all lesson.contets which have component = query.type And User = query.user or null
-	app.use('/lessons/contents/:type/',{
+	class lessonContent{
+		constructor(){
+			this.docs = swaggerDocs.lessonService.contentService;
+		}
+
 		find(params){
 			return lesson.aggregate([
 				{ $unwind :'$contents'},
@@ -29,7 +38,9 @@ module.exports = function () {
 				{ $project : { _id: "$contents._id", content : "$contents.content"} }
 				]).exec();
 		}
-	});
+	};
+
+	app.use('/lessons/contents/:type/', new lessonContent());
 
 	// Get our initialize service to that we can bind hooks
 	const systemService = app.service('/lessons');
