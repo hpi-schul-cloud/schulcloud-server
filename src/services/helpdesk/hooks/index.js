600b778d9c9ccb04ab5f0fac42bf128127381073
Katrin Klein <katrin_klein@icloud.com>
2018-05-23 12:47:45 +0200

    sendEmail now gets an infotext
    I made the var infoText into a function that creates the infoText when given the missing parameters. that function is called in the afterhooks create and returns a correct infotext. that infotext is then given to sendEmail.

--- src/services/helpdesk/hooks/index.js
+++ src/services/helpdesk/hooks/index.js
@@ -5,13 +5,15 @@ const hooks = require('feathers-hooks');
 const auth = require('feathers-authentication');
 const restrictToCurrentSchool = globalHooks.ifNotLocal(globalHooks.restrictToCurrentSchool);
 
-var infoText = "Ein neues Problem wurde gemeldet." + "\n"
+function createinfoText(user, category, subject){
+	return "Ein neues Problem wurde gemeldet." + "\n"
 	//TODO: try to get the submitted data on POST (wont work on find/get I think) and implement
-	+ "User: [get submitted data] \n"
-	+ "Kategorie: [hook.result.data?] \n"
-	+ "Betreff: [hook.result.data?] \n"
+	+ "User: " + user + "\n"
+	+ "Kategorie: "+ category + "\n"
+	+ "Betreff: " + subject + "\n"
 	+ "Schauen Sie für weitere Details und zur Bearbeitung bitte in das Helpdesk der Schul-Cloud.\n\n"
 	+ "Mit Freundlichen Grüßen\nIhr Schul-Cloud Team";
+};
 
 const sendHelpdeskEmail = hook => {
 	let infoText = "Ein neues Problem wurde gemeldet." + "\n"
@@ -44,23 +46,17 @@ exports.before = {
 //TODO: call from local function sendHelpdeskEmail, not directly, not working
 exports.after = {
 	all: [],
-	find: [ globalHooks.sendEmail({
+	find: [],
+	get: [],
+	create: [ hook => {
+		globalHooks.sendEmail({
 			"subject":"Ein Problem wurde gemeldet.",
 			"roles":["helpdesk", "administrator"],
 			"content": {
-				"text": infoText
+				"text": createinfoText(hook.data.userId, hook.data.category, hook.data.subject) //TODO: convert Userid into user
 			}
-		})
+		})}
 	],
-	get: [ globalHooks.sendEmail({
-			"subject":"Ein Problem wurde gemeldet.",
-			"roles":["helpdesk", "administrator"],
-			"content": {
-				"text": infoText
-			}
-		})
-	],
-	create: [],
 	update: [],
 	patch: [],
 	remove: []
