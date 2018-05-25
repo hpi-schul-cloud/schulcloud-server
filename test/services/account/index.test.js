f45d2288fbbc0717f1a6692ed731217bbf2cb566
Langleu <langleu90@gmail.com>
2018-05-25 13:13:05 +0200

    add tests

--- test/services/account/index.test.js
+++ test/services/account/index.test.js
@@ -7,4 +7,28 @@ describe('account service', function () {
 	it('registered the accounts service', () => {
 		assert.ok(app.service('accounts'));
 	});
+
+	it('the account already exists', () => {
+		const accountService = app.service('/accounts');
+
+		let accountObject = {
+			username: "max" + Date.now() + "@mHuEsLtIeXrmann.de",
+			password: "ca4t9fsfr3dsd",
+			userId: "0000d213816abba584714c0a"
+		};
+
+		return accountService.create(accountObject)
+			.catch(exception => {
+				assert.equal(exception.message, 'Der Account existiert bereits!');
+			});
+	});
+
+	it('not able to access whole find', () => {
+		const accountService = app.service('/accounts');
+
+		return accountService.find()
+			.catch(exception => {
+				assert.equal(exception.message, 'Cannot read property \'username\' of undefined');
+			});
+	});
 });
