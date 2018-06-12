a79c6d3ce7454475591032eb107aa0dde512fe1e
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2018-06-12 09:04:22 +0200

    [ci-skip] tests

--- test/services/wopi/index.test.js
+++ test/services/wopi/index.test.js
@@ -3,26 +3,50 @@ const assert = require('assert');
 const app = require('../../../src/app');
 const fileModelService = app.service('files');
 const wopiFileInfoService = app.service('wopi/files/:fileId');
+const mockAws = require('../fileStorage/aws/s3.mock');
+const mockery = require('mockery');
 
 describe('wopi service', function () {
+
+	const testUserId = "5937c000a5896d0515fbf270";
    
-  const mockFile = {
+  const testFile = {
     "_id" : "597860e9667a0659ed0b0006",
-    "key" : "users/5937c000a5896d0515fbf270/Test.docx",
-    "path" : "users/5937c000a5896d0515fbf270/",
+    "key" : `users/${testUserId}/Test.docx`,
+    "path" : `users/${testUserId}/`,
     "name" : "Test.docx",
     "size" : 11348,
     "flatFileName" : "1501061352639-Test.docx",
     "permissions" : [],
     "__v" : 0
+	};
+	
+	const testFile2 = {
+    "_id" : "597860e9667a0659ed0b1006",
+    "key" : `users/${testUserId}/Test1.docx`,
+    "path" : `users/${testUserId}/`,
+    "name" : "Test1.docx",
+    "size" : 11348,
+    "flatFileName" : "1501161352639-Test1.docx",
+    "permissions" : [],
+    "__v" : 0
   };
 
-  const exampleAccessToken = "TEST";
+  const testAccessToken = "TEST";
 
 	before(function (done) {
 		this.timeout(10000);
-		fileModelService.create(mockFile)
+		fileModelService.create(testFile)
 			.then(_ => {
+				// Enable mockery to mock objects
+				mockery.enable({
+					warnOnUnregistered: false
+				});
+
+				mockery.registerMock('aws-sdk', mockAws);
+
+				delete require.cache[require.resolve('../../../src/services/fileStorage/strategies/awsS3')];
+
 				done();
 			});
 	});
@@ -30,8 +54,10 @@ describe('wopi service', function () {
 
 	after(function(done) {
     this.timeout(10000);
-		fileModelService.remove(mockFile._id)
+		fileModelService.remove(testFile._id)
 			.then(_ => {
+				mockery.deregisterAll();
+				mockery.disable();
 				done();
 			});
 	});
@@ -44,15 +70,30 @@ describe('wopi service', function () {
 		assert.ok(app.service('wopi/files/:fileId/contents'));
   });
 
-	it('GET /wopi/files/:fileId', (done) => {
+	it('GET /wopi/files/:fileId', done => {
 		wopiFileInfoService.find({
-      query: {access_token: exampleAccessToken},
-      fileId: mockFile._id,
+      query: {access_token: testAccessToken},
+      fileId: testFile._id,
       account: {userId: "5937c000a5896d0515fbf270"} //same as above
     }).then(result => {
-        assert.equal(result['BaseFileName'], mockFile.name);
-        assert.equal(result['Size'], mockFile.size);
+        assert.equal(result['BaseFileName'], testFile.name);
+        assert.equal(result['Size'], testFile.size);
 				done();
 			});
+	});
+
+	it('POST /wopi/files:fileId Action Delete', done => {
+		let headers = {};
+		headers['X-WOPI-OVERRIDE'] = 'DELETE';
+		headers['authorization'] = testAccessToken;
+		fileModelService.create(testFile2).then(_ => {
+			wopiFileInfoService.create({
+				fileId: testFile2._id,
+				account: {userId: testUserId}
+			}, {headers: headers}).then(result => {
+				console.log(result);
+				done();
+			});
+		}).catch(e => console.log(e));
 	});
 });
