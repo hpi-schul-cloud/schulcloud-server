c7926f4f0d7817f968c056c0f50c38f19bf595ef
schurigh <hannes.schurig@hpi.de>
2018-07-11 17:50:46 +0200

    new gender 'noinfo' and fixes

--- src/services/user/hooks/registrationPins.js
+++ src/services/user/hooks/registrationPins.js
@@ -8,7 +8,6 @@ const _ = require('lodash');
 const removeOldPin = (hook) => {
 	return hook.app.service('registrationPins').find({email: hook.data.email})
 		.then(pins => {
-			let a = 1;
 			if (pins.total > 0) {
 				return hook.app.service('registrationPins').remove(pins.data[0]._id)
 					.then(response => {
@@ -20,7 +19,7 @@ const removeOldPin = (hook) => {
 };
 
 const generatePin = (hook) => {
-	let pin = Math.floor((Math.random() * 9999)+1000);
+	let pin = Math.floor((Math.random() * 8999)+1000);
 	hook.data.pin = pin.toString();
 	return Promise.resolve(hook);
 };
