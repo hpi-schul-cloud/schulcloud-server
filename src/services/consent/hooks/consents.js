5bd7cbe71b77b573ed675eca726da16ac46e5a7b
Langleu <langleu90@gmail.com>
2018-07-17 15:11:31 +0200

    add redirect direction

--- src/services/consent/hooks/consents.js
+++ src/services/consent/hooks/consents.js
@@ -14,8 +14,17 @@ exports.before = {
 	remove: []
 };
 
+const redirectDic = {
+	u14: '/firstLogin/U14/',
+	u18: '/firstLogin/14_17/',
+	ue18: '/firstLogin/UE18/',
+	normal: '/dashboard/',
+	err: '/consentError'
+};
+
 const accessCheck = (hook) => {
 	let access = true;
+	let redirect = redirectDic['ue18'];
 	let data = hook.result.data || hook.result;
     data = (Array.isArray(data))?(data):([data]);
 
@@ -23,32 +32,40 @@ const accessCheck = (hook) => {
 		.then(user => {
 			if (!user.birthday) {
 				access = false;
+				redirect = redirectDic['err'];
 				return Promise.resolve;
 			}
 			let age = user.age;
 
 			if (age < 18) {
-				let parentConsent = data[0].parentConsents[0];
+				redirect = redirectDic['u14'];
+				let parentConsent = data[0].parentConsents[0] || {};
 				//check parent consents
 				if (!(parentConsent.privacyConsent && parentConsent.termsOfUseConsent &&
 					parentConsent.thirdPartyConsent && parentConsent.researchConsent)) {
 						access = false;
+						redirect = redirectDic['err'];
 						return Promise.resolve();
 					}
 			}
 			if (age > 13) {
+				redirect = redirectDic['u18'];
 				//check user consents
-				let userConsent = data[0].userConsent;
+				let userConsent = data[0].userConsent || {};
 				if (!(userConsent.privacyConsent && userConsent.termsOfUseConsent &&
-					userConsent.thirdPartyConsent && userConsent.userConsent)) {
+					userConsent.thirdPartyConsent && userConsent.researchConsent)) {
 						access = false;
+						redirect = redirectDic['err'];
 						return Promise.resolve();
 					}
 			}
-
+			if (age > 17)
+				redirect = redirectDic['ue18'];
+			if ((user.preferences || {}).firstLogin)
+				redirect = redirectDic['normal'];
 		})
 		.then(() => {
-            (hook.result.data)?(hook.result.data[0].access = access):(hook.result.access = access);
+            (hook.result.data) ? (hook.result.data[0].access = access, hook.result.data[0].redirect = redirect) : (hook.result.access = access, hook.result.redirect = redirect);
             return Promise.resolve(hook);
 		})
 		.catch(err => {return Promise.reject(err);});
