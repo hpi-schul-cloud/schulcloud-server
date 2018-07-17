da6cd2d61fdbcc3dc3ac1d28aa84197667f3a836
schurigh <hannes.schurig@hpi.de>
2018-07-17 20:24:24 +0200

    new: send pin verification mail

--- src/services/user/hooks/registrationPins.js
+++ src/services/user/hooks/registrationPins.js
@@ -4,6 +4,7 @@ const globalHooks = require('../../../hooks');
 const hooks = require('feathers-hooks');
 const auth = require('feathers-authentication');
 const _ = require('lodash');
+let pin = null;
 
 const removeOldPin = (hook) => {
 	return hook.app.service('registrationPins').find({email: hook.data.email})
@@ -19,8 +20,33 @@ const removeOldPin = (hook) => {
 };
 
 const generatePin = (hook) => {
-	let pin = Math.floor((Math.random() * 8999)+1000);
+	pin = Math.floor((Math.random() * 8999)+1000);
 	hook.data.pin = pin.toString();
+	return Promise.resolve(hook);
+};
+
+function createinfoHtml(){
+	return "HTML: Vielen Dank, dass Sie Ihrem Kind durch Ihr Einverständnis die Nutzung der HPI Schul-Cloud ermöglichen.\n" +
+		"Bitte geben Sie folgenden Code ein, wenn Sie danach gefragt werden, um die Registrierung abzuschließen.\n\n" +
+		"<span style='font-size:200%;font-weight:bold;'>PIN: " + pin + "</span>\n\n" +
+		"Mit Freundlichen Grüßen\nIhr Schul-Cloud Team";
+}
+function createinfoText(){
+	return "Vielen Dank, dass Sie Ihrem Kind durch Ihr Einverständnis die Nutzung der HPI Schul-Cloud ermöglichen.\n" +
+		"Bitte geben Sie folgenden Code ein, wenn Sie danach gefragt werden, um die Registrierung abzuschließen.\n\n" +
+		"PIN: " + pin + "\n\n" +
+		"Mit Freundlichen Grüßen\nIhr Schul-Cloud Team";
+}
+
+const mailPin = (hook) => {
+	globalHooks.sendEmail(hook, {
+		"subject": "Schul-Cloud: Registrierung mit PIN verifizieren",
+		"emails": (hook.data||{}).email,
+		"content": {
+			"html": createinfoHtml(),
+			"text": createinfoText()
+		}
+	});
 	return Promise.resolve(hook);
 };
 
@@ -28,7 +54,7 @@ exports.before = {
 	all: [],
 	find: [],
 	get: [],
-	create: [removeOldPin,generatePin],
+	create: [removeOldPin, generatePin, mailPin],
 	update: [],
 	patch: [],
 	remove: []
