2a41b392ac3fc7fe84ad00dcc4b19fa869016a85
dominikglandorf <do.gl@gmx.de>
2018-08-07 15:54:59 +0200

    fix pseudonym filtering

--- src/services/pseudonym/hooks/index.js
+++ src/services/pseudonym/hooks/index.js
@@ -55,7 +55,7 @@ const filterValidUsers = context => {
 		}
 		validUserIds = validUserIds.map(element => element.toString());
 		context.result.data = context.result.data.filter(pseudonym =>
-			validUserIds.includes(pseudonym.userId._id.toString())
+			validUserIds.includes(pseudonym.userId.toString())
 		);
 		return context;
 	});
