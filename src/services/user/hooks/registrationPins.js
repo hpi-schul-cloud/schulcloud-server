016d3490f283439c25b02065fcd820991df77553
schurigh <hannes.schurig@hpi.de>
2018-08-07 13:51:40 +0200

    fix: mailtext in js template, remove html

--- src/services/user/hooks/registrationPins.js
+++ src/services/user/hooks/registrationPins.js
@@ -25,26 +25,20 @@ const generatePin = (hook) => {
 	return Promise.resolve(hook);
 };
 
-// implement html later
-function createinfoHtml() {
-	return "Vielen Dank, dass Sie Ihrem Kind durch Ihr Einverständnis die Nutzung der HPI Schul-Cloud ermöglichen.\n" +
-		"Bitte geben Sie folgenden Code ein, wenn Sie danach gefragt werden, um die Registrierung abzuschließen.\n\n" +
-		"<span style='font-size:200%;font-weight:bold;'>PIN: " + pin + "</span>\n\n" +
-		"Mit Freundlichen Grüßen\nIhr Schul-Cloud Team";
-}
-
 function createinfoText(hook) {
-	let text = "";
+	let text;
 	if (hook.data.byParent === true || hook.data.byParent === "true") {
-		text = "Vielen Dank, dass Sie Ihrem Kind durch Ihr Einverständnis die Nutzung der HPI Schul-Cloud ermöglichen.\n" +
-			"Bitte geben Sie folgenden Code ein, wenn Sie danach gefragt werden, um die Registrierung abzuschließen.\n\n" +
-			"PIN: " + pin + "\n\n" +
-			"Mit Freundlichen Grüßen\nIhr Schul-Cloud Team";
+		text = `Vielen Dank, dass Sie Ihrem Kind durch Ihr Einverständnis die Nutzung der HPI Schul-Cloud ermöglichen.
+Bitte geben Sie folgenden Code ein, wenn Sie danach gefragt werden, um die Registrierung abzuschließen.
+\nPIN: ${pin}
+\nMit Freundlichen Grüßen
+Ihr Schul-Cloud Team`;
 	} else {
-		text = "Vielen Dank, dass du die HPI Schul-Cloud nutzen möchtest.\n" +
-			"Bitte gib folgenden Code ein, wenn du danach gefragt wirst, um die Registrierung abzuschließen.\n\n" +
-			"PIN: " + pin + "\n\n" +
-			"Mit Freundlichen Grüßen\nIhr Schul-Cloud Team";
+		text = `Vielen Dank, dass du die HPI Schul-Cloud nutzen möchtest.
+Bitte gib folgenden Code ein, wenn du danach gefragt wirst, um die Registrierung abzuschließen.
+\nPIN: ${pin}
+\nMit Freundlichen Grüßen
+Ihr Schul-Cloud Team`;
 	}
 	return text;
 }
@@ -55,6 +49,7 @@ const mailPin = (hook) => {
 		"emails": (hook.data||{}).email,
 		"content": {
 			"text": createinfoText(hook)
+			// TODO: implement html mails later
 		}
 	});
 	return Promise.resolve(hook);
