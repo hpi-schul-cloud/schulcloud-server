5b2436a35a948e9791b5b5fe802e681f2e8807f1
schurigh <hannes.schurig@hpi.de>
2018-09-03 17:54:33 +0200

    added /registrationLink endpoint for reglink creation

--- src/services/link/index.js
+++ src/services/link/index.js
@@ -3,6 +3,7 @@
 const service = require('feathers-mongoose');
 const link = require('./link-model');
 const hooks = require('./hooks');
+const hashService = require('../helpers/hash');
 
 module.exports = function () {
 	const app = this;
@@ -28,9 +29,44 @@ module.exports = function () {
 			next();
 		}
 	}
+	
+	class registrationLinkService {
+		constructor(options) {
+			this.options = options || {};
+			this.docs = {};
+		}
+		
+		async create(data, params) {
+			let linkData = {};
+			if (data.toHash) {
+				await app.service('hash').create(data).then(generatedHash => {
+					linkData.hash = generatedHash;
+				}).catch(err => {
+					return Promise.reject(new Error('Fehler beim Generieren des Hashes.'));
+				});
+			}
+			
+			// base link
+			if (data.role === 'teacher') linkData.link = `${(data.host || process.env.HOST)}/registration/${data.schoolId}/byemployee`;
+			if (data.role === 'student') linkData.link = `${(data.host || process.env.HOST)}/registration/${data.schoolId}`;
+			if (linkData.hash) linkData.link += `?id=${linkData.hash}`;
+			
+			// generate short url
+			await app.service('link').create({target: linkData.link}).then(generatedShortLink => {
+				linkData.shortLink = `${(data.host || process.env.HOST)}/link/${generatedShortLink._id}`;
+			}).catch(err => {
+				return Promise.reject(new Error('Fehler beim Erstellen des Kurzlinks.'));
+			});
+			
+			return linkData;
+		}
+	}
 
 	// Initialize our service with any options it requires
 	app.use('/link', redirectToTarget, linkService);
+	
+	// generate registration link with optional user hash
+	app.use('/registrationlink', new registrationLinkService());
 
 	// Get our initialize service to that we can bind hooks
 	linkService = app.service('/link');
