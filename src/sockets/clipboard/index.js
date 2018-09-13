f1f3ad825ca555ccbf0bcead76b2a4da73407631
Michael Janke <micha@jankeweb.de>
2018-09-13 17:38:40 +0200

    new socket protocol

--- src/sockets/clipboard/index.js
+++ src/sockets/clipboard/index.js
@@ -7,7 +7,7 @@ const path = require("path");
 
 module.exports = function () {
 	const app = this;
-	var images = {};
+	var courses = {};
 	app.use(siofu.router);
 	app.configure(socketio((io) => {
 
@@ -15,38 +15,68 @@ module.exports = function () {
 		clipboardWs.on('connection', (socket) => {
 
 			let courseId = socket.request._query.courseId;
-			if(!courseId) return;
-			let user = {};
-			app.service('users').get(socket.client.userId).then((result) => { user = result;});
 
+			if(!courseId) return;
 			logger.debug("someone connected to room " + courseId);
 			socket.join(courseId);
 
-			socket.on("refreshClipboard", () => {
-				socket.emit("clipboardState", images[courseId] || []);
+			if(!courses[courseId]) courses[courseId] = {
+				media: [],
+				users: {},
+				board: {},
+				lastId: 0
+			};
+			let course = courses[courseId];
+			socket.emit("clipboardState", course); //on connect send inital state
+
+			let user = {};
+			app.service('users').get(socket.client.userId).then((result) => { 
+				user = result;
+				course.users[user._id] = user;
+				socket.nsp.to(courseId).emit("clipboardState", course);
 			});
 
-			socket.on("pushToClipboard", (media) => {
-				logger.debug(media);
-				clipboardWs.to(courseId).emit("pushToClipboard", media);
+			socket.on("ADD_MEDIA", (media) => {
+				media.id = ++course.lastId;
+				course.media.push(media);
+				socket.nsp.to(courseId).emit("clipboardState", course);
 			});
 
-			var uploader = new siofu();
+			socket.on("ADD_TO_BOARD", (media) => {
+				course.board[media.id] = media;
+				socket.nsp.to(courseId).emit("clipboardState", course);
+			});
+
+			socket.on("UPDATE_MEDIA_ON_BOARD", (media) => {
+				course.board[media.id] = media;
+				socket.nsp.to(courseId).emit("clipboardState", course);
+			});
+
+			initUploadSocket(socket, (filename) => {
+				let file = {
+					file: filename,
+					sender: user && (user.firstName + " " + user.lastName),
+					type: "image",
+					id: ++course.lastId,
+				};
+				course.media.push(file);
+				socket.nsp.to(courseId).emit("clipboardState", course);
+			});
+		});
+	}));
+
+	function initUploadSocket(socket, onUpload) {
+		var uploader = new siofu();
 			uploader.dir = "uploads";
 			uploader.listen(socket);
 			uploader.on("start", function(event){
 				logger.debug("upload started " + event.file.name);
 			});
+
 			uploader.on("saved", function(event){
-				let filename = path.basename(event.file.pathName);
-				let file = {
-					file: filename,
-					sender: user && (user.firstName + " " + user.lastName)
-				};
-				clipboardWs.to(courseId).emit('clipboardUpdate', file);
-				images[courseId] = images[courseId] || [];
-				images[courseId].push(file);
+				onUpload(path.basename(event.file.pathName));
 			});
-		});
-	}));
+
+		return uploader;
+	}
 };
