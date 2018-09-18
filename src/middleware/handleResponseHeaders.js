ccd539f6643647ec9c1496270978cca4bd08f91d
Langleu <langleu90@gmail.com>
2018-09-18 08:28:19 +0200

    correctly respond to PutFile

--- src/middleware/handleResponseHeaders.js
+++ src/middleware/handleResponseHeaders.js
@@ -5,11 +5,15 @@
  */
 module.exports = (req, res, next) => {
 
-  (res.data.headerPipes || []).forEach(h => {
-    if (h.key) res.header(h.key, h.value);
-  });
+	(res.data.headerPipes || []).forEach(h => {
+		if (h.key) res.header(h.key, h.value);
+	});
 
-  delete res.data.headerPipes;
+	// wopi doesn't have a 201 statusCode which would cause problems
+	if (res.statusCode === 201)
+		res.statusCode = 200;
 
-  next();
+	delete res.data.headerPipes;
+
+	next();
 };
