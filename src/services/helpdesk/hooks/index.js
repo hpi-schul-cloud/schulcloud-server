f55e39d117218dbc4815d456390f84f0633bae16
MuPiCo <katrin_klein@icloud.com>
2018-10-02 12:10:56 +0200

    first steps, more or less copy paste from client into server

--- src/services/helpdesk/hooks/index.js
+++ src/services/helpdesk/hooks/index.js
@@ -15,6 +15,14 @@ function createinfoText(user, category, subject, cloud){
 	+ "Deine " + cloud;
 }
 
+function createfeedbackText(user, category, email, schoolName, text){
+	return "User: " + user + "\n"
+	+ "E-Mail: " + email + "\n"
+	+ "Schule: " + schoolName + "\n"
+	+ "Bereich ausgewählt: " + category + "\n"
+	+ "User schrieb folgendes: \n" + text
+}
+
 exports.before = {
 	all: [auth.hooks.authenticate('jwt')],
 	find: [globalHooks.hasPermission('HELPDESK_VIEW')],
@@ -25,20 +33,59 @@ exports.before = {
 	remove: [globalHooks.hasPermission('HELPDESK_CREATE'),globalHooks.permitGroupOperation, globalHooks.ifNotLocal(globalHooks.checkSchoolOwnership)]
 };
 
-const sendEmail = () => {
+const feedback = () => {
 	return hook=>{
 		const data=hook.data||{};
-		globalHooks.sendEmail(hook, {
-			"subject": "Ein Problem wurde gemeldet.",
-			"roles": ["helpdesk", "administrator"],
-			"content": {
-				"text": createinfoText(
-					(hook.params.account||{}).username||"nouser",
-					data.category||"nocategory",
-					data.subject||"nosubject",
-					data.cloud||"Schul-Cloud")
-			}
-		});
+		if (data.type === "problem"){ // case: admin
+			globalHooks.sendEmail(hook, {
+				"subject": "Ein Problem wurde gemeldet.",
+				"roles": ["helpdesk", "administrator"],
+				"content": {
+					"text": createinfoText(
+						(hook.params.account||{}).username||"nouser",
+						data.category||"nocategory",
+						data.subject||"nosubject",
+						data.cloud||"Schul-Cloud")
+				}
+			});
+			let userservice = hook.app.service('/users');
+			userservice.find({query: {roles: ['helpdesk']}})
+			.then(userdata => {
+				userdata.data.map(user => {
+					if (process.env.NOTIFICATION_SERVICE_ENABLED) {
+						let notificationservice = hook.app.service('/notification/messages');
+						notificationservice.post({
+							json: {
+								"title": "Ein neues Problem wurde gemeldet.",
+								"body": "",
+								"token": user._id,
+								"priority": "high",
+								"action": `${(req.headers.origin || process.env.HOST)}/administration/helpdesk`,
+								"scopeIds": [
+									user._id
+								]
+							}
+						});
+					}
+				});
+			})
+			.catch(error => {
+				let e = error;
+			});
+		} else { // case: schulcloud feedback
+			globalHooks.sendEmail(hook, {
+				"subject": data.subject||"nosubject",
+				"emails": ["ticketsystem@schul-cloud.org"],
+				"content": {
+					"text": createfeedbackText(
+						(hook.params.account||{}).username||"nouser",
+						data.category||"nocategory",
+						data.email||"noemail",
+						data.schoolName||"noschoolname",
+						data.text||"notext")
+				}
+			});
+		}
 		return Promise.resolve(hook);
 	}
  }
@@ -47,7 +94,7 @@ exports.after = {
 	all: [],
 	find: [],
 	get: [],
-	create: [sendEmail()],
+	create: [feedback()],
 	update: [],
 	patch: [],
 	remove: []
