6494b0a0496569acb98fbe3bb08b7aaab2ce413d
Maximilian Grundke <maximiliangrundke@googlemail.com>
2018-10-04 16:44:00 +0200

    Update school model to reference ldap config

--- src/services/school/model.js
+++ src/services/school/model.js
@@ -15,6 +15,8 @@ const schoolSchema = new Schema({
 	address: {type: Object},
 	fileStorageType: {type: String, enum: fileStorageTypes},
 	systems: [{type: Schema.Types.ObjectId, ref: 'system'}],
+	ldapConfig: {type: Schema.Types.ObjectId, ref: 'ldapConfig'},
+	ldapSchoolIdentifier: {type: String},
 	federalState: {type: Schema.Types.ObjectId, ref: 'federalstate'},
 	createdAt: {type: Date, 'default': Date.now},
 	updatedAt: {type: Date, 'default': Date.now},
