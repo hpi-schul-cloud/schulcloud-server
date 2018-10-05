eae8bbfe6f48359a0d831163a92ae5fda3d2c80a
Thomas <thomas.feldtkeller@student.hpi.de>
2018-10-05 15:01:41 +0200

    retrieve schools and users seperately.

--- src/services/helpers/sync.js
+++ src/services/helpers/sync.js
@@ -19,16 +19,14 @@ module.exports = function (app) {
 		}
 
 		_syncFromLdap(app) {
-			let configPromise = app.service('ldapConfigs').get("5bb73935de31231cefdad572");
-			let schoolPromise = app.service('schools').get("0000d186816abba584714c5f");
-			return Promise.all([configPromise, schoolPromise])
-				.then(([config, school]) => {
-					config = Object.assign(config, {ou: school.ldapSchoolIdentifier});
-					return this._getDataFromLdap(config);
+			return app.service('ldapConfigs').get("5bb73935de31231cefdad572")
+				.then(config => {
+					return this._getSchoolDataFromLdap(config);
 				});
 		}
 
-		_getDataFromLdap(idmConfig) {
+		_getSchoolDataFromLdap(idmConfig) {
+			//ToDo: move into ldap service
 			const client = ldap.createClient({
 				url: idmConfig.url
 			});
@@ -45,20 +43,18 @@ module.exports = function (app) {
 				});
 			}).then((client) => {
 				const opts = {
-					filter: 'univentionObjectType=users/user',
+					filter: 'univentionObjectType=container/ou',
 					scope: 'sub',
 					attributes: []
 				};
 
 				return new Promise((resolve, reject) => {
 					let users = [];
-					client.search(`cn=users,ou=${idmConfig.ou},${idmConfig.rootPath}`, opts, function (err, res) {
+					//client.search(`cn=users,ou=${idmConfig.ou},${idmConfig.rootPath}`, opts, function (err, res) {
+					client.search(`${idmConfig.rootPath}`, opts, function (err, res) {
 						res.on('searchEntry', function (entry) {
 							users.push(entry.object);
 						});
-						res.on('searchReference', function(referral) {
-							console.log('referral: ' + referral.uris.join());
-						  });
 						res.on('error', function(error) {
 							reject(error);
 						});
@@ -70,8 +66,54 @@ module.exports = function (app) {
 			});
 		}
 
-		_importUsersFromLdapData(data) {
+		_getUserDataFromLdap(idmConfig, school) {
+			//ToDo: move into ldap service
+			const client = ldap.createClient({
+				url: idmConfig.url
+			});
 
+			const qualifiedUser = `uid=${idmConfig.searchUser},cn=users,${idmConfig.rootPath}`;
+
+			return new Promise((resolve, reject) => {
+				client.bind(qualifiedUser, idmConfig.searchUserPw, function(err) {
+					if (err) {
+						reject(new errors.NotAuthenticated('Wrong credentials'));
+					} else {
+						resolve(client);
+					}
+				});
+			}).then((client) => {
+				const opts = {
+					filter: 'univentionObjectType=container/ou',
+					scope: 'sub',
+					attributes: []
+				};
+
+				return new Promise((resolve, reject) => {
+					let users = [];
+					client.search(`cn=users,ou=${school.ldapSchoolIdentifier},${idmConfig.rootPath}`, opts, function (err, res) {
+						res.on('searchEntry', function (entry) {
+							users.push(entry.object);
+						});
+						res.on('error', function(error) {
+							reject(error);
+						});
+						res.on('end', function(result) {
+							resolve(users);
+						});
+					});
+				});
+			});
+		}
+
+		_createSchoolsFromLdapData(data) {
+			//ToDo
+			return true;
+		}
+
+		_createUsersFromLdapData(data, school) {
+			//ToDo
+			return true;
 		}
 	}
 
