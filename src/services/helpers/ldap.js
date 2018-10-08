cae0008248fa804d6f5672a8d7ac91e2b61aaa23
Maximilian Grundke <maximiliangrundke@googlemail.com>
2018-10-08 10:44:45 +0200

    Create LDAP service

---
+++ src/services/helpers/ldap.js
@@ -0,0 +1,108 @@
+const ldap = require('ldapjs');
+const errors = require('feathers-errors');
+
+module.exports = function (app) {
+
+	class LdapService {
+
+		constructor() {
+			this.clients = {};
+		}
+
+		find(params) {
+
+		}
+
+		_addClient(config, client) {
+			this.clients[config._id] = client;
+		}
+
+		_removeClient(config) {
+			return new Promise((resolve, reject) => {
+				if (! (config && config._id)) {
+					reject('Invalid config object');
+				}
+				this._getClient(config).unbind(function(err) {
+					if (err) {
+						reject(err);
+					}
+					delete this.clients[config._id];
+					resolve(true);
+				});
+			});
+		}
+
+		_getClient(config) {
+			let client = this.clients[config];
+			if (client) {
+				return Promise.resolve(client);
+			} else {
+				return this._connect(config);
+			}
+		}
+
+		_connect(config) {
+			const client = ldap.createClient({
+				url: config.url
+			});
+			const loginUser = `uid=${config.searchUser},cn=users,${config.rootPath}`;
+			return new Promise((resolve, reject) => {
+				client.bind(loginUser, config.searchUserPassword, (err) => {
+					if (err) {
+						reject(new errors.NotAuthenticated('Wrong credentials'));
+					} else {
+						this._addClient(config, client);
+						resolve(client);
+					}
+				});
+			});
+		}
+
+		searchCollection(config, searchString, options) {
+			return this._getClient(config).then((client) => {
+				return new Promise((resolve, reject) => {
+					let objects = [];
+					client.search(searchString, options, function (err, res) {
+						if (err) {
+							reject(err);
+						}
+						res.on('error', reject);
+						res.on('searchEntry', function (entry) {
+							objects.push(entry.object);
+						});
+						res.on('end', function(result) {
+							if (result.status === 0) {
+								resolve(objects);
+							} else {
+								reject('LDAP result code != 0');
+							}
+						});
+					});
+				});
+			});
+		}
+
+		getSchools(config) {
+			const options = {
+				filter: 'univentionObjectType=container/ou',
+				scope: 'sub',
+				attributes: []
+			};
+
+			return this.searchCollection(config, `${config.rootPath}`, options);
+		}
+
+		getUsers(config, school) {
+			const options = {
+				filter: 'univentionObjectType=container/ou',
+				scope: 'sub',
+				attributes: []
+			};
+
+			const searchString = `cn=users,ou=${school.ldapSchoolIdentifier},${config.rootPath}`;
+			return this.searchCollection(config, searchString, options);
+		}
+	}
+
+	return LdapService;
+};
--- src/services/helpers/sync.js
+++ src/services/helpers/sync.js
@@ -1,11 +1,10 @@
-const ldap = require('ldapjs');
 const errors = require('feathers-errors');
 
 module.exports = function (app) {
 
 	class SyncService {
 		constructor() {
-
+			this.ldapService = app.service('ldap');
         }
 
 		find(params) {
@@ -20,94 +19,13 @@ module.exports = function (app) {
 
 		_syncFromLdap(app) {
 			let config;
-			return app.service('ldapConfigs').get("5bb73935de31231cefdad572")
+			return app.service('ldapConfigs').get("5bb5fd47ab8f0635cea19db8")
 				.then(foundConfig => {
 					config = foundConfig;
-					return this._getSchoolDataFromLdap(config);
+					return this.ldapService.getSchools(config);
 				}).then(data => {
 					return this._createSchoolsFromLdapData(app, data, config);
 				});
-		}
-
-		_getSchoolDataFromLdap(idmConfig) {
-			//ToDo: move into ldap service
-			const client = ldap.createClient({
-				url: idmConfig.url
-			});
-
-			const qualifiedUser = `uid=${idmConfig.searchUser},cn=users,${idmConfig.rootPath}`;
-
-			return new Promise((resolve, reject) => {
-				client.bind(qualifiedUser, idmConfig.searchUserPw, function(err) {
-					if (err) {
-						reject(new errors.NotAuthenticated('Wrong credentials'));
-					} else {
-						resolve(client);
-					}
-				});
-			}).then((client) => {
-				const opts = {
-					filter: 'univentionObjectType=container/ou',
-					scope: 'sub',
-					attributes: []
-				};
-
-				return new Promise((resolve, reject) => {
-					let users = [];
-					//client.search(`cn=users,ou=${idmConfig.ou},${idmConfig.rootPath}`, opts, function (err, res) {
-					client.search(`${idmConfig.rootPath}`, opts, function (err, res) {
-						res.on('searchEntry', function (entry) {
-							users.push(entry.object);
-						});
-						res.on('error', function(error) {
-							reject(error);
-						});
-						res.on('end', function(result) {
-							resolve(users);
-						});
-					});
-				});
-			});
-		}
-
-		_getUserDataFromLdap(idmConfig, school) {
-			//ToDo: move into ldap service
-			const client = ldap.createClient({
-				url: idmConfig.url
-			});
-
-			const qualifiedUser = `uid=${idmConfig.searchUser},cn=users,${idmConfig.rootPath}`;
-
-			return new Promise((resolve, reject) => {
-				client.bind(qualifiedUser, idmConfig.searchUserPw, function(err) {
-					if (err) {
-						reject(new errors.NotAuthenticated('Wrong credentials'));
-					} else {
-						resolve(client);
-					}
-				});
-			}).then((client) => {
-				const opts = {
-					filter: 'univentionObjectType=container/ou',
-					scope: 'sub',
-					attributes: []
-				};
-
-				return new Promise((resolve, reject) => {
-					let users = [];
-					client.search(`cn=users,ou=${school.ldapSchoolIdentifier},${idmConfig.rootPath}`, opts, function (err, res) {
-						res.on('searchEntry', function (entry) {
-							users.push(entry.object);
-						});
-						res.on('error', function(error) {
-							reject(error);
-						});
-						res.on('end', function(result) {
-							resolve(users);
-						});
-					});
-				});
-			});
 		}
 
 		_createSchoolsFromLdapData(app, data, config) {
@@ -125,8 +43,9 @@ module.exports = function (app) {
 		}
 
 		_createUsersFromLdapData(data, school) {
-			//ToDo
-			return true;
+			return this.ldapService.getUsers(school.ldapConfig, school).then(users => {
+				// TODO
+			});
 		}
 	}
 
