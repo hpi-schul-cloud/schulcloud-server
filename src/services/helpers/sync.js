46b9da30e44b92306d509f89ff6c74cb3c7813ac
Thomas <thomas.feldtkeller@student.hpi.de>
2018-10-08 15:39:35 +0200

    match existing users

--- src/services/helpers/sync.js
+++ src/services/helpers/sync.js
@@ -37,55 +37,72 @@ module.exports = function (app) {
 		}
 
 		_createSchoolsFromLdapData(app, data, config) {
-			return Promise.all(data.map(school => {
-				let schoolData = {
-					name: school.displayName,
-					systems: ["5bb217cf3505d8796a2aa939"], //ToDo: dont hardcode this, do for all ldap
-					ldapConfig: config._id,
-					ldapSchoolIdentifier: school.ou,
-					currentYear: "5b7de0021a3a07c20a1c165e", //18/19
-					federalState: "0000b186816abba584714c58" //Niedersachsen
-				};
-				return app.service('schools').create(schoolData);
+			return Promise.all(data.map(ldapSchool => {
+				return app.service('schools').find({query: {ldapSchoolIdentifier: ldapSchool.ou}})
+				.then(schools => {
+					if (schools.total != 0) {
+						return Promise.resolve(schools.data[0]);
+					}
+					let schoolData = {
+						name: ldapSchool.displayName,
+						systems: ["5bb217cf3505d8796a2aa939"], //ToDo: dont hardcode this, do for all ldap
+						ldapConfig: config._id,
+						ldapSchoolIdentifier: ldapSchool.ou,
+						currentYear: "5b7de0021a3a07c20a1c165e", //18/19
+						federalState: "0000b186816abba584714c58" //Niedersachsen
+					};
+					return app.service('schools').create(schoolData);
+				});
 			}));
+		}
+
+		_createUserAndAccount(app, idmUser, school) {
+			let email = idmUser.mail;
+			//todo: avoid faking a pin verification process
+			return app.service('registrationPins').create({"email": email, verified: true})
+			.then(registrationPin => {
+				let newUserData = {
+					pin: registrationPin.pin,
+					firstName: idmUser.givenName,
+					lastName: idmUser.sn,
+					schoolId: school._id,
+					email: email,
+					ldapDn: idmUser.dn,
+					ldapId: idmUser.entryUUID
+				};
+				if (idmUser.objectClass.includes("ucsschoolTeacher")) {
+					newUserData.role = "teacher";
+				}
+				if (idmUser.objectClass.includes("ucsschoolStudent")) {
+					newUserData.role = "student";
+				}
+				if (idmUser.objectClass.includes("ucsschoolStaff")) {
+					//toDo
+				}
+
+				return app.service('users').create(newUserData);
+			}).then(user => {
+				let accountData = {
+					userId: user._id,
+					username: school.ldapSchoolIdentifier + "/" + idmUser.uid,
+					systemId: "5bb217cf3505d8796a2aa939" //toDo: dont hardcode
+				};
+				return accountModel.create(accountData);
+			});
 		}
 
 		_createUsersFromLdapData(app, data, school) {
 			return Promise.all(data.map(idmUser => {
-
-				if (idmUser.mail == undefined) return Promise.resolve("no email");
-				let email = idmUser.mail;
-				//todo: avoid faking a pin verification process
-				return app.service('registrationPins').create({"email": email, verified: true})
-				.then(registrationPin => {
-					let newUserData = {
-						pin: registrationPin.pin,
-						firstName: idmUser.givenName,
-						lastName: idmUser.sn,
-						schoolId: school._id,
-						email: email,
-						ldapDn: idmUser.dn,
-						ldapId: idmUser.entryUUID //use entryUUID
-					};
-					if (idmUser.objectClass.includes("ucsschoolTeacher")) {
-						newUserData.role = "teacher";
+				
+				return app.service('users').find({query: {ldapId: idmUser.entryUUID}})
+				.then(users => {
+					if (users.total != 0) {
+						return Promise.resolve(users.data[0]);
 					}
-					if (idmUser.objectClass.includes("ucsschoolStudent")) {
-						newUserData.role = "student";
-					}
-					if (idmUser.objectClass.includes("ucsschoolStaff")) {
-						//toDo
-					}
-
-					return app.service('users').create(newUserData);
-				}).then(user => {
-					let accountData = {
-						userId: user._id,
-						username: school.ldapSchoolIdentifier + "/" + idmUser.uid,
-						systemId: "5bb217cf3505d8796a2aa939" //toDo: dont hardcode
-					};
-					return accountModel.create(accountData);
+					if (idmUser.mail == undefined) return Promise.resolve("no email");
+					return this._createUserAndAccount(app, idmUser, school);
 				});
+				
 			}));
 		}
 	}
