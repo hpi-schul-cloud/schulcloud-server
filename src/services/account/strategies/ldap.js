0075cd4d43838c3a6e394bea1081551fac4d8fc7
Maximilian Grundke <maximiliangrundke@googlemail.com>
2018-10-08 16:27:39 +0200

    Remove account creation from LDAP login strategy

--- src/services/account/strategies/ldap.js
+++ src/services/account/strategies/ldap.js
@@ -27,48 +27,8 @@ class LdapLoginStrategy extends AbstractLoginStrategy {
 					.then(config => {
 						return ldapService.authorize(config, school, user.ldapDn, password);
 					});
-			}).then((idmUser) => {
-				let userPromise = app.service('users').find({query: { ldapDn: idmUser.dn }});
-				let accountPromise = app.service('accounts').find({query: {username: username, systemId: system._id}});
-
-				return Promise.all([userPromise, accountPromise, Promise.resolve(idmUser)]);
-			}).then(([users, accounts, idmUser]) => {
-				let userPromise, accountPromise = Promise.resolve();
-
-				if (users.total == 0) {
-					let email = idmUser.email || "test.adress42@SC.de";
-					//todo: avoid faking a pin verification process
-					userPromise = app.service('registrationPins').create({"email": email, verified: true})
-					.then(registrationPin => {
-						let newUserData = {
-							pin: registrationPin.pin,
-							firstName: idmUser.givenName,
-							lastName: idmUser.sn,
-							schoolId: "0000d186816abba584714c5f",
-							email: email,
-							ldapDn: idmUser.dn
-						};
-						if (idmUser.objectClass.includes("ucsschoolTeacher")) {
-							newUserData.role = "teacher";
-						}
-
-						return userPromise = app.service('users').create(newUserData);
-					});
-				} else {
-					userPromise = Promise.resolve(users.data[0]);
-				}
-
-				if (accounts.length != 0) {
-					accountPromise = Promise.resolve(accounts[0]);
-				}
-
-				return Promise.all([userPromise, accountPromise]);
-			}).then(([user, account]) => {
-				if (account && !account.userId == user._id) {
-					return app.service('accounts').patch(account._id, {userId: user._id});
-				}
-				return Promise.resolve();
-			}).catch((err) => {
+			})
+			.catch((err) => {
 				return Promise.reject(err);
 			});
 	}
