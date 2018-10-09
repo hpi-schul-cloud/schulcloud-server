1c8629e3d8111f9d44f287346d90166c778befe5
Hannes Schurig <hannes.schurig@hpi.de>
2018-10-09 18:50:50 +0200

    invite external done, add data to link svc, return link id
    links can now hold a data object to carry more info (for team invite)
    on link get, if data is set, return shortlink id as well in url as query
    invite with mailing of external is done, some little fixes in registration link svc as well

--- src/services/link/index.js
+++ src/services/link/index.js
@@ -7,7 +7,7 @@ const hashService = require('../helpers/hash');
 
 module.exports = function () {
 	const app = this;
-
+	
 	const options = {
 		Model: link,
 		paginate: {
@@ -16,14 +16,20 @@ module.exports = function () {
 		},
 		lean: true
 	};
-
+	
 	let linkService = service(options);
-
+	
 	function redirectToTarget(req, res, next) {
 		if(req.method === 'GET' && !req.query.target) {	// capture these requests and issue a redirect
 			const linkId = req.params.__feathersId;
 			linkService.get(linkId)
-				.then(data => res.redirect(data.target))
+				.then(data => {
+					if (data.data) {
+						res.redirect(data.target+"?shortId="+data._id);
+					} else {
+						res.redirect(data.target);
+					}
+				})
 				.catch(error => res.status(500).send(error));
 		} else {
 			next();
@@ -79,12 +85,24 @@ module.exports = function () {
 			this.docs = {};
 		}
 		
+		/*
+		 * Generates short team invite link and saves additional data to that link.
+		 * @param data = object {
+		 *      role: user role = string "teamexpert"/"teamadministrator"
+		 *      host: current webaddress from client = string
+		 *      teamId: users teamId = string
+		 *      invitee: email of user who gets invited = string
+		 *      inviter: user id of user who generates the invite = ObjectId/string
+		 *      save: make hash link-friendly? = boolean (might be string)
+		 *  }
+		 */
 		async create(data, params) {
-			let linkData = {};
-			if (data.toHash) {
+			let linkInfo = {};
+			if (data.teamId) {
+				data.toHash = data.teamId;
 				try {
 					await app.service('hash').create(data).then(generatedHash => {
-						linkData.hash = generatedHash;
+						linkInfo.teamHash = generatedHash;
 					});
 				} catch (err) {
 					return Promise.reject(new Error(`Fehler beim Generieren des Hashes. ${err}`));
@@ -93,33 +111,40 @@ module.exports = function () {
 			
 			// base link
 			if (data.role === 'teamexpert') {
-				linkData.link = `${(data.host || process.env.HOST)}/teams/${data.teamId}/invite/expert`;
+				linkInfo.link = `${(data.host || process.env.HOST)}/teams/invite/teamexpert/to/${linkInfo.teamHash}`;
 			} else if (data.role === 'teamadministrator') {
-				linkData.link = `${(data.host || process.env.HOST)}/teams/${data.teamId}/invite/admin`;
-			} else if (data.role === 'teammember') {
-				linkData.link = `${(data.host || process.env.HOST)}/teams/${data.teamId}/invite/member`;
+				linkInfo.link = `${(data.host || process.env.HOST)}/teams/invite/teamadministrator/to/${linkInfo.teamHash}`;
 			} else {
 				return Promise.reject(new Error('Fehler bei der Rollenangabe.'));
 			}
-			if (linkData.hash) linkData.link += `?inviteHash=${linkData.hash}`;
+			//if (linkInfo.teamHash) linkInfo.link += `?inviteHash=${linkInfo.hash}`;
 			
 			// remove possible double-slashes in url except the protocol ones
-			linkData.link = linkData.link.replace(/(https?:\/\/)|(\/)+/g, "$1$2");
+			linkInfo.link = linkInfo.link.replace(/(https?:\/\/)|(\/)+/g, "$1$2");
+			
+			// data to enrich link
+			let linkData = {
+				role: data.role,
+				teamId: data.teamId,
+				inviter: data.inviter,
+				invitee: data.invitee
+			};
 			
 			// generate short url
-			await app.service('link').create({target: linkData.link}).then(generatedShortLink => {
-				linkData.shortLink = `${(data.host || process.env.HOST)}/link/${generatedShortLink._id}`;
+			await app.service('link').create({target: linkInfo.link, data: linkData}).then(generatedShortLink => {
+				linkInfo.shortLinkId = generatedShortLink._id;
+				linkInfo.shortLink = `${(data.host || process.env.HOST)}/link/${generatedShortLink._id}`;
 			}).catch(err => {
 				return Promise.reject(new Error('Fehler beim Erstellen des Kurzlinks.'));
 			});
 			
 			// remove possible double-slashes in url except the protocol ones
-			linkData.shortLink = linkData.shortLink.replace(/(https?:\/\/)|(\/)+/g, "$1$2");
+			linkInfo.shortLink = linkInfo.shortLink.replace(/(https?:\/\/)|(\/)+/g, "$1$2");
 			
-			return linkData;
+			return linkInfo;
 		}
 	}
-
+	
 	// Initialize our service with any options it requires
 	app.use('/link', redirectToTarget, linkService);
 	
@@ -128,13 +153,13 @@ module.exports = function () {
 	
 	// generate team invite link with optional user role (leader or expert)
 	app.use('/teaminvitelink', new teamLinkService());
-
+	
 	// Get our initialize service to that we can bind hooks
 	linkService = app.service('/link');
-
+	
 	// Set up our before hooks
 	linkService.before(hooks.before(linkService));
-
+	
 	// Set up our after hooks
 	linkService.after(hooks.after);
 };
