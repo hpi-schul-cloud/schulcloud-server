abc44adc6c1877f4e5db7a6a68bd04511b4bb091
Maximilian Grundke <maximiliangrundke@googlemail.com>
2018-10-16 10:25:46 +0200

    Automatically use default storage options and create school buckets in production

---
+++ src/services/fileStorage/strategies/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const AWSStrategy = require('./awsS3');
+
+const strategies = {
+	'awsS3': AWSStrategy
+};
+
+const createStrategy = (fileStorageType) => {
+	const strategy = strategies[fileStorageType];
+	if (!strategy) throw new errors.BadRequest("No file storage provided");
+	return new strategy();
+};
+
+module.exports = {
+    createStrategy
+};
