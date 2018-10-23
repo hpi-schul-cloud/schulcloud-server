8117c9ce2da90c49ff456ba60b50fc915db13598
Maximilian Grundke <maximiliangrundke@googlemail.com>
2018-10-23 09:14:00 +0200

    Add jsdoc for LDAP strategy selection

--- src/services/ldap/strategies/index.js
+++ src/services/ldap/strategies/index.js
@@ -1,9 +1,18 @@
+const AbstractLDAPStrategy = require('./interface');
 const UniventionLDAPStrategy = require('./univention');
 
 const strategies = {
     'univention': UniventionLDAPStrategy
 };
 
+/**
+ * returns the LDAPStrategy object referenced by the given LDAP config's
+ * `provider` attribute
+ * @param {LdapConfig} config
+ * @return {AbstractLDAPStrategy} an LDAPStrategy object
+ * @throws Will throw an error if the given config is invalid or if there is no
+ * strategy implemented for the referenced provider
+ */
 module.exports = function (config) {
     if (config && config.provider && strategies[config.provider]) {
         const LDAPStrategy = strategies[config.provider];
