240bf557f1087e302751cd8cab991b8db2c4884b
Robert Weber <closingtag@gmail.com>
2018-11-02 21:24:48 +0100

    Remove for now

--- src/services/fileStorage/hooks/model-hooks.js
+++ src/services/fileStorage/hooks/model-hooks.js
@@ -5,40 +5,40 @@ const auth = require('feathers-authentication');
 const permissions = require('../utils/filePermissionHelper');
 
 const restrictToCurrentUser = hook => {
-	let files = hook.result.data;
-	let userId = hook.params.account.userId;
-	let allowedFiles = [];
-	let queries = hook.params.query;
-	// permissions check for each file
+	// let files = hook.result.data;
+	// let userId = hook.params.account.userId;
+	// let allowedFiles = [];
+	// let queries = hook.params.query;
+	// // permissions check for each file
 
-	return Promise.all(files.map(f => {
-		return permissions.checkPermissions(userId, f.key, ['can-write'], false, queries).then(isAllowed => {
-			if (isAllowed) {
-				f.context = isAllowed.context;
-				allowedFiles.push(f);
-			}
-			return;
-		});
-	})).then(_ => {
-		// get context folder name
-		return Promise.all(allowedFiles.map(f => {
-			let context = f.context;
-			if (!context) {
-				f.context = 'geteilte Datei';
-			}
-			else if (context === 'user') {
-				f.context = 'Meine Dateien';
-			}
-			else {
-				f.context = context.name;
-			}
-			return f;
-		})).then(files => {
-			hook.result.data = files;
-			hook.result.total = files.length;
-			return hook;
-		});
-	});
+	// return Promise.all(files.map(f => {
+	// 	return permissions.checkPermissions(userId, f.key, ['can-write'], false, queries).then(isAllowed => {
+	// 		if (isAllowed) {
+	// 			f.context = isAllowed.context;
+	// 			allowedFiles.push(f);
+	// 		}
+	// 		return;
+	// 	});
+	// })).then(_ => {
+	// 	// get context folder name
+	// 	return Promise.all(allowedFiles.map(f => {
+	// 		let context = f.context;
+	// 		if (!context) {
+	// 			f.context = 'geteilte Datei';
+	// 		}
+	// 		else if (context === 'user') {
+	// 			f.context = 'Meine Dateien';
+	// 		}
+	// 		else {
+	// 			f.context = context.name;
+	// 		}
+	// 		return f;
+	// 	})).then(files => {
+	// 		hook.result.data = files;
+	// 		hook.result.total = files.length;
+	// 		return hook;
+	// 	});
+	// });
 };
 
 exports.before = {
