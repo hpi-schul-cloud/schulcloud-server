bdd160d4af4e629f723bf2bd74d34ac32e746117
CeEv <cedric@it-4d.de>
2018-11-05 15:00:28 +0100

    Add additional services on top of the team services to handle extern users handling in future.

--- src/services/teams/index.js
+++ src/services/teams/index.js
@@ -1,12 +1,35 @@
 'use strict';
 
 const service = require('feathers-mongoose');
+const errors = require('feathers-errors');
 const {teamsModel} = require('./model');
 const hooks = require('./hooks');
+//todo docs require 
+
+class Invites {
+	constructor(options) {
+		this.options = options || {};
+		this.docs = {};
+		this.app = options.app;
+	}
+
+	find(params) {
+		const teamsService = this.app.service('teams');
+		const userId = ( params.account || {} ).userId; //if userId is undefined no result is found
+		const restrictedFindMatch = { invitedUserIds: { $elemMatch: { userId } } }
+
+		return teamsService.find({query:restrictedFindMatch});
+	}
+
+	setup(app, path) {
+		this.app = app;
+	}
+}
+
+
 
 module.exports = function () {
 	const app = this;
-
 	const options = {
 		Model: teamsModel,
 		paginate: {
@@ -16,15 +39,15 @@ module.exports = function () {
 		lean: true
 	};
 
-	// Initialize our service with any options it requires
 	app.use('/teams', service(options));
+	app.use('/teams/extern/invites', new Invites({app}) );
 
-	// Get our initialize service to that we can bind hooks
 	const teamsServices = app.service('/teams');
+	const teamsInviteServices = app.service('/teams/extern/invites');
 
-	// Set up our before hooks
 	teamsServices.before(hooks.before);
-
-	// Set up our after hooks
 	teamsServices.after(hooks.after);
+
+	teamsInviteServices.before(hooks.beforeExtern);
+	teamsInviteServices.after(hooks.afterExtern);
 };
