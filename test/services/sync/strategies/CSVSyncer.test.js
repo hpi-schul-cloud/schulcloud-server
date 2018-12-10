129af5faad847a9e7e7db3c7587ad99094d7072d
Maximilian Grundke <maximiliangrundke@googlemail.com>
2018-12-10 16:20:03 +0100

    Add tests for class name parsing

--- test/services/sync/strategies/CSVSyncer.test.js
+++ test/services/sync/strategies/CSVSyncer.test.js
@@ -4,6 +4,7 @@ const chai = require('chai');
 const expect = chai.expect;
 const assert = require('assert');
 
+const app = require('../../../../src/app');
 const Syncer = require('../../../../src/services/sync/strategies/Syncer');
 const CSVSyncer = require('../../../../src/services/sync/strategies/CSVSyncer');
 
@@ -78,6 +79,76 @@ describe('CSVSyncer', () => {
             expect(result[0].class).to.equal('1a');
             expect(result[1].class).to.equal('');
             expect(result[2].class).to.equal('2b');
+        });
+    });
+
+    describe('extractClassesToBeCreated method', () => {
+        const expectResult = (params, expected) => {
+            expect(new CSVSyncer().extractClassesToBeCreated(params)).to.deep.equal(expected);
+        };
+
+        it('should return an empty array if no records are given', () => {
+            expectResult([], []);
+        });
+
+        it('should return all distinct class attribute values', () => {
+            expectResult([
+                {class: '1a'},
+                {class: '12b'},
+                {class: '12b'},
+            ], ['1a', '12b']);
+        });
+
+        it('should ignore empty class attribute values', () => {
+            expectResult([
+                {class: ''},
+                {class: '12b'},
+                {class: '12b'},
+            ], ['12b']);
+        });
+
+        it('should work on composite class attribute values', () => {
+            expectResult([
+                {class: '1a+1b'},
+                {class: '1c'},
+            ], ['1a', '1b', '1c']);
+            expectResult([
+                {class: '1a+1b'},
+                {class: '1b+1a'},
+            ], ['1a', '1b']);
+            expectResult([
+                {class: '1a+1b+1b+1b'},
+                {class: '1b+1a'},
+            ], ['1a', '1b']);
+        });
+    });
+
+    describe('getClassObject method', () => {
+        it('should default to the `static` scheme', async () => {
+            const result = await new CSVSyncer().getClassObject('foobar');
+            expect(result.nameFormat).to.equal('static');
+            expect(result.name).to.equal('foobar');
+        });
+
+        it('should split classes and grade levels if applicable', async () => {
+            const result = await new CSVSyncer(app).getClassObject('1a');
+            expect(result.nameFormat).to.equal('gradeLevel+name');
+            expect(result.gradeLevel.name).to.equal('1');
+            expect(result.name).to.equal('a');
+        });
+
+        it('should only use grade levels for 1. to 13. grade', async () => {
+            for (let i = -5; i <= 20; i++) {
+                const result = await new CSVSyncer(app).getClassObject(`${i}b`);
+                if (i >= 1 && i <= 13) {
+                    expect(result.nameFormat).to.equal('gradeLevel+name');
+                    expect(result.gradeLevel.name).to.equal(String(i));
+                    expect(result.name).to.equal('b');
+                } else {
+                    expect(result.nameFormat).to.equal('static');
+                    expect(result.name).to.equal(`${i}b`);
+                }
+            }
         });
     });
 });
