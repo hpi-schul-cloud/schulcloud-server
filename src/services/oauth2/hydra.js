e48a128f644584e5a17a40cdbb35e7b6fb4f8aab
dominikglandorf <do.gl@gmx.de>
2019-01-03 15:01:58 +0100

    use request instead of fetch

--- src/services/oauth2/hydra.js
+++ src/services/oauth2/hydra.js
@@ -1,46 +1,35 @@
-const fetch = require('node-fetch');
+const request = require('request-promise-native');
 const uj = require('url-join');
-const errors = require('feathers-errors');
 
 const mockTlsTermination = {
 	'X-Forwarded-Proto': 'https'
 };
 
-const handleResponse = (res) => {
-	if (res.status < 200 || res.status > 302) {
-		// This will handle any errors that aren't network related (network related errors are handled automatically)
-		return res.json().then(function (body) {
-			return Promise.reject(new Error(body.error.message));
-		})
-	}
-
-	return res.json();
-}
-
 module.exports = (hydraUrl) => {
 
 	function get(flow, challenge) {
-		return fetch(uj(hydraUrl, '/oauth2/auth/requests/' + flow + '/' + challenge),
-			{
-				headers: {
-					...mockTlsTermination
-				}
-			}).then(handleResponse);
+		const options = {
+			uri: uj(hydraUrl, '/oauth2/auth/requests/' + flow + '/' + challenge),
+			headers: {
+				...mockTlsTermination
+			},
+			json: true
+		};
+		return request(options);
 	}
 
 	// A little helper that takes type (can be "login" or "consent"), the action (can be "accept" or "reject") and a challenge and returns the response from ORY Hydra.
 	function put(flow, action, challenge, body) {
-		return fetch(
-			uj(hydraUrl, '/oauth2/auth/requests/' + flow + '/' + challenge + '/' + action),
-			{
-				method: 'PUT',
-				body: JSON.stringify(body),
-				headers: {
-					'Content-Type': 'application/json',
-					...mockTlsTermination
-				}
-			}
-		).then(handleResponse);
+		const options = {
+			uri: uj(hydraUrl, '/oauth2/auth/requests/' + flow + '/' + challenge + '/' + action),
+			method: 'PUT',
+			body,
+			headers: {
+				...mockTlsTermination
+			},
+			json: true
+		};
+		return request(options);
 	}
 
 	return {
@@ -69,41 +58,43 @@ module.exports = (hydraUrl) => {
 		rejectConsentRequest: function (challenge, body) {
 			return put('consent', 'reject', challenge, body);
 		},
-		introspectOAuth2Token: (token, scope) => fetch(
-			uj(hydraUrl, '/oauth2/introspect'),
-			{
+		introspectOAuth2Token: (token, scope) => {
+			const options = {
+				uri: uj(hydraUrl, '/oauth2/introspect'),
 				method: 'POST',
 				body: `token=${token}&scope=${scope}`,
 				headers: {
-					'Content-Type': 'application/x-www-form-urlencoded',
-					...mockTlsTermination
-				}
+					...mockTlsTermination,
+					'Content-Type': 'application/x-www-form-urlencoded'
+				},
+				json: true
+			};
+			return request(options);
+		},
+		isInstanceAlive: () => request({
+			uri: uj(hydraUrl, '/health/alive')
+		}),
+		listOAuth2Clients: () => request({
+			uri: uj(hydraUrl, '/clients'),
+			headers: {
+				...mockTlsTermination
 			}
-		).then(handleResponse),
-		isInstanceAlive: () => fetch(uj(hydraUrl, '/health/alive')),
-		listOAuth2Clients: () => fetch(uj(hydraUrl, '/clients'),
-			{
-				headers: {
-					...mockTlsTermination
-				}
-			}).then(handleResponse),
-		createOAuth2Client: (data) => fetch(uj(hydraUrl, '/clients'),
-			{
-				method: 'POST',
-				body: JSON.stringify(data),
-				headers: {
-					'Content-Type': 'application/json',
-					...mockTlsTermination
-				}
+		}),
+		createOAuth2Client: (data) => request({
+			uri: uj(hydraUrl, '/clients'),
+			method: 'POST',
+			body: data,
+			headers: {
+				...mockTlsTermination
+			},
+			json: true
+		}),
+		deleteOAuth2Client: (id) => request({
+			uri: uj(hydraUrl, `/clients/${id}`),
+			method: 'DELETE',
+			headers: {
+				...mockTlsTermination
 			}
-		).then(handleResponse),
-		deleteOAuth2Client: (id) => fetch(uj(hydraUrl, `/clients/${id}`),
-			{
-				method: 'DELETE',
-				headers: {
-					...mockTlsTermination
-				}
-			}
-		)
+		})
 	};
 }
