72641b18ad97bd44869dc10fec25d82e0d1e4822
Tobias Wollowski <tobias.wollowski@student.hpi.de>
2019-01-23 12:24:25 +0100

    cleanup logging

--- src/events/courseEmitter.js
+++ src/events/courseEmitter.js
@@ -7,10 +7,22 @@ const { courseModel } = require('../services/user-group/model');
 const courseEmitter = new Emitter();
 const REQUEST_TIMEOUT = 8000;
 
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    transports: [
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+            )
+        })
+    ]
+});
 
 function sendCourseUpdatedPushMessage(course, receivers, tag) {
 
-    if(!receivers) return Promise.reject('receivers missing!');
+    if (!receivers) return Promise.reject('receivers missing!');
 
     // todo retrieve config from app context instead
     const serviceUrls = config.get('services') || {};
@@ -50,28 +62,28 @@ function sendCourseUpdatedPushMessage(course, receivers, tag) {
 }
 
 courseEmitter.on('updated', (courseId) => {
-    console.log('course updated event...', courseId);
+    logger.info('course updated event...', courseId);
 
     return courseModel.findById(courseId).then(course => {
         if (!course) return Promise.reject('courseId not found');
         let userIds = new Set();
         course.teacherIds.forEach(id => userIds.add(id));
         course.userIds.forEach(id => userIds.add(id));
-        course.substitutionIds.forEach(id=>userIds.add(id));
+        course.substitutionIds.forEach(id => userIds.add(id));
 
         // todo send post message
         return sendCourseUpdatedPushMessage(course, [...userIds], 'course-data-updated');
-    }).catch(err => console.log(err));
+    }).catch(err => logger.info(err));
 
 });
 
 courseEmitter.on('removed', (course) => {
-    console.log('course removed event...', course);
-    
+    logger.info('course removed event...', course);
+
     let userIds = new Set();
     course.teacherIds.forEach(id => userIds.add(id));
     course.userIds.forEach(id => userIds.add(id));
-    course.substitutionIds.forEach(id=>userIds.add(id));
+    course.substitutionIds.forEach(id => userIds.add(id));
 
     return sendCourseUpdatedPushMessage(course, [...userIds], 'course-removed');
 
