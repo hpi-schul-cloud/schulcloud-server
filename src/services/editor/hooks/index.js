641e62dcf56b7bccb04b72cdb6bd0b83721329d7
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2019-02-27 15:47:05 +0100

    Add custom group populate after hook to display usernames.

--- src/services/editor/hooks/index.js
+++ src/services/editor/hooks/index.js
@@ -1,10 +1,45 @@
 const auth = require('feathers-authentication');
 const logger = require('../../../logger/');
 
-const populateUsers = (context) => {
-	// todo populate id to {id, name:user.firstname+' '+user.lastname}
-	console.log(context.result);
+const isGroup = e => !undefined && typeof e === 'object' && e.type === 'group' && e.users;
+const mapUsers = users => users.data.map((user) => {
+	return {
+		userId: user._id.toString(),
+		name: `${user.firstName} ${user.lastName}`,
+	};
+});
+
+const addUsers = (context, key) => context.app.service('users').find({
+	query: { _id: { $in: key ? context.result[key].users : context.result.users } },
+}).then((u) => {
+	if (key) {
+		context.result[key].users = mapUsers(u);
+	} else {
+		context.result.users = mapUsers(u);
+	}
 	return context;
+}).catch(() => {
+	logger.warn('Can not populate user information');
+});
+
+/**
+ * Populate each user displayName for groups or keys that contain populated groups.
+ */
+const populateUsers = async (context) => {
+	const { result } = context;
+	// todo populate id to {id, name:user.firstname+' '+user.lastname}
+	if (isGroup(result)) {
+		return addUsers(context);
+	}
+
+	const wait = [];
+	Object.values(result).forEach((key) => {
+		if (isGroup(result[key])) {
+			wait.push(addUsers(context, result[key].users, key));
+		}
+	});
+
+	return Promise.all(wait).then(() => context);
 };
 
 const passRequestDataToParams = (context) => {
@@ -14,6 +49,11 @@ const passRequestDataToParams = (context) => {
 		id: context.id,
 		data: context.data,
 	};
+	return context;
+};
+
+const extractFindData = (context) => {
+	context.result = context.result.data;
 	return context;
 };
 
@@ -43,11 +83,11 @@ exports.before = {
 };
 
 exports.after = {
-	all: [populateUsers],
-	find: [],
-	get: [],
-	create: [],
+	all: [],
+	find: [extractFindData],
+	get: [populateUsers],
+	create: [populateUsers],
 	update: [],
-	patch: [],
+	patch: [populateUsers],
 	remove: [],
 };
