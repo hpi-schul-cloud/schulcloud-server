ab2cfa1b6c27340110c29f9d4d9696be3f68b974
Maximilian Grundke <maximiliangrundke@googlemail.com>
2019-03-04 16:21:51 +0100

    Lint

--- test/services/user/unit.test.js
+++ test/services/user/unit.test.js
@@ -1,55 +1,51 @@
-'use strict';
-
 const assert = require('assert');
-const mongoose = require('mongoose');
+const { expect } = require('chai');
+
 const app = require('../../../src/app');
+
 const userService = app.service('users');
 const registrationPinService = app.service('registrationPins');
 const classesService = app.service('classes');
 const coursesService = app.service('courses');
-const chai = require('chai');
-const loginHelper = require('../helpers/login');
 const testObjects = require('../helpers/testObjects')(app);
-const promisify = require('es6-promisify');
-const expect = chai.expect;
 
-let testUserId = undefined;
+let testUserId;
 
-describe('user service', function () {
+describe('user service', () => {
 	it('registered the users service', () => {
 		assert.ok(userService);
 		assert.ok(classesService);
 		assert.ok(coursesService);
 	});
 
-	it('rejects on group patching', function() {
-		return userService.patch(null, {email: 'test'}).catch(err => {
-			chai.expect(err).to.be.not.undefined;
-			chai.expect(err.message).to.equal('Operation on this service requires an id!');
+	it('rejects on group patching', async () => {
+		await userService.patch(null, { email: 'test' }).catch((err) => {
+			expect(err).to.not.equal(undefined);
+			expect(err.message).to.equal('Operation on this service requires an id!');
 		});
 	});
 
-	it('resolves permissions and attributes correctly', function () {
-		function create_test_base() {
+	it('resolves permissions and attributes correctly', () => {
+		function createTestBase() {
 			return app.service('roles')
 				.create({
-					"name": "test_base",
-					"roles": [],
-					"permissions": [
-						"TEST_BASE",
-						"TEST_BASE_2"
-					]
+					name: 'test_base',
+					roles: [],
+					permissions: [
+						'TEST_BASE',
+						'TEST_BASE_2',
+					],
 				});
 		}
 
-		function create_test_subrole(test_base) {
+		function createTestSubrole(test_base) {
 			return app.service('roles')
 				.create({
-					"name": "test_subrole",
-					"roles": [test_base._id],
-					"permissions": [
-						"TEST_SUB"
-					]
+					name: 'test_subrole',
+					roles: [test_base._id],
+					permissions: [
+						'TEST_SUB',
+					],
 				});
 		}
 
@@ -68,43 +64,25 @@ describe('user service', function () {
 				manualCleanup: true,
 			}))
 			.then(user => userService.get(user._id))
-			.then(user => {
+			.then((user) => {
 				testUserId = user._id;
-				chai.expect(user.avatarInitials).to.eq('MT');
+				expect(user.avatarInitials).to.eq('MT');
 				const array = Array.from(user.permissions);
-				chai.expect(array).to.have.lengthOf(3);
-				chai.expect(array).to.include("TEST_BASE", "TEST_BASE_2", "TEST_SUB");
+				expect(array).to.have.lengthOf(3);
+				expect(array).to.include('TEST_BASE', 'TEST_BASE_2', 'TEST_SUB');
 			});
 	});
 
-	it('deletes user correctly', function () {
-		let classId = undefined;
-		let courseId = undefined;
+	it('deletes user correctly', async () => {
+		const demoClass = (await classesService.find({ query: { name: 'Demo-Klasse', $limit: 1 } })).data[0];
+		const demoCourse = (await coursesService.find({ query: { name: 'Mathe', $limit: 1 } })).data[0];
 
-		classesService.find({query: {"name": "Demo-Klasse", $limit: 1}})
-		.then(classes => {
-			classes.data.map(myClass => {
-				myClass.userIds.push(testUserId);
-				classId = myClass._id;
-			});
-			chai.expect(classId).to.not.be.undefined;
+		await userService.remove(testUserId);
 
-			coursesService.find({query: {"name": "Mathe", $limit: 1}})
-			.then(courses => {
-				courses.data.map(course => {
-					course.userIds.push(testUserId);
-					courseId = course._id;
-				});
-				chai.expect(courseId).to.not.be.undefined;
-
-				return userService.remove(testUserId).then(h => {
-					classesService.get(classId).then(myClass => chai.expect(myClass.userIds).to.not.include(testUserId));
-					coursesService.get(courseId).then(course => chai.expect(course.userIds).to.not.include(testUserId));
-				});
-			});
-		});
+		expect(demoClass.userIds).to.not.include(testUserId);
+		expect(demoCourse.userIds).to.not.include(testUserId);
 	});
-	
+
 	describe('uniqueness check', () => {
 		it('should reject new users with mixed-case variants of existing usernames', async () => {
 			await testObjects.createTestUser({ email: 'existing@account.de' });
@@ -131,26 +109,22 @@ describe('user service', function () {
 	after(testObjects.cleanup);
 });
 
-describe('registrationPin Service', function() {
-	it ('registered the registrationPin Service', () => {
+describe('registrationPin Service', () => {
+	it('registered the registrationPin Service', () => {
 		assert.ok(registrationPinService);
 	});
 
-	it ('creates pins correctly', function() {
-		return registrationPinService
+	it('creates pins correctly', () => registrationPinService
 			.create({"email": "test.adresse@schul-cloud.org"})
 				.then(pinObject => registrationPinService.find({query: {"email": "test.adresse@schul-cloud.org"}}))
 				.then(pinObjects => {
-					chai.expect(pinObjects.data[0]).to.have.property('pin');
-				});
-	});
+					expect(pinObjects.data[0]).to.have.property('pin');
+				}));
 
-	it ('overwrites old pins', function() {
-		return registrationPinService.create({"email": "test.adresse@schul-cloud.org"})
+	it('overwrites old pins', () => registrationPinService.create({"email": "test.adresse@schul-cloud.org"})
 				.then(pinObject => registrationPinService.create({"email": "test.adresse@schul-cloud.org"}))
 				.then(pinObject => registrationPinService.find({query: {"email": "test.adresse@schul-cloud.org"}}))
 				.then(pinObjects => {
-					chai.expect(pinObjects.data).to.have.lengthOf(1);
-				});
-	});
+					expect(pinObjects.data).to.have.lengthOf(1);
+				}));
 });
