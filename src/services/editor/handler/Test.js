2e37e1932d205c83456b5db6c4c0995e29d12ef4
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2019-03-04 15:41:49 +0100

    Fix secret error and make jwt for /editor/test avaible.

--- src/services/editor/handler/Test.js
+++ src/services/editor/handler/Test.js
@@ -1,4 +1,7 @@
+/* eslint-disable no-param-reassign */
 const { userModel } = require('../../user/model');
+const CustomJWTService = require('../../account/CustomJWTService');
+const logger = require('../../../logger/');
 
 class Test {
 	constructor(options) {
@@ -7,11 +10,19 @@ class Test {
 	}
 
 	async find() {
+		const { authentication } = this.app.get('secrets');
+		if (authentication === undefined) {
+			logger.warn('No secrets authentication defined.');
+		}
+		const jwtService = new CustomJWTService(authentication);
 		const [user1, user2] = await userModel.find({}).limit(2).exec();
 		const fakeData = { users: [user2.id] };
 		const fakeParams = { account: { userId: user1.id } };
-
-		return this.app.service('/editor/lessons').create(fakeData, fakeParams);
+		const jwt = await jwtService.create({ userId: user1.id });
+		return this.app.service('/editor/lessons').create(fakeData, fakeParams).then((lesson) => {
+			lesson.jwt = jwt;
+			return lesson;
+		});
 	}
 
 	setup(app) {
