abd0270c5cc7b55b5e2f2905fe5f2c4a0e96a026
Adrian Jost <adrianjost@users.noreply.github.com>
2019-04-06 14:26:24 +0200

    Update timelines.test.js

--- test/services/timelines/timelines.test.js
+++ test/services/timelines/timelines.test.js
@@ -1,5 +1,5 @@
-const assert = require('assert')
-const app = require('../../../src/app')
+const assert = require('assert');
+const app = require('../../../src/app');
 
 describe('timelines service', () => {
 	it('registered the timelines service', () => {
