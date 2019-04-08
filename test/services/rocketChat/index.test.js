38ae97ae1f0a11aaa18ca0b8035b8178ec7d6438
Thomas Feldtkeller <thomas.feldtkeller@hpi.de>
2019-04-08 13:18:12 +0200

    create initial testfile

---
+++ test/services/rocketChat/index.test.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-expressions */
+const assert = require('assert');
+const chai = require('chai');
+const app = require('../../../src/app');
+
+const rocketChatUserService = app.service('/rocketChat/user');
+const rocketChatLoginService = app.service('/rocketChat/login');
+const rocketChatLogoutService = app.service('rocketChat/logout');
+const rocketChatChannelService = app.service('/rocketChat/channel');
+
+const { expect } = chai;
+
+describe('rocket.chat user service', () => {
+	it('registered the RC user service', () => {
+		assert.ok(rocketChatUserService);
+	});
+
+	it('creates a rc user for a sc user');
+});
+
+describe('rocket.chat login service', () => {
+	it('registered the RC user service', () => {
+		assert.ok(rocketChatLoginService);
+	});
+});
+
+describe('rocket.chat logout service', () => {
+	it('registered the RC user service', () => {
+		assert.ok(rocketChatLogoutService);
+	});
+});
+
+describe('rocket.chat user service', () => {
+	it('registered the RC user service', () => {
+		assert.ok(rocketChatChannelService);
+	});
+});
