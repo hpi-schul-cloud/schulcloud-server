f370746ed5e640221293126f0deae765f37a1db4
Maximilian Grundke <maximiliangrundke@googlemail.com>
2019-04-09 13:17:54 +0200

    Improve tests

--- test/services/news/index.test.js
+++ test/services/news/index.test.js
@@ -3,27 +3,69 @@ const { ObjectId } = require('mongoose').Types;
 const sleep = require('util').promisify(setTimeout);
 
 const app = require('../../../src/app');
-const NewsModel = require('../../../src/services/news/model').newsModel;
+const News = require('../../../src/services/news/model').newsModel;
 
 describe('news service', () => {
 	it('registers correctly', () => {
 		expect(app.service('news')).to.not.equal(undefined);
 	});
 
-	it('deletes team news after a team is deleted', async () => {
-		const teamId = new ObjectId();
-		const teamNews = await new NewsModel({
-			schoolId: new ObjectId(),
-			title: 'team news',
-			content: 'here are some news concerning this team',
-			target: teamId,
-			targetModel: 'teams',
+	describe('event handlers', () => {
+		describe('team news', () => {
+			it('is deleted after the coresponding team is deleted', async () => {
+				const teamId = new ObjectId();
+				const teamNews = await new News({
+					schoolId: new ObjectId(),
+					title: 'team news',
+					content: 'here are some news concerning this team',
+					target: teamId,
+					targetModel: 'teams',
+				}).save();
+
+				app.service('teams').emit('removed', { _id: teamId });
+				await sleep(100); // give the event listener time to work
+
+				const result = await News.count({ _id: teamNews._id });
+				expect(result).to.equal(0);
+			});
+
+			it('does not delete any other news', async () => {
+				const schoolId = new ObjectId();
+				const teamId = new ObjectId();
+				await new News({
+					schoolId,
+					title: 'team news',
+					content: 'here are some news concerning this team',
+					target: teamId,
+					targetModel: 'teams',
+				}).save();
+				const courseId = new ObjectId();
+				const courseNews = await new News({
+					schoolId,
+					title: 'course news',
+					content: 'here are some news concerning this course',
+					target: courseId,
+					targetModel: 'courses',
+				}).save();
+				const schoolNews = await new News({
+					schoolId,
+					title: 'global school news',
+					content: 'yo ho ho, and a bottle of rum',
+				}).save();
+
+				const newsCount = await News.count({ schoolId });
+				expect(newsCount).to.equal(3);
+
+				app.service('teams').emit('removed', { _id: teamId });
+				await sleep(100); // give the event listener time to work
+
+				const result = await News.count({ schoolId });
+				expect(result).to.equal(2);
+
+				await News.remove({
+					_id: { $in: [courseNews._id, schoolNews._id] },
+				});
+			});
 		});
-
-		app.service('teams').emit('deleted', { _id: teamId });
-		await sleep(20); // give the event listener time to work
-
-		const result = await NewsModel.count({ _id: teamNews._id });
-		expect(result).to.equal(0);
 	});
 });
