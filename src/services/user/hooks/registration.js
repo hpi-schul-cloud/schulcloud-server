a0ff1148cf9815627f4cb97256d53b6331720775
Wolfgang Rahtgeb <W.Rathgeb@dropanote.de>
2019-04-12 12:07:00 +0200

    check permission

--- src/services/user/hooks/registration.js
+++ src/services/user/hooks/registration.js
@@ -1,4 +1,5 @@
 const auth = require('feathers-authentication');
+const globalHooks = require('../../../hooks');
 
 const mailToLowerCase = (hook) => {
 	if (hook.data) {
@@ -20,22 +21,27 @@ exports.before = {
 	find: [],
 	get: [
 		auth.hooks.authenticate('jwt'),
+		globalHooks.hasPermission('USER_VIEW'),
 		mailToLowerCase,
 	],
 	create: [
 		auth.hooks.authenticate('jwt'),
+		globalHooks.hasPermission('USER_CREATE'),
 		mailToLowerCase,
 	],
 	update: [
 		auth.hooks.authenticate('jwt'),
+		globalHooks.hasPermission('USER_EDIT'),
 		mailToLowerCase,
 	],
 	patch: [
 		auth.hooks.authenticate('jwt'),
+		globalHooks.hasPermission('USER_EDIT'),
 		mailToLowerCase,
 	],
 	remove: [
 		auth.hooks.authenticate('jwt'),
+		globalHooks.hasPermission('USER_CREATE'),
 		mailToLowerCase,
 	],
 };
