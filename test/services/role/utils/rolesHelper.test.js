b0ceb85f2e91e7b2e65a0e050c2d689639b601ed
Robert Weber <closingtag@gmail.com>
2019-05-03 12:39:06 +0200

    Lint test

--- test/services/role/utils/rolesHelper.test.js
+++ test/services/role/utils/rolesHelper.test.js
@@ -4,30 +4,30 @@ const { sortRoles } = require('../../../../src/services/role/utils/rolesHelper')
 
 const testData = [
 	{
-		"_id" : "5bb5c190fb457b1c3c0c7e0f",
-		"roles" : [],
+		_id: '5bb5c190fb457b1c3c0c7e0f',
+		roles: [],
 	},
 	{
-		"_id" : "5bb5c391fb457b1c3c0c7e10",
-		"roles" : [],
+		_id: '5bb5c391fb457b1c3c0c7e10',
+		roles: [],
 	},
 	{
-		"_id" : "5bb5c49efb457b1c3c0c7e11",
-		"roles" : [
-			"5bb5c190fb457b1c3c0c7e0f"
-		]
+		_id: '5bb5c49efb457b1c3c0c7e11',
+		roles: [
+			'5bb5c190fb457b1c3c0c7e0f'
+		],
 	},
 	{
-		"_id" : "5bb5c62bfb457b1c3c0c7e14",
-		"roles" : [
-			"5bb5c545fb457b1c3c0c7e13"
-		]
+		_id: '5bb5c62bfb457b1c3c0c7e14',
+		roles: [
+			'5bb5c545fb457b1c3c0c7e13'
+		],
 	},
 	{
-		"_id" : "5bb5c545fb457b1c3c0c7e13",
-		"roles" : [
-			"5bb5c49efb457b1c3c0c7e11"
-		]
+		_id: '5bb5c545fb457b1c3c0c7e13',
+		roles: [
+			'5bb5c49efb457b1c3c0c7e11'
+		],
 	},
 ];
 
@@ -38,35 +38,35 @@ describe('rolesHelper should', () => {
 		const expected = [
 			[
 				{
-					"_id" : "5bb5c190fb457b1c3c0c7e0f",
-					"roles" : [],
+					_id: '5bb5c190fb457b1c3c0c7e0f',
+					roles: [],
 				},
 				{
-					"_id" : "5bb5c391fb457b1c3c0c7e10",
-					"roles" : [],
+					_id: '5bb5c391fb457b1c3c0c7e10',
+					roles: [],
 				},
 			],
 			[
 				{
-					"_id" : "5bb5c49efb457b1c3c0c7e11",
-					"roles" : [
-						"5bb5c190fb457b1c3c0c7e0f"
+					_id: '5bb5c49efb457b1c3c0c7e11',
+					roles: [
+						'5bb5c190fb457b1c3c0c7e0f'
 					],
 				}
 			],
 			[
 				{
-					"_id" : "5bb5c545fb457b1c3c0c7e13",
-					"roles" : [
-						"5bb5c49efb457b1c3c0c7e11"
+					_id: '5bb5c545fb457b1c3c0c7e13',
+					roles: [
+						'5bb5c49efb457b1c3c0c7e11'
 					],
 				},
 			],
 			[
 				{
-					"_id" : "5bb5c62bfb457b1c3c0c7e14",
-					"roles" : [
-						"5bb5c545fb457b1c3c0c7e13"
+					_id: '5bb5c62bfb457b1c3c0c7e14',
+					roles: [
+						'5bb5c545fb457b1c3c0c7e13'
 					],
 				},
 			]
