6ba0efcbe3f72586cc44023cb4f111e396268624
Yulia Shikhareva <yulia.shikhareva@gmail.com>
2019-05-07 11:42:05 +0200

    wip

--- test/services/news/index.test.js
+++ test/services/news/index.test.js
@@ -5,13 +5,14 @@ const sleep = require('util').promisify(setTimeout);
 const app = require('../../../src/app');
 const News = require('../../../src/services/news/model').newsModel;
 
-const userService = app.service('users');
-const classesService = app.service('classes');
-const coursesService = app.service('courses');
+// const userService = app.service('users');
+// const classesService = app.service('classes');
+// const coursesService = app.service('courses');
+
 // testUser, testClass, testCourse anlegen:
 const User = require('../../../src/services/user/model').userModel;
 const Class = require('../../../src/services/user-group/model').classModel;
-// const Role = require('../../../src/services/role/model');
+const Role = require('../../../src/services/role/model');
 const School = require('../../../src/services/school/model').schoolModel;
 const Course = require('../../../src/services/user-group/model').courseModel;
 const Team = require('../../../src/services/teams/model').teamsModel;
@@ -19,7 +20,6 @@ const Team = require('../../../src/services/teams/model').teamsModel;
 const newsService = app.service('news');
 const testObjects = require('../helpers/testObjects')(app);
 
-const registrationPinService = app.service('registrationPins');
 
 describe('news service', () => {
 	it('registers correctly', () => {
@@ -34,7 +34,7 @@ describe('news service', () => {
 		});
 		expect(newsSchool).not.to.be.equal(null);
 		const dbNews = await News.findById(newsSchool._id);
-		expect(dbNews).not.to.be.eaqual(null);
+		expect(dbNews).not.to.be.equal(null);
 	});
 	it('create news for own class', async () => {
 		const school = await School.findOne({ name: 'Demo Schule' }).exec();
@@ -43,8 +43,8 @@ describe('news service', () => {
 			title: 'class news',
 			content: 'here is news for one class',
 			schoolId: school._id,
-			target: school._id,
-			targetModel: 'school',
+			target: testClass._id,
+			targetModel: 'class',
 		});
 		expect(newsClass).not.to.be.equal(null);
 		const clNews = await News.findById(newsClass._id);
@@ -60,9 +60,9 @@ describe('news service', () => {
 			targetModel: 'courses',
 			target: testCourse._id,
 		});
-		expect(newsCourse).not.to.be.equal(null);
+		expect(newsCourse).not.to.be.equal(0);
 		const coNews = await News.findById(newsCourse._id);
-		expect(coNews).not.to.be.equal(null);
+		expect(coNews).not.to.be.equal(0);
 	});
 	it('create news for own team', async () => {
 		const school = await School.findOne({ name: 'Demo Schule' }).exec();
@@ -127,22 +127,19 @@ describe('news service', () => {
 	});
 	describe('visibility', () => {
 		it('create sample news of school news for school members', async () => {
-			// Nutzer ist in class kann sehen
 			const schoolId = await new ObjectId();
 			const news = await new News({
 				title: 'sample news',
 				content: 'visibility test',
 				schoolId,
 				targetModel: 'class',
-				// target: schoolId,
 			}).save();
 			const newsCount = await News.count({ _id: news._id });
 			expect(newsCount).to.equal(1);
-			// await News.remove({ _id: news._id });
-			// expect(await News.find({ _id: news._id }).to.be(0));
+			await News.remove({ _id: news._id });
+			expect(await News.find({ _id: news._id }).to.be.undefined);
 		});
 		it('of school news for non-school members', async () => {
-			// Nutzer ist in anderer Klasse kann nicht sehen
 			const schoolId = await new ObjectId();
 			const classId = await new ObjectId();
 			const news = await new News({
@@ -155,10 +152,11 @@ describe('news service', () => {
 			const newsCount = await News.count({ _id: news._id });
 			expect(newsCount).to.equal(1);
 		});
-	});
-	// Sichtbarkeiten testen
-	/*
-		it.only('create test user', async () => {
+
+
+		/* _____________________TESTEN, DASS DIE NEWS FÜR TEAMS, KLASSEN und KURSEN ERSTELLT WERDEN_________________ */
+
+		it('create test user', async () => {
 			const schoolId = await new ObjectId();
 			// await testObjects.createTestUser({ schoolId });
 			const newUser = await new User({
@@ -169,28 +167,112 @@ describe('news service', () => {
 				role: 'teacher',
 			}).save();
 			const userId = newUser._id;
+			const testClass = new Class({
+				name: 'test',
+				schoolIds: [schoolId],
+				userIds: [userId],
+			});
+			const classId = testClass._id;
+			const role = await Role.findOne({ name: 'teacher' }).exec();
 			const newTeam = await new Team({
 				name: 'Test Sichtbarkeit',
 				schoolId,
-				userIds: [userId],
+				schoolIds: [schoolId],
+				userIds: [{ userId, role: role._id, schoolId }],
+				classIds: [classId],
 			}).save();
+			const teamId = newTeam._id;
 			const testNews = await new News({
-				title: 'sample news',
+				title: 'teams sample news',
 				content: 'visibility test',
 				schoolId,
-				target: newTeam._id,
-				targetModel: 'team',
+				target: teamId,
+				targetModel: 'teams',
 			}).save();
-
-		// await testObjects.createTestUser({ schoolId2 });
-		// after(testObjects.cleanup);
+			expect(await News.count({ _id: testNews._id })).to.equal(1);
 		});
 	});
-	*/
+	/* __________________________TESTFÄLLE MIT VERSCHIEDENEN SICHTBARKEITEN FÜR USER________________________ */
 
-	// Nutzer im Anschluss löschen after()
-	// Nutzer ist in Schule ist im Team und kann team news sehen
-	// Nutzer ist in Schule nicht im Team und kann die team news nicht sehen
+	describe.only('weiter Sichtbarkeit testen', async () => {
+		let user;
+		let news;
+		let testKurs;
+		let testTeam;
+		let testKlasse;
+		before(async () => {
+			// eslint-disable-next-line max-len
+			user = await testObjects.createTestUser({ name: 'newsTester', roles: ['student'], schoolId: new ObjectId() });
+			const role = await Role.findOne({ name: 'student' }).exec();
+			testKlasse = await testObjects.createTestClass({
+				userIds: [user._id],
+				schoolId: user.schoolId,
+			});
+			const testKlasseId = testKlasse._id;
+			testKurs = await testObjects.createTestCourse({
+				name: 'newsTester',
+				classIds: [testKlasseId],
+				schoolId: user.schoolId,
+				userIds: [testObjects.createdUserIds],
+			});
+			testTeam = await new Team({
+				name: 'sichtbarkeitTestTeam',
+				schoolId: user.schoolId,
+				schoolIds: [await new ObjectId()],
+				userId: [{ userId: user._id, role: role._id, schoolId: user.schoolId }],
+			}).save();
+			news = await new News({
+				title: 'SichtbarkeitTest',
+				content: 'team, course, class',
+				schoolId: user.schoolId,
+			}).save();
+		});
+		/* _____1_____ */
+		it.only('in team, course, class', async () => {
+		});
+		/* ____2___ */
+
+		it.only('in team,course und nicht class', async () => {
+			testKlasse.userIds = await new ObjectId();
+			await app.service('classes').patch(testKlasse._id, testKlasse);
+		});
+		/* ____3_____ */
+		it.only('in in team, not course und class', async () => {
+			testKurs.userIds = [await new ObjectId()];
+			await app.service('courses').patch(testKurs._id, testKurs);
+		});
+		// _______4_______
+		it.only('in team, not course and not class', async () => {
+			testKurs.userIds = await new ObjectId();
+			testKlasse.userIds = await new ObjectId();
+			await app.service('courses').patch(testKurs._id, testKurs);
+			await app.service('classes').patch(testKlasse._id, testKlasse);
+		});
+		// ____5______
+		it.only('not in team, in course and in class', async () => {
+			testTeam.userIds = await new ObjectId();
+		});
+		// ___6____
+		it('not in team, not in course, in class', async () => {
+			testTeam.userIds = await new ObjectId();
+			testKurs.userIds = await new ObjectId();
+		});
+		// ______7________
+		it('not in team, in course, not in class', () => {
+			testTeam.userIds = new ObjectId();
+			testKlasse.userIds = new ObjectId();
+		});
+		// ________8________
+		it('not in team, not in course, not in class', () => {
+			testTeam.userIds = new ObjectId();
+			testKurs.userIds = new ObjectId();
+			testKlasse.userIds = new ObjectId();
+		});
+		after(async () => {
+			testObjects.cleanup();
+			Team.deleteOne({ _id: testTeam._id });
+		});
+	});
 
 	describe('team news', () => {
 		it('is deleted after the coresponding team is deleted', async () => {
