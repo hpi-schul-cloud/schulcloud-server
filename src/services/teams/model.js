9e88cd309b6f034945b6c6c171514098d81407ee
Hannes Schurig <hannes.schurig@hpi.de>
2019-05-14 12:14:07 +0200

    unused (old course-related) stuff in teams?

--- src/services/teams/model.js
+++ src/services/teams/model.js
@@ -17,24 +17,6 @@ const getUserGroupSchema = (additional = {}) => {
 	});
 };
 
-/**
- * duration {Number} - the duration of a course lesson
- * startTime {Number] - the start time of a course lesson as milliseconds
- * weekday {Number} - from 0 to 6, the weekday the course take place
- *		(e.g. 0 = monday, 1 = tuesday ... )
- * eventId {String} - id of the event in the external calendar-service
- * room {String} - a specific location for the recurring course lesson, e.g. a room number
- */
-const timeSchema = new Schema({
-	weekday: {
-		type: Number, min: 0, max: 6, required: true,
-	},
-	startTime: { type: Number },
-	duration: { type: Number },
-	eventId: { type: String },
-	room: { type: String },
-});
-
 const teamInvitedUserSchema = new Schema({
 	email: { type: String, required: true },
 	role: { type: String, required: true, enum: ['teamexpert', 'teamadministrator'] },
@@ -60,14 +42,7 @@ const teamsSchema = getUserGroupSchema({
 	invitedUserIds: [teamInvitedUserSchema],
 	description: { type: String, default: '' },
 	classIds: [{ type: Schema.Types.ObjectId, required: true, ref: 'class' }],
-	// substitutionIds: [{ type: Schema.Types.ObjectId, required: true, ref: 'user' }],
-	// todo: add later
-	ltiToolIds: [{ type: Schema.Types.ObjectId, required: true, ref: 'ltiTool' }],
 	color: { type: String, required: true, default: '#ACACAC' },
-	startDate: { type: Date },
-	untilDate: { type: Date },
-	//	shareToken:  { type: String, unique: true },
-	times: [timeSchema],
 	features: [{ type: String, enum: ['isTeam', 'rocketChat'] }],
 	filePermission: [permissionSchema],
 });
