5f6a241f23959577a2a99aa323b27d1192f9ba15
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2019-05-20 15:44:55 +0200

    Finish rewrite testObjects. In future it should implement via interface and classes.

--- test/services/helpers/testObjects.js
+++ test/services/helpers/testObjects.js
@@ -2,115 +2,30 @@ const logger = require('winston');
 
 const serviceHelpers = require('./services');
 
+const warn = (message, pass) => {
+	logger.warn(message);
+	return pass;
+};
+
 module.exports = (app, opt = {
-	schoolId: '0000d186816abba584714c5f', // '584ad186816abba584714c94',
+	schoolId: '0000d186816abba584714c5f',
 }) => {
-	const { teams, testSystem, login } = serviceHelpers(app, opt);
-
-	const accountService = app.service('accounts');
-	const userService = app.service('users');
-	const classesService = app.service('classes');
-	const coursesService = app.service('courses');
-	const registrationPinsService = app.service('registrationPins');
-
-	const createdAccountIds = [];
-	const createdUserIds = [];
-	const createdClasses = [];
-	const createdCourses = [];
-
-	// should rewrite
-	function createTestAccount(accountParameters, system, user) {
-		if (system) accountParameters.systemId = system.id;
-		accountParameters.userId = user._id;
-		return accountService.create(accountParameters)
-			.then((account) => {
-				createdAccountIds.push(account._id.toString());
-				return account;
-			});
-	}
-
-	function createTestUser({
-		// required fields for user
-		firstName = 'Max',
-		lastName = 'Mustermann',
-		email = `max${Date.now()}@mustermann.de`,
-		schoolId = opt.schoolId,
-		accounts = [], // test if it has a effect
-		roles = [],
-		// manual cleanup, e.g. when testing delete:
-		manualCleanup = false,
-	} = {}) {
-		return registrationPinsService.create({ email })
-			.then(registrationPin => registrationPinsService.find({
-				query: { pin: registrationPin.pin, email: registrationPin.email, verified: false },
-			}))
-			.then(() => userService.create({
-				firstName,
-				lastName,
-				email,
-				schoolId,
-				accounts,
-				roles,
-			})).then((user) => {
-				if (!manualCleanup) {
-					createdUserIds.push(user._id.toString());
-				}
-				return user;
-			});
-	}
-
-	function createTestClass({
-		// required fields
-		name = 'testClass',
-		schoolId = opt.schoolId,
-		userIds = [],
-		teacherIds = [],
-		nameFormat = 'static',
-		gradeLevel = undefined,
-	}) {
-		return classesService.create({
-			// required fields for user
-			name,
-			schoolId,
-			userIds,
-			teacherIds,
-			nameFormat,
-			gradeLevel,
-		}).then((res) => {
-			createdClasses.push(res._id.toString());
-			return res;
-		});
-	}
-
-	function createTestCourse({
-		// required fields for base group
-		name = 'testCourse',
-		schoolId = opt.schoolId,
-		userIds = [],
-		classIds = [],
-		teacherIds = [],
-		ltiToolIds = [],
-	}) {
-		return coursesService.create({
-			// required fields for user
-			name,
-			schoolId,
-			userIds,
-			classIds,
-			teacherIds,
-			ltiToolIds,
-		}).then((course) => {
-			createdCourses.push(course._id.toString());
-			return course;
-		});
-	}
+	const {
+		teams,
+		testSystem,
+		login,
+		classes,
+		users,
+		courses,
+		accounts,
+	} = serviceHelpers(app, opt);
 
 	const cleanup = () => {
-		const accountDeletions = createdAccountIds.map(id => accountService.remove(id));
-		const userDeletions = createdUserIds.map(id => userService.remove(id));
+		const accountDeletions = accounts.cleanup(); // createdAccountIds.map(id => accountService.remove(id));
+		const userDeletions = users.cleanup();
 		const systemDeletions = testSystem.cleanup();
-		const classDeletions = createdClasses.map(id => classesService.remove(id));
-		const courseDeletions = createdCourses.map(id => coursesService.remove(id));
+		const classDeletions = classes.cleanup();
+		const courseDeletions = courses.cleanup(); // createdCourses.map(id => coursesService.remove(id));
 		const teamsDeletion = teams.cleanup();
 
 		return Promise.all([teamsDeletion]
@@ -131,19 +46,23 @@ module.exports = (app, opt = {
 
 	const info = () => ({
 		teams: teams.info,
-		users: createdUserIds,
+		users: users.info,
 		testSystem: testSystem.info,
+		classes: classes.info,
+		tempPins: users.tempPinIds,
+		courses: courses.info,
+		accounts: accounts.info,
 	});
 
 	const createTestTeamWithOwner = async () => {
-		const user = await createTestUser();
+		const user = await users.create();
 		const team = await teams.create(user);
 		return { team, user };
 	};
 
 	const setupUser = async () => {
 		// create account
-		const user = await createTestUser();
+		const user = await users.create();
 		// const account = createTestAccount();
 		// fetch jwt
 		const account = {};
@@ -153,17 +72,17 @@ module.exports = (app, opt = {
 
 	return {
 		createTestSystem: testSystem.create,
-		createTestAccount,
-		createTestUser,
-		createTestClass,
-		createTestCourse,
+		createTestAccount: warn('@implement should rewrite', accounts.create),
+		createTestUser: users.create,
+		createTestClass: classes.create,
+		createTestCourse: courses.create,
 		cleanup,
 		generateJWT: login.generateJWT,
 		generateRequestParams: login.generateRequestParams,
-		createdUserIds, // @deprecated use info
+		createdUserIds: warn('@deprecated use info() instat', users.info),
 		teams,
 		createTestTeamWithOwner,
 		info,
-		setupUser,
+		setupUser: warn('@implement should finished', setupUser),
 	};
 };
--- test/services/teams/events/index.test.js
+++ test/services/teams/events/index.test.js
@@ -4,10 +4,9 @@
 const { expect } = require('chai');
 const { ObjectId } = require('mongoose').Types;
 const sleep = require('util').promisify(setTimeout);
-const testObjects = require('../../helpers/testObjects');
-
 
 const app = require('../../../../src/app');
+const testObjects = require('../../helpers/testObjects')(app);
 const { userModel } = require('../../../../src/services/user/model');
 const { teamsModel, teamUserModel } = require('../../../../src/services/teams/model');
 
@@ -51,35 +50,23 @@ const removeUser = id => userModel.findOneAndRemove({ _id: id });
 describe('Test user remove events for teams.', () => {
 	describe('Test if own of the users is removed.', () => {
 		let team;
-		let teamId;
-		let owner;
-		let user;
-		let test;
 
 		before(async () => {
-			owner = await createUser();
-			team = await createTeam(owner);
-			teamId = team._id.toString();
-			test = await testObjects(app);
-			console.log(test.teams);
+			({ team } = await testObjects.createTestTeamWithOwner());
 			return Promise.resolve();
 		});
 
-		after((done) => {
-			// The test should already removed the data, but if not clear it now.
-			removeTeam(team._id);
-			removeUser(owner._id);
-			removeUser(user._id);
-			done();
+		after(async () => {
+			await testObjects.cleanup();
+			return Promise.resolve();
 		});
 
 		it('should remove user from team', async () => {
 			// Test if team has only owner inside the team.
 			expect(team.userIds).to.be.an('array').with.lengthOf(1);
 
-			user = await createUser();
-			const userId = user._id.toString();
-			const teamWithAddedUser = await patchTeam(teamId, user);
+			const user = await testObjects.createTestUser();
+			const teamWithAddedUser = await patchTeam(team._id.toString(), user);
 
 			// Test if patch user into team works.
 			expect(teamWithAddedUser.userIds).to.be.an('array').with.lengthOf(2);
@@ -89,8 +76,10 @@ describe('Test user remove events for teams.', () => {
 			// Execute the primary test with short delay. That the async event process can finished.
 			await sleep(DELAY_TIME);
 
-			const teamWithRemovedUser = await getTeam(teamId);
-			const found = teamWithRemovedUser.userIds.some(teamUser => teamUser.userId.toString() === userId);
+			const teamWithRemovedUser = await getTeam(team._id.toString());
+			const found = teamWithRemovedUser.userIds.some(
+				teamUser => teamUser.userId.toString() === user._id.toString(),
+			);
 			expect(found).to.equal(false);
 			expect(teamWithRemovedUser.userIds).to.be.an('array').with.lengthOf(1);
 		});
@@ -98,33 +87,28 @@ describe('Test user remove events for teams.', () => {
 
 	describe('Test if the last user is removed.', () => {
 		let team;
-		let teamId;
 		let owner;
 
 		before(async () => {
-			owner = await createUser();
-			team = await createTeam(owner);
-			teamId = team._id.toString();
+			({ team, user: owner } = await testObjects.createTestTeamWithOwner());
 			return Promise.resolve();
 		});
 
-		after((done) => {
-			// The test should already removed the data, but if not clear it now.
-			removeTeam(team._id);
-			removeUser(owner._id);
-			done();
+		after(async () => {
+			await testObjects.cleanup();
+			return Promise.resolve();
 		});
 
 		it('should remove team if last user is deleted.', async () => {
 			// Test if team has only owner inside the team.
 			expect(team.userIds).to.be.an('array').with.lengthOf(1);
-			await app.service('users').remove(owner._id);
+			await app.service('users').remove(owner._id.toString());
 
 			// Execute the primary test with short delay. That the async event process can finished.
 			await sleep(DELAY_TIME);
 
-			const notExistingTeam = await getTeam(teamId);
-			expect(notExistingTeam !== undefined).to.equal(true);
+			const notExistingTeam = await getTeam(team._id.toString());
+			expect(notExistingTeam).to.not.be.undefined;
 		});
 	});
 });
