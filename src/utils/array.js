572269f2fe58b2b8f6c0bf4e131cc32e5459f354
Maximilian Grundke <maximiliangrundke@googlemail.com>
2019-05-28 15:03:25 +0200

    Update sort docs

--- src/utils/array.js
+++ src/utils/array.js
@@ -38,16 +38,30 @@ const paginate = (data, params) => {
 	};
 };
 
+/**
+ * Sort an array using mongodb-notation
+ * @param {Array} data the array to sort
+ * @param {String} sortOrder mongodb-like sort order (see examples)
+ * @example
+ * sort([]) => [];
+ * sort([1, 3, 2]) => [1, 2, 3];
+ * sort([1, 3, 2], '-') => [3, 2, 1];
+ * sort([{a: 2}, {a: 1}], 'a') => [{a: 1}, {a: 2}];
+ * sort([{b: 4}, {b: 5}], '-b') => [{b: 5}, {b: 4}];
+ */
 const sort = (data, sortOrder) => {
 	if (!data) {
 		return data;
 	}
+	// if not otherwise specified, sort by value (descending):
 	let valueOp = v => v;
 	let descending = false;
+
 	if (typeof sortOrder === 'string') {
 		descending = /^-/.test(sortOrder);
 		const match = /^-?(.*)/.test(sortOrder);
 		if (match) {
+			// sort by attribute value
 			const attributeName = sortOrder.match(/-?(.*)/)[1];
 			if (attributeName.length > 0) {
 				valueOp = item => item[attributeName];
@@ -55,6 +69,7 @@ const sort = (data, sortOrder) => {
 		}
 	}
 	return data.sort((a, b) => {
+		// use string.localeCompare to work on Strings, dates, and numbers
 		if (descending) {
 			return (`${valueOp(b)}`).localeCompare(`${valueOp(a)}`);
 		}
