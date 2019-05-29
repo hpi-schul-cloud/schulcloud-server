1a3b6989d3c14b102d5f9ec7f1e5c9c537e36932
Martin Malchow <martin.malchow@hpi.de>
2019-05-29 18:16:35 +0200

    Cord Carl is teacher in TestObjects school. Use school without teachers for discoverable test

--- test/services/user/unit.test.js
+++ test/services/user/unit.test.js
@@ -137,14 +137,17 @@ describe('publicTeachers service', () => {
 		testStudent = await testObjects.createTestUser({
 			roles: ['student'],
 			discoverable: false,
+			schoolId: '0000d186816abba584714c5f',
 		});
 		testTeacherDiscoverable = await testObjects.createTestUser({
 			roles: ['teacher'],
 			discoverable: true,
+			schoolId: '0000d186816abba584714c5f',
 		});
 		testTeacherNotDiscoverable = await testObjects.createTestUser({
 			roles: ['teacher'],
 			discoverable: false,
+			schoolId: '0000d186816abba584714c5f',
 		});
 		assert.ok(userService);
 		assert.ok(publicTeachersService);
@@ -157,7 +160,7 @@ describe('publicTeachers service', () => {
 	it('without IGNORE_DISCOVERABILITY: find 1 discoverable teacher but not find other users', async () => {
 		// test with IGNORE_DISCOVERABILITY = false
 		process.env.IGNORE_DISCOVERABILITY = 'false';
-		result = await publicTeachersService.find({ query: { schoolId: testObjects.options.schoolId } });
+		result = await publicTeachersService.find({ query: { schoolId: '0000d186816abba584714c5f' } });
 		expect(result.total).to.equal(1);
 		expect(result.data[0]._id.toString()).to.equal(testTeacherDiscoverable._id.toString());
 		expect(result.data[0]._id.toString()).to.not.equal(testStudent._id.toString());
@@ -167,7 +170,7 @@ describe('publicTeachers service', () => {
 	it('with IGNORE_DISCOVERABILITY: find all 3 users ignoring their discoverable setting', async () => {
 		// test with IGNORE_DISCOVERABILITY = true
 		process.env.IGNORE_DISCOVERABILITY = 'true';
-		result = await publicTeachersService.find({ query: { schoolId: testObjects.options.schoolId } });
+		result = await publicTeachersService.find({ query: { schoolId: '0000d186816abba584714c5f' } });
 		expect(result.total).to.equal(3);
 	});
 
