4eef414d2464d79921d9a1998d2e36f22b8c340f
Maximilian Grundke <maximiliangrundke@googlemail.com>
2019-06-06 16:49:43 +0200

    Revert "Enable custom team roles in team creation"
    This reverts commit aed7d18e0e341a30f8817d154b5df8f35ec63493.

--- test/services/helpers/services/teams.js
+++ test/services/helpers/services/teams.js
@@ -1,28 +1,35 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable new-cap */
 const { teamsModel, teamUserModel } = require('../../../../src/services/teams/model');
-const Role = require('../../../../src/services/role/model');
 
 let createdTeamIds = [];
 
-const createTeamUser = async (userId, schoolId, roleName = 'teammember') => {
-	const roleId = (await Role.findOne({ name: roleName }))._id;
-	return (new teamUserModel({ role: roleId, schoolId, userId }))._doc;
+// todo replace later over load from db
+const roleId = {
+	teamowner: '5bb5c62bfb457b1c3c0c7e14',
+	teamadministrator: '5bb5c545fb457b1c3c0c7e13',
+	teamleader: '5bb5c49efb457b1c3c0c7e11',
+	teamexpert: '5bb5c391fb457b1c3c0c7e10',
+	teammember: '5bb5c190fb457b1c3c0c7e0f',
 };
 
-const createTeam = opt => async owner => teamsModel.create({
+const createTeamUser = (userId, schoolId, roleName = 'teammember') => (
+	new teamUserModel({ role: roleId[roleName], schoolId, userId })
+)._doc;
+
+const createTeam = opt => owner => teamsModel.create({
 	name: `${Date.now()}_test`,
 	schoolId: opt.schoolId,
 	schoolIds: [opt.schoolId],
-	userIds: [await createTeamUser(owner._id, opt.schoolId, 'teamowner')],
+	userIds: [createTeamUser(owner._id, opt.schoolId, 'teamowner')],
 }).then((team) => {
 	createdTeamIds.push(team._id.toString());
 	return team._doc;
 });
 
-const addTeamUserToTeam = opt => async (id, user, teamRoleName) => teamsModel.findOneAndUpdate(
+const addTeamUserToTeam = opt => (id, user, teamRoleName) => teamsModel.findOneAndUpdate(
 	{ _id: id },
-	{ $push: { userIds: await createTeamUser(user._id, opt.schoolId, teamRoleName) } },
+	{ $push: { userIds: createTeamUser(user._id, opt.schoolId, teamRoleName) } },
 	{ new: true },
 ).lean().exec();
 
@@ -44,6 +51,8 @@ module.exports = (app, opt) => ({
 	create: createTeam(opt),
 	getById: getTeamById,
 	addTeamUserToTeam: addTeamUserToTeam(opt),
+	// service: teamServices(app),
+	roleId,
 	createUser: createTeamUser,
 	removeOne: removeOneTeam,
 	removeMany: removeManyTeams,
