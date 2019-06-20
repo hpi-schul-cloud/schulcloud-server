7421466829803dd423bf31e157fbadf19940f827
Feldtkeller <Thomas.Feldtkeller@hpi.uni-potsdam.de>
2019-06-20 16:23:26 +0200

    updated importpaths for moved migration

--- migrations/done/subfolderrights.js
+++ migrations/done/subfolderrights.js
@@ -3,10 +3,10 @@
 const ran = true; // set to true to exclude migration
 const name = 'Add permission for students to create files in course subfolders.';
 
-const database = require('../src/utils/database');
+const database = require('../../src/utils/database');
 
-const RoleModel = require('../src/services/role/model');
-const { FileModel } = require('../src/services/fileStorage/model.js');
+const RoleModel = require('../../src/services/role/model');
+const { FileModel } = require('../../src/services/fileStorage/model.js');
 
 
 const run = async (dry) => {
