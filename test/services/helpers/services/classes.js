04a033987da324c6f038a1336b71769187f2dbfd
Maximilian Grundke <maximiliangrundke@googlemail.com>
2019-06-26 16:11:51 +0200

    Integrate class helpers into testObjects

--- test/services/helpers/services/classes.js
+++ test/services/helpers/services/classes.js
@@ -27,8 +27,72 @@ const cleanup = app => () => {
 	return ids.map(id => app.service('classes').remove(id));
 };
 
+const createByName = app => async ([gradeLevelName, className, schoolId]) => {
+	const gradeLevels = await app.service('gradeLevels').find({
+		query: {
+			name: gradeLevelName,
+		},
+		paginate: false,
+	});
+	let classObject;
+	if (gradeLevels.length > 0) {
+		classObject = {
+			schoolId,
+			nameFormat: 'gradeLevel+name',
+			gradeLevel: gradeLevels[0]._id,
+			name: className,
+		};
+	} else {
+		classObject = {
+			schoolId,
+			nameFormat: 'static',
+			name: className,
+		};
+	}
+	const klass = await app.service('classes').create(classObject);
+	createdClassesIds.push(klass._id);
+};
+
+const findByName = app => async ([gradeLevelName, className]) => {
+	const gradeLevels = await app.service('gradeLevels').find({
+		query: {
+			name: gradeLevelName,
+		},
+		paginate: false,
+	});
+	let classObject;
+	if (gradeLevels.length > 0) {
+		[classObject] = await app.service('classes').find({
+			query: {
+				nameFormat: 'gradeLevel+name',
+				gradeLevel: gradeLevels[0]._id,
+				name: className,
+			},
+			paginate: false,
+		});
+	} else {
+		[classObject] = await app.service('classes').find({
+			query: {
+				nameFormat: 'static',
+				name: className,
+			},
+			paginate: false,
+		});
+	}
+	return classObject;
+};
+
+const deleteByName = app => async ([gradeLevelName, className]) => {
+	const classObject = await findByName(app)([gradeLevelName, className]);
+	await app.service('classes').remove(classObject._id);
+	createdClassesIds.splice(createdClassesIds.find(i => i.toString() === classObject._id.toString()));
+};
+
 module.exports = (app, opt) => ({
-	create: createTestClass(app, opt),
 	cleanup: cleanup(app),
+	create: createTestClass(app, opt),
+	createByName: createByName(app),
+	deleteByName: deleteByName(app),
+	findByName: findByName(app),
 	info: createdClassesIds,
 });
--- test/services/sync/strategies/CSVSyncer/helper.js
+++ test/services/sync/strategies/CSVSyncer/helper.js
@@ -1,70 +1,9 @@
 const { userModel } = require('../../../../../src/services/user/model.js');
 const accountModel = require('../../../../../src/services/account/model.js');
 
-const app = require('../../../../../src/app');
-
 const deleteUser = async (email = 'foo@bar.baz') => {
 	await userModel.deleteOne({ email });
 	await accountModel.deleteOne({ username: email });
-};
-
-const createClass = async ([gradeLevelName, className, schoolId]) => {
-	const gradeLevels = await app.service('gradeLevels').find({
-		query: {
-			name: gradeLevelName,
-		},
-		paginate: false,
-	});
-	let classObject;
-	if (gradeLevels.length > 0) {
-		classObject = {
-			schoolId,
-			nameFormat: 'gradeLevel+name',
-			gradeLevel: gradeLevels[0]._id,
-			name: className,
-		};
-	} else {
-		classObject = {
-			schoolId,
-			nameFormat: 'static',
-			name: className,
-		};
-	}
-	await app.service('classes').create(classObject);
-};
-
-const findClass = async ([gradeLevelName, className]) => {
-	const gradeLevels = await app.service('gradeLevels').find({
-		query: {
-			name: gradeLevelName,
-		},
-		paginate: false,
-	});
-	let classObject;
-	if (gradeLevels.length > 0) {
-		[classObject] = await app.service('classes').find({
-			query: {
-				nameFormat: 'gradeLevel+name',
-				gradeLevel: gradeLevels[0]._id,
-				name: className,
-			},
-			paginate: false,
-		});
-	} else {
-		[classObject] = await app.service('classes').find({
-			query: {
-				nameFormat: 'static',
-				name: className,
-			},
-			paginate: false,
-		});
-	}
-	return classObject;
-};
-
-const deleteClass = async ([gradeLevelName, className]) => {
-	const classObject = await findClass([gradeLevelName, className]);
-	await app.service('classes').remove(classObject._id);
 };
 
 class MockEmailService {
@@ -80,8 +19,5 @@ class MockEmailService {
 
 module.exports = {
 	deleteUser,
-	createClass,
-	findClass,
-	deleteClass,
 	MockEmailService,
 };
