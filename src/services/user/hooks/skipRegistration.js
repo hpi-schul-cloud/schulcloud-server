dc0694e1cde4a9d1e37e2217a3d34a764b43abab
Feldtkeller <Thomas.Feldtkeller@hpi.uni-potsdam.de>
2019-06-27 13:52:49 +0200

    add permissions

--- src/services/user/hooks/skipRegistration.js
+++ src/services/user/hooks/skipRegistration.js
@@ -2,15 +2,11 @@ const hooks = require('feathers-hooks-common');
 const auth = require('@feathersjs/authentication');
 const globalHooks = require('../../../hooks');
 
-const testHook = (hook) => {
-	return Promise.resolve(hook);
-}
-
 exports.before = {
 	all: [auth.hooks.authenticate('jwt')],
 	find: [hooks.disallow()],
 	get: [hooks.disallow()],
-	create: [testHook, globalHooks.restrictToCurrentSchool],
+	create: [globalHooks.restrictToCurrentSchool],
 	update: [hooks.disallow()],
 	patch: [hooks.disallow()],
 	remove: [hooks.disallow()],
