78a6593e655ee871ff54219038d5f9192006a061
Feldtkeller <Thomas.Feldtkeller@hpi.uni-potsdam.de>
2019-06-27 13:53:29 +0200

    finish validation, overhaul structure

--- src/services/user/services/SkipRegistration.js
+++ src/services/user/services/SkipRegistration.js
@@ -1,4 +1,37 @@
-const { BadRequest } = require('@feathersjs/errors');
+const { BadRequest, Forbidden } = require('@feathersjs/errors');
+
+const validateRequest = (data, targetUser, actingUser) => {
+	// check persmissison
+	const targetIsStudent = targetUser.roles[0].name === 'student';
+	const targetIsTeacher = targetUser.roles[0].name === 'teacher';
+	let hasPermission = false;
+
+	if (targetIsStudent && actingUser.permissions.includes('STUDENT_SKIP_REGISTRATION')) {
+		hasPermission = true;
+	}
+	if (targetIsTeacher && actingUser.permissions.includes('TEACHER_SKIP_REGISTRATION')) {
+		hasPermission = true;
+	}
+	if (!hasPermission) return Promise.reject(new Forbidden('you do not have permission to do this!'));
+
+	// sanitize
+	if (!((data.parent_privacyConsent && data.parent_termsOfUseConsent)
+		|| (data.privacyConsent && data.termsOfUseConsent))) {
+		return Promise.reject(new BadRequest('you have to set valid consents!'));
+	}
+	if (!data.password) return Promise.reject(new BadRequest('you have to set a password!'));
+	if (targetIsStudent && !data.birthday) return Promise.reject(new BadRequest('students require a birtdate'));
+
+	// todo: what status code?
+	if (!targetUser.importHash) return Promise.reject(new BadRequest('this user is not viable for registration'));
+	return Promise.resolve(true);
+};
+
+const createAccount = (data, targetUser) => Promise.resolve(data, targetUser);
+
+const updateConsent = (data, targetUser) => Promise.resolve(data, targetUser);
+
+const updateUserBirthdate = (data, targetUser) => Promise.resolve(data, targetUser);
 
 class SkipRegistrationService {
 	constructor() {
@@ -6,20 +39,22 @@ class SkipRegistrationService {
 	}
 
 	async create(data, params) {
-		// get target user
-		const targetUser = await this.app.service('users').get(params.route.userid);
+		try {
+			const targetUser = await this.app.service('users').get(params.route.userid,
+				{ query: { $populate: 'roles' } });
+			const actingUser = await this.app.service('users').get(params.route.userid);
+			await validateRequest(data, targetUser, actingUser);
 
-		// sanitize
-		if (!((data.parent_privacyConsent && data.parent_termsOfUseConsent)
-			|| (data.privacyConsent && data.termsOfUseConsent))) {
-			return Promise.reject(new BadRequest('you have to set valid consents!'));
+			await Promise.all(createAccount, updateConsent, updateUserBirthdate);
+
+			return Promise.resolve(data);
+		} catch (err) {
+			throw err;
 		}
-		if (!data.password) return Promise.reject(new BadRequest('you have to set a password!'));
-		// check student birthdate
-		// check persmisison, importhash
+
 		// create account
 		// set consents
-		return Promise.resolve(data);
+		// update birthdate
 	}
 
 	setup(app) {
