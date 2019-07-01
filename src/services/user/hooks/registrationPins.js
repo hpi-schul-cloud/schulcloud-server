81ef0280ea2a37eff4350d3be2990b8ea2df681a
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2019-07-01 14:01:10 +0200

    resolve merge errors

--- src/services/user/hooks/registrationPins.js
+++ src/services/user/hooks/registrationPins.js
@@ -48,24 +48,31 @@ Dein ${shortTitle} Team`;
 
 const checkAndVerifyPin = (hook) => {
 	if (hook.result.data.length === 1 && hook.result.data[0].verified === false) {
-		return hook.app.service('registrationPins').patch(hook.result.data[0]._id, { verified: true }).then(() => Promise.resolve(hook));
+		return hook.app
+			.service('registrationPins')
+			.patch(hook.result.data[0]._id, { verified: true })
+			.then(() => Promise.resolve(hook));
+	}
+	return Promise.resolve(hook);
+};
+
 const mailToLowerCase = (hook) => {
 	if (hook.data) {
-			hook.data.email = hook.data.email.toLowerCase();
 		if (hook.data.email) {
+			hook.data.email = hook.data.email.toLowerCase();
 		}
-			hook.data.parent_email = hook.data.parent_email.toLowerCase();
+
 		if (hook.data.parent_email) {
+			hook.data.parent_email = hook.data.parent_email.toLowerCase();
 		}
-			hook.data.student_email = hook.data.student_email.toLowerCase();
+
 		if (hook.data.student_email) {
+			hook.data.student_email = hook.data.student_email.toLowerCase();
 		}
 	}
 	return Promise.resolve(hook);
 };
-	}
-	return Promise.resolve(hook);
-};
+
 
 const mailPin = (hook) => {
 	if (!(hook.data || {}).silent) {
