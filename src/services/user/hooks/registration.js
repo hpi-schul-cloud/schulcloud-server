2f382c3498f662c60993153f810b94666a33aeb1
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2019-07-02 10:11:39 +0200

    replace function with reference to other file

--- src/services/user/hooks/registration.js
+++ src/services/user/hooks/registration.js
@@ -1,20 +1,4 @@
-const auth = require('feathers-authentication');
-const globalHooks = require('../../../hooks');
-
-const mailToLowerCase = (context) => {
-	if (context.data) {
-		if (context.data.email) {
-			context.data.email = context.data.email.toLowerCase();
-		}
-		if (context.data.parent_email) {
-			context.data.parent_email = context.data.parent_email.toLowerCase();
-		}
-		if (context.data.student_email) {
-			context.data.student_email = context.data.student_email.toLowerCase();
-		}
-	}
-	return context;
-};
+const { mailToLowerCase } = require('./global');
 
 exports.before = {
 	all: [],
