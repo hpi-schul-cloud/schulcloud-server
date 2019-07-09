8ae87f217d3c5034374a5b15412a0d12ff0cf062
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2019-07-09 15:22:07 +0200

    workaround to get all messages

--- src/logger/index.js
+++ src/logger/index.js
@@ -1,4 +1,5 @@
 const winston = require('winston');
+const SPLAT = Symbol.for('splat');
 
 let logLevel;
 
@@ -14,12 +15,40 @@ switch (process.env.NODE_ENV) {
 		logLevel = 'info';
 }
 
+// const all = winston.format(function () { return {message: 'abc'}}); //Array.from(arguments).join(' ')} });
+
+function formatObject(param) {
+	if (typeof param === 'object') {
+		return JSON.stringify(param);
+	}
+	return param;
+}
+
+// workaround to call log functions with multiple message parameter.
+// example: logger.info('first', 'secound', 'some other string')
+const all = winston.format((info) => {
+	const splat = info[SPLAT] || [];
+
+	const isSplatTypeMessage = typeof info.message === 'string'
+		&& (info.message.includes('%s') || info.message.includes('%d') || info.message.includes('%j'));
+	if (isSplatTypeMessage) {
+		return info;
+	}
+	const message = formatObject(info.message);
+	const rest = splat
+		.map(formatObject)
+		.join(' ');
+	info.message = `${message} ${rest}`;
+	return info;
+});
+
 const logger = winston.createLogger({
 	levels: winston.config.syslog.levels,
 	format: winston.format.combine(
+		all(),
 		winston.format.timestamp(), // adds current timestamp
 		winston.format.ms(),	// adds time since last log
-		winston.format.simple(), // output as string. Use 'winston.format.prettyPrint()' for well formated json
+		winston.format.prettyPrint(), // output as string. Use 'winston.format.prettyPrint()' for well formated json
 	),
 	transports: [
 		new winston.transports.Console({
