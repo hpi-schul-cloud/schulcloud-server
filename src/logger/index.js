c7b54b02118d186d37b6986511dad538cac2e3c6
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2019-07-12 11:52:48 +0200

    output string instead of json

--- src/logger/index.js
+++ src/logger/index.js
@@ -47,7 +47,7 @@ const logger = winston.createLogger({
 		all(),
 		winston.format.timestamp(), // adds current timestamp
 		winston.format.ms(),	// adds time since last log
-		winston.format.prettyPrint(), // output as prettyfied Json. Use 'winston.format.simple()' for output string
+		winston.format.simple(), // output as string. Use 'winston.format.simple()' for output prettyfied json
 	),
 	transports: [
 		new winston.transports.Console({
