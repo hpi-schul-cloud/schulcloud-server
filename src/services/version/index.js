90779b1181316e38a33f38e5b94c04de690eb47e
Tobias Wollowski <tobias.wollowski@hpi.de>
2019-07-15 09:41:03 +0200

    fix info service

--- src/services/version/index.js
+++ src/services/version/index.js
@@ -1,20 +1,32 @@
 const getRepoInfo = require('git-repo-info');
 const express = require('express');
 
+const packages = require('../../../package.json');
+
 const router = express.Router();
 
-const fields = ['abbreviatedSha', 'sha', 'branch', 'tag'];
+const fields = [
+	'abbreviatedSha',
+	'sha',
+	'branch',
+	'tag',
+	'lastTag',
+	'committerDate',
+	'commitsSinceLastTag',
+	'commitMessage',
+];
 
 router.get('/version', (req, res, next) => {
 	if (!process.env.SHOW_VERSION) {
 		res.send(403);
 	}
 	const info = getRepoInfo();
-	const response = {};
+	const response = { };
+	response.version = packages.version;
 	fields.forEach((field) => {
 		response[field] = info[field];
 	});
-	res.send(response);
+	res.json(response);
 });
 
 module.exports = router;
