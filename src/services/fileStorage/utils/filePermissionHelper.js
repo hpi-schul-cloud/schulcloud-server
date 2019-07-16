5c6981c752fb305b9bc0251d316ceffcaf7d28b4
znnr <alexander@zenner.org>
2019-07-16 15:42:47 +0200

    WIP refactor checkPermissions

--- src/services/fileStorage/utils/filePermissionHelper.js
+++ src/services/fileStorage/utils/filePermissionHelper.js
@@ -79,15 +79,62 @@ const checkPermissions = permission => async (user, file) => {
 	return false;
 };
 
+const checkPermissionsNew = permission => async (user, file) => {
+	const {
+		permissions,
+		refOwnerModel,
+		owner: { _id: owner },
+	} = file;
+
+
+	// return always true for owner of file
+	if (user.toString() === owner.toString()) {
+		return file;
+	}
+
+	const isMember = checkMemberStatus({ file, user });
+
+	const userPermissions = permissions
+		.find(perm => perm.refId && perm.refId.equals(user));
+
+	// User is no member of team or course
+	// and file has no explicit user permissions (sharednetz files)
+	if (!isMember && !userPermissions) {
+		return Promise.reject();
+	}
+
+	const isSubmission = await submissionModel.findOne({ fileIds: file._id }).lean().exec();
+
+	// or legacy course model
+	// TODO: Check member status of teacher if submission
+	if (refOwnerModel === 'course' || isSubmission) {
+		const userObject = await userModel.findOne({ _id: user }).populate({ path: 'roles' }).exec();
+		const isStudent = userObject.roles.find(role => role.name === 'student');
+
+		if (isStudent) {
+			const rolePermissions = permissions.find(
+				perm => perm.refId && perm.refId.toString() === isStudent._id.toString(),
+			);
+
+			return rolePermissions[permission] ? file : Promise.reject();
+		}
+		return file;
+	}
+
+	if (userPermissions) {
+		return userPermissions[permission] ? file : Promise.reject();
+	}
+
+	return Promise.reject();
+};
+
 const getAllowedFiles = permission => (userId, files, app) => {
-	const fileCheck = checkPermissions(permission);
+	const fileCheck = checkPermissionsNew(permission);
 
 	const permissionPromises = files.map(file => fileCheck(userId, file));
 
 	return Promise.all([checkTeamPermissionsNew(userId, files, permission, app), ...permissionPromises])
-		.then(([allowedFilesTeam, ...allowedFilesMask]) => {
-			const allowedFiles = files.filter((_, index) => allowedFilesMask[index]);
-
+		.then(([allowedFilesTeam, ...allowedFiles]) => {
 			const teamFiles = allowedFilesTeam
 				.filter(teamFile => !allowedFiles
 					.some(file => file._id.toString() === teamFile._id.toString()));
