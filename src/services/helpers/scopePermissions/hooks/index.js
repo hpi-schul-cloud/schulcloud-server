7ba2bd6ee80240dbbf9f7b45b2cee0842b9a65d5
Feldtkeller <thomas.feldtkeller@hpi.de>
2019-07-18 16:03:00 +0200

    rejectQueringOtherUsers for scopelist

--- src/services/helpers/scopePermissions/hooks/index.js
+++ src/services/helpers/scopePermissions/hooks/index.js
@@ -1,5 +1,12 @@
 const { Forbidden, BadRequest } = require('@feathersjs/errors');
 
+const resolveScope = (context) => {
+	const scopeName = (context.path || '').match(/^\/?(\w+)\//)[1];
+	const { scopeId } = context.params.route;
+
+	return { scopeName, scopeId };
+};
+
 const rejectQueryingOtherUsers = (context) => {
 	if (context.params === undefined || context.params.account === undefined) {
 		throw new BadRequest('Expected authentication.');
@@ -7,13 +14,21 @@ const rejectQueryingOtherUsers = (context) => {
 	if (!['get', 'find'].includes(context.method)) {
 		throw new BadRequest('Only GET and FIND methods are allowed.');
 	}
-	if (context.method === 'get' && context.id === undefined) {
-		throw new BadRequest('Expected userId.');
+
+	const { scopeName, scopeId } = resolveScope(context);
+	let targetUserId;
+	if (scopeName === 'users') {
+		targetUserId = scopeId;
+	} else {
+		if (context.method === 'get' && context.id === undefined) {
+			throw new BadRequest('Expected userId.');
+		}
+		if (context.method === 'find' && context.params.query === undefined) {
+			throw new BadRequest('Expected query for userId.');
+		}
+		targetUserId = context.id || context.params.query.userId;
 	}
-	if (context.method === 'find' && context.params.query === undefined) {
-		throw new BadRequest('Expected query for userId.');
-	}
-	const targetUserId = context.id || context.params.query.userId;
+
 	const currentUserId = context.params.account.userId.toString();
 	if (targetUserId === undefined || targetUserId !== currentUserId) {
 		throw new Forbidden('Requested and requesting userIds do not match.');
@@ -25,8 +40,7 @@ const lookupScope = async (context) => {
 	if (context.params === undefined || context.params.route === undefined || context.path === undefined) {
 		throw new BadRequest('Missing required params.');
 	}
-	const scopeName = context.path.match(/^\/?(\w+)\//)[1];
-	const { scopeId } = context.params.route;
+	const { scopeName, scopeId } = resolveScope(context);
 	if (scopeName === undefined || scopeId === undefined) {
 		throw new BadRequest('Cannot find scope name or scopeId.');
 	}
