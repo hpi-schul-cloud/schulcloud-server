f67d176b9049a36a8c5856b90c0ad50f3ee3bcf9
Maximilian Grundke <maximiliangrundke@googlemail.com>
2019-07-24 09:56:33 +0200

    Test status route

---
+++ test/services/school/maintenance.integration.test.js
@@ -0,0 +1,75 @@
+const { expect } = require('chai');
+const app = require('./../../../src/app');
+const { cleanup } = require('../helpers/testObjects')(app);
+const { create: createSchool } = require('./../helpers/services/schools')(app);
+const { generateRequestParamsFromUser } = require('./../helpers/services/login')(app);
+const { create: createUser } = require('./../helpers/services/users')(app);
+const { create: createSystem } = require('./../helpers/services/testSystem')(app);
+const { create: createYear } = require('./../helpers/services/years');
+
+describe('school maintenance mode', () => {
+	const maintenanceService = app.service('schools/:schoolId/maintenance');
+
+	let server;
+
+	before((done) => {
+		server = app.listen(0, done);
+	});
+
+	after((done) => {
+		server.close(done);
+	});
+
+	describe('status route', () => {
+		describe('for non-LDAP schools', () => {
+			it('should give information about the current and next school year', async () => {
+				const currentYear = await createYear();
+				const nextYear = await createYear();
+				maintenanceService.years = [currentYear, nextYear];
+				const school = await createSchool({ currentYear });
+				const user = await createUser({ schoolId: school._id, roles: 'administrator' });
+				const params = await generateRequestParamsFromUser(user);
+				params.route = { schoolId: school._id.toString() };
+				params.query = {};
+
+				const result = await maintenanceService.find(params);
+				expect(result).to.not.equal(undefined);
+				expect(result.currentYear._id.toString()).to.equal(currentYear._id.toString());
+				expect(result.nextYear._id.toString()).to.equal(nextYear._id.toString());
+				expect(result.schoolUsesLdap).to.equal(false);
+				expect(result.maintenance.active).to.equal(false);
+			});
+		});
+
+		describe('for LDAP schools', () => {
+			it('should work as expected', async () => {
+				const currentYear = await createYear();
+				const nextYear = await createYear();
+				maintenanceService.years = [currentYear, nextYear];
+				const ldapSystem = await createSystem({
+					type: 'ldap',
+					ldapConfig: {
+						active: true,
+					},
+				});
+				const school = await createSchool({ currentYear, systems: [ldapSystem] });
+				const user = await createUser({ schoolId: school._id, roles: 'administrator' });
+				const params = await generateRequestParamsFromUser(user);
+				params.route = { schoolId: school._id.toString() };
+				params.query = {};
+
+				const result = await maintenanceService.find(params);
+				expect(result).to.not.equal(undefined);
+				expect(result.currentYear._id.toString()).to.equal(currentYear._id.toString());
+				expect(result.nextYear._id.toString()).to.equal(nextYear._id.toString());
+				expect(result.schoolUsesLdap).to.equal(true);
+				expect(result.maintenance.active).to.equal(false);
+			});
+		});
+	});
+
+	afterEach(async () => {
+		maintenanceService.years = [];
+		await cleanup();
+	});
+});
