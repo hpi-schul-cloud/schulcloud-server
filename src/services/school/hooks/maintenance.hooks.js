9a3d76ea0673bd56a6e4c181ececf3faadaf3854
Maximilian Grundke <maximiliangrundke@googlemail.com>
2019-07-29 14:07:53 +0200

    Simplify imports

--- src/services/school/hooks/maintenance.hooks.js
+++ src/services/school/hooks/maintenance.hooks.js
@@ -1,22 +1,26 @@
 const auth = require('@feathersjs/authentication');
-const hooks = require('feathers-hooks-common');
-const globalHooks = require('../../../hooks');
+const { disallow } = require('feathers-hooks-common');
+const {
+	ifNotLocal,
+	restrictToCurrentSchool,
+	hasPermission,
+} = require('../../../hooks');
 const lookupSchool = require('./lookupSchool');
 
 module.exports = {
 	before: {
 		all: [
-			globalHooks.ifNotLocal(auth.hooks.authenticate('jwt')),
-			globalHooks.ifNotLocal(globalHooks.restrictToCurrentSchool),
+			ifNotLocal(auth.hooks.authenticate('jwt')),
+			ifNotLocal(restrictToCurrentSchool),
 			lookupSchool,
 		],
 		find: [],
 		create: [
-			globalHooks.ifNotLocal(globalHooks.hasPermission('SCHOOL_EDIT')),
+			ifNotLocal(hasPermission('SCHOOL_EDIT')),
 		],
-		update: [hooks.disallow()],
-		get: [hooks.disallow()],
-		patch: [hooks.disallow()],
-		remove: [hooks.disallow()],
+		update: [disallow()],
+		get: [disallow()],
+		patch: [disallow()],
+		remove: [disallow()],
 	},
 };
