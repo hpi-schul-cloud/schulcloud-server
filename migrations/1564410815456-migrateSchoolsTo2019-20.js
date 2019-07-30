98937b55d10c69edd5f0b283583ecc8fa2036f14
Maximilian Grundke <maximiliangrundke@googlemail.com>
2019-07-30 09:41:39 +0200

    Rewrite migration

--- migrations/1564410815456-migrateSchoolsTo2019-20.js
+++ migrations/1564410815456-migrateSchoolsTo2019-20.js
@@ -1,27 +1,37 @@
-const { ObjectId } = require('mongoose').Types;
-
 const { connect, close } = require('../src/utils/database');
+// We need to import the systems model to register it with mongoose
+/* eslint-disable-next-line no-unused-vars */
 const System = require('../src/services/system/model');
-const { schoolModel: School } = require('../src/services/school/model');
+const { schoolModel: School, yearModel: Year } = require('../src/services/school/model');
+const { schoolUsesLdap } = require('../src/services/school/maintenance');
 const { info } = require('../src/logger');
 
 module.exports = {
 	up: async function up() {
 		await connect();
-		const ldapSystems = await System.find({ type: 'ldap' }).select('_id').lean().exec();
-		const ldapIds = ldapSystems.map(s => s._id);
 
-		// schools with active LDAP systems are set into maintenance mode effective immediately
-		info('Migrating LDAP schools...');
-		const ldapResult = await School.updateMany({ systems: { $in: ldapIds } }, { inMaintenanceSince: Date.now() });
-		info(ldapResult);
+		info('Fetching next school year...');
+		const nextYear = await Year.findOne({ name: '2019/20' }).lean().exec();
+		info('=>', nextYear);
 
-		// all other schools are migrated to the next year directly
-		info('Migrating non-LDAP schools');
-		const nonLdapResult = await School.updateMany({ systems: { $not: { $in: ldapIds } } }, {
-			currentYear: new ObjectId('5d2ee323d14ce9844e33f51e'), // 2019/20
-		});
-		info(nonLdapResult);
+		info('Fetching schools...');
+		const schools = await School.find({}).select(['name']).populate(['systems']);
+		info(`Got ${schools.length} schools.`);
+
+		for (const school of schools) {
+			info(`Migrating ${school.name} (${school._id})...`);
+			if (schoolUsesLdap(school)) {
+				info('School uses LDAP');
+				// schools with active LDAP systems are set into maintenance mode effective immediately
+				const result = await School.updateOne({ _id: school._id }, { inMaintenanceSince: Date.now() });
+				info(result);
+			} else {
+				info('School does not use LDAP');
+				// all other schools are migrated to the next year directly
+				const result = await School.updateOne({ _id: school._id }, { currentYear: nextYear._id });
+				info(result);
+			}
+		}
 
 		info('Done.');
 		await close();
