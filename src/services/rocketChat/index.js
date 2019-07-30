b19b3962c8c0ad428f1b7dcd5cee103e5eaa51e1
Feldtkeller <thomas.feldtkeller@hpi.de>
2019-07-30 13:18:16 +0200

    fix regex

--- src/services/rocketChat/index.js
+++ src/services/rocketChat/index.js
@@ -56,7 +56,7 @@ const makeStringRCConform = (input) => {
 		ä: 'ae', Ä: 'Ae', ö: 'oe', Ö: 'Oe', ü: 'ue', Ü: 'Ue', ' ': '-', ß: 'ss',
 	};
 	const inputResolvedUmlauts = input.replace(/[äÄöÖüÜß ]/g, match => dict[match]);
-	return inputResolvedUmlauts.replace(/[^\w\d.-_]/g, '_');
+	return inputResolvedUmlauts.replace(/[^\w\d.\-_]/g, '_');
 };
 
 /**
