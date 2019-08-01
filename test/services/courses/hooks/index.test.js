8d9111aac3cfaf7c10d065c7110d843951bfc158
Feldtkeller <thomas.feldtkeller@hpi.de>
2019-08-01 11:17:00 +0200

    const for magic numbers

--- test/services/courses/hooks/index.test.js
+++ test/services/courses/hooks/index.test.js
@@ -3,13 +3,16 @@ const app = require('../../../../src/app');
 const testObjects = require('../../helpers/testObjects')(app);
 const { restrictChangesToArchivedCourse } = require('../../../../src/services/user-group/hooks/courses');
 
+const oneHour = 600000;
+const twoDays = 172800000;
+
 describe('course hooks', () => {
 	describe('restrict changes to archived course', () => {
 		const fut = restrictChangesToArchivedCourse;
 
 		it('returns for course that is not expired', async () => {
 			const activeCourse = await testObjects.createTestCourse({
-				untilDate: Date.now() + 600000,
+				untilDate: Date.now() + oneHour,
 			});
 			const result = await fut({ app, method: 'update', id: activeCourse._id });
 			expect(result).to.not.equal(undefined);
@@ -29,15 +32,15 @@ describe('course hooks', () => {
 
 		it('returns when changing untilDate on expired course', async () => {
 			const archivedCourse = await testObjects.createTestCourse({
-				untilDate: Date.now() - 172800000,
+				untilDate: Date.now() - twoDays,
 			});
 			const result = await fut({
 				app,
 				method: 'update',
 				id: archivedCourse._id,
 				data: {
-					startDate: Date.now() - 172800000,
-					untilDate: Date.now() + 600000,
+					startDate: Date.now() - twoDays,
+					untilDate: Date.now() + oneHour,
 				},
 			});
 			expect(result).to.not.equal(undefined);
@@ -49,15 +52,15 @@ describe('course hooks', () => {
 		it('fails when changing other fields of expired course', async () => {
 			try {
 				const archivedCourse = await testObjects.createTestCourse({
-					untilDate: Date.now() - 172800000,
+					untilDate: Date.now() - twoDays,
 				});
 				await fut({
 					app,
 					method: 'update',
 					id: archivedCourse._id,
 					data: {
-						startDate: Date.now() - 172800000,
-						untilDate: Date.now() + 600000,
+						startDate: Date.now() - twoDays,
+						untilDate: Date.now() + oneHour,
 						otherField: 'this is set',
 					},
 				});
