d56bcea4e3b0ccdd4cb78232fd0d3ec36b497a67
Tobias Wollowski <tobias.wollowski@hpi.de>
2019-08-02 14:19:39 +0200

    add school year 20/21 to have next year available

--- migrations/1564748102584-schoolyear_20_21_added.js
+++ migrations/1564748102584-schoolyear_20_21_added.js
@@ -5,19 +5,19 @@ const yearLogic = require('../src/services/school/logic/year');
 
 const logger = require('../src/logger');
 
-const YEAR_2019_20 = '2019/20';
+const YEAR_2020_21 = '2020/21';
 
 module.exports = {
 	up: async function up() {
 		await connect();
-		const existingYear = await Year.findOne({ name: YEAR_2019_20 }).exec();
+		const existingYear = await Year.findOne({ name: YEAR_2020_21 }).exec();
 		if (existingYear) {
 			logger.warn('the year already has been created');
 		} else {
 			const year = new Year({
-				name: YEAR_2019_20,
-				startDate: yearLogic.getDefaultStartDate(YEAR_2019_20),
-				endDate: yearLogic.getDefaultEndDate(YEAR_2019_20),
+				name: YEAR_2020_21,
+				startDate: yearLogic.getDefaultStartDate(YEAR_2020_21),
+				endDate: yearLogic.getDefaultEndDate(YEAR_2020_21),
 			});
 			await year.save();
 			logger.info('the year was created successfully');
@@ -27,7 +27,7 @@ module.exports = {
 
 	down: async function down() {
 		await connect();
-		await Year.deleteOne({ name: YEAR_2019_20 }).exec();
+		await Year.deleteOne({ name: YEAR_2020_21 }).exec();
 		await close();
 	},
 };
