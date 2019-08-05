2635b788fcf17844efff5bc112b35ba502966013
Maximilian Grundke <maximiliangrundke@googlemail.com>
2019-08-05 18:35:15 +0200

    Refactor migration

--- migrations/1565021712225-addToolPermissionsToSuperheroRole.js
+++ migrations/1565021712225-addToolPermissionsToSuperheroRole.js
@@ -4,13 +4,19 @@ const Roles = require('../src/services/role/model');
 module.exports = {
 	up: async function up() {
 		await connect();
-		await Roles.updateOne({ name: 'superhero' }, { $addToSet: { permissions: { $each: ['TOOL_CREATE', 'TOOL_EDIT'] } } });
+		await Roles.updateOne(
+			{ name: 'superhero' },
+			{ $addToSet: { permissions: { $each: ['TOOL_CREATE', 'TOOL_EDIT'] } } },
+		).exec();
 		await close();
 	},
 
 	down: async function down() {
 		await connect();
-		await Roles.updateOne({ name: 'superhero' }, { $pull: { permissions: { $in: ['TOOL_CREATE', 'TOOL_EDIT'] } } });
+		await Roles.updateOne(
+			{ name: 'superhero' },
+			{ $pull: { permissions: { $in: ['TOOL_CREATE', 'TOOL_EDIT'] } } },
+		).exec();
 		await close();
 	},
 };
