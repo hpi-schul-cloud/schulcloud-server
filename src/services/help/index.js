2935ded0b8988983355d11d89b8675ca3fc35f96
Adrian Jost <adrianjost@users.noreply.github.com>
2019-08-20 10:00:34 +0200

    remove mock data

--- src/services/help/index.js
+++ src/services/help/index.js
@@ -2,33 +2,6 @@ const auth = require('@feathersjs/authentication');
 const hooks = require('feathers-hooks-common');
 const { helpDocumentsModel } = require('./model');
 
-const MOCK = [
-    {
-      "title": "Allgemeines",
-      "content": "<a target='_blank' rel='noopener' href='https://schul-cloud-hpi.s3.hidrive.strato.com/Willkommenordner/Presse-und-Oeffentlichkeitsarbeit.pdf'>Presse- und Öffentlichkeitsarbeit [.pdf]</a>"
-    },
-    {
-      "title": "Arbeitsgruppen",
-      "content": "<a target='_blank' rel='noopener' href='https://schul-cloud-hpi.s3.hidrive.strato.com/Willkommenordner/Arbeitsgruppen/Ergebnisse-der-Arbeitsgruppen.pdf'>Ergebnisse der Arbeitsgruppen [.pdf]</a>"
-    },
-    {
-      "title": "Begleitmaterial",
-      "content": "<a target='_blank' rel='noopener' href='https://schul-cloud-hpi.s3.hidrive.strato.com/Willkommenordner/Begleitmaterial/Broschuere_Die-Schul-Cloud-im-Unterricht-Fachuebergreifende-Unterrichtsszenarien-und-Methoden.pdf'>Broschüre: Die Schul-Cloud im Unterricht: Fachübergreifende Unterrichtsszenarien und Methoden [.pdf]</a><br>  <a target='_blank' rel='noopener' href='https://schul-cloud-hpi.s3.hidrive.strato.com/Willkommenordner/Begleitmaterial/Broschuere_Die-Schul-Cloud-im-Unterricht-und-Schulalltag-Mehrwert-und-Voraussetzungen.pdf'>Broschüre: Die Schul-Cloud im Unterricht und Schulalltag: Mehrwert und Voraussetzungen [.pdf]</a><br>  <a target='_blank' rel='noopener' href='https://schul-cloud-hpi.s3.hidrive.strato.com/Willkommenordner/Begleitmaterial/HPI-Schul-Cloud-MOOCs.pdf'>HPI Schul-Cloud MOOCs [.pdf]</a><br>  <a target='_blank' rel='noopener' href='https://schul-cloud-hpi.s3.hidrive.strato.com/Willkommenordner/Begleitmaterial/MINT-EC-Leitfaeden-zur-Einfuehrung-in-die-HPI-Schul-Cloud.pdf'>MINT-EC-Leitfäden zur Einführung in die HPI Schul-Cloud [.pdf]</a><br>"
-    },
-    {
-      "title": "Datenschutz",
-      "content": "<a target='_blank' rel='noopener' href='https://schul-cloud-hpi.s3.hidrive.strato.com/Willkommenordner/Datenschutz/Anlagen-zum-Vertrag.pdf'>Anlagen zum Vertrag [.pdf]</a><br>  <a target='_blank' rel='noopener' href='https://schul-cloud-hpi.s3.hidrive.strato.com/Willkommenordner/Datenschutz/Datenschutz_Checkliste.pdf'>Datenschutz-Checkliste [.pdf]</a><br>  <a target='_blank' rel='noopener' href='https://schul-cloud-hpi.s3.hidrive.strato.com/Willkommenordner/Datenschutz/Datenschutzerklaerung-Muster-Schulen.pdf'>Datenschutzerklärung Muster Schulen [.pdf]</a><br>  <a target='_blank' rel='noopener' href='https://schul-cloud-hpi.s3.hidrive.strato.com/Willkommenordner/Datenschutz/Dienstvereinbarung-Schule-zum-HPI.pdf'>Dienstvereinbarung [.pdf]</a><br>  <a target='_blank' rel='noopener' href='https://schul-cloud-hpi.s3.hidrive.strato.com/Willkommenordner/Datenschutz/Einwilligungserklaerung-Muster-Schulen.pdf'>Einwilligungserklärung Muster Schulen [.pdf]</a><br>  <a target='_blank' rel='noopener' href='https://schul-cloud-hpi.s3.hidrive.strato.com/Willkommenordner/Datenschutz/Einwilligungskonzept.pdf'>Einwilligungskonzept [.pdf]</a><br>  <a target='_blank' rel='noopener' href='https://schul-cloud-hpi.s3.hidrive.strato.com/Willkommenordner/Datenschutz/Nutzungsordnung-HPI-Schule-Schueler.pdf'>Nutzungsordnung zwischen Schule und Schüler [.pdf]</a><br>  <a target='_blank' rel='noopener' href='https://schul-cloud-hpi.s3.hidrive.strato.com/Willkommenordner/Datenschutz/VVT-Muster-Schulen.pdf'>VVT Muster Schulen [.pdf]</a>"
-    },
-    {
-      "title": "Datenschutz-Merkblätter",
-      "content": "<a target='_blank' rel='noopener' href='https://schul-cloud-hpi.s3.hidrive.strato.com/Willkommenordner/DS-Merkblaetter/Datenschutz-Tipps-fuer-SuS.pdf'>Datenschutz-Tipps für SuS [.pdf]</a><br>  <a target='_blank' rel='noopener' href='https://schul-cloud-hpi.s3.hidrive.strato.com/Willkommenordner/DS-Merkblaetter/Merkblatt-E-Mail-Adressen.pdf'>Merkblatt E-Mail-Adressen [.pdf]</a><br>  <a target='_blank' rel='noopener' href='https://schul-cloud-hpi.s3.hidrive.strato.com/Willkommenordner/DS-Merkblaetter/Merkblatt-Urheberrecht.pdf'>Merkblatt Urheberrecht [.pdf]</a><br>  <a target='_blank' rel='noopener' href='https://schul-cloud-hpi.s3.hidrive.strato.com/Willkommenordner/DS-Merkblaetter/Merkblatt-fuer-Lehrkraefte.pdf'>Merkblatt für Lehrkräfte [.pdf]</a><br>  <a target='_blank' rel='noopener' href='https://schul-cloud-hpi.s3.hidrive.strato.com/Willkommenordner/DS-Merkblaetter/Merkblatt-zur-Nutzung-schuleigener-Geraete.pdf'>Merkblatt zur Nutzung schuleigener Geräte [.pdf]</a>"
-    },
-    {
-      "title": "Vorlagen",
-      "content": "<a target='_blank' rel='noopener' href='https://schul-cloud-hpi.s3.hidrive.strato.com/Willkommenordner/Vorlagen/Uebersicht-Vorlagen.pdf'>Übersicht der Vorlagen [.pdf]</a><br>  <a target='_blank' rel='noopener' href='https://schul-cloud-hpi.s3.hidrive.strato.com/Willkommenordner/Vorlagen/Checkliste_Struktur-einer-Testphase.pdf'>Checkliste: Struktur einer Testphase [.pdf]</a><br>  <a target='_blank' rel='noopener' href='https://schul-cloud-hpi.s3.hidrive.strato.com/Willkommenordner/Vorlagen/Elternbrief.pdf'>Elternbrief [.pdf]</a><br>  <a target='_blank' rel='noopener' href='https://schul-cloud-hpi.s3.hidrive.strato.com/Willkommenordner/Vorlagen/Laptopordnung.pdf'>Laptopordnung [.pdf]</a><br>  <a target='_blank' rel='noopener' href='https://schul-cloud-hpi.s3.hidrive.strato.com/Willkommenordner/Vorlagen/Nutzungsordnung-fuer-Informations-und-Kommunikationstechnik.pdf'>Nutzungsordnung für Informations- und Kommunikationstechnik [.pdf]</a><br>  <a target='_blank' rel='noopener' href='https://schul-cloud-hpi.s3.hidrive.strato.com/Willkommenordner/Vorlagen/Nuzungsordnung-fuer-mobile-Endgeraete.pdf'>Nuzungsordnung für mobile Endgeräte [.pdf]</a><br>  <a target='_blank' rel='noopener' href='https://schul-cloud-hpi.s3.hidrive.strato.com/Willkommenordner/Vorlagen/Schulleiterbrief.pdf'>Schulleiterbrief [.pdf]</a><br>"
-    }
-  ]
-
 class HelpDocumentsService {
 	async find(params) {
 		const themeResult = await helpDocumentsModel.find({ theme: params.query.theme });
@@ -39,11 +12,6 @@ class HelpDocumentsService {
 		if (themeResult.length > 0) {
 			result = themeResult[0].data;
 		}
-		/*
-		TODO
-			- check if Documents with schoolId = currentUser.schoolId exist and send them out
-			- if not check for theme based documents (theme can be accessed from `params.query.theme` ) and send those
-		*/
 		return result;
 	}
 
