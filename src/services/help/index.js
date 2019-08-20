04cd899148b1f7d66c38a17bb9ec5302a1ad20fa
Feldtkeller <thomas.feldtkeller@hpi.de>
2019-08-20 11:35:42 +0200

    error logging

--- src/services/help/index.js
+++ src/services/help/index.js
@@ -2,30 +2,40 @@ const auth = require('@feathersjs/authentication');
 const hooks = require('feathers-hooks-common');
 const errors = require('@feathersjs/errors');
 const { helpDocumentsModel } = require('./model');
+const logger = require('../../logger');
+
+const findDocuments = async (app, params) => {
+	if (!(params.query || {}).theme) {
+		return Promise.reject(new errors.BadRequest(
+			'this method requires querying for a theme - query:{theme:"themename"}',
+		));
+	}
+	const promises = [helpDocumentsModel.find({ theme: params.query.theme })];
+	if (params.account) {
+		const school = await app.service('users').get(params.account.userId)
+			.then(user => app.service('schools').get(user.schoolId));
+		promises.push(helpDocumentsModel.find({ schoolId: school._id }));
+	}
+	const [themeResult, schoolResult] = await Promise.all(promises);
+	let result;
+	if (schoolResult && schoolResult.length > 0) {
+		result = schoolResult[0].data;
+	} else if (themeResult && themeResult.length > 0) {
+		result = themeResult[0].data;
+	} else {
+		return Promise.reject(new errors.NotFound('could not find help documents for this user or theme.'));
+	}
+	return result;
+};
 
 class HelpDocumentsService {
 	async find(params) {
-		if (!(params.query || {}).theme) {
-			return Promise.reject(new errors.BadRequest(
-				'this method requires querying for a theme - query:{theme:"themename"}',
-			));
+		try {
+			return findDocuments(this.app, params);
+		} catch (err) {
+			logger.log('warn', err);
+			throw err;
 		}
-		const promises = [helpDocumentsModel.find({ theme: params.query.theme })];
-		if (params.account) {
-			const school = await this.app.service('users').get(params.account.userId)
-				.then(user => this.app.service('schools').get(user.schoolId));
-			promises.push(helpDocumentsModel.find({ schoolId: school._id }));
-		}
-		const [themeResult, schoolResult] = await Promise.all(promises);
-		let result;
-		if (schoolResult && schoolResult.length > 0) {
-			result = schoolResult[0].data;
-		} else if (themeResult && themeResult.length > 0) {
-			result = themeResult[0].data;
-		} else {
-			return Promise.reject(new errors.NotFound('could not find help documents for this user or theme.'));
-		}
-		return result;
 	}
 
 	setup(app) {
