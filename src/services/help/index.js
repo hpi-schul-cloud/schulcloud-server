77bf363f2305914abd409dbf2f4e8c4e51489eed
Feldtkeller <thomas.feldtkeller@hpi.de>
2019-08-20 11:27:21 +0200

    finish school specific documents

--- src/services/help/index.js
+++ src/services/help/index.js
@@ -1,16 +1,24 @@
 const auth = require('@feathersjs/authentication');
 const hooks = require('feathers-hooks-common');
+const errors = require('@feathersjs/errors');
 const { helpDocumentsModel } = require('./model');
 
 class HelpDocumentsService {
 	async find(params) {
-		const themeResult = await helpDocumentsModel.find({ theme: params.query.theme });
-		const school = await this.app.service('users').get(params.account.userId)
-			.then(user => this.app.service('schools').get(user.schoolId));
-		const schoolResult = await helpDocumentsModel.find({ schoolId: school._id });
+		const promises = [helpDocumentsModel.find({ theme: params.query.theme })];
+		if (params.account) {
+			const school = await this.app.service('users').get(params.account.userId)
+				.then(user => this.app.service('schools').get(user.schoolId));
+			promises.push(helpDocumentsModel.find({ schoolId: school._id }));
+		}
+		const [themeResult, schoolResult] = await Promise.all(promises);
 		let result;
-		if (themeResult.length > 0) {
+		if (schoolResult && schoolResult.length > 0) {
+			result = schoolResult[0].data;
+		} else if (themeResult && themeResult.length > 0) {
 			result = themeResult[0].data;
+		} else {
+			return Promise.reject(new errors.NotFound('could not find help documents for this user or theme.'));
 		}
 		return result;
 	}
--- test/services/help/index.test.js
+++ test/services/help/index.test.js
@@ -1,11 +1,9 @@
 const assert = require('assert');
 const chai = require('chai');
 const app = require('../../../src/app');
-const {
-	createTestSchool,
-	createTestUser,
-} = require('../helpers/testObjects')(app);
+const { createTestSchool, createTestUser } = require('../helpers/testObjects')(app);
 const { generateRequestParamsFromUser } = require('../helpers/services/login')(app);
+const { helpDocumentsModel } = require('../../../src/services/help/model');
 
 const { expect } = chai;
 
@@ -26,9 +24,21 @@ describe.only('help documents service', () => {
 		assert.ok(helpDocumentService);
 	});
 
+	it('FIND throws an error if no documents are found', async () => {
+		try {
+			await helpDocumentService.find({ query: { theme: 'thisThemeDoesntExist' } });
+			throw new Error('should have failed');
+		} catch (err) {
+			expect(err).to.not.equal('should have failed');
+			expect(err.code).to.equal(404);
+		}
+	});
+
 	it('FIND returns valid default document links', async () => {
 		const response = await helpDocumentService.find({ query: { theme: 'default' } });
 		expect(response).to.not.equal(undefined);
+		expect(Array.isArray(response)).to.equal(true);
+		expect(response.length).to.be.greaterThan(0);
 		response.forEach((element) => {
 			expect(element.title).to.exist;
 			expect(typeof element.title).to.equal('string');
@@ -42,13 +52,27 @@ describe.only('help documents service', () => {
 		const user = await createTestUser({ schoolId, roles: 'student' });
 		const params = await generateRequestParamsFromUser(user);
 		params.query = { theme: 'default' };
+		const data = [
+			{
+				title: 'welcome',
+				content: 'somelink',
+			},
+			{
+				title: 'unnecessary explanations',
+				content: 'another link',
+			},
+		];
+		await helpDocumentsModel.create({ schoolId, data });
+
 		const response = await helpDocumentService.find(params);
 		expect(response).to.not.equal(undefined);
+		expect(Array.isArray(response)).to.equal(true);
+		expect(response.length).to.equal(2);
 		response.forEach((element) => {
-			expect(element.title).to.exist;
-			expect(typeof element.title).to.equal('string');
-			expect(element.content).to.exist;
-			expect(typeof element.content).to.equal('string');
+			/* eslint-disable no-underscore-dangle */
+			expect(element.title).to.equal(data[element.__index].title);
+			expect(element.content).to.equal(data[element.__index].content);
+			/* eslint-enable no-underscore-dangle */
 		});
 	});
 });
