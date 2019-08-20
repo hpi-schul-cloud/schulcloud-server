9818c8fc42491baceeaa9a9841d8f575d2d06e68
Willy Scheibel <willy.scheibel@hpi.de>
2019-08-20 16:43:19 +0200

    Prepare creation of Schul-Cloud objects from WebUntis results

--- src/services/sync/strategies/WebUntisHourlySyncer.js
+++ src/services/sync/strategies/WebUntisHourlySyncer.js
@@ -1,5 +1,7 @@
 const Syncer = require('./Syncer');
 
+const assert = require('assert');
+
 const Lesson = require('../../lesson/model');
 const { userModel: User } = require('../../user/model');
 const { schoolModel: School, yearModel: Year } = require('../../school/model');
@@ -57,7 +59,7 @@ class WebUntisHourlySyncer extends Syncer {
         ? */
         return this.getUser()
             .then(user => this.getWebUntisSystems(user))
-			.then(systems => {
+			.then((systems, school) => {
                 /* TODO: Remove later */
                 if (systems.length === 0) {
                     /*systems[0] = {
@@ -90,7 +92,7 @@ class WebUntisHourlySyncer extends Syncer {
                 this.logInfo(`Found ${systems.length} WebUntis configurations.`);
 				return Promise.all(systems.map(system => {
 					this.stats.systems[system.alias] = {};
-					return this.syncFromSystem(system, this.stats.systems[system.alias], this.app);
+					return this.syncFromSystem(system, this.stats.systems[system.alias], this.app, school);
 				}));
 			});
     }
@@ -103,25 +105,27 @@ class WebUntisHourlySyncer extends Syncer {
         this.logInfo(`Get Systems for user ${user}, ${user.id}, ${user.schoolId}.`);
         return this.app.service('schools').get(user.schoolId)
             .then((school) => {
-                return school.systems.filter(system => system.type == 'webuntis');
+                return Promise.all(
+                    school.systems.filter(system => system.type == 'webuntis'),
+                    school
+                );
             });
     }
 
-    async syncFromSystem(system, stats, app) {
+    async syncFromSystem(system, stats, app, school) {
         var api = new WebUntisApi(
             system.webuntisConfig.url,
             system.webuntisConfig.schoolname
         );
 
-        return Promise.resolve()
-            .then(() => api.login(system.webuntisConfig.user, system.webuntisConfig.password))
+        return api.login(system.webuntisConfig.user, system.webuntisConfig.password)
             .then(() => this.fetchInformation(api, stats, app))
             .then((data) => {
                 api.logout();
                 return Promise.resolve(data)
             })
             .then((data) => {
-                this.migrateData(data, stats);
+                this.migrateData(data, stat, school);
             })
             .then(() => {
                 stats.success = true;
@@ -133,7 +137,7 @@ class WebUntisHourlySyncer extends Syncer {
         let data = {};
         data.currentSchoolYear = await api.getCurrentSchoolyear();
         // intermediateData.holidays = await api.getHolidays();
-        data.subjects = await api.getSubjects(); // Ignore subjects for now
+        // data.subjects = await api.getSubjects(); // Ignore subjects for now
         intermediateData.timeGrid = await api.getTimegrid();
         intermediateData.classes = await api.getClasses(data.currentSchoolYear.id);
         intermediateData.rooms = await api.getRooms();
@@ -201,7 +205,9 @@ class WebUntisHourlySyncer extends Syncer {
         return Promise.resolve(data);
     }
 
-    async migrateData(data, stats) {
+    async migrateData(data, stats, school) {
+		const courseService = this.app.service('courses');
+		const classService = this.app.service('classes');
         /**
          * Mapping:
          * 
@@ -210,17 +216,104 @@ class WebUntisHourlySyncer extends Syncer {
          * German: Klasse, Kurs, Fach, Schulstunde
          */
 
-        /**
-         * Obtain classes
-         */
+        for (let classIndex in data.classes) {
+            /**
+             * Obtain classes
+             */
+            const klass = data.classes[classIndex];
+            const className = klass.id;
+            const scClasses = await classService.find({ query: { name: klass.longname }, paginate: false });
 
-        /** Obtain courses for subjects:
-         * 
-         * - class
-         * - (teacher)
-         * - time series
-         * - room
-        */
+            let scClass = undefined;
+            if (scClasses.total === 0) {
+                // Create Schul-Cloud class?
+                const newClass = {
+					name: className,
+					schoolId: school._id,
+					nameFormat: 'static',
+					year: school.currentYear,
+				};
+                scClass = await classService.create(newClass);
+            } else {
+                scClass = scClasses[0];
+            }
+
+            const courses = {};
+            const times = {};
+            for (let timetableIndex in klass.timetable) {
+                const timetableEntry = klass.timetable[timetableIndex];
+
+                /** Obtain courses for subjects:
+                 * 
+                 * - class
+                 * - (teacher)
+                 * - time series
+                 * - room
+                */
+                const subjectName = timetableEntry.subject;
+
+                let scCourse = courses[subjectName];
+                if (scCourse === undefined) {
+                    const courseName = subjectName + scClass.name;
+                    scCourses = await courseService.find({ query: {
+                        name: courseName,
+                        classIds: scClass._id,
+                        schoolId: school._id
+                    }, paginate: false });
+
+                    if (scCourses.total === 0) {
+                        // Create Course
+                        newCourse = {
+                            name: courseName,
+                            classIds: [ scClass._id ],
+                            schoolId: school._id,
+                            teacherIds: [ /* TODO: user id */ ],
+                        };
+
+                        scCourse = await courseService.create(newCourse);
+                        courses[subjectName] = scCourse;
+                        times[subjectName] = [];
+                    } else {
+                        scCourse = scCourses[0];
+                    }
+                }
+                
+                const newEntry = {
+                    weekday: this.getWeekDay(timetableEntry.date),
+                    startTime: this.getStartTime(timetableEntry.startTime),
+                    duration: this.getDuration(timetableEntry.startTime, timetableEntry.endTime),
+                    room: timetableEntry.room,
+                    count: 1
+                };
+                let entryFound = false;
+                for (index in times[subjectName]) {
+                    const givenEntry = times[subjectName][index];
+                    if (givenEntry.weekday === newEntry.weekday &&
+                        givenEntry.startTime === newEntry.startTime &&
+                        givenEntry.duration === newEntry.duration &&
+                        givenEntry.room === newEntry.room) {
+                        givenEntry.count += 1;
+                    }
+                }
+                if (!entryFound) {
+                    times[subjectName].push(newEntry);
+                }
+            }
+
+            for (let subjectName in courses) {
+                scCourse = courses[subjectName];
+                // Update times, considered are events that occurs at least twice a year
+                scCourse.times = times.filter(entry => entry.count >= 2).map(entry => {
+                    return {
+                        weekday: entry.weekday,
+                        startTime: entry.startTime,
+                        duration: entry.duration,
+                        eventId: undefined,
+                        room: entry.room,
+                    }
+                });
+            }
+        }
 
         /** Derive course dates:
          * 
@@ -233,6 +326,77 @@ class WebUntisHourlySyncer extends Syncer {
         /* Populate stats */
 
         return Promise.resolve();
+    }
+
+    /**
+     * Convert a WebUntis-encoded date to a Schul-Cloud-encoded weekday
+     * 
+     * @param {int} date - A date encoded in YYYYMMDD
+     * 
+     * @return Corresponding weekday - from 0 to 6, the weekday the course take place (e.g. 0 = monday, 1 = tuesday ... )
+     */
+    getWeekDay(date) {
+        const jsDate = new Date(
+            Math.floor(date / 10000), /* year */
+            Math.floor(date / 100) % 100, /* month */
+            date % 100, /* day */
+            0, /* hour */
+            0, /* minute */
+            0, /* second */
+            0); // remainder
+        
+        return (jsDate.getDay() + 7 - 1 /* monday offset */) % 7;
+    }
+
+    /**
+     * Convert a WebUntis-encoded timestamp to a Schul-Cloud-encoded timestamp
+     * 
+     * @param {int} startTime - A time encoded in HHMM
+     * 
+     * @return Schul-Cloud-encoded timestamp
+     */
+    getStartTime(startTime) {
+        assert.ok(startTime >= 0);
+        assert.ok(startTime < 2400);
+        assert.ok(startTime % 100 >= 0);
+        assert.ok(startTime % 100 < 60);
+
+        return 1000 /* milliseconds per second */
+            * 60 /* seconds per minute */
+            * (60 /* minutes per hour */ * Math.floor(startTime / 100)
+              + startTime % 100 /* minutes */) /* minutes */;
+    }
+
+    /**
+     * Convert a WebUntis-encoded start and end timestamp to a Schul-Cloud-encoded duration
+     * 
+     * @param {int} startTime - A time encoded in HHMM
+     * @param {int} endTimeTime  - A time encoded in HHMM
+     * 
+     * @return Schul-Cloud-encoded duration
+     */
+    getDuration(startTime, endTime) {
+        assert.ok(startTime >= 0);
+        assert.ok(startTime < 2400);
+        assert.ok(startTime % 100 >= 0);
+        assert.ok(startTime % 100 < 60);
+        assert.ok(endTime >= 0);
+        assert.ok(endTime < 2400);
+        assert.ok(endTime % 100 >= 0);
+        assert.ok(endTime % 100 < 60);
+        assert.ok(endTime >= startTime);
+
+        const durationHours = Math.floor(endTime / 100) - Math.floor(startTime / 100);
+        assert.ok(durationHours);
+        const durationMinutes = (endTime % 100) - (startTime % 100);
+        assert.ok(durationHours > 0 || durationMinutes > 0);
+        const duration = durationHours + durationMinutes;
+
+        assert.ok(duration > 0);
+
+        return 1000 /* milliseconds per second */
+            * 60 /* seconds per minute */
+            * duration/* minutes */;
     }
 }
 
