538b2c0c56898263763583d3cf7498ec3581e9bf
Hannes Schurig <hannes.schurig@hpi.de>
2019-08-22 14:36:13 +0100

    remove comments

--- migrations/1565861699243-set schoolspecific documentbasedir for instance open.js
+++ migrations/1565861699243-set schoolspecific documentbasedir for instance open.js
@@ -4,9 +4,6 @@ const { info, warning } = require('../src/logger');
 const { schoolModel: School } = require('../src/services/school/model');
 const globals = require('../config/globals');
 
-// How to use more than one schema per collection on mongodb
-// https://stackoverflow.com/questions/14453864/use-more-than-one-schema-per-collection-on-mongodb
-
 module.exports = {
 	up: async function up() {
 		if (globals.SC_THEME !== 'open') {
@@ -15,15 +12,10 @@ module.exports = {
 		}
 
 		await connect();
-		// ////////////////////////////////////////////////////
-		// Make changes to the database here.
-		// Hint: Access models via this('modelName'), not an imported model to have
-		// access to the correct database connection. Otherwise Mongoose calls never return.
 		await School.updateMany({}, {
 			documentBaseDirType: 'school',
 		}).lean().exec();
 
-		// ////////////////////////////////////////////////////
 		await close();
 
 		info('Migration has been applied to this instance of open');
