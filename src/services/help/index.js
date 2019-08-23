7bc9489076c50af3f32041c3bd63b61458590175
Feldtkeller <thomas.feldtkeller@hpi.de>
2019-08-23 14:54:29 +0200

    internal calls can query with users

--- src/services/help/index.js
+++ src/services/help/index.js
@@ -4,14 +4,13 @@ const errors = require('@feathersjs/errors');
 const { helpDocumentsModel } = require('./model');
 const logger = require('../../logger');
 
-const findDocuments = async (app, params) => {
-	if (!(params.query || {}).theme) {
-		throw new errors.BadRequest('this method requires querying for a theme - query:{theme:"themename"}');
-	}
-
-	let query = { theme: params.query.theme };
-	if (params.account) {
-		const school = await app.service('users').get(params.account.userId)
+/**
+ * retrieve documents from database according to theme and userId
+ */
+const findDocuments = async (app, userId, theme) => {
+	let query = { theme };
+	if (userId) {
+		const school = await app.service('users').get(userId)
 			.then(user => app.service('schools').get(user.schoolId));
 
 		if (school.documentBaseDirType === 'school') query = { schoolId: school._id };
@@ -23,10 +22,24 @@ const findDocuments = async (app, params) => {
 	return result[0].data;
 };
 
+
+/**
+ * Service to get an array of help Document data, specific to school, schoolgroup, or theme.
+ */
 class HelpDocumentsService {
+	/**
+	 * find the correct help document data for the user.
+	 * @param {Object} params has to include a query object with a theme.
+	 * May also include a userId for internal calls.
+	 */
 	async find(params) {
 		try {
-			return findDocuments(this.app, params);
+			if (!(params.query || {}).theme) {
+				throw new errors.BadRequest('this method requires querying for a theme - query:{theme:"themename"}');
+			}
+			const { userId } = params.account || params.query || {};
+			const { theme } = params.query || {};
+			return findDocuments(this.app, userId, theme);
 		} catch (err) {
 			logger.log('warn', err);
 			throw err;
