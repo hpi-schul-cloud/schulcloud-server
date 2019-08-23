da8e7b81c2628fe2337db88918f4336f528d780e
Feldtkeller <thomas.feldtkeller@hpi.de>
2019-08-23 14:22:23 +0200

    update model

--- src/services/help/model.js
+++ src/services/help/model.js
@@ -5,6 +5,7 @@ const { Schema } = mongoose;
 const helpDocumentsSchema = new Schema({
 	schoolId: { type: Schema.Types.ObjectId, ref: 'school' },
 	theme: { type: String },
+	schoolGroupId: { type: Schema.Types.ObjectId, ref: 'schoolGroup' },
 	data: [{
 		title: { type: String, required: true },
 		content: { type: String, required: true },
