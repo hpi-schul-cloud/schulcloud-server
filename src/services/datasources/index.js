524aad23392acf9ee22b044885d6780dbe0135a7
Thomas Feldtkeller <thomas.feldtkeller@hpi.de>
2019-09-17 13:37:37 +0200

    secure datasources

--- src/services/datasources/index.js
+++ src/services/datasources/index.js
@@ -1,4 +1,7 @@
+const auth = require('@feathersjs/authentication');
 const service = require('feathers-mongoose');
+
+const globalHooks = require('../../hooks');
 const { datasourceModel, datasourceRunModel } = require('./model');
 
 module.exports = function setup() {
@@ -11,8 +14,31 @@ module.exports = function setup() {
 			max: 50,
 		},
 	}));
-	// const datasourcesService = app.service('/datasources');
-	// hooks
+	const datasourceHooks = {
+		before: {
+			all: [
+				auth.hooks.authenticate('jwt'),
+				globalHooks.ifNotLocal(globalHooks.restrictToCurrentSchool),
+			],
+			find: [globalHooks.ifNotLocal(globalHooks.hasPermission('DATASOURCES_VIEW'))],
+			get: [globalHooks.ifNotLocal(globalHooks.hasPermission('DATASOURCES_VIEW'))],
+			create: [globalHooks.ifNotLocal(globalHooks.hasPermission('DATASOURCES_CREATE'))],
+			update: [globalHooks.ifNotLocal(globalHooks.hasPermission('DATASOURCES_EDIT'))],
+			patch: [globalHooks.ifNotLocal(globalHooks.hasPermission('DATASOURCES_EDIT'))],
+			remove: [globalHooks.ifNotLocal(globalHooks.hasPermission('DATASOURCES_DELETE'))],
+		},
+		after: {
+			all: [],
+			find: [],
+			get: [],
+			create: [],
+			update: [],
+			patch: [],
+			remove: [],
+		},
+	};
+	const datasourcesService = app.service('/datasources');
+	datasourcesService.hooks(datasourceHooks);
 
 	app.use('/datasourceRuns', service({
 		Model: datasourceRunModel,
