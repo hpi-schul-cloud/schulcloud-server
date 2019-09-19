c7d4049a7b49fc7a988f8d3b7cff9f66e266bac2
Maximilian Grundke <maximiliangrundke@googlemail.com>
2019-09-19 09:59:09 +0200

    Move hooks and services to dedicated files

---
+++ src/services/helpers/scopePermissions/ScopeService.js
@@ -0,0 +1,68 @@
+const auth = require('@feathersjs/authentication');
+const globalHooks = require('../../../hooks');
+const { lookupScope, rejectQueryingOtherUsers } = require('./hooks');
+
+/**
+ * Base class for scope services (nested under a top-level service)
+ * @class ScopeService
+ */
+class ScopeService {
+	/**
+     * Creates an instance of ScopeService.
+     * @param {Function} handler instance-specific code
+     * @memberof ScopeService
+     */
+	constructor(handler) {
+		this.handler = handler;
+	}
+
+	/**
+     * Will be called by feathers upon initialization of this service.	 *
+     * @param {App} app
+     * @memberof ScopeService
+     */
+	async setup(app) {
+		this.app = app;
+	}
+
+	/**
+     * Default set of hooks. Ready to override by sub-classes.
+     * @static
+     * @returns Object<FeathersHooksCollection>
+     * @memberof ScopeService
+     */
+	static hooks() {
+		return {
+			before: {
+				all: [
+					globalHooks.ifNotLocal(auth.hooks.authenticate('jwt')),
+					globalHooks.ifNotLocal(rejectQueryingOtherUsers),
+					lookupScope,
+				],
+			},
+		};
+	}
+
+	/**
+     * Initializes this service at a specific route and adds hooks and use-case-specific handler.
+     * @static
+     * @param {App} app the feathers app
+     * @param {String} path service path to use
+     * @param {Function} handler use-case-specific code
+     * @returns {ScopePermissionService} the service instance
+     * @memberof ScopeService
+     */
+	static initialize(app, path, handler) {
+		if (!handler) {
+			throw new Error(`ScopePermisionService initialized at '${path}' without handler.`);
+		}
+		app.use(path, new this(handler));
+		const scopePermissionService = app.service(path);
+		scopePermissionService.hooks(this.hooks());
+		return scopePermissionService;
+	}
+}
+
+module.exports = {
+	ScopeService,
+};
---
+++ src/services/helpers/scopePermissions/hooks/rejectQueryingForOtherUsers.js
@@ -0,0 +1,35 @@
+const { Forbidden, BadRequest } = require('@feathersjs/errors');
+const { resolveScope } = require('./resolveScope');
+
+const rejectQueryingOtherUsers = (context) => {
+	if (context.params === undefined || context.params.account === undefined) {
+		throw new BadRequest('Expected authentication.');
+	}
+	if (!['get', 'find'].includes(context.method)) {
+		throw new BadRequest('Only GET and FIND methods are allowed.');
+	}
+
+	const { scopeName, scopeId } = resolveScope(context);
+	let targetUserId;
+	if (scopeName === 'users') {
+		targetUserId = scopeId.toString();
+	} else {
+		if (context.method === 'get' && context.id === undefined) {
+			throw new BadRequest('Expected userId.');
+		}
+		if (context.method === 'find' && context.params.query === undefined) {
+			throw new BadRequest('Expected query for userId.');
+		}
+		targetUserId = context.id || context.params.query.userId;
+	}
+
+	const currentUserId = context.params.account.userId.toString();
+	if (targetUserId === undefined || targetUserId !== currentUserId) {
+		throw new Forbidden('Requested and requesting userIds do not match.');
+	}
+	return context;
+};
+
+module.exports = {
+	rejectQueryingOtherUsers,
+};
