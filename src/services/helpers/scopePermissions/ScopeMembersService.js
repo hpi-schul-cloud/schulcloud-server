608375e240256fe17d3c8cdd9bd8c97180fb695a
Maximilian Grundke <maximiliangrundke@googlemail.com>
2019-09-19 16:13:07 +0200

    Scaffold ScopeMembersService template

---
+++ src/services/helpers/scopePermissions/ScopeMembersService.js
@@ -0,0 +1,42 @@
+const { ScopeService } = require('./ScopeService');
+
+/**
+ * Implements retrieving a list of all users who are associated with a scope.
+ * @class ScopeMemberService
+ * @extends {ScopeService}
+ */
+class ScopeMemberService extends ScopeService {
+	/**
+     * Custom set of hooks.
+     * @static
+     * @returns Object<FeathersHooksCollection>
+	 * @override ScopeService#hooks
+     * @memberof ScopeMemberService
+     */
+	static hooks() {
+		return {
+			before: {
+				all: [
+					globalHooks.ifNotLocal(auth.hooks.authenticate('jwt')),
+					lookupScope,
+					checkScopePermission('SCOPE_PERMISSIONS_VIEW'),
+				],
+			},
+		};
+	}
+
+	/**
+     * Implements the route
+     * @param {Object} params Feathers request params
+     * @returns {Array<ObjectId>} a list of userIds
+     * @memberof ScopeMemberService
+     */
+	async find(params) {
+		const members = await this.handler.apply(this, [params]) || [];
+		return members;
+	}
+}
+
+module.exports = {
+	ScopeMemberService,
+};
