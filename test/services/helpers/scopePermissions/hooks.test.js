5a98704bb421143f64c30f3fc7d521cf9d1e8145
Maximilian Grundke <maximiliangrundke@googlemail.com>
2019-09-23 12:03:34 +0200

    Organize scope service helper tests

---
+++ test/services/helpers/scopePermissions/hooks.test.js
@@ -0,0 +1,8 @@
+/* eslint-disable global-require */
+/**
+ * This file is only used to group the hook tests together
+ */
+describe('ScopeService hooks', () => {
+	require('./hooks/lookupScope');
+	require('./hooks/rejectQueryingForOtherUsers');
+});
