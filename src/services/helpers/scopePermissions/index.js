14ac5b341ad680e7209de07cfb99105d6618a2dc
Maximilian Grundke <maximiliangrundke@googlemail.com>
2019-09-24 15:58:34 +0200

    Add scope members service to index

--- src/services/helpers/scopePermissions/index.js
+++ src/services/helpers/scopePermissions/index.js
@@ -1,9 +1,11 @@
 const { ScopeService } = require('./ScopeService');
 const { ScopeListService } = require('./ScopeListService');
 const { ScopePermissionService } = require('./ScopePermissionService');
+const { ScopeMembersService } = require('./ScopeMembersService');
 
 module.exports = {
 	ScopeService,
 	ScopePermissionService,
 	ScopeListService,
+	ScopeMembersService,
 };
