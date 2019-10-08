b26f51e6abee8e10e89a66586ec6610c1ab467f9
Christian S <36112357+BlackGhostHD@users.noreply.github.com>
2019-10-08 17:14:03 +0200

    Update service.js

--- src/services/helpers/service.js
+++ src/services/helpers/service.js
@@ -39,7 +39,7 @@ module.exports = function setup(app) {
 						logger.info(`E-Mail Message sent: ${info.messageId}`);
 						return;
 					}
-					 otherwise print email message object on console
+					// otherwise print email message object on console
 					logger.debug('E-Mail Message not sent (not in production mode):', mail);
 				}).catch((err) => Promise.reject(err));
 		}
