4d480a2b341a2f326028bfb36c93bb171e66e65f
Martin Malchow <martin.malchow@hpi.de>
2019-10-15 15:59:41 +0200

    Basic implementation. missing: - add jwt to redis after successful authentication - move check to authentication hook - use a single connection to redis and not establish and close every time

--- src/middleware/handleAutoLogout.js
+++ src/middleware/handleAutoLogout.js
@@ -1,16 +1,42 @@
 const errors = require('@feathersjs/errors');
+const redis = require('redis');
 const jwt = require('jsonwebtoken');
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
+	// Connect to redis if defined
+	let redisClient = false;
+	const redisUrl = process.env.REDIS_URI;
+	if (redisUrl) {
+		redisClient = redis.createClient({
+			url: redisUrl,
+		});
+	}
 
 	// Check if jwt is available, if not let request pass
-	if (req.headers.authorization) {
+	if (redisClient && req.headers.authorization) {
 		const decodedToken = jwt.decode(req.headers.authorization.replace('Bearer ', ''));
-		const tokenJti = decodedToken.jti; // UID of the token 
+		const { accountId, jti } = decodedToken; // jti - UID of the token
 
-		// Check if JTI is in white list and still valid (with redis)
+		const redisIdentifier = `jwt:${accountId}:${jti}`;
 
-		// Update JTI valid time
+		redisClient.on('connect', async () => {
+			redisClient.get(redisIdentifier, (err, redisResponse) => {
+				if (redisResponse) {
+					// Check if JTI is in white list and still valid
+					redisClient.set(redisIdentifier, '{"IP": "NONE", "Browser": "NONE"}', 'EX', 100);
+				} else {
+					// Throw not Authenticated
+					redisClient.quit();
+					throw new errors.NotAuthenticated('jwt is not whitelisted anymore - autologout');
+				}
+			});
+		});
+
+		// client.set('account:uuid', '{"jtis": [jwt:uuid:jti]}', 'EX', NEVER);
+	}
+
+	if (redisClient) {
+		// redisClient.quit();
 	}
 
 	next();
