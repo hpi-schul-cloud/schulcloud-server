d2210fda7f8ccb76dc97e8cfa4372388fa7707f0
Tobias Wollowski <tobias.wollowski@hpi.de>
2019-10-16 17:08:39 +0200

    fix homework tests by returning real promise via HomeworkModel.create

--- src/services/homework/copy-service.js
+++ src/services/homework/copy-service.js
@@ -36,7 +36,7 @@ class HomeworkCopyService {
 	create(data, params) {
 		const userId = data.newTeacherId || params.payload.userId || (params.account || {}).userId;
 
-		return HomeworkModel.findOne({ _id: data._id })
+		return HomeworkModel.findById(data._id).exec()
 			.then((copyAssignment) => {
 				let tempAssignment = JSON.parse(JSON.stringify(copyAssignment));
 				tempAssignment = _.omit(tempAssignment, ['_id', 'stats', 'isTeacher', 'archived', '__v', 'courseId', 'lessonId']);
@@ -48,10 +48,7 @@ class HomeworkCopyService {
 						tempAssignment.schoolId = user.schoolId;
 						tempAssignment.teacherId = user._id;
 
-						return HomeworkModel.create(tempAssignment, (err, res) => {
-							if (err) return err;
-							return res;
-						});
+						return HomeworkModel.create(tempAssignment);
 					});
 			});
 	}
