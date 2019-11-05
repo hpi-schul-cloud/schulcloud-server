a34f94f77086b4e52d79243caa8eb7a414ff85a2
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2019-11-05 14:48:50 +0100

    persist migration seeds

--- migrations/1572358406935-addCreateSupportJwtPermission.js
+++ migrations/1572358406935-addCreateSupportJwtPermission.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-// eslint-disable-next-line no-unused-vars
-const { info, error } = require('../src/logger');
 
 const { connect, close } = require('../src/utils/database');
 
