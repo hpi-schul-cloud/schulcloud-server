c4eed2084e8d95b7d1c6f358310468039e032eea
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2019-11-05 12:49:00 +0100

    Add account by params is fixed now. Add some comments.

--- test/services/helpers/services/login.js
+++ test/services/helpers/services/login.js
@@ -14,6 +14,10 @@ const generateJWT = (app) => async ({ username, password }) => {
 	return result.accessToken;
 };
 
+/**
+ * Execute login with username and password
+ * @returns { authentication = {accessToken, strategy}, provider = 'rest' }
+ */
 const generateRequestParams = (app) => async ({ username, password }) => {
 	const fetchJwt = generateJWT(app);
 	const jwt = await fetchJwt({ username, password });
@@ -26,41 +30,24 @@ const generateRequestParams = (app) => async ({ username, password }) => {
 	};
 };
 
-const generateRequestParamsFromUser = (app) => async (user, withAccount = false) => {
+/**
+ * I ) Create a account for this user
+ * II) Execute login for this user to get the jwt
+ * @param {User} user
+ * @param {Boolean} withAccount if true the account is also return
+ * @returns { [account], authentication = {accessToken, strategy}, provider = 'rest' }
+ */
+const generateRequestParamsFromUser = (app) => async (user) => {
 	const credentials = { username: user.email, password: user.email };
 	const $account = await accountsHelper(app).create(credentials, 'local', user);
 
 	const requestParams = await generateRequestParams(app)(credentials);
-	if (withAccount === true) {
-		requestParams.account = $account.toObject();
-	}
+	requestParams.account = $account.toObject();
 	return requestParams;
 };
-
-// hook.params.account.userId
-const fakeLoginParams = (app) => ({
-	userId,
-	account,
-	headers = {},
-	query = {},
-	route = {},
-}) => ({
-	account: account || {
-		userId,
-	},
-	authenticated: true,
-	provider: 'rest',
-	headers,
-	query,
-	route,
-	payload: {
-		accountId: (account || {})._id,
-	},
-});
 
 module.exports = (app) => ({
 	generateJWT: generateJWT(app),
 	generateRequestParams: generateRequestParams(app),
 	generateRequestParamsFromUser: generateRequestParamsFromUser(app),
-	fakeLoginParams: fakeLoginParams(app),
 });
