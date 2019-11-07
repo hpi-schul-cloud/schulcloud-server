f89142b8d4c828e11f9fa556b80737494520847e
Maximilian Grundke <maximiliangrundke@googlemail.com>
2019-11-07 17:14:35 +0100

    Scaffold TSP school sync

--- src/services/sync/strategies/TSP/TSPSchoolSyncer.js
+++ src/services/sync/strategies/TSP/TSPSchoolSyncer.js
@@ -1,5 +1,162 @@
+const Syncer = require('../Syncer');
+const { TspApi } = require('./TSP');
+const SchoolYearFacade = require('../../../school/logic/year');
+
 const SYNCER_TARGET = 'tsp-school';
 
+class TSPSchoolSyncer extends Syncer {
+	respondsTo(target) {
+		return target === SYNCER_TARGET;
+	}
+
+	params(params, data) {
+		const config = (params || {}).config || (data || {}).config;
+		if (!config) {
+			this.logError('Missing parameter "config".');
+			return false;
+		}
+		if (!config.baseUrl) {
+			this.logError('Missing parameter "config.baseUrl" (URL that points to the TSP Server).');
+			return false;
+		}
+		return [config];
+	}
+
+	constructor(app, stats, logger, config) {
+		super(app, stats, logger);
+		this.stats = Object.assign(this.stats, {
+			schools: {},
+		});
+		this.config = config;
+		this.api = new TspApi(config.baseUrl);
+
+		this.currentYear = undefined;
+		this.federalState = undefined;
+	}
+
+	async steps() {
+		let allTeachers = [];
+		let allStudents = [];
+		const allClasses = [];
+
+		const systems = await this.findSystems();
+		if (systems.length > 0) {
+			allTeachers = await this.fetchTeachers();
+			allStudents = await this.fetchStudents();
+		}
+		for (const system of systems) {
+			let school = await this.findSchool(system);
+			if (!school) school = await this.createSchool(system);
+			const [teachers, students, classes] = [allTeachers, allStudents, allClasses].map((list) => list.filter(
+				(item) => item.schuleNummer === system.schoolIdentifier,
+			));
+
+			// todo: create or update people
+			// todo: create or update classes and associate people (throw out others)
+		}
+	}
+
+	createSchool(system) {
+		return this.app.service('schools').create({
+			name: system.tsp.schoolName,
+			systems: [system._id],
+			currentYear: this.getCurrentYear(),
+			federalState: this.getFederalState(),
+		});
+	}
+
+	async getCurrentYear() {
+		if (!this.currentYear) {
+			const years = await this.app.service('years').find();
+			if (years.total === 0) {
+				throw new Error('At least one year has to exist in the database.');
+			}
+			this.currentYear = new SchoolYearFacade(years.data).defaultYear;
+		}
+		return this.currentYear;
+	}
+
+	async getFederalState() {
+		if (!this.federalState) {
+			const states = await this.app.service('federalStates').find({ query: { abbreviation: 'TH' } });
+			if (states.total === 0) {
+				throw new Error('The federal state does not exist.');
+			}
+			this.federalState = states.data[0]._id;
+		}
+		return this.federalState;
+	}
+
+	async findSystems() {
+		if (this.config.systemId) {
+			const system = await this.app.service('systems').get(this.config.systemId);
+			this.config.schoolIdentifier = (system.tsp || {}).identifier;
+			return [system];
+		}
+		return this.app.service('systems').find({
+			query: {
+				type: SYNCER_TARGET,
+			},
+			paginate: false,
+		});
+	}
+
+	async findSchool(system) {
+		const response = await this.app.service('schools').find({
+			query: {
+				systems: system._id,
+				$limit: 1,
+			},
+			paginate: false,
+		});
+		if (!Array.isArray(response) || response.length < 1) {
+			this.logWarning(`No school found for system "${system.alias}" (${system._id}).`, system);
+			this.stats.errors.push({
+				type: 'find-school',
+				entity: system._id.toString(),
+				message: 'Zum System gehörende Schule existiert nicht.',
+			});
+			return null;
+		}
+		return response[0];
+	}
+
+	async fetchTeachers() {
+		let teachers = [];
+		try {
+			teachers = await this.api.request('/tip-ms/api/schulverwaltung_export_lehrer');
+			if (this.config.schoolIdentifier) {
+				teachers = teachers.filter((t) => t.schuleNummer === this.config.schoolIdentifier);
+			}
+		} catch (err) {
+			this.logError('Cannot fetch teachers.', err);
+			this.stats.errors.push({
+				type: 'fetch-teachers',
+				message: 'Es ist derzeit nicht möglich Lehrerdaten aus dem TSP zu aktualisieren.',
+			});
+		}
+		return teachers;
+	}
+
+	async fetchStudents() {
+		let students = [];
+		try {
+			students = await this.api.request('/tip-ms/api/schulverwaltung_export_schueler');
+			if (this.config.schoolIdentifier) {
+				students = students.filter((t) => t.schuleNummer === this.config.schoolIdentifier);
+			}
+		} catch (err) {
+			this.logError('Cannot fetch students.', err);
+			this.stats.errors.push({
+				type: 'fetch-students',
+				message: 'Es ist derzeit nicht möglich Schülerdaten aus dem TSP zu aktualisieren.',
+			});
+		}
+		return students;
+	}
+}
+
 module.exports = {
+	TSPSchoolSyncer,
 	SYNCER_TARGET,
 };
