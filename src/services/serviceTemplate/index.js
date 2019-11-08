1200d75e778769e0120f3fa6e603c0f28889384a
Thomas Feldtkeller <thomas.feldtkeller@hpi.de>
2019-11-08 12:15:24 +0100

    pass app into setup function

--- src/services/serviceTemplate/index.js
+++ src/services/serviceTemplate/index.js
@@ -2,9 +2,7 @@ const { modelService, modelServiceHooks } = require('./services/modelService');
 
 /* In addition to setting up the service code, the service has to be included in src/index.js. */
 
-module.exports = function setup() {
-	const app = this;
-
+module.exports = (app) => {
 	app.use('/serviceTemplate/modelService', modelService);
 
 	const serviceTemplate = app.service('/modelTemplate');
