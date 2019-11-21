875ff4c9ad75aa32fbd86b6235a46ff0bb057f6f
Tormod Flesjø <tormod.flesjo@gmail.com>
2019-11-21 13:04:36 +0100

    minor fixes

--- src/services/edusharing/hooks/getCookie.js
+++ src/services/edusharing/hooks/getCookie.js
@@ -2,22 +2,19 @@ const request = require('request-promise-native');
 
 const getCookie = async (context) => {
 	let response;
-
 	const regexp = /^JSESSIONID=|[a-f\d]{32}$/;
 	const url = 'https://mv-repo.schul-cloud.org/edu-sharing/rest/authentication/v1/validateSession';
 	const userName = process.env.eduUserName || 'admin';
-	const pw = process.env.eduPassword || 'password';
+	const pw = process.env.eduPassword || '';
 	const headers = {
 		Accept: 'application/json',
 		'Content-type': 'application/json',
 		Authorization: `Basic ${Buffer.from(`${userName}:${pw}`).toString('base64')}`,
-
 	};
 	const options = {
 		url,
 		method: 'GET',
 		headers,
-
 		resolveWithFullResponse: true,
 	};
 	try {
@@ -28,9 +25,10 @@ const getCookie = async (context) => {
 	const cookie = response.statusCode === 200 ? response.headers['set-cookie'][0].slice(0, 43) : '';
 	// checks if cookie is correctly formatted
 	if (!regexp.test(cookie)) {
-		const cookieError = new Error([
+		const cookieError = new Error(
 			`Incorrect cookie format. Expected "JSESSIONID=hexademicalValueHere",
-			 instead got: ${JSON.stringify(cookie)}`]);
+			 instead got: ${JSON.stringify(cookie)}`,
+		);
 		console.error(cookieError);
 		return context;
 	}
@@ -40,7 +38,7 @@ const getCookie = async (context) => {
 	context.arguments[0].headers.Accept = 'application/json';
 	context.arguments[0].headers.accept = 'application/json';
 	context.arguments[0].headers.Cookie = cookie;
-
+	console.log(cookie, '<---- COOKIE');
 	return context;
 };
 
