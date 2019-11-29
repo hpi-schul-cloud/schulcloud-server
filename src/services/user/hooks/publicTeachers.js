ec5072447d7ecd7f706b0d6ae56ac5f5cc61831b
Tobias Wollowski <tobias.wollowski@hpi.de>
2019-11-29 15:59:10 +0100

    configure publicTeacher route

--- src/services/user/hooks/publicTeachers.js
+++ src/services/user/hooks/publicTeachers.js
@@ -1,5 +1,8 @@
 const { authenticate } = require('@feathersjs/authentication');
+const { Forbidden, Conflict } = require('@feathersjs/errors');
 const globalHooks = require('../../../hooks');
+const { lookupSchool } = require('../../news/hooks/news.hooks');
+const { equal: equalIds } = require('../../../helper/compare').ObjectId;
 
 const mapRoleFilterQuery = (hook) => {
 	if (hook.params.query.roles) {
@@ -18,8 +21,33 @@ const filterForPublicTeacher = (hook) => {
 	// Limit accessible user (only teacher which are discoverable)
 	hook.params.query.roles = ['teacher'];
 
-	if (process.env.IGNORE_DISCOVERABILITY !== 'true') {
-		hook.params.query.discoverable = true;
+	const TEACHER_VISIBILITY_FOR_EXTERNAL_TEAM_INVITATION =	hook.app.Settings
+		.get('TEACHER_VISIBILITY_FOR_EXTERNAL_TEAM_INVITATION');
+	switch (TEACHER_VISIBILITY_FOR_EXTERNAL_TEAM_INVITATION) {
+		case 'enabled':
+			break;
+		case 'opt-in':
+			// own school and other schools if opted-in
+			hook.params.query.$or = [
+				{ schoolId: hook.params.account.schoolId },
+				{ discoverable: true },
+			];
+			break;
+		case 'opt-out':
+			// everybody who did not opt-out
+			hook.params.query.discoverable = { $ne: false }; // must not be false
+			break;
+		case 'disabled':
+			// force foreign school id to be set
+			if (equalIds(hook.params.account.schoolId, hook.params.query.schoolId) !== true) {
+				throw new Forbidden('access to foreign school teachers prohibited');
+			}
+			break;
+		default:
+			throw new Conflict(
+				'invalid settings for TEACHER_VISIBILITY_FOR_EXTERNAL_TEAM_INVITATION',
+				{ TEACHER_VISIBILITY_FOR_EXTERNAL_TEAM_INVITATION },
+			);
 	}
 
 	return Promise.resolve(hook);
@@ -28,11 +56,12 @@ const filterForPublicTeacher = (hook) => {
 exports.before = {
 	all: [],
 	find: [
+		authenticate('jwt'),
+		lookupSchool,
 		globalHooks.mapPaginationQuery.bind(this),
 		filterForPublicTeacher,
 		// resolve ids for role strings (e.g. 'TEACHER')
 		globalHooks.resolveToIds.bind(this, '/roles', 'params.query.roles', 'name'),
-		authenticate('jwt'),
 		mapRoleFilterQuery,
 	],
 	get: [authenticate('jwt')],
