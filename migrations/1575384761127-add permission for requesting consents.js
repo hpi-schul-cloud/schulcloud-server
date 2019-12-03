e0731315a8ac92ccd1e605b1d56b18781cdebada
Tobias Wollowski <tobias.wollowski@hpi.de>
2019-12-03 16:14:49 +0100

    add permission REQUEST_CONSTENTS

---
+++ migrations/1575384761127-add permission for requesting consents.js
@@ -0,0 +1,62 @@
+// eslint-disable-next-line no-unused-vars
+const mongoose = require('mongoose');
+// eslint-disable-next-line no-unused-vars
+const { info, error } = require('../src/logger');
+
+const { connect, close } = require('../src/utils/database');
+
+const Role = require('../src/services/role/model');
+
+// How to use more than one schema per collection on mongodb
+// https://stackoverflow.com/questions/14453864/use-more-than-one-schema-per-collection-on-mongodb
+
+const REQUEST_CONSENTS = 'REQUEST_CONSENTS';
+const roleNames = ['teacher', 'administrator', 'superhero'];
+
+module.exports = {
+	up: async function up() {
+		await connect();
+		// ////////////////////////////////////////////////////
+		for (const roleName of roleNames) {
+			const role = await Role.findOne({ name: roleName }).exec();
+			if (role === null) {
+				throw new Error(`role not found: ${roleName}`);
+			}
+			if (role.permissions.includes(REQUEST_CONSENTS)) {
+				info(`role ${roleName} already has permission ${REQUEST_CONSENTS}, ignore...`);
+				break;
+			} else {
+				info(`add permission ${REQUEST_CONSENTS} into role ${roleName}...`);
+				role.permissions.push(REQUEST_CONSENTS);
+				role.permissions.sort();
+				await role.save();
+				info(`successfully added permission ${REQUEST_CONSENTS} for role ${roleName}.`);
+			}
+		}
+
+		// ////////////////////////////////////////////////////
+		await close();
+	},
+
+	down: async function down() {
+		await connect();
+		// ////////////////////////////////////////////////////
+		// Implement the necessary steps to roll back the migration here.
+
+		for (const roleName of roleNames) {
+			const role = await Role.findOne({ name: roleName }).exec();
+			if (role === null) {
+				throw new Error(`role not found: ${roleName}`);
+			}
+			if (role.permissions.includes(REQUEST_CONSENTS)) {
+				info(`role ${roleName} has permission ${REQUEST_CONSENTS}, remove...`);
+				role.permissions.pull(REQUEST_CONSENTS);
+				await role.save();
+				info(`permission ${REQUEST_CONSENTS} successfully removed from role ${roleName}.`);
+			}
+		}
+
+		// ////////////////////////////////////////////////////
+		await close();
+	},
+};
