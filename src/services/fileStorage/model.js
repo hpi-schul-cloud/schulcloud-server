e76dc9d97a6bcc1cb3857dcbea39bb9dd9f622e4
Tobias Wollowski <tobias.wollowski@hpi.de>
2019-12-11 14:25:23 +0100

    remove required from file type for wopi

--- src/services/fileStorage/model.js
+++ src/services/fileStorage/model.js
@@ -56,7 +56,7 @@ const fileSchema = new Schema({
 	isDirectory: { type: Boolean, default: false }, // should be required
 	name: { type: String, required: true },
 	size: { type: Number, required() { return !this.isDirectory; } },
-	type: { type: String, required() { return !this.isDirectory; } },
+	type: { type: String }, // todo add required but then wopi fails
 	storageFileName: { type: String, required() { return !this.isDirectory; } },
 	thumbnail: { type: String },
 	thumbnailRequestToken: { type: String, default: uuid },
