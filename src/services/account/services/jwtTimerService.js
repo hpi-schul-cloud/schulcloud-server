630b789cca299c1b1369b638978f1377a4074757
Thomas Feldtkeller <thomas.feldtkeller@hpi.de>
2019-12-13 16:03:35 +0100

    extract docs

--- src/services/account/services/jwtTimerService.js
+++ src/services/account/services/jwtTimerService.js
@@ -1,6 +1,7 @@
 const auth = require('@feathersjs/authentication');
 const { disallow } = require('feathers-hooks-common');
 const { MethodNotAllowed } = require('@feathersjs/errors');
+const docs = require('./jwtTimerDocs');
 
 const {
 	getRedisClient, redisSetAsync, redisTtlAsync, getRedisIdentifier, getRedisValue,
@@ -46,52 +47,6 @@ class JwtTimerService {
 		this.app = app;
 	}
 }
-
-const docs = {
-	definitions: {
-		jwtTimer: {
-			type: 'object',
-			properties: {
-				ttl: {
-					type: 'integer',
-				},
-			},
-		},
-	},
-	operations: {
-		create: {
-			summary: 'reset jwt ttl',
-			description: 'resets the remaining time the JWT used to authenticate this request is whitelisted,'
-				+ ' and returns the value it was reset to.'
-				+ ' throws an 405 error if the instance does not have support for JWT whitelisting',
-			requestBody: { description: 'no request body required' },
-			responses: {
-				200: {
-					description: 'success',
-					content: { 'application/json': { schema: { $ref: '#/components/schemas/jwtTimer' } } },
-				},
-				405: {
-					description: 'feature is disabled on this instance',
-				},
-			},
-		},
-		find: {
-			summary: 'get ttl of the jwt',
-			description: 'returns the remaining seconds the JWT used to authenticate this request is whitelisted.'
-				+ ' throws an 405 error if the instance does not have support for JWT whitelisting',
-			parameters: {},
-			responses: {
-				200: {
-					description: 'success',
-					content: { 'application/json': { schema: { $ref: '#/components/schemas/jwtTimer' } } },
-				},
-				405: {
-					description: 'feature is disabled on this instance',
-				},
-			},
-		},
-	},
-};
 
 const jwtTimerService = new JwtTimerService();
 
