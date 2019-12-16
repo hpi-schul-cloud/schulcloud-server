7f3e4ea9993b7492951d3821a08ff166a444ac58
CeEv <cedric@it-4d.de>
2019-12-16 13:40:12 +0100

    Move sanitize to utils. Mutate tests.

--- src/app.hooks.js
+++ src/app.hooks.js
@@ -1,85 +1,10 @@
-/* eslint-disable no-param-reassign */
 // Global hooks that run for every service
-const sanitizeHtml = require('sanitize-html');
-const Entities = require('html-entities').AllHtmlEntities;
-const { GeneralError, BadRequest } = require('@feathersjs/errors');
+const { GeneralError } = require('@feathersjs/errors');
 const { iff, isProvider } = require('feathers-hooks-common');
+const { sanitizeHtml: { sanitizeDeep } } = require('./utils');
 
-const entities = new Entities();
 
-const globalHooks = require('./hooks/');
-
-const sanitize = (data, options) => {
-	// https://www.npmjs.com/package/sanitize-html
-	if ((options || {}).html === true) {
-		// editor-content data
-		data = sanitizeHtml(data, {
-			allowedTags: ['h1', 'h2', 'h3', 'blockquote', 'p', 'a', 'ul', 'ol', 's', 'u', 'span', 'del',
-				'li', 'b', 'i', 'img', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
-				'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'audio', 'video', 'iframe'],
-			allowedAttributes: false, // allow all attributes of allowed tags
-			allowedSchemes: ['http', 'https', 'ftp', 'mailto'],
-			parser: {
-				decodeEntities: true,
-			},
-		});
-		data = data.replace(/(&lt;script&gt;).*?(&lt;\/script&gt;)/gim, ''); // force remove script tags
-		data = data.replace(/(<script>).*?(<\/script>)/gim, ''); // force remove script tags
-	} else {
-		// non editor-content data
-		data = sanitizeHtml(data, {
-			allowedTags: [], // disallow all tags
-			allowedAttributes: [], // disallow all attributes
-			allowedSchemes: [], // disallow url schemes
-			parser: {
-				decodeEntities: true,
-			},
-		});
-	}
-	// something during sanitizeHtml() is encoding HTML Entities like & => &amp;
-	// I wasn't able to figure out which option disables this so I just decode it again.
-	// BTW: html-entities is already a dependency of sanitize-html so no new imports where done here.
-	return entities.decode(data);
-};
-
-/**
- * Strips JS/HTML Code from data and returns clean version of it
- * @param data {object/array/string}
- * @returns data - clean without JS
- */
-const maxDeep = 10;
-const sanitizeDeep = (data, path, depth = 0) => {
-	if (depth >= maxDeep) {
-		throw new BadRequest('Data level is to deep. (sanitizeDeep)', { path, data });
-	}
-	if (typeof data === 'object' && data !== null) {
-		Object.entries(data).forEach(([key, value]) => {
-			if (typeof value === 'string') {
-				// ignore values completely
-				if (['password'].includes(key)) return data;
-				// enable html for all current editors
-				const needsHtml = ['content', 'text', 'comment', 'gradeComment', 'description'].includes(key)
-                    && ['lessons', 'news', 'newsModel', 'homework', 'submissions'].includes(path);
-				data[key] = sanitize(value, { html: needsHtml });
-			} else {
-				sanitizeDeep(value, path, depth + 1);
-			}
-		});
-	} else if (typeof data === 'string') {
-		data = sanitize(data, { html: false });
-	} else if (Array.isArray(data)) {
-		for (let i = 0; i < data.length; i += 1) {
-			if (typeof data[i] === 'string') {
-				data[i] = sanitize(data[i], { html: false });
-			} else {
-				sanitizeDeep(data[i], path, depth + 1);
-			}
-		}
-	}
-	return data;
-};
-
-const sanitizeData = (context) => {
+const sanitizeDataHook = (context) => {
 	if (context.data && context.path && context.path !== 'authentication') {
 		sanitizeDeep(context.data, context.path);
 	}
@@ -154,17 +79,17 @@ function setupAppHooks(app) {
 		get: [],
 		create: [
 			iff(isProvider('external'), [
-				sanitizeData, removeObjectIdInData,
+				sanitizeDataHook, removeObjectIdInData,
 			]),
 		],
 		update: [
 			iff(isProvider('external'), [
-				sanitizeData,
+				sanitizeDataHook,
 			]),
 		],
 		patch: [
 			iff(isProvider('external'), [
-				sanitizeData,
+				sanitizeDataHook,
 			]),
 		],
 		remove: [],
@@ -196,6 +121,9 @@ function setupAppHooks(app) {
 		before.all.unshift(displayInternRequests(app.get('DISPLAY_REQUEST_LEVEL')));
 	}
 	app.hooks({ before, after, error });
-};
+}
 
-module.exports = { setupAppHooks, sanitizeData };
+module.exports = {
+	setupAppHooks,
+	sanitizeDataHook,
+};
--- test/app.hooks.test.js
+++ test/app.hooks.test.js
@@ -1,95 +1,58 @@
-const assert = require('assert');
 const chai = require('chai');
 const { ObjectId } = require('mongoose').Types;
 
 const { expect } = chai;
 
 const app = require('../src/app');
-const { sanitizeData } = require('../src/app.hooks');
+const { sanitizeDataHook } = require('../src/app.hooks');
+const { sanitizeHtml: { sanitizeDeep } } = require('../src/utils');
 const { cleanup, createTestUser } = require('./services/helpers/testObjects')(app);
 
-describe.only('Sanitization Hook', () => {
-	let server;
-
-	let newsService;
-	let helpdeskService;
-	let courseService;
-	let lessonService;
-
-	let currentUsedId;
-	let currentLessonId = null;
-	let createContext = () => ({});
-
-	before((done) => {
-		server = app.listen(0);
-		newsService = app.service('news');
-		helpdeskService = app.service('helpdesk');
-		courseService = app.service('courses');
-		lessonService = app.service('lessons');
-
-		createContext = () => ({
-			payload: { userId: '0000d213816abba584714c0a' },
-			account: { userId: '0000d213816abba584714c0a' },
-			provider: 'rest',
-		});
-		done();
+describe('Sanitization Hook', () => {
+	// TODO: Test if it work for create, post and update
+	it('hook ignor authentication route', () => {
+		const testString = '<h1> test </h1>';
+		const context = {
+			path: 'authentication',
+			data: { testString },
+		};
+		const result = sanitizeDataHook(context);
+		expect(result.data).to.not.be.an('undefined');
+		expect(result.data.testString).to.equal(testString);
 	});
-
-	after((done) => {
-		server.close();
-		done();
-	});
-
-	it('registered the news service (Sanitization)', (done) => {
-		assert.ok(newsService);
-		done();
-	});
-
-	it('POST /news (Sanitization)', async () => {
-		const postBody = {
+	// TODO: Map test to generic output for sanitizeConst keys, paths, saveKeys
+	it('sanitize in news, example', () => {
+		const data = {
 			schoolId: '0000d186816abba584714c5f',
 			title: '<script>alert("test");</script>SanitizationTest äöüß§$%/()=',
 			content: '<p>SanitizationTest<script>alert("test);</script>'
 					+ '<a href="javascript:test();">SanitizationTest</a></p>äöüß§$%/()=',
 		};
 
-		const result = await newsService
-			.create(postBody, createContext());
-
-		currentUsedId = result._id;
+		const path = 'news';
+		const result = sanitizeDeep(data, path);
+		expect(result.schoolId).to.equal('0000d186816abba584714c5f');
 		expect(result.title).to.equal('SanitizationTest äöüß§$%/()=');
 		expect(result.content).to.equal('<p>SanitizationTest<a>SanitizationTest</a></p>äöüß§$%/()=');
 	});
 
-	it('POST FAIL /news (Sanitization)', () => {
-		const postBody = {
+	it('sanitize in news, example 2', () => {
+		const data = {
 			schoolId: '0000d186816abba584714c5f',
 			title: '<script>alert("test");</script><b></b><i></i><img src="bla" />',
 			content: 'a',
 		};
 
-		return newsService.create(postBody, createContext())
-			.catch((exception) => {
-				expect(exception).to.not.be.undefined;
-				expect(exception.code).to.equal(400);
-				expect(exception.message).to.equal('news validation failed: title: Path `title` is required.');
-			});
+		const path = 'news';
+		const result = sanitizeDeep(data, path);
+
+		expect(result.schoolId).to.equal('0000d186816abba584714c5f');
+		expect(result.title).to.equal(''); // filter all
+		expect(result.content).to.equal('a');
 	});
 
-	it('DELETE /news (Sanitization)', () => newsService
-		.remove(currentUsedId, createContext())
-		.then((result) => {
-			expect(result).to.not.be.undefined;
-			expect(result.title).to.equal('SanitizationTest äöüß§$%/()=');
-		}));
-
-	it('registered the helpdesk service (Sanitization)', (done) => {
-		assert.ok(helpdeskService);
-		done();
-	});
-
-	it('POST /helpdesk (Sanitization)', () => {
-		const postBody = {
+	it('sanitize in helpdesk, example 1', () => {
+		const data = {
 			subject: '<script>alert("test");</script>SanitizationTest äöüß§$%/()=',
 			type: 'problem',
 			currentState: '<p>SanitizationTest<script>alert("test);</script>'
@@ -100,50 +63,30 @@ describe.only('Sanitization Hook', () => {
 			schoolId: '0000d186816abba584714c5f',
 		};
 
-		return helpdeskService.create(postBody, createContext())
-			.then((result) => {
-				currentUsedId = result._id;
-				expect(result.subject).to.equal('SanitizationTest äöüß§$%/()=');
-				expect(result.currentState).to.equal('SanitizationTestSanitizationTestäöüß§$%/()=');
-				expect(result.targetState).to.equal('SanitizationTestSanitizationTestäöüß§$%/()=');
-			});
+		const path = 'helpdesk';
+		const result = sanitizeDeep(data, path);
+
+		expect(result.subject).to.equal('SanitizationTest äöüß§$%/()=');
+		expect(result.type).to.equal('problem');
+		expect(result.currentState).to.equal('SanitizationTestSanitizationTestäöüß§$%/()=');
+		expect(result.targetState).to.equal('SanitizationTestSanitizationTestäöüß§$%/()=');
+		expect(result.category).to.equal('dashboard');
+		expect(result.schoolId).to.equal('0000d186816abba584714c5f');
 	});
 
-	it('POST FAIL /helpdesk (Sanitization)', () => {
-		const postBody = {
+	it('sanitize in helpdesk, example 2', () => {
+		const data = {
 			subject: '<script>alert("test");</script><b></b><i></i><img src="bla" />',
-			type: 'problem',
-			currentState: '<p>SanitizationTest<script>alert("test);</script>'
-						+ '<a href="javascript:test();">SanitizationTest</a></p>äöüß§$%/()=',
-			targetState: '<p>SanitizationTest<script>alert("test);</script>'
-						+ '<a href="javascript:test();">SanitizationTest</a></p>äöüß§$%/()=',
-			category: 'dashboard',
-			schoolId: '0000d186816abba584714c5f',
 		};
 
-		return helpdeskService.create(postBody, createContext())
-			.catch((exception) => {
-				expect(exception).to.not.be.undefined;
-				expect(exception.code).to.equal(400);
-				expect(exception.message).to.equal('problem validation failed: subject: Path `subject` is required.');
-			});
+		const path = 'helpdesk';
+		const result = sanitizeDeep(data, path);
+
+		expect(result.subject).to.equal(''); // filter all
 	});
 
-	it('DELETE /helpdesk (Sanitization)',
-		() => helpdeskService.remove(currentUsedId, createContext())
-			.then((result) => {
-				expect(result).to.not.be.undefined;
-				expect(result.subject).to.equal('SanitizationTest äöüß§$%/()=');
-			}));
-
-	it('registered the courses service (Sanitization)', (done) => {
-		assert.ok(courseService);
-		assert.ok(lessonService);
-		done();
-	});
-
-	it('POST /courses and /lessons (Sanitization)', () => {
-		const postBody = {
+	it('sanitize in course, example 1', () => {
+		const data = {
 			name: '<script>alert("test");</script>SanitizationTest äöüß§$%/()=',
 			description: '<p>SanitizationTest<script>alert("test);</script>'
 						+ '<a href="javascript:test();">SanitizationTest</a></p>äöüß§$%/()=',
@@ -152,35 +95,30 @@ describe.only('Sanitization Hook', () => {
 			schoolId: '0000d186816abba584714c5f',
 		};
 
-		return courseService.create(postBody, createContext())
-			.then((result) => {
-				currentUsedId = result._id;
-				expect(result.name).to.equal('SanitizationTest äöüß§$%/()=');
-				expect(result.description).to.equal('SanitizationTestSanitizationTestäöüß§$%/()=');
-			});
+		const path = 'course';
+		const result = sanitizeDeep(data, path);
+
+		expect(result.name).to.equal('SanitizationTest äöüß§$%/()=');
+		expect(result.description).to.equal('SanitizationTestSanitizationTestäöüß§$%/()=');
+		expect(result.color).to.equal('#d32f22');
+		expect(result.teacherIds[0]).to.equal('0000d213816abba584714c0a');
+		expect(result.schoolId).to.equal('0000d186816abba584714c5f');
 	});
 
-	it('POST FAIL /courses (Sanitization)', () => {
-		const postBody = {
+	it('sanitize in course, example 2', () => {
+		const data = {
 			name: '<script>alert("test");</script><b></b><i></i><img src="bla" />',
-			description: '<p>SanitizationTest<script>alert("test);</script>'
-						+ '<a href="javascript:test();">SanitizationTest</a></p>äöüß§$%/()=',
-			color: '#d32f22',
-			teacherIds: ['0000d213816abba584714c0a'],
-			schoolId: '0000d186816abba584714c5f',
 		};
 
-		return courseService.create(postBody, createContext())
-			.catch((exception) => {
-				expect(exception).to.not.be.undefined;
-				expect(exception.code).to.equal(400);
-				expect(exception.message).to.equal('course validation failed: name: Path `name` is required.');
-			});
+		const path = 'course';
+		const result = sanitizeDeep(data, path);
+
+		expect(result.name).to.equal(''); // filter all
 	});
 
-	it('POST /lessons (Sanitization)', () => {
-		const postBody = {
-			courseId: currentUsedId,
+	it('sanitize in lessons, example 1', () => {
+		const data = {
+			courseId: '0000d186816abba584714c5d',
 			name: '<script>alert("test");</script>SanitizationTest äöüß§$%/()=',
 			contents: [
 				{
@@ -199,57 +137,27 @@ describe.only('Sanitization Hook', () => {
 			materialIds: [],
 		};
 
-		return lessonService.create(postBody, createContext())
-			.then((lresult) => {
-				currentLessonId = lresult._id;
-				expect(lresult.name).to.equal('SanitizationTest äöüß§$%/()=');
-				expect(lresult.contents[0].content.text)
-					.to.equal('<p>SanitizationTest<a>SanitizationTest</a></p>äöüß§$%/()=');
-			});
+		const path = 'lessons';
+		const result = sanitizeDeep(data, path);
+
+		expect(result.courseId).to.equal('0000d186816abba584714c5d');
+		expect(result.name).to.equal('SanitizationTest äöüß§$%/()=');
+		expect(result.contents[0].content.text).to.equal('<p>SanitizationTest<a>SanitizationTest</a></p>äöüß§$%/()=');
+		expect(result.schoolId).to.equal('0000d186816abba584714c5f');
+		expect(result.position).to.equal(0);
+		expect(result.materialIds.length).to.equal(0);
 	});
 
-	it('POST FAIL /lessons (Sanitization)', () => {
-		const postBody = {
-			courseId: currentUsedId,
+	it('sanitize in lessons, example 2', () => {
+		const data = {
 			name: '<script>alert("test");</script><b></b><i></i><img src="bla" />',
-			contents: [
-				{
-					title: '<script>alert("test");</script>SanitizationTest äöüß§$%/()=',
-					hidden: false,
-					component: 'text',
-					user: '0000d213816abba584714c0a',
-					content: {
-						text: '<p>SanitizationTest<script>alert("test);</script>'
-							+ '<a href="javascript:test();">SanitizationTest</a></p>äöüß§$%/()=',
-					},
-				},
-			],
-			schoolId: '0000d186816abba584714c5f',
-			position: 0,
-			materialIds: [],
 		};
 
-		return lessonService.create(postBody, createContext())
-			.catch((exception) => {
-				expect(exception).to.not.be.undefined;
-				expect(exception.code).to.equal(400);
-				expect(exception.message).to.equal('lesson validation failed: name: Path `name` is required.');
-			});
+		const path = 'lessons';
+		const result = sanitizeDeep(data, path);
+
+		expect(result.name).to.equal('');
 	});
-
-	it('DELETE /lessons (Sanitization)',
-		() => lessonService.remove(currentLessonId, createContext())
-			.then((result) => {
-				expect(result).to.not.be.undefined;
-				expect(result.name).to.equal('SanitizationTest äöüß§$%/()=');
-			}));
-
-	it('DELETE /courses (Sanitization)',
-		() => courseService.remove(currentUsedId, createContext())
-			.then((result) => {
-				expect(result).to.not.be.undefined;
-				expect(result.name).to.equal('SanitizationTest äöüß§$%/()=');
-			}));
 });
 
 describe('removeObjectIdInData hook', () => {
