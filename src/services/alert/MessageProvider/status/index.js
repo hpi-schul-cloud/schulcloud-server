430876ccb63a0b9a97f41a6cc2605070aa4ae1c5
BlackGhostHD <36112357+BlackGhostHD@users.noreply.github.com>
2019-12-20 10:54:11 +0100

    Update index.js

--- src/services/alert/MessageProvider/status/index.js
+++ src/services/alert/MessageProvider/status/index.js
@@ -22,7 +22,7 @@ function isInstance(instance, componentId) {
 		// componentId 0: message not instance specific
 		if (componentId !== 0) {
 			/* eslint-disable max-len */
-			request.get(`${process.env.STATUS_API_URL}/components/${componentId}`, { timeout: 1500 }, (err, response, body) => {
+			request.get(`${process.env.STATUS_API_URL}/components/${componentId}`, { timeout: 3000 }, (err, response, body) => {
 				if (!err && response.statusCode === 200) {
 					const data = JSON.parse(body);
 					// translate instance into group_id
@@ -47,7 +47,7 @@ function isInstance(instance, componentId) {
  */
 function getRawData() {
 	return new Promise((resolve, reject) => {
-		request.get(`${process.env.STATUS_API_URL}/incidents`, { timeout: 1500 }, (err, response, body) => {
+		request.get(`${process.env.STATUS_API_URL}/incidents`, { timeout: 3000 }, (err, response, body) => {
 			if (!err && response.statusCode === 200) {
 				resolve(JSON.parse(body));
 			} else {
