6c96322d2eb11a94b648a5a67bc21735ab135a82
dasGoogle <aaronschlitt@gmx.de>
2019-12-31 15:16:52 +0100

    add test for link created now

--- test/services/link/index.test.js
+++ test/services/link/index.test.js
@@ -7,6 +7,7 @@ const app = require('../../../src/app');
 chai.use(chaiHttp);
 
 const subtractDays = (date, d) => {
+	// calculation method intentionally different from what is being used in production
 	date.setTime(date.getTime() - (d * 24 * 60 * 60 * 1000));
 	return date;
 };
@@ -134,6 +135,29 @@ describe('link service', () => {
 	it('generates a valid link two days in the future', function () {
 		this.timeout(10000);
 		const createdAt = subtractDays(new Date(), -2);
+
+		const url = 'http://localhost:3100/registration/';
+		return service.create({ target: url, createdAt })
+			.then((data) => {
+				chai.expect(data.id).to.have.lengthOf(service.Model.linkLength);
+				chai.expect(data.target).to.equal(url);
+				return Promise.resolve(data.id);
+			})
+			.then((id) => new Promise((resolve, reject) => {
+				chai.request(app)
+					.get(`/link/${id}`)
+					.end((error, result) => {
+						if (error) return reject(error);
+						chai.expect(result.status).to.equal(200);
+						chai.expect(result.body).to.include.keys('target');
+						return resolve();
+					});
+			}));
+	});
+
+	it('generates a valid link from now', function () {
+		this.timeout(10000);
+		const createdAt = new Date();
 
 		const url = 'http://localhost:3100/registration/';
 		return service.create({ target: url, createdAt })
