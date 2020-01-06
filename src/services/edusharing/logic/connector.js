7d9ef54ddcde0f9bf3b5d70d2bde786ffb492d68
Tormod Flesjø <tormod.flesjo@gmail.com>
2020-01-06 13:21:40 +0100

    add minor formatting

--- src/services/edusharing/logic/connector.js
+++ src/services/edusharing/logic/connector.js
@@ -1,19 +1,21 @@
-const request = require('request-promise-native');
+const request = require("request-promise-native");
 // const feathersError = require('@feathersjs/errors');
-// const URL = require('url');
+const url = require("url");
 
 const REQUEST_TIMEOUT = 8000; // ms
 
 function validURL(str) {
-	const pattern = new RegExp('^(https?:\\/\\/)?'
-    + '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'
-    + '((\\d{1,3}\\.){3}\\d{1,3}))'
-    + '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'
-    + '(\\?[;&a-z\\d%_.~+=-]*)?'
-    + '(\\#[-a-z\\d_]*)?$', 'i');
+	const pattern = new RegExp(
+		"^(https?:\\/\\/)?" +
+			"((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" +
+			"((\\d{1,3}\\.){3}\\d{1,3}))" +
+			"(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" +
+			"(\\?[;&a-z\\d%_.~+=-]*)?" +
+			"(\\#[-a-z\\d_]*)?$",
+		"i"
+	);
 	return !!pattern.test(str);
 }
-
 
 class EduSharingConnector {
 	constructor() {
@@ -21,7 +23,7 @@ class EduSharingConnector {
 			return EduSharingConnector.instance;
 		}
 		if (!validURL(this.url)) {
-			return 'Invalid ES_DOMAIN, check your .env';
+			return "Invalid ES_DOMAIN, check your .env";
 		}
 		this.authorization = null;
 		EduSharingConnector.instance = this;
@@ -29,8 +31,8 @@ class EduSharingConnector {
 
 	static get headers() {
 		return {
-			Accept: 'application/json',
-			'Content-type': 'application/json',
+			Accept: "application/json",
+			"Content-type": "application/json"
 		};
 	}
 
@@ -39,33 +41,45 @@ class EduSharingConnector {
 		const pw = process.env.ES_PASSWORD;
 
 		const headers = Object.assign({}, EduSharingConnector.headers, {
-			Authorization: `Basic ${Buffer.from(`${userName}:${pw}`).toString('base64')}`,
+			Authorization: `Basic ${Buffer.from(`${userName}:${pw}`).toString(
+				"base64"
+			)}`
 		});
 		return headers;
 	}
-
-	 getCookie() {
+	// todo, path combine with URL
+	getCookie() {
 		const cookieOptions = {
 			uri: `${process.env.ES_DOMAIN}/edu-sharing/rest/authentication/v1/validateSession`,
-			method: 'GET',
+			method: "GET",
 			headers: EduSharingConnector.authorization,
 			resolveWithFullResponse: true,
+			json: true
 		};
-		return request(cookieOptions).then((result) => {
-			if (result.statusCode !== 200) {
-				throw Error('authentication error with edu sharing');
-			}
-			return result.headers['set-cookie'][0];
-		}).catch((err) => {
-			// eslint-disable-next-line no-console
-			console.error('error: ', err);
-		});
+		return request(cookieOptions)
+			.then(result => {
+				console.log(result.statusCode === 200, "statusCode");
+				console.log(result.body.isValidLogin === true, "validLogin");
+				if (
+					result.statusCode !== 200 ||
+					result.body.isValidLogin !== true
+				) {
+					throw Error("authentication error with edu sharing");
+				}
+				return result.headers["set-cookie"][0];
+			})
+			.catch(err => {
+				// eslint-disable-next-line no-console
+				console.error("error: ", err);
+			});
 	}
 
 	checkEnv() {
-		return process.env.ES_DOMAIN
-		&& process.env.ES_USER
-		&& process.env.ES_PASSWORD;
+		return (
+			process.env.ES_DOMAIN &&
+			process.env.ES_USER &&
+			process.env.ES_PASSWORD
+		);
 	}
 
 	async login() {
@@ -76,34 +90,68 @@ class EduSharingConnector {
 		return !!this.authorization;
 	}
 
-
 	async GET(data) {
+		const myVar = process.env.ES_DOMAIN; // "https://mv-repo.schul-cloud.org"
+		const myHost = process.env.HOST; // "http://localhost:3030"
+
+		// proxy task
+		if (data.mode && data.mode === "preview") {
+			console.log(data.params, "paramsurl");
+			const newUrl = new url.URL(
+				"/edu-sharing/preview",
+				process.env.ES_DOMAIN
+			);
+			const modeOptions = {
+				method: "GET",
+				resolveWithFullResponse: true,
+				transform: function(body) {
+					return body;
+				},
+				qs: data.params.query,
+				url: newUrl.toString(),
+				headers: Object.assign(
+					{},
+					{
+						cookie: this.authorization
+					}
+				)
+			};
+			return request(modeOptions);
+		}
 		const limit = data.query.$limit || 9;
 		const skip = data.query.$skip || 0;
-		const searchQuery = data.query['_all[#match]'] || 'img'; // will give pictures of flowers as default
-		const contentType = data.query.contentType || 'ALL'; // enum: see swagger
+		const searchWord = data.query.searchWord || "img"; // will give pictures of flowers as default
+		const contentType = data.query.contentType || "FILES"; // enum: ['FILES', FILES_AND_FOLDERS,COLLECTIONS, TOOLPERMISSIONS, ALL]
 
 		if (!this.checkEnv()) {
-			return 'Update your env variables. See --> src/services/edusharing/envTemplate';
+			return "Update your env variables. See --> src/services/edusharing/envTemplate";
 		}
 
 		if (this.isLoggedin() === false) {
 			await this.login();
 		}
+		console.log(searchWord, "searchword");
 		const options = {
-			method: 'POST',
+			method: "POST",
 			url: `${process.env.ES_DOMAIN}/edu-sharing/rest/search/v1/queriesV2/mv-repo.schul-cloud.org/mds/ngsearch/?contentType=${contentType}&skipCount=${skip}&maxItems=${limit}&sortProperties=score&sortProperties=cm%3Amodified&sortAscending=false&sortAscending=false&propertyFilter=-all-&`,
 			headers: Object.assign({}, EduSharingConnector.headers, {
-				cookie: this.authorization,
+				cookie: this.authorization
 			}),
-			body: JSON.stringify({ criterias: [{ property: 'ngsearchword', values: [`${searchQuery}`] }], facettes: ['cclom:general_keyword'] }),
-			timeout: REQUEST_TIMEOUT,
+			body: JSON.stringify({
+				criterias: [
+					{ property: "ngsearchword", values: [`${searchWord}`] }
+				],
+				facettes: ["cclom:general_keyword"]
+			}),
+			timeout: REQUEST_TIMEOUT
 		};
+
 		const eduResponse = await request(options);
+		const regex = new RegExp(process.env.ES_DOMAIN, "g");
+		const parsed = JSON.parse(eduResponse.replace(regex, process.env.HOST));
 
-		return eduResponse;
+		return parsed;
 	}
-
 
 	static get Instance() {
 		if (!EduSharingConnector.instance) {
@@ -112,6 +160,5 @@ class EduSharingConnector {
 		return EduSharingConnector.instance;
 	}
 }
-
 
 module.exports = EduSharingConnector.Instance;
--- src/services/edusharing/services/eduSearch.js
+++ src/services/edusharing/services/eduSearch.js
@@ -1,16 +1,28 @@
-const request = require('request-promise-native');
-const hooks = require('../hooks');
-const EduSharingConnector = require('../logic/connector');
+//const request = require("request-promise-native");
+const hooks = require("../hooks");
+const EduSharingConnector = require("../logic/connector");
+const { BadRequest } = require("@feathersjs/errors");
 
 class EduSearch {
 	find(data) {
+		console.log("EduSearch.find");
 		return EduSharingConnector.GET(data);
+	}
+	get(mode, params) {
+		console.log(mode, "<-- MODE");
+		if (mode !== "preview") {
+			throw new BadRequest("Mode must be preview");
+		}
+		// expect edusharingconnector fetches the image from edu sharing and return the image data back.
+		return EduSharingConnector.GET({ mode, params });
 	}
 }
 
-module.exports = (app) => {
-	const eduRoute = '/edusharing/search';
-	app.use(eduRoute, new EduSearch());
-	const eduService = app.service('/edusharing/search');
+module.exports = app => {
+	const eduRoute = "/edu-sharing";
+	app.use(eduRoute, new EduSearch(), (req, res) => {
+		res.send(res.data);
+	});
+	const eduService = app.service("/edu-sharing");
 	eduService.hooks(hooks);
 };
