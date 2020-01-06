c6241279719edb13a125fa484be513379a2aa325
dominikglandorf <do.gl@gmx.de>
2020-01-06 17:25:51 +0100

    add deep link endpoint

--- src/services/ltiTool/index.js
+++ src/services/ltiTool/index.js
@@ -23,9 +23,32 @@ module.exports = function () {
 
 	app.use('/tools/sign', {
 		create(data) {
+			data.request.name = decodeURI(data.request.name);
 			return new Promise((resolve) => resolve(
 				jwt.sign(data.request, fs.readFileSync('private_key.pem'), { algorithm: 'RS256' })
 			));
+		},
+	});
+
+	app.use('/tools/:id/link', {
+		create(data, params) {
+			return app.service('ltiTools').find({
+				query: {
+					oAuthClientId: jwt.decode(data.id_token).iss,
+					lti_version: '1.3.0',
+					isTemplate: true,
+				},
+			}).then((tool) => {
+				const idToken = jwt.verify(data.id_token, tool.data[0].key, { algorithm: 'RS256' });
+				const link = idToken['https://purl.imsglobal.org/spec/lti-dl/claim/content_items'];
+				return `<!DOCTYPE><html>
+	<body>
+		<script>
+		if(window.parent['link-${params.route.id}']) window.parent['link-${params.route.id}']('${link.url}');
+		</script>
+	</body>
+</html>`;
+			});
 		},
 	});
 };
