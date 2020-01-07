d083d819793953c2c015bd09c08b70d62631433e
adrianjost <22987140+adrianjost@users.noreply.github.com>
2020-01-07 11:13:59 +0100

    Update globals.js

--- config/globals.js
+++ config/globals.js
@@ -4,7 +4,7 @@ module.exports = {
 	DOCUMENT_BASE_DIR: process.env.DOCUMENT_BASE_DIR || 'https://s3.hidrive.strato.com/schul-cloud-hpi/',
 	FORCE_SEND_EMAIL: process.env.FORCE_SEND_EMAIL || false,
 	MAXIMUM_ALLOWABLE_TOTAL_ATTACHMENTS_SIZE_BYTE: (5 * 1024 * 1024), // 5MB
-	NOTIFICATION_PLATFORM: process.env.NOTIFICATION_PLATFORM  || false,
+	NOTIFICATION_PLATFORM: process.env.NOTIFICATION_PLATFORM || false,
 	REQUEST_TIMEOUT: 8000,
 	SC_THEME: process.env.SC_THEME || 'default',
 	SMTP_SENDER: process.env.SMTP_SENDER || 'noreply@schul-cloud.org',
