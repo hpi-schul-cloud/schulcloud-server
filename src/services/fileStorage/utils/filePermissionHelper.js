464a5b4a9b12cb7f8e5c63c76b24e97ec1721730
Dominik Brüchner <dominik.bruechner@hpi.uni-potsdam.de>
2020-01-08 16:58:29 +0100

    added check

--- src/services/fileStorage/utils/filePermissionHelper.js
+++ src/services/fileStorage/utils/filePermissionHelper.js
@@ -112,15 +112,15 @@ const checkPermissions = (permission) => async (user, file) => {
 		return Promise.reject();
 	}
 
-	const homework = await Homework.findOne({ fileIds: fileObject._id }).lean().exec();
+	const homework = await Homework.findOne({ fileIds: fileObject._id }).populate('courseId').lean().exec();
 	if (homework && !homework.private) {
-		// TODO: Check if user as access to homework
 		// let courseFile = fileObject;
-		// courseFile = { ...fileObject, owner: homework.courseId || {} };
-		// const isMember = checkMemberStatus({ file: courseFile, user });
-		// if isMember??
+		let courseFile = fileObject;
+		courseFile = { ...fileObject, owner: homework.courseId || {} };
+		const isMember = checkMemberStatus({ file: courseFile, user });
+		if (isMember) return Promise.resolve(true);
 
-		return Promise.resolve(true);
+		return Promise.resolve(false);
 	}
 
 	const isMember = checkMemberStatus({ file: fileObject, user });
