798416d1f3e08f82d1bbbaa71b0affff198d5c87
Tormod Flesjø <tormod.flesjo@gmail.com>
2020-01-09 15:38:37 +0100

    revert changes on this file, not finished with it yet

--- src/services/edusharing/services/eduSearch.js
+++ src/services/edusharing/services/eduSearch.js
@@ -1,19 +1,18 @@
-//const request = require("request-promise-native");
-const hooks = require("../hooks");
-const EduSharingConnector = require("../logic/connector");
+const hooks = require('../hooks');
+const EduSharingConnector = require('../logic/connector');
 
 class EduSearch {
 	find(data) {
-		console.log("EduSearch.find");
+		console.log('EduSearch.find');
 		return EduSharingConnector.GET(data);
 	}
 }
 
 module.exports = app => {
-	const eduRoute = "/edu-sharing";
+	const eduRoute = '/edu-sharing';
 	app.use(eduRoute, new EduSearch(), (req, res) => {
 		res.send(res.data);
 	});
-	const eduService = app.service("/edu-sharing");
+	const eduService = app.service('/edu-sharing');
 	eduService.hooks(hooks);
 };
