f6c76a588ef39f6ea42f65bd156182e6d43f5076
Tobias Wollowski <tobias.wollowski@hpi.de>
2020-01-14 18:39:11 +0100

    modify bbb tool creation

--- src/services/ltiTool/hooks/index.js
+++ src/services/ltiTool/hooks/index.js
@@ -1,11 +1,27 @@
 const { authenticate } = require('@feathersjs/authentication');
 const globalHooks = require('../../../hooks');
+const { isValid } = require('../../../helper/compare').ObjectId;
+
+
+const isBBBCreateRequest = (context) => context.data && context.data.url && context.data.url === 'BBB_URL';
+
+const isValidCourseTool = (context) => context.data && context.data.courseId && isValid(context.data.courseId);
+
+const setupBBB = (context) => {
+	// ignore if no bbb request or no valid courseId given
+	if (!isBBBCreateRequest(context) || !isValidCourseTool(context)) {
+		return context;
+	}
+	// update url
+	context.data.url = `/videoconference/course/${context.data.courseId}`;
+	return context;
+};
 
 exports.before = {
 	all: [authenticate('jwt')],
 	find: [globalHooks.hasPermission('TOOL_VIEW')],
 	get: [globalHooks.hasPermission('TOOL_VIEW')],
-	create: [globalHooks.hasPermission('TOOL_CREATE')],
+	create: [globalHooks.hasPermission('TOOL_CREATE'), setupBBB],
 	update: [globalHooks.hasPermission('TOOL_EDIT')],
 	patch: [globalHooks.hasPermission('TOOL_EDIT')],
 	remove: [globalHooks.hasPermission('TOOL_CREATE')],
