5c4502482d7383bd41a981936e84c588ae9fd1f6
dasGoogle <aaronschlitt@gmx.de>
2020-01-19 12:07:19 +0100

    move duplicate prevention to hooks

--- src/services/link/hooks/index.js
+++ src/services/link/hooks/index.js
@@ -1,12 +1,33 @@
 const hooks = require('feathers-hooks-common');
 const { authenticate } = require('@feathersjs/authentication');
 const globalHooks = require('../../../hooks');
+const logger = require('../../../logger');
+
+const preventDuplicates = async (hook) => {
+	const linkData = hook.data;
+	// get all links to the same target
+	const currentLinks = await hook.app.service('link').find({ query: { target: linkData.target } });
+
+	// check if link to specified target already exists
+	if (currentLinks && currentLinks.total > 0) {
+		// if so, set createdAt date to now so that link expires a month from now
+		const id = currentLinks.data[0]._id;
+		await hook.app.service('link').patch(id, { createdAt: new Date() }).then((updatedShortlink) => {
+			// return the updated link and don't create a new one
+			hook.result = updatedShortlink;
+		}).catch((err) => {
+			logger.warning(err);
+			return Promise.reject(new Error('Fehler beim Aktualisieren des Kurzlinks.'));
+		});
+	}
+	return hook;
+};
 
 exports.before = {
 	all: [],
 	find: [],
 	get: [hooks.disallow('external')], // handled by redirection middleware
-	create: [authenticate('jwt'), globalHooks.hasPermission('LINK_CREATE')],
+	create: [authenticate('jwt'), globalHooks.hasPermission('LINK_CREATE'), preventDuplicates],
 	update: [hooks.disallow()],
 	patch: [hooks.disallow('external')],
 	remove: [globalHooks.ifNotLocal(hooks.disallow())],
