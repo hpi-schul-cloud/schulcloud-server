a5621a41ab0db534a8995aa9f8909fcdddd62718
Thomas Feldtkeller <thomas.feldtkeller@hpi.de>
2020-01-27 17:05:35 +0100

    test course patch permissions

--- test/services/courses/services/courses.test.js
+++ test/services/courses/services/courses.test.js
@@ -1,27 +1,59 @@
-const assert = require('assert');
-const chai = require('chai');
+const { expect } = require('chai');
 
 const app = require('../../../../src/app');
 
 const courseService = app.service('courses');
 
-// const testObjects = require('../helpers/testObjects')(app);
+const testObjects = require('../../helpers/testObjects')(app);
 
 describe('course service', () => {
 	it('registered the courses service', () => {
-		assert.ok(courseService);
+		expect(courseService).to.not.be.undefined;
 	});
 
-	it('creates a course', () => courseService.create({
-		name: 'testCourse',
-		schoolId: '0000d186816abba584714c5f',
-		userIds: [],
-		classIds: [],
-		teacherIds: [],
-		ltiToolIds: [],
-	})
-		.then((course) => {
-			chai.expect(course.name).to.equal('testCourse');
-			chai.expect(course.userIds).to.have.lengthOf(0);
-		}));
+	it('creates a course', async () => {
+		const course = await courseService.create({
+			name: 'testCourse',
+			schoolId: '0000d186816abba584714c5f',
+			userIds: [],
+			classIds: [],
+			teacherIds: [],
+			ltiToolIds: [],
+		});
+		expect(course.name).to.equal('testCourse');
+		expect(course.userIds).to.have.lengthOf(0);
+	});
+
+	it('teacher can PATCH course', async () => {
+		const teacher = await testObjects.createTestUser({ roles: ['teacher'] });
+		const course = await testObjects.createTestCourse({ name: 'courseNotChanged', teacherIds: [teacher._id] });
+		const params = await testObjects.generateRequestParamsFromUser(teacher);
+
+		const result = await courseService.patch(course._id, { name: 'courseChanged' }, params);
+		expect(result.name).to.equal('courseChanged');
+	});
+
+	it('substitution teacher can not PATCH course', async () => {
+		try {
+			const teacher = await testObjects.createTestUser({ roles: ['teacher'] });
+			const course = await testObjects.createTestCourse({
+				name: 'courseNotChanged', substitutionIds: [teacher._id],
+			});
+			const params = await testObjects.generateRequestParamsFromUser(teacher);
+			await courseService.patch(course._id, { name: 'courseChanged' }, params);
+			throw new Error('should have failed');
+		} catch (err) {
+			expect(err.message).to.not.equal('should have failed');
+			expect(err.code).to.eq(403);
+		}
+	});
+
+	/* it('teacher can DELETE course', async () => {
+		const teacher = await testObjects.createTestUser({ roles: ['teacher'] });
+		const course = await testObjects.createTestCourse({ name: 'course', teacherIds: [teacher._id] });
+		const params = await testObjects.generateRequestParamsFromUser(teacher);
+
+		const result = await courseService.remove(course._id, params);
+		expect(result.name).to.equal('courseChanged');
+	}); */
 });
