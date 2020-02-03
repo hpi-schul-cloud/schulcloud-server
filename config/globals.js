ea4856047e28aed55ffd1cfb59dd7d7b789db472
Tobias Wollowski <tobias.wollowski@hpi.de>
2020-02-03 14:18:11 +0100

    remove bbb from globals

--- config/globals.js
+++ config/globals.js
@@ -1,17 +1,6 @@
 module.exports = {
 	BODYPARSER_JSON_LIMIT: process.env.BODYPARSER_JSON_LIMIT || '20mb',
 	DATABASE_AUDIT: process.env.DATABASE_AUDIT || 'false',
-	FEATURE_VIDEOCONFERENCE_ENABLED: process.env.FEATURE_VIDEOCONFERENCE_ENABLED === 'true',
-	SERVICES: {
-		/**
-		 * Required, if FEATURE_VIDEOCONFERENCE_ENABLED='true',
-		 * API-Mate: https://mconf.github.io/api-mate/
- 		*/
-		VIDEOCONFERENCE: {
-			HOST: process.env.BBB_URL,
-			SALT: process.env.BBB_SALT,
-		},
-	},
 	DOCUMENT_BASE_DIR: process.env.DOCUMENT_BASE_DIR || 'https://s3.hidrive.strato.com/schul-cloud-hpi/',
 	MAXIMUM_ALLOWABLE_TOTAL_ATTACHMENTS_SIZE_BYTE: (5 * 1024 * 1024), // 5MB
 	REQUEST_TIMEOUT: 8000,
