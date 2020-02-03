0065cbca01151b474d71c74c856ad69b22275591
adrianjost <22987140+adrianjost@users.noreply.github.com>
2020-02-03 08:42:04 +0100

    add error handling for results without protection

--- src/services/datasources/hooks/protectFields.js
+++ src/services/datasources/hooks/protectFields.js
@@ -2,6 +2,9 @@
  * replaces the values of protected fields with 'secret'
  */
 module.exports = async (context) => {
+	if (!context || !context.result || !Array.isArray(context.result.protected)) {
+		return context;
+	}
 	context.result.protected.forEach((key) => {
 		if (key !== 'target' && context.result.config[key]) {
 			context.result.config[key] = 'secret';
