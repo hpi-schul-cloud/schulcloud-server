c1f0b25dd5518da6023689334ec9a7bc325f09b0
Tobias Wollowski <tobias.wollowski@hpi.de>
2020-02-04 08:46:29 +0100

    ignore unused vars in migration

--- migrations/1578929548449-add bbb template to lti tools.js
+++ migrations/1578929548449-add bbb template to lti tools.js
@@ -1,5 +1,5 @@
+/* eslint-disable no-unused-vars */
 const mongoose = require('mongoose');
-// eslint-disable-next-line no-unused-vars
 const { info, error } = require('../src/logger');
 
 const { connect, close } = require('../src/utils/database');
