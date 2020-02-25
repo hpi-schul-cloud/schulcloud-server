df8a734d4dc5e14643d9f2f6e697e1e610a59515
CeEv <cedric@it-4d.de>
2020-02-25 11:17:42 +0100

    update migration to handle more cases.

--- migrations/1579006126081-addSchoolIdsForUsersInTeams.js
+++ migrations/1579006126081-addSchoolIdsForUsersInTeams.js
@@ -8,14 +8,16 @@ const { teamsModel: TeamsModel } = require('../src/services/teams/model'); // mo
 
 const equal = (id1, id2) => id1.toString() === id2.toString();
 
-const filterTeamWithMissingSchoolIds = (teams) => teams.filter((team) => team.userIds.some((user) => !user.schoolId));
+const filterTeamWithMissingSchoolIds = (teams = []) => teams.filter(
+	(team) => team.userIds.some((user) => !user.schoolId),
+);
 
-const selectTeamUserWithoutSchoolIds = (teams) => {
+const selectTeamUserWithoutSchoolIds = (teams = []) => {
 	const usersIds = [];
 	teams.forEach((team) => {
-		team.userIds.forEach(({ userId, schoolId }) => {
-			if (!schoolId) {
-				usersIds.push(userId);
+		team.userIds.forEach((teamUser) => {
+			if (teamUser && !teamUser.schoolId && teamUser.userId) {
+				usersIds.push(teamUser.userId);
 			}
 		});
 	});
@@ -23,13 +25,48 @@ const selectTeamUserWithoutSchoolIds = (teams) => {
 	return [...new Set(usersIds)];
 };
 
-const idsToOrCondition = (ids) => ids.map((_id) => ({ _id }));
+const idsToOrCondition = (ids = []) => ids.map((_id) => ({ _id }));
 
+const getReducer = (users = []) => (array, teamUser) => {
+	// skip and remove not existing teamUser
+	if (!teamUser || !typeof teamUser === 'object' || !teamUser.userId) {
+		return array;
+	}
+	// already exist
+	if (teamUser.schoolId) {
+		array.push(teamUser);
+		return array;
+	}
 
+	const baseUser = users.find(({ _id }) => equal(teamUser.userId, _id));
+	if (!baseUser || !baseUser.schoolId) {
+		return array;
+	}
+
+	teamUser.schoolId = baseUser.schoolId;
+
+	array.push(teamUser);
+	return array;
+};
+
+/**
+ * Fix userIds in teams that are:
+ * - miss schoolId
+ * - null
+ * - undefined
+ * - has only userId inside and not a teamUserObject
+ * It handle and fix also not existing users, that are linked in teams. this teamUsers are removed.
+ *
+ * It do NOT touched the schoolIds array that are related if users removed.
+ * Because it should not modified the team vs school permissions.
+ *
+ * It can also handle the case that all is fine. :P
+ */
 module.exports = {
 	up: async function up() {
 		await connect();
 		let teamsForFixing; let users;
+		let fixTeamUsersReducer;
 		// pre conditions and data selects
 		try {
 			// TeamsModel.find({ 'userIds.0.schoolId': { $exists: true } })
@@ -38,6 +75,7 @@ module.exports = {
 			const userIdsForFixing = selectTeamUserWithoutSchoolIds(teamsForFixing);
 			const $or = idsToOrCondition(userIdsForFixing);
 			users = await UserModel.find({ $or }).select('schoolId').lean().exec();
+			fixTeamUsersReducer = getReducer(users);
 		} catch (err) {
 			error('Pre select for data failed.', err);
 		}
@@ -45,12 +83,7 @@ module.exports = {
 		// pre fix team data
 		info(`Found ${teamsForFixing.length} teams for fixing.`, teamsForFixing.map((team) => team._id));
 		teamsForFixing.forEach((team) => {
-			team.userIds.forEach((user) => {
-				if (!user.schoolId) {
-					const baseUser = users.find(({ _id }) => equal(user.userId, _id));
-					user.schoolId = baseUser.schoolId;
-				}
-			});
+			team.userIds = team.userIds.reduce(fixTeamUsersReducer, []);
 		});
 
 		// create querys for fixes
