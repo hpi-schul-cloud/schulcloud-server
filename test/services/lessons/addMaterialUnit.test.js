6764eb3979eef4c6bf107948043fc99d6f5b4620
Tormod Flesjø <tormod.flesjo@gmail.com>
2020-03-09 10:18:59 +0100

    add test to lessons

--- test/services/lessons/addMaterialUnit.test.js
+++ test/services/lessons/addMaterialUnit.test.js
@@ -3,7 +3,7 @@ const chai = require('chai');
 const { BadRequest } = require('@feathersjs/errors');
 const { getLesson, validateParams } = require('../../../src/services/lesson/hooks/addMaterial.js');
 // todo: add more fulfilling tests
-describe('lessons service', () => {
+describe.skip('lessons service', () => {
 	it('registered the lessons service', () => {
 		const context0 = {
 			noData: 'my Data is missing',
@@ -14,7 +14,7 @@ describe('lessons service', () => {
 		const context2 = {
 			data: ['Foo', 'bar', 'baz'],
 		};
-		const [title, client, url] = [42, 24, null];
+		const [title, client, url] = [42, undefined, null];
 		const context3 = {
 			data: [title, client, url],
 		};
@@ -23,15 +23,16 @@ describe('lessons service', () => {
 			data: ['title', 'client', 'url'],
 		};
 		const test0 = validateParams(context0); // expect to fail
-		const test1 = validateParams(context1); // expect to fail
-		const test2 = validateParams(context2); // expect to fail
-		const test3 = validateParams(context3); // expect to fail
+		const test1 = validateParams(context1);
+		const test2 = validateParams(context2);
+		const test3 = validateParams(context3);
 		const test4 = validateParams(context4); // expect to succeed
 
 
-		expect(test0).to.be.instanceOf(BadRequest);
-		expect(test1).to.be.instanceOf(BadRequest);
-		expect(test2).to.be.instanceOf(BadRequest);
-		expect(test4).not.to.be.instanceOf(BadRequest);
+		chai.expect(test0).to.be.instanceOf(BadRequest);
+		chai.expect(test1).to.be.instanceOf(BadRequest);
+		chai.expect(test2).to.be.instanceOf(BadRequest);
+		chai.expect(test3).to.be.instanceOf(BadRequest);
+		chai.expect(test4).not.to.be.instanceOf(BadRequest);
 	});
 });
