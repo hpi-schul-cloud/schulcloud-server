59052044ce727ba517b5b30400d9806277114ee6
Thomas Feldtkeller <thomas.feldtkeller@hpi.de>
2020-03-16 13:06:13 +0100

    add buildAddUserMessage

---
+++ src/services/messengerSync/utils.js
@@ -0,0 +1,95 @@
+const { userModel, displayName } = require('../user/model');
+const { schoolModel } = require('../school/model');
+const roleModel = require('../role/model');
+
+const getUserData = (userId) => userModel.findOne(
+	{ _id: userId },
+	{
+		_id: 1, firstName: 1, lastName: 1, roles: 1, email: 1, schoolId: 1,
+	},
+).lean().exec();
+
+const getSchoolData = (schoolId) => schoolModel.findOne(
+	{ _id: schoolId },
+	{
+		_id: 1, name: 1,
+	},
+).lean().exec();
+
+let roles;
+
+const getRoles = async () => {
+	if (!roles) {
+		const [teacherRoleId, adminRoleId] = await Promise.all([
+			roleModel.findOne({ name: 'teacher' }, { _id: 1 }).lean().exec().then((r) => r._id),
+			roleModel.findOne({ name: 'administrator' }, { _id: 1 }).lean().exec().then((r) => r._id),
+		]);
+		roles = { teacherRoleId, adminRoleId };
+	}
+	return roles;
+};
+
+/* {
+	method: 'adduser',
+    school:{
+		id: 1223435,
+		has_allhands_channel : true,
+		name: "Peanuts High"
+    },
+    user: {
+        id: 1234566@matrix.schul-cloud.org,
+        name: "Joe Cool"",
+		is_school_admin: true,
+		is_school_teacher: true,
+    },
+    room: {
+		id: 1234566,
+		name: 'Mathe 6b',
+		type: 'course',
+		is_moderator: false,
+		bidirectional: true
+    }
+} */
+
+const buildAddUserMessage = async (data) => {
+	const { userId, team, course } = data;
+	const user = await getUserData(userId);
+	const school = await getSchoolData(user.schoolId);
+	const { teacherRoleId, adminRoleId } = await getRoles();
+	const rooms = [];
+	console.log(course.teacherIds);
+	console.log(course.teacherIds.includes(userId.toString()));
+	console.log(userId.toString());
+	if (course) {
+		rooms.push({
+			id: course._id.toString(),
+			name: course.name,
+			type: 'course',
+			bidirectional: (course.features || []).includes('messenger'),
+			is_moderator: course.teacherIds.some(
+				(el) => el.toString() === userId.toString() || el.toString() === userId.toString(),
+			),
+		});
+	}
+	const message = {
+		method: 'adduser',
+		school: {
+			id: school._id.toString(),
+			has_allhands_channel: true,
+			name: school.name,
+			email: user.email,
+		},
+		user: {
+			id: `${user._id.toString()}@matrix.schul-cloud.org`,
+			name: displayName(user),
+			is_school_admin: user.roles.some((el) => el.toString() === adminRoleId.toString()),
+			is_school_teacher: user.roles.some((el) => el.toString() === teacherRoleId.toString()),
+		},
+		rooms,
+	};
+	console.log(message);
+	return message;
+};
+
+
+module.exports = { buildAddUserMessage };
