fb27aa13689394b138137d8d1bb77213188856d7
BlackGhostHD <36112357+BlackGhostHD@users.noreply.github.com>
2020-03-20 09:53:48 +0100

    added sorting

--- src/services/alert/MessageProvider/status/index.js
+++ src/services/alert/MessageProvider/status/index.js
@@ -25,14 +25,14 @@ async function isInstance(instance, componentId) {
 		try {
 			const response = await api(apiUri).get(`/components/${componentId}`);
 			if (dict[instance] && response.data.group_id === dict[instance]) {
-				return true;
+				return response.data.group_id;
 			}
-			return false;
+			return -1;
 		} catch (error) {
-			return false;
+			return -1;
 		}
 	} else {
-		return true;
+		return 0;
 	}
 }
 
@@ -45,23 +45,42 @@ async function getRawData() {
 	return response;
 }
 
+function compare(a, b) {
+	const dateA = new Date(a.updated_at);
+	const dateB = new Date(b.updated_at);
+
+	if (a.status > b.status) return 1;
+	if (b.status > a.status) return -1;
+	if (dateA > dateB) return -1;
+	if (dateB > dateA) return 1;
+
+	return 0;
+}
+
 module.exports = {
 	async getData(instance) {
-		const data = [];
 		if (apiUri !== undefined) {
 			try {
 				const rawData = await getRawData();
+				const instanceSpecific = [];
+				const noneSpecific = [];
 				for (const element of rawData.data) {
 					// only mind incidents not older than 2 days
 					if (Date.parse(element.updated_at) + 1000 * 60 * 60 * 24 * 2 >= Date.now()) {
 						// only mind messages for own instance (including none instance specific messages)
 						const isinstance = await isInstance(instance, element.component_id);
-						if (isinstance) {
-							data.push(element);
+						if (isinstance !== 0 && isinstance !== -1) {
+							instanceSpecific.push(element);
+						} else if (isinstance !== -1) {
+							noneSpecific.push(element);
 						}
 					}
 				}
-				return data;
+				// do some sorting
+				instanceSpecific.sort(compare);
+				noneSpecific.sort(compare);
+
+				return instanceSpecific.concat(noneSpecific);
 			} catch (e) { // return null on error
 				return null;
 			}
