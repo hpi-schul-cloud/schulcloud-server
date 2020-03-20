959090562bf1d2f5ef990edb0c8f022198dd7273
Thomas Feldtkeller <thomas.feldtkeller@hpi.de>
2020-03-20 11:18:04 +0100

    add messengerTokenService

--- src/services/messengerSync/index.js
+++ src/services/messengerSync/index.js
@@ -1,10 +1,14 @@
 const eventListener = require('./eventListener');
 const consumer = require('./consumer');
 const { messengerSchoolSyncService, messengerSchoolSyncHooks } = require('./services/schoolSyncService');
+const { messengerTokenService, messengerTokenHooks } = require('./services/messengerTokenService');
 
 module.exports = (app) => {
 	app.use('schools/:schoolId/messengerSync', messengerSchoolSyncService);
 	app.service('schools/:schoolId/messengerSync').hooks(messengerSchoolSyncHooks);
+
+	app.use('messengerToken', messengerTokenService);
+	app.service('messengerToken').hooks(messengerTokenHooks);
 
 	app.configure(eventListener);
 	app.configure(consumer);
