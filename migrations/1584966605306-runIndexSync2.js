01703b31e8013b33ca293be0be8950e50b59e0f0
Jan Renz <jan.renz@gmail.com>
2020-03-23 13:50:23 +0100

    Prepared migration for ongoing homework performance fix

---
+++ migrations/1584966605306-runIndexSync2.js
@@ -0,0 +1,22 @@
+require('mongoose');
+// eslint-disable-next-line no-unused-vars
+const { info, error } = require('../src/logger');
+
+const { connect, close } = require('../src/utils/database');
+
+// ? How to access permissions?
+require('../src/services/homework/model');
+
+module.exports = {
+	up: async function up() {
+		await connect();
+		// ////////////////////////////////////////////////////
+		// Make changes to the database here.
+		// Hint: Access models via this('modelName'), not an imported model to have
+		// access to the correct database connection. Otherwise Mongoose calls never return.
+		this('homework').syncIndexes();
+		this('submission').syncIndexes();
+		// ////////////////////////////////////////////////////
+		await close();
+	},
+};
