a413683568bcf068c5e15f53c303e78e5b8d280e
Tobias Wollowski <tobias.wollowski@hpi.de>
2020-03-26 16:08:13 +0100

    added info

--- config/globals.js
+++ config/globals.js
@@ -23,7 +23,7 @@ const globals = {
 	 * it's used in different filenames
 	 */
 	NODE_ENV: process.env.NODE_ENV || 'default',
-	HOST: process.env.HOST || 'localhost:3030',
+	HOST: process.env.HOST || 'localhost:3030', // todo this is client url in config
 	TOKEN_SUB: process.env.TOKEN_SUB
 	|| process.env.HOST || 'https://schulcloud-thueringen.de', // added TOKEN_SUB on env
 	TOKEN_ISS: process.env.TOKEN_ISS || process.env.SC_DOMAIN || 'schulcloud-thueringen.de', // added TOKEN_ISS on env
