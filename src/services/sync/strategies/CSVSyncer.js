ecb5c5662c14c257ce3609296405d33a2de67113
Tormod Flesjø <tormod.flesjo@gmail.com>
2020-03-26 12:15:37 +0100

    eslint fix

--- src/services/sync/strategies/CSVSyncer.js
+++ src/services/sync/strategies/CSVSyncer.js
@@ -403,11 +403,11 @@ class CSVSyncer extends mix(Syncer).with(ClassImporter) {
 		if (!dateInput) {
 			// todo: change to logger
 			bdayErrors.push('missing birthday value');
-			return bdayErrors
+			return bdayErrors;
 		}
 		if (typeof dateInput !== 'string') {
 			bdayErrors.push('incorrect values, birthday must be a string');
-			return bdayErrors
+			return bdayErrors;
 		}
 		// https://stackoverflow.com/questions/15491894/regex-to-validate-date-format-dd-mm-yyyy
 		const dateValidationRegex = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
