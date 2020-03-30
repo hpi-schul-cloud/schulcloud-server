1d1c550347f8b930b4433e80a0737161c19e774c
Maximilian Grundke <maximiliangrundke@googlemail.com>
2020-03-30 14:39:41 +0200

    Migrate existing files to include creator if possible

---
+++ migrations/1585222780409-addFileCreatorAndPermissionIndexes.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const { info } = require('../src/logger');
+const { connect, close } = require('../src/utils/database');
+const { FileModel } = require('../src/services/fileStorage/model');
+
+module.exports = {
+	up: async function up() {
+		await connect();
+		// ////////////////////////////////////////////////////
+		info('Updating files...');
+		const allFiles = await FileModel.estimatedDocumentCount();
+		const filesToUpdate = await FileModel.countDocuments({
+			'permissions.0.refPermModel': 'user',
+			'permissions.0.refId': { $exists: true },
+		}).exec();
+		info(`${allFiles} files exist. ${filesToUpdate} files have creator information.`);
+
+		info('Adding "creator" attribute. This may take a while...');
+		// We need to use the raw mongodb connection, because it allows us to let the DB handle
+		// the attribute creation within an aggregate stage, whereas in Mongoose we would need to
+		// do this in our code.
+		await mongoose.connection.collection('files').updateMany({
+			'permissions.0.refPermModel': 'user',
+			'permissions.0.refId': { $exists: true },
+		}, [
+			{ $set: { creator: { $arrayElemAt: ['$permissions.refId', 0] } } },
+		]);
+		info('Done.');
+
+		info('Updating file indexes...');
+		await FileModel.syncIndexes();
+		info('Done.');
+		// ////////////////////////////////////////////////////
+		await close();
+	},
+
+	down: async function down() {
+		await connect();
+		// ////////////////////////////////////////////////////
+		info('Removing the creator field...');
+		await FileModel.updateMany({ $unset: { creator: true } }).exec();
+		info('Done.');
+
+		info('Updating file indexes...');
+		await FileModel.syncIndexes();
+		info('Done.');
+		// ////////////////////////////////////////////////////
+		await close();
+	},
+};
