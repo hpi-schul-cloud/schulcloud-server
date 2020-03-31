3e52b5d62ca9e48bd3591ccf45cf6a8684c18b5c
Maximilian Grundke <maximiliangrundke@googlemail.com>
2020-03-31 08:31:40 +0200

    Also update inconsistent user files

--- migrations/1585222780409-addFileCreatorAndPermissionIndexes.js
+++ migrations/1585222780409-addFileCreatorAndPermissionIndexes.js
@@ -8,13 +8,22 @@ module.exports = {
 		// ////////////////////////////////////////////////////
 		info('Updating files...');
 		const allFiles = await FileModel.estimatedDocumentCount();
-		const filesToUpdate = await FileModel.countDocuments({
-			'permissions.0.refPermModel': 'user',
-			'permissions.0.refId': { $exists: true },
-		}).exec();
+		const [filesWithOwnerPermissions, userOwnedFilesWithInvalidPermissions] = await Promise.all([
+			FileModel.countDocuments({
+				'permissions.0.refPermModel': 'user',
+				'permissions.0.refId': { $exists: true },
+			}).exec(),
+			FileModel.countDocuments({
+				refOwnerModel: 'user',
+				'permissions.0.refPermModel': { $ne: 'user' },
+			}).exec(),
+		]);
+		const filesToUpdate = filesWithOwnerPermissions + userOwnedFilesWithInvalidPermissions;
 		info(`${allFiles} files exist. ${filesToUpdate} files have creator information.`);
 
 		info('Adding "creator" attribute. This may take a while...');
+		// This needs to be done before the additional indexes are created,
+		// because we expect a lot of updates
 		await FileModel.updateMany({
 			'permissions.0.refPermModel': 'user',
 			'permissions.0.refId': { $exists: true },
@@ -24,7 +33,19 @@ module.exports = {
 		info('Done.');
 
 		info('Updating file indexes...');
+		// Creates an indexes for creator and permissions.refId/refPermModel
 		await FileModel.syncIndexes();
+		info('Done.');
+
+		info('Adding "creator" attribute for inconsistent user-owned files...');
+		// This will be faster here, because there's an index on creator and we
+		// don't expect many files to be updated
+		await FileModel.updateMany({
+			refOwnerModel: 'user',
+			creator: { $exists: false },
+		}, [
+			{ $set: { creator: '$owner' } },
+		]);
 		info('Done.');
 		// ////////////////////////////////////////////////////
 		await close();
