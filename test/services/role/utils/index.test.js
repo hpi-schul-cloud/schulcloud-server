0f48f452e779b2016859bda9a86096754f4d0d82
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2020-04-02 20:18:54 +0200

    Add role util test.

--- test/services/role/utils/index.test.js
+++ test/services/role/utils/index.test.js
@@ -10,7 +10,81 @@ const {
 
 describe('Role Service utils', async () => {
 	describe('preparedRoles', () => {
-		// TODO add tests
+		const role0 = {
+			_id: '0',
+			name: 'user',
+			roles: [],
+			permissions: ['a', 'b', 'c', 'd', 'e'],
+		};
+		const role1 = {
+			_id: '1',
+			name: 'student',
+			roles: ['0'],
+			permissions: ['a', 'student'],
+		};
+		const role2 = {
+			_id: '2',
+			name: 'teacher',
+			roles: ['0'],
+			permissions: ['b', 'teacher1', 'teacher2'],
+		};
+		const role3 = {
+			_id: '3',
+			name: 'administrator',
+			roles: ['2'],
+			permissions: ['c', 'administrator'],
+		};
+		const role4 = {
+			_id: '4',
+			name: 'superhero',
+			roles: ['0', '1', '3'],
+			permissions: ['d', 'superhero'],
+		};
+		// change the sorting will effect the test result
+		const roles = [role0, role1, role2, role3, role4];
+
+		it('should prepare inherit and multiple roles in different levels and add display names if exist.', () => {
+			const result = preparedRoles(roles);
+			expect(result).to.deep.equal([
+				{
+					_id: '0',
+					name: 'user',
+					roles: [],
+					displayName: '',
+					permissions: ['a', 'b', 'c', 'd', 'e'],
+				},
+				{
+					_id: '1',
+					name: 'student',
+					roles: ['0'],
+					displayName: 'Schüler',
+					permissions: ['a', 'student', 'b', 'c', 'd', 'e'],
+				},
+				{
+					_id: '2',
+					name: 'teacher',
+					roles: ['0'],
+					displayName: 'Lehrer',
+					permissions: ['b', 'teacher1', 'teacher2', 'a', 'c', 'd', 'e'],
+				},
+				{
+					_id: '3',
+					name: 'administrator',
+					roles: ['2'],
+					displayName: 'Administrator',
+					permissions: ['c', 'administrator', 'b', 'teacher1', 'teacher2', 'a', 'd', 'e'],
+				},
+				{
+					_id: '4',
+					name: 'superhero',
+					roles: ['0', '1', '3'],
+					displayName: 'Schul-Cloud Admin',
+					permissions: [
+						'd', 'superhero', 'a', 'b', 'c', 'e', 'student', 'administrator', 'teacher1', 'teacher2'
+					],
+				},
+			]);
+		});
 	});
 
 	describe('unique', () => {
