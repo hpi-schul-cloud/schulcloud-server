3213f793c979febce2a556921d9d5b2ba755e7b8
Max Klenk <max@klenk.biz>
2020-04-15 16:08:50 +0200

    use rabbitmqMock in all messengerSync files

--- test/services/messengerSync/rabbitmqMock.js
+++ test/services/messengerSync/rabbitmqMock.js
@@ -6,12 +6,16 @@ const reset = () => {
 
 const assertQueue = async () => {};
 
+const prefetch = async () => {};
+
+const consume = async () => {};
+
 const sendToQueue = (queue, messageBuffer, options) => {
 	if (!queues[queue]) queues[queue] = [];
 	queues[queue].push(messageBuffer);
 };
 
-const createChannel = async () => ({ sendToQueue, assertQueue });
+const createChannel = async () => ({ sendToQueue, assertQueue, prefetch, consume });
 
 const setup = async (app) => {};
 
