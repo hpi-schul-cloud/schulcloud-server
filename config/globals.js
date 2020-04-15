41005e10cd348a2eaada2ac803aa1e1cf030c55a
Tobias Wollowski <tobias.wollowski@hpi.de>
2020-04-15 14:26:13 +0200

    force database audit to be enabled

--- config/globals.js
+++ config/globals.js
@@ -1,6 +1,6 @@
 module.exports = {
 	BODYPARSER_JSON_LIMIT: process.env.BODYPARSER_JSON_LIMIT || '20mb',
-	DATABASE_AUDIT: process.env.DATABASE_AUDIT || 'false',
+	DATABASE_AUDIT: 'true',
 	DOCUMENT_BASE_DIR: process.env.DOCUMENT_BASE_DIR || 'https://s3.hidrive.strato.com/schul-cloud-hpi/',
 	MAXIMUM_ALLOWABLE_TOTAL_ATTACHMENTS_SIZE_BYTE: (5 * 1024 * 1024), // 5MB
 	REQUEST_TIMEOUT: 8000,
