9f3461bf8c7fdef99b4761bbe2afdb161c1c0791
Max Klenk <max@klenk.biz>
2020-04-20 09:22:52 +0200

    fix lint

--- src/services/user-group/logic/courses.js
+++ src/services/user-group/logic/courses.js
@@ -1,8 +1,8 @@
-
+/* eslint-disable-next-line arrow-body-style */
 const getAllCourseUserIds = ({ userIds = [], teacherIds = [], substitutionIds = [] }) => {
 	return userIds.concat(teacherIds).concat(substitutionIds);
 };
 
 module.exports = {
-	getAllCourseUserIds: getAllCourseUserIds
+	getAllCourseUserIds,
 };
