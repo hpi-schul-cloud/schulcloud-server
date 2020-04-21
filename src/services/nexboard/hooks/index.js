1b3d8511b9907015ffff53f5707fb46802a7e6c0
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2020-04-21 14:37:56 +0200

    Add base permissions for nexboard service

--- src/services/nexboard/hooks/index.js
+++ src/services/nexboard/hooks/index.js
@@ -1,13 +1,16 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
+const { disallow } = require('feathers-hooks-common');
+
+const { hasPermission } = require('../../../hooks');
 
 const before = {
 	all: [authenticate('jwt')],
-	find: [],
-	get: [],
-	create: [],
-	update: [],
-	patch: [],
-	remove: [],
+	find: [hasPermission('TOOL_VIEW')],
+	get: [hasPermission('TOOL_VIEW')],
+	create: [hasPermission('TOOL_CREATE')],
+	update: [disallow()],
+	patch: [hasPermission('TOOL_EDIT')],
+	remove: [hasPermission('TOOL_EDIT')],
 };
 
 const after = {
