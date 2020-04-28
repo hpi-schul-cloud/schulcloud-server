865be5405934b5dabeb6977350d847ab2e4c90f0
Maximilian Grundke <maximiliangrundke@googlemail.com>
2020-04-28 11:57:52 +0200

    Fix disconnect method

--- src/services/ldap/index.js
+++ src/services/ldap/index.js
@@ -108,18 +108,12 @@ module.exports = function () {
          * @return {Promise} resolves if successfully disconnected, otherwise
          * rejects with error
          */
-		_disconnect(config) {
-			return new Promise((resolve, reject) => {
-				if (!(config && config._id)) {
-					reject('Invalid config object');
-				}
-				this._getClient(config).unbind((err) => {
-					if (err) {
-						reject(err);
-					}
-					resolve();
-				});
-			});
+		disconnect(config) {
+			return this._getClient(config)
+				.then((client) => client.unbind((err) => {
+					if (err) return Promise.reject(err);
+					return Promise.resolve();
+				}));
 		}
 
 		/**
