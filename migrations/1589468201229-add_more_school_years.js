cc70d9d57a63c3e2fd9eee6557d50f86831ed681
Michael Wolowyk <michael.wolowyk@capgemini.com>
2020-05-15 10:55:09 +0200

    SC-4352 migration add test year model

--- migrations/1589468201229-add_more_school_years.js
+++ migrations/1589468201229-add_more_school_years.js
@@ -1,12 +1,20 @@
+const mongoose = require('mongoose');
 const { info, warning } = require('../src/logger');
 
 const { connect, close } = require('../src/utils/database');
-const { yearModel: Year } = require('../src/services/school/model');
 const yearLogic = require('../src/services/school/logic/year');
 
 const YEARS_TO_ADD = ['2021/22', '2022/23', '2023/24', '2024/25', '2025/26'];
 
-const createYear = (year) => new Year({
+const yearSchema = new mongoose.Schema({
+	name: { type: String, required: true },
+	startDate: { type: Date, required: true },
+	endDate: { type: Date, required: true },
+});
+
+const YearModel = mongoose.model('year3244382362348795', yearSchema, 'years');
+
+const createYear = (year) => new YearModel({
 	name: year,
 	startDate: yearLogic.getDefaultStartDate(year),
 	endDate: yearLogic.getDefaultEndDate(year),
@@ -16,7 +24,7 @@ module.exports = {
 	up: async function up() {
 		await connect();
 		const chain = [];
-		const existingYears = await Year.find().where('name').in(YEARS_TO_ADD).exec();
+		const existingYears = await YearModel.find().where('name').in(YEARS_TO_ADD).exec();
 		if (existingYears.length > 0) {
 			warning(`some year already has been created: ${existingYears}`);
 		} else {
@@ -36,7 +44,7 @@ module.exports = {
 
 	down: async function down() {
 		await connect();
-		await Year.deleteMany({
+		await YearModel.deleteMany({
 			name: {
 				$in: YEARS_TO_ADD,
 			},
