56140175f7a3a7fd260dc5d8f38f77c7359dab61
Gratza <rg@ciphron.de>
2020-05-18 13:06:55 +0200

    fix SC-3690 etherpad access

---
+++ src/services/etherpad/hooks/Pad.js
@@ -0,0 +1,60 @@
+const { authenticate } = require('@feathersjs/authentication').hooks;
+const { disallow } = require('feathers-hooks-common');
+const { Forbidden } = require('@feathersjs/errors');
+
+const logger = require('../../../logger');
+const globalHooks = require('../../../hooks');
+
+const getGroupData = async (context) => {
+	context.data = {
+		...context.data,
+		userId: context.params.account.userId,
+	};
+	const groupService = context.app.service('etherpad/groups').create(context.data);
+	try {
+		const response = JSON.parse(await groupService);
+		context.data = {
+			...context.data,
+			groupID: response.data.groupID,
+		};
+		return context;
+	} catch (err) {
+		logger.error('Failed to get course data: ', err);
+		throw new Forbidden('Failed to get course data');
+	}
+};
+
+const injectCourseId = async (context) => {
+	context.id = context.data.courseId;
+	return context;
+};
+
+const before = {
+	all: [authenticate('jwt')],
+	find: [disallow()],
+	get: [disallow()],
+	create: [
+		globalHooks.hasPermission('TOOL_CREATE'),
+		injectCourseId,
+		globalHooks.restrictToUsersOwnCourses,
+		getGroupData,
+	],
+	update: [disallow()],
+	patch: [disallow()],
+	remove: [disallow()],
+};
+
+const after = {
+	all: [],
+	find: [],
+	get: [],
+	create: [],
+	update: [],
+	patch: [],
+	remove: [],
+};
+
+module.exports = {
+	before,
+	after,
+};
