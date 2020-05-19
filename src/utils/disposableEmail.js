06c167478f407c8c46f3e640994cfcfcc57e7520
Max Klenk <max@klenk.biz>
2020-05-19 17:07:52 +0200

    block additional domains with custom wildcard

--- src/utils/disposableEmail.js
+++ src/utils/disposableEmail.js
@@ -1,6 +1,11 @@
 const domains = require('disposable-email-domains');
 const wildcards = require('disposable-email-domains/wildcard.json');
 
+const customWildcards = [
+	'grugrug.ru',
+	'yeezus.ru',
+];
+
 module.exports = {
 	isDisposableEmail: (email) => {
 		if (!email) {
@@ -20,6 +25,13 @@ module.exports = {
 
 		// check wildcards to include subdomains
 		for (const wildcard of wildcards) {
+			if (domain.endsWith(wildcard)) {
+				return true;
+			}
+		}
+
+		// check custom wildcards
+		for (const wildcard of customWildcards) {
 			if (domain.endsWith(wildcard)) {
 				return true;
 			}
