adbdea492e1381ccbf9df46e7e510ca509672c80
Philipp Kleybolte <philipp.kleybolte@capgemini.com>
2020-05-26 17:44:04 +0200

    SC-4058 Encrypt ldap search user password

--- migrations/1590501606910-encrypt-ldap-search-user-password.js
+++ migrations/1590501606910-encrypt-ldap-search-user-password.js
@@ -6,11 +6,7 @@ const { connect, close } = require('../src/utils/database');
 
 // eslint-disable-next-line no-unused-vars
 const { info, error } = require('../src/logger');
-const { systemModel } = require('../src/services/system/model');
-
-// How to use more than one schema per collection on mongodb
-// https://stackoverflow.com/questions/14453864/use-more-than-one-schema-per-collection-on-mongodb
-
+const systemModel = require('../src/services/system/model');
 
 // TODO npm run migration-persist and remove this line
 // TODO update seed data and remove this line
@@ -22,34 +18,40 @@ module.exports = {
 		}
 		await connect();
 
-		const systems = systemModel.find();
+		const systems = await systemModel.find({});
 
-		Promise.all(systems.map((system) => {
+		info('Start encrypting searchUserPasswords of "systems"');
+		await Promise.all(systems.map((system) => {
+			if (system.type !== 'ldap') return Promise.resolve();
+
 			const encryptedPW = CryptoJS.AES
 				.encrypt(system.ldapConfig.searchUserPassword, Configuration.get('LDAP_PASSWORD_ENCRYPTION_KEY'))
 				.toString();
 			const newLdapConfig = { ...system.ldapConfig };
 			newLdapConfig.searchUserPassword = encryptedPW;
-
-			return systemModel.update({
+			info(`Encrypting searchUserPassword of system { id: ${system.id}), alias: ${system.alias} }`);
+			return systemModel.updateOne({
 				_id: system._id,
 			}, {
 				ldapConfig: newLdapConfig,
 			});
 		}));
+		info('Finished encrypting searchUserPasswords of "systems"');
 
 		await close();
 	},
 
 	down: async function down() {
 		if (!Configuration.has('LDAP_PASSWORD_ENCRYPTION_KEY')) {
-			throw new Error('You need to set LDAP_PASSWORD_ENCRYPTION_KEY to encrypt the old key!');
+			throw new Error('You need to set LDAP_PASSWORD_ENCRYPTION_KEY to decrypt the key!');
 		}
 		await connect();
 
-		const systems = systemModel.find();
+		const systems = await systemModel.find();
 
-		Promise.all(systems.map((system) => {
+		info('Start decrypting searchUserPasswords of "systems"');
+		await Promise.all(systems.map((system) => {
+			if (system.type !== 'ldap') return Promise.resolve();
 			const decryptedPW = CryptoJS.AES
 				.decrypt(system.ldapConfig.searchUserPassword, Configuration.get('LDAP_PASSWORD_ENCRYPTION_KEY'))
 				.toString();
@@ -62,6 +64,7 @@ module.exports = {
 				ldapConfig: newLdapConfig,
 			});
 		}));
+		info('Finished decrypting searchUserPasswords of "systems"');
 
 		await close();
 	},
--- src/services/system/hooks/searchUserPasswordEncryption.js
+++ src/services/system/hooks/searchUserPasswordEncryption.js
@@ -2,7 +2,7 @@ const CryptoJS = require('crypto-js');
 const { Configuration } = require('@schul-cloud/commons');
 
 const encryptSecret = (context) => {
-	if (context.data.ldapConfig.searchUserPassword) {
+	if (context.data.ldapConfig && context.data.ldapConfig.searchUserPassword) {
 		context.data.ldapConfig.searchUserPassword = CryptoJS.AES
 			.encrypt(context.data.ldapConfig.searchUserPassword, Configuration.get('LDAP_PASSWORD_ENCRYPTION_KEY'))
 			.toString();
@@ -12,18 +12,17 @@ const encryptSecret = (context) => {
 
 const decryptSecret = (context) => {
 	const ldapPasswordKey = Configuration.get('LDAP_PASSWORD_ENCRYPTION_KEY');
-	if (ldapPasswordKey) {
-		if (Array.isArray(context.result)) {
-			context.result.forEach((element) => {
-				element.ldapConfig.searchUserPassword = CryptoJS.AES
-					.decrypt(element.ldapConfig.searchUserPassword, ldapPasswordKey)
-					.toString(CryptoJS.enc.Utf8);
-			});
-		} else {
-			context.result.ldapConfig.searchUserPassword = CryptoJS.AES
-				.decrypt(context.result.ldapConfig.searchUserPassword, ldapPasswordKey)
+	if (Array.isArray(context.result)) {
+		context.result.forEach((element) => {
+			if (!element.ldapConfig) return;
+			element.ldapConfig.searchUserPassword = CryptoJS.AES
+				.decrypt(element.ldapConfig.searchUserPassword, ldapPasswordKey)
 				.toString(CryptoJS.enc.Utf8);
-		}
+		});
+	} else if (context.result.ldapConfig && context.result.ldapConfig.searchUserPassword) {
+		context.result.ldapConfig.searchUserPassword = CryptoJS.AES
+			.decrypt(context.result.ldapConfig.searchUserPassword, ldapPasswordKey)
+			.toString(CryptoJS.enc.Utf8);
 	}
 	return context;
 };
