12a381ef7aabe9616a4b03a5646d7d024680cbee
j-jmnz <jmnz.jesus@gmail.com>
2020-06-03 12:35:59 +0200

    remove model calls, add exception handling

--- src/services/school/services/permissions.js
+++ src/services/school/services/permissions.js
@@ -1,6 +1,5 @@
 const { authenticate } = require('@feathersjs/authentication');
 const globalHooks = require('../../../hooks');
-const { schoolModel: School } = require('../model');
 const { lookupSchool } = require('../../../hooks');
 
 
@@ -25,13 +24,14 @@ const hooks = {
 	},
 };
 
-const getSchool = async (schoolId) => {
-	const school = await School
-		.findById(schoolId)
-		.select(['permissions'])
-		.exec();
-	return school;
-};
+const getSchool = (ctx, schoolId) => ctx.service('schools').get(schoolId);
+
+const updateSchoolPermissions = (ctx, schoolId, permissions) => ctx
+	.service('schools')
+	.patch(
+		schoolId,
+		{ permissions },
+	);
 
 class HandlePermissions {
 	constructor(role, permission) {
@@ -40,28 +40,34 @@ class HandlePermissions {
 	}
 
 	async find(params) {
-		const school = await getSchool(params.account.schoolId);
+		const school = await getSchool(this.app, params.account.schoolId);
 		return school.permissions[this.role] || {};
 	}
 
 	async patch(id, data, params) {
 		const { permissions } = data;
-		const school = await getSchool(params.account.schoolId);
+		const { schoolId } = params.account;
+		const school = await getSchool(this.app, schoolId);
 
 		if (school) {
 			const schoolPermissions = school.permissions || {};
 
 			switch (this.permission) {
 				case 'studentVisibility':
-					if (Object.prototype.hasOwnProperty.call(schoolPermissions[this.role], 'STUDENT_LIST')) {
-						schoolPermissions[this.role].STUDENT_LIST = permissions.studentVisibility;
-						await school.updateOne({ permissions: schoolPermissions });
+					if (!Object.prototype.hasOwnProperty.call(schoolPermissions, this.role)) {
+						schoolPermissions[this.role] = {};
 					}
+					schoolPermissions[this.role].STUDENT_LIST = permissions.studentVisibility;
+					await updateSchoolPermissions(this.app, schoolId, schoolPermissions);
 					break;
 				default:
 					break;
 			}
 		}
+	}
+
+	setup(app) {
+		this.app = app;
 	}
 }
 
