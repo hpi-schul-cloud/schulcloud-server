42f1cb8ba5e12b6e09349f2ddff9fc59dc52bb26
Michael Wolowyk <michael.wolowyk@capgemini.com>
2020-06-05 15:47:52 +0200

    SC-4577: allow create route only for school admins

--- src/services/consent/hooks/consentversions.js
+++ src/services/consent/hooks/consentversions.js
@@ -1,12 +1,23 @@
 const { authenticate } = require('@feathersjs/authentication');
 const hooks = require('feathers-hooks-common');
 
+const { iff, isProvider } = require('feathers-hooks-common');
+const globalHooks = require('../../../hooks');
+
+const {
+	restrictToCurrentSchool,
+} = require('../../../hooks');
+
 // todo check userId
 exports.before = {
-	all: [authenticate('jwt')],
+	all: [iff(isProvider('external'), [
+		authenticate('jwt'),
+		globalHooks.populateCurrentSchool,
+		restrictToCurrentSchool,
+	])],
 	find: [],
 	get: [],
-	create: [hooks.disallow()],
+	create: [iff(isProvider('external'), globalHooks.hasPermission('SCHOOL_EDIT'))],
 	update: [hooks.disallow()],
 	patch: [hooks.disallow()],
 	remove: [hooks.disallow()],
