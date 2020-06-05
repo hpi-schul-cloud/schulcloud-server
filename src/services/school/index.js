1c756e4cb748e5ae2295794e4bfe3d18ceb80e3c
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2020-06-05 14:49:37 +0200

    chane the original url

--- src/services/school/index.js
+++ src/services/school/index.js
@@ -65,7 +65,7 @@ module.exports = function schoolServices() {
 		.get('FEATURE_ADMIN_TOGGLE_STUDENT_VISIBILITY');
 
 	if (FEATURE_ADMIN_TOGGLE_STUDENT_VISIBILITY !== 'disabled') {
-		app.use('/school/teacher/permissions', new HandlePermissions('teacher', 'STUDENT_LIST'));
+		app.use('/school/teacher/studentvisibility', new HandlePermissions('teacher', 'STUDENT_LIST'));
 		const handlePermissionsService = app.service('/school/teacher/studentvisibility');
 		handlePermissionsService.hooks(handlePermissionsHooks);
 	}
