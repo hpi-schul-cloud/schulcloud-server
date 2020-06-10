254126c8058e941490321cf460393fdfe1dc7067
Robert Strzepka <robert.strzepka@partner.bmwgroup.com>
2020-06-10 16:30:00 +0200

    Disable no-param-reassign in order to filter error parameter

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -132,6 +132,7 @@ const filterSecrets = (error, req, res, next) => {
 		// originalUrl is used by sentry
 		req.originalUrl = filterQuery(req.originalUrl);
 		req.body = filter(req.body);
+		// eslint-disable-next-line no-param-reassign
 		error = filter(error);
 	}
 	next(error);
