ed9b66157eba7cf733ad7eabd43f716e92939a96
Thomas Feldtkeller <thomas.feldtkeller@hpi.de>
2020-06-12 12:27:12 +0200

    remove unneeded variable

--- test/services/link/index.test.js
+++ test/services/link/index.test.js
@@ -2,7 +2,6 @@ const assert = require('assert');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../../../src/app');
-const { API_HOST } = require('../../../config/globals');
 
 chai.use(chaiHttp);
 
