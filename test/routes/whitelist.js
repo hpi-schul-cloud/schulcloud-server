c7166a5b67a32a857d310989640214ecd91a9724
Gratza <rg@ciphron.de>
2020-06-26 18:18:00 +0200

    move whitelist entries to their right place (ignore)

--- test/routes/whitelist.js
+++ test/routes/whitelist.js
@@ -1,18 +1,13 @@
 const whitelistNoJwt = {
 	'accounts/confirm':        { post: 201 },
 	'alert':                   { get:  200 },
-	'consents':                { post: 400 },
 	'consentVersions':         { get:  200 },
 	'gradeLevels':             { get:  200 },
-	'hash':                    { post: 400 },
 	'lessons/contents/{type}': { get:  200 },
-	'link':                    { get:  500 },
 	'passwordRecovery':        { post: 201 },
 	'passwordRecovery/reset':  { post: 201 },
 	'oauth2/baseUrl':          { get:  200 },
-	'registration':            { post: 400 },
 	'registrationlink':        { post: 201 },
-	'registrationPins':        { post: 400 },
 	'roster':                  { get:  200 },
 	'schools':                 { get:  200 },
 	'tools/link':              { post: 404 },
@@ -35,6 +30,11 @@ const ignorelistNoJwt = {
 	'wopi/files/{fileId}':          ['get', 'post'],
 	'roster/users/{user}/metadata': ['get'],
 	'roster/users/{user}/groups':   ['get'],
+	'link':                         ['get'],
+	'consents':                     ['post'],
+	'hash':                         ['post'],
+	'registration':                 ['post'],
+	'registrationPins':             ['post'],
 };
 
 const ignorelistInvalidJwt = {
