eab1e1e1208aba1334d9d7d9c398d31ee66f0689
Michael Wolowyk <michael.wolowyk@capgemini.com>
2020-07-09 18:05:04 +0200

    SC-5345 we don't need to filter the results anymore since a guest user was created which can access only public data

--- src/services/edusharing/logic/EduSearchResponse.js
+++ src/services/edusharing/logic/EduSearchResponse.js
@@ -1,5 +1,3 @@
-const { Configuration } = require('@schul-cloud/commons');
-
 class EduSearchResponse {
 	constructor(parsed) {
 		this.total = 0;
@@ -8,17 +6,10 @@ class EduSearchResponse {
 		this.data = [];
 
 		if (parsed) {
-			// filter out the resources without the external url
-			const filteredNodes = Configuration.get('FEATURE_ES_SHOW_ONLY_EXTERNAL_RESULTS')
-				? parsed.nodes.filter((node) => {
-					const location = node.properties['ccm:wwwurl'];
-					return location && location.length > 0;
-				}) : parsed.nodes;
-			const totalDif = parsed.nodes.length - filteredNodes.length;
-			this.total = parsed.pagination.total - totalDif;
+			this.total = parsed.pagination.total;
 			this.limit = parsed.pagination.count;
 			this.skip = parsed.pagination.from;
-			this.data = filteredNodes;
+			this.data = parsed.nodes;
 		}
 	}
 }
