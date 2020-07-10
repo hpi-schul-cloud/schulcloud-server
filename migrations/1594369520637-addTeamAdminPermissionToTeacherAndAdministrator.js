b662d0e42cb47646decc9d2b9959e0a217ccf926
Kamil Nowak <kamil.nowak@capgemini.com>
2020-07-10 13:55:43 +0200

    [SC-4536] Added migration script

---
+++ migrations/1594369520637-addTeamAdminPermissionToTeacherAndAdministrator.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+// eslint-disable-next-line no-unused-vars
+const { info, error } = require('../src/logger');
+
+const { connect, close } = require('../src/utils/database');
+
+const RoleModel = mongoose.model('role120987652346', new mongoose.Schema({
+	name: { type: String, required: true },
+	permissions: [{ type: String }],
+}, {
+	timestamps: true,
+}), 'roles');
+
+const permissions = ['CAN_BE_TEAM_ADMIN'];
+
+// How to use more than one schema per collection on mongodb
+// https://stackoverflow.com/questions/14453864/use-more-than-one-schema-per-collection-on-mongodb
+
+module.exports = {
+	up: async function up() {
+		await connect();
+		// ////////////////////////////////////////////////////
+		// Make changes to the database here.
+		// Hint: Access models via this('modelName'), not an imported model to have
+		// access to the correct database connection. Otherwise Mongoose calls never return.
+
+		await RoleModel.findOneAndUpdate(
+			{
+				name: 'administrator',
+			}, {
+				$addToSet: { permissions: { $each: permissions } },
+			},
+		).lean().exec();
+
+		await RoleModel.findOneAndUpdate(
+			{
+				name: 'teacher',
+			}, {
+				$addToSet: { permissions: { $each: permissions } },
+			},
+		).lean().exec();
+		// ////////////////////////////////////////////////////
+		await close();
+	},
+
+	down: async function down() {
+		await connect();
+		// ////////////////////////////////////////////////////
+		// Implement the necessary steps to roll back the migration here.
+
+		await RoleModel.findOneAndUpdate(
+			{
+				name: 'administrator',
+			}, {
+				$pull: { permissions: { $in: permissions } },
+			},
+		).lean().exec();
+
+		await RoleModel.findOneAndUpdate(
+			{
+				name: 'teacher',
+			}, {
+				$pull: { permissions: { $in: permissions } },
+			},
+		).lean().exec();
+		// ////////////////////////////////////////////////////
+		await close();
+	},
+};
