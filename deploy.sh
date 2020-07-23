deb0087debe90d924eb613132cbf4db5aabe8701
tobigremmer-cg <56836518+tobigremmer-cg@users.noreply.github.com>
2020-07-23 08:39:55 +0200

    include team 6 and 7

--- deploy.sh
+++ deploy.sh
@@ -135,7 +135,7 @@ then
 elif [[ $TRAVIS_BRANCH = hotfix* ]]
 then
 	TEAM="$(cut -d'/' -f2 <<< $TRAVIS_BRANCH)"
-	if [[ "$TEAM" -gt 0 && "$TEAM" -lt 6 ]]; then
+	if [[ "$TEAM" -gt 0 && "$TEAM" -lt 8 ]]; then
 		buildandpush
 		deploytohotfix $TEAM
 		inform_hotfix $TEAM
