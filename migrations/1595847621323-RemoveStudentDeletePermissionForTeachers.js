9a108da2c4d4225f1153ee19b2baec98b8604ec5
Tormod Flesjø <tormod.flesjo@gmail.com>
2020-07-27 13:17:38 +0200

    add timechange to migration file

---
+++ migrations/1595847621323-RemoveStudentDeletePermissionForTeachers.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+// eslint-disable-next-line no-unused-vars
+const { info, error } = require('../src/logger');
+
+const { connect, close } = require('../src/utils/database');
+
+const RoleModel = mongoose.model('role20202707', new mongoose.Schema({
+	name: { type: String, required: true },
+	permissions: [{ type: String }],
+}, {
+	timestamps: true,
+}), 'roles');
+
+const permission = 'STUDENT_DELETE';
+
+// How to use more than one schema per collection on mongodb
+// https://stackoverflow.com/questions/14453864/use-more-than-one-schema-per-collection-on-mongodb
+
+module.exports = {
+	up: async function up() {
+		await connect();
+		// ////////////////////////////////////////////////////
+		// Make changes to the database here.
+		// Hint: Access models via this('modelName'), not an imported model to have
+		// access to the correct database connection. Otherwise Mongoose calls never return.
+
+		await RoleModel.findOneAndUpdate(
+			{
+				name: 'teacher',
+			}, {
+				$pull: { permissions: permission },
+			},
+		).lean().exec();
+		// ////////////////////////////////////////////////////
+		await close();
+	},
+
+	down: async function down() {
+		await connect();
+		// ////////////////////////////////////////////////////
+		// Implement the necessary steps to roll back the migration here.
+
+		await RoleModel.findOneAndUpdate(
+			{
+				name: 'administrator',
+			}, {
+				$addToSet: { permissions: permission },
+			},
+		).lean().exec();
+		// ////////////////////////////////////////////////////
+		await close();
+	},
+};
