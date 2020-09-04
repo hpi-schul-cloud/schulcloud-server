b0504a572d46beb2e60047caa15c162393536246
Alexander Riese <alexander.riese@capgemini.com>
2020-09-04 16:31:13 +0200

    SC-6506 escape route value

--- test/services/helpers/getAllRoutes.js
+++ test/services/helpers/getAllRoutes.js
@@ -2,11 +2,11 @@ const _ = require('lodash');
 const app = require('../../../src/app');
 
 const getAllRoutes = () => {
-	// new Set(app._router.stack.filter((x) => x.route && x.route.path).map((x => x.route.path)))
 	const routes = app._router.stack.filter((x) => x.route && x.route.path).map((x) => x.route);
 	const groupedRoutes = _.groupBy(routes, (r) => r.path.replace(/\/:__feathersId$/, ''));
 	return Object.fromEntries(
-		Object.entries(groupedRoutes).map(([route, group]) => {
+		Object.entries(groupedRoutes).map(([unescapedRoute, group]) => {
+			const route = unescapedRoute.replace(/\/:([^\/]*)?($|\/)/, '/{\$1}/')
 			const name = route.replace(/^\//, '');
 			const methods = _.uniq(group.map((r) => Object.keys(r.methods).filter((k) => r.methods[k])).flat());
 
