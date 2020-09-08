78ad156d8eb12ac80ee6af63bacc310a1a1075cc
Alexander Riese <alexander.riese@capgemini.com>
2020-09-08 14:05:15 +0200

    SC-6506 fix code style

--- test/services/helpers/getAllRoutes.js
+++ test/services/helpers/getAllRoutes.js
@@ -2,16 +2,17 @@ const _ = require('lodash');
 const app = require('../../../src/app');
 
 const getAllRoutes = () => {
-	const routes = app._router.stack.filter((x) => x.route && x.route.path).map((x) => x.route);
+	// eslint-disable-next-line no-underscore-dangle
+	const routes = app._router.stack.filter((x) => x.route && x.route.path && x.route.path !== '*').map((x) => x.route);
 	const groupedRoutes = _.groupBy(routes, (r) => r.path.replace(/\/:__feathersId$/, ''));
 	return Object.fromEntries(
 		Object.entries(groupedRoutes).map(([unescapedRoute, group]) => {
-			const route = unescapedRoute.replace(/\/:([^\/]*)?($|\/)/, '/{\$1}/')
+			const route = unescapedRoute.replace(/\/:([^/]*)?($|\/)/, '/{$1}/').replace(/\/$/, '');
 			const name = route.replace(/^\//, '');
 			const methods = _.uniq(group.map((r) => Object.keys(r.methods).filter((k) => r.methods[k])).flat());
 
 			return [name, { methods, route, name }];
-		}),
+		})
 	);
 };
 
