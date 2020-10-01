e3273ea652b97435b4ec2a524d371bf59a48e145
Thomas Feldtkeller <thomas.feldtkeller@hpi.de>
2020-10-01 12:15:22 +0200

    minor formatting

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const appPromise = require('./app');
 const logger = require('./logger');
 
-appPromise.then((app) => {
-	const port = app.get('port');
-	const server = app.listen(port);
+appPromise
+	.then((app) => {
+		const port = app.get('port');
+		const server = app.listen(port);
 
-	server.on('listening', () => {
-		logger.log('info', `Schul-Cloud application started on http://${app.get('host')}:${port}`);
+		server.on('listening', () => {
+			logger.log('info', `Schul-Cloud application started on http://${app.get('host')}:${port}`);
+		});
+	})
+	.catch((err) => {
+		logger.error('server startup failed', err);
 	});
-});
-
-appPromise.catch((err) => {
-	logger.error('server startup failed', err);
-});
