2c7d006668c07dd3ca806b77c9e14fab25e22db7
Tobias Wollowski <tobias.wollowski@hpi.de>
2020-10-02 15:18:16 +0200

    explictly print hierarchy on startup of app via index file only to disable implict logging during test execution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+const { Configuration } = require('@schul-cloud/commons');
 const app = require('./app');
 const logger = require('./logger');
+
+Configuration.printHierarchy();
 
 const port = app.get('port');
 const server = app.listen(port);
