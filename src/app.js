abb7ae5a095a71cb519b4f2fc83ff5a2f036cc23
Tobias Wollowski <tobias.wollowski@hpi.de>
2020-10-07 17:11:46 +0200

    apply suggestions

--- src/app.js
+++ src/app.js
@@ -47,7 +47,10 @@ const metricsOptions = {};
 if (Configuration.has('PROMETHEUS__METRICS_PATH')) {
 	metricsOptions.metricsPath = Configuration.get('PROMETHEUS__METRICS_PATH');
 }
-metricsOptions.durationBuckets = Configuration.data.PROMETHEUS.DURATION_BUCKETS_SECONDS;
+if (Configuration.has('PROMETHEUS__DURATION_BUCKETS_SECONDS[0]')) {
+	// TODO rewrite configuration to support arrays via get()
+	metricsOptions.durationBuckets = Configuration.data.PROMETHEUS.DURATION_BUCKETS_SECONDS;
+}
 app.use(apiMetrics(metricsOptions));
 
 setupSwagger(app);
