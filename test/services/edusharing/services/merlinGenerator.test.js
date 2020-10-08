1d2b5fe57dc59ed924b7814f2852f737cb1505f5
Tormod Flesjø <tormod.flesjo@gmail.com>
2020-10-08 13:31:16 +0200

    Add codacy pleaser

--- test/services/edusharing/services/merlinGenerator.test.js
+++ test/services/edusharing/services/merlinGenerator.test.js
@@ -3,6 +3,7 @@ const chai = require('chai');
 const sinon = require('sinon');
 const request = require('request-promise-native');
 const appPromise = require('../../../../src/app');
+
 const mockResponse =
 	'http://live.download.nibis.de/refid=2255/8bNPrKXGbAYiYxadgTVe6y80FquWD6AnYcLyMiKqO2leca144Hc9GkoVomDucxLAJlXsQQkKeT4rfC4EDf3rm4OIBiIHo1PDe0w8FwvQwjo./35d590fdd29fb20a750a0934af7025a3';
 
