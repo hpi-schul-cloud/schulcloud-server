898cfcc7a2ea01d12765974880f97fb923950e05
Tormod Flesjø <tormod.flesjo@gmail.com>
2020-10-09 14:58:07 +0200

    Add correct mocking

--- test/services/edusharing/services/merlinGenerator.test.js
+++ test/services/edusharing/services/merlinGenerator.test.js
@@ -7,7 +7,7 @@ const appPromise = require('../../../../src/app');
 const mockResponse =
 	'http://live.download.nibis.de/refid=2255/8bNPrKXGbAYiYxadgTVe6y80FquWD6AnYcLyMiKqO2leca144Hc9GkoVomDucxLAJlXsQQkKeT4rfC4EDf3rm4OIBiIHo1PDe0w8FwvQwjo./35d590fdd29fb20a750a0934af7025a3';
 
-describe('Merlin Token Generator', () => {
+describe.only('Merlin Token Generator', () => {
 	let MerlinTokenGeneratorService;
 	let app;
 	before(async () => {
@@ -23,11 +23,19 @@ describe('Merlin Token Generator', () => {
 		assert.ok(MerlinTokenGeneratorService);
 	});
 
+	it('should thrown an error when not giving the correct query', async () => {
+		try {
+			const response = await MerlinTokenGeneratorService.find({ query: { foo: 'baz' } });
+		} catch (err) {
+			chai.expect(err.type).to.be.equal('FeathersError');
+		}
+	});
+
 	it('should return a string when requesting a url', async () => {
 		try {
-			sinon.stub(request, 'get').returns(mockResponse);
-			const response = await MerlinTokenGeneratorService.find({ query: { merlinReference: 'FWU-05510597' } });
-			chai.expect(response).to.be.a('string');
+			const get = sinon.stub(request, 'get').returns(mockResponse);
+			const result = await get(MerlinTokenGeneratorService.find({ query: { merlinReference: 'FWU-05510597' } }));
+			chai.expect(result).to.be.equal(mockResponse);
 			request.get.restore();
 		} catch (err) {
 			throw new Error(err);
