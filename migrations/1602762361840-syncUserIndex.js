d2fda61b9e63ff007c064abe46b56f77fe86def4
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2020-10-15 13:48:22 +0200

    add migration to sync indexes

---
+++ migrations/1602762361840-syncUserIndex.js
@@ -0,0 +1,16 @@
+const { info } = require('../src/logger');
+
+const { connect, close } = require('../src/utils/database');
+
+require('../src/services/user/model');
+
+module.exports = {
+	up: async function up() {
+		await connect();
+		// ////////////////////////////////////////////////////
+		info('sync User indexes');
+		this('user').syncIndexes();
+		// ////////////////////////////////////////////////////
+		await close();
+	},
+};
