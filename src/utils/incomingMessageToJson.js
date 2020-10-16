803d7a3bf8161ebdded772c3f7aa2bd4631f2019
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2020-10-16 12:45:50 +0200

    Add missing return

--- src/utils/incomingMessageToJson.js
+++ src/utils/incomingMessageToJson.js
@@ -8,6 +8,7 @@ const logger = reqlib('src/logger');
 const incomingMessageToJson = (incomingMessage) => {
 	if (!(incomingMessage instanceof http.IncomingMessage)) { 
 		logger.error('Input is not a instance of http.IncomingMessage', incomingMessage);
+		return incomingMessage;
 	}
 
 	const { statusMessage, statusCode, method, httpVersion, headers, url } = incomingMessage;
