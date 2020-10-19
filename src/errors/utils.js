948e9b20705d692459cd3224e9a6de4b1855f830
Cedric Evers <cedric@it-4d.de>
2020-10-19 12:08:41 +0200

    Merge pull request #1843 from hpi-schul-cloud/feature/SC-6567/AddFilterForIncomingMessage
    add util to cleanup IncomingMessage for errors

--- src/errors/utils.js
+++ src/errors/utils.js
@@ -1,3 +1,8 @@
+const http = require('http');
+const reqlib = require('app-root-path').require;
+
+const { incomingMessageToJson } = reqlib('src/utils');
+
 const { errorsByCode } = require('./index.js');
 
 const isFeatherError = (error) => error.type === 'FeathersError';
@@ -10,7 +15,21 @@ const convertToFeathersError = (error) => {
 	return new errorsByCode[code](error);
 };
 
+const cleanupIncomingMessage = (error = {}) => {
+	if (error.response instanceof http.IncomingMessage) {
+		error.response = incomingMessageToJson(error.response);
+	}
+	if (typeof error.options === 'object') {
+		if (Buffer.isBuffer(error.options.body)) {
+			delete error.options.body;
+		}
+		// Possible to pass secret Filter for headers and querys in uri
+		// Possible to move out all functions keys like callback
+	}
+};
+
 module.exports = {
 	isFeatherError,
 	convertToFeathersError,
+	cleanupIncomingMessage,
 };
