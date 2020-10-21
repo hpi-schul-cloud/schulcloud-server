071b0a9e0d8a5a2ad39c03049b599b65828f5c60
Philipp Kleybolte <philipp.kleybolte@capgemini.com>
2020-10-21 13:48:00 +0200

    SC-7091 Changes to RocketChat-Matrix-Migration

--- migrations/1603114571772-rocketchat-to-matrix-messenger.js
+++ migrations/1603114571772-rocketchat-to-matrix-messenger.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-await-in-loop */
 // eslint-disable-next-line no-unused-vars
-const { info, error } = require('../src/logger');
+const { info, warn, error } = require('../src/logger');
 
 const { connect, close } = require('../src/utils/database');
 
@@ -25,27 +25,32 @@ module.exports = {
 		let looped = 0;
 
 		while (looped < amount) {
-			const schools = await schoolModel
-				.find({ features: 'rocketChat' })
-				.sort({
-					updatedAt: 1,
-					createdAt: 1,
-				})
-				.skip(looped)
-				.limit(limit)
-				.lean()
-				.exec();
-			// eslint-disable-next-line no-loop-func
-			await Promise.all(
-				schools.map((school) => {
-					if (!school.features.find((feature) => feature === 'messenger')) {
-						school.features.push('messenger');
-					}
-					return schoolModel.findByIdAndUpdate(school._id, school);
-				})
-			);
+			try {
+				const schools = await schoolModel
+					.find({ features: 'rocketChat' })
+					.sort({
+						updatedAt: 1,
+						createdAt: 1,
+					})
+					.skip(looped)
+					.limit(limit)
+					.lean()
+					.exec();
+				// eslint-disable-next-line no-loop-func
+				await Promise.all(
+					schools.map((school) => {
+						if (!school.features.find((feature) => feature === 'messenger')) {
+							school.features.push('messenger');
+						}
+						return schoolModel.findByIdAndUpdate(school._id, { $set: { features: school.features } });
+					})
+				);
 
-			looped += schools.length;
+				looped += schools.length;
+			} catch (e) {
+				error('migration form rocketChat to Matrix failed');
+				error(e);
+			}
 		}
 		// ////////////////////////////////////////////////////
 		info(`migrated ${looped} schools from rocketChat to Matrix messenger`);
@@ -53,41 +58,6 @@ module.exports = {
 	},
 
 	down: async function down() {
-		await connect();
-		// ////////////////////////////////////////////////////
-		// Make changes to the database here.
-		// Hint: Access models via this('modelName'), not an imported model to have
-		// access to the correct database connection. Otherwise Mongoose calls never return.
-		const amount = await schoolModel.find({ features: 'messenger' }).countDocuments();
-		info(`${amount} schools with rocketChat enabled found. Start removing of Matrix messenger from these school`);
-		const limit = 200;
-		let looped = 0;
-		let removed = 0;
-
-		while (looped < amount) {
-			const schools = await schoolModel
-				.find({ features: 'rocketChat' })
-				.sort({
-					updatedAt: 1,
-					createdAt: 1,
-				})
-				.skip(looped)
-				.limit(limit)
-				.lean()
-				.exec();
-			// eslint-disable-next-line no-loop-func
-			await Promise.all(
-				schools.map((school) => {
-					school.features = school.features.filter((feature) => feature !== 'messenger');
-					this.removed += 1;
-					return schoolModel.findByIdAndUpdate(school._id, school);
-				})
-			);
-
-			looped += schools.length;
-		}
-		// ////////////////////////////////////////////////////
-		info(`Removed Matrix messenger from ${removed} schools`);
-		await close();
+		warn("Schools using Matrix Messenger won't be migrated back to RocketChat automatically. If you need to switch Matrix Messenger of use the instance feature flag.");
 	},
 };
