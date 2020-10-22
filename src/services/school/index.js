d6dd5b151c7bac485ed3eb57f6cfcf3bff006dbe
Virgil Chiriac <virgil.chiriac@hpi.de>
2020-10-22 16:37:50 +0200

    SC-6293 - fixes lint

--- src/services/school/index.js
+++ src/services/school/index.js
@@ -83,7 +83,9 @@ module.exports = function schoolServices() {
 		handlePermissionsService.hooks(handlePermissionsHooks);
 	}
 
-	const FEATURE_ADMIN_TOGGLE_STUDENT_LERNSTORE_VIEW_ENABLED = Configuration.get('FEATURE_ADMIN_TOGGLE_STUDENT_LERNSTORE_VIEW_ENABLED');
+	const FEATURE_ADMIN_TOGGLE_STUDENT_LERNSTORE_VIEW_ENABLED = Configuration.get(
+		'FEATURE_ADMIN_TOGGLE_STUDENT_LERNSTORE_VIEW_ENABLED'
+	);
 	if (FEATURE_ADMIN_TOGGLE_STUDENT_LERNSTORE_VIEW_ENABLED) {
 		app.use('/school/student/studentlernstorevisibility', new HandlePermissions('student', 'LERNSTORE_VIEW'));
 		const handleLernStorePermissionsService = app.service('/school/student/studentlernstorevisibility');
