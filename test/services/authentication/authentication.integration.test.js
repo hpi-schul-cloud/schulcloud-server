34f0ecac4e90e8b6c17d36f5be6fe70a8e532196
Thomas Feldtkeller <thomas.feldtkeller@hpi.de>
2020-10-30 14:49:09 +0100

    specifiy logout

--- test/services/authentication/authentication.integration.test.js
+++ test/services/authentication/authentication.integration.test.js
@@ -32,7 +32,7 @@ describe('authentication service integration tests', function test() {
 		Configuration.reset(configBefore);
 	});
 
-	describe('API tests', () => {
+	describe('Login', () => {
 		it('When a user sends valid local authentication details, then he recieves a jwt', async () => {
 			const accountDetails = { username: `${Date.now()}poweruser@mail.schul.tech`, password: 'passwordA' };
 			const { _id: schoolId } = await testObjects.createTestSchool();
@@ -50,6 +50,24 @@ describe('authentication service integration tests', function test() {
 				foo: 'bar',
 			});
 			expect(response.status).to.equal(201);
+			expect(response.body).to.haveOwnProperty('accessToken');
+		});
+	});
+
+	describe('Logout', () => {
+		it('When a user successfully removes his authentication, then he is logged out', async () => {
+			const accountDetails = { username: `${Date.now()}poweruser@mail.schul.tech`, password: 'passwordA' };
+			const { _id: schoolId } = await testObjects.createTestSchool();
+			const user = await testObjects.createTestUser({ roles: ['teacher'], schoolId });
+			const token = await testObjects.generateJWTFromUser(user);
+			const request = chai
+				.request(app)
+				.delete('/authentication')
+				.set('Accept', 'application/json')
+				.set('Authorization', token)
+				.set('content-type', 'application/json');
+			const response = await request.send();
+			expect(response.status).to.equal(200);
 			expect(response.body).to.haveOwnProperty('accessToken');
 		});
 	});
