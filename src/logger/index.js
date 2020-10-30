6858f056f5e936b707842cee35f64d9209589da9
Christoph Arndt <christoph.arndt@dataport.de>
2020-10-30 03:07:53 -0700

    changed loger to new config

--- src/logger/index.js
+++ src/logger/index.js
@@ -1,7 +1,8 @@
 const winston = require('winston');
+const { Configuration } = require('@schul-cloud/commons');
 
 const { format, transports, createLogger } = winston;
-const { LOG_LEVEL, NODE_ENV, ENVIRONMENTS } = require('../../config/globals');
+const { NODE_ENV, ENVIRONMENTS } = require('../../config/globals');
 
 const addType = format.printf((log) => {
 	if (log.stack || log.level === 'error') {
@@ -27,11 +28,11 @@ if (NODE_ENV === ENVIRONMENTS.TEST) {
 
 const logger = createLogger({
 	levels: winston.config.syslog.levels,
-	level: LOG_LEVEL,
+	level: Configuration.get('LOG_LEVEL'),
 	format: formater,
 	transports: [
 		new transports.Console({
-			level: LOG_LEVEL,
+			level: Configuration.get('LOG_LEVEL'),
 			handleExceptions: true,
 			// https://github.com/winstonjs/winston#handling-uncaught-promise-rejections-with-winston
 			handleRejections: true,
