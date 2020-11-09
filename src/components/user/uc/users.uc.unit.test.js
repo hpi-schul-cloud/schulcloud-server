4e0da87db5680cf138b002d7c2841929eeb89595
Michael Wolowyk <michael.wolowyk@capgemini.com>
2020-11-09 15:21:40 +0100

    SC-7555: UC unit test with stubbed repositories

--- src/components/user/uc/users.uc.unit.test.js
+++ src/components/user/uc/users.uc.unit.test.js
@@ -1,7 +1,44 @@
+const sinon = require('sinon');
+
 const { expect } = require('chai');
 const { deleteUserUC, replaceUserWithTombstoneUC } = require('./users.uc');
 const appPromise = require('../../../app');
 const testObjects = require('../../../../test/services/helpers/testObjects')(appPromise);
+const { userRepo, accountRepo, trashbinRepo } = require('../repo/index');
+
+const USER_ID = 'USER_ID';
+
+const createTestUser = (userId) => {
+	return {
+		_id: userId,
+		firstName: 'Max',
+		lastName: 'Mustermann',
+		email: `delete_me@test.de`,
+		schoolId: 'SCHOOL_ID',
+		roles: [],
+	};
+};
+
+const createTestAccount = (user) => {
+	return {
+		_id: 'ACCOUNT_ID',
+		userId: user._id,
+		username: user.email,
+		password: 'PASSWORD',
+	};
+};
+
+const createTestTrashbin = (userId, data) => {
+	return {
+		_id: 'TRASHBIN_ID',
+		userId,
+		...data,
+	};
+};
+
+let getUserStub;
+let getUserAccountStub;
+let createUserTrashbinStub;
 
 describe('users usecase', () => {
 	let app;
@@ -10,9 +47,32 @@ describe('users usecase', () => {
 	before(async () => {
 		app = await appPromise;
 		server = await app.listen(0);
+
+		const user = createTestUser(USER_ID);
+		const account = createTestAccount(user);
+		const trashbin = createTestTrashbin(user._id, { user, account });
+
+		// init stubs
+		getUserStub = sinon.stub(userRepo, 'getUser');
+		getUserStub.withArgs(USER_ID).returns(user);
+		sinon.stub(userRepo, 'replaceUserWithTombstone');
+
+		getUserAccountStub = sinon.stub(accountRepo, 'getUserAccount');
+		getUserAccountStub.withArgs(USER_ID).returns(account);
+
+		sinon.stub(accountRepo, 'deleteUserAccount');
+		createUserTrashbinStub = sinon.stub(trashbinRepo, 'createUserTrashbin');
+		createUserTrashbinStub.withArgs(USER_ID).returns(trashbin);
 	});
 
 	after(async () => {
+		// restore stubbed functions
+		getUserStub.restore();
+		userRepo.replaceUserWithTombstone.restore();
+		getUserAccountStub.restore();
+		accountRepo.deleteUserAccount.restore();
+		createUserTrashbinStub.restore();
+
 		await server.close();
 	});
 
@@ -25,15 +85,32 @@ describe('users usecase', () => {
 	});
 
 	describe('user delete orchestrator', () => {
-		it('when the function is called, then it returns (replace with useful test)', async () => {
-			// arrange
-			const user = await testObjects.createTestUser();
-
+		it('should successfully mark user for deletion', async () => {
 			// act
-			const result = await deleteUserUC(user._id, app);
+			const result = await deleteUserUC(USER_ID, app);
 
-			// assert
 			expect(result).to.deep.equal({ success: true });
+		});
+
+		it('should return not found if user cannot be found', async () => {
+			// init stubs
+			const userId = 'NOT_FOUND_USER';
+			getUserStub.withArgs(userId);
+			expect(() => deleteUserUC(userId, app), "if user wasn't found it should fail").to.throw;
+		});
+
+		it("should return error if trashbin couldn't be created", async () => {
+			// init stubs
+			const userId = 'USER_ID_2';
+			const user = createTestUser(userId);
+			const account = createTestAccount(user);
+
+			// init stubs
+			getUserStub.withArgs(userId).returns(user);
+			getUserAccountStub.withArgs(userId).returns(account);
+			createUserTrashbinStub.withArgs(userId);
+
+			expect(() => deleteUserUC(userId, app), "if trashbin couldn't be created it should fail").to.throw;
 		});
 	});
 });
