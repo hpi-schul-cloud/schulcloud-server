ef5cd4ab6e8709aade65ca6b696786a37b7a6ddc
Dominik Glandorf <do.gl@gmx.de>
2020-11-09 10:30:15 +0100

    make codacy happy

--- src/services/pseudonym/hooks/index.js
+++ src/services/pseudonym/hooks/index.js
@@ -77,8 +77,7 @@ const populateUsername = (context) => {
 				user: {
 					firstName: response.firstName,
 					lastName: response.lastName,
-					iframe: encodeURI(oauth2.getSubject(context.result.data[0].pseudonym,
-						context.app.settings.services.web)),
+					iframe: oauth2.getSubject(context.result.data[0].pseudonym, context.app.settings.services.web),
 				},
 			};
 			return context;
