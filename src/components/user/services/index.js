23d02f5cddd131fdecdb30cc35fbe11368f07986
Daniel Rieckhof <Daniel.Rieckhof@hpi.uni-potsdam.de>
2020-11-18 12:55:31 +0100

    Updated code style

--- src/components/user/services/index.js
+++ src/components/user/services/index.js
@@ -1,11 +1,11 @@
-const userServiceV2 = require('./user.service.v2');
+const { UserServiceV2, adminHookGenerator } = require('./user.service.v2');
 
 module.exports = (app) => {
 	const adminStudentsRoute = '/users/v2/admin/student';
-	app.use(adminStudentsRoute, new userServiceV2.UserServiceV2('student'));
-	app.service(adminStudentsRoute).hooks(userServiceV2.adminHookGenerator('STUDENT'));
+	app.use(adminStudentsRoute, new UserServiceV2('student'));
+	app.service(adminStudentsRoute).hooks(adminHookGenerator('STUDENT'));
 
 	const adminTeachersRoute = '/users/v2/admin/teacher';
-	app.use(adminTeachersRoute, new userServiceV2.UserServiceV2('teacher'));
-	app.service(adminTeachersRoute).hooks(userServiceV2.adminHookGenerator('TEACHER'));
+	app.use(adminTeachersRoute, new UserServiceV2('teacher'));
+	app.service(adminTeachersRoute).hooks(adminHookGenerator('TEACHER'));
 };
