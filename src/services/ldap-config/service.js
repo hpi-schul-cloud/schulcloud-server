440c2ec91e8d9ad18588399068d9b22b05067fd5
Maximilian Grundke <maximiliangrundke@googlemail.com>
2020-11-19 18:00:57 +0100

    Connect to LDAP server to verify config

--- src/services/ldap-config/service.js
+++ src/services/ldap-config/service.js
@@ -1,4 +1,10 @@
+const logger = require('../../logger');
+
 class LdapConfigService {
+	setup(app) {
+		this.app = app;
+	}
+
 	async create(config, params) {
 		const { verifyOnly } = params.query;
 
@@ -10,25 +16,52 @@ class LdapConfigService {
 		return verificationResult;
 	}
 
-	async verifyConfig(/* config */) {
-		return {
-			ok: true,
-			users: {
-				total: 1396,
-				admin: 4,
-				teacher: 20,
-				student: 1372,
-				sample: {},
-			},
-			classes: {
-				total: 27,
-				sample: {},
-			},
+	async verifyConfig(config) {
+		const ldap = this.app.service('ldap');
+		const result = {
+			ok: false,
+			users: {},
+			classes: {},
 		};
+		try {
+			const users = await ldap.getUsers(config);
+			result.users = LdapConfigService.generateUserStats(users);
+			const classes = await ldap.getClasses(config);
+			result.classes = LdapConfigService.generateClassStats(classes);
+			result.ok = true;
+		} catch (error) {
+			logger.warning('Error during LDAP config verification', { error });
+		} finally {
+			ldap.disconnect(config);
+		}
+		return result;
 	}
 
 	async saveConfig(/* config */) {
 		return 42;
+	}
+
+	static generateUserStats(users = []) {
+		const result = {
+			total: users.length,
+			admin: 0,
+			teacher: 0,
+			student: 0,
+			sample: users.length > 0 ? users[0] : {},
+		};
+		for (const user of users) {
+			if (user.roles.includes('administrator')) result.admin += 1;
+			if (user.roles.includes('teacher')) result.teacher += 1;
+			if (user.roles.includes('student')) result.student += 1;
+		}
+		return result;
+	}
+
+	static generateClassStats(classes = []) {
+		return {
+			total: classes.length,
+			sample: classes.length > 0 ? classes[0] : {},
+		};
 	}
 }
 
