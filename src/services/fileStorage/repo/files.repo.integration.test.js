bd7e91e3d6395d75e5ff88ccffede9956d7f03bb
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2020-11-19 11:44:06 +0100

    wip add first test

--- src/services/fileStorage/repo/files.repo.integration.test.js
+++ src/services/fileStorage/repo/files.repo.integration.test.js
@@ -1,21 +1,37 @@
 const { expect } = require('chai');
+const { FileModel } = require('../model');
 
 const { findFilesShardWithUserId, deleteFilesByIDs } = require('./files.repo');
 
 describe('user.repo.integration.test', () => {
-	let testUtils;
+	let fileTestUtils;
 	let server;
+	let generateObjectId;
 	let app;
 
 	before(async () => {
 		/* eslint-disable global-require */
 		app = await require('../../../app');
-		testUtils = require('../../../../test/services/helpers/testObjects')(app);
+		({ files: fileTestUtils, generateObjectId } = require('../../../../test/services/helpers/testObjects')(app));
 		/* eslint-enable global-require */
 		server = await app.listen(0);
 	});
 
 	after(async () => {
-		await server.closed();
+		await fileTestUtils.cleanup();
+		await server.close();
+	});
+
+	it('findFilesShardWithUserId', async () => {
+		const userToBeDeleted = generateObjectId();
+		const fileOwner = generateObjectId();
+
+		const userToBeDeletedPermission = fileTestUtils.createPermission({ refId: userToBeDeleted });
+		const additonalPermissions = [userToBeDeletedPermission];
+		const fileToBeFound = await fileTestUtils.create({ owner: fileOwner, additonalPermissions });
+		const fileNOTToBeFound = await fileTestUtils.create({ owner: fileOwner });
+		const fileWhereUserToBeDeletedIsOwner = await fileTestUtils.create({ owner: userToBeDeleted });
+
+		const result = await findFilesShardWithUserId(userToBeDeleted);
 	});
 });
