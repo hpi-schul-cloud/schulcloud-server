a3b3e103e6f806584fbbd2cd70f31febcc7856d1
Alexander Riese <alexander.riese@capgemini.com>
2020-11-19 17:11:45 +0100

    SC-7827 experiment with aggregate

--- src/services/fileStorage/repo/files.repo.integration.test.js
+++ src/services/fileStorage/repo/files.repo.integration.test.js
@@ -3,11 +3,13 @@ const { FileModel } = require('../model');
 
 const { findFilesThatUserCanAccess, deleteFilesByIDs } = require('./files.repo');
 
-describe('user.repo.integration.test', () => {
+describe('user.repo.integration.test', function test() {
 	let fileTestUtils;
 	let server;
 	let generateObjectId;
 	let app;
+
+	this.timeout(100000);
 
 	before(async () => {
 		/* eslint-disable global-require */
@@ -24,7 +26,7 @@ describe('user.repo.integration.test', () => {
 
 	// FileId do not exist
 	// found no results
-	it('findFilesShardWithUserId', async () => {
+	it('find files shared with the user that should be deleted', async () => {
 		const userToBeDeletedId = generateObjectId();
 		const fileOwnerId = generateObjectId();
 
@@ -44,7 +46,7 @@ describe('user.repo.integration.test', () => {
 
 	// FileId do not exist
 	// found no results
-	it('deleteFilesByIDs', async () => {
+	it('delete files by IDs', async () => {
 		const userId = generateObjectId();
 
 		const toBeDeletedFile1 = await fileTestUtils.create({ owner: userId });
@@ -55,10 +57,25 @@ describe('user.repo.integration.test', () => {
 		const batchTaskResult = await deleteFilesByIDs(toBeDeltedFileIds);
 		expect(batchTaskResult).to.eql({ n: 2, ok: 1, deletedCount: 2 });
 
-		const searchDBResult = await FileModel.find({ _id: { $in: [...toBeDeltedFileIds, fileToNOTBeDeleted._id] } });
+		const searchDBResult = await FileModel.find({ _id: {$in: [...toBeDeltedFileIds, fileToNOTBeDeleted._id] } });
 
 		const findFileIds = searchDBResult.map(({ _id }) => _id.toString());
 		expect(findFileIds, 'should include the not deleted file').to.be.an('array').with.lengthOf(1);
 		expect(findFileIds).to.include(fileToNOTBeDeleted._id.toString());
+	});
+
+	describe('delete permission connections for user that should be deleted', () => {
+		it('if the file is shared with the user to be deleted', async () => {
+			const userToBeDeletedId = generateObjectId();
+			const fileOwnerId = generateObjectId();
+
+			const userToBeDeletedPermission = fileTestUtils.createPermission({ refId: userToBeDeletedId });
+			const additonalPermissions = [userToBeDeletedPermission];
+			const sharedFile = await fileTestUtils.create({ owner: fileOwnerId, additonalPermissions });
+			expect(sharedFile.permissions.some((permission) => permission.refId.equals(userToBeDeletedId))).to.be.true;
+			// call function to delete permission
+			const sharedFileFromDB = await FileModel.get(sharedFile._id);
+			expect(sharedFileFromDB.permissions.some((permission) => permission.refId.equals(userToBeDeletedId))).to.be.false;
+		});
 	});
 });
