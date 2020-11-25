4c6a9f90c5110b1087ea5184c3a8d01fe3af9115
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2020-11-25 18:15:37 +0100

    Auto stash before merge of "feature/SC-7543-ldap-config-collector" and "origin/feature/SC-7543-ldap-config-collector"

--- src/services/ldap-config/index.js
+++ src/services/ldap-config/index.js
@@ -1,10 +1,14 @@
 const { static: staticContent } = require('@feathersjs/express');
 const path = require('path');
+const reqlib = require('app-root-path').require;
 
 const LdapConfigService = require('./service');
 const LdapConfigServiceHooks = require('./hooks');
 
+const { registerApiValidation } = reqlib('src/utils/apiValidation');
+
 module.exports = (app) => {
+	registerApiValidation(app, path.join(__dirname, '/docs/openapi.yaml'));
 	app.use('/ldap-config/api', staticContent(path.join(__dirname, '/docs/openapi.yaml')));
 
 	app.use('/ldap-config', new LdapConfigService());
