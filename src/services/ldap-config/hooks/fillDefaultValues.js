452246b8d9a1266d9eb0cb87fe6c840ab9c134e0
Maximilian Grundke <maximiliangrundke@googlemail.com>
2020-11-25 13:57:17 +0100

    Add tests and docs for fillDefaultValues hook

--- src/services/ldap-config/hooks/fillDefaultValues.js
+++ src/services/ldap-config/hooks/fillDefaultValues.js
@@ -2,6 +2,14 @@ const reqlib = require('app-root-path').require;
 
 const { BadRequest } = reqlib('src/errors');
 
+/**
+ * Sets sensible default values for LDAP configs to be consumed by
+ * the ldap-config service. Specifically, attribute mappings for the
+ * dn attribute are set to "dn" and provider is set to "general".
+ * @param {Object} context Feathers hook context
+ * @returns {Object} context if successful
+ * @throws {BadRequest} if input format is invalid
+ */
 module.exports = (context) => {
 	const { data } = context;
 	if (
