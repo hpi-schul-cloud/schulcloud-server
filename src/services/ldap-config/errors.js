1987e20939e5e9d753b182a51389febb0e1391eb
Maximilian Grundke <maximiliangrundke@googlemail.com>
2020-11-25 14:04:52 +0100

    Convert message to function for all errors

--- src/services/ldap-config/errors.js
+++ src/services/ldap-config/errors.js
@@ -7,7 +7,7 @@ const errorHandlers = [
 	{
 		match: (error) => error instanceof NotAuthenticated,
 		type: 'WRONG_CREDENTIALS',
-		message: 'Wrong search-user credentials',
+		message: () => 'Wrong search-user credentials',
 	},
 	{
 		match: (error) => error instanceof LDAPConnectionError,
@@ -17,12 +17,12 @@ const errorHandlers = [
 	{
 		match: (error) => error.lde_message === 'No Such Object',
 		type: 'WRONG_SEARCH_PATH',
-		message: 'Wrong search or base path',
+		message: () => 'Wrong search or base path',
 	},
 	{
 		match: (error) => error.message === 'Invalid configuration object',
 		type: 'INVALID_CONFIGURATION_OBJECT',
-		message: 'Invalid configuration object',
+		message: () => 'Invalid configuration object',
 	},
 ];
 
--- src/services/ldap-config/service.js
+++ src/services/ldap-config/service.js
@@ -131,10 +131,9 @@ class LdapConfigService {
 			}
 			result.ok = true;
 		} catch (error) {
-			for (const { match, message: messagefn, type } of errorHandlers) {
+			for (const { match, message, type } of errorHandlers) {
 				if (match(error)) {
-					const message = messagefn instanceof Function ? messagefn(error) : messagefn;
-					result.errors.push({ message, type });
+					result.errors.push({ message: message(error), type });
 				}
 			}
 			if (result.errors.length === 0) {
