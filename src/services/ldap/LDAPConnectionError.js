73491ba0c8e535ada60c3ec3d037b5198c2f57e2
Maximilian Grundke <maximiliangrundke@googlemail.com>
2020-11-26 18:55:52 +0100

    Add default for error data

--- src/services/ldap/LDAPConnectionError.js
+++ src/services/ldap/LDAPConnectionError.js
@@ -3,7 +3,7 @@ const reqlib = require('app-root-path').require;
 const { ApplicationError } = reqlib('src/errors');
 
 class LDAPConnectionError extends ApplicationError {
-	constructor(message, data) {
+	constructor(message, data = {}) {
 		super(message);
 		this.data = data;
 	}
