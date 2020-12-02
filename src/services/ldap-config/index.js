1fc78d15b999cfde0336bb3303151513dac05346
Maximilian Grundke <maximiliangrundke@googlemail.com>
2020-12-02 14:51:25 +0100

    Fix scope and add get path to openapi spec

--- src/services/ldap-config/index.js
+++ src/services/ldap-config/index.js
@@ -1,17 +1,8 @@
-const { static: staticContent } = require('@feathersjs/express');
-const path = require('path');
-const reqlib = require('app-root-path').require;
-
 const LdapConfigService = require('./service');
 const LdapConfigServiceHooks = require('./hooks');
 
-const { registerApiValidation } = reqlib('src/utils/apiValidation');
-
 module.exports = (app) => {
-	registerApiValidation(app, path.join(__dirname, '/docs/openapi.yaml'));
-	app.use('/ldap-config/api', staticContent(path.join(__dirname, '/docs/openapi.yaml')));
-
-	app.use('/ldap-config', new LdapConfigService());
-	const systemService = app.service('/ldap-config');
+	app.use('/legacy/v1/ldap-config', new LdapConfigService());
+	const systemService = app.service('/legacy/v1/ldap-config');
 	systemService.hooks(LdapConfigServiceHooks);
 };
