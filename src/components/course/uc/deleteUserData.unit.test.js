ceced5db11ce40511fc642b81e18880a22ca50f1
Tobias Wollowski <tobias.wollowski@hpi.de>
2020-12-03 12:17:26 +0100

    add uc test skeleton

---
+++ src/components/course/uc/deleteUserData.unit.test.js
@@ -0,0 +1,16 @@
+const sinon = require('sinon');
+const { ObjectId } = require('mongoose').Types;
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const deleteUserDataUC = require('./deleteUserData.uc');
+
+const { expect } = chai;
+chai.use(chaiAsPromised);
+
+describe('when removing user relations in courses', () => {
+	describe('all the lessons', () => {
+		it('should be addded to trashbindata', () => {
+			// TODO
+		});
+	});
+});
