0a76a3ac77b88fc3c768ef4f75b0b53b81bb9b83
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2020-12-04 13:05:29 +0100

    Add some error handling, that we not lost data by throwing an error

--- src/components/task/uc/deleteUserData.uc.js
+++ src/components/task/uc/deleteUserData.uc.js
@@ -1,3 +1,6 @@
+const reqlib = require('app-root-path').require;
+
+const { DeletedUserDataError } = reqlib('src/errors');
 const { findPrivateHomeworksFromUser, deletePrivateHomeworksFromUser } = require('../repo/task.repo');
 
 // TODO
@@ -19,17 +22,19 @@ const removeConnectionToSharedSubmissions = async (userId) => {
 	};
 };
 
-const deletePrivateUserHomeworks = async (userId) => {
+const deletePrivateUserHomeworks = async (userId, context) => {
 	// TODO
 	const result = await findPrivateHomeworksFromUser(userId);
 	const execute = await deletePrivateHomeworksFromUser(userId);
 
-	return {
+	const status = {
 		context: 'homeworks',
 		type: 'private',
 		data: result,
 		success: execute.success,
 	};
+	context.push(status);
+	return status;
 };
 
 const removeConnectionToSharedHomeworks = async (userId) => {
@@ -43,12 +48,18 @@ const removeConnectionToSharedHomeworks = async (userId) => {
 
 const deleteUserRelatedData = async (userId) => {
 	// TODO Promise.all
-	const privateSubmissions = await deletePrivateSubmissions(userId);
-	const sharedSubmissions = await removeConnectionToSharedSubmissions(userId);
-	const privateHomeworks = await deletePrivateUserHomeworks(userId);
-	const sharedHomeworks = await removeConnectionToSharedHomeworks(userId);
-	// TODO clearify the latest conclusio array, or object ..but is the array matched in user to object then array make no sense
-	return [privateSubmissions, sharedSubmissions, privateHomeworks, sharedHomeworks];
+	const context = [];
+	try {
+		const privateSubmissions = await deletePrivateSubmissions(userId, context);
+		const sharedSubmissions = await removeConnectionToSharedSubmissions(userId, context);
+		const privateHomeworks = await deletePrivateUserHomeworks(userId, context);
+		const sharedHomeworks = await removeConnectionToSharedHomeworks(userId, context);
+		// TODO clearify the latest conclusio array, or object ..but is the array matched in user to object then array make no sense
+		return context;
+	} catch (err) {
+		// must include which steps failed, otherwise it can not handle
+		throw new DeletedUserDataError('Delete user related data from task failed.', err, context);
+	}
 };
 
 module.exports = {
