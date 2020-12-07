b671259038ad5f86665f65ffec4b29f90edce66a
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2020-12-07 13:16:25 +0100

    Add findGroupSubmissionsFromUser repo with tests

--- src/components/task/uc/deleteUserData.uc.js
+++ src/components/task/uc/deleteUserData.uc.js
@@ -1,12 +1,17 @@
-const reqlib = require('app-root-path').require;
-
-const { DeletedUserDataError } = reqlib('src/errors');
+const logger = require('../../../logger');
 const {
 	findPrivateHomeworksFromUser,
 	deletePrivateHomeworksFromUser,
 	findPublicHomeworksFromUser,
 	replaceUserInPublicHomeworks,
 } = require('../repo/task.repo');
+
+const checkStatus = (status) => {
+	if (status.success !== 1) {
+		// TODO: who is really look into logs for this?
+		logger.warning('Task step failed', status);
+	}
+};
 
 // TODO
 const deletePrivateSubmissions = async (userId) => {
@@ -27,50 +32,45 @@ const removeConnectionToSharedSubmissions = async (userId) => {
 	};
 };
 
-const deletePrivateUserHomeworks = async (userId, context = []) => {
+const deletePrivateUserHomeworks = async (userId) => {
 	const result = await findPrivateHomeworksFromUser(userId);
-	const success = await deletePrivateHomeworksFromUser(userId);
 
-	const status = {
+	if (result.length > 0) {
+		const status = await deletePrivateHomeworksFromUser(userId);
+		checkStatus(status);
+	}
+
+	const trashbinData = {
 		context: 'homeworks',
 		type: 'private',
 		data: result,
-		success,
 	};
-	context.push(status);
-	return context;
+
+	return { trashbinData, complete: undefined };
 };
 
-const removeConnectionToSharedHomeworks = async (userId, replaceUserId, context = []) => {
+const removeConnectionToSharedHomeworks = async (userId, replaceUserId) => {
 	const result = await findPublicHomeworksFromUser(userId, ['_id', 'teacherId']);
-	const success = await replaceUserInPublicHomeworks(userId, replaceUserId);
 
-	const status = {
+	if (result.length > 0) {
+		const status = await replaceUserInPublicHomeworks(userId, replaceUserId);
+		checkStatus(status);
+	}
+
+	const trashbinData = {
 		context: 'homeworks',
 		type: 'shared',
 		data: result,
-		success,
 	};
-	context.push(status);
-	return context;
+
+	return { trashbinData, complete: undefined };
 };
 
-const deleteUserRelatedData = async (userId, schoolTombStoneUserId) => {
-	const context = [];
-	try {
-		// TODO Promise.all
-		await deletePrivateSubmissions(userId, context);
-		await removeConnectionToSharedSubmissions(userId, schoolTombStoneUserId, context);
-		await deletePrivateUserHomeworks(userId, context);
-		await removeConnectionToSharedHomeworks(userId, context);
-		// TODO clearify the latest conclusio array, or object ..but is the array matched in user to object then array make no sense
-		return context;
-	} catch (err) {
-		// must include which steps failed, otherwise it can not handle
-		throw new DeletedUserDataError('Delete user related data from task failed.', err, context);
-	}
-};
+const deleteUserRelatedData = () => [
+	deletePrivateSubmissions,
+	removeConnectionToSharedSubmissions,
+	deletePrivateUserHomeworks,
+	removeConnectionToSharedHomeworks,
+];
 
-module.exports = {
-	deleteUserRelatedData,
-};
+module.exports = deleteUserRelatedData;
