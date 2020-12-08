ca90d30d75c2f51fbdcc485976560fe73f38e5c8
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2020-12-08 09:34:42 +0100

    replace env

--- test/services/helpers/service.test.js
+++ test/services/helpers/service.test.js
@@ -102,7 +102,7 @@ describe('Mail Service', async () => {
 
 	describe('invalid emails', () => {
 		beforeEach(() => {
-			nock(config.services.notification).post('/mails').replyWithError('invalid data send');
+			nock(config.NOTIFICATION_URI).post('/mails').replyWithError('invalid data send');
 		});
 		it('should throw if notification server returns error', async () => {
 			try {
