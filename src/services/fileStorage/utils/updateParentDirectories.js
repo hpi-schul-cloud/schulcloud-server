fb4e6f1da1873e981256650e45f5866c872a78a3
Matti <contact@schmidtchen.me>
2020-12-28 10:46:09 +0100

    hopefully fixed tab problem for Codacy

--- src/services/fileStorage/utils/updateParentDirectories.js
+++ src/services/fileStorage/utils/updateParentDirectories.js
@@ -3,18 +3,18 @@ const { courseModel } = require('../../user-group/model');
 const { teamsModel } = require('../../teams/model');
 
 const updateParentDirectories = async (resourceID) => {
-    const file = await FileModel.findById(resourceID).lean().exec();
-    if (file) {
-        const parentID = file.parent;
-        if (parentID || file.refOwnerModel === 'user') {
-            await FileModel.updateOne({ _id: parentID }, { updatedAt: new Date().toISOString() }).exec();
-            await updateParentDirectories(parentID);
-        } else if (file.refOwnerModel === 'course') {
-            await courseModel.updateOne({ _id: file.owner }, { updatedAt: new Date().toISOString() }).exec();
-        } else if (file.refOwnerModel === 'teams') {
-            await teamsModel.updateOne({ _id: file.owner }, { updatedAt: new Date().toISOString() }).exec();
-        }
-    }
+	const file = await FileModel.findById(resourceID).lean().exec();
+	if (file) {
+		const parentID = file.parent;
+		if (parentID || file.refOwnerModel === 'user') {
+			await FileModel.updateOne({ _id: parentID }, { updatedAt: new Date().toISOString() }).exec();
+			await updateParentDirectories(parentID);
+		} else if (file.refOwnerModel === 'course') {
+			await courseModel.updateOne({ _id: file.owner }, { updatedAt: new Date().toISOString() }).exec();
+		} else if (file.refOwnerModel === 'teams') {
+			await teamsModel.updateOne({ _id: file.owner }, { updatedAt: new Date().toISOString() }).exec();
+		}
+	}
 }
 
 module.exports = updateParentDirectories;
