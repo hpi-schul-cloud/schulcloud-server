719022dd5199df428540f8fe4ddbe23b3fc4766f
Tobias Wollowski <tobias.wollowski@hpi.de>
2021-01-05 10:00:21 +0100

    moved silent error into getResponse qs it influences the response only

--- src/errors/errorUtils.js
+++ src/errors/errorUtils.js
@@ -1,12 +1,15 @@
 const { FeathersError } = require('@feathersjs/errors');
-const { ApplicationError } = require('./errors');
+const { ApplicationError, SilentError } = require('./errors');
 
+const isSilentError = (error) => error instanceof SilentError || (error && error.error instanceof SilentError); // TODO why checking error.error here
 const isApplicationError = (error) => error instanceof ApplicationError;
+
 const isFeathersError = (error) => error instanceof FeathersError;
 const isUncaughtError = (error) => !isApplicationError(error) && !isFeathersError(error);
 
 module.exports = {
-	isUncaughtError,
+	isSilentError,
 	isApplicationError,
 	isFeathersError,
+	isUncaughtError,
 };
