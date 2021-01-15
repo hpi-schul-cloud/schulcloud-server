f2b1b0a1b867e632e608509faf18c025fe8811d8
Alexander Riese <alexander.riese@capgemini.com>
2021-01-15 09:10:34 +0100

    SC-8379 extend test for ltiTools delete route

--- test/services/ltiTool/index.test.js
+++ test/services/ltiTool/index.test.js
@@ -1,37 +1,129 @@
-const assert = require('assert');
+const { expect } = require('chai');
 const appPromise = require('../../../src/app');
+const testObjects = require('../helpers/testObjects')(appPromise);
+const { generateRequestParamsFromUser } = require('../helpers/services/login')(appPromise);
 
 
-describe('ltiTool service', () => {
+describe.only('ltiTool service', () => {
 	let app;
-	let ltiToolService;
+	let server;
 
-	const testTool = {
-		_id: '5836bb5664582c35df3bc215',
-		name: 'LTI Tools',
-		url: 'http://lti.tools/test/tp.php',
-		key: 'jisc.ac.uk',
-		secret: 'secret',
-		logo_url: '',
-		lti_message_type: 'basic-lti-launch-request',
-		lti_version: 'LTI-1p0',
-		resource_link_id: '0',
-	};
-
-	before(async function before() {
+	before(async () => {
 		app = await appPromise;
-		ltiToolService = app.service('ltiTools');
-		this.timeout(10000);
-		await ltiToolService.create(testTool);
+		server = await app.listen(0);
 	});
 
-	after((done) => {
-		ltiToolService.remove(testTool).then((result) => {
-			done();
-		});
+	afterEach(async () => {
+		await testObjects.cleanup();
+	});
+
+	after(async () => {
+		await server.close();
 	});
 
 	it('registered the ltiTools service', () => {
-		assert.ok(ltiToolService);
+		expect(app.service('ltiTools')).to.be.ok;
+	});
+
+	it('students can not patch the tools in their own courses', async () => {
+		try {
+			const student = await testObjects.createTestUser({ roles: 'student' });
+			const tool = await testObjects.createTestLtiTool();
+			await testObjects.createTestCourse({ userIds: [student._id], ltiToolIds: [tool._id] });
+
+			const params = await generateRequestParamsFromUser(student);
+			await app.service('ltiTools').patch(tool._id, { name: 'toolChanged' }, params);
+			throw new Error('should have failed');
+		} catch (err) {
+			expect(err.message).to.not.equal('should have failed');
+			expect(err.code).to.eq(403);
+		}
+	});
+
+	it('teachers can patch the tools in their own courses', async () => {
+		const teacher = await testObjects.createTestUser({ roles: 'teacher' });
+		const tool = await testObjects.createTestLtiTool();
+		await testObjects.createTestCourse({ teacherIds: [teacher._id], ltiToolIds: [tool._id] });
+
+		const params = await generateRequestParamsFromUser(teacher);
+		const result = await app.service('ltiTools').patch(tool._id, { name: 'toolChanged' }, params);
+		expect(result.name).to.equal('toolChanged');
+	});
+
+	it('substitute teachers can patch the tools in their own courses', async () => {
+		const substitutionTeacher = await testObjects.createTestUser({ roles: 'teacher' });
+		const tool = await testObjects.createTestLtiTool();
+		await testObjects.createTestCourse({ substitutionIds: [substitutionTeacher._id], ltiToolIds: [tool._id] });
+
+		const params = await generateRequestParamsFromUser(substitutionTeacher);
+		const result = await app.service('ltiTools').patch(tool._id, { name: 'toolChanged' }, params);
+		expect(result.name).to.equal('toolChanged');
+	});
+
+	it('teachers can not patch the tools in foreign courses', async () => {
+		try {
+			const courseTeacher = await testObjects.createTestUser({ roles: 'teacher' });
+			const otherTeacher = await testObjects.createTestUser({ roles: 'teacher' });
+			const tool = await testObjects.createTestLtiTool();
+			await testObjects.createTestCourse({ teacherIds: [courseTeacher._id], ltiToolIds: [tool._id] });
+
+			const params = await generateRequestParamsFromUser(otherTeacher);
+			await app.service('ltiTools').patch(tool._id, { name: 'toolChanged' }, params);
+			throw new Error('should have failed');
+		} catch (err) {
+			expect(err.message).to.not.equal('should have failed');
+			expect(err.code).to.eq(403);
+		}
+	});
+
+	it('patch does not return the secret', async () => {
+		const teacher = await testObjects.createTestUser({ roles: 'teacher' });
+		const tool = await testObjects.createTestLtiTool();
+		await testObjects.createTestCourse({ teacherIds: [teacher._id], ltiToolIds: [tool._id] });
+
+		const params = await generateRequestParamsFromUser(teacher);
+		const result = await app.service('ltiTools').patch(tool._id, { name: 'toolChanged' }, params);
+		expect(result.secret).to.be.undefined;
+	});
+
+	it('students can not delete the tools in their own courses', async () => {
+		try {
+			const student = await testObjects.createTestUser({ roles: 'student' });
+			const tool = await testObjects.createTestLtiTool();
+			await testObjects.createTestCourse({ userIds: [student._id], ltiToolIds: [tool._id] });
+
+			const params = await generateRequestParamsFromUser(student);
+			await app.service('ltiTools').delete(tool._id, params);
+			throw new Error('should have failed');
+		} catch (err) {
+			expect(err.message).to.not.equal('should have failed');
+			expect(err.code).to.eq(403);
+		}
+	});
+
+	it('teachers can delete the tools in their own courses', async () => {
+		const teacher = await testObjects.createTestUser({ roles: 'teacher' });
+		const tool = await testObjects.createTestLtiTool();
+		await testObjects.createTestCourse({ teacherIds: [teacher._id], ltiToolIds: [tool._id] });
+
+		const params = await generateRequestParamsFromUser(teacher);
+		const result = await app.service('ltiTools').delete(tool._id, params);
+		expect(result).to.not.be.undefined;
+	});
+
+	it('teachers can not delete the tools in foreign courses', async () => {
+		try {
+			const courseTeacher = await testObjects.createTestUser({ roles: 'teacher' });
+			const otherTeacher = await testObjects.createTestUser({ roles: 'teacher' });
+			const tool = await testObjects.createTestLtiTool();
+			await testObjects.createTestCourse({ teacherIds: [courseTeacher._id], ltiToolIds: [tool._id] });
+
+			const params = await generateRequestParamsFromUser(otherTeacher);
+			await app.service('ltiTools').delete(tool._id, params);
+			throw new Error('should have failed');
+		} catch (err) {
+			expect(err.message).to.not.equal('should have failed');
+			expect(err.code).to.eq(403);
+		}
 	});
 });
