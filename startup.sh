6f41f0b0d3dbff7bb8a4c3324b92275fe923c785
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2021-01-27 15:22:43 +0100

    Add storage provider workflow

--- startup.sh
+++ startup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Seeding the database
+# cat ./backup/setup/storageprovider.json | sed -e "s/<AWS_ACCESS_KEY_ID>/$AWS_ACCESS_KEY_ID/g" | sed -e "s/<AWS_SECRET_ACCESS_KEY>/$AWS_SECRET_ACCESS_KEY/g" > ./backup/setup/storageprovider.json
+sed -i "s/<AWS_ACCESS_KEY_ID>/$AWS_ACCESS_KEY_ID/g" ./backup/setup/storageprovider.json
+sed -i "s/<AWS_SECRET_ACCESS_KEY>/$AWS_SECRET_ACCESS_KEY/g" ./backup/setup/storageprovider.json
 node ./backup.js -p setup/ import
 
 npm start
