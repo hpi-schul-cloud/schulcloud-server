e35a4c382ee4a567eafd378258954aa066a734dd
Alexander Riese <alexander.riese@capgemini.com>
2021-01-28 14:37:32 +0100

    SC-8499 delte pseudonym documents

--- migrations/1611829684290-addUniquePseudonymIndex.js
+++ migrations/1611829684290-addUniquePseudonymIndex.js
@@ -7,9 +7,6 @@ const { connect, close } = require('../src/utils/database');
 // How to use more than one schema per collection on mongodb
 // https://stackoverflow.com/questions/14453864/use-more-than-one-schema-per-collection-on-mongodb
 
-// TODO npm run migration-persist and remove this line
-// TODO update seed data and remove this line
-
 const syncIndexes = async () => {
 	await connect();
 	alert('start updating pseudonym indexes...');
@@ -20,42 +17,12 @@ const syncIndexes = async () => {
 
 module.exports = {
 	up: async function up() {
-		const duplicates = [];
-		alert('start searching for duplicated pseudonyms');
-		const duplicatedDocuments = pseudonymModel
-			.aggregate([
-				{
-					$group: {
-						_id: { toolId: '$toolId', userId: '$userId' },
-						dups: { $addToSet: '$_id' },
-						count: { $sum: 1 },
-					},
-				},
-				{
-					$match: {
-						count: { $gt: 1 },
-					},
-				},
-			])
-			.cursor()
-			.exec();
-		alert('finished searching for duplicated pseudonyms');
-
-		alert('start collecting documents which should be deleted');
-		alert(duplicatedDocuments);
-		duplicatedDocuments.eachAsync((document) => {
-			document.dups.shift(); // First element skipped for deleting
-			document.dups.forEach((id) => {
-				duplicates.push(id);
-			});
-		});
-		alert('finished collecting documents which should be deleted');
-
-		alert('start deleting duplicated pseudonyms');
-		pseudonymModel.remove({ _id: { $in: duplicates }});
-		alert('finished deleting duplicated pseudonyms');
-
+		await connect();
+		alert('start deleting existing pseudonyms');
+		await pseudonymModel.deleteMany({});
+		alert('finished deleting existing pseudonyms');
 		await syncIndexes();
+		await close();
 	},
 
 	down: async function down() {
