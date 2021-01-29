086e596cd2eff9743961dd484a74e0b6cac96281
Tratori <FabianMahling@web.de>
2021-01-29 17:11:13 +0100

    forward file to wallet and send message

--- src/services/wallet/index.js
+++ src/services/wallet/index.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 const FormData = require('form-data');
 const logger = require('../../logger');
 const hooks = require('./hooks');
+const { Readable } = require('stream');
 
 const apiToken = require('../../../config/secrets').IDAS_API_KEY;
 
@@ -157,25 +158,21 @@ class WalletFileService {
 	async create(data, params) {
 
 		const form = new FormData();
+		form.append('file', params.file.buffer, {filename: data.title});
 		form.append('title', data.title);
+		form.append('expiresAt', new Date(Date.now() + 1000*60*60).toISOString())
 		form.append('description', data.description);
-		form.append('file', params.file.buffer.toString('binary'));
-
-		logger.info(form.getHeaders());
-		logger.info(JSON.stringify(form));
-
-		return data;
-
-		/*
-		//TODO: multipart/form-data has to be used
-		const file = await axios.post("https://daad.idas.solutions/api/v1/Files", data, {
+		
+		const file = await axios.post("https://daad.idas.solutions/api/v1/Files",
+			form, 
+			{
 			headers: {
+				...form.getHeaders(),
 				"X-API-KEY": apiToken,
-				"Content-Type": "multipart/form-data"
 			}
-		})
+		}).catch((error) => {logger.error(error.response)});
 
-		logger.info(file.data.result)
+		logger.info(file.data.result);
 
 		const fileID = file.data.result.id;
 
@@ -183,7 +180,7 @@ class WalletFileService {
 		const user = await this.app.service('users').get(userId);
 
 		const relationshipId = user.relationshipId;
-
+		logger.info(`RelationshipID: ${relationshipId}`)
 		const relationship = await axios.get("https://daad.idas.solutions/api/v1/Relationships/" + relationshipId, {
 			headers: {
 				"X-API-KEY": apiToken
@@ -215,8 +212,6 @@ class WalletFileService {
 		logger.info(message.data.result);
 
 		return message.data.result;
-
-		 */
 	}
 }
 
