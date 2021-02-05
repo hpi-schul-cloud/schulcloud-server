1e33c90becc76a80dec5db8988e04ea6ace992bf
Alexander Riese <alexander.riese@capgemini.com>
2021-02-05 15:34:45 +0100

    SC-7868 add personal file deletion in use case layer

--- src/components/fileStorage/uc/applicationInternal/removeFiles.js
+++ src/components/fileStorage/uc/applicationInternal/removeFiles.js
@@ -12,12 +12,12 @@ const removePersonalFiles = async (userId) => {
 		scope: 'files',
 		data: personalFiles,
 	};
-	const personalFileIds = personalFiles.map((file) => file._id);
 
+	const personalFileIds = personalFiles.map((file) => file._id);
 	const storageType = 'awsS3'; // ToDo: get storage type from school of the given user
 	const fileStorageStrategy = getFileStorageStrategy(storageType);
 
-	const complete = fileStorageStrategy.moveFilesToTrash(personalFileIds) && repo.deleteFilesByIds(personalFileIds);
+	const complete = fileStorageStrategy.moveFilesToTrash(personalFileIds) && repo.removePersonalFilesByUserId(userId);
 
 	return { trashBinData, complete };
 };
--- src/components/fileStorage/uc/deleteUserData.uc.unit.test.js
+++ src/components/fileStorage/uc/deleteUserData.uc.unit.test.js
@@ -3,15 +3,60 @@ const sinon = require('sinon');
 const { ObjectId } = require('mongoose').Types;
 
 const { removePermissionsThatUserCanAccess } = require('./applicationInternal/removePermissions');
+const { removePersonalFiles } = require('./applicationInternal/removeFiles');
 
 const fileRepo = require('../repo/files.repo');
+const fileStrategy = require('../repo/strategies');
 const { GeneralError } = require('../../../errors');
 
-describe('deletedUserData.uc.unit', () => {
+describe.only('deletedUserData.uc.unit', () => {
 	afterEach(sinon.restore);
 
-	describe('removePermissionsThatUserCanAccess', () => {
+	describe('removePersonalFiles', () => {
+		it('when the function is called with valid user id, then it returns with valid trash bin format', async () => {
+			const userId = new ObjectId();
+			const removePersonalFilesStub = sinon.stub(fileRepo, 'removePersonalFilesByUserId');
+			removePersonalFilesStub.withArgs(userId).returns(true);
+			const getPersonalFilesStub = sinon.stub(fileRepo, 'getPersonalFilesByUserId');
+			getPersonalFilesStub.withArgs(userId).returns([
+				{
+					_id: 1,
+					name: 'Dummy',
+					isDirectory: false,
+					owner: userId,
+					creator: userId,
+				},
+			]);
+			const createStrategyStub = sinon.stub(fileStrategy, 'createStrategy');
+			createStrategyStub.withArgs('awsS3').returns({ moveFilesToTrash: () => true });
 
+			const result = await removePersonalFiles(userId);
+
+			expect(result.complete).to.be.true;
+			expect(result.trashBinData).to.be.an('object');
+			expect(result.trashBinData).to.haveOwnProperty('scope');
+			expect(result.trashBinData).to.haveOwnProperty('data');
+		});
+
+		it('when the function is called with a user id from a user who has no personal files the result is empty', async () => {
+			const userId = new ObjectId();
+			const removePersonalFilesStub = sinon.stub(fileRepo, 'removePersonalFilesByUserId');
+			removePersonalFilesStub.withArgs(userId).returns(true);
+			const getPersonalFilesStub = sinon.stub(fileRepo, 'getPersonalFilesByUserId');
+			getPersonalFilesStub.withArgs(userId).returns([]);
+			const createStrategyStub = sinon.stub(fileStrategy, 'createStrategy');
+			createStrategyStub.withArgs('awsS3').returns({ moveFilesToTrash: () => true });
+
+			const result = await removePersonalFiles(userId);
+
+			expect(result.complete).to.be.true;
+			expect(result.trashBinData).to.be.an('object');
+			expect(result.trashBinData).to.haveOwnProperty('scope');
+			expect(result.trashBinData).to.haveOwnProperty('data');
+		});
+	});
+
+	describe('removePermissionsThatUserCanAccess', () => {
 		it('when the function is called with valid user id, then it returns with valid trash bin format', async () => {
 			const userId = new ObjectId();
 			const removeFilePermissionStub = sinon.stub(fileRepo, 'removeFilePermissionsByUserId');
@@ -63,25 +108,6 @@ describe('deletedUserData.uc.unit', () => {
 			getFilePermissionStub.withArgs(userId).rejects(new GeneralError());
 
 			expect(removePermissionsThatUserCanAccess(userId)).to.be.rejected;
-		});
-	});
-
-	describe('removePersonalFiles', () => {
-		it('when the function is called with valid user id, then it returns with valid trash bin format', async () => {
-		});
-		it('when the function is called with a user id, which is not connected with a file, the result is empty', async () => {
-			const userId = new ObjectId();
-			const removeFilePermissionStub = sinon.stub(fileRepo, 'removePersonalFilesByUserId');
-			removeFilePermissionStub.withArgs(userId).returns(true);
-			const getFilePermissionStub = sinon.stub(fileRepo, 'getPersonalFilesByUserId');
-			getFilePermissionStub.withArgs(userId).returns([]);
-
-			const result = await removePermissionsThatUserCanAccess(userId);
-
-			expect(result.complete).to.be.true;
-			expect(result.trashBinData).to.be.an('object');
-			expect(result.trashBinData).to.haveOwnProperty('scope');
-			expect(result.trashBinData).to.haveOwnProperty('data');
 		});
 	});
 });
