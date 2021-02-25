a122a6ef0e61dce68a5fda5b2a53dfa7a2b769f0
Alexander Riese <alexander.riese@capgemini.com>
2021-02-25 17:28:56 +0100

    SC-7868 remove unused setStorageProvider method

--- src/components/school/repo/school.repo.js
+++ src/components/school/repo/school.repo.js
@@ -9,15 +9,8 @@ const getTombstoneSchool = async () => School.findOne({ purpose: SCHOOL_OF_DELET
 const setTombstoneUser = async (schoolId, tombstoneUserId) =>
 	School.findByIdAndUpdate(schoolId, { tombstoneUserId }, { new: true }).lean().exec();
 
-const setStorageProvider = async (schoolId, storageProviderId, session) =>
-	School.findByIdAndUpdate(schoolId, { $set: { storageProvider: storageProviderId } })
-		.session(session)
-		.lean()
-		.exec();
-
 module.exports = {
 	getSchool,
 	getTombstoneSchool,
 	setTombstoneUser,
-	setStorageProvider,
 };
--- src/components/school/uc/school.facade.js
+++ src/components/school/uc/school.facade.js
@@ -14,10 +14,6 @@ class SchoolFacade {
 		checkPermissions(currentUser, schoolId, ['STUDENT_DELETE', 'TEACHER_DELETE'], 'OR');
 		return schoolUc.setTombstoneUser(schoolId, tombstoneUserId);
 	}
-
-	async setStorageProvider(schoolId, storageProviderId, session) {
-		return schoolUc.setStorageProvider(schoolId, storageProviderId, session);
-	}
 }
 
 module.exports = function setupSchoolFacade(app) {
