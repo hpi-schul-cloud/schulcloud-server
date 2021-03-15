e35ede06b66be0ea03817b458b0cdf6ad856c8b0
Thomas Feldtkeller <thomas.feldtkeller@hpi.de>
2021-03-15 15:04:05 +0100

    add tests for hidden lesson contents

--- test/services/lessons/lessonsContentService.test.js
+++ test/services/lessons/lessonsContentService.test.js
@@ -116,5 +116,70 @@ describe('Lesson Content Service', () => {
 				expect(err.message).to.equal('requires a user in the query');
 			}
 		});
+
+		it('finds a nexboard that is visible', async () => {
+			const user = await testObjects.createTestUser();
+			const params = await testObjects.generateRequestParamsFromUser(user);
+			params.query = {
+				type: 'neXboard',
+				user: user._id,
+			}
+			params.route = {
+				type: 'neXboard'
+			}
+
+			const course = await testObjects.createTestCourse({ userIds: [user._id]});
+			const lesson = await testObjects.createTestLesson({ courseId: course._id })
+			await app.service('lessons').patch(lesson._id, { $push: { contents: {
+				title: 'testcontent',
+				component: 'neXboard',
+				hidden: false,
+				content: {
+					title:"sdfasd",
+					description:"sdfasdf",
+					board:"25380",
+					url:"https://nexboard.nexenio.com/app/client/pub/25380/someid"
+				}
+			} } })
+
+			const result = await app.service('/lessons/contents/:type/').find(
+				params
+			);
+			
+			expect(result.length).to.equal(1);
+			expect(result[0].content.title).to.equal('sdfasd');
+		});
+
+		it('does not find a nexboard that is visible', async () => {
+			const user = await testObjects.createTestUser();
+			const params = await testObjects.generateRequestParamsFromUser(user);
+			params.query = {
+				type: 'neXboard',
+				user: user._id,
+			}
+			params.route = {
+				type: 'neXboard'
+			}
+
+			const course = await testObjects.createTestCourse({ userIds: [user._id]});
+			const lesson = await testObjects.createTestLesson({ courseId: course._id })
+			await app.service('lessons').patch(lesson._id, { $push: { contents: {
+				title: 'testcontent',
+				component: 'neXboard',
+				hidden: true,
+				content: {
+					title:"sdfasd",
+					description:"sdfasdf",
+					board:"25380",
+					url:"https://nexboard.nexenio.com/app/client/pub/25380/someid"
+				}
+			} } })
+
+			const result = await app.service('/lessons/contents/:type/').find(
+				params
+			);
+			
+			expect(result.length).to.equal(0);
+		});
 	});
 });
