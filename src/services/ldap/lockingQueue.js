6bf8c409102bdbabcc47d06c9d4ecb3acd2a3037
Philipp Kleybolte <philipp.kleybolte@capgemini.com>
2021-03-29 15:24:14 +0200

    add js doc for LockingQueue

--- src/services/ldap/lockingQueue.js
+++ src/services/ldap/lockingQueue.js
@@ -1,3 +1,19 @@
+
+/**
+ * Used for locking a resource. The LockingQueue has no reference to the resource itself.
+ * getLock() will return a promise, that resolves when it is the clients's turn to use the resource.
+ * 
+ * Once the resource is no longer needed releaseLock() needs to be called. The next client's promise will be resolved.
+ * 
+ * E.g.
+ * client 1 calls getLock() and is returned a promise that resolves immediatly.
+ * client 2 calls getLock() and is returned a promise that stays unresolved for now.
+ * client 3 calls getLock() and is returned a promise that stays unresolved for now.
+ * client 1 calls releaseLock(). client 2's promise resolves.
+ * client 2 calls releaseLock(). client 3's promise resolves.
+ * client 3 calls releaseLock().
+ * client 4 calls getLock() and is returned a promise that resolves immediatly.
+ */
 class LockingQueue {
 	constructor() {
 		this.locked = false;
