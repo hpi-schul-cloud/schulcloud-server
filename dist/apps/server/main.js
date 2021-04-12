cd60c0cf069e344c051431b0f2057fbf25da7d73
Tobias Wollowski <tobias.wollowski@hpi.de>
2021-04-12 13:05:09 +0200

    let nest start

--- dist/apps/server/main.js
+++ dist/apps/server/main.js
@@ -52,9 +52,14 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var _a;
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.ServerController = void 0;
 const common_1 = __webpack_require__(3);
+const server_service_1 = __webpack_require__(5);
 let ServerController = class ServerController {
     constructor(serverService) {
         this.serverService = serverService;
@@ -64,10 +69,14 @@ let ServerController = class ServerController {
     }
 };
 __decorate([
-    common_1.Get()
+    common_1.Get(),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", []),
+    __metadata("design:returntype", String)
 ], ServerController.prototype, "getHello", null);
 ServerController = __decorate([
-    common_1.Controller()
+    common_1.Controller(),
+    __metadata("design:paramtypes", [typeof (_a = typeof server_service_1.ServerService !== "undefined" && server_service_1.ServerService) === "function" ? _a : Object])
 ], ServerController);
 exports.ServerController = ServerController;
 
