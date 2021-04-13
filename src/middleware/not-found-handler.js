fda546cfee595ec2affa86b69d3e58203fb78e9e
Tobias Wollowski <tobias.wollowski@hpi.de>
2021-04-13 15:59:10 +0200

    let nest handle not found to enable nest routes

--- src/middleware/not-found-handler.js
+++ src/middleware/not-found-handler.js
@@ -1,5 +1,5 @@
 const { PageNotFound } = require('../errors');
 // todo do not work well with error handling, it is produce a 404 and pass it 500er Internal Error
-module.exports = (req, res, next) => {
-	next(new PageNotFound());
-};
+module.exports = (req, res, next) =>
+	// next(new PageNotFound()); enabling would disable NestJS we handle this inside of the NestJS app
+	next();
