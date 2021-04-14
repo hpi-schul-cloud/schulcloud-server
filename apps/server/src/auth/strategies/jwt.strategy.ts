07e646bd5b715ff8a882048c41d2064263dc81a4
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2021-04-14 18:58:29 +0200

    fix directory naming

--- apps/server/src/auth/strategies/jwt.strategy.ts
+++ apps/server/src/auth/strategies/jwt.strategy.ts
@@ -2,7 +2,7 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { jwtConstants } from '../constants';
-import { JwtPayload } from '../Interfaces/JwtPayload';
+import { JwtPayload } from '../interfaces/jwt-payload';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
