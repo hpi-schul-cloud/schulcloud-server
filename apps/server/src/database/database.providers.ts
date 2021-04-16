be0c71f0bb8e92ee386ccdc0dd37386167adad41
Tobias Wollowski <tobias.wollowski@hpi.de>
2021-04-16 12:21:30 +0200

    database provider added

---
+++ apps/server/src/database/database.providers.ts
@@ -0,0 +1,8 @@
+import * as mongoose from 'mongoose';
+
+export const databaseProviders = [
+	{
+		provide: 'DATABASE_CONNECTION',
+		useFactory: async (): Promise<typeof mongoose> => await mongoose.connect('mongodb://localhost/schulcloud'),
+	},
+];
