c3ad7f1e2f37abc102ea6ebf201b8a1601a7be31
Tobias Wollowski <tobias.wollowski@hpi.de>
2021-04-22 10:13:25 +0200

    add hints in the database provider

--- apps/server/src/database/database.providers.ts
+++ apps/server/src/database/database.providers.ts
@@ -4,7 +4,8 @@ export const databaseProviders = [
 	{
 		provide: 'DATABASE_CONNECTION',
 		useFactory: () => {
-			//	return await mongoose.connect('mongodb://localhost/schulcloud')
+			// TODO return await mongoose.connect('mongodb://localhost/schulcloud')
+			// we use the existing connection now
 			return mongoose;
 		},
 	},
