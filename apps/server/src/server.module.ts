a114b01293d1c23f17f2169ed7b8880a93b23c72
Tobias Wollowski <tobias.wollowski@hpi.de>
2021-04-22 11:14:56 +0200

    remove mongoose from server module

--- apps/server/src/server.module.ts
+++ apps/server/src/server.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { NewsModule } from './news/news.module';
 import { ServerController } from './server.controller';
@@ -7,7 +6,7 @@ import { ServerService } from './server.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-	imports: [AuthModule, UsersModule, NewsModule, MongooseModule.forRoot('mongodb://localhost/schulcloud')],
+	imports: [AuthModule, UsersModule, NewsModule],
 	controllers: [ServerController],
 	providers: [ServerService],
 })
