6a1acf026d1105e076f94ccef9ade1268f942f23
Tobias Wollowski <tobias.wollowski@hpi.de>
2021-04-22 10:36:52 +0200

    added parse objectid pipe

--- apps/server/src/news/news.controller.ts
+++ apps/server/src/news/news.controller.ts
@@ -16,6 +16,8 @@ import { ApiTags } from '@nestjs/swagger';
 import { NewsEntity } from './entities/news.entity';
 import { Authenticate, CurrentUser } from '../auth/auth.decorator';
 import { ICurrentUser } from '../auth/interfaces/jwt-payload';
+import { ParseObjectIdPipe } from './parse-object-id.pipe';
+import { ObjectId } from 'mongoose';
 
 @ApiTags('News')
 @Authenticate('jwt')
@@ -34,15 +36,28 @@ export class NewsController {
 		return this.newsService.findAll(currentUser);
 	}
 
+	/**
+	 * Retrieve a specific news entry by id.
+	 * The current user must be part of related scope.
+	 * */
 	@Get(':id')
-	findOne(@Param('id') newsId: string, @CurrentUser() currentUser: ICurrentUser): Promise<Partial<NewsEntity>> {
-		const {userId}=currentUser;
-		return this.newsService.getByIdForUserId(newsId, userId);
+	async findOne(
+		// A parameter pipe like ParseObjectIdPipe gives us the guarantee of typesafety for @Param
+		@Param('id', ParseObjectIdPipe) newsId: ObjectId,
+		@CurrentUser() currentUser: ICurrentUser
+	): Promise<NewsEntity> {
+		const { userId } = currentUser;
+		const news = await this.newsService.getOne(newsId, userId);
+		return news;
 	}
 
 	@Patch(':id')
-	update(@Param('id') id: string, @Body() updateNewsDto: UpdateNewsDto) {
-		return this.newsService.update(id, updateNewsDto);
+	update(
+		@Param('id', ParseObjectIdPipe)
+		newsId: ObjectId,
+		@Body() updateNewsDto: UpdateNewsDto
+	) {
+		return this.newsService.update(newsId, updateNewsDto);
 	}
 
 	@Delete(':id')
