c77728e21564995d3b4b221af54ae4631a5b60f0
j_jmnz <jmnz.jesus@gmail.com>
2021-04-29 12:30:11 +0200

    add tests

---
+++ test/services/config/hooks/services/configServiceV2.test.js
@@ -0,0 +1,32 @@
+const { expect } = require('chai');
+const appPromise = require('../../../../../src/app');
+const { Configuration } = require('@hpi-schul-cloud/commons');
+
+describe.only('ConfigServiceV2', () => {
+	let app;
+	let server;
+	let configService;
+
+	const envsToTest = ['ADMIN_TABLES_DISPLAY_CONSENT_COLUMN'];
+
+	before(async () => {
+		app = await appPromise;
+		configService = app.service('/config/v2');
+		server = await app.listen(0);
+	});
+
+	after(async () => {
+		await server.close();
+	});
+
+	it('is properly registered', () => {
+		expect(configService).to.not.equal(undefined);
+	});
+
+	it('returns environment variables', async () => {
+		const serviceEnvs = await configService.find();
+		const testEnvs = {};
+		envsToTest.forEach((env) => (testEnvs[env] = Configuration.get(env)));
+		expect(serviceEnvs).to.eql(testEnvs);
+	});
+});
