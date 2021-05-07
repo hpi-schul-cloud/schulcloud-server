1438fa558f1113b85ed76503a7a7d58d4be0d1d8
Tobias Wollowski <tobias.wollowski@hpi.de>
2021-05-07 14:15:50 +0200

    make pagination query params optional

--- apps/server/src/shared/core/controller/dto/pagination.query.dto.ts
+++ apps/server/src/shared/core/controller/dto/pagination.query.dto.ts
@@ -5,10 +5,10 @@ export class PaginationQueryDto {
 	@IsInt()
 	@Min(0)
 	@ApiProperty({ description: 'number of elements (not pages) to be skipped' })
-	skip: number = 0;
+	skip?: number = 0;
 	@IsInt()
 	@Min(1)
 	@Max(100)
 	@ApiProperty({ description: 'Page limit, defaults to 10.', minimum: 1, maximum: 99 })
-	limit: number = 10; // TODO dice
+	limit?: number = 10;
 }
