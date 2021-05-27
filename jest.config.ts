73e0f5a03ad23be034aa15114a0ee1e7e2e3b33a
Tobias Wollowski <tobias.wollowski@hpi.de>
2021-05-27 17:55:58 +0200

    align jest to use tsconfig path

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+// jest.config.ts
+import type { Config } from '@jest/types';
+
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig.json');
+
+// Sync object
+const config: Config.InitialOptions = {
+	moduleFileExtensions: ['js', 'json', 'ts'],
+	rootDir: '.',
+	testRegex: '.*\\.spec\\.ts$',
+	transform: {
+		'^.+\\.(t|j)s$': 'ts-jest',
+	},
+	collectCoverageFrom: ['**/*.(t|j)s'],
+	coverageDirectory: './coverage',
+	testEnvironment: 'node',
+	roots: ['<rootDir>/apps/'],
+	moduleNameMapper: {
+		// add ts-config path's here as regex
+		'^@shared/(.*)$': '<rootDir>/apps/server/src/shared/$1',
+	},
+};
+export default config;
