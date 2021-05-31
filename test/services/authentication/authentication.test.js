661158c7df94f99bdbb7fda25958088dce4c5bab
Tobias Wollowski <tobias.wollowski@hpi.de>
2021-05-31 15:04:11 +0200

    cleanup async authentication tests

--- test/services/authentication/authentication.test.js
+++ test/services/authentication/authentication.test.js
@@ -12,12 +12,10 @@ chai.use(chaiHttp);
 describe('start server', () => {
 	let app;
 	let accountService;
-	let logger;
 
 	let server;
 	before(async () => {
 		app = await appPromise;
-		({ logger } = app);
 		accountService = app.service('accounts');
 		server = await app.listen(0);
 	});
@@ -37,87 +35,49 @@ describe('start server', () => {
 			testObjects.createTestUser().then((testUser) => testObjects.createTestAccount(testAccount, null, testUser))
 		);
 
-		it('should get a JWT which includes accountId', () =>
-			new Promise((resolve, reject) => {
-				chai
-					.request(app)
-					.post('/authentication')
-					.set('Accept', 'application/json')
-					.set('content-type', 'application/x-www-form-urlencoded')
-					// send credentials
-					.send({
-						username: testAccount.username,
-						password: testAccount.password,
-						strategy: 'local',
-					})
-					.end((err, res) => {
-						if (err) {
-							reject(err);
-							return;
-						}
+		it('should get a JWT which includes accountId', async () => {
+			const res = await chai
+				.request(app)
+				.post('/authentication')
+				.set('Accept', 'application/json')
+				.set('content-type', 'application/x-www-form-urlencoded')
+				// send credentials
+				.send({
+					username: testAccount.username,
+					password: testAccount.password,
+					strategy: 'local',
+				});
 
-						const decodedToken = jwt.decode(res.body.accessToken);
+			const decodedToken = jwt.decode(res.body.accessToken);
 
-						// get the account id from JWT
-						decodedToken.should.have.property('accountId');
+			// get the account id from JWT
+			decodedToken.should.have.property('accountId');
 
-						accountService
-							.get(decodedToken.accountId)
-							.then((account) => {
-								account.username.should.equal(testAccount.username);
-								resolve();
-							})
-							.catch((error) => {
-								logger.error(`failed to get the account from the service: ${error}`);
-								// throw error;
-								reject(error);
-								// done();
-							});
+			const account = await accountService.get(decodedToken.accountId);
+			account.username.should.equal(testAccount.username);
+		});
 
-						resolve();
-					});
-			}));
+		it('should get a JWT when credentials are padded in spaces', async () => {
+			const res = await chai
+				.request(app)
+				.post('/authentication')
+				.set('Accept', 'application/json')
+				.set('content-type', 'application/x-www-form-urlencoded')
+				// send credentials
+				.send({
+					username: `     ${testAccount.username} `,
+					password: `  ${testAccount.password} `,
+					strategy: 'local',
+				});
 
-		it('should get a JWT when credentials are padded in spaces', () =>
-			new Promise((resolve, reject) => {
-				chai
-					.request(app)
-					.post('/authentication')
-					.set('Accept', 'application/json')
-					.set('content-type', 'application/x-www-form-urlencoded')
-					// send credentials
-					.send({
-						username: `     ${testAccount.username} `,
-						password: `  ${testAccount.password} `,
-						strategy: 'local',
-					})
-					.end((err, res) => {
-						if (err) {
-							reject(err);
-							return;
-						}
+			const decodedToken = jwt.decode(res.body.accessToken);
 
-						const decodedToken = jwt.decode(res.body.accessToken);
+			// get the account id from JWT
+			decodedToken.should.have.property('accountId');
 
-						// get the account id from JWT
-						decodedToken.should.have.property('accountId');
-
-						accountService
-							.get(decodedToken.accountId)
-							.then((account) => {
-								account.username.should.equal(testAccount.username);
-								resolve();
-							})
-							.catch((error) => {
-								logger.error(`failed to get the account from the service: ${error}`);
-								// throw error;
-								reject(error);
-								// done();
-							});
-
-						resolve();
-					});
-			}));
+			const account = await accountService.get(decodedToken.accountId);
+			account.username.should.equal(testAccount.username);
+		});
 
 		it('should not get a JWT with wrong credentials', () =>
 			new Promise((resolve, reject) => {
@@ -157,91 +117,62 @@ describe('start server', () => {
 				Configuration.set('BLOCK_DISPOSABLE_EMAIL_DOMAINS', originalConfiguration);
 			});
 
-			it('should be blocked if activated', () =>
-				new Promise((resolve, reject) => {
-					Configuration.set('BLOCK_DISPOSABLE_EMAIL_DOMAINS', true);
-					const testDisposableAccount = {
-						username: `${Date.now()}poweruser@my10minutemail.com`,
-						password: 'passwordA',
-					};
+			it('should be blocked if activated', async () => {
+				Configuration.set('BLOCK_DISPOSABLE_EMAIL_DOMAINS', true);
+				const testDisposableAccount = {
+					username: `${Date.now()}poweruser@my10minutemail.com`,
+					password: 'passwordA',
+				};
 
-					testObjects
-						.createTestUser()
-						.then((testUser) => testObjects.createTestAccount(testDisposableAccount, null, testUser))
-						.then(() => {
-							chai
-								.request(app)
-								.post('/authentication')
-								.set('Accept', 'application/json')
-								.set('content-type', 'application/x-www-form-urlencoded')
-								// send credentials
-								.send({
-									username: testDisposableAccount.username,
-									password: testDisposableAccount.password,
-									strategy: 'local',
-								})
-								.end((err, res) => {
-									if (err) {
-										reject(err);
-										return;
-									}
+				await testObjects
+					.createTestUser()
+					.then((testUser) => testObjects.createTestAccount(testDisposableAccount, null, testUser));
+				const res = await chai
+					.request(app)
+					.post('/authentication')
+					.set('Accept', 'application/json')
+					.set('content-type', 'application/x-www-form-urlencoded')
+					// send credentials
+					.send({
+						username: testDisposableAccount.username,
+						password: testDisposableAccount.password,
+						strategy: 'local',
+					});
 
-									// Bad Request
-									should.equal(res.status, 400);
-									should.equal(res.body.message, 'EMAIL_DOMAIN_BLOCKED');
+				// Bad Request
+				should.equal(res.status, 400);
+				should.equal(res.body.message, 'EMAIL_DOMAIN_BLOCKED');
+			});
 
-									resolve();
-								});
-						});
-				}));
+			it('should not be blocked if deactivated', async () => {
+				Configuration.set('BLOCK_DISPOSABLE_EMAIL_DOMAINS', false);
+				const testDisposableAccount = {
+					username: `${Date.now()}poweruser@my10minutemail.com`,
+					password: 'passwordA',
+				};
 
-			it('should not be blocked if deactivated', () =>
-				new Promise((resolve, reject) => {
-					Configuration.set('BLOCK_DISPOSABLE_EMAIL_DOMAINS', false);
-					const testDisposableAccount = {
-						username: `${Date.now()}poweruser@my10minutemail.com`,
-						password: 'passwordA',
-					};
+				await testObjects
+					.createTestUser()
+					.then((testUser) => testObjects.createTestAccount(testDisposableAccount, null, testUser));
+				const res = await chai
+					.request(app)
+					.post('/authentication')
+					.set('Accept', 'application/json')
+					.set('content-type', 'application/x-www-form-urlencoded')
+					// send credentials
+					.send({
+						username: testDisposableAccount.username,
+						password: testDisposableAccount.password,
+						strategy: 'local',
+					});
 
-					testObjects
-						.createTestUser()
-						.then((testUser) => testObjects.createTestAccount(testDisposableAccount, null, testUser))
-						.then(() => {
-							chai
-								.request(app)
-								.post('/authentication')
-								.set('Accept', 'application/json')
-								.set('content-type', 'application/x-www-form-urlencoded')
-								// send credentials
-								.send({
-									username: testDisposableAccount.username,
-									password: testDisposableAccount.password,
-									strategy: 'local',
-								})
-								.end((err, res) => {
-									if (err) {
-										reject(err);
-										return;
-									}
-
-									// Works
-									should.equal(res.status, 201);
-									resolve();
-								});
-						});
-				}));
+				// Works
+				should.equal(res.status, 201);
+			});
 		});
 
-		after((done) => {
-			testObjects
-				.cleanup()
-				.then(() => {
-					done();
-				})
-				.catch((error) => {
-					logger.error(`Could not remove test account(s): ${error}`);
-					done();
-				});
+		after(async () => {
+			await testObjects.cleanup();
 		});
 	});
 });
