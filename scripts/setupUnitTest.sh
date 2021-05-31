007853b329d530c9936294e3401db4143f1a18e3
Evers <git@it-4d.de>
2021-05-31 22:56:49 +0200

    rename and cleanup

--- scripts/setupUnitTest.sh
+++ scripts/setupUnitTest.sh
@@ -4,11 +4,17 @@ set -e # fail with exit 1 on any error
 
 echo $( bash pwd)
 
+# Preconditions
 mkdir -p .build
 openssl aes-256-cbc -K "$encrypted_0ddd2445e49f_key" -iv "$encrypted_0ddd2445e49f_iv" -in travis_rsa.enc -out .build/travis_rsa -d
 
 echo "$MY_DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
 
+# Envirments
+ROCKET_CHAT_URI=http://localhost:5000
+echo "ROCKET_CHAT_URI" $ROCKET_CHAT_URI
+
+# Execute
 curl https://raw.githubusercontent.com/hpi-schul-cloud/schulcloud-authorization-server/master/docker-compose-test.yml > docker-compose-oauthserver.yml
 curl https://raw.githubusercontent.com/hpi-schul-cloud/schulcloud-authorization-server/master/.env.example > .env
 sudo docker-compose -f docker-compose-oauthserver.yml up -d
