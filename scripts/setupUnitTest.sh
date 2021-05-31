1e1225fffa90dc7f9ccba05b6ae76a13e7557466
Evers <git@it-4d.de>
2021-05-31 22:24:17 +0200

    create directory

--- scripts/setupUnitTest.sh
+++ scripts/setupUnitTest.sh
@@ -4,6 +4,7 @@ set -e # fail with exit 1 on any error
 
 echo $( bash pwd)
 
+mkdir -p .build
 openssl aes-256-cbc -K "$encrypted_bce910623bb2_key" -iv "$encrypted_bce910623bb2_iv" -in travis_rsa.enc -out .build/travis_rsa -d
 
 echo "$MY_DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
