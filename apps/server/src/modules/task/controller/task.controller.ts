dd86a70b7238776b47e7d1e1033f79fc97652829
Thomas Feldtkeller <thomas.feldtkeller@hpi.de>
2021-06-04 11:18:51 +0200

    use shared pagination types

--- apps/server/src/modules/task/controller/task.controller.ts
+++ apps/server/src/modules/task/controller/task.controller.ts
@@ -3,8 +3,9 @@ import { ApiTags } from '@nestjs/swagger';
 import { ICurrentUser } from '../../authentication/interface/jwt-payload';
 import { TaskUC } from '../uc/task.uc';
 import { Controller, Get, Query } from '@nestjs/common';
-import { TaskResponseDto, TaskQueryDto } from './dto';
+import { TaskResponseDto } from './dto';
 import { PaginationResponse } from '../../../shared/core/controller/dto/pagination.response.dto';
+import { PaginationQueryDto } from '../../../shared/core/controller/dto/pagination.query.dto';
 
 // TODO: swagger doku do not read from combined query object only from passed single parameter in Query(), but this do not allowed optional querys only required querys
 
@@ -17,9 +18,12 @@ export class TaskController {
 	@Get('dashboard')
 	async findAll(
 		@CurrentUser() currentUser: ICurrentUser,
-		@Query() query: TaskQueryDto
+		@Query() paginationQuery: PaginationQueryDto
 	): Promise<PaginationResponse<TaskResponseDto[]>> {
-		const { data, limit, offset: skip, total } = await this.taskUc.findAllOpenForUser(currentUser.userId, query);
+		const { data, limit, offset: skip, total } = await this.taskUc.findAllOpenForUser(
+			currentUser.userId,
+			paginationQuery
+		);
 		return new PaginationResponse(data, limit, skip, total);
 	}
 }
--- apps/server/src/modules/task/uc/task.uc.ts
+++ apps/server/src/modules/task/uc/task.uc.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { PaginationQueryDto } from '../../../shared/core/controller/dto/pagination.query.dto';
 import { Paginated } from '../../../shared/core/types/paginated';
-import { ITaskOption, EntityId, ITaskMetadata } from '../entity';
+import { EntityId, ITaskMetadata } from '../entity';
 import { TaskRepo } from '../repo/task.repo';
 
 // filter tasks older than 3 weeks
@@ -8,7 +9,7 @@ import { TaskRepo } from '../repo/task.repo';
 export class TaskUC {
 	constructor(private taskRepo: TaskRepo) {}
 
-	async findAllOpenForUser(userId: EntityId, option: ITaskOption): Promise<Paginated<ITaskMetadata[]>> {
-		return this.taskRepo.findAllOpenByStudent(userId, option);
+	async findAllOpenForUser(userId: EntityId, pagination: PaginationQueryDto): Promise<Paginated<ITaskMetadata[]>> {
+		return this.taskRepo.findAllOpenByStudent(userId, pagination);
 	}
 }
