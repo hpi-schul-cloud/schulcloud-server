378cbab741a09ab15bbfdb0f3551ec6353bbec4c
Tobias Wollowski <tobias.wollowski@hpi.de>
2021-06-25 14:01:13 +0200

    revert

--- src/services/news/events/index.js
+++ src/services/news/events/index.js
@@ -2,15 +2,12 @@ const logger = require('../../../logger');
 
 const removeTeamNews = (app) => async (deletedTeam) => {
 	try {
-		const now = new Date();
-		console.log('before...');
 		await app.service('/newsModel').remove(null, {
 			query: {
 				target: deletedTeam._id,
 				targetModel: 'teams',
 			},
 		});
-		console.log('after...', now - Date.now(), 'ms');
 	} catch (e) {
 		logger.warning(`Cannot remove news for team ${deletedTeam._id}`, e);
 	}
