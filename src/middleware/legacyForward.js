dd036e6d7591c6f3ba416b954b44b92d0772a50d
virgil <virgil.chiriac@hpi.de>
2021-06-29 10:40:38 +0200

    fix lint

--- src/middleware/legacyForward.js
+++ src/middleware/legacyForward.js
@@ -1,6 +1,5 @@
-const urlContainsVersion = (url) => {
-	return url.search(/\/v\d\//) > -1;
-};
+const urlContainsVersion = (url) => url.search(/\/v\d\//) > -1;
+
 module.exports = (req, res, next) => {
 	if (!urlContainsVersion(req.url)) {
 		req.url = `/legacy/v1${req.url}`;
