426b46028de929305cf656e47045411f57025575
Tobias Wollowski <tobias.wollowski@hpi.de>
2021-07-02 13:38:31 +0200

    SC-9130_s3_whitelist_filename_fix_wip

--- src/services/fileStorage/utils/filePathHelper.js
+++ src/services/fileStorage/utils/filePathHelper.js
@@ -1,9 +1,18 @@
 const removeLeadingSlash = (path) => (path[0] === '/' ? path.substring(1) : path);
 
-const generateFileNameSuffix = (fileName) => {
+const whitelistFileName = (fileName) => {
+	// see https://docs.aws.amazon.com/AmazonS3/latest/userguide/object-keys.html for valid characters
+	// the regex below reduces allowed special chars to hyphen, underscore, period
 	// eslint-disable-next-line no-useless-escape
-	const newString = fileName.replace(/[';:=#*+\[\]~<{\\()}>§$%&|^£@±?!"`□\s]/g, '-');
-	return `${Date.now()}-${newString.toLowerCase()}`;
+	const result = fileName.replace(/[^0-9A-Za-z\-_.]+/g, '-').toLowerCase();
+	return result;
+};
+
+const generateFileNameSuffix = (fileName) => {
+	const whitelistedFileName = whitelistFileName(fileName);
+	const now = Date.now();
+	const result = `${now}-${whitelistedFileName}`;
+	return result;
 };
 
 const returnFileType = (fileName) =>
@@ -28,6 +37,7 @@ const returnFileType = (fileName) =>
 
 module.exports = {
 	removeLeadingSlash,
+	whitelistFileName,
 	generateFileNameSuffix,
 	returnFileType,
 };
---
+++ test/services/fileStorage/utils/filePathHelper.test.js
@@ -0,0 +1,64 @@
+const chai = require('chai');
+
+const { expect } = chai;
+
+const {
+	generateFileNameSuffix,
+	whitelistFileName,
+} = require('../../../../src/services/fileStorage/utils/filePathHelper');
+
+describe.only('filePathHelper', () => {
+	describe('when fileNames are whitelisted using whitelistFileName', () => {
+		it('should keep alphanumeric characters', () => {
+			const lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz';
+			const lowerCaseResult = whitelistFileName(lowerCaseChars);
+			expect(lowerCaseResult).to.be.equal(lowerCaseChars);
+
+			const numbers = '0123456789';
+			const numbersResult = whitelistFileName(numbers);
+			expect(numbersResult).to.be.equal(numbers);
+		});
+
+		it('should lowercase alphabetical characters', () => {
+			const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+			const lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz';
+			const result = whitelistFileName(upperCaseChars);
+			expect(result).to.be.equal(lowerCaseChars);
+		});
+
+		it('should keep allowed special chars', () => {
+			const allowedSpecialChars = '-_.';
+			const result = whitelistFileName(allowedSpecialChars);
+			expect(result).to.be.equal(allowedSpecialChars);
+		});
+
+		it('should replace spaces with minus', () => {
+			const spacedName = 'hello to the world';
+			const result = whitelistFileName(spacedName);
+			expect(result).to.be.equal('hello-to-the-world');
+		});
+
+		it('should replace all other characters from samples', () => {
+			// url: slash and double-point with minus
+			const urlResult = whitelistFileName('https://youtube.com:1234/watch.exe');
+			expect(urlResult).to.be.equal('https-youtube.com-1234-watch.exe');
+			// todo all others?
+			const allReplacedSample =
+				'🴋🪭🔆🀚🮿🇍🅣🨚📟🻣🸴🇟📲🇧🇛🹨🞺😲🬦🌸🧖🅋🁍🔋📿🲣😿🳥🤞🶄🎙🺳🏶🌧🭤🯙🵉🍭🯌🍏🟋🍴🙈🬆🊥🕾🥁🙇🟿🊅🣑🦑🔩🯭🊳🀷🏮🞱🡆🧮🭹🅋🢣🚉🡓🣬🲥🚯🥳🡄🣒🝵🁜🻫🔿🨤🰎🏾🔠🖒🬦🆾🂻🥿🣜🧀🞞🜹🝲🵆🹸🈨🵩🢎🢀🳬🬡🻖🕘🃷🋢🔆🟠🀵🴴🂛🻪🕒🞠🫊🡃🭗🊫🫯💱🌟🋐🮽🥼🗑🔻😧🰳🡴🵊🇝🏎🶀🯠🫓🟀🹱🞊🅰🷅🋉🭊🶙🌟🸺🩊🏚🤻🹜🺡🌸🕇🔰🚑🦂🠒🇛🔢👌🁴🧖😔🷏🱻🇥😪🋱🡢🴘🞟🚂🅅🙸🟄🋵💓🍜🨒🲎🌌🰳🋙🱗🣋🏰🏂🖶🆤🙙🊳🩭🡑🞊🦼🛈🟕🛺🴵👠🟆🆾🺧🐌🇻🴻🼀🏔🫉🗀🸗🱐🛼🢔🦔😀🍍🶆🩴🨗🩼🹚🹯🤋😇🚾🐘🕝🨬🩞🉖📉🖢🡏🢡🴈🔰📁🸺🃐🪝🀘🨄🌜🺍🋽🱵🬪🖑🜐🮴🍕🐞😄🐒🜪🏉🕞🧽🚆🙲🸸🲈🁩🠜😜🣬🤪🏛🻮🤽🄴🻦🉳🯪🝜🥸🊚🦞🲫🟀🃄🇙🰗🃧👳🕧🋣🁬🮅🣅🪱🶡🦾🦕🚸💴🅊🢅🻃🮨🦦🯨🜖👁🞌';
+			const allReplacedResult = whitelistFileName(allReplacedSample);
+			// TODO expect - only
+		});
+	});
+
+	describe('when filenames are prefixed using generateFileNameSuffix', () => {
+		it('should add current time stamp in front of filename', () => {
+			const suffixedFileName = generateFileNameSuffix('sampleFileName.ext');
+			const now = Date.now();
+			const fileDateSuffix = suffixedFileName.split('-')[0]; // extract timestamp suffix
+			const timestamp = Number.parseInt(fileDateSuffix);
+			expect(timestamp).to.be.a('number');
+			const timespan = now - timestamp;
+			expect(timespan, 'expect a small value, when filename generation just happened').to.be.a('number').lessThan(100);
+		});
+	});
+});
