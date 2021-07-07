d31aa2a46f71856dcdad1cb75cf4afc16e2371cd
Alexander Riese <alexander.riese@capgemini.com>
2021-07-07 15:05:48 +0200

    SC-9157 add tests for mail service

--- apps/server/src/modules/mail/mail.service.spec.ts
+++ apps/server/src/modules/mail/mail.service.spec.ts
@@ -1,19 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MailService } from './mail.service';
-import { MailModule } from './mail.module';
+import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
+import { Mail } from './interfaces/mail';
 
-describe.skip('MailService', () => {
+describe('MailService', () => {
 	let service: MailService;
+	let amqpConnection: AmqpConnection;
+
+	const mailServiceOptions = {
+		exchange: 'exchange',
+		routingKey: 'routingKey',
+	};
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [MailModule, MailService],
+			providers: [
+				MailService,
+				{ provide: AmqpConnection, useValue: { publish: () => {} } },
+				{ provide: 'MAIL_SERVICE_OPTIONS', useValue: mailServiceOptions },
+			],
 		}).compile();
 
-		service = module.get<MailService>(MailService);
+		service = module.get(MailService);
+		amqpConnection = module.get(AmqpConnection);
 	});
 
-	it.todo('should be defined', () => {
+	it('should be defined', () => {
 		expect(service).toBeDefined();
 	});
+
+	it('should send given data to queue', () => {
+		const data: Mail = { mail: { plainTextContent: 'content', subject: 'Test' }, recipients: ['test@example.com']};
+		const amqpConnectionSpy = jest.spyOn(amqpConnection, 'publish');
+		service.send(data);
+		const expectedParams = [mailServiceOptions.exchange, mailServiceOptions.routingKey, data];
+		expect(amqpConnectionSpy).toHaveBeenCalledWith(...expectedParams);
+	})
 });
