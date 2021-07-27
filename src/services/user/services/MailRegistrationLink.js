68bd5570effeccfe93a36889d8086dc1e866267d
Alexander Riese <alexander.riese@capgemini.com>
2021-07-27 12:14:38 +0200

    SC-9053 improve error handling

--- src/services/user/services/MailRegistrationLink.js
+++ src/services/user/services/MailRegistrationLink.js
@@ -1,11 +1,10 @@
 /* eslint-disable no-await-in-loop */
 const { authenticate } = require('@feathersjs/authentication').hooks;
 
-const { Forbidden, BadRequest } = require('../../../errors');
+const { BadRequest } = require('../../../errors');
 const { hasPermission } = require('../../../hooks');
 
 const { SC_SHORT_TITLE, SC_TITLE } = require('../../../../config/globals');
-const logger = require('../../../logger');
 
 const mailContent = (firstName, lastName, registrationLink) => {
 	const mail = {
@@ -56,46 +55,41 @@ class SendRegistrationLinkService {
 		});
 		accounts = accounts.map((account) => String(account.userId));
 
-		// dont't send mail if user has account
-		mailUsers = mailUsers.filter((u) => !accounts.includes(u._id.toString()));
+		const mailUsersWithoutAccount = mailUsers.filter((u) => !accounts.includes(u._id.toString()));
 
-		for (const mailUser of mailUsers) {
-			try {
-				const mailUserRole = !mailUser.roles[0].name
-					? await this.app.service('/roles').get(mailUser.roles[0])
-					: mailUser.roles[0];
+		for (const mailUser of mailUsersWithoutAccount) {
+			const mailUserRole = !mailUser.roles[0].name
+				? await this.app.service('/roles').get(mailUser.roles[0])
+				: mailUser.roles[0];
 
-				const { shortLink } = await this.app.service('/registrationlink').create({
-					role: mailUserRole,
-					save: true,
-					patchUser: true,
-					schoolId: mailUser.schoolId,
-					toHash: mailUser.email,
-				});
+			const { shortLink } = await this.app.service('/registrationlink').create({
+				role: mailUserRole,
+				save: true,
+				patchUser: true,
+				schoolId: mailUser.schoolId,
+				toHash: mailUser.email,
+			});
 
-				// send mail
-				const { subject, content } = mailContent(mailUser.firstName, mailUser.lastName, shortLink);
-				await this.app.service('/mails').create({
-					email: mailUser.email,
-					subject,
-					content,
-				});
+			// send mail
+			const { subject, content } = mailContent(mailUser.firstName, mailUser.lastName, shortLink);
+			await this.app.service('/mails').create({
+				email: mailUser.email,
+				subject,
+				content,
+			});
 
-				if (!(mailUser.preferences || {}).registrationMailSend) {
-					const updatedPreferences = mailUser.preferences || {};
-					updatedPreferences.registrationMailSend = true;
-					await this.app.service('users').patch(mailUser._id, { preferences: updatedPreferences }, params);
-				}
-				totalMailsSend += 1;
-			} catch (err) {
-				logger.warning(`registration link mail for user ${mailUser._id.toString()} could not be sent`, err);
+			if (!(mailUser.preferences || {}).registrationMailSend) {
+				const updatedPreferences = mailUser.preferences || {};
+				updatedPreferences.registrationMailSend = true;
+				await this.app.service('users').patch(mailUser._id, { preferences: updatedPreferences }, params);
 			}
+			totalMailsSend += 1;
 		}
 
 		return {
-			totalReceivedIds: userIds.length + users.length,
+			totalReceivedIds: mailUsers,
 			totalMailsSend,
-			alreadyRegisteredUsers: userIds.length + users.length - totalMailsSend,
+			alreadyRegisteredUsers: mailUsers - totalMailsSend,
 		};
 	}
 
