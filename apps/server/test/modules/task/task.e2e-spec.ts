861c037d900a7db904e62c9ff824b8e728fde7a9
Evers <git@it-4d.de>
2021-08-20 17:02:49 +0200

    Add e2e test for task.

--- apps/server/test/modules/task/task.e2e-spec.ts
+++ apps/server/test/modules/task/task.e2e-spec.ts
@@ -103,7 +103,20 @@ describe('Task Controller (e2e)', () => {
 			const response = await request(app.getHttpServer()).get('/task/dashboard');
 			expect(response.status).toEqual(200);
 		});
-		// remove data?
+
+		it('[FIND] /task/dashboard can open it', async () => {
+			const response = await request(app.getHttpServer()).get('/task/dashboard');
+
+			const paginatedResult = response.body as PaginationResponse<TaskResponse[]>;
+
+			expect(paginatedResult).toEqual({
+				total: 0,
+				data: [],
+				limit: 10,
+				skip: 0,
+			});
+		});
+
 		it('[FIND] /task/dashboard return tasks that include the appropriate information.', async () => {
 			const helperL = new LearnroomTestHelper();
 			const helper = new TaskTestHelper();
@@ -126,9 +139,116 @@ describe('Task Controller (e2e)', () => {
 			expect(paginatedResult.data[0]).toHaveProperty('name');
 		});
 
-		it('[FIND] /task/dashboard return a list of', async () => {});
+		it('[FIND] /task/dashboard return tasks that include the appropriate information.', async () => {
+			const helperL = new LearnroomTestHelper();
+			const helper = new TaskTestHelper();
+			const user = helper.getFirstUser() as UserTaskInfo;
+			const student = helper.getOtherUser() as UserTaskInfo;
+			modifiedCurrentUserId(currentUser, user);
+			helperL.createAndAddUser(user);
 
-		// write test for pagination
+			const parent = helperL.createTeacherCourse();
+			const task = helper.createTask(parent.id);
+			task.changePrivate(false);
+			const submission = helper.createSubmission(task, student);
+
+			await em.persistAndFlush([task, parent, user, student, submission]);
+
+			const response = await request(app.getHttpServer()).get('/task/dashboard');
+			const paginatedResult = response.body as PaginationResponse<TaskResponse[]>;
+
+			expect(paginatedResult.data[0]).toBeDefined();
+			expect(paginatedResult.data[0].status).toEqual({
+				submitted: 1,
+				maxSubmissions: parent.getStudentsNumber(),
+				graded: 0,
+			});
+		});
+
+		it('[FIND] /task/dashboard return a list of tasks', async () => {
+			const helperL = new LearnroomTestHelper();
+			const helper = new TaskTestHelper();
+			const user = helper.getFirstUser() as UserTaskInfo;
+			modifiedCurrentUserId(currentUser, user);
+			helperL.createAndAddUser(user);
+
+			const parent = helperL.createTeacherCourse();
+			const task1 = helper.createTask(parent.id);
+			const task2 = helper.createTask(parent.id);
+			const task3 = helper.createTask(parent.id);
+			task1.changePrivate(false);
+			task2.changePrivate(false);
+			task3.changePrivate(false);
+
+			await em.persistAndFlush([task1, task2, task3, parent, user]);
+
+			const response = await request(app.getHttpServer()).get('/task/dashboard');
+			const paginatedResult = response.body as PaginationResponse<TaskResponse[]>;
+
+			expect(paginatedResult.total).toEqual(3);
+		});
+
+		it('[FIND] /task/dashboard return a list of tasks from multiple parents', async () => {
+			const helperL = new LearnroomTestHelper();
+			const helper = new TaskTestHelper();
+			const user = helper.getFirstUser() as UserTaskInfo;
+			modifiedCurrentUserId(currentUser, user);
+			helperL.createAndAddUser(user);
+
+			const parent1 = helperL.createTeacherCourse();
+			const parent2 = helperL.createTeacherCourse();
+			const parent3 = helperL.createTeacherCourse();
+			const task1 = helper.createTask(parent1.id);
+			const task2 = helper.createTask(parent2.id);
+			// parent3 has no task
+			task1.changePrivate(false);
+			task2.changePrivate(false);
+
+			await em.persistAndFlush([task1, task2, parent1, parent2, parent3, user]);
+
+			const response = await request(app.getHttpServer()).get('/task/dashboard');
+			const paginatedResult = response.body as PaginationResponse<TaskResponse[]>;
+
+			expect(paginatedResult.total).toEqual(2);
+		});
+
+		it('[FIND] /task/dashboard should not return private tasks', async () => {
+			const helperL = new LearnroomTestHelper();
+			const helper = new TaskTestHelper();
+			const user = helper.getFirstUser() as UserTaskInfo;
+			modifiedCurrentUserId(currentUser, user);
+			helperL.createAndAddUser(user);
+
+			const parent = helperL.createTeacherCourse();
+			const task = helper.createTask(parent.id);
+			task.changePrivate(true);
+
+			await em.persistAndFlush([task, parent, user]);
+
+			const response = await request(app.getHttpServer()).get('/task/dashboard');
+			const paginatedResult = response.body as PaginationResponse<TaskResponse[]>;
+
+			expect(paginatedResult.total).toEqual(0);
+		});
+
+		it('[FIND] /task/dashboard should nothing return from parent where the user has read permissions', async () => {
+			const helperL = new LearnroomTestHelper();
+			const helper = new TaskTestHelper();
+			const user = helper.getFirstUser() as UserTaskInfo;
+			modifiedCurrentUserId(currentUser, user);
+			helperL.createAndAddUser(user);
+
+			const parent = helperL.createStudentCourse();
+			const task = helper.createTask(parent.id);
+			task.changePrivate(false);
+
+			await em.persistAndFlush([task, parent, user]);
+
+			const response = await request(app.getHttpServer()).get('/task/dashboard');
+			const paginatedResult = response.body as PaginationResponse<TaskResponse[]>;
+
+			expect(paginatedResult.total).toEqual(0);
+		});
 	});
 
 	describe('as user with read permissions in parents', () => {});
