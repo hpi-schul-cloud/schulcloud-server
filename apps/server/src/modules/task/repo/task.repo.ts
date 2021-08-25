53dd5b054c70be5891a3974adf4aa5c69cedb357
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2021-08-25 09:05:41 +0200

    remove unused code

--- apps/server/src/modules/task/repo/task.repo.ts
+++ apps/server/src/modules/task/repo/task.repo.ts
@@ -58,60 +58,6 @@ export class TaskRepo {
 		return countedTaskList;
 	}
 
-	// // TODO: rename
-	// async findAllByStudent(
-	// 	parentIds: EntityId[],
-	// 	{ limit, skip }: IPagination = {},
-	// 	ignoredTasks: EntityId[] = []
-	// ): Promise<Counted<Task[]>> {
-	// 	const lessonsOfStudent = await this.em.find(LessonTaskInfo, {
-	// 		courseId: { $in: parentIds },
-	// 		hidden: false,
-	// 	});
-
-	// 	// TODO: date sounds like domain logic if this go out, after it student and teacher has only different order logic
-	// 	// that also sound like it should manage over scope helper in uc.
-	// 	const oneWeekAgo = new Date();
-	// 	oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
-	// 	const [usersTasks, total] = await this.em.findAndCount(
-	// 		Task,
-	// 		{
-	// 			$and: [
-	// 				{ id: { $nin: ignoredTasks } },
-	// 				{ private: { $ne: true } },
-	// 				{ courseId: { $in: parentIds } },
-	// 				{ $or: [{ lesson: null }, { lesson: { $in: lessonsOfStudent } }] },
-	// 				{ $or: [{ dueDate: { $gte: oneWeekAgo } }, { dueDate: null }] },
-	// 			],
-	// 		},
-	// 		// TODO extract pagination, oderby
-	// 		{ limit, offset: skip, orderBy: { dueDate: QueryOrder.ASC } }
-	// 	);
-
-	// 	return [usersTasks, total];
-	// }
-
-	// // TODO: rename
-	// async findAllAssignedByTeacher(parentIds: EntityId[], { limit, skip }: IPagination = {}): Promise<Counted<Task[]>> {
-	// 	const publishedLessons = await this.em.find(LessonTaskInfo, {
-	// 		courseId: { $in: parentIds },
-	// 		hidden: false,
-	// 	});
-
-	// 	const [usersTasks, count] = await this.em.findAndCount(
-	// 		Task,
-	// 		{
-	// 			$and: [
-	// 				{ courseId: { $in: parentIds } },
-	// 				{ private: { $ne: true } },
-	// 				{ $or: [{ lesson: null }, { lesson: { $in: publishedLessons } }] },
-	// 			],
-	// 		}, // TODO: { createdAt: QueryOrder.DESC } vs orderBy: { dueDate: QueryOrder.ASC } teacher vs student
-	// 		{ limit, offset: skip, orderBy: { createdAt: QueryOrder.DESC } }
-	// 	);
-	// 	return [usersTasks, count];
-	// }
-
 	private async findTasksAndCount(query: FilterQuery<Task>, options?: IFindOptions<Task>): Promise<Counted<Task[]>> {
 		const { pagination, order } = options || {};
 		const [taskEntities, count] = await this.em.findAndCount(Task, query, {
