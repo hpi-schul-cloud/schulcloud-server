535d111ddea074175519b2296e873a8b5018ddf1
Alexander Riese <alexander.riese@capgemini.com>
2021-08-26 17:35:13 +0200

    BC-121 first working version

--- scripts/deleteExpiredData.js
+++ scripts/deleteExpiredData.js
@@ -2,15 +2,16 @@ const aws = require('aws-sdk');
 const CryptoJS = require('crypto-js');
 const { Configuration } = require('@hpi-schul-cloud/commons');
 
-const { error } = require('../src/logger');
-const { facadeLocator } = require('../../../utils/facadeLocator');
+const { alert, error } = require('../src/logger');
+const { facadeLocator } = require('../src/utils/facadeLocator');
 
 const appPromise = require('../src/app');
 
 const trashbinModel = require('../src/components/user/repo/db/trashbin.schema');
 const fileStorageProviderRepo = require('../src/components/fileStorage/repo/fileStorageProvider.repo');
 
-const backupPeriod = 7 * 24 * 60 * 60 * 1000 // 7 days
+const backupPeriodThreshold = new Date()
+backupPeriodThreshold.setDate(backupPeriodThreshold.getDate() - 0);
 
 const decryptAccessKey = (secretAccessKey) => {
 	const S3_KEY = Configuration.get('S3_KEY');
@@ -18,76 +19,88 @@ const decryptAccessKey = (secretAccessKey) => {
 };
 
 const filesToBeDeletedAggregate = [
-    // filter by data older then the defined backup period
-    {
-        '$match': {
-            'createdAt': {
-                '$lt': new Date(Date.now - backupPeriod)
-            }
-        }
-    },
-    // unwind the trashbin data of all users
-    {
-        '$unwind': {
-            'path': '$data'
-        }
-    },
-    // discard unneeded data
-    {
-        '$project': {
-            'data': 1
-        }
-    },
-    // filter by trashbin data regarding file deletion
-    {
-        '$match': {
-            'data.scope': 'files'
-        }
-    },
-    // unwind files
-    {
-        '$unwind': {
-            'path': '$data.data'
-        }
-    }
+	// filter by data older then the defined backup period
+	{
+		$match: {
+			createdAt: {
+				$lt: backupPeriodThreshold,
+			},
+		},
+	},
+	// unwind the trashbin data of all users
+	{
+		$unwind: {
+			path: '$data',
+		},
+	},
+	// discard unneeded data
+	{
+		$project: {
+			data: 1,
+		},
+	},
+	// filter by trashbin data regarding file deletion
+	{
+		$match: {
+			'data.scope': 'files',
+		},
+	},
+	// unwind files
+	{
+		$unwind: {
+			path: '$data.data',
+		},
+	},
 ];
 
 const getSchoolIdForFile = async (file) => {
-    const userId = (file.refOwnerModel === 'user') && owner;
-    if (!userId) return null;
+	const userId = file.refOwnerModel === 'user' && file.owner;
+	if (!userId) return null;
 
-    const trashbinObject = await trashbinModel.find({userId: userId});
-    const userData = trashbinObject.data.find(d => d.scope === 'user');
-    if (!userData) return null;
+	const trashbinObjects = await trashbinModel.find({ userId: userId });
+	if (trashbinObjects.length < 1) return null;
 
-    return userData.data.schoolId;
+	const userData = trashbinObjects[0].data.find((d) => d.scope === 'user');
+	if (!userData) return null;
+
+	return userData.data.schoolId;
 };
 
 const deleteFiles = async () => {
-    const schoolFacade = facadeLocator.facade('school/v2');
+	const schoolFacade = facadeLocator.facade('school/v2');
 
-    const filesToBeDeleted = trashbinModel.aggregate(filesToBeDeletedAggregate);
-    for await (const file of filesToBeDeleted) {
-        const fileData = file.data.data;
-        const schoolId = await getSchoolIdForFile(fileData);
-        if (!bucket) {
-            // ToDo: Error handling
-            continue;
-        }
-        const storageProviderId = await schoolFacade.getStorageProviderIdForSchool(schoolId);
-        const storageProviderInfo = await fileStorageProviderRepo.getStorageProviderMetaInformation(storageProviderId);
-        storageProviderInfo.secretAccessKey = decryptAccessKey(storageProviderInfo.secretAccessKey);
-        const storageProvider = fileStorageProviderRepo.private.createStorageProviderInstance(storageProviderInfo);
+	const filesToBeDeleted = trashbinModel.aggregate(filesToBeDeletedAggregate);
+	for await (const file of filesToBeDeleted) {
+		const fileData = file.data.data;
+		alert('file to be deleted: ' + fileData.storageFileName);
 
-        const bucketName = `bucket-${schoolId}`;
+		const schoolId = await getSchoolIdForFile(fileData);
+		if (!schoolId) {
+			// ToDo: Error handling
+			continue;
+		}
+		const storageProviderId = await schoolFacade.getStorageProviderIdForSchool(schoolId);
+		const storageProviderInfo = await fileStorageProviderRepo.getStorageProviderMetaInformation(storageProviderId);
+		// storageProviderInfo.secretAccessKey = decryptAccessKey(storageProviderInfo.secretAccessKey);
+		const storageProvider = fileStorageProviderRepo.private.createStorageProviderInstance(storageProviderInfo);
 
-        await storageProvider.deleteObject({'Bucket': bucketName, Key: fileData.storageFileName});
-    }
+		const bucketName = `bucket-${schoolId}`;
+
+        const fileName = `expiring_${fileData.storageFileName}`;
+		alert('going to delete file ' + fileName + ' in bucket ' + bucketName);
+
+		await storageProvider.deleteObject({ Bucket: bucketName, Key: fileName }).promise();
+	}
 };
 
-
 appPromise
-    .then(async () => {
-        await deleteFiles();
-        await trashbinModel.deleteMany({ createdAt: {'$lt': new Date(Date.now - backupPeriod)} })
-    })
+	.then(async () => {
+		alert('start script');
+		await deleteFiles();
+		// await trashbinModel.deleteMany({ createdAt: { $lt: backupPeriodThreshold }});
+        return process.exit(0);
+	})
+	.catch((error) => {
+		console.error(error);
+		return process.exit(1);
+	});
