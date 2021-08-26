d6deb93d7c5db69cbae4ba911ad73556cb40e452
Tobias W <tobias.wollowski@hpi.de>
2021-08-26 17:57:37 +0200

    BC-44 - integrate jwt whitelist check in nestjs jwt authentication (#2924)
    * wip
    
    * apply some legacy renaming
    
    * BC-44 add jwt whitelist check to nestjs
    
    * refactor names
    
    * add missing await
    
    * add changelog
    
    * documentation cleanup
    
    * update import
    
    * cleanup
    
    * test whitelisting
    
    * add strategy spec
    
    * remove dependencies to em
    
    * generics cleanup in spec

--- apps/server/src/modules/authentication/auth.module.ts
+++ apps/server/src/modules/authentication/auth.module.ts
@@ -4,15 +4,10 @@ import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategy/jwt.strategy';
 import { jwtConstants } from './constants';
 import { UserModule } from '../user';
-// import { AuthController } from './controller/auth.controller';
-// import { LocalStrategy } from './strategy/local.strategy';
+import { JwtValidationAdapter } from './strategy/jwt-validation.adapter';
 
 @Module({
 	imports: [PassportModule, JwtModule.register(jwtConstants), UserModule],
-	providers: [JwtStrategy],
-	exports: [],
-	controllers: [
-		/* AuthController */
-	],
+	providers: [JwtStrategy, JwtValidationAdapter],
 })
 export class AuthModule {}
---
+++ apps/server/src/modules/authentication/strategy/jwt-validation.adapter.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+
+import jwtWhitelist = require('../../../../../../src/services/authentication/logic/whitelist');
+
+const { ensureTokenIsWhitelisted, addTokenToWhitelist, createRedisIdentifierFromJwtData } = jwtWhitelist;
+
+@Injectable()
+export class JwtValidationAdapter {
+	/**
+	 * When validating a jwt it must be added to a whitelist, here we check this.
+	 * When the jwt is validated, the expiration time will be extended with this call.
+	 * @param accountId users account id
+	 * @param jti jwt id (here required to make jwt identifiers identical in redis)
+	 */
+	async isWhitelisted(accountId: string, jti: string): Promise<void> {
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+		await ensureTokenIsWhitelisted(false, { accountId, jti, privateDevice: false });
+	}
+
+	async addToWhitelist(accountId: string, jti: string): Promise<void> {
+		const redisIdentifier = createRedisIdentifierFromJwtData(accountId, jti);
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+		await addTokenToWhitelist(redisIdentifier);
+	}
+}
