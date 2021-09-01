51183e1cd9f543692a81eca6583dcc9bec30adae
Tobias Wollowski <tobias.wollowski@hpi.de>
2021-09-01 12:11:38 +0200

    add comments

---
+++ apps/console/README.md
@@ -0,0 +1,7 @@
+# Admin console application
+
+# Start the application
+
+```bash
+    nest start console
+```
--- apps/console/src/main.ts
+++ apps/console/src/main.ts
@@ -1,12 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
 import { NestFactory } from '@nestjs/core';
 import { ConsoleModule } from './console.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(ConsoleModule);
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 	const server = await app.listen(3333);
+
+	// extend timeout to one minute for long running tasks
 	server.setTimeout(600000);
 }
 void bootstrap();
