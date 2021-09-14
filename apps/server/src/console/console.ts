91ce8e8631a560377716f62b3fc1b78edf761b74
Tobias Wollowski <tobias.wollowski@hpi.de>
2021-09-14 16:49:39 +0200

    lint

--- apps/server/src/console/console.ts
+++ apps/server/src/console/console.ts
@@ -13,6 +13,7 @@ void bootstrap.init().then(async (app) => {
 		await bootstrap.boot();
 		await app.close();
 	} catch (e) {
+		// eslint-disable-next-line no-console
 		console.error(e);
 		await app.close();
 		process.exit(1);
