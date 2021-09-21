798e3d5a0a4bbaca2616292cce801b673def8727
mamutmk5 <paul.poppe@dataport.de>
2021-09-21 09:35:53 +0200

    Update deltaSyncUtils.js

--- src/services/ldap/strategies/deltaSyncUtils.js
+++ src/services/ldap/strategies/deltaSyncUtils.js
@@ -10,20 +10,6 @@ const dateToLdapTimestamp = (date) => {
 };
 
 /**
- * Returns an LDAP filter to retrieve only recently updated entities (e.g. based on a
- * timestamp of a previous previous full sync). If the given attribute name does not
- * exist on the server, the resulting query gracefully falls back to all entities. The
- * logic is basically "search for all entities that either have a timestamp greater than
- * the given timestamp in the attribute with the given name, or don't have the attribute
- * at all".
- * @param {String} timestamp time of last modification, formatted as "YYYYMMDDHHmmssZ"
- * @param {String} attributeName optional attribute name to check
- */
-const getModifiedFilter = (timestamp, attributeName = 'modifyTimestamp') =>
-	// do unquel in combination with <= to get a > which is not supported by ldap
-	`(|(!(${attributeName}=*))(!(${attributeName}<=${timestamp})))`;
-
-/**
  * Returns an LDAP filter based on a given filter and a last change time. If lastChange is
  * undefined or does not match the correct format, the base filter is returned. Otherwise,
  * the base filter is combined with a filter for recently updated entities (i.e. updated
@@ -34,15 +20,10 @@ const getModifiedFilter = (timestamp, attributeName = 'modifyTimestamp') =>
  * @example
  *    filterForModifiedEntities(); // => ''
  *    filterForModifiedEntities('20201020000000Z', '(objectClass=person)');
- *    // => '(&(objectClass=person)(|(!(modifyTimestamp=*))(modifyTimestamp>=20201020000000Z)))'
+ *    // => '(objectClass=person)'
  */
 const filterForModifiedEntities = (school, existingFilter = '') => {
-	const lastChange = school && school.ldapLastSync;
-	if (!/\d{14}Z/.test(lastChange)) {
-		return existingFilter;
-	}
-	const filter = getModifiedFilter(lastChange);
-	return `(&${existingFilter}${filter})`;
+	return existingFilter;
 };
 
 module.exports = {
