f4a3fc0552db5a7ebcb91ea7d1ff01b4ddc29fd8
Tobias Wollowski <tobias.wollowski@hpi.de>
2021-10-06 12:21:12 +0200

    cleanup bootstrapping

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,6 +1,82 @@
-/* istanbul ignore file */
-import { bootstrap } from './bootstrap';
-import { ServerModule } from './server.module';
+/* eslint-disable no-console */
+import { NestFactory } from '@nestjs/core';
+import * as express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
-// start default application
-void bootstrap(ServerModule, 3030);
+// register source-map-support for debugging
+import { install as sourceMapInstall } from 'source-map-support';
+
+// application imports
+import { Logger } from '@nestjs/common';
+import { ServerModule } from './server.module';
+import legacyAppPromise = require('../../../src/app');
+import { enableOpenApiDocs } from './shared/controller/swagger';
+import { Mail } from './modules/mail/mail.interface';
+import { MailService } from './modules/mail/mail.service';
+
+async function bootstrap() {
+	sourceMapInstall();
+
+	// load the legacy feathers/express server
+	const feathersExpress = await legacyAppPromise;
+	feathersExpress.setup();
+
+	// create the NestJS application on a seperate express instance
+	const nestExpress = express();
+
+	// set reference to legacy app as an express setting so we can
+	// access it over the current request within FeathersServiceProvider
+	// TODO remove if not needed anymore
+	nestExpress.set('feathersApp', feathersExpress);
+
+	const nestExpressAdapter = new ExpressAdapter(nestExpress);
+	const nestApp = await NestFactory.create(ServerModule, nestExpressAdapter);
+
+	// customize nest app settings
+	nestApp.enableCors();
+	enableOpenApiDocs(nestApp, 'docs');
+
+	await nestApp.init();
+
+	// provide NestJS mail service to feathers app
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+	feathersExpress.services['nest-mail'] = {
+		async send(data: Mail): Promise<void> {
+			const mailService = nestApp.get(MailService);
+			await mailService.send(data);
+		},
+	};
+
+	// mount instances
+	const rootExpress = express();
+
+	// exposed alias mounts
+	rootExpress.use('/api/v1', feathersExpress);
+	rootExpress.use('/api/v3', nestExpress);
+
+	// logger middleware for deprecated paths
+	// TODO remove when all calls to the server are migrated
+	const logDeprecatedPaths = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+		Logger.error(req.path, undefined, 'DEPRECATED-PATH');
+		next();
+	};
+
+	// safety net for deprecated paths not beginning with version prefix
+	// TODO remove when all calls to the server are migrated
+	rootExpress.use('/api', logDeprecatedPaths, feathersExpress);
+	rootExpress.use('/', logDeprecatedPaths, feathersExpress);
+
+	const port = 3030;
+	rootExpress.listen(port);
+
+	console.log('#################################');
+	console.log(`### Start Server              ###`);
+	console.log(`### Port: ${port}                ###`);
+	console.log(`### Mounts                    ###`);
+	console.log(`### /api/v1 --> feathers      ###`);
+	console.log(`### /api/v3 --> nest          ###`);
+	console.log(`### /api    --> feathers      ###`);
+	console.log(`### /       --> feathers      ###`);
+	console.log('#################################');
+}
+void bootstrap();
--- apps/server/src/management.ts
+++ apps/server/src/management.ts
@@ -1,6 +1,44 @@
-/* istanbul ignore file */
-import { bootstrap } from './bootstrap';
-import { ManagementModule } from './modules/management/management.module';
+/* eslint-disable no-console */
+import { NestFactory } from '@nestjs/core';
+import * as express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
-// start management api
-void bootstrap(ManagementModule, 3333);
+// register source-map-support for debugging
+import { install as sourceMapInstall } from 'source-map-support';
+
+// application imports
+import { ManagementModule } from './modules/management/management.module';
+import { enableOpenApiDocs } from './shared/controller/swagger';
+
+async function bootstrap() {
+	sourceMapInstall();
+
+	// create the NestJS application on a seperate express instance
+	const nestExpress = express();
+
+	const nestExpressAdapter = new ExpressAdapter(nestExpress);
+	const nestApp = await NestFactory.create(ManagementModule, nestExpressAdapter);
+
+	// customize nest app settings
+	nestApp.enableCors();
+	enableOpenApiDocs(nestApp, 'docs');
+
+	await nestApp.init();
+
+	// mount instances
+	const rootExpress = express();
+
+	const port = 3333;
+	const basePath = '/api';
+
+	// exposed alias mounts
+	rootExpress.use(basePath, nestExpress);
+	rootExpress.listen(port);
+
+	console.log('#################################');
+	console.log(`### Start Management Server   ###`);
+	console.log(`### Port:     ${port}            ###`);
+	console.log(`### Base path: ${basePath}           ###`);
+	console.log('#################################');
+}
+void bootstrap();
