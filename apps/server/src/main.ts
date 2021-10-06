3589abe3a90e520760ed6068dba20a846bea0872
Tobias Wollowski <tobias.wollowski@hpi.de>
2021-10-06 11:25:30 +0200

    cleanup start logs

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -3,7 +3,4 @@ import { bootstrap } from './bootstrap';
 import { ServerModule } from './server.module';
 
 // start default application
-console.log('######################################################################');
-console.log('#####   start server module on port 3030                         #####');
-console.log('######################################################################');
 void bootstrap(ServerModule, 3030);
