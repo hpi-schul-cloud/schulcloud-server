db80446da100d13739f6e2f2a964015492e5c62f
Alexander Riese <alexander.riese@capgemini.com>
2021-10-07 17:07:29 +0200

    BC-215 add file controller test

--- apps/server/src/modules/files/controller/delete-files.controller.spec.ts
+++ apps/server/src/modules/files/controller/delete-files.controller.spec.ts
@@ -7,13 +7,19 @@ import { FilesRepo, FileStorageRepo } from '../repo';
 
 describe('DeleteFilesController', () => {
 	let controller: DeleteFilesController;
+	let deleteFilesUc: DeleteFilesUc;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
 			imports: [ConsoleModule, LoggerModule],
 			controllers: [DeleteFilesController],
 			providers: [
-				DeleteFilesUc,
+				{
+					provide: DeleteFilesUc,
+					useValue: {
+						removeDeletedFilesData: jest.fn,
+					},
+				},
 				{
 					provide: FilesRepo,
 					useValue: {},
@@ -26,9 +32,22 @@ describe('DeleteFilesController', () => {
 		}).compile();
 
 		controller = module.get<DeleteFilesController>(DeleteFilesController);
+		deleteFilesUc = module.get<DeleteFilesUc>(DeleteFilesUc);
 	});
 
 	it('should be defined', () => {
 		expect(controller).toBeDefined();
+	});
+
+	describe('removeDeletedFilesData', () => {
+		it('should call removeDeletedFilesData use case with removedSince date', async () => {
+			const removedSinceDays = 7;
+			const deleteFilesUcSpy = jest.spyOn(deleteFilesUc, 'removeDeletedFilesData');
+			await controller.removeDeletedFilesData(7);
+			expect(deleteFilesUcSpy).toHaveBeenCalledTimes(1);
+			const useCaseArg = deleteFilesUcSpy.mock.calls[0][0];
+			const dateDifferenceInDays = new Date(new Date().getTime() - useCaseArg.getTime()).getDate() - 1;
+			expect(dateDifferenceInDays).toEqual(removedSinceDays);
+		});
 	});
 });
