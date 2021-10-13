adafeee7997aedeee7fc23250006bea3ad47d03d
Alexander Riese <alexander.riese@capgemini.com>
2021-10-13 14:43:32 +0200

    BC-215 add dummy S3 key for development

--- apps/server/src/modules/files/files.module.ts
+++ apps/server/src/modules/files/files.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConsoleModule } from 'nestjs-console';
-import { DeleteFilesConsole } from './console/delete-files.console';
+import { DeleteFilesConsole } from './job/delete-files.console';
 import { LoggerModule } from '../../core/logger/logger.module';
 import { FilesRepo, FileStorageRepo } from './repo';
 import { DeleteFilesUc } from './uc';
