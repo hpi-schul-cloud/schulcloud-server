7643295f99c1703a7d0c543f48efebc331abbbf1
Alexander Riese <alexander.riese@capgemini.com>
2021-10-13 16:01:42 +0200

    BC-215 fix parameter handling

--- apps/server/src/modules/files/job/delete-files.console.ts
+++ apps/server/src/modules/files/job/delete-files.console.ts
@@ -12,19 +12,12 @@ export class DeleteFilesConsole {
 
 	@Command({
 		command: 'remove-deleted-files <days>',
-		options: [
-			{
-				flags: '-d, --days <days>',
-				required: true,
-				description: 'removed files that were marked for deletion <days> days ago',
-			},
-		],
 		description: 'cleanup job to remove files that were marked for deletion <days> days ago',
 	})
-	async removeDeletedFilesData(options: { days: number }): Promise<void> {
-		this.logger.log(`cleanup job will remove files that were marked for deletion ${options.days} days ago`);
+	async removeDeletedFilesData(removedSinceDays: number): Promise<void> {
+		this.logger.log(`cleanup job will remove files that were marked for deletion ${removedSinceDays} days ago`);
 		const removedSince = new Date();
-		removedSince.setDate(removedSince.getDate() - options.days);
+		removedSince.setDate(removedSince.getDate() - removedSinceDays);
 
 		await this.deleteFilesUc.removeDeletedFilesData(removedSince);
 		this.logger.log('cleanup job finished');
