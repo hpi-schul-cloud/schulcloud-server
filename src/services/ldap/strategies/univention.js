64cb5af3a97a5607a33c7887e5d0a72d6d8262da
mamutmk5 <paul.poppe@dataport.de>
2021-10-14 10:32:08 +0200

    Update univention.js

--- src/services/ldap/strategies/univention.js
+++ src/services/ldap/strategies/univention.js
@@ -13,7 +13,7 @@ class UniventionLDAPStrategy extends AbstractLDAPStrategy {
 	 */
 	getSchools() {
 		let ignoredSchools = '';
-		if ( this.config.providerOptions.ignoreSchools !== undefined ) {
+		if (this.config.providerOptions.ignoreSchools !== undefined) {
 			this.config.providerOptions.ignoreSchools.forEach((schoolOu) => {
 				ignoredSchools += `(!(ou=${schoolOu}))`;
 			});
