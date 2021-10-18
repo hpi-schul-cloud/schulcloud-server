c01feebc9a0111e4562e8837bb88ad8eb4da7b21
Tobias Wollowski <tobias.wollowski@hpi.de>
2021-10-18 15:15:31 +0200

    use custom type for serialization which encrypts the value

--- apps/server/src/shared/domain/entity/storageprovider.entity.ts
+++ apps/server/src/shared/domain/entity/storageprovider.entity.ts
@@ -1,6 +1,5 @@
-import { Configuration } from '@hpi-schul-cloud/commons';
 import { Entity, Property } from '@mikro-orm/core';
-import { SymetricKeyEncryptionService } from '@shared/infra/encryption';
+import { EncryptedStringType } from '@shared/repo/types/EncryptedString.type';
 import { BaseEntityWithTimestamps } from './base.entity';
 
 interface IStorageProviderProperties {
@@ -12,34 +11,23 @@ interface IStorageProviderProperties {
 
 @Entity({ tableName: 'storageproviders' })
 export class StorageProvider extends BaseEntityWithTimestamps {
-	private static encryptionService = Configuration.has('S3_KEY')
-		? new SymetricKeyEncryptionService(Configuration.get('S3_KEY'))
-		: undefined;
-
 	@Property()
 	endpointUrl!: string;
 
 	@Property()
 	accessKeyId!: string;
 
-	@Property({ fieldName: 'secretAccessKey' })
-	_secretAccessKey!: string;
+	@Property({ fieldName: 'secretAccessKey', type: EncryptedStringType })
+	secretAccessKey!: string;
 
 	@Property()
 	region?: string;
-
-	get secretAccessKey(): string {
-		if (!StorageProvider.encryptionService) {
-			throw new Error('Encryption service could not be created because no S3_KEY was specified.');
-		}
-		return StorageProvider.encryptionService.decrypt(this._secretAccessKey);
-	}
 
 	constructor(props: IStorageProviderProperties) {
 		super();
 		this.endpointUrl = props.endpointUrl;
 		this.accessKeyId = props.accessKeyId;
-		this._secretAccessKey = props.secretAccessKey;
+		this.secretAccessKey = props.secretAccessKey;
 		this.region = props.region;
 	}
 }
---
+++ apps/server/src/shared/repo/types/EncryptedString.type.spec.ts
@@ -0,0 +1,47 @@
+import { Platform } from '@mikro-orm/core';
+import { EncryptedStringType } from './EncryptedString.type';
+
+describe('EncryptedString type', () => {
+	const serializer = new EncryptedStringType();
+	const text = 'sample text input';
+	const textEncrypted = 'U2FsdGVkX19f3ahtkMU8/Sma2NLiDhv6Pqdcoh0Co7EPwTiluyDZgR+6mtq6FeWR';
+
+	describe('When convertToDatabaseValue', () => {
+		it('should pass null value', () => {
+			const result = serializer.convertToDatabaseValue(null as unknown as undefined, undefined as unknown as Platform);
+			expect(result).toBeNull();
+		});
+		it('should pass undefined value', () => {
+			const result = serializer.convertToDatabaseValue(undefined, undefined as unknown as Platform);
+			expect(result).toBeUndefined();
+		});
+		it('should pass empty string value', () => {
+			const result = serializer.convertToDatabaseValue('', undefined as unknown as Platform);
+			expect(result).toEqual('');
+		});
+		it('should encrypt not-empty string value', () => {
+			const result = serializer.convertToDatabaseValue(text, undefined as unknown as Platform);
+			// TODO why does the encrypted string not stay same?
+			expect(result.length).toEqual(textEncrypted.length);
+		});
+	});
+
+	describe('When convertToJSValue', () => {
+		it('should pass null value', () => {
+			const result = serializer.convertToJSValue(null as unknown as undefined, undefined as unknown as Platform);
+			expect(result).toBeNull();
+		});
+		it('should pass undefined value', () => {
+			const result = serializer.convertToJSValue(undefined as unknown as undefined, undefined as unknown as Platform);
+			expect(result).toBeUndefined();
+		});
+		it('should pass empty string value', () => {
+			const result = serializer.convertToJSValue('', undefined as unknown as Platform);
+			expect(result).toEqual('');
+		});
+		it('should decrypt not-empty string value', () => {
+			const result = serializer.convertToJSValue(textEncrypted, undefined as unknown as Platform);
+			expect(result).toEqual(text);
+		});
+	});
+});
