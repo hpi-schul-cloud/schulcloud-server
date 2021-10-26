cb20860d68e23a60e38cf76298c2dab683685ff1
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2021-10-26 16:43:14 +0200

    add unit test for base entity

---
+++ apps/server/src/shared/domain/entity/base.entity.spec.ts
@@ -0,0 +1,19 @@
+import { setupEntities } from '@src/modules/database';
+import { ObjectId } from 'mongodb';
+import { BaseEntity } from './base.entity';
+
+describe('BaseEntity', () => {
+	beforeAll(async () => {
+		await setupEntities();
+	});
+
+	class User extends BaseEntity {}
+
+	describe('when _id property is set to ObjectId', () => {
+		it('should serialize the ObjectId to the id property', () => {
+			const user = new User();
+			user._id = new ObjectId();
+			expect(user.id).toEqual(user._id.toHexString());
+		});
+	});
+});
