1a4ce91f2b8ad2113d1d8f2ffe814dae944be5c3
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2021-10-27 21:02:47 +0200

    add test factory for directory entity

--- apps/server/src/shared/testing/factory/file.factory.ts
+++ apps/server/src/shared/testing/factory/file.factory.ts
@@ -1,11 +1,21 @@
-import { File, IFileProperties } from '@shared/domain';
+import { File, Directory, IFileProperties } from '@shared/domain';
 import { storageProviderFactory } from './storageprovider.factory';
 import { userFactory } from './user.factory';
 import { BaseFactory } from './base.factory';
 
 export const fileFactory = BaseFactory.define<File, IFileProperties>(File, ({ sequence }) => {
 	return {
-		storageFileName: `storageFileName-${sequence}`,
+		storageFileName: `file-${sequence}`,
+		bucket: 'test-bucket',
+		storageProvider: storageProviderFactory.build(),
+		creator: userFactory.build(),
+	};
+});
+
+/* istanbul ignore next */
+export const directoryFactory = BaseFactory.define<Directory, IFileProperties>(Directory, ({ sequence }) => {
+	return {
+		storageFileName: `directory-${sequence}`,
 		bucket: 'test-bucket',
 		storageProvider: storageProviderFactory.build(),
 		creator: userFactory.build(),
