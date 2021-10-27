a62bf21444b1a879c1e42d05a87f126937bb2305
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2021-10-27 21:10:10 +0200

    update tests

--- apps/server/src/shared/domain/entity/base.entity.spec.ts
+++ apps/server/src/shared/domain/entity/base.entity.spec.ts
@@ -16,4 +16,13 @@ describe('BaseEntity', () => {
 			expect(user.id).toEqual(user._id.toHexString());
 		});
 	});
+
+	describe('when id property is set to serialized ObjectId', () => {
+		it('should wrap the serialized id to the _id property', () => {
+			const user = new User();
+			user.id = new ObjectId().toHexString();
+			expect(user._id).toBeInstanceOf(ObjectId);
+			expect(user._id.toHexString()).toEqual(user.id);
+		});
+	});
 });
