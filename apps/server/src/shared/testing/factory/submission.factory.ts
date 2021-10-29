17d5bc5b1a3e63945f6775b8113e2791e1aa7217
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2021-10-29 14:58:08 +0200

    Add submission factory chaining for graded submissions

--- apps/server/src/shared/testing/factory/submission.factory.ts
+++ apps/server/src/shared/testing/factory/submission.factory.ts
@@ -1,12 +1,20 @@
+import { DeepPartial } from 'fishery';
 import { Submission, ISubmissionProperties } from '@shared/domain';
 import { BaseFactory } from './base.factory';
 import { taskFactory } from './task.factory';
 import { userFactory } from './user.factory';
 
-export const submissionFactory = BaseFactory.define<Submission, ISubmissionProperties>(Submission, () => {
+class SubmissionFactory extends BaseFactory<Submission, ISubmissionProperties> {
+	graded(isGraded = true): this {
+		const params: DeepPartial<ISubmissionProperties> = { gradeComment: isGraded ? 'graded text' : undefined };
+		return this.params(params);
+	}
+}
+
+export const submissionFactory = SubmissionFactory.define(Submission, ({ sequence }) => {
 	return {
 		task: taskFactory.build(),
 		student: userFactory.build(),
-		comment: 'submission comment',
+		comment: `submission comment #${sequence}`,
 	};
 });
