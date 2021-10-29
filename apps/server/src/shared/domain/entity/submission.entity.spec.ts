3403b9c5261a7ed35b63c60a757cefd137787810
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2021-10-29 15:03:49 +0200

    Add lesson entity test

--- apps/server/src/shared/domain/entity/submission.entity.spec.ts
+++ apps/server/src/shared/domain/entity/submission.entity.spec.ts
@@ -18,12 +18,14 @@ describe('Submission entity', () => {
 		it('should be graded if grade percentage is set', () => {
 			const submission = buildSubmission();
 			submission.grade = 50;
+
 			expect(submission.isGraded()).toEqual(true);
 		});
 
 		it('should be graded if grade comment is set', () => {
 			const submission = buildSubmission();
 			submission.gradeComment = 'well done!';
+
 			expect(submission.isGraded()).toEqual(true);
 		});
 
@@ -32,11 +34,21 @@ describe('Submission entity', () => {
 			const teacher = userFactory.build();
 			const file = fileFactory.build({ creator: teacher });
 			submission.gradeFiles = new Collection<File>(submission, [file]);
+
 			expect(submission.isGraded()).toEqual(true);
 		});
 	});
 
 	describe('getStudentId', () => {
-		it('should return the id of the student that upload the submission', () => {});
+		it('should return the id of the student', () => {
+			const student = userFactory.build();
+			student.id = '0123456789ab';
+			const task = taskFactory.build();
+			const submission = submissionFactory.build({ task, student });
+
+			const result = submission.getStudentId();
+
+			expect(result).toEqual(student.id);
+		});
 	});
 });
