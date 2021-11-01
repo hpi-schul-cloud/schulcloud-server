cb1e9a1625e128ebb80f679f0b6575f9ed683595
mamutmk5 <paul.poppe@dataport.de>
2021-11-01 13:48:55 +0100

    Update stringHelper.js

--- src/helper/stringHelper.js
+++ src/helper/stringHelper.js
@@ -1,13 +1,13 @@
  
 const isNotEmptyString = (data, trim=false) => {
   const  result = false;
-  result &= typeof data === 'string';
+  result = typeof data === 'string';
   if(result) {
     if(trim) {
-      result &= data.trim().length > 0 ;
+      result = data.trim().length > 0 ;
     }
     else {
-      result &= data.length > 0;
+      result = data.length > 0;
     }
   }
   return result;
