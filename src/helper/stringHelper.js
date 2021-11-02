0e1ed565130063020cfce188925e378b584c408d
mamutmk5 <paul.poppe@dataport.de>
2021-11-02 18:25:13 +0100

    Update stringHelper.js

--- src/helper/stringHelper.js
+++ src/helper/stringHelper.js
@@ -1,4 +1,4 @@
-const isNotEmptyString = (data, trim = false) => typeof data === 'string' && ((trim && data.trim().length > 0) || data.length > 0));
+const isNotEmptyString = (data, trim = false) => typeof data === 'string' && ((trim && data.trim().length > 0) || data.length > 0);
 
 module.exports = {
 	isNotEmptyString,
