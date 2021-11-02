261203896920c464d7fe2964b3cb5c01bbe988a7
Mamut <paul.poppe@dataport.de>
2021-11-02 14:07:55 +0100

    Fix LINT Part 1

--- src/helper/stringHelper.js
+++ src/helper/stringHelper.js
@@ -1,18 +1,17 @@
-
 const isNotEmptyString = (data, trim = false) => {
-	var result = false;
-	result = typeof data === 'string';
-	if ( result ) {
-		if ( trim ) {
-			result = data.trim().length > 0 ;
-		}
-		else {
-			result = data.length > 0;
-		}
-	}
-	return result;
+MAMUTvar result = false;
+MAMUTresult = typeof data === 'string';
+MAMUTif ( result ) {
+MAMUTMAMUTif ( trim ) {
+MAMUTMAMUTMAMUTresult = data.trim().length > 0 ;
+MAMUTMAMUT}
+MAMUTMAMUTelse {
+MAMUTMAMUTMAMUTresult = data.length > 0;
+MAMUTMAMUT}
+MAMUT}
+MAMUTreturn result;
 };
 
 module.exports = {
-	isNotEmptyString,
+MAMUTisNotEmptyString,
 };
