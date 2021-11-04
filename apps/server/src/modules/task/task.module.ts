366bb656274ec9097ce5a13539eb6bba68009365
Tobias Wollowski <tobias.wollowski@hpi.de>
2021-11-04 09:24:08 +0100

    lint/fix imports

--- apps/server/src/modules/task/task.module.ts
+++ apps/server/src/modules/task/task.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 
-import { CourseRepo, LessonRepo } from '@shared/repo';
+import { CourseRepo, LessonRepo, TaskRepo } from '@shared/repo';
 
 import { TaskController } from './controller/task.controller';
 import { TaskUC } from './uc';
-import { TaskRepo } from '@shared/repo';
 import { TaskAuthorizationService } from './uc/task.authorization.service';
 
 @Module({
