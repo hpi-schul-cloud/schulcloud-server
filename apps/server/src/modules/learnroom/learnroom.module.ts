486cc8f45901b8f056a9f55a5a6ffa3030e5438b
Thomas Feldtkeller <thomas.feldtkeller@hpi.de>
2021-11-19 14:13:58 +0100

    Bc 723 get courses (#3200)
    * get active courses.
    
    Course Repo findAllByUserId
    - now support sorting and pagination.
    - now only returns active courses.
    
    * implement course findByUser usecase
    
    * implement course find route
    
    returning the metadata necessary for the course list
    wired up to the application and its services as needed
    
    * explicit filter for active courses on uc layer
    
    add fiters to repo function to support future changes
    
    * add indices

--- apps/server/src/modules/learnroom/learnroom.module.ts
+++ apps/server/src/modules/learnroom/learnroom.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 
-import { DashboardRepo, DashboardModelMapper } from '@shared/repo';
+import { DashboardRepo, DashboardModelMapper, CourseRepo } from '@shared/repo';
 
 import { DashboardController } from './controller/dashboard.controller';
+import { CourseController } from './controller/course.controller';
 import { DashboardUc } from './uc/dashboard.uc';
+import { CourseUc } from './uc/course.uc';
 
 @Module({
 	imports: [],
-	controllers: [DashboardController],
+	controllers: [DashboardController, CourseController],
 	providers: [
 		DashboardUc,
 		{
@@ -15,6 +17,8 @@ import { DashboardUc } from './uc/dashboard.uc';
 			useClass: DashboardRepo,
 		},
 		DashboardModelMapper,
+		CourseUc,
+		CourseRepo,
 	],
 })
 export class LearnroomModule {}
