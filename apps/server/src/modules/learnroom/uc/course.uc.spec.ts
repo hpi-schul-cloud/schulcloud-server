c6800ba06a93f68f636822753ec2554b9f456d29
Martin Schuhmacher <55735359+MartinSchuhmacher@users.noreply.github.com>
2021-12-07 07:37:14 +0100

    BC-830 sort rooms-list to currently updated descending (#3230)
    * sort total courses in rooms-list by updatedAt descending
    
    UX recommended sorting descending for updatedAt is implemented.
    tests for courses UC are adapted to courseFactories buildlist.
    changelog edited.

--- apps/server/src/modules/learnroom/uc/course.uc.spec.ts
+++ apps/server/src/modules/learnroom/uc/course.uc.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CourseRepo } from '@shared/repo';
 import { Course, Counted, EntityId, IFindOptions, SortOrder } from '@shared/domain';
+import { courseFactory } from '@shared/testing';
 import { CourseUc } from './course.uc';
 
 describe('course uc', () => {
@@ -28,7 +29,7 @@ describe('course uc', () => {
 
 	describe('findByUser', () => {
 		it('should return courses of user', async () => {
-			const courses = new Array(5).map(() => ({} as Course));
+			const courses = courseFactory.buildList(5);
 			const spy = jest.spyOn(repo, 'findAllByUserId').mockImplementation((userId: EntityId) => {
 				return Promise.resolve([courses, 5]);
 			});
@@ -39,13 +40,13 @@ describe('course uc', () => {
 		});
 
 		it('should pass on options correctly', async () => {
-			const courses = new Array(5).map(() => ({} as Course));
+			const courses = courseFactory.buildList(5);
 			const spy = jest.spyOn(repo, 'findAllByUserId').mockImplementation((userId: EntityId) => {
 				return Promise.resolve([courses, 5]);
 			});
 
 			const pagination = { skip: 1, limit: 2 };
-			const resultingOptions = { pagination, order: { name: SortOrder.asc } };
+			const resultingOptions = { pagination, order: { updatedAt: SortOrder.desc } };
 
 			const [array, count] = await service.findAllByUser('someUserId', pagination);
 
