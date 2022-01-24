dc33cbebe92d557b41f152387be6561eb9b04613
Tobias Wollowski <tobias@jbbr.net>
2022-01-24 11:49:01 +0100

    change flag

--- apps/server/src/modules/user-import/controller/dto/update-flag.params.ts
+++ apps/server/src/modules/user-import/controller/dto/update-flag.params.ts
@@ -1,8 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { StringToBoolean } from '@shared/controller';
 import { IsBoolean } from 'class-validator';
 
 export class UpdateFlagParams {
 	@ApiProperty({ description: 'updates flag for an import user' })
+	@StringToBoolean()
 	@IsBoolean()
 	flagged!: boolean;
 }
