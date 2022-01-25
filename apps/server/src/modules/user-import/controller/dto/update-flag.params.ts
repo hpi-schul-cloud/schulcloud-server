e459fa99d6cc8bae8fceec40d90bb49c511577a4
Tobias Wollowski <tobias@jbbr.net>
2022-01-25 09:37:47 +0100

    no string to boolean

--- apps/server/src/modules/user-import/controller/dto/update-flag.params.ts
+++ apps/server/src/modules/user-import/controller/dto/update-flag.params.ts
@@ -1,10 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { StringToBoolean } from '@shared/controller';
 import { IsBoolean } from 'class-validator';
 
 export class UpdateFlagParams {
 	@ApiProperty({ description: 'updates flag for an import user' })
 	@IsBoolean()
-	@StringToBoolean()
 	flagged!: boolean;
 }
