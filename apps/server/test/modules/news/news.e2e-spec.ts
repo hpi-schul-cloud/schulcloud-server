1e2f80ac8daac02796f26db18b343387ff77e831
Tobias W <tobias.wollowski@hpi.de>
2022-01-25 18:12:15 +0100

    BC-1104 in memory databse for e2e tests (#3295)

--- apps/server/test/modules/news/news.e2e-spec.ts
+++ apps/server/test/modules/news/news.e2e-spec.ts
@@ -6,7 +6,7 @@ import { MikroORM } from '@mikro-orm/core';
 import { EntityManager, ObjectId } from '@mikro-orm/mongodb';
 import moment from 'moment';
 import { EntityId, News, NewsTargetModel } from '@shared/domain';
-import { ServerModule } from '@src/server.module';
+import { ServerTestModule } from '@src/server.module';
 import { JwtAuthGuard } from '@src/modules/authentication/guard/jwt-auth.guard';
 import { AuthorizationService } from '@src/modules/authorization/authorization.service';
 import { API_VALIDATION_ERROR_TYPE } from '@src/core/error/server-error-types';
@@ -39,7 +39,7 @@ describe('News Controller (e2e)', () => {
 
 	beforeAll(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			imports: [ServerModule],
+			imports: [ServerTestModule],
 		})
 			.overrideGuard(JwtAuthGuard)
 			.useValue({
@@ -214,9 +214,9 @@ describe('News Controller (e2e)', () => {
 		});
 
 		it('should throw an error if trying to update of object which doesnt exists', async () => {
-			const notExistedNews = new ObjectId().toHexString();
+			const randomId = new ObjectId().toHexString();
 			const params = {} as UpdateNewsParams;
-			await request(app.getHttpServer()).patch(`/news/${notExistedNews}`).send(params).expect(404);
+			await request(app.getHttpServer()).patch(`/news/${randomId}`).send(params).expect(404);
 		});
 	});
 	describe('DELETE /news/{id}', () => {
@@ -227,8 +227,8 @@ describe('News Controller (e2e)', () => {
 		});
 
 		it('should throw not found error, if news doesnt exists', async () => {
-			const newsId = new ObjectId().toHexString();
-			await request(app.getHttpServer()).delete(`/news/${newsId}`).expect(404);
+			const randomId = new ObjectId().toHexString();
+			await request(app.getHttpServer()).delete(`/news/${randomId}`).expect(404);
 		});
 	});
 });
