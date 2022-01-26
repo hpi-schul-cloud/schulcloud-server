1e4ddec4739eff8e028fc54d3f0447427f7c5568
Igor Richter <igor.richter@capgemini.com>
2022-01-26 23:38:28 +0100

    fixed casting and return value
    Co-authored-by: Forat Zoubi <fzoubi-cap@users.noreply.github.com>
    Co-authored-by: mrikallab <mrikallab@users.noreply.github.com>

--- apps/server/src/modules/authorization/feathers-jwt.provider.ts
+++ apps/server/src/modules/authorization/feathers-jwt.provider.ts
@@ -6,9 +6,9 @@ import { FeathersServiceProvider } from '@shared/infra/feathers';
 export class FeathersJwtProvider {
 	constructor(private feathersServiceProvider: FeathersServiceProvider) {}
 
-	async generateJwt(userId: EntityId): Promise<unknown> {
+	async generateJwt(userId: EntityId): Promise<string> {
 		const service = this.feathersServiceProvider.getService('accounts/supportJWT');
-		const jwt = (await service.create({ userId }, { account: { userId } })) as unknown;
+		const jwt = (await service.create({ userId }, { account: { userId } })) as Promise<string>;
 		if (jwt == null) throw new NotFoundException();
 		return jwt;
 	}
