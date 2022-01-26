021d545ba73e661c1b43530434dcbf8ae8e5ef67
Igor Richter <igor.richter@capgemini.com>
2022-01-27 00:44:17 +0100

    removed unreached code
    Co-authored-by: Forat Zoubi <fzoubi-cap@users.noreply.github.com>
    Co-authored-by: mrikallab <mrikallab@users.noreply.github.com>

--- apps/server/src/modules/authorization/feathers-jwt.provider.ts
+++ apps/server/src/modules/authorization/feathers-jwt.provider.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { EntityId } from '@shared/domain';
 import { FeathersServiceProvider } from '@shared/infra/feathers';
 
@@ -9,7 +9,6 @@ export class FeathersJwtProvider {
 	async generateJwt(userId: EntityId): Promise<string> {
 		const service = this.feathersServiceProvider.getService('accounts/supportJWT');
 		const jwt = (await service.create({ userId }, { account: { userId } })) as Promise<string>;
-		if (jwt == null) throw new NotFoundException();
 		return jwt;
 	}
 }
