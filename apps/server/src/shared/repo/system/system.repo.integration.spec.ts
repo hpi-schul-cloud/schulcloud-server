53e2c7f7a5582ac6d5a37e6d3f3067b61fe151ae
Forat Zoabi <forat.zoubi@capgemini.com>
2022-01-28 10:59:02 +0100

    - system integration test base

--- apps/server/src/shared/repo/system/system.repo.integration.spec.ts
+++ apps/server/src/shared/repo/system/system.repo.integration.spec.ts
@@ -1,12 +1,9 @@
-import { NotFoundError } from '@mikro-orm/core';
-import { EntityManager, ObjectId } from '@mikro-orm/mongodb';
+import { EntityManager } from '@mikro-orm/mongodb';
 import { Test, TestingModule } from '@nestjs/testing';
-import { System, User } from '@shared/domain';
 import { MongoMemoryDatabaseModule } from '@shared/infra/database';
-import { userFactory } from '@shared/testing';
 import { SystemRepo } from './system.repo';
 
-describe('user repo', () => {
+describe('system repo', () => {
 	let module: TestingModule;
 	let repo: SystemRepo;
 	let em: EntityManager;
@@ -25,38 +22,7 @@ describe('user repo', () => {
 	});
 
 	it('should be defined', () => {
-		expect(repo.toBeDefined();
+		expect(repo).toBeDefined();
 		expect(typeof repo.findById).toEqual('function');
-	});
-
-	describe('findById', () => {
-		afterEach(async () => {
-			await em.nativeDelete(System, {});
-		});
-
-		it('should return right keys', async () => {
-			const system = userFactory.build();
-
-			await em.persistAndFlush([system]);
-			const result = await repo.findById(system.id);
-			expect(Object.keys(result).sort()).toEqual(
-				['createdAt', 'updatedAt', 'roles', 'firstName', 'lastName', 'email', 'school', '_id'].sort()
-			);
-		});
-
-		it('should return one role that matched by id', async () => {
-			const userA = userFactory.build();
-			const userB = userFactory.build();
-
-			await em.persistAndFlush([userA, userB]);
-			const result = await repo.findById(userA.id);
-			expect(result).toEqual(userA);
-		});
-
-		it('should throw an error if roles by id doesnt exist', async () => {
-			const idA = new ObjectId().toHexString();
-
-			await expect(repo.findById(idA)).rejects.toThrow(NotFoundError);
-		});
 	});
 });
