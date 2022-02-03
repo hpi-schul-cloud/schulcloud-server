667defdc9c7e5c1e1b677fbd2365a1b9641fa66c
SevenWaysDP <sergej.hoffmann@dataport.de>
2022-02-03 14:52:57 +0100

    BC-1199 add configuration

--- apps/server/src/modules/files-storage/files-storage.module.ts
+++ apps/server/src/modules/files-storage/files-storage.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { S3Client } from '@aws-sdk/client-s3';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { ALL_ENTITIES } from '@shared/domain';
+import { Configuration } from '@hpi-schul-cloud/commons';
 import { FilesStorageController } from './controller/files-storage.controller';
 import { S3ClientAdapter } from './client/s3-client.adapter';
 import { S3Config } from './interface/config';
@@ -9,13 +10,12 @@ import { AuthModule } from '../authentication/auth.module';
 import { DB_URL, DB_USERNAME, DB_PASSWORD } from '../../config';
 import { FilesStorageUC } from './uc/files-storage.uc';
 
-// @TODO implement such as project config
 const config: S3Config = {
-	endpoint: process.env.FILES_STORAGE_S3_ENDPOINT || 'http://localhost:9000/',
-	region: process.env.FILES_STORAGE_S3_REGION || 'de-west',
-	bucket: process.env.FILES_STORAGE_S3_BUCKET || 'schulcloud',
-	accessKeyId: process.env.FILES_STORAGE_S3_ACCESS_KEY_ID || 'testtest',
-	secretAccessKey: process.env.FILES_STORAGE_S3_SECRET_ACCESS_KEY || 'testtest',
+	endpoint: Configuration.get('FILES_STORAGE__S3_ENDPOINT') as string,
+	region: Configuration.get('FILES_STORAGE__S3_REGION') as string,
+	bucket: Configuration.get('FILES_STORAGE__S3_BUCKET') as string,
+	accessKeyId: Configuration.get('FILES_STORAGE__S3_ACCESS_KEY_ID') as string,
+	secretAccessKey: Configuration.get('FILES_STORAGE__S3_SECRET_ACCESS_KEY') as string,
 };
 
 @Module({
