74312abf0bbe7ce2faa91e8d98751da9d14e2ce5
Igor Richter <igor.richter@capgemini.com>
2022-02-04 14:55:24 +0100

    Migration script done!

--- migrations/1643883650091-encrypt_iserv_client_secret.js
+++ migrations/1643883650091-encrypt_iserv_client_secret.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const CryptoJS = require('crypto-js');
 const { Configuration } = require('@hpi-schul-cloud/commons');
 // eslint-disable-next-line no-unused-vars
 const { alert, info, error } = require('../src/logger');
@@ -8,88 +7,82 @@ const { connect, close } = require('../src/utils/database');
 
 const systemModel = require('../src/services/system/model');
 
-// use your own name for your model, otherwise other migrations may fail.
-// The third parameter is the actually relevent one for what collection to write to.
-// const User = mongoose.model(
-// 	'makeMeUnique',
-// 	new mongoose.Schema(
-// 		{
-// 			firstName: { type: String, required: true },
-// 			lastName: { type: String, required: true },
-// 		},
-// 		{
-// 			timestamps: true,
-// 		}
-// 	),
-// 	'users'
-// );
-
-// How to use more than one schema per collection on mongodb
-// https://stackoverflow.com/questions/14453864/use-more-than-one-schema-per-collection-on-mongodb
-
 // TODO npm run migration-persist and remove this line
 // TODO update seed data and remove this line
 
+const stages = [
+	{
+		name: 'staging',
+		id: 'staging_id_iserv',
+		config: {
+			client_id: '58_qe54d8bh0v4gog8sw0w88c0s8cwwocc8wk8oo00s4c0g8gkc8',
+			client_secret:
+				'U2FsdGVkX18JzdW2LOjhhsn4aIj3En6BBh9QSO9dgUrjIoZp97ptJ1sAeEElTH6/NY0dUX8k3C5JL75dZqJlFGaTpfKkoPuw0tSGZGyw8dQ=',
+			token_endpoint: 'http://iserv.n21.dbildungscloud.de/iserv/auth/public/token',
+			grant_type: 'authorization_code',
+			token_redirect_uri: 'http://localhost:3030/api/v3/oauth/0000d186816abba584714c92/token',
+			scope: 'openid uuid',
+			response_type: 'code',
+			code_redirect_uri: 'http://localhost:3030/api/v3/oauth/0000d186816abba584714c92',
+			base_redirect_uri: 'http://localhost:3030/api/v3/oauth',
+			auth_endpoint: 'http://iserv.n21.dbildungscloud.de/iserv/auth/auth',
+		},
+	},
+	{
+		name: 'produktion',
+		id: 'produktion_id_iserv',
+		config: {
+			client_id: '58_qe54d8bh0v4gog8sw0w88c0s8cwwocc8wk8oo00s4c0g8gkc8',
+			client_secret:
+				'U2FsdGVkX18JzdW2LOjhhsn4aIj3En6BBh9QSO9dgUrjIoZp97ptJ1sAeEElTH6/NY0dUX8k3C5JL75dZqJlFGaTpfKkoPuw0tSGZGyw8dQ=',
+			token_endpoint: 'http://iserv.n21.dbildungscloud.de/iserv/auth/public/token',
+			grant_type: 'authorization_code',
+			token_redirect_uri: 'http://localhost:3030/api/v3/oauth/0000d186816abba584714c92/token',
+			scope: 'openid uuid',
+			response_type: 'code',
+			code_redirect_uri: 'http://localhost:3030/api/v3/oauth/0000d186816abba584714c92',
+			base_redirect_uri: 'http://localhost:3030/api/v3/oauth',
+			auth_endpoint: 'http://iserv.n21.dbildungscloud.de/iserv/auth/auth',
+		},
+	},
+];
+
 module.exports = {
 	up: async function up() {
-		if (!Configuration.has('LDAP_PASSWORD_ENCRYPTION_KEY')) {
-			throw new Error('You need to set LDAP_PASSWORD_ENCRYPTION_KEY to encrypt the old key!');
-		}
 		await connect();
-		// ////////////////////////////////////////////////////
-		// Make changes to the database here.
-		// Hint: Access models via this('modelName'), not an imported model to have
-		// access to the correct database connection. Otherwise Mongoose calls never return.
-		const systems = await systemModel.find({});
 
-		info('Start encrypting client_secret of the "iserv" system');
+		info('creating oauthconfig for iserv');
+
 		await Promise.all(
-			systems.map((system) => {
-				if (system.type !== 'iserv') return Promise.resolve();
-
-				const encryptedSecret = CryptoJS.AES.encrypt(
-					system.oautchconfig.client_secret,
-					Configuration.get('LDAP_PASSWORD_ENCRYPTION_KEY')
-				).toString();
-				const newoauthconfig = { ...system.oauthconfig };
-				newoauthconfig.cleint_secret = encryptedSecret;
-				info(`Encrypting client_secret of system { id: ${system.id}), alias: ${system.alias} }`);
-				return systemModel.updateOne({
-					oauthconfig: newoauthconfig,
-				});
+			stages.map(async (stage) => {
+				const systems = await systemModel.find({ _id: stage.id });
+				if (systems.length === 1) {
+					info(`Found iserv system for ${stage.name}.`);
+					systems[0].oauthconfig = stage.config;
+					return systems[0].save();
+				}
+				return Promise.resolve();
 			})
 		);
-		info('Finished encrypting client_secret of the "iserv" system');
-
 		await close();
 	},
 
 	down: async function down() {
-		if (!Configuration.has('LDAP_PASSWORD_ENCRYPTION_KEY')) {
-			throw new Error('You need to set LDAP_PASSWORD_ENCRYPTION_KEY to decrypt the key!');
-		}
 		await connect();
-		// ////////////////////////////////////////////////////
-		// Implement the necessary steps to roll back the migration here.
-		const systems = await systemModel.find();
 
-		info('Start decrypting the client_secret of the "iserv" system');
+		info('deleting oauthconfig for iserv');
+
 		await Promise.all(
-			systems.map((system) => {
-				if (system.type !== 'iserv') return Promise.resolve();
-				const decryptedSecret = CryptoJS.AES.decrypt(
-					system.oautchconfig.client_secret,
-					Configuration.get('LDAP_PASSWORD_ENCRYPTION_KEY')
-				).toString();
-				const newoauthconfig = { ...system.oauthconfig };
-				newoauthconfig.cleint_secret = decryptedSecret;
-
-				return systemModel.updateOne({
-					oauthconfig: newoauthconfig,
-				});
+			stages.map(async (stage) => {
+				const systems = await systemModel.find({ _id: stage.id });
+				if (systems.length === 1) {
+					info(`Found iserv system for ${stage.name}.`);
+					delete systems[0].oauthconfig;
+					return systems[0].save();
+				}
+				return Promise.resolve();
 			})
 		);
-		info('Finished decrypting client_secret of the "iserv" system');
 		await close();
 	},
 };
