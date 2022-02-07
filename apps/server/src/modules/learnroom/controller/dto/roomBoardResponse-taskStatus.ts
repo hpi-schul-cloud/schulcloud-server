52f171032fdc850aa468d369ce946d788cc61f61
Martin Schuhmacher <55735359+MartinSchuhmacher@users.noreply.github.com>
2022-02-07 16:06:46 +0100

    BC-1149 - task card reimplementation (#3305)
    * actual description to use in frontend
    
    * task entity getDescription renamed
    
    * add description to task entity
    
    * adding isFinished to room task status
    
    to show the correct closing button on the room task, the property
    isFinished for the task needs to be given to the frontend

--- apps/server/src/modules/learnroom/controller/dto/roomBoardResponse-taskStatus.ts
+++ apps/server/src/modules/learnroom/controller/dto/roomBoardResponse-taskStatus.ts
@@ -1,12 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class BoardTaskStatusResponse {
-	constructor({ submitted, maxSubmissions, graded, isDraft, isSubstitutionTeacher }: BoardTaskStatusResponse) {
+	constructor({
+		submitted,
+		maxSubmissions,
+		graded,
+		isDraft,
+		isSubstitutionTeacher,
+		isFinished,
+	}: BoardTaskStatusResponse) {
 		this.submitted = submitted;
 		this.maxSubmissions = maxSubmissions;
 		this.graded = graded;
 		this.isDraft = isDraft;
 		this.isSubstitutionTeacher = isSubstitutionTeacher;
+		this.isFinished = isFinished;
 	}
 
 	@ApiProperty()
@@ -23,4 +31,7 @@ export class BoardTaskStatusResponse {
 
 	@ApiProperty()
 	isSubstitutionTeacher: boolean;
+
+	@ApiProperty()
+	isFinished: boolean;
 }
