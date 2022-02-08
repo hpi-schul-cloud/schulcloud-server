753988e7fe975c9b5e83f4a525221b428a1a801c
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2022-02-08 07:32:03 +0100

    BC-1071 fix sorting bug by similar dueDates for paginated requests (#3309)

--- apps/server/src/shared/repo/task/task.repo.ts
+++ apps/server/src/shared/repo/task/task.repo.ts
@@ -74,7 +74,10 @@ export class TaskRepo {
 		scope.addQuery(allForFinishedCoursesAndLessons.query);
 		scope.addQuery(allForCreator.query);
 
-		const order = { dueDate: SortOrder.desc };
+		// The dueDate can be similar to solve pagination request missmatches we must sort it over id too.
+		// Because after executing limit() in mongoDB it is resort by similar dueDates.
+		// It exist indexes for dueDate and for _id but no combined index, because it is to expensive for only small performance boost.
+		const order = { dueDate: SortOrder.desc, id: SortOrder.asc };
 
 		const [tasks, count] = await this.em.findAndCount(Task, scope.query, {
 			offset: pagination?.skip,
