ee48196cdd0a05b17f86d917b7175bc20a5b3c9d
Thomas Feldtkeller <thomas.feldtkeller@hpi.de>
2022-02-11 17:21:38 +0100

    fix course uc tests

--- apps/server/src/shared/testing/factory/boardelement.factory.ts
+++ apps/server/src/shared/testing/factory/boardelement.factory.ts
@@ -3,16 +3,13 @@ import { lessonFactory } from './lesson.factory';
 import { BaseFactory } from './base.factory';
 import { taskFactory } from './task.factory';
 
-export const taskBoardElementFactory = BaseFactory.define<TaskBoardElement, { target: Task }>(
-	TaskBoardElement,
-	({ sequence }) => {
-		return { target: taskFactory.build() };
-	}
-);
+export const taskBoardElementFactory = BaseFactory.define<TaskBoardElement, { target: Task }>(TaskBoardElement, () => {
+	return { target: taskFactory.build() };
+});
 
 export const lessonBoardElementFactory = BaseFactory.define<LessonBoardElement, { target: Lesson }>(
 	LessonBoardElement,
-	({ sequence }) => {
+	() => {
 		return { target: lessonFactory.build() };
 	}
 );
