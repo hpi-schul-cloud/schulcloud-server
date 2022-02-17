52f87c895f2bf72c69ccef6e6ebd6933d1a27c09
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2022-02-17 13:12:36 +0100

    Resort code for uc file upload

--- apps/server/src/modules/files-storage/uc/files-storage.uc.ts
+++ apps/server/src/modules/files-storage/uc/files-storage.uc.ts
@@ -1,9 +1,12 @@
 import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import * as path from 'path';
 import { Request } from 'express';
+import busboy from 'busboy';
+import internal from 'stream';
+
 import { FileRecordRepo } from '@shared/repo';
 import { EntityId, FileRecord } from '@shared/domain';
-import busboy from 'busboy';
+
 import { S3ClientAdapter } from '../client/s3-client.adapter';
 import { FileDownloadDto, FileMetaDto } from '../controller/dto/file.dto';
 import { IFile } from '../interface/file';
@@ -12,51 +15,64 @@ import { IFile } from '../interface/file';
 export class FilesStorageUC {
 	constructor(private readonly storageClient: S3ClientAdapter, private readonly fileRecordRepo: FileRecordRepo) {}
 
-	async upload(userId: EntityId, params: FileMetaDto, req: Request) {
+	async upload(userId: EntityId, params: FileMetaDto, req: Request): Promise<FileRecord> {
 		// @TODO check permissions of schoolId by user
 		// @TODO scan virus on demand?
 		// @TODO add thumbnail on demand
 		try {
-			let record!: FileRecord;
+			const result: FileRecord = await new Promise((resolve, reject) => {
+				const requestStream = busboy({ headers: req.headers });
 
-			const result = await new Promise((resolve, reject) => {
-				const bb = busboy({ headers: req.headers });
 				// eslint-disable-next-line @typescript-eslint/no-misused-promises
-				bb.on('file', async (_name, file, info): Promise<void> => {
-					const { filename, mimeType } = info;
+				requestStream.on('file', async (_name, file, info): Promise<void> => {
+					const fileDescription = this.createFileDescription(file, info, req);
+					const record = await this.uploadFile(userId, params, fileDescription);
 
-					const size = Number(req.get('content-length'));
-					const contentType = mimeType;
-					const fileName = filename;
-					const fileR: IFile = {
-						name: fileName,
-						buffer: file,
-						size,
-						type: contentType,
-					};
-					record = this.getFileRecord(userId, params, fileR);
-					await this.fileRecordRepo.save(record);
-
-					const folder = path.join(params.schoolId, record.id);
-					const s3Res = await this.storageClient.uploadFile(folder, fileR);
-
-					return resolve({ s3Res, record });
-				});
-
-				bb.on('close', () => {
 					resolve(record);
 				});
-				bb.on('error', (e) => {
+
+				requestStream.on('error', (e) => {
 					reject(e);
 				});
 
-				req.pipe(bb);
+				req.pipe(requestStream);
 			});
 
 			return result;
 		} catch (error) {
 			throw new BadRequestException(error);
 		}
+	}
+
+	createFileDescription(file: internal.Readable, info: busboy.FileInfo, req: Request): IFile {
+		const size = Number(req.get('content-length'));
+		const fileDescription: IFile = {
+			name: info.filename,
+			buffer: file,
+			size,
+			type: info.mimeType, // IFile rename in contentType or mimeType ?
+		};
+
+		return fileDescription;
+	}
+
+	private async uploadFile(userId: EntityId, params: FileMetaDto, fileDescription: IFile): Promise<FileRecord> {
+		const entity = new FileRecord({
+			size: fileDescription.size,
+			name: fileDescription.name,
+			type: fileDescription.type,
+			targetType: params.targetType,
+			targetId: params.targetId,
+			creatorId: userId,
+			schoolId: params.schoolId,
+		});
+		await this.fileRecordRepo.save(entity);
+
+		// todo on error roll back
+		const folder = path.join(params.schoolId, entity.id);
+		await this.storageClient.uploadFile(folder, fileDescription);
+
+		return entity;
 	}
 
 	async download(userId: EntityId, params: FileDownloadDto) {
@@ -74,18 +90,5 @@ export class FilesStorageUC {
 		} catch (error) {
 			throw new BadRequestException(error);
 		}
-	}
-
-	getFileRecord(userId: EntityId, params: FileMetaDto, file: IFile) {
-		const entity = new FileRecord({
-			size: file.size,
-			name: file.name,
-			type: file.type,
-			targetType: params.targetType,
-			targetId: params.targetId,
-			creatorId: userId,
-			schoolId: params.schoolId,
-		});
-		return entity;
 	}
 }
