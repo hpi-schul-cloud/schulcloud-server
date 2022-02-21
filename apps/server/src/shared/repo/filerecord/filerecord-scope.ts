de2b7a37954fec3b98f2a677d49ca8bcff73c8d3
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2022-02-21 14:05:17 +0100

    BC-1199 - fix test coverage

--- apps/server/src/shared/repo/filerecord/filerecord-scope.ts
+++ apps/server/src/shared/repo/filerecord/filerecord-scope.ts
@@ -1,13 +1,8 @@
-import { EntityId, FileRecord, FileRecordTargetType } from '@shared/domain';
+import { EntityId, FileRecord } from '@shared/domain';
 import { ObjectId } from '@mikro-orm/mongodb';
 import { Scope } from '../scope';
 
 export class FileRecordScope extends Scope<FileRecord> {
-	byTargetType(targetType: FileRecordTargetType): FileRecordScope {
-		this.addQuery({ targetType });
-		return this;
-	}
-
 	byTargetId(targetId: EntityId): FileRecordScope {
 		this.addQuery({ _targetId: new ObjectId(targetId) });
 		return this;
