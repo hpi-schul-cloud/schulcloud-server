a3c2f06cc3465c88b2136f52db720d0b9e9a0d16
Patrick Sachmann <patrick.sachmann@capgemini.com>
2022-03-15 14:06:04 +0100

    EW-124 Removed resolveJsonModule from tsconfig.json

--- apps/server/src/keycloak-user.spec.ts
+++ apps/server/src/keycloak-user.spec.ts
@@ -1,12 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import accounts from '../../../backup/setup/accounts.json';
-import users from '../../../backup/setup/users.json';
-import kcUsers from '../../../backup/keycloak/dBildungscloud-users-0.json';
+import { readFile } from 'fs/promises';
 
 describe('KeycloakUserDataCheck', () => {
+	let accounts: any;
+	let users: any;
+	let kcUsers: any;
+
+	beforeAll(async () => {
+		accounts = JSON.parse(await readFile('./backup/setup/accounts.json', 'utf-8'));
+		users = JSON.parse(await readFile('./backup/setup/users.json', 'utf-8'));
+		kcUsers = JSON.parse(await readFile('./backup/keycloak/dBildungscloud-users-0.json', 'utf-8'));
+	});
+
 	it('Accounts exists', () => {
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 		kcUsers.users
 			.filter((kcUser) => kcUser.username !== 'dbildungscloud') // excludes the realm admin
 			.forEach((kcUser) => {
