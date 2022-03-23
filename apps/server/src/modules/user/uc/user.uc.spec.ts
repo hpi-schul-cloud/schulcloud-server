ebd4114688b9ecca11fe3cf3e4a5e35318d28116
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2022-03-23 16:23:34 +0100

    add test for patch language uc in user
    BC-1471

--- apps/server/src/modules/user/uc/user.uc.spec.ts
+++ apps/server/src/modules/user/uc/user.uc.spec.ts
@@ -1,14 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { PermissionService } from '@shared/domain';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
+
+import { PermissionService, User, LanguageType } from '@shared/domain';
 import { UserRepo } from '@shared/repo';
-import { setupEntities, userFactory, mapUserToCurrentUser } from '@shared/testing';
+import { setupEntities, userFactory } from '@shared/testing';
 import { MikroORM } from '@mikro-orm/core';
+
 import { UserUC } from './user.uc';
 
 describe('UserUc', () => {
 	let service: UserUC;
-	let userRepo: UserRepo;
-	let permissionService: PermissionService;
+	let userRepo: DeepMocked<UserRepo>;
+	let permissionService: DeepMocked<PermissionService>;
 	let orm: MikroORM;
 
 	beforeAll(async () => {
@@ -25,19 +28,11 @@ describe('UserUc', () => {
 				UserUC,
 				{
 					provide: UserRepo,
-					useValue: {
-						findById() {
-							throw new Error('Please write a mock for UserRepo.findById');
-						},
-					},
+					useValue: createMock<UserRepo>(),
 				},
 				{
 					provide: PermissionService,
-					useValue: {
-						resolvePermissions() {
-							throw new Error('Please write a mock for PermissionService.resolvePermissions');
-						},
-					},
+					useValue: createMock<PermissionService>(),
 				},
 			],
 		}).compile();
@@ -51,19 +46,46 @@ describe('UserUc', () => {
 		expect(service).toBeDefined();
 	});
 
-	it('should provide information about the user', async () => {
-		const user = userFactory.build();
-		user.roles.set([]);
-		const currentUser = mapUserToCurrentUser(user);
-		const spyFindUser = jest.spyOn(userRepo, 'findById').mockImplementation(async () => Promise.resolve(user));
-		const spyResolve = jest.spyOn(permissionService, 'resolvePermissions').mockImplementation(() => []);
+	describe('me', () => {
+		let user: User;
 
-		await service.me(currentUser.userId);
+		beforeEach(() => {
+			user = userFactory.buildWithId({ roles: [] });
+			userRepo.findById.mockResolvedValue(user);
+		});
 
-		expect(spyFindUser).toHaveBeenCalled();
-		expect(spyResolve).toHaveBeenCalled();
+		afterEach(() => {
+			userRepo.findById.mockRestore();
+			permissionService.resolvePermissions.mockRestore();
+		});
 
-		spyFindUser.mockRestore();
-		spyResolve.mockRestore();
+		it('should provide information about the passed userId', async () => {
+			await service.me(user.id);
+
+			expect(userRepo.findById).toHaveBeenCalled();
+			expect(permissionService.resolvePermissions).toHaveBeenCalledWith(user);
+		});
+	});
+
+	describe('patchLanguage', () => {
+		let user: User;
+
+		beforeEach(() => {
+			user = userFactory.buildWithId({ roles: [] });
+			userRepo.findById.mockResolvedValue(user);
+			userRepo.persistAndFlush.mockResolvedValue(user);
+		});
+
+		afterEach(() => {
+			userRepo.findById.mockRestore();
+			userRepo.persistAndFlush.mockRestore();
+		});
+
+		it('should patch language auf passed userId', async () => {
+			await service.patchLanguage(user.id, { language: LanguageType.DE });
+
+			expect(userRepo.findById).toHaveBeenCalledWith(user.id);
+			expect(userRepo.persistAndFlush).toHaveBeenCalledWith(user);
+		});
 	});
 });
