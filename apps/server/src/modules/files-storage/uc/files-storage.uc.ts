3a86effbdcba6797a5a3d3e5047c9fd0f9c9f253
SevenWaysDP <sergej.hoffmann@dataport.de>
2022-03-23 16:51:13 +0100

    fixup! bc-1327 - copying of files via file service

--- apps/server/src/modules/files-storage/uc/files-storage.uc.ts
+++ apps/server/src/modules/files-storage/uc/files-storage.uc.ts
@@ -18,6 +18,7 @@ import {
 	SingleFileParams,
 } from '../controller/dto/file-storage.params';
 import { IFile } from '../interface/file';
+import { ICopyFiles } from '../interface';
 
 @Injectable()
 export class FilesStorageUC {
@@ -75,16 +76,7 @@ export class FilesStorageUC {
 	private async uploadFile(userId: EntityId, params: FileRecordParams, fileDescription: IFile) {
 		const [fileRecords] = await this.fileRecordRepo.findBySchoolIdAndParentId(params.schoolId, params.parentId);
 		const fileName = this.checkFilenameExists(fileDescription.name, fileRecords);
-
-		const entity = new FileRecord({
-			size: fileDescription.size,
-			name: fileName,
-			mimeType: fileDescription.mimeType,
-			parentType: params.parentType,
-			parentId: params.parentId,
-			creatorId: userId,
-			schoolId: params.schoolId,
-		});
+		const entity = this.getNewFileRecord(fileName, fileDescription.size, fileDescription.mimeType, params, userId);
 		try {
 			await this.fileRecordRepo.save(entity);
 			const folder = [params.schoolId, entity.id].join('/');
@@ -96,6 +88,19 @@ export class FilesStorageUC {
 			await this.fileRecordRepo.delete(entity);
 			throw error;
 		}
+	}
+
+	private getNewFileRecord(name: string, size: number, mimeType: string, params: FileRecordParams, userId: string) {
+		const entity = new FileRecord({
+			size,
+			name,
+			mimeType,
+			parentType: params.parentType,
+			parentId: params.parentId,
+			creatorId: userId,
+			schoolId: params.schoolId,
+		});
+		return entity;
 	}
 
 	private createPath(schoolId: EntityId, fileRecordId: EntityId): string {
@@ -231,18 +236,53 @@ export class FilesStorageUC {
 		copyFilesParams: CopyFilesOfParentParams
 	): Promise<Counted<FileRecord[]>> {
 		const [fileRecords, count] = await this.fileRecordRepo.findBySchoolIdAndParentId(params.schoolId, params.parentId);
-		if (count > 0) {
-			console.log(copyFilesParams);
+
+		if (count === 0) {
+			throw new NotFoundException();
 		}
 
-		return [fileRecords, count];
+		const newRecords = await this.copy(userId, fileRecords, copyFilesParams.target);
+
+		return newRecords;
 	}
 
 	async copyOneFile(userId: string, params: SingleFileParams, copyFileParams: CopyFileParams) {
 		const fileRecord = await this.fileRecordRepo.findOneById(params.fileRecordId);
-		console.log(copyFileParams);
+		const [newRecord] = await this.copy(userId, [fileRecord], copyFileParams.target);
 
-		return fileRecord;
+		return newRecord[0];
+	}
+
+	private async copy(
+		userId: EntityId,
+		sourceFileRecords: FileRecord[],
+		targetParams: FileRecordParams
+	): Promise<Counted<FileRecord[]>> {
+		this.logger.debug({ action: 'copy', sourceFileRecords, targetParams });
+		let newRecords: FileRecord[] = [];
+		const paths: Array<ICopyFiles> = [];
+
+		newRecords = await Promise.all(
+			sourceFileRecords.map(async (item) => {
+				const entity = this.getNewFileRecord(item.name, item.size, item.mimeType, targetParams, userId);
+				entity.securityCheck = item.securityCheck;
+				await this.fileRecordRepo.save(entity);
+
+				paths.push({
+					sourcePath: [item.schoolId, item.id].join('/'),
+					targetPath: [entity.schoolId, entity.id].join('/'),
+				});
+				return entity;
+			})
+		);
+
+		try {
+			await this.storageClient.copy(paths);
+			return [newRecords, newRecords.length];
+		} catch (error) {
+			await this.fileRecordRepo.delete(newRecords);
+			throw error;
+		}
 	}
 
 	private async restore(fileRecords: FileRecord[]) {
