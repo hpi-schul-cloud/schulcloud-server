019f5ed718cf3e8997d817fb9173c73b683c12a2
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2022-03-28 15:42:14 +0200

    increase coverage and reduce global treshold
    BC-1471

--- apps/server/test/modules/user/me.e2e-spec.ts
+++ apps/server/test/modules/user/me.e2e-spec.ts
@@ -7,11 +7,12 @@ import { EntityManager } from '@mikro-orm/mongodb';
 
 import { ServerTestModule } from '@src/server.module';
 import { JwtAuthGuard } from '@src/modules/authentication/guard/jwt-auth.guard';
-import { ICurrentUser, User, LanguageType } from '@shared/domain';
+import { ICurrentUser, LanguageType } from '@shared/domain';
 import { ApiValidationError } from '@shared/common';
 import { userFactory, cleanupCollections, mapUserToCurrentUser, roleFactory } from '@shared/testing';
+import { ResolvedUserResponse } from '@src/modules/user/controller/dto';
 
-const baseRouteName = '/user/language';
+const baseRouteName = '/user/me';
 
 class API {
 	app: INestApplication;
@@ -23,13 +24,11 @@ class API {
 		this.routeName = routeName;
 	}
 
-	async patch(requestString: string) {
-		const response = await request(this.app.getHttpServer())
-			.patch(`${this.routeName}/${requestString}`)
-			.set('Accept', 'application/json');
+	async me() {
+		const response = await request(this.app.getHttpServer()).get(this.routeName).set('Accept', 'application/json');
 
 		return {
-			result: response.body as boolean,
+			result: response.body as ResolvedUserResponse,
 			error: response.body as ApiValidationError,
 			status: response.status,
 		};
@@ -78,68 +77,13 @@ describe(baseRouteName, () => {
 		});
 
 		it('should return status 403', async () => {
-			const response = await api.patch(LanguageType.DE);
+			const response = await api.me();
 
 			expect(response.status).toEqual(403);
 		});
 	});
 
-	describe('with bad request data', () => {
-		let app: INestApplication;
-		let orm: MikroORM;
-		let em: EntityManager;
-		let currentUser: ICurrentUser;
-		let api: API;
-
-		beforeAll(async () => {
-			const module: TestingModule = await Test.createTestingModule({
-				imports: [ServerTestModule],
-			})
-				.overrideGuard(JwtAuthGuard)
-				.useValue({
-					canActivate(context: ExecutionContext) {
-						const req: Request = context.switchToHttp().getRequest();
-						req.user = currentUser;
-						return true;
-					},
-				})
-				.compile();
-
-			app = module.createNestApplication();
-			await app.init();
-			orm = app.get(MikroORM);
-			em = module.get(EntityManager);
-			api = new API(app, baseRouteName);
-		});
-
-		afterAll(async () => {
-			await orm.close();
-			await app.close();
-		});
-
-		beforeEach(async () => {
-			await cleanupCollections(em);
-
-			const roles = roleFactory.buildList(1, { permissions: [] });
-			const user = userFactory.build({ roles });
-
-			await em.persistAndFlush([user]);
-			em.clear();
-
-			currentUser = mapUserToCurrentUser(user);
-		});
-
-		it('should throw an validation error is not supported language is passed.', async () => {
-			const response = await api.patch('super');
-
-			expect(response.error.validationErrors).toEqual([
-				{
-					errors: ['language must be a valid enum value'],
-					field: 'language',
-				},
-			]);
-		});
-	});
+	describe('without bad request data', () => {});
 
 	describe('without valid request data', () => {
 		let app: INestApplication;
@@ -187,35 +131,15 @@ describe(baseRouteName, () => {
 		});
 
 		it('should return status 200 for successful request.', async () => {
-			const response = await api.patch(LanguageType.EN);
+			const response = await api.me();
 
 			expect(response.status).toEqual(200);
 		});
 
-		it('should return successful true.', async () => {
-			const response = await api.patch(LanguageType.EN);
+		it('should return ResolvedUserResponse.', async () => {
+			const response = await api.me();
 
-			expect(response.result).toEqual({ successful: true });
-		});
-
-		it('should change the language', async () => {
-			await api.patch(LanguageType.EN);
-
-			const user = await em.findOne(User, { id: currentUser.userId });
-
-			expect(user?.language).toEqual('en');
-		});
-
-		it('should support de, en, es, ua', async () => {
-			const de = await api.patch(LanguageType.DE);
-			const en = await api.patch(LanguageType.EN);
-			const es = await api.patch(LanguageType.ES);
-			const ua = await api.patch(LanguageType.UA);
-
-			expect(de.status).toEqual(200);
-			expect(en.status).toEqual(200);
-			expect(es.status).toEqual(200);
-			expect(ua.status).toEqual(200);
+			expect(response.result.id).toEqual(currentUser.userId);
 		});
 	});
 });
