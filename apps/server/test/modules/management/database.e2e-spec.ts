1c2d241ade7c18bdecc24e81b698af7844363e10
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2022-03-31 14:14:57 +0200

    BC-1471 - change import to named path

--- apps/server/test/modules/management/database.e2e-spec.ts
+++ apps/server/test/modules/management/database.e2e-spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { MikroORM } from '@mikro-orm/core';
-import { ManagementTestModule } from '../../../src/modules/management/management.module';
+import { ManagementTestModule } from '@src/modules/management/management.module';
 
 describe('Database Management Controller (e2e)', () => {
 	let app: INestApplication;
