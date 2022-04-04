976e820fbe4fd9627b3e0cccf009b2edd6c2b721
SevenWaysDP <sergej.hoffmann@dataport.de>
2022-04-04 12:17:10 +0200

    BC_1332 - add findAndCount method

--- apps/server/src/shared/repo/base.repo.ts
+++ apps/server/src/shared/repo/base.repo.ts
@@ -1,6 +1,6 @@
 import { EntityManager } from '@mikro-orm/mongodb';
 import { Injectable } from '@nestjs/common';
-import { EntityName, FilterQuery } from '@mikro-orm/core';
+import { EntityName, FilterQuery, FindOptions } from '@mikro-orm/core';
 import { BaseEntity, EntityId } from '@shared/domain';
 
 /**
@@ -26,10 +26,22 @@ export abstract class BaseRepo<T extends BaseEntity> {
 		return entity;
 	}
 
-	async findOne(query: FilterQuery<T>): Promise<T> {
+	async findOne(query: FilterQuery<T>): Promise<T | null> {
+		const entity = await this._em.findOne(this.entityName, query);
+
+		return entity;
+	}
+
+	async findOneOrFail(query: FilterQuery<T>): Promise<T> {
 		const entity = await this._em.findOneOrFail(this.entityName, query);
 
 		return entity;
+	}
+
+	async findAndCount(query: FilterQuery<T>, options?: FindOptions<T>): Promise<[T[], number]> {
+		const [entities, count] = await this._em.findAndCount(this.entityName, query, options);
+
+		return [entities, count];
 	}
 
 	async populate(entities: T[], keys: string[]) {
