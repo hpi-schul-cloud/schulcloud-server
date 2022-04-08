f101bab92e86849632d75ae1d1c03aec923a1404
Simone Radtke <simone.radtke@capgemini.com>
2022-04-08 11:51:57 +0200

    EW-166 Add 'get' endpoint

--- apps/server/src/modules/authentication/controller/account.controller.ts
+++ apps/server/src/modules/authentication/controller/account.controller.ts
@@ -1,12 +1,13 @@
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
-import { Body, Controller, Param, Patch } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Query } from '@nestjs/common';
 import { ICurrentUser } from '@shared/domain';
 
 import { Authenticate, CurrentUser } from '@src/modules/authentication/decorator/auth.decorator';
 import { ParseObjectIdPipe } from '@shared/controller';
 import { AccountUc } from '../uc/account.uc';
 import { ChangePasswordParams, PatchMyAccountParams, PatchMyPasswordParams } from './dto';
+import { AccountResponse } from './dto/account.response';
 
 @ApiTags('account')
 @Authenticate('jwt')
@@ -34,5 +35,13 @@ export class AccountController {
 		@Body() params: PatchMyPasswordParams
 	): Promise<void> {
 		return this.accountUc.replaceMyTemporaryPassword(currentUser.userId, params.password, params.confirmPassword);
+	}
+
+	@ApiOperation({ description: 'Finds accounts, currently only by UserId' })
+	@Get()
+	async findAccount(@Query('userId') userId: string): Promise<AccountResponse> {
+		const accountEntity = await this.accountUc.findByUserId(userId);
+		const response = new AccountResponse(accountEntity.id);
+		return response;
 	}
 }
