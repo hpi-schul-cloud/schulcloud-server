15907b41c8e08c946542d0e74c0fbc65ea81ea93
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2022-04-08 15:33:43 +0200

    remove unused file

--- apps/server/src/core/interfaces/core-modul-config.ts
+++ apps/server/src/core/interfaces/core-modul-config.ts
@@ -1,5 +1,6 @@
 import { IInterceptorConfig } from '@shared/common';
 
+// Add additional interfaces over extends Interface1, Interface2, ..
 export interface ICoreModuleConfig extends IInterceptorConfig {
 	INCOMING_REQUEST_TIMEOUT: number;
 }
--- apps/server/src/shared/common/interceptor/constants.ts
+++
@@ -1,5 +0,0 @@
-import config = require('@hpi-schul-cloud/commons');
-
-const { Configuration } = config;
-
-export const INCOMING_REQUEST_TIMEOUT_API = Configuration.get('INCOMING_REQUEST_TIMEOUT_API') as number;
