d082b2e9a370f4e845d6d2e67be09db4313d5bbd
Thomas Feldtkeller <thomas.feldtkeller@dataport.de>
2022-04-22 11:27:42 +0200

    remove LockedTask
    we recently changed the permissions of course teachers to be allowed to
    access private tasks within their courses. Due to that change, there is
    no longer a need for a class representing tasks the teacher may know
    about, but not open.

---
+++ apps/server/src/modules/learnroom/controller/dto/board/board-task.response.ts
@@ -0,0 +1,46 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { DecodeHtmlEntities } from '@shared/controller';
+import { BoardTaskStatusResponse } from './board-task-status.response';
+
+export class BoardTaskResponse {
+	constructor({ id, name, createdAt, updatedAt, status }: BoardTaskResponse) {
+		this.id = id;
+		this.name = name;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+		this.status = status;
+	}
+
+	@ApiProperty()
+	id: string;
+
+	@ApiProperty()
+	@DecodeHtmlEntities()
+	name: string;
+
+	@ApiPropertyOptional()
+	availableDate?: Date;
+
+	@ApiPropertyOptional()
+	duedate?: Date;
+
+	@ApiPropertyOptional()
+	@DecodeHtmlEntities()
+	courseName?: string;
+
+	@ApiPropertyOptional()
+	@DecodeHtmlEntities()
+	description?: string;
+
+	@ApiPropertyOptional()
+	displayColor?: string;
+
+	@ApiProperty()
+	createdAt: Date;
+
+	@ApiProperty()
+	updatedAt: Date;
+
+	@ApiProperty()
+	status: BoardTaskStatusResponse;
+}
