e107dafcfae112178c0ed95ceeb9ed99c58646fd
SevenWaysDP <sergej.hoffmann@dataport.de>
2022-04-27 17:31:21 +0200

    fix add Logger as provider

--- apps/server/src/core/logger/logger.module.ts
+++ apps/server/src/core/logger/logger.module.ts
@@ -1,7 +1,6 @@
-import { Global, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { Logger } from './logger.service';
 
-@Global()
 @Module({
 	providers: [Logger],
 	exports: [Logger],
