7c78b9e8c90bf989b015b0ddaaa41b74da077a57
Marvin Öhlerking <marvin.oehlerking@capgemini.com>
2022-05-04 08:52:27 +0200

    test setup for oauth2 token

---
+++ test/services/oauth2/hooks/index.test.js
@@ -0,0 +1,80 @@
+const { expect } = require('chai');
+const appPromise = require('../../../../src/app');
+const testObjects = require('../../helpers/testObjects')(appPromise);
+
+const { setIdToken } = require('../../../../src/services/oauth2/hooks/index');
+
+describe.only('oauth2 token test', () => {
+	let app;
+	let server;
+
+	before(async () => {
+		app = await appPromise;
+		server = app.listen(0);
+	});
+
+	after(() => {
+		server.close();
+	});
+
+	it('scope groups set', async () => {
+        const testTeam = await testObjects.createTestTeamWithOwner();
+        const testTool = await testObjects.createTestLtiTool();
+
+        const result = await setIdToken({
+            app,
+            params: {
+                consentRequest:{
+                    requested_scope: "groups",
+                    client: {
+                        client_id: testTool.oAuthClientId
+                    }
+                },
+                account: {
+                    userId: testTeam.user._id
+                },
+                query: {
+                    accept: true
+                }
+            },
+            data:{
+                session:{}
+            }
+        });
+
+        expect(result).to.not.equal(undefined);
+        expect(result.data.session.id_token.groups).to.not.equal(undefined);
+	});
+
+	it('scope groups unset', async () => {
+        const testTeam = await testObjects.createTestTeamWithOwner();
+        const testTool = await testObjects.createTestLtiTool({
+            oAuthClientId: "123",
+            isLocal: true
+        });
+        
+        const result = await setIdToken({
+            app,
+            params: {
+                consentRequest:{
+                    requested_scope: "",
+                    client: {
+                        client_id: testTool.oAuthClientId
+                    }
+                },
+                account: {
+                    userId: testTeam.user._id
+                },
+                query: {
+                    accept: true
+                }
+            },
+            data:{
+                session:{}
+            }
+        });
+
+        expect(result).to.not.equal(undefined);
+        expect(result.data.session.id_token.groups).to.equal(undefined);
+	});
+});
