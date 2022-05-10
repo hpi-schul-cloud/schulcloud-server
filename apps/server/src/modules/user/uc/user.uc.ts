25464c6563fed57529c3257bab84492107024494
Simone Radtke <simone.radtke@capgemini.com>
2022-05-10 23:36:04 +0200

    EW-166 Get endpoint and find method to user module

--- apps/server/src/modules/user/uc/user.uc.ts
+++ apps/server/src/modules/user/uc/user.uc.ts
@@ -1,16 +1,22 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { EntityId, LanguageType, PermissionService, User } from '@shared/domain';
+import { ForbiddenOperationError } from '@shared/common';
+import { Actions, EntityId, ICurrentUser, LanguageType, PermissionService, User } from '@shared/domain';
 import { UserRepo } from '@shared/repo';
-import { ChangeLanguageParams } from '../controller/dto';
+import { AccountService } from '@src/modules/authentication/services/account.service';
+import { AuthorizationService } from '@src/modules/authorization';
+import { AccountResponse, ChangeLanguageParams } from '../controller/dto';
 import { IUserConfig } from '../interfaces';
+import { AccountResponseMapper } from '../mapper/account-response.mapper';
 
 @Injectable()
 export class UserUC {
 	constructor(
 		private readonly userRepo: UserRepo,
 		private readonly permissionService: PermissionService,
-		private readonly configService: ConfigService<IUserConfig, true>
+		private readonly configService: ConfigService<IUserConfig, true>,
+		private readonly authorizationService: AuthorizationService,
+		private readonly accountService: AccountService
 	) {}
 
 	async me(userId: EntityId): Promise<[User, string[]]> {
@@ -33,5 +39,30 @@ export class UserUC {
 		await this.userRepo.save(user);
 
 		return true;
+	}
+
+	/**
+	 * This method processes the GET request on the user/:id/account endpoint from the user controller
+	 * @param currentUser the request user
+	 * @param id the request parameter
+	 * @throws {ForbiddenOperationError}
+	 * @throws {NotImplementedException}
+	 * @throws {EntityNotFoundError}
+	 */
+	async findAccountByUserId(currentUser: ICurrentUser, id: string): Promise<AccountResponse | null> {
+		const executingUser = await this.userRepo.findById(currentUser.userId, true);
+		const targetUser = await this.userRepo.findById(id, true);
+
+		const permission = this.authorizationService.hasPermission(executingUser, targetUser, Actions.read);
+
+		if (!permission) {
+			throw new ForbiddenOperationError('Current user is not authorized to search for accounts by user id.');
+		}
+
+		const account = await this.accountService.findByUserId(id);
+		if (!account) {
+			return null;
+		}
+		return AccountResponseMapper.mapToResponse(account);
 	}
 }
--- apps/server/src/modules/user/user.module.ts
+++ apps/server/src/modules/user/user.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PermissionService } from '@shared/domain';
-import { UserRepo } from '@shared/repo';
+import { AccountRepo, SystemRepo, UserRepo } from '@shared/repo';
+import { AccountService } from '../authentication/services/account.service';
+import { AuthorizationModule } from '../authorization';
 import { UserController } from './controller';
 import { UserUC } from './uc';
 
 @Module({
+	imports: [AuthorizationModule],
 	controllers: [UserController],
-	providers: [UserRepo, PermissionService, UserUC],
+	providers: [UserRepo, PermissionService, UserUC, AccountService, AccountRepo, SystemRepo],
 })
 export class UserModule {}
