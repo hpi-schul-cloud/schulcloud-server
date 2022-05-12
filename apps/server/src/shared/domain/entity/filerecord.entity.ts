ec83b35d898c9c55e964fb823fbf7b32d6a2cf1d
SevenWaysDP <97111299+SevenWaysDP@users.noreply.github.com>
2022-05-12 17:35:18 +0200

    Feature/bc 1499 load authorization into file service (#3441)
    * BC-1499 - refactoring
    
    - remove unused module
    - change logic of get user
    
    Co-authored-by: Cedric Evers <12080057+CeEv@users.noreply.github.com>

--- apps/server/src/shared/domain/entity/filerecord.entity.ts
+++ apps/server/src/shared/domain/entity/filerecord.entity.ts
@@ -1,8 +1,8 @@
-import { Entity, Enum, Property, Index, Embeddable, Embedded } from '@mikro-orm/core';
-import { v4 as uuid } from 'uuid';
+import { Embeddable, Embedded, Entity, Enum, Index, Property } from '@mikro-orm/core';
 import { ObjectId } from '@mikro-orm/mongodb';
-import { BaseEntityWithTimestamps } from './base.entity';
+import { v4 as uuid } from 'uuid';
 import type { EntityId } from '../types/entity-id';
+import { BaseEntityWithTimestamps } from './base.entity';
 
 export enum ScanStatus {
 	PENDING = 'pending',
@@ -15,10 +15,7 @@ export enum FileRecordParentType {
 	'User' = 'users',
 	'School' = 'schools',
 	'Course' = 'courses',
-	'Team' = 'teams',
-	'DashboardModel' = 'dashboard', // without s
 	'Task' = 'tasks',
-	// card
 }
 export interface IFileSecurityCheckProperties {
 	status?: ScanStatus;
