dbf0cf10f65dd0ccd6c5a604cbc5d0d8da3fb67e
Thomas Feldtkeller <thomas.feldtkeller@dataport.de>
2022-05-17 11:26:44 +0200

    Bc 1551 task copy (#3459)
    * task copy service
    
    service to determine which parts of a task need to be copied
    
    * extract types aroud copying
    
    * implement copy uc
    
    * Add controller and mapper for copy task service
    including tests
    
    Copy function is given via the task API and received parameter
    as well as send parameter are mapped via dto. Consistent naming
    is provided as well as authentication via jwt.
    
    * add e2e test
    
    Co-authored-by: MartinSchuhmacher <mschuhmacher@gmx.de>
    Co-authored-by: Martin Schuhmacher <55735359+MartinSchuhmacher@users.noreply.github.com>

--- apps/server/src/modules/task/controller/dto/index.ts
+++ apps/server/src/modules/task/controller/dto/index.ts
@@ -1,2 +1,3 @@
-export * from './task.response';
+export * from './task-copy.response';
 export * from './task-status.response';
+export * from './task.response';
--- apps/server/src/modules/task/task.module.ts
+++ apps/server/src/modules/task/task.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TaskCopyService } from '@shared/domain/service/task-copy.service';
 import { CourseRepo, LessonRepo, TaskRepo } from '@shared/repo';
 import { AuthorizationModule } from '../authorization';
 import { TaskController } from './controller/task.controller';
 import { TaskUC } from './uc';
+import { TaskCopyUC } from './uc/task-copy.uc';
 
 @Module({
 	imports: [AuthorizationModule],
 	controllers: [TaskController],
-	providers: [TaskUC, TaskRepo, LessonRepo, CourseRepo],
+	providers: [TaskUC, TaskRepo, LessonRepo, CourseRepo, TaskCopyUC, TaskCopyService],
 })
 export class TaskModule {}
