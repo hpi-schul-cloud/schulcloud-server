f44e70df7bb8346717c4aa859cdcd61a1a7c8f7a
Martin Schuhmacher <55735359+MartinSchuhmacher@users.noreply.github.com>
2022-05-20 10:42:37 +0200

    BC-1556 - Course copy service (Backend) (#3468)
    * implement unfinished version of course copy service
    
    * add id to status during course copy UC
    
    * add e2e test for copy course

---
+++ apps/server/src/shared/domain/service/course-copy.service.ts
@@ -0,0 +1,78 @@
+import { Injectable } from '@nestjs/common';
+import { Course, User } from '@shared/domain/entity';
+import { CopyElementType, CopyStatusDTO, CopyStatusEnum } from '@shared/domain/types';
+
+export type CourseCopyParams = {
+	originalCourse: Course;
+	user: User;
+};
+
+export type CourseCopyResponse = {
+	copy: Course;
+	status: CopyStatusDTO;
+};
+
+@Injectable()
+export class CourseCopyService {
+	copyCourseMetadata(params: CourseCopyParams): CourseCopyResponse {
+		const copy = new Course({
+			school: params.user.school,
+			name: params.originalCourse.name,
+			color: params.originalCourse.color,
+			teachers: [params.user],
+		});
+		const status = {
+			title: copy.name,
+			type: CopyElementType.COURSE,
+			status: CopyStatusEnum.SUCCESS,
+			elements: [
+				{
+					title: 'substitutionTeachers',
+					type: CopyElementType.LEAF,
+					status: CopyStatusEnum.NOT_DOING,
+				},
+				{
+					title: 'students',
+					type: CopyElementType.LEAF,
+					status: CopyStatusEnum.NOT_DOING,
+				},
+				{
+					title: 'classes',
+					type: CopyElementType.LEAF,
+					status: CopyStatusEnum.NOT_DOING,
+				},
+				{
+					title: 'ltiTools',
+					type: CopyElementType.LEAF,
+					status: CopyStatusEnum.NOT_DOING,
+				},
+				{
+					title: 'tasks',
+					type: CopyElementType.TASK,
+					status: CopyStatusEnum.NOT_IMPLEMENTED,
+				},
+				{
+					title: 'times',
+					type: CopyElementType.LEAF,
+					status: CopyStatusEnum.NOT_IMPLEMENTED,
+				},
+				{
+					title: 'lessons',
+					type: CopyElementType.LEAF,
+					status: CopyStatusEnum.NOT_IMPLEMENTED,
+				},
+				{
+					title: 'files',
+					type: CopyElementType.FILE,
+					status: CopyStatusEnum.NOT_IMPLEMENTED,
+				},
+				{
+					title: 'coursegroups',
+					type: CopyElementType.LEAF,
+					status: CopyStatusEnum.NOT_IMPLEMENTED,
+				},
+			],
+		};
+		return { copy, status };
+	}
+}
