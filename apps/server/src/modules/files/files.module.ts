06b5179cbb36839b89b63b3bb08052e0044df9dd
SevenWaysDP <sergej.hoffmann@dataport.de>
2022-05-31 18:27:57 +0200

    BC-1777 - fix resatrt loop

--- apps/server/src/modules/files/files.module.ts
+++ apps/server/src/modules/files/files.module.ts
@@ -1,7 +1,11 @@
 import { Configuration } from '@hpi-schul-cloud/commons/lib';
-import { Module } from '@nestjs/common';
+import { Dictionary, IPrimaryKey } from '@mikro-orm/core';
+import { MikroOrmModule, MikroOrmModuleSyncOptions } from '@mikro-orm/nestjs';
+import { Module, NotFoundException } from '@nestjs/common';
+import { ALL_ENTITIES } from '@shared/domain';
 import { FileStorageAdapter } from '@shared/infra/filestorage';
 import { FileRecordRepo, FilesRepo, StorageProviderRepo } from '@shared/repo';
+import { DB_PASSWORD, DB_URL, DB_USERNAME } from '@src/config';
 import { LoggerModule } from '@src/core/logger';
 import { S3Config } from '../files-storage/interface';
 import { DeleteFilesConsole } from './job/delete-files.console';
@@ -19,8 +23,31 @@ export const config: S3Config = {
 	secretAccessKey: Configuration.get('FILES_STORAGE__S3_SECRET_ACCESS_KEY') as string,
 };
 
+export const defaultMikroOrmOptions: MikroOrmModuleSyncOptions = {
+	findOneOrFailHandler: (entityName: string, where: Dictionary | IPrimaryKey) => {
+		// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+		return new NotFoundException(`The requested ${entityName}: ${where} has not been found.`);
+	},
+};
+
 @Module({
-	imports: [LoggerModule],
+	imports: [
+		LoggerModule,
+		MikroOrmModule.forRoot({
+			...defaultMikroOrmOptions,
+			// TODO repeats server module definitions
+			type: 'mongo',
+			clientUrl: DB_URL,
+			password: DB_PASSWORD,
+			user: DB_USERNAME,
+			entities: ALL_ENTITIES,
+			allowGlobalContext: true,
+			findOneOrFailHandler: (entityName: string, where: Dictionary | IPrimaryKey) => {
+				// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+				return new NotFoundException(`The requested ${entityName}: ${where} has not been found.`);
+			},
+		}),
+	],
 	providers: [
 		DeleteFilesConsole,
 		SyncFilerecordsConsole,
