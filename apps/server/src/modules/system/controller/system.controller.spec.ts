a01d9318a127032f8b62e01f535d367e0fe83fb3
agnisa <arne.gnisa@capgemini.com>
2022-06-14 13:32:16 +0200

    nest_tests_cov extended

--- apps/server/src/modules/system/controller/system.controller.spec.ts
+++ apps/server/src/modules/system/controller/system.controller.spec.ts
@@ -1,17 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SystemController } from '@src/modules/system/controller/system.controller';
 import { SystemUc } from '@src/modules/system/uc/system.uc';
+import { OauthResponse } from '@src/modules/system/controller/dto/oauth.response';
 
 describe('system.controller', () => {
 	let module: TestingModule;
 	let controller: SystemController;
+	const mockResponse: OauthResponse = new OauthResponse([]);
+
 	beforeAll(async () => {
 		module = await Test.createTestingModule({
 			providers: [
 				SystemController,
 				{
 					provide: SystemUc,
-					useValue: {},
+					useValue: {
+						findOauthConfigs: jest.fn().mockImplementation(() => Promise.resolve(mockResponse)),
+					},
 				},
 			],
 		}).compile();
@@ -20,5 +25,12 @@ describe('system.controller', () => {
 
 	it('should be defined', () => {
 		expect(controller).toBeDefined();
+	});
+
+	describe('findOauthConfigs', () => {
+		it('should return oauthresponse', async () => {
+			const resultConfigs = await controller.findOauthConfigs();
+			expect(resultConfigs).toBe(mockResponse);
+		});
 	});
 });
