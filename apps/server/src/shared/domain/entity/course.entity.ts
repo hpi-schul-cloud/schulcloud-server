0614b7a3d1b594052581b3b00620b4c7180517cb
MartinSchuhmacher <mschuhmacher@gmx.de>
2022-06-21 15:54:29 +0200

    adjust shorttitle pattern and test

--- apps/server/src/shared/domain/entity/course.entity.ts
+++ apps/server/src/shared/domain/entity/course.entity.ts
@@ -107,7 +107,7 @@ export class Course extends BaseEntityWithTimestamps implements ILearnroom, IEnt
 
 	private getShortTitle(): string {
 		const [firstChar, secondChar] = [...this.name];
-		const pattern = /[\p{Extended_Pictographic}\u{1F3FB}-\u{1F3FF}\u{1F9B0}-\u{1F9B3}]/u;
+		const pattern = /\p{Extended_Pictographic}/u;
 		if (pattern.test(firstChar)) {
 			return firstChar;
 		}
