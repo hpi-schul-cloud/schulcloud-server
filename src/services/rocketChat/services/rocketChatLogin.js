5565eccc896feb598de664ed2109f1e7280cc18c
SevenWaysDP <sergej.hoffmann@dataport.de>
2022-06-21 08:19:28 +0200

    fixup! BC-1809 - add await

--- src/services/rocketChat/services/rocketChatLogin.js
+++ src/services/rocketChat/services/rocketChatLogin.js
@@ -27,8 +27,11 @@ class RocketChatLogin {
 			if (authToken !== '' && authToken !== undefined) {
 				try {
 					const res = await service.me(authToken, rcAccount.rcId);
-					await service.setUserStatus(authToken, rcAccount.rcId, 'offline');
-					if (res.success) return { authToken };
+					if (res.success) {
+						await service.setUserStatus(authToken, rcAccount.rcId, 'offline');
+						return { authToken };
+					}
+					return Promise.reject(new BadRequest('False response data from rocketChat'));
 				} catch (err) {
 					logger.error(err);
 					authToken = '';
