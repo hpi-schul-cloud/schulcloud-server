43ea4cba4073464ebcf6fd21c223846ae83b14b6
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2022-06-22 11:05:06 +0200

    Fix strict location of args and resort code.

--- scripts/generate-open-api-client.js
+++ scripts/generate-open-api-client.js
@@ -5,40 +5,62 @@ const { exec } = require('child_process');
 
 const { log, error } = console;
 
-const args = arg(
-	{
-		'--help': Boolean,
-		'-h': '--help',
+const readArgs = () => {
+	const args = arg(
+		{
+			'--help': Boolean,
+			'-h': '--help',
 
-		'--url': String,
-		'-u': '--url',
+			'--url': String,
+			'-u': '--url',
 
-		'--path': String,
-		'-p': '--path',
-	},
-	{
-		argv: process.argv.slice(2),
+			'--path': String,
+			'-p': '--path',
+		},
+		{
+			argv: process.argv.slice(2),
+		}
+	);
+
+	if ('--help' in args) {
+		log(`Usage: node generate-client.js [opts]
+	OPTIONS:
+		--help (-h)		Show this help.
+		--path (-p)		Path to the newly created client's directory.
+		--url (-u)		URL/path to the spec file in yml/json format.
+	`);
+		process.exit(0);
 	}
-);
 
-if ('--help' in args) {
-	log(`Usage: node generate-client.js [opts]
-OPTIONS:
-	--help (-h)		Show this help.
-	--path (-p)		Path to the newly created client's directory.
-	--url (-u)		URL/path to the spec file in yml/json format.
-`);
-	process.exit(0);
-}
-
-const config = {
-	/** url to load the open-api definition from */
-	url: args._[1],
-	/** folder to save the open-api client */
-	path: args._[3],
+	return args;
 };
 
-log('Create config from arguments', config, args);
+const extractArg = (args, shortArg, longArg) => {
+	const index = args._.findIndex((a) => a === shortArg || a === longArg);
+	if (index === -1) {
+		throw new Error(`The parameter ${longArg} or  ${longArg} is required.`);
+	}
+
+	const value = args._[index + 1];
+
+	return value;
+};
+
+const extractConfig = (args) => {
+	const url = extractArg(args, '-u', '--url');
+	const path = extractArg(args, '-p', '--path');
+
+	const config = {
+		/** url to load the open-api definition from */
+		url,
+		/** folder to save the open-api client */
+		path,
+	};
+
+	log('Create config from arguments', config, args);
+
+	return config;
+};
 
 const errorMessageContains = (includedString, error) =>
 	error && error.message && typeof error.message === 'string' && error.message.includes(includedString);
@@ -59,8 +81,7 @@ const asyncExec = (command) =>
 		});
 	});
 
-const generateClient = () => {
-	const cmd = getOpenApiCommand(config);
+const generateClient = (config, cmd) => {
 	log(`Try updating the openapi client in the folder ${config.path} from ${config.url} ...`);
 	asyncExec(cmd)
 		.then((stdout) => log(stdout))
@@ -72,7 +93,10 @@ const generateClient = () => {
 };
 
 const main = () => {
-	generateClient();
+	const args = readArgs();
+	const config = extractConfig(args);
+	const cmd = getOpenApiCommand(config);
+	generateClient(config, cmd);
 };
 
 main();
