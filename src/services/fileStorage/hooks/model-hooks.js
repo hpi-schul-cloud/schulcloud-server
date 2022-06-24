e3dbce7c9d2cfe088cd4131f3853386da490312e
hoeppner-dataport <marcus.hoeppner@dataport.de>
2022-06-24 10:17:17 +0200

    chore: added hacky way to log,lacking alternatives

--- src/services/fileStorage/hooks/model-hooks.js
+++ src/services/fileStorage/hooks/model-hooks.js
@@ -26,8 +26,23 @@ const restrictToCurrentUser = (hook) => {
 };
 
 const handleShareCode = (hook) => {
+	if (hook.params === undefined) {
+		throw new Forbidden('hook.params');
+	}
+	if (hook.params.query === undefined) {
+		throw new Forbidden('hook.params.query === undefined');
+	}
+	if (hook.params.query.shareToken === undefined) {
+		throw new Forbidden('hook.params.query.shareToken === undefined');
+	}
+	if (hook.result === undefined) {
+		throw new Forbidden('hook.result === undefined');
+	}
+	if (hook.result.shareToken === undefined) {
+		throw new Forbidden('hook.result.shareToken === undefined');
+	}
 	if (hook.params.query.shareToken && hook.result.shareToken !== hook.params.query.shareToken) {
-		throw new Forbidden('Invalid shareToken.');
+		throw new Forbidden('Invalid share token.');
 	}
 	return hook;
 };
