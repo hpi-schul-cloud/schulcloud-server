bbb7e56cd46cb8f22aa9ebd6348a46e4bcc855bf
hoeppner-dataport <marcus.hoeppner@dataport.de>
2022-06-24 10:15:40 +0200

    chore: refactored tests to increase readability

--- test/services/fileStorage/model-service.test.js
+++ test/services/fileStorage/model-service.test.js
@@ -19,72 +19,59 @@ describe('files model service', () => {
 		server.close(done);
 	});
 
-	describe('get', () => {
-		it('should return error when file does not exist', async () => {
-			await testObjects.createTestRole({
-				name: 'studentList',
-				permissions: ['STUDENT_LIST', 'FILESTORAGE_VIEW'],
-			});
-			const user = await testObjects.createTestUser({ roles: ['teacher'] });
-			const params = await testObjects.generateRequestParamsFromUser(user);
-			try {
-				await modelService.get(new ObjectId(), params);
-				throw new Error('should have failed');
-			} catch (err) {
-				expect(err.message).to.not.equal('should have failed');
-				expect(err.code).to.equal(404);
-			}
-		});
-
-		it('should return file when filtering by correct share code', async () => {
+	describe.only('get', () => {
+		const setup = async () => {
 			const owner = await testObjects.createTestUser();
 			const file = await testObjects.createTestFile({ owner: owner._id, shareToken: 'abc' });
 
-			await testObjects.createTestRole({
-				name: 'studentList',
-				permissions: ['STUDENT_LIST', 'FILESTORAGE_VIEW'],
-			});
 			const user = await testObjects.createTestUser({ roles: ['teacher'] });
-			const params = await testObjects.generateRequestParamsFromUser(user);
-			params.query = { shareToken: 'abc' };
-			const result = await modelService.get(file._id, params);
-			expect(result._id.toString()).to.equal(file._id.toString());
+			const userRequestParams = await testObjects.generateRequestParamsFromUser(user);
+			return { file, userRequestParams };
+		};
+
+		describe('block file requests', () => {
+			it('should return error when file does not exist', async () => {
+				const { userRequestParams } = await setup({ shareToken: 'abc' });
+
+				try {
+					await modelService.get(new ObjectId(), userRequestParams);
+					throw new Error('should have failed');
+				} catch (err) {
+					expect(err.message).to.not.equal('should have failed');
+					expect(err.code).to.equal(404);
+				}
+			});
+
+			it('should return error when filtering by wrong share token', async () => {
+				const { file, userRequestParams } = await setup();
+
+				userRequestParams.query = { shareToken: 'def' };
+				try {
+					await modelService.get(file._id, userRequestParams);
+					throw new Error('should have failed');
+				} catch (err) {
+					expect(err.message).to.not.equal('should have failed');
+					expect(err.code).to.equal(403);
+				}
+			});
 		});
 
-		it('should return error when filtering by wrong share code', async () => {
-			const owner = await testObjects.createTestUser();
-			const file = await testObjects.createTestFile({ owner: owner._id, shareToken: 'abc' });
+		describe('answer file requests', () => {
+			it('should return file when not filtering by share token', async () => {
+				// these requests should only come so farä, when the user is the owner
+				const { file, userRequestParams } = await setup();
 
-			await testObjects.createTestRole({
-				name: 'studentList',
-				permissions: ['STUDENT_LIST', 'FILESTORAGE_VIEW'],
-			});
-			const user = await testObjects.createTestUser({ roles: ['teacher'] });
-			const params = await testObjects.generateRequestParamsFromUser(user);
-			params.query = { shareToken: 'def' };
-			try {
-				await modelService.get(file._id, params);
-				throw new Error('should have failed');
-			} catch (err) {
-				expect(err.message).to.not.equal('should have failed');
-				expect(err.code).to.equal(403);
-			}
-		});
-
-		it('should return file when not filtering by share code', async () => {
-			const owner = await testObjects.createTestUser();
-			const file = await testObjects.createTestFile({ owner: owner._id, shareToken: 'abc' });
-
-			await testObjects.createTestRole({
-				name: 'studentList',
-				permissions: ['STUDENT_LIST', 'FILESTORAGE_VIEW'],
+				const result = await modelService.get(file._id, userRequestParams);
+				expect(result._id.toString()).to.equal(file._id.toString());
 			});
 
-			const user = await testObjects.createTestUser({ roles: ['teacher'] });
-			const params = await testObjects.generateRequestParamsFromUser(user);
+			it('should return file when filtering by correct share token', async () => {
+				const { file, userRequestParams } = await setup();
 
-			const result = await modelService.get(file._id, params);
-			expect(result._id.toString()).to.equal(file._id.toString());
+				userRequestParams.query = { shareToken: 'abc' };
+				const result = await modelService.get(file._id, userRequestParams);
+				expect(result._id.toString()).to.equal(file._id.toString());
+			});
 		});
 	});
 });
