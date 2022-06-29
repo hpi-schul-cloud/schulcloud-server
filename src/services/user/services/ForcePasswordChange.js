ed8d7776a5d989422730e7069af8d904b764053e
thormann <timm.hoermann@capgemini.com>
2022-06-29 09:59:09 +0200

    EW-181 move password change logic

--- src/services/user/services/ForcePasswordChange.js
+++ src/services/user/services/ForcePasswordChange.js
@@ -1,9 +1,7 @@
 const { disallow } = require('feathers-hooks-common');
 const { authenticate } = require('@feathersjs/authentication');
-const bcrypt = require('bcryptjs');
 
 const { BadRequest } = require('../../../errors');
-const { passwordsMatch } = require('../../../utils/passwordHelpers');
 
 const addUserForcedToChangePasswordFlag = (context) => {
 	context.params.userForcedToChangePassword = true;
@@ -32,39 +30,14 @@ const ForcePasswordChangeServiceHooks = {
 };
 
 class ForcePasswordChangeService {
-	constructor(options) {
-		this.options = options || {};
-		this.err = Object.freeze({
-			missmatch: 'Die neuen Passwörter stimmen nicht überein.',
-			failed: 'Can not update the password. Please contact the administrator',
-			passwordSameAsPrevious: 'You need to setup your new unique password',
-		});
-	}
-
 	async newPasswordProvidedByUser(data, params) {
+		const currentUserId = params.account.userId;
 		const newPassword = data['password-1'];
-		if (!passwordsMatch(newPassword, data['password-2'])) {
-			throw new BadRequest(this.err.missmatch);
-		}
-
-		const passwordSameAsPrevious = await bcrypt.compare(newPassword, params.account.password);
-		if (passwordSameAsPrevious) {
-			throw new BadRequest(this.err.passwordSameAsPrevious);
-		}
+		const newPasswordConfirm = data['password-2'];
 
 		await this.app
-			.service('nest-account-service')
-			.save({
-				id: params.account._id.toString(),
-				password: newPassword,
-			})
-			.catch((err) => {
-				throw new BadRequest(this.err.failed, err);
-			});
-
-		const userPromise = this.app.service('/users').patch(params.account.userId, { forcePasswordChange: false });
-		return userPromise
-			.then((result) => Promise.resolve(result))
+			.service('nest-account-uc')
+			.replaceMyTemporaryPassword(currentUserId, newPassword, newPasswordConfirm)
 			.catch((err) => {
 				throw new BadRequest(this.err.failed, err);
 			});
