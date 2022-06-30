2b4ba68983506cad003d2a5c2c8c6b0ea2d6e58c
Marvin Öhlerking <marvin.oehlerking@capgemini.com>
2022-06-30 08:57:06 +0200

    simplified return to a single line

--- apps/server/src/modules/school/service/school.service.ts
+++ apps/server/src/modules/school/service/school.service.ts
@@ -10,12 +10,19 @@ export class SchoolService {
 
 	async saveSchool(schoolDto: SchoolDto): Promise<SchoolDto> {
 		const school: School = SchoolMapper.mapToEntity(schoolDto);
+
+		let savedSchool: School;
 		if (school.id) {
 			const entity: School = await this.schoolRepo.findById(school.id);
-			SchoolMapper.mapEntityToEntity(entity, school);
-			await this.schoolRepo.save(entity);
-			return SchoolMapper.mapToDto(school);
+			const patchedEntity = SchoolMapper.mapEntityToEntity(entity, school);
+
+			await this.schoolRepo.save(patchedEntity);
+
+			savedSchool = patchedEntity;
+		} else {
+			savedSchool = await this.schoolRepo.createAndSave(school);
 		}
-		return SchoolMapper.mapToDto(await this.schoolRepo.createAndSave(school));
+
+		return SchoolMapper.mapToDto(savedSchool);
 	}
 }
