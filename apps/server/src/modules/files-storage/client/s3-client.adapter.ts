6b98db33418c6af95d909825c5590c468bd2a5fe
SevenWaysDP <sergej.hoffmann@dataport.de>
2022-07-06 14:17:12 +0200

    BC-2095 - fix error if file not found

--- apps/server/src/modules/files-storage/client/s3-client.adapter.ts
+++ apps/server/src/modules/files-storage/client/s3-client.adapter.ts
@@ -108,6 +108,11 @@ export class S3ClientAdapter implements IStorageClient {
 
 			return result;
 		} catch (err) {
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+			if (err.Code && err.Code === 'NoSuchKey') {
+				this.logger.log(`could not find one of the files for deletion with ids ${paths.join(',')}`);
+				return [];
+			}
 			throw new InternalServerErrorException(err, 'S3ClientAdapter:delete');
 		}
 	}
