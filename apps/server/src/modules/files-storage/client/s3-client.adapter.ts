87ee185b29a23d8740a59bc045becba84f5c4acd
SevenWaysDP <sergej.hoffmann@dataport.de>
2022-07-07 12:12:38 +0200

    BC-2095 close open s3 connections via scope.request

--- apps/server/src/modules/files-storage/client/s3-client.adapter.ts
+++ apps/server/src/modules/files-storage/client/s3-client.adapter.ts
@@ -8,12 +8,12 @@ import {
 	ServiceOutputTypes,
 } from '@aws-sdk/client-s3';
 import { Upload } from '@aws-sdk/lib-storage';
-import { Inject, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Inject, Injectable, InternalServerErrorException, Scope } from '@nestjs/common';
 import { Logger } from '@src/core/logger';
 import { Readable } from 'stream';
 import { ICopyFiles, IFile, IGetFileResponse, IStorageClient, S3Config } from '../interface';
 
-@Injectable()
+@Injectable({ scope: Scope.REQUEST })
 export class S3ClientAdapter implements IStorageClient {
 	private deletedFolderName = 'trash';
 
@@ -88,8 +88,6 @@ export class S3ClientAdapter implements IStorageClient {
 			}
 
 			throw new InternalServerErrorException(err, 'S3ClientAdapter:create');
-		} finally {
-			this.client.destroy();
 		}
 	}
 
