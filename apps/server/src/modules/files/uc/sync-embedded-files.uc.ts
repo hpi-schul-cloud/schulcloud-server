88ed8d7777657fd44f739c2f8e2cfb4ba4aae0f5
maxbischof <maximilian.bischof@dataport.de>
2022-07-11 12:03:37 +0200

    BC-1512 Extract fileIds from content

--- apps/server/src/modules/files/uc/sync-embedded-files.uc.ts
+++ apps/server/src/modules/files/uc/sync-embedded-files.uc.ts
@@ -3,13 +3,32 @@
 import { Injectable } from '@nestjs/common';
 import { Logger } from '@src/core/logger/logger.service';
 import { EmbeddedFilesRepo } from '../repo/embedded-files.repo';
+import { ExtendedLesson } from '../types/extended-lesson';
 
 @Injectable()
 export class SyncEmbeddedFilesUc {
 	constructor(private embeddedFilesRepo: EmbeddedFilesRepo, private logger: Logger) {}
 
+	extractFileIds(content: ExtendedLesson): string[] {
+		const contentText = content.lesson.contents[0].content.text;
+		const regEx = /(?<=src="(https?:\/\/[^"]*)?\/files\/file\?file=).+?(?=&amp;)/g;
+		const fileIds = contentText.match(regEx);
+
+		if (fileIds === null) {
+			return [];
+		}
+
+		return fileIds;
+	}
+
 	async syncEmbeddedFilesForLesson() {
-		const result = await this.embeddedFilesRepo.findEmbeddedFilesForLessons();
-		this.logger.log(`Found ${result.length} lesson contents with embedded files.`);
+		const contents = await this.embeddedFilesRepo.findEmbeddedFilesForLessons();
+		this.logger.log(`Found ${contents.length} lesson contents with embedded files.`);
+
+		contents.map((content) => {
+			const fileIds = this.extractFileIds(content);
+
+			console.log(fileIds);
+		});
 	}
 }
