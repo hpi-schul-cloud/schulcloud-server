f298e736e2a60cae39d7e6f350f2895eb8f48048
maxbischof <maximilian.bischof@dataport.de>
2022-07-15 10:10:44 +0200

    BC-1512 Add istanbul ignore test coverage

--- apps/server/src/modules/files/repo/embedded-files.repo.ts
+++ apps/server/src/modules/files/repo/embedded-files.repo.ts
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 import { EntityManager, ObjectId } from '@mikro-orm/mongodb';
 import { Injectable } from '@nestjs/common';
 import { Lesson, FileRecordParentType, File } from '@shared/domain';
