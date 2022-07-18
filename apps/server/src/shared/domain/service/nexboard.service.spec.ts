085d20393c957c83380de123d092caee8eb7e375
virgil chiriac <virgil.chiriac@dataport.de>
2022-07-18 18:43:25 +0200

    BC-2012 - nest wrapper for feathers nexboard create

---
+++ apps/server/src/shared/domain/service/nexboard.service.spec.ts
@@ -0,0 +1,52 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { FeathersServiceProvider } from '@shared/infra/feathers/feathers-service.provider';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { Logger } from '@src/core/logger';
+import { EntityId } from '@shared/domain';
+import { ObjectId } from '@mikro-orm/mongodb';
+import { NexboardService } from './nexboard.service';
+
+describe('Nexboard service', () => {
+	let module: TestingModule;
+	let nexboardService: NexboardService;
+	let feathersServiceProvider: DeepMocked<FeathersServiceProvider>;
+
+	beforeEach(async () => {
+		module = await Test.createTestingModule({
+			providers: [
+				NexboardService,
+				{
+					provide: FeathersServiceProvider,
+					useValue: createMock<FeathersServiceProvider>(),
+				},
+				{
+					provide: Logger,
+					useValue: createMock<Logger>(),
+				},
+			],
+		}).compile();
+
+		nexboardService = module.get(NexboardService);
+		feathersServiceProvider = module.get(FeathersServiceProvider);
+	});
+
+	describe('create Nexboard', () => {
+		it('it should call feathers service', async () => {
+			feathersServiceProvider.getService('/nexboard/boards').create.mockResolvedValue({ publiclink: 'boardId' });
+
+			const userId: EntityId = new ObjectId().toHexString();
+
+			const nexboardUrl = await nexboardService.createNexboard(userId, 'title', 'description');
+			expect(nexboardUrl).toEqual('boardId');
+		});
+
+		it('should return false if nexboard call fails', async () => {
+			feathersServiceProvider.getService('/nexboard/boards').create.mockRejectedValue({});
+
+			const userId: EntityId = new ObjectId().toHexString();
+
+			const nexboardId = await nexboardService.createNexboard(userId, 'title', 'description');
+			expect(nexboardId).toEqual(false);
+		});
+	});
+});
