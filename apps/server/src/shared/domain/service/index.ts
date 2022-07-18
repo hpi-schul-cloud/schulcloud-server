002ac6b963b9b9ccb579b850a6eb14a40c6bb16a
virgil chiriac <virgil.chiriac@dataport.de>
2022-07-18 19:28:06 +0200

    BC-2012 - fix nexBoard module ref

--- apps/server/src/shared/domain/service/index.ts
+++ apps/server/src/shared/domain/service/index.ts
@@ -3,5 +3,6 @@ export * from './copy-helper.service';
 export * from './course-copy.service';
 export * from './etherpad.service';
 export * from './lesson-copy.service';
+export * from './nexboard.service';
 export * from './permission.service';
 export * from './task-copy.service';
