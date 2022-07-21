03e5b5e3f8f36ff73a5723e55af65f52922763c7
MartinSchuhmacher <mschuhmacher@gmx.de>
2022-07-21 11:15:30 +0200

    adjust order of material factory values to entity

--- apps/server/src/shared/testing/factory/material.factory.ts
+++ apps/server/src/shared/testing/factory/material.factory.ts
@@ -5,14 +5,15 @@ class MaterialFactory extends BaseFactory<Material, IMaterialProperties> {}
 
 export const materialFactory = MaterialFactory.define<Material, IMaterialProperties>(Material, ({ sequence }) => {
 	return {
-		title: `material #${sequence}`,
 		client: 'test material client',
-		url: 'test material url',
 		description: 'test material description',
+		license: [],
+		merlinReference: '',
 		relatedResources: [],
+		subjects: [],
 		tags: [],
 		targetGroups: [],
-		subjects: [],
-		license: [],
+		title: `material #${sequence}`,
+		url: 'test material url',
 	};
 });
