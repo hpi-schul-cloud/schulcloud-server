6814d4119f3e2759b55c4e5164f63e7ecbc81349
Martin Schuhmacher <55735359+MartinSchuhmacher@users.noreply.github.com>
2022-07-21 14:48:51 +0200

    BC-2098 - Lesson copy service (Attached Lernstore Content/Materials collection) (#3571)
    * implement materials entity, repo and test factory
    
    * implement materials collection into lessons
    
    * implement getLessonMaterials method on lesson entity
    
    * implement copy of linked materials
    
    * implement merlinReference in material entity
    
    * refactor error type of lesson entity methods for more details

--- apps/server/src/shared/domain/entity/all-entities.ts
+++ apps/server/src/shared/domain/entity/all-entities.ts
@@ -9,6 +9,7 @@ import { File } from './file.entity';
 import { FileRecord, FileSecurityCheck } from './filerecord.entity';
 import { ImportUser } from './import-user.entity';
 import { Lesson } from './lesson.entity';
+import { Material } from './materials.entity';
 import { CourseNews, News, SchoolNews, TeamNews } from './news.entity';
 import { Role } from './role.entity';
 import { School, SchoolRolePermission, SchoolRoles } from './school.entity';
@@ -33,6 +34,7 @@ export const ALL_ENTITIES = [
 	File,
 	StorageProvider,
 	Lesson,
+	Material,
 	Role,
 	School,
 	SchoolRoles,
