5510a5680d37c24cefc0d735450ac11245f8dfc8
hoeppner-dataport <marcus.hoeppner@dataport.de>
2022-07-27 08:44:07 +0200

    chore: removed title for file_group-status

--- apps/server/src/shared/domain/service/task-copy.service.ts
+++ apps/server/src/shared/domain/service/task-copy.service.ts
@@ -65,7 +65,6 @@ export class TaskCopyService {
 				status: CopyStatusEnum.NOT_IMPLEMENTED,
 			}));
 			const fileStatus = {
-				title: 'files',
 				type: CopyElementType.FILE_GROUP,
 				status: CopyStatusEnum.NOT_IMPLEMENTED,
 				elements,
