1af9edddbdbba208214a7bc5135549c3435867a8
hoeppner-dataport <marcus.hoeppner@dataport.de>
2022-07-29 17:17:42 +0200

    fix: corrected logger

--- apps/server/src/modules/task/task.module.ts
+++ apps/server/src/modules/task/task.module.ts
@@ -1,10 +1,11 @@
-import { Logger, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { CopyHelperService } from '@shared/domain';
 import { FileCopyAppendService } from '@shared/domain/service/file-copy-append.service';
 import { FileLegacyService } from '@shared/domain/service/file-legacy.service';
 import { TaskCopyService } from '@shared/domain/service/task-copy.service';
 import { FeathersServiceProvider } from '@shared/infra/feathers';
 import { CourseRepo, LessonRepo, TaskRepo } from '@shared/repo';
+import { Logger } from '@src/core/logger';
 import { AuthorizationModule } from '../authorization';
 import { FilesStorageClientModule } from '../files-storage-client';
 import { TaskController } from './controller';
