fab3a5804b8e3508a2f90b47da09d342d8dd897b
Igor Richter <igor.richter@capgemini.com>
2022-08-15 13:49:15 +0200

    added provisioningStrategy into model.js

--- src/services/system/model.js
+++ src/services/system/model.js
@@ -29,6 +29,7 @@ const systemSchema = new Schema(
 		alias: { type: String },
 		oaClientId: { type: String }, // just for oauth2-systems
 		oaClientSecret: { type: String }, // just for oauth2-systems
+		provisioningStrategy: { type: String, required: false },
 		provisioningUrl: { type: String, required: false },
 		oauthConfig: {
 			type: {
