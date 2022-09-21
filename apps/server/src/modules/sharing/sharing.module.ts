225f92071fb956423b42ce3c33f93557f8c6d8ed
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2022-09-21 09:53:54 +0200

    make controller accessible

--- apps/server/src/modules/sharing/sharing.module.ts
+++ apps/server/src/modules/sharing/sharing.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ShareTokenRepo } from '@shared/repo/sharetoken';
+import { ShareTokenController } from './controller/share-token.controller';
 import { ShareTokenService } from './share-token.service';
 import { TokenGenerator } from './token-generator.service';
 
 @Module({
 	imports: [],
-	controllers: [],
+	controllers: [ShareTokenController],
 	providers: [ShareTokenService, TokenGenerator, ShareTokenRepo],
 	exports: [ShareTokenService],
 })
