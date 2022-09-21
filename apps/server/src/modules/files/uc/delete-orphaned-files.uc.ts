492cae1aeb330bfe8f7cc1fa31dde89e609e4e20
Phillip Wirth <phillip.wirth@dataport.de>
2022-09-21 15:24:56 +0200

    improved delete orphan files

--- apps/server/src/modules/files/uc/delete-orphaned-files.uc.ts
+++ apps/server/src/modules/files/uc/delete-orphaned-files.uc.ts
@@ -30,19 +30,24 @@ export class DeleteOrphanedFilesUc {
 		this.logger.log('Start deletion process.');
 
 		const orphanedFileRecords = await this.orphanedFilesRepo.findDuplicatedFileRecords(parentType);
-
-		// eslint-disable-next-line no-restricted-syntax
-		for await (const fileRecord of orphanedFileRecords) {
-			const found = await this.orphanedFilesRepo.findLessonsByFileRecordId(fileRecord.id);
-			this.logger.log(`FileRecord with id ${fileRecord.id} and lesson found: ${found}`);
-			if (!found) {
-				await this.deleteOrphan(fileRecord);
-			}
+		const chunkSize = 100;
+		const chunks: FileRecord[][] = [];
+		for (let i = 0; i < orphanedFileRecords.length; i += chunkSize) {
+			const chunk = orphanedFileRecords.slice(i, i + chunkSize);
+			chunks.push(chunk);
 		}
 
-		/* 		this.logger.log(`Found ${orphanedFileRecords.length} orphaned fileRecords.`);
-		await this.deleteOrphans(orphanedFileRecords);
-		this.logger.log(`Finished removing orphaned fileRecords.`); */
+		// eslint-disable-next-line no-restricted-syntax
+		for await (const chunk of chunks) {
+			const promises = chunk.map(async (fileRecord) => {
+				const found = await this.orphanedFilesRepo.findLessonsByFileRecordId(fileRecord.id);
+				this.logger.log(`FileRecord with id ${fileRecord.id} and lesson found: ${found}`);
+				if (!found) {
+					await this.deleteOrphan(fileRecord);
+				}
+			});
+			await Promise.all(promises);
+		}
 	}
 
 	private async deleteOrphans(fileRecords: FileRecord[]) {
