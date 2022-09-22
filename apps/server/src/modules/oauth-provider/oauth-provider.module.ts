f5ac54378b7c1c599c75e2319d474ee9c0524fa0
Marvin Öhlerking <marvin.oehlerking@capgemini.com>
2022-09-22 15:33:44 +0200

    add provider to module

--- apps/server/src/modules/oauth-provider/oauth-provider.module.ts
+++ apps/server/src/modules/oauth-provider/oauth-provider.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { OauthProviderServiceModule } from '@shared/infra/oauth-provider';
+import { UserRepo } from '@shared/repo/user';
+import { AuthorizationModule } from '../authorization/authorization.module';
 import { OauthProviderController } from './controller/oauth-provider.controller';
 import { OauthProviderResponseMapper } from './mapper/oauth-provider-response.mapper';
 import { OauthProviderClientCrudUc } from './uc/oauth-provider.client-crud.uc';
 
 @Module({
-	imports: [OauthProviderServiceModule],
-	providers: [OauthProviderClientCrudUc, OauthProviderResponseMapper],
+	imports: [OauthProviderServiceModule, AuthorizationModule],
+	providers: [OauthProviderClientCrudUc, OauthProviderResponseMapper, UserRepo],
 	controllers: [OauthProviderController],
 })
 export class OauthProviderModule {}
