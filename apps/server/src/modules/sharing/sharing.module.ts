1094df6bfcb88f6997b7b7e5ea57405a06f2a512
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2022-09-22 09:19:53 +0200

    un-expose controller

--- apps/server/src/modules/sharing/sharing.module.ts
+++ apps/server/src/modules/sharing/sharing.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ShareTokenRepo } from '@shared/repo/sharetoken';
-import { ShareTokenController } from './controller/share-token.controller';
 import { ShareTokenService } from './share-token.service';
 import { TokenGenerator } from './token-generator.service';
 
 @Module({
 	imports: [],
-	controllers: [ShareTokenController],
+	controllers: [],
 	providers: [ShareTokenService, TokenGenerator, ShareTokenRepo],
 	exports: [ShareTokenService],
 })
