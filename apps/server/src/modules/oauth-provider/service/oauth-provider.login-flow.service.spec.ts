399968c039a1a556fb2feb685a59a7589994cc39
Mrika Llabani <mrika.llabani@capgemini.com>
2022-09-23 12:36:12 +0200

    uc and service test WIP

---
+++ apps/server/src/modules/oauth-provider/service/oauth-provider.login-flow.service.spec.ts
@@ -0,0 +1,163 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { LtiToolRepo, PseudonymsRepo, RoleRepo, UserRepo } from '@shared/repo';
+import { ICurrentUser, Permission, PermissionService, PseudonymDO } from '@shared/domain';
+import { LoginRequestBody } from '@src/modules/oauth-provider/controller/dto';
+import { OauthProviderLoginFlowService } from '@src/modules/oauth-provider/service/oauth-provider.login-flow.service';
+import { LoginResponse } from '@shared/infra/oauth-provider/dto';
+import { LtiToolDO } from '@shared/domain/domainobject/ltitool.do';
+import { NotFoundException } from '@nestjs/common';
+import { roleFactory, userFactory } from '@shared/testing';
+
+describe('OauthProviderLoginFlowService', () => {
+	let module: TestingModule;
+	let service: DeepMocked<OauthProviderLoginFlowService>;
+	let ltiToolRepo: DeepMocked<LtiToolRepo>;
+	let pseudonymsRepo: DeepMocked<PseudonymsRepo>;
+	let userRepo: DeepMocked<UserRepo>;
+	let roleRepo: DeepMocked<RoleRepo>;
+	let permissionService: DeepMocked<PermissionService>;
+
+	const currentUser: ICurrentUser = { userId: 'userId' } as ICurrentUser;
+
+	beforeAll(async () => {
+		module = await Test.createTestingModule({
+			providers: [
+				OauthProviderLoginFlowService,
+				{
+					provide: UserRepo,
+					useValue: createMock<UserRepo>(),
+				},
+				{
+					provide: RoleRepo,
+					useValue: createMock<RoleRepo>(),
+				},
+				{
+					provide: LtiToolRepo,
+					useValue: createMock<LtiToolRepo>(),
+				},
+				{
+					provide: PseudonymsRepo,
+					useValue: createMock<PseudonymsRepo>(),
+				},
+				{
+					provide: PermissionService,
+					useValue: createMock<PermissionService>(),
+				},
+			],
+		}).compile();
+
+		service = module.get(OauthProviderLoginFlowService);
+		userRepo = module.get(UserRepo);
+		ltiToolRepo = module.get(LtiToolRepo);
+		pseudonymsRepo = module.get(PseudonymsRepo);
+		roleRepo = module.get(RoleRepo);
+		permissionService = module.get(PermissionService);
+	});
+
+	afterAll(async () => {
+		await module.close();
+	});
+
+	describe('setSubject', () => {
+		const loginRequestBodyMock: LoginRequestBody = {
+			remember: true,
+			remember_for: 0,
+		};
+
+		it('set the subject in acceptLoginRequestBody succesfully', async () => {
+			const loginResponse: LoginResponse = {
+				challenge: 'challenge',
+				client: {
+					client_id: 'clientId',
+				},
+				oidc_context: {},
+				request_url: 'request_url',
+				requested_access_token_audience: ['requested_access_token_audience'],
+				requested_scope: ['requested_scope'],
+				session_id: 'session_id',
+				skip: true,
+				subject: 'subject',
+			} as LoginResponse;
+			const ltiToolDoMock: LtiToolDO = {
+				id: 'toolId',
+				name: 'name',
+				oAuthClientId: 'oAuthClientId',
+				isLocal: true,
+			};
+			const pseudonym: PseudonymDO = {
+				pseudonym: 'pseudonym',
+				toolId: 'toolId',
+				userId: 'userId',
+			};
+
+			ltiToolRepo.findByOauthClientIdAndIsLocal.mockResolvedValue(ltiToolDoMock);
+			pseudonymsRepo.findByUserIdAndToolId.mockResolvedValue(pseudonym);
+
+			const acceptLoginRequestBody = await service.setSubject(currentUser.userId, loginResponse, loginRequestBodyMock);
+
+			expect(ltiToolRepo.findByOauthClientIdAndIsLocal).toHaveBeenCalledWith(loginResponse.client.client_id);
+			expect(pseudonymsRepo.findByUserIdAndToolId).toHaveBeenCalledWith(ltiToolDoMock.id, currentUser.userId);
+			expect(acceptLoginRequestBody.subject).toStrictEqual('userId');
+			expect(acceptLoginRequestBody.amr).toBeUndefined();
+			expect(acceptLoginRequestBody.acr).toBeUndefined();
+			expect(acceptLoginRequestBody.remember).toBeTruthy();
+			expect(acceptLoginRequestBody.remember_for).toStrictEqual(0);
+			expect(acceptLoginRequestBody.force_subject_identifier).toStrictEqual(
+				acceptLoginRequestBody.force_subject_identifier
+			);
+		});
+		it('could not set the subject in acceptLoginRequestBody and throw error', async () => {
+			const loginResponse: LoginResponse = {
+				challenge: 'challenge',
+				client: {},
+				oidc_context: {},
+				skip: true,
+				subject: 'subject',
+			} as LoginResponse;
+			await expect(service.setSubject(currentUser.userId, loginResponse, loginRequestBodyMock)).rejects.toThrow(
+				NotFoundException
+			);
+		});
+	});
+	describe('validateNextcloudPermission', () => {
+		it('should validate Nextcloud permissions', async () => {
+			const role = roleFactory.build({ permissions: ['NEXTCLOUD_USER' as Permission] });
+			const user = userFactory.build({
+				email: 'email',
+				firstName: 'firstname',
+				lastName: 'lastname',
+				ldapDn: 'ldapDn',
+				ldapId: 'ldapId',
+				roles: [role],
+			});
+			user.roles.add(role);
+			user.roles[0].permissions = ['NEXTCLOUD_USER' as Permission];
+			const loginResponse: LoginResponse = {
+				challenge: 'challenge',
+				client: {
+					client_id: 'clientId',
+				},
+				oidc_context: {},
+			} as LoginResponse;
+			const ltiToolDoMock: LtiToolDO = {
+				id: 'toolId',
+				name: 'name',
+				oAuthClientId: 'oAuthClientId',
+				isLocal: true,
+			};
+
+			userRepo.findById.mockResolvedValue(user);
+			permissionService.resolvePermissions.mockReturnValue(role.permissions);
+			ltiToolRepo.findByOauthClientIdAndIsLocal.mockResolvedValue(ltiToolDoMock);
+
+			const validation = await service.validateNextcloudPermission(currentUser.userId, loginResponse);
+
+			expect(userRepo.findById).toHaveBeenCalledWith(user.id);
+			expect(permissionService.resolvePermissions).toHaveBeenCalledWith(user);
+			expect(ltiToolRepo.findByOauthClientIdAndIsLocal).toHaveBeenCalledWith(loginResponse.client.client_id);
+			expect(validation).toHaveBeenCalled();
+		});
+	});
+	describe('rejectLoginRequest', () => {});
+});
---
+++ apps/server/src/modules/oauth-provider/uc/oauth-provider.login-flow.uc.spec.ts
@@ -0,0 +1,226 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { OauthProviderLoginFlowUc } from '@src/modules/oauth-provider/uc/oauth-provider.login-flow.uc';
+import { OauthProviderService } from '@shared/infra/oauth-provider';
+import { LtiToolRepo, PseudonymsRepo, RoleRepo } from '@shared/repo';
+import { ICurrentUser, PermissionService } from '@shared/domain';
+import {
+	AcceptLoginRequestBody,
+	LoginResponse,
+	RedirectResponse,
+	RejectRequestBody,
+} from '@shared/infra/oauth-provider/dto';
+import { AcceptQuery, ChallengeParams, LoginRequestBody } from '@src/modules/oauth-provider/controller/dto';
+import { OauthProviderLoginFlowService } from '@src/modules/oauth-provider/service/oauth-provider.login-flow.service';
+import resetAllMocks = jest.resetAllMocks;
+
+class OauthProviderLoginFlowUcSpec extends OauthProviderLoginFlowUc {
+	public acceptLoginRequestSpec(
+		currentUserId: string,
+		loginResponse: LoginResponse,
+		loginRequestBody: LoginRequestBody
+	) {
+		return super.acceptLoginRequest(currentUserId, loginResponse, loginRequestBody);
+	}
+
+	public rejectLoginRequestSpec(challenge: string, rejectRequestBody: RejectRequestBody): Promise<RedirectResponse> {
+		return super.rejectLoginRequest(challenge, rejectRequestBody);
+	}
+}
+
+describe('OauthProviderLoginFlowUc', () => {
+	let module: TestingModule;
+	let uc: OauthProviderLoginFlowUcSpec;
+	let service: DeepMocked<OauthProviderService>;
+	let oauthProviderLoginFlowService: DeepMocked<OauthProviderLoginFlowService>;
+
+	const params: ChallengeParams = { challenge: 'challenge' };
+	const currentUser: ICurrentUser = { userId: 'userId' } as ICurrentUser;
+	const acceptLoginRequestBody: AcceptLoginRequestBody = {
+		subject: 'userId',
+		acr: 'acr',
+		amr: ['amr'],
+		context: {},
+		force_subject_identifier: 'pseudonym',
+		remember: true,
+		remember_for: 0,
+	};
+	const query: AcceptQuery = {
+		accept: 1,
+	};
+	const redirectResponse: RedirectResponse = {
+		redirect_to: 'redirect_to',
+	};
+
+	beforeAll(async () => {
+		module = await Test.createTestingModule({
+			providers: [
+				OauthProviderLoginFlowUcSpec,
+				{
+					provide: OauthProviderService,
+					useValue: createMock<OauthProviderService>(),
+				},
+				{
+					provide: OauthProviderLoginFlowService,
+					useValue: createMock<OauthProviderLoginFlowService>(),
+				},
+				{
+					provide: RoleRepo,
+					useValue: createMock<RoleRepo>(),
+				},
+				{
+					provide: LtiToolRepo,
+					useValue: createMock<LtiToolRepo>(),
+				},
+				{
+					provide: PseudonymsRepo,
+					useValue: createMock<PseudonymsRepo>(),
+				},
+				{
+					provide: PermissionService,
+					useValue: createMock<PermissionService>(),
+				},
+			],
+		}).compile();
+
+		uc = module.get(OauthProviderLoginFlowUcSpec);
+		service = module.get(OauthProviderService);
+		oauthProviderLoginFlowService = module.get(OauthProviderLoginFlowService);
+	});
+
+	afterAll(async () => {
+		await module.close();
+	});
+
+	afterEach(() => {
+		resetAllMocks();
+	});
+
+	describe('getLoginRequest', () => {
+		it('should get the login request', async () => {
+			const loginResponseMock = {
+				challenge: 'challenge',
+				client: {},
+				oidc_context: {},
+				request_url: 'request_url',
+				requested_access_token_audience: ['requested_access_token_audience'],
+				requested_scope: ['requested_scope'],
+				session_id: 'session_id',
+				skip: true,
+				subject: 'subject',
+			} as LoginResponse;
+
+			service.getLoginRequest.mockResolvedValue(loginResponseMock);
+
+			const response = await uc.getLoginRequest(params.challenge);
+
+			expect(service.getLoginRequest).toHaveBeenCalledWith(params.challenge);
+			expect(response).toStrictEqual(loginResponseMock);
+		});
+	});
+	describe('patchLoginRequest', () => {
+		it('should call the login request', async () => {
+			const loginResponseMock: LoginResponse = {
+				challenge: 'challenge',
+				client: {},
+				oidc_context: {},
+				request_url: 'request_url',
+				requested_access_token_audience: ['requested_access_token_audience'],
+				requested_scope: ['requested_scope'],
+				session_id: 'session_id',
+				skip: true,
+				subject: 'subject',
+			} as LoginResponse;
+			const loginRequestBodyMock: LoginRequestBody = {
+				remember: true,
+				remember_for: 0,
+			};
+			service.getLoginRequest.mockResolvedValue(loginResponseMock);
+			oauthProviderLoginFlowService.setSubject.mockResolvedValue(acceptLoginRequestBody);
+			oauthProviderLoginFlowService.validateNextcloudPermission.mockResolvedValue();
+			service.acceptLoginRequest.mockResolvedValue(redirectResponse);
+
+			const response = await uc.patchLoginRequest(currentUser.userId, params.challenge, loginRequestBodyMock, query);
+
+			expect(response.redirect_to).toStrictEqual(redirectResponse.redirect_to);
+			expect(oauthProviderLoginFlowService.setSubject).toHaveBeenCalledWith(
+				currentUser.userId,
+				loginResponseMock,
+				loginRequestBodyMock
+			);
+			expect(oauthProviderLoginFlowService.validateNextcloudPermission).toHaveBeenCalledWith(
+				currentUser.userId,
+				loginResponseMock
+			);
+			expect(service.rejectLoginRequest).not.toHaveBeenCalled();
+		});
+
+		it('should call the reject login request', async () => {
+			const rejectBody: RejectRequestBody = {
+				error: 'error',
+				error_debug: 'error_debug',
+				error_description: 'error_description',
+				error_hint: 'error_hint',
+				status_code: 404,
+			};
+
+			service.rejectLoginRequest.mockResolvedValue(redirectResponse);
+			const response = await uc.patchLoginRequest(currentUser.userId, params.challenge, rejectBody, query);
+
+			expect(response).toStrictEqual(redirectResponse);
+			expect(service.acceptLoginRequest).not.toHaveBeenCalled();
+		});
+	});
+	describe('acceptLoginRequest', () => {
+		it('should accept the login request', async () => {
+			const loginResponse: LoginResponse = {
+				challenge: 'challenge',
+				client: {},
+				oidc_context: {},
+				request_url: 'request_url',
+				requested_access_token_audience: ['requested_access_token_audience'],
+				requested_scope: ['requested_scope'],
+				session_id: 'session_id',
+				skip: true,
+				subject: 'subject',
+			} as LoginResponse;
+			const loginRequestBodyMock: LoginRequestBody = {
+				remember: true,
+				remember_for: 0,
+			};
+
+			oauthProviderLoginFlowService.setSubject.mockResolvedValue(acceptLoginRequestBody);
+			oauthProviderLoginFlowService.validateNextcloudPermission.mockResolvedValue();
+			service.acceptLoginRequest.mockResolvedValue(redirectResponse);
+
+			const response = await uc.acceptLoginRequestSpec(currentUser.userId, loginResponse, loginRequestBodyMock);
+
+			expect(oauthProviderLoginFlowService.setSubject).toHaveBeenCalledWith(
+				currentUser.userId,
+				loginResponse,
+				loginRequestBodyMock
+			);
+			expect(service.acceptLoginRequest).toHaveBeenCalledWith(params.challenge, acceptLoginRequestBody);
+			expect(response.redirect_to).toStrictEqual(redirectResponse.redirect_to);
+		});
+	});
+	describe('rejectLoginRequest', () => {
+		it('should reject the login request', async () => {
+			const rejectBody: RejectRequestBody = {
+				error: 'error',
+				error_debug: 'error_debug',
+				error_description: 'error_description',
+				error_hint: 'error_hint',
+				status_code: 404,
+			};
+			oauthProviderLoginFlowService.setSubject.mockResolvedValue(acceptLoginRequestBody);
+			oauthProviderLoginFlowService.validateNextcloudPermission.mockResolvedValue();
+			service.rejectLoginRequest.mockResolvedValue(redirectResponse);
+
+			const response = await uc.rejectLoginRequestSpec(params.challenge, rejectBody);
+
+			expect(service.rejectLoginRequest).toHaveBeenCalledWith(params.challenge, rejectBody);
+			expect(response).toStrictEqual(redirectResponse);
+		});
+	});
+});
