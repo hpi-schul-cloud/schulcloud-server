981483c6817e8fdfcf7bb3b51c9aee7f633af321
Mrika Llabani <mrika.llabani@capgemini.com>
2022-09-23 12:32:01 +0200

    controller test

--- apps/server/src/modules/oauth-provider/controller/oauth-provider.controller.spec.ts
+++ apps/server/src/modules/oauth-provider/controller/oauth-provider.controller.spec.ts
@@ -1,32 +1,84 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { OauthProviderUc } from '@src/modules/oauth-provider/uc/oauth-provider.uc';
-import { createMock } from '@golevelup/ts-jest';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
 import { OauthProviderController } from './oauth-provider.controller';
+import { LoginResponse, RedirectResponse } from '../../../shared/infra/oauth-provider/dto';
+import { ICurrentUser } from '../../../shared/domain';
+import { AcceptQuery, ChallengeParams, LoginRequestBody } from './dto';
+import { OauthProviderLoginFlowUc } from '../uc/oauth-provider.login-flow.uc';
 
 describe('OauthProviderController', () => {
 	let module: TestingModule;
 	let controller: OauthProviderController;
+	let oauthProviderLoginFlowUc: DeepMocked<OauthProviderLoginFlowUc>;
 
 	beforeAll(async () => {
 		module = await Test.createTestingModule({
 			providers: [
 				OauthProviderController,
 				{
-					provide: OauthProviderUc,
-					useValue: createMock<OauthProviderUc>(),
+					provide: OauthProviderLoginFlowUc,
+					useValue: createMock<OauthProviderLoginFlowUc>(),
 				},
 			],
 		}).compile();
 
 		controller = module.get(OauthProviderController);
+		oauthProviderLoginFlowUc = module.get(OauthProviderLoginFlowUc);
 	});
 
 	afterAll(async () => {
 		await module.close();
 	});
+	const params: ChallengeParams = { challenge: 'challenge' };
 
-	// TODO remove after implementation
-	it('should be defined', () => {
-		expect(controller).toBeDefined();
+	describe('getLoginRequest', () => {
+		it('should get the login request response', async () => {
+			const loginResponseMock = {
+				challenge: 'challenge',
+				client: {},
+				oidc_context: {},
+				request_url: 'request_url',
+				requested_access_token_audience: ['requested_access_token_audience'],
+				requested_scope: ['requested_scope'],
+				session_id: 'session_id',
+				skip: true,
+				subject: 'subject',
+			} as LoginResponse;
+
+			oauthProviderLoginFlowUc.getLoginRequest.mockResolvedValue(loginResponseMock);
+
+			const response = await controller.getLoginRequest(params);
+
+			expect(oauthProviderLoginFlowUc.getLoginRequest).toHaveBeenCalledWith(params.challenge);
+			expect(response).toStrictEqual(loginResponseMock);
+		});
+	});
+	describe('patchLoginRequest', () => {
+		it('should patch the login request', async () => {
+			const query: AcceptQuery = {
+				accept: 1,
+			};
+			const loginRequestBody: LoginRequestBody = {
+				remember: true,
+				remember_for: 0,
+			};
+			const redirectResponse: RedirectResponse = {
+				redirect_to: 'redirect_to',
+			};
+			const currentUser: ICurrentUser = { userId: 'userId' } as ICurrentUser;
+			const expected = [currentUser.userId, params.challenge, loginRequestBody, query];
+
+			oauthProviderLoginFlowUc.patchLoginRequest.mockResolvedValue(redirectResponse);
+
+			const response = await controller.patchLoginRequest(params, query, loginRequestBody, currentUser);
+			expect(oauthProviderLoginFlowUc.patchLoginRequest).toHaveBeenCalledWith(
+				currentUser.userId,
+				params.challenge,
+				loginRequestBody,
+				query
+			);
+			expect(oauthProviderLoginFlowUc.patchLoginRequest).toHaveBeenCalledWith(...expected);
+			expect(response).toStrictEqual(redirectResponse);
+		});
 	});
 });
