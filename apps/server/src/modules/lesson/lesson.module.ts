a31f20d14e796d0f088d38c71c6cf0201dfe4ec6
maxbischof <maximilian.bischof@dataport.de>
2022-09-23 10:31:46 +0200

    BC-1513 Move and rename lesson api module

--- apps/server/src/modules/lesson/lesson.module.ts
+++ apps/server/src/modules/lesson/lesson.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
 import { LessonRepo } from '@shared/repo';
-import { AuthorizationModule } from '../authorization';
 import { FilesStorageClientModule } from '../files-storage-client';
-import { LessonController } from './controller';
 import { LessonService } from './service';
-import { LessonUC } from './uc';
 
 @Module({
 	imports: [FilesStorageClientModule],
@@ -13,10 +10,3 @@ import { LessonUC } from './uc';
 	exports: [LessonService],
 })
 export class LessonModule {}
-
-@Module({
-	imports: [LessonModule, AuthorizationModule],
-	controllers: [LessonController],
-	providers: [LessonUC],
-})
-export class LessonRootModule {}
