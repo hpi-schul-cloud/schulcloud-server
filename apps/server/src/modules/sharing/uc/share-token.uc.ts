d3b9f2254849c415046984629e766272e2a09c31
Thomas Feldtkeller <thomas.feldtkeller@dataport.de>
2022-09-26 14:46:40 +0200

    remove unreachable code

--- apps/server/src/modules/sharing/uc/share-token.uc.ts
+++ apps/server/src/modules/sharing/uc/share-token.uc.ts
@@ -6,27 +6,15 @@ import { ShareTokenService } from '../share-token.service';
 export class ShareTokenUC {
 	constructor(private readonly shareTokenService: ShareTokenService) {}
 
-	async lookupShareToken(userId: EntityId, token: string): Promise<ShareTokenDO> {
+	lookupShareToken(userId: EntityId, token: string): Promise<ShareTokenDO> {
 		throw new NotImplementedException();
-
-		// WIP add authorization
-
-		const shareToken = await this.shareTokenService.lookupToken(token);
-
-		return shareToken;
 	}
 
-	async createShareToken(
+	createShareToken(
 		userId: EntityId,
 		payload: ShareTokenPayload,
 		options?: { context?: ShareTokenContext; expiresAt?: Date }
 	): Promise<ShareTokenDO> {
 		throw new NotImplementedException();
-
-		// WIP add authorization
-
-		const shareToken = await this.shareTokenService.createToken(payload, options);
-
-		return shareToken;
 	}
 }
