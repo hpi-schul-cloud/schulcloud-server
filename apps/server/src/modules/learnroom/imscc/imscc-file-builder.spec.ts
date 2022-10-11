93bf6e6b9e7bb928d9f00d86e70d46be3f873a19
Patrick Sachmann <patrick.sachmann@capgemini.com>
2022-10-11 11:02:55 +0200

    EW-197 improved tests

--- apps/server/src/modules/learnroom/imscc/imscc-file-builder.spec.ts
+++ apps/server/src/modules/learnroom/imscc/imscc-file-builder.spec.ts
@@ -1,23 +1,38 @@
 import { ImsccFileBuilder } from '@src/modules/learnroom/imscc/imscc-file-builder';
+import { Readable } from 'stream';
+import AdmZip from 'adm-zip';
 
 describe('ImsccFileBuilder', () => {
 	let builder: ImsccFileBuilder;
 
-	beforeAll(() => {
+	beforeEach(() => {
 		builder = new ImsccFileBuilder();
 	});
 
 	describe('addTitle', () => {
-		it('should return builder', () => {
-			expect(builder.addTitle('Placeholder')).toEqual<ImsccFileBuilder>(builder);
+		it('should return builder', async () => {
+			const title = 'Placeholder Title';
+
+			expect(builder.addTitle(title)).toEqual(builder);
+
+			const file = await builder.build();
+			const buffers = [] as unknown[];
+			// eslint-disable-next-line no-restricted-syntax
+			for await (const chunk of file) {
+				buffers.push(chunk);
+			}
+			const zip = new AdmZip(Buffer.concat(buffers as Uint8Array[]));
+			const manifest = zip.getEntry('imsmanifest.xml');
+
+			expect(manifest?.getData().toString()).toContain(title);
 		});
 	});
 
 	describe('build', () => {
-		it('should return a readable stream', () => {
+		it('should return a readable stream', async () => {
 			builder.addTitle('Placeholder');
 
-			expect(builder.build()).toBeDefined();
+			await expect(builder.build()).resolves.toBeInstanceOf(Readable);
 		});
 	});
 });
