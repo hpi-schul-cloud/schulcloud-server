af78a07fe8aad626e096e9c194761efbdadb1499
Bjoern Hass <bjoern.hass@dataport.de>
2022-10-17 15:30:16 +0200

    BC-1972 create transport for console

--- apps/server/src/core/logger/logger.module.ts
+++ apps/server/src/core/logger/logger.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { WinstonModule } from 'nest-winston';
+import winston from 'winston';
 import { Logger } from './logger.service';
 
 @Module({
-	imports: [WinstonModule.forRoot({})],
+	imports: [
+		WinstonModule.forRoot({
+			transports: [
+				new winston.transports.Console({
+					format: winston.format.combine(winston.format.timestamp(), winston.format.ms()),
+				}),
+			],
+		}),
+	],
 	providers: [Logger],
 	exports: [Logger],
 })
