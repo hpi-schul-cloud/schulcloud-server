9a89a7f6842554a59023457fd40ce7332d788aa5
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2022-10-18 17:49:29 +0200

    sanitize name input

--- apps/server/src/modules/sharing/controller/dto/share-token-import.body.params.ts
+++ apps/server/src/modules/sharing/controller/dto/share-token-import.body.params.ts
@@ -1,8 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { SanitizeHtml } from '@shared/controller';
 import { IsString } from 'class-validator';
 
 export class ShareTokenImportBodyParams {
 	@IsString()
+	@SanitizeHtml()
 	@ApiProperty({
 		description: 'the new name of the imported object.',
 		required: true,
