f4dbbd6ce3ef0d684f9218c6f2b86729018098d4
maxbischof <maximilian.bischof@dataport.de>
2022-10-19 10:00:03 +0200

    BC-2552 Remove undefined check from getStatusFromScanResult

--- apps/server/src/modules/files-storage/helper/scan-status.ts
+++ apps/server/src/modules/files-storage/helper/scan-status.ts
@@ -2,10 +2,7 @@ import { FileRecord, FileSecurityCheck, ScanStatus } from '@shared/domain';
 import { ScanResultParams } from '../controller/dto';
 
 export function getStatusFromScanResult(scanResultParams: ScanResultParams): ScanStatus {
-	const status =
-		scanResultParams.virus_detected === undefined || scanResultParams.virus_detected
-			? ScanStatus.BLOCKED
-			: ScanStatus.VERIFIED;
+	const status = scanResultParams.virus_detected ? ScanStatus.BLOCKED : ScanStatus.VERIFIED;
 
 	return status;
 }
