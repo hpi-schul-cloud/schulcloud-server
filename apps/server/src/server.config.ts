bb28cce14c9d9461fc4ceecc2cd0caa8e2fd7ea3
Bjoern Hass <bjoern.hass@dataport.de>
2022-10-19 13:30:55 +0200

    BC-1972-available-log-levels

--- apps/server/src/server.config.ts
+++ apps/server/src/server.config.ts
@@ -1,6 +1,6 @@
 import { Configuration } from '@hpi-schul-cloud/commons';
 import type { ICoreModuleConfig } from '@src/core';
-import type { IUserConfig, IFilesStorageClientConfig, IAccountConfig } from '@src/modules/';
+import type { IAccountConfig, IFilesStorageClientConfig, IUserConfig } from '@src/modules/';
 import { AvailableLogLevel } from './core/logger';
 
 export enum NodeEnvType {
@@ -26,7 +26,7 @@ export interface IServerConfig
 const config: IServerConfig = {
 	INCOMING_REQUEST_TIMEOUT: Configuration.get('INCOMING_REQUEST_TIMEOUT_API') as number,
 	INCOMING_REQUEST_TIMEOUT_COPY_API: Configuration.get('INCOMING_REQUEST_TIMEOUT_COPY_API') as number,
-	AVAILABLE_LOG_LEVELS: (Configuration.get('NEST_AVAILABLE_LOG_LEVELS') as string).split(',') as AvailableLogLevel[],
+	AVAILABLE_LOG_LEVELS: (Configuration.get('AVAILABLE_LOG_LEVELS') as string).split(',') as AvailableLogLevel[],
 	AVAILABLE_LANGUAGES: (Configuration.get('I18N__AVAILABLE_LANGUAGES') as string).split(','),
 	FILE_STORAGE_BASE_URL: Configuration.get('FILES_STORAGE__SERVICE_BASE_URL') as string,
 	NODE_ENV: Configuration.get('NODE_ENV') as NodeEnvType,
