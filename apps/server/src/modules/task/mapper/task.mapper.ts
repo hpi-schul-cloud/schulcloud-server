2c045936120e0fb17c60038e8499d497020922f5
hoeppner-dataport <marcus.hoeppner@dataport.de>
2022-10-24 09:15:10 +0200

    readd mapUpdateTaskToDomain

--- apps/server/src/modules/task/mapper/task.mapper.ts
+++ apps/server/src/modules/task/mapper/task.mapper.ts
@@ -1,5 +1,5 @@
-import { TaskWithStatusVo } from '@shared/domain';
-import { TaskResponse } from '../controller/dto';
+import { ITaskUpdate, TaskWithStatusVo } from '@shared/domain';
+import { TaskResponse, TaskUpdateParams } from '../controller/dto';
 import { TaskStatusMapper } from './task-status.mapper';
 
 export class TaskMapper {
@@ -26,6 +26,15 @@ export class TaskMapper {
 		dto.description = taskDesc.lessonName;
 		dto.lessonHidden = taskDesc.lessonHidden;
 
+		return dto;
+	}
+
+	static mapUpdateTaskToDomain(params: TaskUpdateParams): ITaskUpdate {
+		const dto: ITaskUpdate = {
+			name: params.name,
+			courseId: params.courseId,
+			lessonId: params.lessonId,
+		};
 		return dto;
 	}
 }
