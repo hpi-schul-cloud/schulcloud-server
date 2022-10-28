e82bb782915305af5bafc3566f80532b0f842917
agnisa <arne.gnisa@capgemini.com>
2022-10-28 14:24:53 +0200

    Fixed school import

--- apps/server/src/modules/tool/tool.module.ts
+++ apps/server/src/modules/tool/tool.module.ts
@@ -3,23 +3,14 @@ import { LtiToolRepo, PseudonymsRepo } from '@shared/repo';
 import { Lti11ResponseMapper } from '@src/modules/tool/mapper/lti11-response.mapper';
 import { LoggerModule } from '@src/core/logger';
 import { LtiToolService } from '@src/modules/tool/service/lti-tool.service';
-import { SchoolService } from '@src/modules/school';
+import { SchoolModule } from '@src/modules/school';
 import { Lti11Uc } from './uc/lti11.uc';
 import { LtiRoleMapper } from './mapper/lti-role.mapper';
 import { Lti11Service } from './service/lti11.service';
 import { UserModule } from '../user';
 
 @Module({
-	imports: [UserModule, LoggerModule],
-	providers: [
-		Lti11Service,
-		Lti11Uc,
-		LtiRoleMapper,
-		Lti11ResponseMapper,
-		LtiToolRepo,
-		PseudonymsRepo,
-		LtiToolService,
-		SchoolService,
-	],
+	imports: [UserModule, LoggerModule, SchoolModule],
+	providers: [Lti11Service, Lti11Uc, LtiRoleMapper, Lti11ResponseMapper, LtiToolRepo, PseudonymsRepo, LtiToolService],
 })
 export class ToolModule {}
