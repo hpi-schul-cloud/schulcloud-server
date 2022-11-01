ac7bbd5213de7843510ad4a57f812a64d7365427
Bjoern Hass <bjoern.hass@dataport.de>
2022-11-01 08:46:33 +0100

    BC-1972 remove check

--- apps/server/src/core/logger/logger.service.ts
+++ apps/server/src/core/logger/logger.service.ts
@@ -26,12 +26,8 @@ export class Logger implements ILogger {
 		this.logger.log('info', this.createMessage(message, context));
 	}
 
-	warn(message: unknown, context?): void {
-		if (typeof context === 'undefined' || typeof context === 'string') {
-			this.logger.warn(this.createMessage(message, context));
-		} else {
-			this.logger.warn(this.createMessage(message));
-		}
+	warn(message: unknown, context?: string | undefined): void {
+		this.logger.warn(this.createMessage(message, context));
 	}
 
 	debug(message: unknown, context?: string | undefined): void {
