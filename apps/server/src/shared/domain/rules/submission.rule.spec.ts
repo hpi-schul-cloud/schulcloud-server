eac509249c28fb663e502deb0e5f7e7c75824248
Max Becker <maximilian.becker@dataport.de>
2022-11-07 15:56:15 +0100

    BC-2479 Refactor tests to have setup function

--- apps/server/src/shared/domain/rules/submission.rule.spec.ts
+++ apps/server/src/shared/domain/rules/submission.rule.spec.ts
@@ -10,19 +10,29 @@ import {
 	userFactory,
 } from '@shared/testing';
 import { CourseGroupRule, CourseRule, LessonRule, SubmissionRule, TaskRule } from '.';
-import { User } from '../entity';
 import { Permission, RoleName } from '../interface';
 import { Actions } from './actions.enum';
+
+const buildStudent = (permission) => {
+	const studentRole = roleFactory.build({
+		permissions: [permission],
+		name: RoleName.STUDENT,
+	});
+	const student = userFactory.build({ roles: [studentRole] });
+
+	return student;
+};
+
+const buildTeacher = (permission) => {
+	const teacherRole = roleFactory.build({ permissions: [permission], name: RoleName.TEACHER });
+	const teacher = userFactory.build({ roles: [teacherRole] });
+
+	return teacher;
+};
 
 describe('SubmissionRule', () => {
 	let orm: MikroORM;
 	let submissionRule: SubmissionRule;
-	let student: User;
-	let student2: User;
-	let teacher: User;
-	const permissionA = 'a' as Permission;
-	const permissionB = 'b' as Permission;
-	const permissionC = 'c' as Permission;
 
 	beforeAll(async () => {
 		orm = await setupEntities();
@@ -32,12 +42,6 @@ describe('SubmissionRule', () => {
 		}).compile();
 
 		submissionRule = await module.get(SubmissionRule);
-
-		const studentRole = roleFactory.build({ permissions: [permissionA, permissionB], name: RoleName.STUDENT });
-		student = userFactory.build({ roles: [studentRole] });
-		student2 = userFactory.build({ roles: [studentRole] });
-		const teacherRole = roleFactory.build({ permissions: [permissionC], name: RoleName.TEACHER });
-		teacher = userFactory.build({ roles: [teacherRole] });
 	});
 
 	afterAll(async () => {
@@ -45,33 +49,58 @@ describe('SubmissionRule', () => {
 	});
 
 	describe('isApplicable', () => {
-		it('should return true if entity is instance of Submission', () => {
-			const task = taskFactory.build();
-			const submission = submissionFactory.build({ task, student });
+		describe('when entity is instance of Submission', () => {
+			const setup = () => {
+				const user = userFactory.build({ roles: [] });
+				const task = taskFactory.build();
+				const submission = submissionFactory.build({ task });
 
-			const result = submissionRule.isApplicable(student, submission);
+				return { user, submission };
+			};
 
-			expect(result).toBe(true);
+			it('should return true', () => {
+				const { user, submission } = setup();
+
+				const result = submissionRule.isApplicable(user, submission);
+
+				expect(result).toBe(true);
+			});
 		});
 
-		it('should return false if entity is not instance of Submission', () => {
-			const task = taskFactory.build();
+		describe('when entity is not instance of Submission', () => {
+			const setup = () => {
+				const user = userFactory.build({ roles: [] });
+				const task = taskFactory.build();
 
-			const result = submissionRule.isApplicable(student, task);
+				return { user, task };
+			};
 
-			expect(result).toBe(false);
+			it('should return false', () => {
+				const { user, task } = setup();
+
+				const result = submissionRule.isApplicable(user, task);
+
+				expect(result).toBe(false);
+			});
 		});
 	});
 
 	describe('hasPermission', () => {
 		describe('when user does not have required permissions', () => {
-			it('should return false', () => {
+			const setup = () => {
+				const permission = 'a' as Permission;
+				const user = userFactory.build({ roles: [] });
 				const task = taskFactory.build();
-				const submission = submissionFactory.build({ task, student });
+				const submission = submissionFactory.build({ task, student: user });
+				return { user, submission, permission };
+			};
 
-				const result = submissionRule.hasPermission(student, submission, {
+			it('should return false', () => {
+				const { user, submission, permission } = setup();
+
+				const result = submissionRule.hasPermission(user, submission, {
 					action: Actions.write,
-					requiredPermissions: [permissionC],
+					requiredPermissions: [permission],
 				});
 
 				expect(result).toBe(false);
@@ -81,111 +110,188 @@ describe('SubmissionRule', () => {
 		describe('when user has required permissions', () => {
 			describe('when user is student', () => {
 				describe('when action is "write"', () => {
-					it('should return true if user is creator', () => {
-						const task = taskFactory.build();
-						const submission = submissionFactory.build({ task, student });
+					describe('when user is creator', () => {
+						const setup = () => {
+							const permission = 'a' as Permission;
+							const student = buildStudent(permission);
+							const task = taskFactory.build();
+							const submission = submissionFactory.build({ task, student });
+							return { student, submission, permission };
+						};
 
-						const result = submissionRule.hasPermission(student, submission, {
-							action: Actions.write,
-							requiredPermissions: [permissionA],
+						it('should return true', () => {
+							const { student, submission, permission } = setup();
+
+							const result = submissionRule.hasPermission(student, submission, {
+								action: Actions.write,
+								requiredPermissions: [permission],
+							});
+
+							expect(result).toBe(true);
 						});
-
-						expect(result).toBe(true);
 					});
 
-					it('should return true if user is team member', () => {
-						const task = taskFactory.build();
-						const submission = submissionFactory.build({ task, student: student2, teamMembers: [student] });
+					describe('when user is team member', () => {
+						const setup = () => {
+							const permission = 'a' as Permission;
+							const student = buildStudent(permission);
+							const student2 = buildStudent(permission);
+							const task = taskFactory.build();
+							const submission = submissionFactory.build({ task, student: student2, teamMembers: [student] });
+							return { student, submission, permission };
+						};
 
-						const result = submissionRule.hasPermission(student, submission, {
-							action: Actions.write,
-							requiredPermissions: [permissionA],
+						it('should return true', () => {
+							const { student, submission, permission } = setup();
+
+							const result = submissionRule.hasPermission(student, submission, {
+								action: Actions.write,
+								requiredPermissions: [permission],
+							});
+
+							expect(result).toBe(true);
 						});
-
-						expect(result).toBe(true);
 					});
 
-					it('should return true if user is in associated course group', () => {
-						const task = taskFactory.build();
-						const courseGroup = courseGroupFactory.build({ students: [student, student2] });
-						const submission = submissionFactory.build({ task, student: student2, courseGroup });
+					describe('when user is in associated course group', () => {
+						const setup = () => {
+							const permission = 'a' as Permission;
+							const student = buildStudent(permission);
+							const student2 = buildStudent(permission);
+							const task = taskFactory.build();
+							const courseGroup = courseGroupFactory.build({ students: [student, student2] });
+							const submission = submissionFactory.build({ task, student: student2, courseGroup });
+							return { student, submission, permission };
+						};
 
-						const result = submissionRule.hasPermission(student, submission, {
-							action: Actions.write,
-							requiredPermissions: [permissionA],
+						it('should return true', () => {
+							const { student, submission, permission } = setup();
+
+							const result = submissionRule.hasPermission(student, submission, {
+								action: Actions.write,
+								requiredPermissions: [permission],
+							});
+
+							expect(result).toBe(true);
 						});
-
-						expect(result).toBe(true);
 					});
 
-					it('should return false if user is not creator nor team member nor in associated course group (even if submissions are public)', () => {
-						const course = courseFactory.build({ students: [student, student2] });
-						const task = taskFactory.build({ course, publicSubmissions: true });
-						const submission = submissionFactory.build({ task, student: student2 });
+					describe('when user is not creator nor team member nor in associated course group (even if submissions are public)', () => {
+						const setup = () => {
+							const permission = 'a' as Permission;
+							const student = buildStudent(permission);
+							const student2 = buildStudent(permission);
+							const course = courseFactory.build({ students: [student, student2] });
+							const task = taskFactory.build({ course, publicSubmissions: true });
+							const submission = submissionFactory.build({ task, student: student2 });
+							return { student, submission, permission };
+						};
 
-						const result = submissionRule.hasPermission(student, submission, {
-							action: Actions.write,
-							requiredPermissions: [permissionA],
+						it('should return false', () => {
+							const { student, submission, permission } = setup();
+
+							const result = submissionRule.hasPermission(student, submission, {
+								action: Actions.write,
+								requiredPermissions: [permission],
+							});
+
+							expect(result).toBe(false);
 						});
-
-						expect(result).toBe(false);
 					});
 				});
 
 				describe('when action is "read"', () => {
-					it('should return true if user is in same course and submissions are public', () => {
-						const course = courseFactory.build({ students: [student, student2] });
-						const task = taskFactory.build({ course, publicSubmissions: true });
-						const submission = submissionFactory.build({ task, student: student2 });
+					describe('when user is in same course and submissions are public', () => {
+						const setup = () => {
+							const permission = 'a' as Permission;
+							const student = buildStudent(permission);
+							const student2 = buildStudent(permission);
+							const course = courseFactory.build({ students: [student, student2] });
+							const task = taskFactory.build({ course, publicSubmissions: true });
+							const submission = submissionFactory.build({ task, student: student2 });
+							return { student, submission, permission };
+						};
 
-						const result = submissionRule.hasPermission(student, submission, {
-							action: Actions.read,
-							requiredPermissions: [permissionA],
+						it('should return true', () => {
+							const { student, submission, permission } = setup();
+
+							const result = submissionRule.hasPermission(student, submission, {
+								action: Actions.read,
+								requiredPermissions: [permission],
+							});
+
+							expect(result).toBe(true);
 						});
-
-						expect(result).toBe(true);
 					});
 
-					it('should return false if user does not have write access nor are submissions public', () => {
-						const course = courseFactory.build({ students: [student, student2] });
-						const task = taskFactory.build({ course });
-						const submission = submissionFactory.build({ task, student: student2 });
+					describe('when user does not have write access nor are submissions public', () => {
+						const setup = () => {
+							const permission = 'a' as Permission;
+							const student = buildStudent(permission);
+							const student2 = buildStudent(permission);
+							const course = courseFactory.build({ students: [student, student2] });
+							const task = taskFactory.build({ course });
+							const submission = submissionFactory.build({ task, student: student2 });
+							return { student, submission, permission };
+						};
 
-						const result = submissionRule.hasPermission(student, submission, {
-							action: Actions.read,
-							requiredPermissions: [permissionA],
+						it('should return false', () => {
+							const { student, submission, permission } = setup();
+
+							const result = submissionRule.hasPermission(student, submission, {
+								action: Actions.read,
+								requiredPermissions: [permission],
+							});
+
+							expect(result).toBe(false);
 						});
-
-						expect(result).toBe(false);
 					});
 				});
 			});
 
 			describe('when user is teacher', () => {
 				describe('when action is "write" (or "read" respectively)', () => {
-					it('should return true if user is course teacher', () => {
-						const course = courseFactory.build({ teachers: [teacher] });
-						const task = taskFactory.build({ course });
-						const submission = submissionFactory.build({ task, student });
+					describe('when user is course teacher', () => {
+						const setup = () => {
+							const permission = 'a' as Permission;
+							const teacher = buildTeacher(permission);
+							const course = courseFactory.build({ teachers: [teacher] });
+							const task = taskFactory.build({ course });
+							const submission = submissionFactory.build({ task });
+							return { teacher, submission, permission };
+						};
 
-						const result = submissionRule.hasPermission(teacher, submission, {
-							action: Actions.write,
-							requiredPermissions: [permissionC],
+						it('should return true', () => {
+							const { teacher, submission, permission } = setup();
+
+							const result = submissionRule.hasPermission(teacher, submission, {
+								action: Actions.write,
+								requiredPermissions: [permission],
+							});
+
+							expect(result).toBe(true);
 						});
-
-						expect(result).toBe(true);
 					});
 
-					it('should return false if user is teacher but not in course', () => {
-						const task = taskFactory.build();
-						const submission = submissionFactory.build({ task, student });
+					describe('when user is not course teacher', () => {
+						const setup = () => {
+							const permission = 'a' as Permission;
+							const teacher = buildTeacher(permission);
+							const task = taskFactory.build();
+							const submission = submissionFactory.build({ task });
+							return { teacher, submission, permission };
+						};
 
-						const result = submissionRule.hasPermission(teacher, submission, {
-							action: Actions.write,
-							requiredPermissions: [permissionC],
+						it('should return false', () => {
+							const { teacher, submission, permission } = setup();
+
+							const result = submissionRule.hasPermission(teacher, submission, {
+								action: Actions.write,
+								requiredPermissions: [permission],
+							});
+
+							expect(result).toBe(false);
 						});
-
-						expect(result).toBe(false);
 					});
 				});
 			});
