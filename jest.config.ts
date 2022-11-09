1de0fc5139bc6980d0ce144dc14a806a257fca82
SevenWaysDP <sergej.hoffmann@dataport.de>
2022-11-09 08:19:08 +0100

    BC-2690 - change instance of mongo-memory-server

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,11 @@
 // jest.config.ts
 import type { Config } from '@jest/types';
 
-const { pathsToModuleNameMapper } = require('ts-jest');
-const { compilerOptions } = require('./tsconfig.json');
-
 // Sync object
 const config: Config.InitialOptions = {
 	moduleFileExtensions: ['js', 'json', 'ts'],
 	rootDir: '.',
+	preset: 'ts-jest',
 	/* we have tests in src/...*.spec.ts and test/...*.e2e-spec.ts either we test all files via `npm run nest:test` or override the regex in npm scripts to separate the execution via `npm run nest:test:spec` or `npm run nest:test:e2e` */
 	testRegex: '\\.(e2e-)?spec\\.ts$',
 	// ignore legacy mocha tests
@@ -43,6 +41,8 @@ const config: Config.InitialOptions = {
 	// detectOpenHandles: true,
 	// detectLeaks: true,
 	roots: ['<rootDir>/apps/'],
+	globalSetup: '<rootDir>/apps/server/test/globalSetup.ts',
+	globalTeardown: '<rootDir>/apps/server/test/globalTeardown.ts',
 	moduleNameMapper: {
 		// add ts-config path's here as regex
 		'^@shared/(.*)$': '<rootDir>/apps/server/src/shared/$1',
