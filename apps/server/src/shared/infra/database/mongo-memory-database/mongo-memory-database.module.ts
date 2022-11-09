b3c96650d9e3e1fd9974c28790957839a5ca5010
SevenWaysDP <sergej.hoffmann@dataport.de>
2022-11-09 16:51:26 +0100

    BC-2690 - change logic of create dbName

--- apps/server/src/shared/infra/database/mongo-memory-database/mongo-memory-database.module.ts
+++ apps/server/src/shared/infra/database/mongo-memory-database/mongo-memory-database.module.ts
@@ -2,13 +2,16 @@ import { MikroORM } from '@mikro-orm/core';
 import { MikroOrmModule, MikroOrmModuleAsyncOptions } from '@mikro-orm/nestjs';
 import { DynamicModule, Inject, Module, OnModuleDestroy } from '@nestjs/common';
 import { ALL_ENTITIES } from '@shared/domain';
+import _ from 'lodash';
 import { MongoDatabaseModuleOptions } from './types';
+
+const dbName = () => _.times(20, () => _.random(35).toString(36)).join('');
 
 const createMikroOrmModule = (options: MikroOrmModuleAsyncOptions): DynamicModule => {
 	const mikroOrmModule = MikroOrmModule.forRootAsync({
 		useFactory: () => {
 			// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-			const clientUrl = `${process.env.MONGO_TEST_URI}/${String(10000 + Math.floor(Math.random() * 1000))}`;
+			const clientUrl = `${process.env.MONGO_TEST_URI}/${dbName()}`;
 			return {
 				allowGlobalContext: true, // can be overridden by options
 				...options,
