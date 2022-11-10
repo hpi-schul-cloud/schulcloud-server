e1875c7344c7c415bd3e426d7c3b2128630ae565
virgilchiriac <17074330+virgilchiriac@users.noreply.github.com>
2022-11-10 12:56:32 +0100

    fix lint

--- apps/server/src/shared/common/validator/date.validator.ts
+++ apps/server/src/shared/common/validator/date.validator.ts
@@ -2,11 +2,11 @@ import { ValidatorConstraint, ValidatorConstraintInterface, ValidationArguments 
 
 @ValidatorConstraint({ name: 'isBefore', async: false })
 export class IsBeforeConstraint implements ValidatorConstraintInterface {
-	validate(propertyValue: string, args: ValidationArguments) {
-		return propertyValue < args.object[args.constraints[0]];
+	validate(propertyValue: Date, args: ValidationArguments) {
+		return propertyValue < args.object[args.constraints[0] as unknown as string];
 	}
 
 	defaultMessage(args: ValidationArguments) {
-		return `${args.property} must be before ${args.constraints[0]}`;
+		return `${args.property} must be before ${args.constraints[0] as unknown as string}`;
 	}
 }
