7fdfc4a9e9cb57850004679c77ee67a11ae750f9
Mrika Llabani <mrika.llabani@capgemini.com>
2022-11-11 12:53:39 +0100

    map ExternalTool to DO WIP

---
+++ apps/server/src/modules/tool/mapper/external-tool-do.mapper.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { ExternalToolParams } from '@src/modules/tool/controller/dto/request/external-tool-create.params';
+import {
+	CustomParameterProperty,
+	ExternalToolConfigProperty,
+	ExternalToolDO,
+} from '@shared/domain/domainobject/external-tool.do';
+import { ExternalToolConfigCreateParams } from '@src/modules/tool/controller/dto/request/external-tool-config.params';
+import { CustomParameterCreateParams } from '@src/modules/tool/controller/dto/request/custom-parameter.params';
+import { CustomParameterScope } from '@shared/domain';
+
+@Injectable()
+export class ExternalToolDOMapper {
+	mapRequestToExternalToolDO(externalToolParams: ExternalToolParams): ExternalToolDO {
+		const config: ExternalToolConfigProperty = this.mapExternalToolConfigToDO(externalToolParams.config);
+		const customParameter: CustomParameterProperty = this.mapExternalToolCustomParameterToDO(
+			externalToolParams.parameters
+		);
+		return new ExternalToolDO({ ...externalToolParams });
+	}
+
+	private mapExternalToolConfigToDO(externalToolConfigParams: ExternalToolConfigCreateParams) {
+		return new ExternalToolConfigProperty({ ...externalToolConfigParams });
+	}
+
+	private mapExternalToolCustomParameterToDO(
+		customParameterParams: CustomParameterCreateParams[]
+	): CustomParameterProperty[] {
+		let customParameterPropertyArr: CustomParameterProperty[];
+		customParameterParams.forEach((customParameterParam) => {
+			const mappedCustomParameterParam: CustomParameterProperty = new CustomParameterProperty({
+				name: customParameterParam.name,
+				default: customParameterParam.default,
+				regex: customParameterParam.regex || '',
+				scope: customParameterParam.scope,
+				type: customParameterParam.type,
+				location: customParameterParam.location,
+			});
+			customParameterPropertyArr.push(mappedCustomParameterParam);
+		});
+		return customParameterPropertyArr;
+	}
+}
