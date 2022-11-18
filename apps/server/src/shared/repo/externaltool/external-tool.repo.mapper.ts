307aec7ff6d70fbffeab729b7dc821cb019f954f
Mrika Llabani <mrika.llabani@capgemini.com>
2022-11-18 15:38:00 +0100

    fix import

--- apps/server/src/shared/repo/externaltool/external-tool.repo.mapper.ts
+++ apps/server/src/shared/repo/externaltool/external-tool.repo.mapper.ts
@@ -1,12 +1,4 @@
 import {
-	BasicToolConfig,
-	CustomParameter,
-	ExternalTool,
-	IExternalToolProperties,
-	Lti11ToolConfig,
-	Oauth2ToolConfig,
-} from '@shared/domain';
-import {
 	BasicToolConfigDO,
 	CustomParameterDO,
 	ExternalToolDO,
@@ -15,6 +7,15 @@ import {
 } from '@shared/domain/domainobject/external-tool';
 import { EntityProperties } from '@shared/repo';
 import { Injectable } from '@nestjs/common';
+import { from } from 'rxjs';
+import {
+	BasicToolConfig,
+	CustomParameter,
+	ExternalTool,
+	IExternalToolProperties,
+	Lti11ToolConfig,
+	Oauth2ToolConfig,
+} from '../../domain';
 
 @Injectable()
 export class ExternalToolRepoMapper {
