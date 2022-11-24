252294b80236279f9638961ca50f9fcc12764187
hoeppner-dataport <marcus.hoeppner@dataport.de>
2022-11-24 12:38:50 +0100

    chore: removed unused import

--- apps/server/src/modules/learnroom/service/task-copy.service.ts
+++ apps/server/src/modules/learnroom/service/task-copy.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CopyHelperService } from '@shared/domain';
 import { Course, Lesson, Task, User } from '@shared/domain/entity';
-import { CopyElementType, CopyStatus, CopyStatusEnum, EntityId } from '@shared/domain/types';
+import { CopyElementType, CopyStatus, CopyStatusEnum } from '@shared/domain/types';
 import { TaskRepo } from '@shared/repo';
 import { CopyFilesService } from '@src/modules/files-storage-client';
 import { FileUrlReplacement } from '@src/modules/files-storage-client/service/copy-files.service';
