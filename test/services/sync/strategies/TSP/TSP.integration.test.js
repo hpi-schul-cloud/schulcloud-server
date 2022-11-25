7a8759d4752e8cbaf31ce0f82d414958c01a6276
Malte Berg <malte.berg@capgemini.com>
2022-11-25 10:26:14 +0100

    EW-179 Adjust TSP.integration.test

--- test/services/sync/strategies/TSP/TSP.integration.test.js
+++ test/services/sync/strategies/TSP/TSP.integration.test.js
@@ -3,28 +3,27 @@ const { expect } = require('chai');
 const appPromise = require('../../../../../src/app');
 const { setupNestServices, closeNestServices } = require('../../../../utils/setup.nest.services');
 
-const {
-	cleanup,
-	createTestSchool: createSchool,
-	createTestSystem: createSystem,
-	info,
-} = require('../../../helpers/testObjects')(appPromise());
-
-const { users: createdTestUsers, accounts: createdAccounts } = info();
+const testObjects = require('../../../helpers/testObjects')(appPromise());
 
 const { equal: equalIds } = require('../../../../../src/helper/compare').ObjectId;
 
 const { findSchool, createUserAndAccount } = require('../../../../../src/services/sync/strategies/TSP/TSP');
+const { userModel } = require('../../../../../src/services/user/model');
 
 describe('TSP API integration tests', () => {
 	let app;
 	let server;
 	let nestServices;
+	let nestAccountService;
+
+	let createdAccount;
+	let createdUser;
 
 	before(async () => {
 		app = await appPromise();
 		server = app.listen(0);
 		nestServices = await setupNestServices(app);
+		nestAccountService = app.service('nest-account-service');
 	});
 
 	after(async () => {
@@ -32,10 +31,27 @@ describe('TSP API integration tests', () => {
 		await closeNestServices(nestServices);
 	});
 
+	afterEach(async () => {
+		if (createdAccount) {
+			await nestAccountService.delete(createdAccount.id);
+			createdAccount = undefined;
+		}
+
+		if (createdUser) {
+			await userModel.remove(createdUser);
+			createdUser = undefined;
+		}
+
+		await testObjects.cleanup();
+	});
+
 	describe('#findSchool', () => {
 		it('should find a school based on its TSP school identifier', async () => {
 			const tspIdentifier = '47274';
-			const school = await createSchool({ source: 'tsp', sourceOptions: { schoolIdentifier: tspIdentifier } });
+			const school = await testObjects.createTestSchool({
+				source: 'tsp',
+				sourceOptions: { schoolIdentifier: tspIdentifier },
+			});
 			const foundSchool = await findSchool(app, tspIdentifier);
 			expect(equalIds(school._id, foundSchool._id)).to.equal(true);
 		});
@@ -44,41 +60,29 @@ describe('TSP API integration tests', () => {
 			const foundSchool = await findSchool(app, '63472');
 			expect(foundSchool).to.equal(null);
 		});
-
-		after(cleanup);
 	});
 
 	describe('#createUserAndAccount', () => {
 		it('should create an activated user and account based on the given details', async () => {
-			const school = await createSchool();
+			const school = await testObjects.createTestSchool();
 			const userDetails = {
 				firstName: 'Thor',
 				lastName: 'Heyerdahl',
 				email: 'sailing@pacific.ocean',
 				schoolId: school._id,
 				source: 'tsp',
-				sourceOptions: { awesome: true, tspUid: '234567' },
+				sourceOptions: { awesome: true, tspUid: '2345' },
 			};
 			const roles = ['administrator', 'teacher'];
-			const systemId = (await createSystem())._id;
-			const createdUser = await createUserAndAccount(app, userDetails, roles, systemId);
-			createdTestUsers.push(createdUser._id);
+			const systemId = (await testObjects.createTestSystem())._id;
+			createdUser = await createUserAndAccount(app, userDetails, roles, systemId);
+			createdAccount = await nestAccountService.findByUserId(createdUser._id);
 
 			expect(createdUser).to.be.ok;
 			expect(createdUser.source).to.equal('tsp');
 			expect(createdUser.sourceOptions.awesome).to.equal(true);
-
-			// const [account] = await app.service('accounts').find({
-			// 	query: { userId: createdUser._id },
-			// });
-			const account = await app.service('nest-account-service').findByUserId(createdUser._id);
-			// createdAccounts.push(account._id);
-			createdAccounts.push(account.id);
-
-			expect(account.username).to.equal('tsp/234567');
-			expect(account.activated).to.equal(true);
+			expect(createdAccount.username).to.equal('tsp/2345');
+			expect(createdAccount.activated).to.equal(true);
 		});
-
-		after(cleanup);
 	});
 });
