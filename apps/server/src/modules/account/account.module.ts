5e26d7689917bb778f5d7a1cffefbef6e083f012
HeftyTea <93934058+HeftyTea@users.noreply.github.com>
2022-11-29 10:59:08 +0100

    EW-378 Move account repo into account module (#3742)

--- apps/server/src/modules/account/account.module.ts
+++ apps/server/src/modules/account/account.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PermissionService } from '@shared/domain';
-import { AccountRepo, SystemRepo, UserRepo } from '@shared/repo';
+import { SystemRepo, UserRepo } from '@shared/repo';
 import { AccountController } from './controller/account.controller';
+import { AccountRepo } from './repo/account.repo';
 import { AccountService } from './services/account.service';
 import { AccountValidationService } from './services/account.validation.service';
 import { AccountUc } from './uc/account.uc';
