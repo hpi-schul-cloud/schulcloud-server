87a58c975d2f6a0d3f1ba4dbed779390ecba1683
MateuszSapalaPassCon <Mateusz.Sapala@gca.pass-consulting.com>
2022-12-02 12:15:03 +0100

    BC-2740 Fix tests

--- test/services/resolve/scopes.test.js
+++ test/services/resolve/scopes.test.js
@@ -1,55 +1,60 @@
 const assert = require('assert');
 const appPromise = require('../../../src/app');
+const testObjects = require('../helpers/testObjects')(appPromise());
 
 describe('resolve/scopes service', () => {
 	let app;
 	let service;
+	const testData = {};
 
 	before(async () => {
 		app = await appPromise();
 		service = app.service('resolve/scopes');
+		testData.school = await testObjects.createTestSchool();
+		testData.user = await testObjects.createTestUser({ schoolId: testData.school._id });
+		testData.course = await testObjects.createTestCourse({
+			schoolId: testData.school._id,
+			userIds: [testData.user._id],
+		});
+		testData.admin = await testObjects.createTestUser({ roles: 'admin', schoolId: testData.school._id });
 	});
 
 	it('registered the resolve/scopes service', () => {
 		assert.ok(service);
 	});
 
-	it('get error if id is no object id', () =>
-		service
-			.get('123')
-			.then((_) => {
-				throw new Error('was not supposed to succeed');
-			})
-			.catch((err) => {
-				assert(err.message.includes('Cast to ObjectId failed'));
-				assert(err.name === 'BadRequest');
-				assert(err.code === 400);
-			}));
+	it('get error if id is no object id', () => {
+		try {
+			service.get('123');
+			throw new Error('was not supposed to succeed');
+		} catch (err) {
+			assert(err.message.includes('Cast to ObjectId failed'));
+			assert(err.name === 'BadRequest');
+			assert(err.code === 400);
+		}
+	});
 
-	it('get 404 if no user is found', () =>
-		service
-			.get('00006e13b101c8742dc2d092')
-			.then((_) => {
-				throw new Error('was not supposed to succeed');
-			})
-			.catch((err) => {
-				assert(err.message.includes('No record found for id'));
-				assert(err.name === 'NotFound');
-				assert(err.code === 404);
-			}));
+	it('get 404 if no user is found', () => {
+		try {
+			service.get('00006e13b101c8742dc2d092');
+			throw new Error('was not supposed to succeed');
+		} catch (err) {
+			assert(err.message.includes('No record found for id'));
+			assert(err.name === 'NotFound');
+			assert(err.code === 404);
+		}
+	});
 
-	it('return scopes if user is found', () =>
-		service.get('0000d224816abba584714c9c').then((data) => {
-			assert(data.data.length > 0);
-			return data;
-		}));
+	it('return scopes if user is found', async () => {
+		const data = await service.get(testData.user._id);
+		assert(data.data.length > 0);
+	});
 
-	it('return courseAdmin scope if admin is found and his school has a course', () =>
-		service.get('0000d213816abba584714c0a').then((data) => {
-			const courseAdmins = data.data.filter(
-				(scope) => scope.type === 'scope' && scope.attributes.scopeType === 'courseAdmin'
-			);
-			assert(courseAdmins.length > 0);
-			return data;
-		}));
+	it('return courseAdmin scope if admin is found and his school has a course', async () => {
+		const data = await service.get(testData.admin._id);
+		const courseAdmin = data.data.filter(
+			(scope) => scope.attributes.scopeType === 'courseAdmin' && scope.id === testData.course._id
+		);
+		assert(courseAdmin.length === 1);
+	});
 });
--- test/services/resolve/users.test.js
+++ test/services/resolve/users.test.js
@@ -1,37 +1,39 @@
 const assert = require('assert');
 const _ = require('lodash');
 const appPromise = require('../../../src/app');
+const testObjects = require('../helpers/testObjects')(appPromise());
 
 describe('resolve/users service', () => {
 	let app;
 	let service;
+	const testData = {};
 
 	before(async () => {
 		app = await appPromise();
 		service = app.service('resolve/users');
+		testData.user = await testObjects.createTestUser();
+		testData.course = await testObjects.createTestCourse({ userIds: [testData.user._id] });
 	});
 
 	it('registered the resolve/users service', () => {
 		assert.ok(service);
 	});
 
-	it('get error if no scope is found', () =>
-		service
-			.get('123')
-			.then((_) => {
-				throw new Error('was not supposed to succeed');
-			})
-			.catch((err) => {
-				assert(err.message.includes('No scope found for given id.'));
-				assert(err.name === 'NotFound');
-				assert(err.code === 404);
-			}));
+	it('get error if no scope is found', () => {
+		try {
+			service.get('00006e13b101c8742dc2d092');
+			throw new Error('was not supposed to succeed');
+		} catch (err) {
+			assert(err.message.includes('No scope found for given id.'));
+			assert(err.name === 'NotFound');
+			assert(err.code === 404);
+		}
+	});
 
-	it('return users if scope is found', () =>
-		service.get('0000dcfbfb5c7a3f00bf21ab').then((data) => {
-			assert(data.data.length > 0);
-			assert(data.data[0].type === 'user');
-			assert(_.find(data.data, (user) => user.id === '0000d213816abba584714c0a' || '0000d231816abba584714c9e'));
-			return data;
-		}));
+	it('return users if scope is found', () => {
+		const data = service.get(testData.user._id);
+		assert(data.data.length > 0);
+		assert(data.data[0].type === 'user');
+		assert(_.find(data.data, (user) => user.id === testData.course._id));
+	});
 });
