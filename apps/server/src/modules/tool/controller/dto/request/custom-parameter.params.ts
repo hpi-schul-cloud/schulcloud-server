95fae457d83d4aae9bc83e3438a758e874ebde7d
Igor Richter <igor.richter@capgemini.com>
2022-12-07 11:27:32 +0100

    add new attributes to custom parameters

--- apps/server/src/modules/tool/controller/dto/request/custom-parameter.params.ts
+++ apps/server/src/modules/tool/controller/dto/request/custom-parameter.params.ts
@@ -1,4 +1,4 @@
-import { IsEnum, IsOptional, IsString } from 'class-validator';
+import { IsBoolean, IsEnum, IsOptional, IsString } from 'class-validator';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import {
 	CustomParameterLocationParams,
@@ -21,6 +21,11 @@ export class CustomParameterCreateParams {
 	@ApiPropertyOptional()
 	regex?: string;
 
+	@IsString()
+	@IsOptional()
+	@ApiPropertyOptional()
+	regexComment?: string;
+
 	@IsEnum(CustomParameterScopeParams)
 	@ApiProperty()
 	scope!: CustomParameterScopeParams;
@@ -32,4 +37,8 @@ export class CustomParameterCreateParams {
 	@IsEnum(CustomParameterTypeParams)
 	@ApiProperty()
 	type!: CustomParameterTypeParams;
+
+	@IsBoolean()
+	@ApiProperty()
+	isOptional!: boolean;
 }
