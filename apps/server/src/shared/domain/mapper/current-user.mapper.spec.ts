2408436a8748367f535af836eeb87c655662e48a
Patrick Sachmann <patrick.sachmann@capgemini.com>
2022-12-15 15:38:01 +0100

    adding a scenario to test

--- apps/server/src/shared/domain/mapper/current-user.mapper.spec.ts
+++ apps/server/src/shared/domain/mapper/current-user.mapper.spec.ts
@@ -1,5 +1,5 @@
 import { MikroORM } from '@mikro-orm/core';
-import { Permission, Role, RoleName, User } from '@shared/domain';
+import { Permission, Role, RoleName } from '@shared/domain';
 import { schoolFactory, setupEntities, userFactory } from '@shared/testing';
 import { ICurrentUser } from '../interface';
 import { CurrentUserMapper } from './current-user.mapper';
@@ -16,64 +16,66 @@ describe('CurrentUserMapper', () => {
 		await orm.close();
 	});
 
-	it('should map with roles', () => {
-		const user = userFactory.buildWithId({
-			roles: [new Role({ name: RoleName.TEACHER, permissions: [Permission.STUDENT_EDIT] })],
+	describe('when mapping from a user entity to the current user object', () => {
+		it('should map with roles', () => {
+			const user = userFactory.buildWithId({
+				roles: [new Role({ name: RoleName.TEACHER, permissions: [Permission.STUDENT_EDIT] })],
+			});
+			const currentUser: ICurrentUser = CurrentUserMapper.userToICurrentUser(accountId, user);
+			expect(currentUser).toMatchObject({
+				accountId,
+				systemId: undefined,
+				roles: ['teacher'],
+				schoolId: null,
+				user: {
+					firstName: user.firstName,
+					lastName: user.lastName,
+					roles: [
+						{
+							id: null,
+							name: 'teacher',
+						},
+					],
+					permissions: ['STUDENT_EDIT'],
+				},
+			});
 		});
-		const currentUser: ICurrentUser = CurrentUserMapper.userToICurrentUser(accountId, user);
-		expect(currentUser).toMatchObject({
-			accountId,
-			systemId: undefined,
-			roles: ['teacher'],
-			schoolId: null,
-			user: {
-				firstName: user.firstName,
-				lastName: user.lastName,
-				roles: [
-					{
-						id: null,
-						name: 'teacher',
-					},
-				],
-				permissions: ['STUDENT_EDIT'],
-			},
-		});
-	});
 
-	it('should map without roles', () => {
-		const user = userFactory.buildWithId();
-		const currentUser: ICurrentUser = CurrentUserMapper.userToICurrentUser(accountId, user);
-		expect(currentUser).toMatchObject({
-			accountId,
-			systemId: undefined,
-			roles: [],
-			schoolId: null,
-			user: {
-				firstName: user.firstName,
-				lastName: user.lastName,
+		it('should map without roles', () => {
+			const user = userFactory.buildWithId();
+			const currentUser: ICurrentUser = CurrentUserMapper.userToICurrentUser(accountId, user);
+			expect(currentUser).toMatchObject({
+				accountId,
+				systemId: undefined,
 				roles: [],
-				permissions: [],
-			},
+				schoolId: null,
+				user: {
+					firstName: user.firstName,
+					lastName: user.lastName,
+					roles: [],
+					permissions: [],
+				},
+			});
 		});
-	});
 
-	it('should map system and school', () => {
-		const user = userFactory.buildWithId({
-			school: schoolFactory.buildWithId(),
-		});
-		const systemId = 'mockSystemId';
-		const currentUser: ICurrentUser = CurrentUserMapper.userToICurrentUser(accountId, user, systemId);
-		expect(currentUser).toMatchObject({
-			accountId,
-			systemId,
-			roles: [],
-			schoolId: user.school.id,
-			user: {
-				firstName: user.firstName,
-				lastName: user.lastName,
+		it('should map system and school', () => {
+			const user = userFactory.buildWithId({
+				school: schoolFactory.buildWithId(),
+			});
+			const systemId = 'mockSystemId';
+			const currentUser: ICurrentUser = CurrentUserMapper.userToICurrentUser(accountId, user, systemId);
+			expect(currentUser).toMatchObject({
+				accountId,
+				systemId,
 				roles: [],
-				permissions: [],
-			},
+				schoolId: user.school.id,
+				user: {
+					firstName: user.firstName,
+					lastName: user.lastName,
+					roles: [],
+					permissions: [],
+				},
+			});
 		});
 	});
 });
