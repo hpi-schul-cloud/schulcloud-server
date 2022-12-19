1f71b641d854b4dcf685a1d5e2ae667d3a4971ef
Max Bischof <106820326+bischofmax@users.noreply.github.com>
2022-12-19 13:17:22 +0100

    BC-1508 - Delete files on submission delete (#3780)
    * Add submission delete api to controller

--- apps/server/src/modules/task/service/submission.service.ts
+++ apps/server/src/modules/task/service/submission.service.ts
@@ -1,14 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { Counted, EntityId, Submission } from '@shared/domain';
 import { SubmissionRepo } from '@shared/repo';
+import { FileParamBuilder, FilesStorageClientAdapterService } from '@src/modules/files-storage-client';
 
 @Injectable()
 export class SubmissionService {
-	constructor(private readonly submissionRepo: SubmissionRepo) {}
+	constructor(
+		private readonly submissionRepo: SubmissionRepo,
+		private readonly filesStorageClientAdapterService: FilesStorageClientAdapterService
+	) {}
+
+	async findById(submissionId: EntityId): Promise<Submission> {
+		return this.submissionRepo.findById(submissionId);
+	}
 
 	async findAllByTask(taskId: EntityId): Promise<Counted<Submission[]>> {
 		const submissions = this.submissionRepo.findAllByTaskIds([taskId]);
 
 		return submissions;
+	}
+
+	async delete(submission: Submission): Promise<void> {
+		const params = FileParamBuilder.build(submission.school.id, submission);
+		await this.filesStorageClientAdapterService.deleteFilesOfParent(params);
+
+		await this.submissionRepo.delete(submission);
 	}
 }
