3169673393f73421ec85eb729321bca58369df95
dyedwiper <maximilian.becker@dataport.de>
2022-12-20 12:16:49 +0100

    BC-2798 Update comment

--- apps/server/src/modules/authorization/errors/loggable-forbidden.exception.ts
+++ apps/server/src/modules/authorization/errors/loggable-forbidden.exception.ts
@@ -3,9 +3,15 @@ import { EntityId, IPermissionContext } from '@shared/domain';
 import { ErrorLogMessage, ILoggable } from '@src/core/logger/interfaces/loggable';
 
 // This and the ForbiddenError are alternative implementations.
-// I think LoggableForbiddenException is more expressive about what it is.
-// ForbiddenError is more in line with our naming style because we always(?) use "error" instead of "exception".
-// I (Max) tend to favor LoggableForbiddenException.
+// There are two decisions to make here:
+// 1) naming:
+// 		I think LoggableForbiddenException is more expressive about what it is.
+//		ForbiddenError is more in line with our naming style because we always/mostly(?) use "error" instead of "exception".
+// 2) which class to extend:
+//		Extending the pre-defined Nest HttpExceptions like ForbiddenException, we automatically stick to errors corresponding to the HTTP status codes.
+//		Extending HttpException would basically do the same but less clear IMO.
+//		Extending JavaScript's Error class, we would either need to add HTTP functionality ourself or otherwise map to HttpExceptions in some place (probably the error filter), which seems overly complicated.
+// I (Max) favor LoggableForbiddenException extending ForbiddenException.
 export class LoggableForbiddenException extends ForbiddenException implements ILoggable {
 	constructor(private userId: EntityId, private entityId: EntityId, private context: IPermissionContext) {
 		super();
