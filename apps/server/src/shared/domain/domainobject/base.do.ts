1a8d211772f6b11055219d80b38aee72f02633aa
Bartosz Nowicki <bartosz.nowicki@gca.pass-consulting.com>
2023-01-03 12:38:03 +0100

    remove BaseWithTimestampsDO class as it is no longer used

--- apps/server/src/shared/domain/domainobject/base.do.ts
+++ apps/server/src/shared/domain/domainobject/base.do.ts
@@ -6,21 +6,6 @@ export abstract class BaseDO {
 	}
 }
 
-export abstract class BaseWithTimestampsDO extends BaseDO {
-	id?: string;
-
-	createdAt?: Date;
-
-	updatedAt?: Date;
-
-	protected constructor(props: BaseWithTimestampsDO) {
-		super();
-		this.id = props.id;
-		this.createdAt = props.createdAt;
-		this.updatedAt = props.updatedAt;
-	}
-}
-
 export interface IBaseEntityProps {
 	id?: string;
 }
