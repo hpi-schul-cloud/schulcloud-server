093cee8cca52a72e6bb095c9bc6ad62c398b8695
Marvin Öhlerking <marvin.oehlerking@capgemini.com>
2023-01-10 16:14:47 +0100

    imports

--- apps/server/src/modules/oauth/oauth-api.module.ts
+++ apps/server/src/modules/oauth/oauth-api.module.ts
@@ -3,12 +3,13 @@ import { ProvisioningModule } from '@src/modules/provisioning';
 import { SystemModule } from '@src/modules/system';
 import { UserModule } from '@src/modules/user';
 import { UserMigrationModule } from '@src/modules/user-migration';
+import { LoggerModule } from '../../core/logger';
 import { OauthSSOController } from './controller/oauth-sso.controller';
 import { OauthModule } from './oauth.module';
 import { HydraOauthUc, OauthUc } from './uc';
 
 @Module({
-	imports: [OauthModule, SystemModule, UserModule, UserMigrationModule, ProvisioningModule],
+	imports: [OauthModule, SystemModule, UserModule, UserMigrationModule, ProvisioningModule, LoggerModule],
 	controllers: [OauthSSOController],
 	providers: [OauthUc, HydraOauthUc],
 })
