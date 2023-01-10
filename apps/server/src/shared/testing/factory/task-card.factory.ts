01c96173fa745dbe91d152d2fcd6c73dabffe902
MartinSchuhmacher <mschuhmacher@gmx.de>
2023-01-10 15:25:10 +0100

    fix properties in task card factory

--- apps/server/src/shared/testing/factory/task-card.factory.ts
+++ apps/server/src/shared/testing/factory/task-card.factory.ts
@@ -1,7 +1,7 @@
 import { CardType, ITaskCardProps, TaskCard } from '@shared/domain';
-import { taskFactory } from './task.factory';
 import { BaseFactory } from './base.factory';
 import { schoolFactory } from './school.factory';
+import { taskFactory } from './task.factory';
 import { userFactory } from './user.factory';
 
 class TaskCardFactory extends BaseFactory<TaskCard, ITaskCardProps> {}
@@ -11,10 +11,8 @@ export const taskCardFactory = TaskCardFactory.define(TaskCard, ({ sequence }) =
 	const school = schoolFactory.build();
 	const creator = userFactory.build({ school });
 	const cardElements = [];
-	// private is by default in constructor true, but in the most test cases we need private: false
 	return {
-		name: `task card #${sequence}`,
-		private: false,
+		isDraft: false,
 		cardType: CardType.Task,
 		cardElements,
 		draggable: true,
