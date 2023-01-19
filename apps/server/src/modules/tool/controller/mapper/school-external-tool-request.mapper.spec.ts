fbef4d76e3c7865a4c19fbbc0d6ba8c64e57bb0e
Arne Gnisa <arne.gnisa@capgemini.com>
2023-01-19 11:02:05 +0100

    Fixed test

--- apps/server/src/modules/tool/controller/mapper/school-external-tool-request.mapper.spec.ts
+++ apps/server/src/modules/tool/controller/mapper/school-external-tool-request.mapper.spec.ts
@@ -1,6 +1,7 @@
 import { SchoolExternalToolRequestMapper } from './school-external-tool-request.mapper';
-import { SchoolExternalToolPostParams } from '../dto';
+import { CustomParameterEntryParam, SchoolExternalToolPostParams } from '../dto';
 import { SchoolExternalTool } from '../../uc/dto/school-external-tool.types';
+import { CustomParameterEntryDO } from '@shared/domain/domainobject/external-tool';
 
 describe('SchoolExternalToolRequestMapper', () => {
 	const mapper: SchoolExternalToolRequestMapper = new SchoolExternalToolRequestMapper();
@@ -8,23 +9,22 @@ describe('SchoolExternalToolRequestMapper', () => {
 	describe('mapSchoolExternalToolRequest is called', () => {
 		describe('when SchoolExternalToolPostParams is given', () => {
 			it('should return an schoolExternalTool', () => {
+				const param: CustomParameterEntryParam = {
+					name: 'name',
+					value: 'value',
+				};
 				const params: SchoolExternalToolPostParams = {
 					toolId: 'toolId',
 					version: 1,
 					schoolId: 'schoolId',
-					parameters: [
-						{
-							name: 'name',
-							value: 'value',
-						},
-					],
+					parameters: [param],
 				};
 
 				const schoolExternalTool: SchoolExternalTool = mapper.mapSchoolExternalToolRequest(params);
 
 				expect(schoolExternalTool).toEqual<SchoolExternalTool>({
 					toolId: params.toolId,
-					parameters: [{ name: params.parameters[0].name, value: params.parameters[0].value }],
+					parameters: [{ name: param.name, value: param.value }],
 					schoolId: params.schoolId,
 					toolVersion: params.version,
 				});
