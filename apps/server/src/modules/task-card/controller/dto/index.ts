b1d71247b77869c9fa8f314872e2755bfa41449e
virgilchiriac <17074330+virgilchiriac@users.noreply.github.com>
2023-01-30 18:42:24 +0100

    BC-2994 - refactor task card create params (#3855)
    Co-authored-by: MartinSchuhmacher <mschuhmacher@gmx.de>

--- apps/server/src/modules/task-card/controller/dto/index.ts
+++ apps/server/src/modules/task-card/controller/dto/index.ts
@@ -1,4 +1,3 @@
-export * from './create-task-card.params';
 export * from './task-card.response';
 export * from './task-card.url.params';
-export * from './update-task-card.params';
+export * from './task-card.params';
