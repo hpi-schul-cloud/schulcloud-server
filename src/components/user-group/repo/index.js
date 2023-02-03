715bbe78007492b6219f23fd2bc0bc899ad963d0
Błażej Szczepanowski <blazej1679@gmail.com>
2023-02-03 14:51:47 +0100

    Separate teams component from users-group.

--- src/components/user-group/repo/index.js
+++ src/components/user-group/repo/index.js
@@ -1,7 +1,5 @@
 const classesRepo = require('./classes.repo');
-const teamsRepo = require('./teams.repo');
 
 module.exports = {
 	classesRepo,
-	teamsRepo,
 };
--- src/components/user-group/uc/deleteUserData.unit.test.js
+++ src/components/user-group/uc/deleteUserData.unit.test.js
@@ -5,10 +5,9 @@ const { ObjectId } = require('mongoose').Types;
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 const deleteUserData = require('./deleteUserData.uc');
-const deleteUserTeamsData = require('./deleteUserTeamsData.uc');
 const { setupNestServices, closeNestServices } = require('../../../../test/utils/setup.nest.services');
 
-const { classesRepo, teamsRepo } = require('../repo/index');
+const { classesRepo } = require('../repo/index');
 const { AssertionError } = require('../../../errors');
 const { toString: idToString } = require('../../../helper/compare').ObjectId;
 
@@ -18,7 +17,6 @@ chai.use(chaiAsPromised);
 const USER_ID = new ObjectId();
 const USER_ID_WITH_NO_CLASS_AND_TEAMS = new ObjectId();
 const CLASS_ID = new ObjectId();
-const TEAMS_ID = new ObjectId();
 const createTestGetClassForUserResult = (classId, student, teacher) => {
 	if (!classId) {
 		return [];
@@ -29,18 +27,6 @@ const createTestGetClassForUserResult = (classId, student, teacher) => {
 			_id: classId,
 			student,
 			teacher,
-		},
-	];
-};
-
-const createTestGetTeamsForUserResult = (teamId) => {
-	if (!teamId) {
-		return [];
-	}
-
-	return [
-		{
-			_id: teamId,
 		},
 	];
 };
@@ -56,18 +42,7 @@ const initClassStubs = ({ classId, isStudent, isTeacher }) => {
 	return { getClassesStub, removeClassesStub };
 };
 
-const initTeamsStubs = ({ teamsId }) => {
-	const getTeamsStub = sinon.stub(teamsRepo, 'getTeamsIdsForUser');
-	getTeamsStub.callsFake(() => []);
-	getTeamsStub.withArgs(USER_ID).returns(createTestGetTeamsForUserResult(teamsId));
-
-	const removeTeamsStub = sinon.stub(teamsRepo, 'removeUserFromTeams');
-	removeTeamsStub.withArgs(USER_ID).returns({ success: true, modifiedDocuments: 1 });
-
-	return { getTeamsStub, removeTeamsStub };
-};
-
-describe('delete class and teams user data usecase', () => {
+describe('delete class user data usecase', () => {
 	let app;
 	let server;
 	let nestServices;
@@ -134,37 +109,6 @@ describe('delete class and teams user data usecase', () => {
 		it('should throw an error if called with an invalid ObjectId', async () => {
 			const deleteUserDataFromClasses = deleteUserData.deleteUserData[0];
 			await expect(deleteUserDataFromClasses('NOT_AN_ID')).to.be.rejectedWith(AssertionError);
-		});
-	});
-
-	describe('delete teams user data usecase', () => {
-		it('should return a function', async () => {
-			const deleteUserDataFromClasses = deleteUserTeamsData.deleteUserTeamsData;
-			expect(deleteUserDataFromClasses).to.be.an('Array');
-			expect(deleteUserDataFromClasses.length).to.be.equal(1);
-			expect(deleteUserDataFromClasses[0]).to.be.a('function');
-		});
-
-		it('should return a valid result (trashbin) object', async () => {
-			const { getTeamsStub, removeTeamsStub } = initTeamsStubs({ teamsId: TEAMS_ID });
-
-			const deleteUserDataFromTeams = deleteUserTeamsData.deleteUserTeamsData[0];
-			const result = await deleteUserDataFromTeams(USER_ID);
-			getTeamsStub.restore();
-			removeTeamsStub.restore();
-
-			expect(result.complete).to.be.true;
-			expect(result.trashBinData.data).to.be.an('object');
-			expect(result.trashBinData.scope).to.be.equal('teams');
-			const { data } = result.trashBinData;
-			expect(data.teamIds).to.be.an('object');
-		});
-
-
-
-		it('should throw an error if called with an invalid ObjectId', async () => {
-			const deleteUserDataFromTeams = deleteUserTeamsData.deleteUserTeamsData[0];
-			await expect(deleteUserDataFromTeams('NOT_AN_ID')).to.be.rejectedWith(AssertionError);
 		});
 	});
 });
