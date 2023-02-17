ab57d7216efe1c38b3ab312f3e054a0870195653
Patrick Sachmann <patrick.sachmann@capgemini.com>
2023-02-17 14:29:15 +0100

    fixing system.service.spec

--- apps/server/src/modules/system/service/system.service.spec.ts
+++ apps/server/src/modules/system/service/system.service.spec.ts
@@ -3,7 +3,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { EntityNotFoundError } from '@shared/common';
 import { OauthConfig, System, SystemTypeEnum } from '@shared/domain';
 import { SystemProvisioningStrategy } from '@shared/domain/interface/system-provisioning.strategy';
-import { KeycloakSystemService } from '@shared/infra/identity-management/keycloak/service/keycloak-system.service';
+import { IdentityManagementOauthService } from '@shared/infra/identity-management';
 import { SystemRepo } from '@shared/repo';
 import { systemFactory } from '@shared/testing';
 import { SystemMapper } from '../mapper/system.mapper';
@@ -12,8 +12,8 @@ import { SystemService } from './system.service';
 describe('SystemService', () => {
 	let module: TestingModule;
 	let systemService: SystemService;
-	let systemRepo: DeepMocked<SystemRepo>;
-	let keycloakSystemService: DeepMocked<KeycloakSystemService>;
+	let systemRepoMock: DeepMocked<SystemRepo>;
+	let kcIdmOauthServiceMock: DeepMocked<IdentityManagementOauthService>;
 
 	beforeAll(async () => {
 		module = await Test.createTestingModule({
@@ -24,14 +24,14 @@ describe('SystemService', () => {
 					useValue: createMock<SystemRepo>(),
 				},
 				{
-					provide: KeycloakSystemService,
-					useValue: createMock<KeycloakSystemService>(),
+					provide: IdentityManagementOauthService,
+					useValue: createMock<IdentityManagementOauthService>(),
 				},
 			],
 		}).compile();
-		systemRepo = module.get(SystemRepo);
+		systemRepoMock = module.get(SystemRepo);
 		systemService = module.get(SystemService);
-		keycloakSystemService = module.get(KeycloakSystemService);
+		kcIdmOauthServiceMock = module.get(IdentityManagementOauthService);
 	});
 
 	afterAll(async () => {
@@ -52,9 +52,9 @@ describe('SystemService', () => {
 				provisioningStrategy: SystemProvisioningStrategy.UNDEFINED,
 			});
 			const setup = (system: System) => {
-				systemRepo.findById.mockResolvedValue(system);
-				keycloakSystemService.isAvailable.mockResolvedValue(true);
-				keycloakSystemService.getConfig.mockResolvedValue(keycloakSystem.oauthConfig as OauthConfig);
+				systemRepoMock.findById.mockResolvedValue(system);
+				kcIdmOauthServiceMock.isOauthConfigAvailable.mockResolvedValue(true);
+				kcIdmOauthServiceMock.getOauthConfig.mockResolvedValue(keycloakSystem.oauthConfig as OauthConfig);
 			};
 
 			it('should return found system', async () => {
@@ -79,8 +79,8 @@ describe('SystemService', () => {
 			const standaloneSystem = systemFactory.buildWithId();
 			const oidcSystem = systemFactory.withOidcConfig().buildWithId();
 			const setup = (system: System) => {
-				systemRepo.findById.mockResolvedValue(system);
-				keycloakSystemService.isAvailable.mockResolvedValue(false);
+				systemRepoMock.findById.mockResolvedValue(system);
+				kcIdmOauthServiceMock.isOauthConfigAvailable.mockResolvedValue(false);
 			};
 
 			it('should return found system', async () => {
@@ -107,15 +107,15 @@ describe('SystemService', () => {
 				provisioningStrategy: SystemProvisioningStrategy.UNDEFINED,
 			});
 			const setup = () => {
-				systemRepo.findAll.mockResolvedValue([ldapSystem, oauthSystem, oidcSystem]);
-				systemRepo.findByFilter.mockImplementation((type: SystemTypeEnum) => {
+				systemRepoMock.findAll.mockResolvedValue([ldapSystem, oauthSystem, oidcSystem]);
+				systemRepoMock.findByFilter.mockImplementation((type: SystemTypeEnum) => {
 					if (type === SystemTypeEnum.LDAP) return Promise.resolve([ldapSystem]);
 					if (type === SystemTypeEnum.OAUTH) return Promise.resolve([oauthSystem]);
 					if (type === SystemTypeEnum.OIDC) return Promise.resolve([oidcSystem]);
 					return Promise.resolve([]);
 				});
-				keycloakSystemService.isAvailable.mockResolvedValue(true);
-				keycloakSystemService.getConfig.mockResolvedValue(keycloakSystem.oauthConfig as OauthConfig);
+				kcIdmOauthServiceMock.isOauthConfigAvailable.mockResolvedValue(true);
+				kcIdmOauthServiceMock.getOauthConfig.mockResolvedValue(keycloakSystem.oauthConfig as OauthConfig);
 			};
 
 			it('should return all systems', async () => {
@@ -151,12 +151,12 @@ describe('SystemService', () => {
 			const oauthSystem = systemFactory.withOauthConfig().buildWithId();
 			const oidcSystem = systemFactory.withOidcConfig().buildWithId();
 			const setup = () => {
-				systemRepo.findByFilter.mockImplementation((type: SystemTypeEnum) => {
+				systemRepoMock.findByFilter.mockImplementation((type: SystemTypeEnum) => {
 					if (type === SystemTypeEnum.OAUTH) return Promise.resolve([oauthSystem]);
 					if (type === SystemTypeEnum.OIDC) return Promise.resolve([oidcSystem]);
 					return Promise.resolve([]);
 				});
-				keycloakSystemService.isAvailable.mockResolvedValue(false);
+				kcIdmOauthServiceMock.isOauthConfigAvailable.mockResolvedValue(false);
 			};
 			it('should filter out oidc systems', async () => {
 				setup();
@@ -170,7 +170,7 @@ describe('SystemService', () => {
 		describe('when creating a new system', () => {
 			const newSystem = systemFactory.build();
 			const setup = () => {
-				systemRepo.save.mockResolvedValue();
+				systemRepoMock.save.mockResolvedValue();
 			};
 
 			it('should save new system', async () => {
@@ -183,13 +183,13 @@ describe('SystemService', () => {
 		describe('when updating an existing system', () => {
 			const existingSystem = systemFactory.buildWithId();
 			const setup = () => {
-				systemRepo.findById.mockResolvedValue(existingSystem);
+				systemRepoMock.findById.mockResolvedValue(existingSystem);
 			};
 
 			it('should update existing system', async () => {
 				setup();
 				const result = await systemService.save(existingSystem);
-				expect(systemRepo.findById).toHaveBeenCalledTimes(1);
+				expect(systemRepoMock.findById).toHaveBeenCalledTimes(1);
 				expect(result).toStrictEqual(SystemMapper.mapFromEntityToDto(existingSystem));
 			});
 		});
