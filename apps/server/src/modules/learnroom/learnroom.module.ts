611a120bcb021d416bd8fd8f6bdc2954092030c7
Max Bischof <maximilian.bischof@dataport.de>
2023-02-20 13:24:13 +0100

    Remove course file copy and file legacy service

--- apps/server/src/modules/learnroom/learnroom.module.ts
+++ apps/server/src/modules/learnroom/learnroom.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { FileLegacyService } from '@shared/domain/service/file-legacy.service';
+import { AuthorisationUtils } from '@shared/domain/rules/authorisation.utils';
 import { FeathersServiceProvider } from '@shared/infra/feathers';
 import {
 	BoardRepo,
@@ -16,7 +16,6 @@ import { CopyHelperModule } from '@src/modules/copy-helper';
 import { FilesStorageClientModule } from '@src/modules/files-storage-client';
 import { LessonModule } from '@src/modules/lesson';
 import { TaskModule } from '@src/modules/task';
-import { AuthorisationUtils } from '@shared/domain/rules/authorisation.utils';
 import { CourseController } from './controller/course.controller';
 import { DashboardController } from './controller/dashboard.controller';
 import { RoomsController } from './controller/rooms.controller';
@@ -60,7 +59,6 @@ import { RoomsUc } from './uc/rooms.uc';
 		CourseCopyService,
 		CourseCopyUC,
 		RoomsService,
-		FileLegacyService,
 		FeathersServiceProvider,
 		Logger,
 		CourseService,
