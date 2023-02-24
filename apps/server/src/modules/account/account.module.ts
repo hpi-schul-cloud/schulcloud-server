7132f9e2e846686fc35ab3ee29a67f02a6e8dc29
Philipp Kleybolte <philipp.kleybolte@capgemini.com>
2023-02-24 10:16:09 +0100

    fix some cyclic dependencies

--- apps/server/src/modules/account/account.module.ts
+++ apps/server/src/modules/account/account.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { PermissionService } from '@shared/domain';
 import { SystemRepo, UserRepo } from '@shared/repo';
 import { IdentityManagementModule } from '@shared/infra/identity-management';
+import { LoggerModule } from '@src/core/logger/logger.module';
 import { AccountRepo } from './repo/account.repo';
 import { AccountService } from './services/account.service';
 import { AccountValidationService } from './services/account.validation.service';
-import { LoggerModule } from '@src/core/logger/logger.module';
 import { AccountServiceDb } from './services/account-db.service';
 import { AccountServiceIdm } from './services/account-idm.service';
 
