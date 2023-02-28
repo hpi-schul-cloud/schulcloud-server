dc8a1ae616f8d128f2359c43b7d06567f4bc0e90
Thomas Feldtkeller <thomas.feldtkeller@dataport.de>
2023-02-28 13:46:04 +0100

    remove unneeded code

--- apps/server/src/shared/testing/factory/column.factory.ts
+++
@@ -1,9 +0,0 @@
-import { Column, ColumnProps } from '@shared/domain';
-import { BaseFactory } from './base.factory';
-
-export const columnFactory = BaseFactory.define<Column, ColumnProps>(Column, ({ sequence }) => {
-	return {
-		title: `column #${sequence}`,
-		cardSkeletons: [],
-	};
-});
