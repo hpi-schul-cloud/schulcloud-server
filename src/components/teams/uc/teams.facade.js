7e9397ee6d170f67b8e831e3c1b8c0d9a19cd415
virgilchiriac <17074330+virgilchiriac@users.noreply.github.com>
2023-02-28 19:25:58 +0100

    Revert "BC-2848 Add deleting school-switched User reference form teams. (#3863)" (#3930)
    This reverts commit 1ccf9ca402d1251433afbd017fc65665b49154a5.

--- src/components/teams/uc/teams.facade.js
+++
@@ -1,9 +0,0 @@
-const deleteUserTeamsDataUc = require('./deleteUserTeamsData.uc');
-
-const facade = {
-	deleteUserData: deleteUserTeamsDataUc.deleteUserTeamsData,
-};
-
-module.exports = function setupUsersFacade(app) {
-	app.registerFacade('/teams/v2', facade);
-};
