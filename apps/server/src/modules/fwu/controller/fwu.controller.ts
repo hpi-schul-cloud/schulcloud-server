d6a5dafdfd390968ff007bc050b5740f6856d16e
stekrause <stephan.krause.ext@capgemini.com>
2023-03-01 09:59:20 +0100

    Fix eslint errors for fwu controller

--- apps/server/src/modules/fwu/controller/fwu.controller.ts
+++ apps/server/src/modules/fwu/controller/fwu.controller.ts
@@ -1,29 +1,29 @@
-import { Body, Controller, Get, Param, Req, Res } from '@nestjs/common';
+import { Controller, Get, Param, Req, Res } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { Authenticate } from '@src/modules/authentication/decorator/auth.decorator';
 import { Request, Response } from 'express';
-import { FwuUc } from '../uc/fwu.uc'
+import { FwuUc } from '../uc/fwu.uc';
 
 @ApiTags('FWU')
 @Authenticate('jwt')
 @Controller('fwu')
 export class FwuController {
-    constructor(private readonly fwuUc: FwuUc) {}
+	constructor(private readonly fwuUc: FwuUc) {}
 
-    @Get('*')
-    async get(@Req() req: Request, @Res() res: Response) {
-        // TODO: make sure its a user from Brandenburg
-        const path = req.params[0];
-        console.log('fwu: ', path);
-        try {
-            var file = await this.fwuUc.get(path);
-        } catch (errorStatus) {
-            res.sendStatus(errorStatus);
-            return;
-        }
-        const i = path.lastIndexOf('.');
-        const contentType = (i !== -1) ? path.slice(i) : 'application/octet-stream';
-        res.type(contentType);
-        res.send(file);
-    }
+	@Get('*')
+	async get(@Req() req: Request, @Res() res: Response) {
+		// TODO: make sure its a user from Brandenburg
+		const path = req.params[0];
+		console.log('fwu: ', path);
+		try {
+			var file = await this.fwuUc.get(path);
+		} catch (errorStatus) {
+			res.sendStatus(errorStatus);
+			return;
+		}
+		const i = path.lastIndexOf('.');
+		const contentType = i !== -1 ? path.slice(i) : 'application/octet-stream';
+		res.type(contentType);
+		res.send(file);
+	}
 }
