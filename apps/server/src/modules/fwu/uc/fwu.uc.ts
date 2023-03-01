2ce197e0bed1927bbf229e9dfd20523d49bb038f
stekrause <stephan.krause.ext@capgemini.com>
2023-03-01 10:20:01 +0100

    fix eslint errors in fwu uc

--- apps/server/src/modules/fwu/uc/fwu.uc.ts
+++ apps/server/src/modules/fwu/uc/fwu.uc.ts
@@ -6,38 +6,38 @@ import { Configuration } from '@hpi-schul-cloud/commons';
 
 @Injectable()
 export class FwuUc {
-    constructor(private authorizationService: FeathersAuthorizationService, private logger: Logger) {
-        this.logger.setContext(FwuUc.name);
-    }
+	constructor(private authorizationService: FeathersAuthorizationService, private logger: Logger) {
+		this.logger.setContext(FwuUc.name);
+	}
 
-    async get(path: String): Promise<Uint8Array> {
-        const client = new S3Client({
-            endpoint: Configuration.get('FWU_CONTENT__S3_ENDPOINT'),
-            credentials: {
-                accessKeyId: Configuration.get('FWU_CONTENT__S3_ACCESS_KEY'),
-                secretAccessKey: Configuration.get('FWU_CONTENT__S3_SECRET_KEY')
-            },
-            region: Configuration.get('FWU_CONTENT__S3_REGION'),
-            tls: true,
-            forcePathStyle: true
-        });
-        const request = new GetObjectCommand({
-            Bucket: Configuration.get('FWU_CONTENT__S3_BUCKET'),
-            Key: path.toString()
-        });
-        const response = await client.send(request);
-        if (response.$metadata.httpStatusCode !== 200) {
-            console.log(`S3 request failed for: ${path}`);
-            return Promise.reject(response.$metadata.httpStatusCode);
-        }
-        var readableStream = response.Body as NodeJS.ReadableStream;
-        return new Promise<Uint8Array>(async (resolve, reject) => {
-            const chunks = [new Uint8Array];
-            readableStream.on('data', (chunk: Uint8Array) => chunks.push(chunk));
-            readableStream.on('error', reject);
-            readableStream.on('end', () => resolve(Buffer.concat(chunks)));
-        })
-        
-        // TODO: handle other types of responses such as Blob
-    }
+	async get(path: string): Promise<Uint8Array> {
+		const client = new S3Client({
+			endpoint: Configuration.get('FWU_CONTENT__S3_ENDPOINT'),
+			credentials: {
+				accessKeyId: Configuration.get('FWU_CONTENT__S3_ACCESS_KEY'),
+				secretAccessKey: Configuration.get('FWU_CONTENT__S3_SECRET_KEY'),
+			},
+			region: Configuration.get('FWU_CONTENT__S3_REGION'),
+			tls: true,
+			forcePathStyle: true,
+		});
+		const request = new GetObjectCommand({
+			Bucket: Configuration.get('FWU_CONTENT__S3_BUCKET'),
+			Key: path.toString(),
+		});
+		const response = await client.send(request);
+		if (response.$metadata.httpStatusCode !== 200) {
+			console.log(`S3 request failed for: ${path}`);
+			return Promise.reject(response.$metadata.httpStatusCode);
+		}
+		const readableStream = response.Body as NodeJS.ReadableStream;
+		return new Promise<Uint8Array>(async (resolve, reject) => {
+			const chunks = [new Uint8Array()];
+			readableStream.on('data', (chunk: Uint8Array) => chunks.push(chunk));
+			readableStream.on('error', reject);
+			readableStream.on('end', () => resolve(Buffer.concat(chunks)));
+		});
+
+		// TODO: handle other types of responses such as Blob
+	}
 }
