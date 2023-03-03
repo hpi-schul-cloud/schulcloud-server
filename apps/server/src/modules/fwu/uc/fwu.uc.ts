42ad5b493d1397f21288f13db9a2d96633e5866b
stekrause <stephan.krause.ext@capgemini.com>
2023-03-03 11:49:54 +0100

    add string type to imported s3 configuration data

--- apps/server/src/modules/fwu/uc/fwu.uc.ts
+++ apps/server/src/modules/fwu/uc/fwu.uc.ts
@@ -12,26 +12,27 @@ export class FwuUc {
 
 	async get(path: string): Promise<Uint8Array> {
 		const client = new S3Client({
-			endpoint: Configuration.get('FWU_CONTENT__S3_ENDPOINT'),
+			endpoint: Configuration.get('FWU_CONTENT__S3_ENDPOINT') as string,
 			credentials: {
-				accessKeyId: Configuration.get('FWU_CONTENT__S3_ACCESS_KEY'),
-				secretAccessKey: Configuration.get('FWU_CONTENT__S3_SECRET_KEY'),
+				accessKeyId: Configuration.get('FWU_CONTENT__S3_ACCESS_KEY') as string,
+				secretAccessKey: Configuration.get('FWU_CONTENT__S3_SECRET_KEY') as string,
 			},
-			region: Configuration.get('FWU_CONTENT__S3_REGION'),
+			region: Configuration.get('FWU_CONTENT__S3_REGION') as string,
 			tls: true,
 			forcePathStyle: true,
 		});
 		const request = new GetObjectCommand({
-			Bucket: Configuration.get('FWU_CONTENT__S3_BUCKET'),
+			Bucket: Configuration.get('FWU_CONTENT__S3_BUCKET') as string,
 			Key: path.toString(),
 		});
 		const response = await client.send(request);
 		if (response.$metadata.httpStatusCode !== 200) {
+			// eslint-disable-next-line no-console
 			console.log(`S3 request failed for: ${path}`);
 			return Promise.reject(response.$metadata.httpStatusCode);
 		}
 		const readableStream = response.Body as NodeJS.ReadableStream;
-		return new Promise<Uint8Array>(async (resolve, reject) => {
+		return new Promise<Uint8Array>((resolve, reject) => {
 			const chunks = [new Uint8Array()];
 			readableStream.on('data', (chunk: Uint8Array) => chunks.push(chunk));
 			readableStream.on('error', reject);
