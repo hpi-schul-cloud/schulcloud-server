dc93e2331bdb261babfe8e2902b9ab3995d27d4b
hoeppner-dataport <marcus.hoeppner@dataport.de>
2023-03-06 18:00:37 +0100

    enable card titles

--- apps/server/src/shared/domain/entity/boardnode/payload/card-payload.ts
+++ apps/server/src/shared/domain/entity/boardnode/payload/card-payload.ts
@@ -6,13 +6,18 @@ export class CardPayload extends BoardNodePayload implements CardPayloadProps {
 	constructor(props: CardPayloadProps) {
 		super({ name: props.name });
 		this.type = BoardNodeType.CARD;
+		this.title = props.title;
 		this.height = props.height;
 	}
+
+	@Property()
+	title?: string;
 
 	@Property()
 	height: number;
 }
 
 export interface CardPayloadProps extends BoardNodePayloadProps {
+	title?: string;
 	height: number;
 }
