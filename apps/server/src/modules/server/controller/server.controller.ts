78a202d89bf7a50d5eee4813249196af0454f2c4
Marvin Öhlerking <marvin.oehlerking@capgemini.com>
2023-03-07 14:15:23 +0100

    wip

--- apps/server/src/modules/server/controller/server.controller.ts
+++ apps/server/src/modules/server/controller/server.controller.ts
@@ -1,7 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
+import { Authenticate, JWT } from '@src/modules/authentication/decorator/auth.decorator';
+import { AuthenticationService } from '../../authentication';
 
 @Controller()
 export class ServerController {
+	constructor(private readonly authserv: AuthenticationService) {}
+
+	@Get('test')
+	@Authenticate('jwt')
+	async getTest(@JWT() jwt: string): Promise<string> {
+		await this.authserv.removeJwtFromWhitelist(jwt);
+		return 'TEST';
+	}
+
 	/** default route to test public access */
 	@Get()
 	getHello(): string {
