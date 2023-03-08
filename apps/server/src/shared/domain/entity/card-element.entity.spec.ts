92446dcffef18856b1d58602d9a0397408f04cca
wolfganggreschus <wolfgang@greschus.de>
2023-03-08 11:27:49 +0100

    adjusted card-element..entity.spec.ts so that the title element is no longer included

--- apps/server/src/shared/domain/entity/card-element.entity.spec.ts
+++ apps/server/src/shared/domain/entity/card-element.entity.spec.ts
@@ -1,27 +1,7 @@
 import { MikroORM } from '@mikro-orm/core';
 import { setupEntities } from '@shared/testing';
-import { CardElementType, RichTextCardElement, TitleCardElement } from '.';
+import { CardElementType, RichTextCardElement } from '.';
 import { InputFormat, RichText } from '../types';
-
-describe('TitleCardElementEntity', () => {
-	let orm: MikroORM;
-
-	beforeAll(async () => {
-		orm = await setupEntities();
-	});
-
-	afterAll(async () => {
-		await orm.close();
-	});
-
-	describe('constructor', () => {
-		it('should have correct type', () => {
-			const titleCardElement = new TitleCardElement('title example');
-
-			expect(titleCardElement.cardElementType).toEqual(CardElementType.Title);
-		});
-	});
-});
 
 describe('RichTextCardElementEntity', () => {
 	let orm: MikroORM;
