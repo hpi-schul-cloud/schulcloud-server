9da04c88b0855bca665be02efe53a08394fae91b
hoeppner-dataport <marcus.hoeppner@dataport.de>
2023-03-08 14:31:11 +0100

    completed tests for board-do-builder

--- apps/server/src/shared/domain/entity/boardnode/board-do.builder.spec.ts
+++ apps/server/src/shared/domain/entity/boardnode/board-do.builder.spec.ts
@@ -1,9 +1,99 @@
-import { cardNodeFactory, columnBoardNodeFactory, setupEntities, textElementNodeFactory } from '@shared/testing';
+import {
+	cardNodeFactory,
+	columnBoardNodeFactory,
+	columnNodeFactory,
+	setupEntities,
+	textElementNodeFactory,
+} from '@shared/testing';
 import { BoardDoBuilder } from './board-do.builder';
 
 describe(BoardDoBuilder.name, () => {
 	beforeAll(async () => {
 		await setupEntities();
+	});
+
+	describe('when building a column board', () => {
+		it('should work without descendants', () => {
+			const columnBoardNode = columnBoardNodeFactory.build();
+
+			const domainObject = new BoardDoBuilder().buildColumnBoard(columnBoardNode);
+
+			expect(domainObject.constructor.name).toBe('ColumnBoard');
+		});
+
+		it('should throw error with wrong type of children', () => {
+			const columnBoardNode1 = columnBoardNodeFactory.buildWithId();
+			const columnBoardNode2 = columnBoardNodeFactory.buildWithId({ parent: columnBoardNode1 });
+
+			expect(() => {
+				new BoardDoBuilder([columnBoardNode2]).buildColumnBoard(columnBoardNode1);
+			}).toThrowError();
+		});
+
+		it('should assign the children', () => {
+			const columnBoardNode = columnBoardNodeFactory.buildWithId();
+			const columnNode1 = columnNodeFactory.buildWithId({ parent: columnBoardNode });
+			const columnNode2 = columnNodeFactory.buildWithId({ parent: columnBoardNode });
+
+			const domainObject = new BoardDoBuilder([columnNode1, columnNode2]).buildColumnBoard(columnBoardNode);
+
+			expect(domainObject.columns.map((el) => el.id).sort()).toEqual([columnNode1.id, columnNode2.id]);
+		});
+
+		it('should sort the children by their node position', () => {
+			const columnBoardNode = columnBoardNodeFactory.buildWithId();
+			const columnNode1 = columnNodeFactory.buildWithId({ parent: columnBoardNode, position: 3 });
+			const columnNode2 = columnNodeFactory.buildWithId({ parent: columnBoardNode, position: 2 });
+			const columnNode3 = columnNodeFactory.buildWithId({ parent: columnBoardNode, position: 1 });
+
+			const domainObject = new BoardDoBuilder([columnNode1, columnNode2, columnNode3]).buildColumnBoard(
+				columnBoardNode
+			);
+
+			const elementIds = domainObject.columns.map((el) => el.id);
+			expect(elementIds).toEqual([columnNode3.id, columnNode2.id, columnNode1.id]);
+		});
+	});
+
+	describe('when building a column', () => {
+		it('should work without descendants', () => {
+			const columnNode = columnNodeFactory.build();
+
+			const domainObject = new BoardDoBuilder().buildColumn(columnNode);
+
+			expect(domainObject.constructor.name).toBe('Column');
+		});
+
+		it('should throw error with wrong type of children', () => {
+			const columnNode1 = columnNodeFactory.buildWithId();
+			const columnNode2 = columnNodeFactory.buildWithId({ parent: columnNode1 });
+
+			expect(() => {
+				new BoardDoBuilder([columnNode2]).buildColumn(columnNode1);
+			}).toThrowError();
+		});
+
+		it('should assign the children', () => {
+			const columnNode = columnNodeFactory.buildWithId();
+			const cardNode1 = cardNodeFactory.buildWithId({ parent: columnNode });
+			const cardNode2 = cardNodeFactory.buildWithId({ parent: columnNode });
+
+			const domainObject = new BoardDoBuilder([cardNode1, cardNode2]).buildColumn(columnNode);
+
+			expect(domainObject.cards.map((el) => el.id).sort()).toEqual([cardNode1.id, cardNode2.id]);
+		});
+
+		it('should sort the children by their node position', () => {
+			const columnNode = columnNodeFactory.buildWithId();
+			const cardNode1 = cardNodeFactory.buildWithId({ parent: columnNode, position: 3 });
+			const cardNode2 = cardNodeFactory.buildWithId({ parent: columnNode, position: 2 });
+			const cardNode3 = cardNodeFactory.buildWithId({ parent: columnNode, position: 1 });
+
+			const domainObject = new BoardDoBuilder([cardNode1, cardNode2, cardNode3]).buildColumn(columnNode);
+
+			const cardIds = domainObject.cards.map((el) => el.id);
+			expect(cardIds).toEqual([cardNode3.id, cardNode2.id, cardNode1.id]);
+		});
 	});
 
 	describe('when building a card', () => {
@@ -17,10 +107,10 @@ describe(BoardDoBuilder.name, () => {
 
 		it('should throw error with wrong type of children', () => {
 			const cardNode = cardNodeFactory.buildWithId();
-			const boardNode = columnBoardNodeFactory.buildWithId({ parent: cardNode });
+			const columnNode = columnNodeFactory.buildWithId({ parent: cardNode });
 
 			expect(() => {
-				new BoardDoBuilder([boardNode]).buildCard(cardNode);
+				new BoardDoBuilder([columnNode]).buildCard(cardNode);
 			}).toThrowError();
 		});
 
@@ -31,7 +121,7 @@ describe(BoardDoBuilder.name, () => {
 
 			const domainObject = new BoardDoBuilder([elementNode1, elementNode2]).buildCard(cardNode);
 
-			expect(domainObject.elements.map((el) => el.id).sort()).toEqual([elementNode1.id, elementNode2.id].sort());
+			expect(domainObject.elements.map((el) => el.id).sort()).toEqual([elementNode1.id, elementNode2.id]);
 		});
 
 		it('should sort the children by their node position', () => {
@@ -44,6 +134,25 @@ describe(BoardDoBuilder.name, () => {
 
 			const elementIds = domainObject.elements.map((el) => el.id);
 			expect(elementIds).toEqual([elementNode3.id, elementNode1.id, elementNode2.id]);
+		});
+	});
+
+	describe('when building an element', () => {
+		it('should work without descendants', () => {
+			const textElementNode = textElementNodeFactory.build();
+
+			const domainObject = new BoardDoBuilder().buildTextElement(textElementNode);
+
+			expect(domainObject.constructor.name).toBe('TextElement');
+		});
+
+		it('should throw error if children are provided', () => {
+			const textElementNode = textElementNodeFactory.buildWithId();
+			const columnNode = columnNodeFactory.buildWithId({ parent: textElementNode });
+
+			expect(() => {
+				new BoardDoBuilder([columnNode]).buildTextElement(textElementNode);
+			}).toThrowError();
 		});
 	});
 });
