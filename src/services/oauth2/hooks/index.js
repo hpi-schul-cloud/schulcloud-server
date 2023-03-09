1bb582b7e3ff4c3047ad78e965f1c54345bc97d8
Viktoriia <1>
2023-03-09 17:44:11 +0100

    Changes

--- src/services/oauth2/hooks/index.js
+++ src/services/oauth2/hooks/index.js
@@ -39,6 +39,7 @@ const setSubject = (hook) => {
 					console.log(hook.data, 111111111111);
 					console.log(hook.params.account, 2222222222222222222)
 					console.log(hook.params,333333333333)
+					console.log(hook.params.session, 4444444444444444444444444)
 					hook.data.force_subject_identifier = pseudonym;
 				})
 		);
