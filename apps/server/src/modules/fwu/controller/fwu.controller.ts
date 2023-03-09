b31c505a0fd1134f5cee09d6f42dd3cc09345528
stekrause <stephan.krause.ext@capgemini.com>
2023-03-09 08:36:05 +0100

    add FEATURE_FWU_CONTENT_ENABLED

--- apps/server/src/modules/fwu/controller/fwu.controller.ts
+++ apps/server/src/modules/fwu/controller/fwu.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Param, Req, Res } from '@nestjs/common';
+import { Controller, Get, Req, Res, InternalServerErrorException } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { Authenticate } from '@src/modules/authentication/decorator/auth.decorator';
 import { Request, Response } from 'express';
+import { Configuration } from '@hpi-schul-cloud/commons';
 import { FwuUc } from '../uc/fwu.uc';
 
 @ApiTags('FWU')
@@ -12,10 +13,12 @@ export class FwuController {
 
 	@Get('*')
 	async get(@Req() req: Request, @Res() res: Response) {
-		// TODO: make sure its a user from Brandenburg
+		const fwuContentEnabled = Configuration.get('FEATURE_FWU_CONTENT_ENABLED') as boolean;
+		if (!fwuContentEnabled) {
+			throw new InternalServerErrorException('Feature FWU content is not enabled.');
+		}
 		const path = req.params[0];
 		try {
-			// console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>fwu: ', path.toString());
 			var file = await this.fwuUc.get(path);
 		} catch (error) {
 			throw new Error('HttpStatusCode: ' + error.$metadata.httpStatusCode + ' at ' + path + ' ' + error);
