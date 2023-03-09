eb94d5c9563ae1d5e2fa142c2db8e747ecd8b527
HeftyTea <timm.hoermann@capgemini.com>
2023-03-09 10:08:39 +0100

    EW-463 Include types to esbuild bundle step

--- apps/esbuild/esmodules-bundler.js
+++ apps/esbuild/esmodules-bundler.js
@@ -1,11 +1,12 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-const esbuild = require('esbuild');
+/* eslint-disable import/no-extraneous-dependencies */
+const { dtsPlugin } = require('esbuild-plugin-d.ts');
+const { build } = require('esbuild');
 
 // add files to be transformed from CommonJs to EsModules in the following list
 const options = [
 	{
 		name: 'keycloak-admin-client-lib',
-		entryPoints: ['apps/server/src/shared/infra/identity-management/keycloak-admin-client-cjs-index.ts'],
+		entryPoints: ['apps/esbuild/content/keycloak-admin-client-cjs-index.ts'],
 		outdir: 'node_modules/@keycloak/keycloak-admin-client-cjs',
 		platform: 'node',
 		format: 'cjs',
@@ -18,7 +19,7 @@ const options = [
 for (const option of options) {
 	const { entryPoints, outdir, platform, format, bundle, minify, loader } = option;
 	try {
-		esbuild.build({
+		build({
 			entryPoints,
 			outdir,
 			platform,
@@ -26,6 +27,7 @@ for (const option of options) {
 			bundle,
 			minify,
 			loader,
+			plugins: [dtsPlugin()],
 		});
 	} catch (e) {
 		process.exit(1);
