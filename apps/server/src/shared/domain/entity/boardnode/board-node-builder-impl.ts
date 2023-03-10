8f31b3464a07d624db024f0dacbe7009276b8e68
hoeppner-dataport <marcus.hoeppner@dataport.de>
2023-03-10 17:36:16 +0100

    add ensureBoardType and registerNode to builder impl

--- apps/server/src/shared/domain/entity/boardnode/board-node-builder-impl.ts
+++ apps/server/src/shared/domain/entity/boardnode/board-node-builder-impl.ts
@@ -6,6 +6,7 @@ import { CardNode } from './card-node.entity';
 import { ColumnBoardNode } from './column-board-node.entity';
 import { ColumnNode } from './column-node.entity';
 import { TextElementNode } from './text-element-node.entity';
+import { BoardNodeType } from './types';
 
 export class BoardNodeBuilderImpl implements BoardNodeBuilder {
 	private parentsMap: Map<EntityId, BoardNode> = new Map();
@@ -26,30 +27,28 @@ export class BoardNodeBuilderImpl implements BoardNodeBuilder {
 			id: columnBoard.id,
 			title: columnBoard.title,
 		});
+		this.registerNode(columnBoardNode);
 
-		this.parentsMap.set(columnBoardNode.id, columnBoardNode);
-		this.resultNodes.push(columnBoardNode);
 		this.buildChildren(columnBoard.columns, columnBoardNode.id);
 	}
 
 	buildColumnNode(column: Column, parentId: EntityId): void {
 		const parent = this.parentsMap.get(parentId);
-
-		// this.ensureBoardNodeType(parent, BoardNodeType.COLUMN_BOARD);
+		this.ensureBoardNodeType(parent, BoardNodeType.COLUMN_BOARD);
 
 		const columnNode = new ColumnNode({
 			id: column.id,
 			title: column.title,
 			parent,
 		});
+		this.registerNode(columnNode);
 
-		this.parentsMap.set(columnNode.id, columnNode);
-		this.resultNodes.push(columnNode);
 		this.buildChildren(column.cards, columnNode.id);
 	}
 
 	buildCardNode(card: Card, parentId: EntityId): void {
 		const parent = this.parentsMap.get(parentId);
+		this.ensureBoardNodeType(parent, BoardNodeType.COLUMN);
 
 		const cardNode = new CardNode({
 			id: card.id,
@@ -57,23 +56,32 @@ export class BoardNodeBuilderImpl implements BoardNodeBuilder {
 			title: card.title,
 			parent,
 		});
+		this.registerNode(cardNode);
 
-		this.parentsMap.set(cardNode.id, cardNode);
-		this.resultNodes.push(cardNode);
 		this.buildChildren(card.elements, cardNode.id);
 	}
 
 	buildTextElementNode(textElement: TextElement, parentId: EntityId): void {
 		const parent = this.parentsMap.get(parentId);
+		this.ensureBoardNodeType(parent, BoardNodeType.CARD);
 
 		const textElementNode = new TextElementNode({
 			id: textElement.id,
 			text: textElement.text,
 			parent,
 		});
+		this.registerNode(textElementNode);
+	}
 
-		this.parentsMap.set(textElementNode.id, textElementNode);
-		this.resultNodes.push(textElementNode);
+	registerNode(boardNode: BoardNode) {
+		this.parentsMap.set(boardNode.id, boardNode);
+		this.resultNodes.push(boardNode);
+	}
+
+	ensureBoardNodeType(boardNode: BoardNode | undefined, ...allowedBoardNodeTypes: BoardNodeType[]) {
+		if (!boardNode || !allowedBoardNodeTypes.includes(boardNode.type)) {
+			throw new Error(`board node type is not allowed: >${boardNode?.type ?? 'undefined'}<`);
+		}
 	}
 
 	buildChildren(children: AnyBoardDo[], parentId: EntityId): void {
