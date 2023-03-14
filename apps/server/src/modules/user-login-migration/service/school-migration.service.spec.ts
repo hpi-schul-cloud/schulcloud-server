4be91a57394577ef15541f21ec8d805d67c44a81
Igor Richter <igor.richter@capgemini.com>
2023-03-14 06:17:57 +0100

    coverage up

--- apps/server/src/modules/user-login-migration/service/school-migration.service.spec.ts
+++ apps/server/src/modules/user-login-migration/service/school-migration.service.spec.ts
@@ -196,6 +196,21 @@ describe('SchoolMigrationService', () => {
 				});
 			});
 
+			describe('when there are no systems in School', () => {
+				it('should add the system to migrated school', async () => {
+					const { schoolDO, targetSystemId } = setup();
+					schoolDO.systems = undefined;
+
+					await service.migrateSchool('newExternalId', schoolDO, targetSystemId);
+
+					expect(schoolService.save).toHaveBeenCalledWith(
+						expect.objectContaining<Partial<SchoolDO>>({
+							systems: [targetSystemId],
+						})
+					);
+				});
+			});
+
 			describe('when an error occurred', () => {
 				it('should save the old schoolDo (rollback the migration)', async () => {
 					const { schoolDO, targetSystemId } = setup();
