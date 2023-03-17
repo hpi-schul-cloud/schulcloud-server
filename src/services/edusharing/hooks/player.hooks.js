a63f3887f970f0e4e5731dab67b7565754cdb4a3
DDEITERS <dirk.deiters.ext@capgemini.com>
2023-03-17 11:15:26 +0100

    change error 'NotFound' to 'MethodNotAllowed'

--- src/services/edusharing/hooks/player.hooks.js
+++ src/services/edusharing/hooks/player.hooks.js
@@ -2,11 +2,11 @@ const { Configuration } = require('@hpi-schul-cloud/commons');
 const { authenticate } = require('@feathersjs/authentication');
 const { disallow } = require('feathers-hooks-common');
 const { hasPermission, hasSchoolPermission } = require('../../../hooks');
-const { NotFound } = require('@feathersjs/errors');
+const { MethodNotAllowed } = require('@feathersjs/errors');
 
 const isEdusharing = (context) => {
 	if (Configuration.get('FEATURE_LERNSTORE_ENABLED') !== true) {
-		throw new NotFound('This feature is disabled.');
+		throw new MethodNotAllowed('This feature is disabled on this instance!');
 	}
 	context.safeAttributes = ['url'];
 	return Promise.resolve(context);
