afb845b68a17981ea84b7ef7aa9707e44eeddf8c
Thomas Feldtkeller <thomas.feldtkeller@dataport.de>
2023-03-20 20:17:37 +0100

    BC-3344 - create board entities (#3980)
    Creating of board entities (card, element, column, columnBoard)
    
    * implement tree persistence using board nodes
    * mapping of board nodes to domain objects
    
    ---------
    
    Co-authored-by: Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
    Co-authored-by: hoeppner-dataport <marcus.hoeppner@dataport.de>
    Co-authored-by: Nicole Friedo <nicole.friedo@dataport.de>

--- apps/server/src/shared/domain/domainobject/board/column.do.ts
+++ apps/server/src/shared/domain/domainobject/board/column.do.ts
@@ -1,7 +1,8 @@
 import { EntityId } from '@shared/domain/types';
 import { Card } from './card.do';
+import type { BoardNodeBuildable, BoardNodeBuilder } from './types';
 
-export class Column implements ColumnProps {
+export class Column implements ColumnProps, BoardNodeBuildable {
 	id: EntityId;
 
 	title?: string;
@@ -19,9 +20,17 @@ export class Column implements ColumnProps {
 		this.createdAt = props.createdAt;
 		this.updatedAt = props.updatedAt;
 	}
+
+	addCard(card: Card, position?: number) {
+		this.cards.splice(position || this.cards.length, 0, card);
+	}
+
+	useBoardNodeBuilder(builder: BoardNodeBuilder, parentId?: EntityId, position?: number): void {
+		builder.buildColumnNode(this, parentId, position);
+	}
 }
 
-interface ColumnProps {
+export interface ColumnProps {
 	id: EntityId;
 
 	title?: string;
