a12c380880b6c97febf454b06049d269087edc73
hoeppner-dataport <marcus.hoeppner@dataport.de>
2023-03-27 17:09:47 +0200

    fix column.do.factory

--- apps/server/src/shared/testing/factory/domainobject/board/column.do.factory.ts
+++ apps/server/src/shared/testing/factory/domainobject/board/column.do.factory.ts
@@ -1,9 +1,9 @@
 /* istanbul ignore file */
-import { Column, ColumnProps } from '@shared/domain';
+import { BoardCompositeProps, Column } from '@shared/domain';
 import { ObjectId } from 'bson';
 import { BaseFactory } from '../../base.factory';
 
-export const columnFactory = BaseFactory.define<Column, ColumnProps>(Column, ({ sequence }) => {
+export const columnFactory = BaseFactory.define<Column, BoardCompositeProps>(Column, ({ sequence }) => {
 	return {
 		id: new ObjectId().toHexString(),
 		title: `column #${sequence}`,
