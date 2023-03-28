0ce1274579129692bc31c724c0a10c65307335ed
stekrause <stephan.krause.ext@capgemini.com>
2023-03-28 16:42:17 +0200

    add .min.css to class-validator

--- apps/server/src/modules/fwu-learning-contents/controller/dto/fwu-learning-contents.params.ts
+++ apps/server/src/modules/fwu-learning-contents/controller/dto/fwu-learning-contents.params.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsString, Matches } from 'class-validator';
 
 export class GetFwuLearningContentParams {
 	@ApiProperty()
-	@Matches('([A-Za-z]|[0-9])+(.html|.css|.mp4|.pdf|.doc|.png|.jpg|.gif|.min.js|.js|.ico|.txt)')
+	@Matches('([A-Za-z]|[0-9])+(.html|.css|.mp4|.pdf|.doc|.png|.jpg|.gif|.min.js|.js|.ico|.txt|.min.css)')
 	@IsString()
 	@IsNotEmpty()
 	fwuLearningContent!: string;
