3e51a0c12da4f32f8430d1e808c3ee5d4fa3549e
Nicole Friedo <nicole.friedo@dataport.de>
2023-03-29 14:54:23 +0200

    fix card.uc.spec.ts

--- apps/server/src/modules/board/uc/card.uc.spec.ts
+++ apps/server/src/modules/board/uc/card.uc.spec.ts
@@ -3,7 +3,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { setupEntities, userFactory } from '@shared/testing';
 import { cardFactory, textElementFactory } from '@shared/testing/factory/domainobject';
 import { Logger } from '@src/core/logger';
-import { BoardDoService, ContentElementService } from '../service';
+import { ContentElementService } from '../service';
 import { CardService } from '../service/card.service';
 import { CardUc } from './card.uc';
 
@@ -11,17 +11,12 @@ describe(CardUc.name, () => {
 	let module: TestingModule;
 	let uc: CardUc;
 	let cardService: DeepMocked<CardService>;
-	let boardDoService: DeepMocked<BoardDoService>;
 	let elementService: DeepMocked<ContentElementService>;
 
 	beforeAll(async () => {
 		module = await Test.createTestingModule({
 			providers: [
 				CardUc,
-				{
-					provide: BoardDoService,
-					useValue: createMock<BoardDoService>(),
-				},
 				{
 					provide: CardService,
 					useValue: createMock<CardService>(),
@@ -39,7 +34,6 @@ describe(CardUc.name, () => {
 
 		uc = module.get(CardUc);
 		cardService = module.get(CardService);
-		boardDoService = module.get(BoardDoService);
 		elementService = module.get(ContentElementService);
 		await setupEntities();
 	});
@@ -48,76 +42,79 @@ describe(CardUc.name, () => {
 		await module.close();
 	});
 
-	describe('finding many cards', () => {
-		const setup = () => {
-			const user = userFactory.buildWithId();
-			const cards = cardFactory.buildList(3);
-			const cardIds = cards.map((c) => c.id);
+	describe('findCards', () => {
+		describe('when finding many cards', () => {
+			const setup = () => {
+				const user = userFactory.buildWithId();
+				const cards = cardFactory.buildList(3);
+				const cardIds = cards.map((c) => c.id);
 
-			return { user, cards, cardIds };
-		};
+				return { user, cards, cardIds };
+			};
 
-		it('should call the service', async () => {
-			const { user, cardIds } = setup();
+			it('should call the service', async () => {
+				const { user, cardIds } = setup();
 
-			await uc.findCards(user.id, cardIds);
+				await uc.findCards(user.id, cardIds);
 
-			expect(cardService.findByIds).toHaveBeenCalledWith(cardIds);
-		});
+				expect(cardService.findByIds).toHaveBeenCalledWith(cardIds);
+			});
 
-		it('should return the card objects', async () => {
-			const { user, cards, cardIds } = setup();
-			cardService.findByIds.mockResolvedValueOnce(cards);
+			it('should return the card objects', async () => {
+				const { user, cards, cardIds } = setup();
+				cardService.findByIds.mockResolvedValueOnce(cards);
 
-			const result = await uc.findCards(user.id, cardIds);
+				const result = await uc.findCards(user.id, cardIds);
 
-			expect(result).toEqual(cards);
-		});
-	});
-
-	describe('creating a content element', () => {
-		const setup = () => {
-			const user = userFactory.buildWithId();
-			const card = cardFactory.build();
-			return { user, card };
-		};
-
-		it('should call the service to find the card', async () => {
-			const { user, card } = setup();
-
-			await uc.createElement(user.id, card.id);
-
-			expect(cardService.findById).toHaveBeenCalledWith(card.id);
-		});
-
-		it('should call the service to create the content element', async () => {
-			const { user, card } = setup();
-			cardService.findById.mockResolvedValueOnce(card);
-
-			await uc.createElement(user.id, card.id);
-
-			expect(elementService.create).toHaveBeenCalledWith(card.id);
+				expect(result).toEqual(cards);
+			});
 		});
 	});
 
-	describe('deleting a content element', () => {
-		const setup = () => {
-			const user = userFactory.buildWithId();
-			const contentElement = textElementFactory.buildWithId();
-			const card = cardFactory.build();
+	describe('createElement', () => {
+		describe('when creating a content element', () => {
+			const setup = () => {
+				const user = userFactory.buildWithId();
+				const card = cardFactory.build();
+				return { user, card };
+			};
 
-			return { user, card, contentElement };
-		};
+			it('should call the service to find the card', async () => {
+				const { user, card } = setup();
 
-		it('should delete element', async () => {
-			const { user, card, contentElement } = setup();
+				await uc.createElement(user.id, card.id);
 
-			cardService.findById.mockResolvedValue(card);
+				expect(cardService.findById).toHaveBeenCalledWith(card.id);
+			});
 
-			await uc.deleteElement(user.id, card.id, contentElement.id);
+			it('should call the service to create the content element', async () => {
+				const { user, card } = setup();
+				cardService.findById.mockResolvedValueOnce(card);
 
-			expect(cardService.findById).toHaveBeenCalledWith(card.id);
-			expect(boardDoService.deleteChildWithDescendants).toHaveBeenCalledWith(card, contentElement.id);
+				await uc.createElement(user.id, card.id);
+
+				expect(elementService.create).toHaveBeenCalledWith(card.id);
+			});
+		});
+	});
+
+	describe('deleteElement', () => {
+		describe('when deleting a content element', () => {
+			const setup = () => {
+				const user = userFactory.buildWithId();
+				const contentElement = textElementFactory.buildWithId();
+				const card = cardFactory.build();
+
+				return { user, card, contentElement };
+			};
+
+			it('should call deleteById of the element service', async () => {
+				const { user, card, contentElement } = setup();
+
+				await uc.deleteElement(user.id, card.id, contentElement.id);
+
+				expect(elementService.deleteById).toHaveBeenCalledWith(contentElement.id);
+			});
 		});
 	});
 });
