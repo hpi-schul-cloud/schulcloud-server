416864778e0149e6b4ee7f5fbd43618de8a5fbea
HeftyTea <timm.hoermann@capgemini.com>
2023-03-29 10:29:27 +0200

    EW-130 Fix email validation for AdminUser use case

--- src/services/authentication/configuration.js
+++ src/services/authentication/configuration.js
@@ -5,7 +5,9 @@ module.exports = {
 	audience,
 	authConfig: {
 		entity: 'account', // name of the found user in the request context
-		service: 'usersModel', // never queried, but an existing feathers service needs to be provided otherwise the server doesn't start
+		// entityId and service are never queried, but need to be provided otherwise the server doesn't start
+		entityId: 'noId',
+		service: 'emptyService', // This service is registered in 'index.js'
 		secret: authenticationSecret,
 		authStrategies: ['jwt', 'tsp', 'api-key'],
 		jwtOptions: {
