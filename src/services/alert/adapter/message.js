704333baf56b3bf2c44fc4363ae1c7e58144df4d
Michael Linares <michaellinaresdbildungs@gmail.com>
2023-03-31 11:01:42 -0400

    BC-3348 Add conditional to evaluate if timestamp is undefined or null and set de default.

--- src/services/alert/adapter/message.js
+++ src/services/alert/adapter/message.js
@@ -6,7 +6,7 @@ class Message {
 	constructor(title, text, timestamp, page, messageId, url, status, created_at) {
 		this.mTitle = title || '';
 		this.mText = text || '';
-		this.mTimestamp = timestamp || '1970-01-01 00:00:00';
+		this.mTimestamp = timestamp === undefined ? '' : timestamp;
 		// Origin of Message
 		this.mPage = page || '';
 		this.mMessageId = messageId || '-1';
