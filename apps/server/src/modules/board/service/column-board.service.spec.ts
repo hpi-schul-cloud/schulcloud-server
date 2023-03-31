941b0c6d2d8fc050bababf0f620b6df1dd0e31a7
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2023-03-31 11:00:16 +0200

    BC-3689 - Clean up board repositories and builders (#4027)
    * refactor board repos and builder implementations
    
    * add typed finder method to boad domain object repo
    
    * add BoardNodeRepo.findDescendantsOfMany
    
    * refactor module public services
    
    * add services to module index file
    
    ---------
    
    Co-authored-by: hoeppner-dataport <marcus.hoeppner@dataport.de>
    Co-authored-by: Nicole Friedo <nicole.friedo@dataport.de>
    Co-authored-by: NFriedo <69233063+NFriedo@users.noreply.github.com>
    Co-authored-by: Thomas Feldtkeller <thomas.feldtkeller@dataport.de>

--- apps/server/src/modules/board/service/column-board.service.spec.ts
+++ apps/server/src/modules/board/service/column-board.service.spec.ts
@@ -1,11 +1,9 @@
 import { createMock, DeepMocked } from '@golevelup/ts-jest';
-import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { ColumnBoard } from '@shared/domain';
 import { setupEntities } from '@shared/testing';
 import { columnBoardFactory, columnFactory } from '@shared/testing/factory/domainobject';
-import { Logger } from '@src/core/logger';
-import { ObjectId } from 'bson';
-import { BoardDoRepo } from '../repo';
+import { BoardDoRepo, BoardNodeRepo } from '../repo';
 import { ColumnBoardService } from './column-board.service';
 
 describe(ColumnBoardService.name, () => {
@@ -22,8 +20,8 @@ describe(ColumnBoardService.name, () => {
 					useValue: createMock<BoardDoRepo>(),
 				},
 				{
-					provide: Logger,
-					useValue: createMock<Logger>(),
+					provide: BoardNodeRepo,
+					useValue: createMock<BoardNodeRepo>(),
 				},
 			],
 		}).compile();
@@ -37,47 +35,37 @@ describe(ColumnBoardService.name, () => {
 		await module.close();
 	});
 
+	const setup = () => {
+		const board = columnBoardFactory.build();
+		const boardId = board.id;
+		const column = columnFactory.build();
+
+		return { board, boardId, column };
+	};
+
 	describe('finding a board', () => {
-		const setup = () => {
-			const board = columnBoardFactory.build();
-			const boardId = board.id;
-			const column = columnFactory.build();
-
-			return { board, boardId, column };
-		};
-
 		it('should call the board do repository', async () => {
 			const { boardId, board } = setup();
-			boardDoRepo.findById.mockResolvedValueOnce(board);
+			boardDoRepo.findByClassAndId.mockResolvedValueOnce(board);
 
 			await service.findById(boardId);
 
-			expect(boardDoRepo.findById).toHaveBeenCalledWith(boardId, 2);
+			expect(boardDoRepo.findByClassAndId).toHaveBeenCalledWith(ColumnBoard, boardId);
 		});
 
 		it('should return the columnBoard object of the given', async () => {
 			const { board } = setup();
-			boardDoRepo.findById.mockResolvedValueOnce(board);
+			boardDoRepo.findByClassAndId.mockResolvedValueOnce(board);
 
 			const result = await service.findById(board.id);
 
 			expect(result).toEqual(board);
 		});
-
-		it('should throw error when id does not belong to a columnboard', async () => {
-			const { column } = setup();
-
-			const expectedError = new NotFoundException(`There is no columboard with this id`);
-
-			boardDoRepo.findById.mockResolvedValue(column);
-
-			await expect(service.findById(column.id)).rejects.toThrowError(expectedError);
-		});
 	});
 
 	describe('creating a board', () => {
 		it('should save a board using the repo', async () => {
-			await service.createBoard();
+			await service.create();
 
 			expect(boardDoRepo.save).toHaveBeenCalledWith(
 				expect.objectContaining({
@@ -91,77 +79,13 @@ describe(ColumnBoardService.name, () => {
 		});
 	});
 
-	describe('creating a column', () => {
-		const setup = () => {
-			const board = columnBoardFactory.build();
-			const boardId = board.id;
+	describe('deleting a board', () => {
+		it('should call the service to delete the board', async () => {
+			const { board } = setup();
 
-			return { board, boardId };
-		};
+			await service.delete(board.id);
 
-		it('should save a list of columns using the repo', async () => {
-			const { board, boardId } = setup();
-
-			boardDoRepo.findById.mockResolvedValueOnce(board);
-
-			await service.createColumn(boardId);
-
-			expect(boardDoRepo.save).toHaveBeenCalledWith(
-				[
-					expect.objectContaining({
-						id: expect.any(String),
-						title: '',
-						children: [],
-						createdAt: expect.any(Date),
-						updatedAt: expect.any(Date),
-					}),
-				],
-				boardId
-			);
-		});
-	});
-
-	describe('creating a card', () => {
-		const setup = () => {
-			const column = columnFactory.build();
-			const board = columnBoardFactory.build({ children: [column] });
-			const boardId = board.id;
-			const columnId = column.id;
-
-			return { board, boardId, column, columnId };
-		};
-
-		it('should save a list of cards using the repo', async () => {
-			const { board, boardId, columnId } = setup();
-
-			boardDoRepo.findById.mockResolvedValueOnce(board);
-
-			await service.createCard(boardId, columnId);
-
-			expect(boardDoRepo.save).toHaveBeenCalledWith(
-				[
-					expect.objectContaining({
-						id: expect.any(String),
-						title: '',
-						height: 150,
-						children: [],
-						createdAt: expect.any(Date),
-						updatedAt: expect.any(Date),
-					}),
-				],
-				columnId
-			);
-		});
-
-		it('should throw not found exception if requested column id has not been found', async () => {
-			const { board, boardId } = setup();
-
-			const notExistingColumnId = new ObjectId().toHexString();
-			const error = new NotFoundException(`child is not child of this parent`);
-
-			boardDoRepo.findById.mockResolvedValueOnce(board);
-
-			await expect(service.createCard(boardId, notExistingColumnId)).rejects.toThrowError(error);
+			expect(boardDoRepo.deleteById).toHaveBeenCalledWith(board.id);
 		});
 	});
 });
