e95b1d96a152a6554ae8456b947e9b8cb1e4d770
stekrause <stephan.krause.ext@capgemini.com>
2023-04-03 10:08:54 +0200

    add filetypes to class-validator

--- apps/server/src/modules/fwu-learning-contents/controller/dto/fwu-learning-contents.params.ts
+++ apps/server/src/modules/fwu-learning-contents/controller/dto/fwu-learning-contents.params.ts
@@ -3,7 +3,9 @@ import { IsNotEmpty, IsString, Matches } from 'class-validator';
 
 export class GetFwuLearningContentParams {
 	@ApiProperty()
-	@Matches('([A-Za-z]|[0-9])+(.html|.css|.mp4|.pdf|.doc|.png|.jpg|.gif|.min.js|.js|.ico|.txt|.min.css)')
+	@Matches(
+		'([A-Za-z]|[0-9])+(.html|.css|.mp4|.pdf|.doc|.png|.jpg|.gif|.min.js|.js|.ico|.txt|.min.css|.ttf|.svg|.woff|.ui.l|.mf.l)'
+	)
 	@IsString()
 	@IsNotEmpty()
 	fwuLearningContent!: string;
