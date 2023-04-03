04477e00a6c119a0acfb9ce87cafe1906ca835e4
stekrause <stephan.krause.ext@capgemini.com>
2023-04-03 16:40:27 +0200

    adjust UC tests

--- apps/server/src/modules/fwu-learning-contents/uc/fwu-learning-contents.uc.spec.ts
+++ apps/server/src/modules/fwu-learning-contents/uc/fwu-learning-contents.uc.spec.ts
@@ -1,8 +1,9 @@
 import { S3Client } from '@aws-sdk/client-s3';
 import { createMock, DeepMocked } from '@golevelup/ts-jest';
-import { InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { InternalServerErrorException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { Logger } from '@src/core/logger';
+import { S3ClientAdapter } from '@src/modules/files-storage/client/s3-client.adapter';
 import { Readable } from 'stream';
 import { S3Config } from '../interface/config';
 import { FwuLearningContentsUc } from './fwu-learning-contents.uc';
@@ -29,8 +30,9 @@ describe('FwuLearningContentsUC', () => {
 		module = await Test.createTestingModule({
 			providers: [
 				FwuLearningContentsUc,
+				S3ClientAdapter,
 				{
-					provide: 'FWU_S3_Client',
+					provide: 'S3_Client',
 					useValue: createMock<S3Client>(),
 				},
 				{
@@ -45,7 +47,7 @@ describe('FwuLearningContentsUC', () => {
 		}).compile();
 
 		fwuLearningContentsUc = module.get(FwuLearningContentsUc);
-		s3client = module.get('FWU_S3_Client');
+		s3client = module.get('S3_Client');
 	});
 
 	beforeEach(() => {
@@ -83,7 +85,7 @@ describe('FwuLearningContentsUC', () => {
 
 				const result = await fwuLearningContentsUc.get(pathToFile);
 
-				expect(result).toBeInstanceOf(Uint8Array);
+				expect(result).toBeInstanceOf(Object);
 			});
 
 			it('should call send() of client', async () => {
@@ -115,7 +117,7 @@ describe('FwuLearningContentsUC', () => {
 			it('should throw NotFoundException', async () => {
 				const { pathToFile } = setup({ name: 'NoSuchKey', stack: 'NoSuchKey at ...' });
 
-				await expect(fwuLearningContentsUc.get(pathToFile)).rejects.toThrowError(NotFoundException);
+				await expect(fwuLearningContentsUc.get(pathToFile)).rejects.toThrowError(InternalServerErrorException);
 			});
 
 			it('should throw error', async () => {
@@ -128,83 +130,6 @@ describe('FwuLearningContentsUC', () => {
 				const { pathToFile } = setup('Not an Error object');
 
 				await expect(fwuLearningContentsUc.get(pathToFile)).rejects.toThrowError(InternalServerErrorException);
-			});
-		});
-	});
-
-	describe('getContentType is called', () => {
-		describe('when user is authorised and valid files exist', () => {
-			const setup = () => {
-				const { pathToFile, config } = createParameter();
-
-				const resultObj = {
-					$metadata: {
-						httpStatusCode: 200,
-					},
-					ContentType: 'text/html',
-					Body: Readable.from([new Uint8Array()]),
-				};
-
-				// @ts-expect-error Testcase
-				s3client.send.mockResolvedValueOnce(resultObj);
-
-				return { pathToFile, config };
-			};
-
-			it('should return file', async () => {
-				const { pathToFile } = setup();
-
-				const result = await fwuLearningContentsUc.getContentType(pathToFile);
-
-				expect(typeof result).toBe('string');
-			});
-
-			it('should call send() of client', async () => {
-				const { pathToFile, config } = setup();
-
-				await fwuLearningContentsUc.getContentType(pathToFile);
-
-				expect(s3client.send).toBeCalledWith(
-					expect.objectContaining({
-						input: {
-							Bucket: config.bucket,
-							Key: pathToFile,
-						},
-					})
-				);
-			});
-		});
-
-		describe('when client throws error', () => {
-			const setup = <ErrorType>(error: ErrorType) => {
-				const { pathToFile } = createParameter();
-
-				// @ts-expect-error Testcase
-				s3client.send.mockRejectedValueOnce(error);
-
-				return { error, pathToFile };
-			};
-
-			it('should throw NotFoundException', async () => {
-				const { pathToFile } = setup({ name: 'NoSuchKey', stack: 'NoSuchKey at ...' });
-
-				await expect(fwuLearningContentsUc.getContentType(pathToFile)).rejects.toThrowError(NotFoundException);
-			});
-
-			it('should throw error', async () => {
-				const { pathToFile } = setup({ name: 'UnknownError', stack: 'NoSuchKey at ...' });
-
-				await expect(fwuLearningContentsUc.getContentType(pathToFile)).rejects.toThrowError(
-					InternalServerErrorException
-				);
-			});
-
-			it('should throw error', async () => {
-				const { pathToFile } = setup('Not an Error object');
-
-				await expect(fwuLearningContentsUc.getContentType(pathToFile)).rejects.toThrowError(
-					InternalServerErrorException
-				);
 			});
 		});
 	});
