03fad87405af31cdab53fcdeede531242aca7b23
wolfganggreschus <wolfgang@greschus.de>
2023-04-05 22:02:10 +0200

    BC-2807 Validate beta task (#4010)
    * added courseUntilDate to TaskCardResponse
    
    * added maxLength-Validation to TaskCardParams
    
    * dueDate is mandatory now
    
    * validation of dueDate scenarios
    
    * course is mandatory in task-card.entity
    
    * task is not created if validation in the uc fails
    
    * text in a cardElement can be empty

--- apps/server/src/shared/domain/entity/task-card.entity.ts
+++ apps/server/src/shared/domain/entity/task-card.entity.ts
@@ -6,7 +6,7 @@ import { Course } from './course.entity';
 import { Task } from './task.entity';
 import { User } from './user.entity';
 
-export type ITaskCardProps = ICardCProps & { task: Task; dueDate: Date; course?: Course };
+export type ITaskCardProps = ICardCProps & { task: Task; dueDate: Date; course: Course };
 
 export interface ITaskCard extends ICard {
 	task: Task;
@@ -44,8 +44,8 @@ export class TaskCard extends BaseEntityWithTimestamps implements ICard, ITaskCa
 	creator!: User;
 
 	@Index()
-	@ManyToOne('Course', { fieldName: 'courseId', nullable: true })
-	course?: Course;
+	@ManyToOne('Course', { fieldName: 'courseId' })
+	course!: Course;
 
 	@Property()
 	draggable = true;
