b1b8089bcb10cb81ea8956ce49467583980984b1
dyedwiper <maximilian.becker@dataport.de>
2023-04-13 12:41:40 +0200

    Add tests for rule manager

---
+++ apps/server/src/modules/authorization/rule-manager.spec.ts
@@ -0,0 +1,184 @@
+import { DeepMocked, createMock } from '@golevelup/ts-jest';
+import { InternalServerErrorException, NotImplementedException } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { courseFactory, setupEntities, userFactory } from '@shared/testing';
+import { AuthorizationContextBuilder } from './authorization-context.builder';
+import { RuleManager } from './rule-manager';
+import {
+	CourseGroupRule,
+	CourseRule,
+	LessonRule,
+	SchoolExternalToolRule,
+	SchoolRule,
+	SubmissionRule,
+	TaskCardRule,
+	TaskRule,
+	TeamRule,
+	UserRule,
+} from './rules';
+
+describe('RuleManager', () => {
+	let service: RuleManager;
+	let courseRule: DeepMocked<CourseRule>;
+	let courseGroupRule: DeepMocked<CourseGroupRule>;
+	let lessonRule: DeepMocked<LessonRule>;
+	let schoolRule: DeepMocked<SchoolRule>;
+	let userRule: DeepMocked<UserRule>;
+	let taskRule: DeepMocked<TaskRule>;
+	let taskCardRule: DeepMocked<TaskCardRule>;
+	let teamRule: DeepMocked<TeamRule>;
+	let submissionRule: DeepMocked<SubmissionRule>;
+	let schoolExternalToolRule: DeepMocked<SchoolExternalToolRule>;
+
+	beforeAll(async () => {
+		await setupEntities();
+
+		const module = await Test.createTestingModule({
+			providers: [
+				RuleManager,
+				{ provide: CourseRule, useValue: createMock<CourseRule>() },
+				{ provide: CourseGroupRule, useValue: createMock<CourseGroupRule>() },
+				{ provide: LessonRule, useValue: createMock<LessonRule>() },
+				{ provide: SchoolRule, useValue: createMock<SchoolRule>() },
+				{ provide: UserRule, useValue: createMock<UserRule>() },
+				{ provide: TaskRule, useValue: createMock<TaskRule>() },
+				{ provide: TaskCardRule, useValue: createMock<TaskCardRule>() },
+				{ provide: TeamRule, useValue: createMock<TeamRule>() },
+				{ provide: SubmissionRule, useValue: createMock<SubmissionRule>() },
+				{ provide: SchoolExternalToolRule, useValue: createMock<SchoolExternalToolRule>() },
+			],
+		}).compile();
+
+		service = await module.get(RuleManager);
+		courseRule = await module.get(CourseRule);
+		courseGroupRule = await module.get(CourseGroupRule);
+		lessonRule = await module.get(LessonRule);
+		schoolRule = await module.get(SchoolRule);
+		userRule = await module.get(UserRule);
+		taskRule = await module.get(TaskRule);
+		taskCardRule = await module.get(TaskCardRule);
+		teamRule = await module.get(TeamRule);
+		submissionRule = await module.get(SubmissionRule);
+		schoolExternalToolRule = await module.get(SchoolExternalToolRule);
+	});
+
+	afterEach(() => {
+		jest.resetAllMocks();
+	});
+
+	it('should be defined', () => {
+		expect(service).toBeDefined();
+	});
+
+	describe('isAuthorized', () => {
+		// To discuss: I would say it is enough to test it for one rule as an example.
+		describe('when courseRule is applicable', () => {
+			const setup = () => {
+				const user = userFactory.build();
+				const object = courseFactory.build();
+				const context = AuthorizationContextBuilder.read([]);
+
+				courseRule.isApplicable.mockReturnValueOnce(true);
+				courseGroupRule.isApplicable.mockReturnValueOnce(false);
+				lessonRule.isApplicable.mockReturnValueOnce(false);
+				schoolRule.isApplicable.mockReturnValueOnce(false);
+				userRule.isApplicable.mockReturnValueOnce(false);
+				taskRule.isApplicable.mockReturnValueOnce(false);
+				taskCardRule.isApplicable.mockReturnValueOnce(false);
+				teamRule.isApplicable.mockReturnValueOnce(false);
+				submissionRule.isApplicable.mockReturnValueOnce(false);
+				schoolExternalToolRule.isApplicable.mockReturnValueOnce(false);
+
+				return { user, object, context };
+			};
+
+			it('should call isApplicable on all rules', () => {
+				const { user, object, context } = setup();
+
+				service.isAuthorized(user, object, context);
+
+				expect(courseRule.isApplicable).toBeCalled();
+				expect(courseGroupRule.isApplicable).toBeCalled();
+				expect(lessonRule.isApplicable).toBeCalled();
+				expect(schoolRule.isApplicable).toBeCalled();
+				expect(userRule.isApplicable).toBeCalled();
+				expect(taskRule.isApplicable).toBeCalled();
+				expect(taskCardRule.isApplicable).toBeCalled();
+				expect(teamRule.isApplicable).toBeCalled();
+				expect(submissionRule.isApplicable).toBeCalled();
+				expect(schoolExternalToolRule.isApplicable).toBeCalled();
+			});
+
+			it('should call isAuthorized only on CourseRule', () => {
+				const { user, object, context } = setup();
+
+				service.isAuthorized(user, object, context);
+
+				expect(courseRule.isAuthorized).toBeCalledWith(user, object, context);
+				expect(courseGroupRule.isAuthorized).not.toBeCalled();
+				expect(lessonRule.isAuthorized).not.toBeCalled();
+				expect(schoolRule.isAuthorized).not.toBeCalled();
+				expect(userRule.isAuthorized).not.toBeCalled();
+				expect(taskRule.isAuthorized).not.toBeCalled();
+				expect(taskCardRule.isAuthorized).not.toBeCalled();
+				expect(teamRule.isAuthorized).not.toBeCalled();
+				expect(submissionRule.isAuthorized).not.toBeCalled();
+				expect(schoolExternalToolRule.isAuthorized).not.toBeCalled();
+			});
+		});
+
+		describe('when no rule is applicable', () => {
+			const setup = () => {
+				const user = userFactory.build();
+				const object = courseFactory.build();
+				const context = AuthorizationContextBuilder.read([]);
+
+				courseRule.isApplicable.mockReturnValueOnce(false);
+				courseGroupRule.isApplicable.mockReturnValueOnce(false);
+				lessonRule.isApplicable.mockReturnValueOnce(false);
+				schoolRule.isApplicable.mockReturnValueOnce(false);
+				userRule.isApplicable.mockReturnValueOnce(false);
+				taskRule.isApplicable.mockReturnValueOnce(false);
+				taskCardRule.isApplicable.mockReturnValueOnce(false);
+				teamRule.isApplicable.mockReturnValueOnce(false);
+				submissionRule.isApplicable.mockReturnValueOnce(false);
+				schoolExternalToolRule.isApplicable.mockReturnValueOnce(false);
+
+				return { user, object, context };
+			};
+
+			it('should throw NotImplementedException', () => {
+				const { user, object, context } = setup();
+
+				expect(() => service.isAuthorized(user, object, context)).toThrow(NotImplementedException);
+			});
+		});
+
+		describe('when more than one rule is applicable', () => {
+			const setup = () => {
+				const user = userFactory.build();
+				const object = courseFactory.build();
+				const context = AuthorizationContextBuilder.read([]);
+
+				courseRule.isApplicable.mockReturnValueOnce(true);
+				courseGroupRule.isApplicable.mockReturnValueOnce(true);
+				lessonRule.isApplicable.mockReturnValueOnce(false);
+				schoolRule.isApplicable.mockReturnValueOnce(false);
+				userRule.isApplicable.mockReturnValueOnce(false);
+				taskRule.isApplicable.mockReturnValueOnce(false);
+				taskCardRule.isApplicable.mockReturnValueOnce(false);
+				teamRule.isApplicable.mockReturnValueOnce(false);
+				submissionRule.isApplicable.mockReturnValueOnce(false);
+				schoolExternalToolRule.isApplicable.mockReturnValueOnce(false);
+
+				return { user, object, context };
+			};
+
+			it('should throw InternalServerErrorException', () => {
+				const { user, object, context } = setup();
+
+				expect(() => service.isAuthorized(user, object, context)).toThrow(InternalServerErrorException);
+			});
+		});
+	});
+});
