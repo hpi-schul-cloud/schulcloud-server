b2280c6a90bf70b343536e225e95872db47462b8
Arne Gnisa <arne.gnisa@capgemini.com>
2023-04-19 09:15:03 +0200

    N21-793 changed imports

--- apps/server/src/modules/user-login-migration/user-login-migration-api.module.ts
+++ apps/server/src/modules/user-login-migration/user-login-migration-api.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
+import { OauthModule } from '@src/modules/oauth';
+import { LoggerModule } from '@src/core/logger';
+import { ProvisioningModule } from '@src/modules/provisioning';
+import { AuthenticationModule } from '@src/modules/authentication/authentication.module';
 import { UserLoginMigrationController } from './controller/user-login-migration.controller';
 import { UserMigrationController } from './controller/user-migration.controller';
 import { PageContentMapper } from './mapper/page-content.mapper';
 import { UserLoginMigrationUc } from './uc/user-login-migration.uc';
 import { UserLoginMigrationModule } from './user-login-migration.module';
-import { OauthModule } from '../oauth';
-import { ProvisioningModule } from '../provisioning';
-import { AuthenticationModule } from '../authentication/authentication.module';
 import { MigrationUc } from './uc/migration.uc';
-import { LoggerModule } from '../../core/logger';
 
 @Module({
 	imports: [UserLoginMigrationModule, OauthModule, ProvisioningModule, AuthenticationModule, LoggerModule],
