6c48875dad6550c60f5df82b25453438c4c800e0
Firas Shmit <firas.shmit@capgemini.com>
2023-04-20 14:41:38 +0200

    EW-347 deleted a comment

--- apps/server/src/modules/learnroom/service/common-cartridge-export.service.ts
+++ apps/server/src/modules/learnroom/service/common-cartridge-export.service.ts
@@ -24,7 +24,6 @@ export class CommonCartridgeExportService {
 		})
 			.addOrganizationItems(this.mapLessonsToOrganizationItems(lessons))
 			.addAssignments(this.mapTasksToAssignments(tasks));
-		/* addTopics(); */
 		return builder.build();
 	}
 
