923b724f6c827021f08cb80b273b1a374d9ab5bd
MartinSchuhmacher <mschuhmacher@gmx.de>
2023-04-24 11:26:54 +0200

    remove unnecessary module links

--- apps/server/src/modules/task-card/task-card.module.ts
+++ apps/server/src/modules/task-card/task-card.module.ts
@@ -1,15 +1,14 @@
 import { Module } from '@nestjs/common';
-import { CardElementRepo, CourseRepo, RichTextCardElementRepo, TaskCardRepo, TaskRepo } from '@shared/repo';
+import { CardElementRepo, CourseRepo, RichTextCardElementRepo, TaskCardRepo } from '@shared/repo';
 import { AuthorizationModule } from '../authorization/authorization.module';
-import { FilesStorageClientModule } from '../files-storage-client';
 import { TaskModule } from '../task/task.module';
 import { TaskCardController } from './controller/task-card.controller';
 import { TaskCardUc } from './uc/task-card.uc';
 
 @Module({
-	imports: [AuthorizationModule, TaskModule, FilesStorageClientModule],
+	imports: [AuthorizationModule, TaskModule],
 	controllers: [TaskCardController],
-	providers: [TaskCardUc, CardElementRepo, CourseRepo, TaskRepo, TaskCardRepo, RichTextCardElementRepo],
+	providers: [TaskCardUc, CardElementRepo, CourseRepo, TaskCardRepo, RichTextCardElementRepo],
 	exports: [],
 })
 export class TaskCardModule {}
