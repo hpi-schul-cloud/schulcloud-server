e4b59a73f08ec88f96b2df03002cd742f14c986a
SevenWaysDP <sergej.hoffmann@dataport.de>
2023-04-25 14:14:39 +0200

    BC-3298 - add entity tests

---
+++ apps/server/src/shared/domain/entity/boardnode/file-element-node.entity.spec.ts
@@ -0,0 +1,59 @@
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { fileElementFactory } from '@shared/testing';
+import { FileElementNode } from './file-element-node.entity';
+import { BoardDoBuilder, BoardNodeType } from './types';
+
+describe(FileElementNode.name, () => {
+	describe('when trying to create a file element', () => {
+		const setup = () => {
+			const elementProps = { caption: 'Test' };
+			const builder: DeepMocked<BoardDoBuilder> = createMock<BoardDoBuilder>();
+
+			return { elementProps, builder };
+		};
+
+		it('should create a FileElementNode', () => {
+			const { elementProps } = setup();
+
+			const element = new FileElementNode(elementProps);
+
+			expect(element.type).toEqual(BoardNodeType.FILE_ELEMENT);
+		});
+	});
+
+	describe('useDoBuilder()', () => {
+		const setup = () => {
+			const element = new FileElementNode({ caption: 'Test' });
+			const builder: DeepMocked<BoardDoBuilder> = createMock<BoardDoBuilder>();
+			const elementDo = fileElementFactory.build();
+
+			builder.buildFileElement.mockReturnValue(elementDo);
+
+			return { element, builder, elementDo };
+		};
+
+		it('should call the specific builder method', () => {
+			const { element, builder } = setup();
+
+			element.useDoBuilder(builder);
+
+			expect(builder.buildFileElement).toHaveBeenCalledWith(element);
+		});
+
+		it('should call the specific builder method', () => {
+			const { element, builder } = setup();
+
+			element.useDoBuilder(builder);
+
+			expect(builder.buildFileElement).toHaveBeenCalledWith(element);
+		});
+
+		it('should return FileElementDo', () => {
+			const { element, builder, elementDo } = setup();
+
+			const result = element.useDoBuilder(builder);
+
+			expect(result).toEqual(elementDo);
+		});
+	});
+});
