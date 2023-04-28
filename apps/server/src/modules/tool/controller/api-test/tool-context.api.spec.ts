574bd584d3b458b25c149cc0bc7fd1b60b3ea26e
Mrika Llabani <mrika.llabani@capgemini.com>
2023-04-28 17:08:33 +0200

    N21-838 test structure

--- apps/server/src/modules/tool/controller/api-test/tool-context.api.spec.ts
+++ apps/server/src/modules/tool/controller/api-test/tool-context.api.spec.ts
@@ -57,93 +57,147 @@ describe('ToolContextController (API)', () => {
 		await orm.getSchemaGenerator().clearDatabase();
 	});
 
-	const setup = async () => {
-		const teacherRole: Role = roleFactory.build({
-			name: RoleName.TEACHER,
-			permissions: [Permission.CONTEXT_TOOL_ADMIN],
-		});
-
-		const teacherUser: User = userFactory.buildWithId({ roles: [teacherRole] });
-		const userWithMissingPermission: User = userFactory.buildWithId();
-
-		const course: Course = courseFactory.buildWithId({ teachers: [teacherUser, userWithMissingPermission] });
-
-		const schoolExternalTool: SchoolExternalTool = schoolExternalToolFactory.buildWithId({
-			schoolParameters: [],
-			toolVersion: 1,
-		});
-
-		const contextExternalTool: ContextExternalTool = contextExternalToolFactory.buildWithId({
-			schoolTool: schoolExternalTool,
-			contextId: course.id,
-			parameters: [],
-			toolVersion: 1,
-		});
-
-		em.persist([teacherRole, course, teacherUser, userWithMissingPermission, schoolExternalTool, contextExternalTool]);
-		await em.flush();
-		em.clear();
-
-		return {
-			schoolExternalTool,
-			contextExternalTool,
-			course,
-			teacherUser,
-			userWithMissingPermission,
-		};
-	};
-
 	describe('[POST] tools/context', () => {
-		it('should return forbidden when user is not authorized', async () => {
-			const { userWithMissingPermission } = await setup();
-			currentUser = mapUserToCurrentUser(userWithMissingPermission);
-
-			const randomTestId = new ObjectId().toString();
-			const postParams: ContextExternalToolPostParams = {
-				schoolToolId: randomTestId,
-				contextId: randomTestId,
-				contextType: ToolContextType.COURSE,
-				parameters: [],
-				toolVersion: 1,
-			};
-			await request(app.getHttpServer()).post(basePath).send(postParams).expect(403);
-		});
-
-		it('should create an context external tool', async () => {
-			const { schoolExternalTool, course, teacherUser } = await setup();
-			currentUser = mapUserToCurrentUser(teacherUser);
-
-			const postParams: ContextExternalToolPostParams = {
-				schoolToolId: schoolExternalTool.id,
-				contextId: course.id,
-				contextType: ToolContextType.COURSE,
-				parameters: [],
-				toolVersion: 1,
-			};
-
-			await request(app.getHttpServer())
-				.post(basePath)
-				.send(postParams)
-				.expect(201)
-				.then((res: Response) => {
-					expect(res.body).toEqual(
-						expect.objectContaining(<ContextExternalToolResponse>{
-							id: expect.any(String),
-							schoolToolId: postParams.schoolToolId,
-							contextId: postParams.contextId,
-							contextType: ToolContextType.COURSE,
-							parameters: [],
-							toolVersion: postParams.toolVersion,
-						})
-					);
-					return res;
+		describe('when creation of contextExternalTool is successfully', () => {
+			const setup = async () => {
+				const teacherRole: Role = roleFactory.build({
+					name: RoleName.TEACHER,
+					permissions: [Permission.CONTEXT_TOOL_ADMIN],
 				});
 
-			const createdContextExternalTool: ContextExternalTool | null = await em.findOne(ContextExternalTool, {
-				schoolTool: postParams.schoolToolId,
-				contextId: postParams.contextId,
+				const teacherUser: User = userFactory.buildWithId({ roles: [teacherRole] });
+				const userWithMissingPermission: User = userFactory.buildWithId();
+
+				const course: Course = courseFactory.buildWithId({ teachers: [teacherUser, userWithMissingPermission] });
+
+				const schoolExternalTool: SchoolExternalTool = schoolExternalToolFactory.buildWithId({
+					schoolParameters: [],
+					toolVersion: 1,
+				});
+
+				const contextExternalTool: ContextExternalTool = contextExternalToolFactory.buildWithId({
+					schoolTool: schoolExternalTool,
+					contextId: course.id,
+					parameters: [],
+					toolVersion: 1,
+				});
+
+				em.persist([
+					teacherRole,
+					course,
+					teacherUser,
+					userWithMissingPermission,
+					schoolExternalTool,
+					contextExternalTool,
+				]);
+				await em.flush();
+				em.clear();
+
+				return {
+					schoolExternalTool,
+					contextExternalTool,
+					course,
+					teacherUser,
+					userWithMissingPermission,
+				};
+			};
+
+			it('should create an contextExternalTool', async () => {
+				const { schoolExternalTool, course, teacherUser } = await setup();
+				currentUser = mapUserToCurrentUser(teacherUser);
+
+				const postParams: ContextExternalToolPostParams = {
+					schoolToolId: schoolExternalTool.id,
+					contextId: course.id,
+					contextType: ToolContextType.COURSE,
+					parameters: [],
+					toolVersion: 1,
+				};
+
+				await request(app.getHttpServer())
+					.post(basePath)
+					.send(postParams)
+					.expect(201)
+					.then((res: Response) => {
+						expect(res.body).toEqual(
+							expect.objectContaining(<ContextExternalToolResponse>{
+								id: expect.any(String),
+								schoolToolId: postParams.schoolToolId,
+								contextId: postParams.contextId,
+								contextType: 'course',
+								parameters: [],
+								toolVersion: postParams.toolVersion,
+							})
+						);
+						return res;
+					});
+
+				const createdContextExternalTool: ContextExternalTool | null = await em.findOne(ContextExternalTool, {
+					schoolTool: postParams.schoolToolId,
+					contextId: postParams.contextId,
+				});
+				expect(createdContextExternalTool).toBeDefined();
 			});
-			expect(createdContextExternalTool).toBeDefined();
+		});
+
+		describe('when creation of contextExternalTool failed', () => {
+			const setup = async () => {
+				const teacherRole: Role = roleFactory.build({
+					name: RoleName.TEACHER,
+					permissions: [Permission.CONTEXT_TOOL_ADMIN],
+				});
+
+				const teacherUser: User = userFactory.buildWithId({ roles: [teacherRole] });
+				const userWithMissingPermission: User = userFactory.buildWithId();
+
+				const course: Course = courseFactory.buildWithId({ teachers: [teacherUser, userWithMissingPermission] });
+
+				const schoolExternalTool: SchoolExternalTool = schoolExternalToolFactory.buildWithId({
+					schoolParameters: [],
+					toolVersion: 1,
+				});
+
+				const contextExternalTool: ContextExternalTool = contextExternalToolFactory.buildWithId({
+					schoolTool: schoolExternalTool,
+					contextId: course.id,
+					parameters: [],
+					toolVersion: 1,
+				});
+
+				em.persist([
+					teacherRole,
+					course,
+					teacherUser,
+					userWithMissingPermission,
+					schoolExternalTool,
+					contextExternalTool,
+				]);
+				await em.flush();
+				em.clear();
+
+				return {
+					schoolExternalTool,
+					contextExternalTool,
+					course,
+					teacherUser,
+					userWithMissingPermission,
+				};
+			};
+
+			it('should return forbidden when user is not authorized', async () => {
+				const { userWithMissingPermission } = await setup();
+				currentUser = mapUserToCurrentUser(userWithMissingPermission);
+
+				const randomTestId = new ObjectId().toString();
+				const postParams: ContextExternalToolPostParams = {
+					schoolToolId: randomTestId,
+					contextId: randomTestId,
+					contextType: ToolContextType.COURSE,
+					parameters: [],
+					toolVersion: 1,
+				};
+				await request(app.getHttpServer()).post(basePath).send(postParams).expect(403);
+			});
 		});
 	});
 });
