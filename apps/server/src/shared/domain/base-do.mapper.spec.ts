4b19aa6ec0e307877591fd97e5801c39a8e5b5b6
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2023-05-02 08:40:51 +0200

    add test plan

---
+++ apps/server/src/shared/domain/base-do.mapper.spec.ts
@@ -0,0 +1,12 @@
+describe('base-do.mapper', () => {
+	describe('when one entity resolve in one domain object', () => {});
+	describe('when one entity resolve in multiple domain objects', () => {});
+	describe('when multiple entities resolve in one domain object', () => {});
+	describe('when one generic object resolve in one domain object', () => {});
+	describe('when one generic object and entity resolve in one domain object', () => {});
+	//---
+	describe('when keys from do are different to entity', () => {});
+	describe('when embedded objects with id must be merged', () => {});
+	describe('when arrays must be merged into entites', () => {});
+	describe('when arrays that include embedded objects with id must be merged', () => {});
+});
