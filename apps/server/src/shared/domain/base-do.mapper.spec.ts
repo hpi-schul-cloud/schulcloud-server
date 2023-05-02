d9dbc0fe22eae35c3ca086994e8ddc2115fba57d
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2023-05-02 08:42:49 +0200

    change test plan

--- apps/server/src/shared/domain/base-do.mapper.spec.ts
+++ apps/server/src/shared/domain/base-do.mapper.spec.ts
@@ -1,11 +1,12 @@
 describe('base-do.mapper', () => {
 	describe('when one entity resolve in one domain object', () => {});
 	describe('when one entity resolve in multiple domain objects', () => {});
-	describe('when multiple entities resolve in one domain object', () => {});
+	describe.skip('when multiple entities resolve in one domain object', () => {});
 	describe('when one generic object resolve in one domain object', () => {});
-	describe('when one generic object and entity resolve in one domain object', () => {});
+	describe.skip('when one generic object and entity resolve in one domain object', () => {});
 	//---
-	describe('when keys from do are different to entity', () => {});
+	describe('when keys from domainObject are different to entity', () => {});
+	describe.skip('when keys from domainObject are missed by mapping', () => {});
 	describe('when embedded objects with id must be merged', () => {});
 	describe('when arrays must be merged into entites', () => {});
 	describe('when arrays that include embedded objects with id must be merged', () => {});
