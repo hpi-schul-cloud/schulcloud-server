b2fbd9cfd343b5ab6cbb48389d864c8b70e39c43
MartinSchuhmacher <mschuhmacher@gmx.de>
2023-05-05 09:02:33 +0200

    update script

--- scripts/ldapSync.sh
+++ scripts/ldapSync.sh
@@ -2,17 +2,17 @@
 # Start server in the background and redirect logs to file
 nohup npm run nest:start > server.log 2>&1 &
 
-# Wait for server to start up
+# Wait for server to start up and show logs
 echo "Wait for server to start..."
-until curl -s -w "%{http_code}\n" "http://localhost:3030/serverversion" | grep 200; do
-    sleep 1
-done
-
-# Show server logs and start sync
 echo "Server logs:"
 tail -f server.log &
 
-curl --retry 360 --retry-connrefused --retry-delay 10 --max-time {{ SERVER_LDAP_SYNC_FULL_CRONJOB_TIMEOUT|default("39600", true) }} -H "X-API-Key: $SYNC_API_KEY" "http://{{ API_LDAP_SYNC_SVC|default("api-ldapsync-svc", true) }}:3030/api/v1/sync?target=ldap&forceFullSync=true" | python3 -m json.tool
+until curl -s -w --retry 360 --retry-connrefused --retry-delay 10 "%{http_code}\n" "http://localhost:3030/serverversion" | grep 200; do
+    sleep 1
+done
+
+# Start sync
+curl --max-time {{ SERVER_LDAP_SYNC_FULL_CRONJOB_TIMEOUT|default("39600", true) }} -H "X-API-Key: $SYNC_API_KEY" "http://{{ API_LDAP_SYNC_SVC|default("api-ldapsync-svc", true) }}:3030/api/v1/sync?target=ldap&forceFullSync=true" | python3 -m json.tool
 
 # Stop server and cleanup
 kill %1
