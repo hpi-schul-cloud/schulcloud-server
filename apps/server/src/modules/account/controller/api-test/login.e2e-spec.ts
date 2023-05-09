67897200fe77f37ecec3220af6a78a5cf3c0c7ac
Omar Ezzat <omar.ezzat@dataport.de>
2023-05-09 13:36:47 +0200

    revert file renaming

--- apps/server/src/modules/account/controller/api-test/login.e2e-spec.ts
+++ apps/server/src/modules/account/controller/api-test/login.e2e-spec.ts
@@ -1,11 +1,11 @@
-import request from 'supertest';
 import { EntityManager } from '@mikro-orm/core';
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { Account, RoleName, School, System, User } from '@shared/domain';
 import { accountFactory, roleFactory, schoolFactory, systemFactory, userFactory } from '@shared/testing';
-import { ServerTestModule } from '@src/modules/server/server.module';
 import { LdapAuthorizationBodyParams } from '@src/modules/authentication/controllers/dto';
+import { ServerTestModule } from '@src/modules/server/server.module';
+import request from 'supertest';
 
 const schoolExternalId = 'mockSchoolExternalId';
 const ldapAccountUserName = 'ldapAccountUserName';
