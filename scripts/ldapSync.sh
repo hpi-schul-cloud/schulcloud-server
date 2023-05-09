9e48274beb5cf8ccc97977d1bbae5ea8426075a6
MartinSchuhmacher <mschuhmacher@gmx.de>
2023-05-09 22:12:15 +0200

    finalize script

--- scripts/ldapSync.sh
+++ scripts/ldapSync.sh
@@ -10,20 +10,18 @@ value_api_key="${SYNC_API_KEY:-$default_api_key}"
 # Start server in the background and redirect logs to file
 nohup npm run nest:start > server.log 2>&1 &
 
-# Wait for server to start up and show logs
-echo "Wait for server to start..."
+# Show logs and wait for server to start up
 echo "Server logs:"
 tail -f server.log &
 
+echo "Wait for server to start..."
 until curl -s -w --retry 360 --retry-connrefused --retry-delay 10 http://localhost:3030/serverversion >/dev/null 2>&1; do
     sleep 1
     echo "asleep"
 done
 
-echo "Starting"
 # Start sync
+echo "Starting"
 curl --max-time $value_time_out -H "X-API-Key: $value_api_key" "http://localhost:3030/api/v1/sync?target=ldap&forceFullSync=true" | python3 -m json.tool
 
-# Stop server and cleanup
-# kill %1
-# rm server.log
+# Process stops automatically and deletes logs with end of job pod
