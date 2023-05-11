7df31e537881e748d526d466d255f792ce16ee89
dyedwiper <maximilian.becker@dataport.de>
2023-05-11 17:22:49 +0200

    Add error handling

--- apps/server/src/modules/files/uc/delete-files.uc.ts
+++ apps/server/src/modules/files/uc/delete-files.uc.ts
@@ -36,28 +36,50 @@ export class DeleteFilesUc {
 		}
 	}
 
-	public async deleteMarkedFiles(removedSince: Date, batchSize: number) {
+	public async deleteMarkedFiles(removedSince: Date, batchSize: number): Promise<void> {
 		let batchCounter = 0;
-		let numberOfFilesFound = 0;
+		let numberOfFilesInBatch = 0;
+		let numberOfProcessedFiles = 0;
+		const failingFileIds: string[] = [];
 
 		do {
-			const [files, count] = await this.filesRepo.findAndCountFilesForCleanup(removedSince, batchSize);
+			const offset = failingFileIds.length;
+			const [files, count] = await this.filesRepo.findAndCountFilesForCleanup(removedSince, batchSize, offset);
 
 			const promises = files.map((file) => this.deleteFile(file));
-			await Promise.all(promises);
+			const results = await Promise.all(promises);
 
-			numberOfFilesFound = count;
+			results.forEach((result) => !result.success && failingFileIds.push(result.fileId));
+
+			numberOfFilesInBatch = count;
+			numberOfProcessedFiles += count;
 			batchCounter += 1;
-			this.logger.debug(`Finished batch ${batchCounter} with ${numberOfFilesFound} files`);
-		} while (numberOfFilesFound > 0);
+			this.logger.debug(`Finished batch ${batchCounter} with ${numberOfFilesInBatch} files`);
+		} while (numberOfFilesInBatch > 0);
+
+		this.logger.log(
+			`${
+				numberOfProcessedFiles - failingFileIds.length
+			} out of ${numberOfProcessedFiles} files were successfully deleted`
+		);
+
+		if (failingFileIds.length > 0) {
+			this.logger.error(`the following files could not be deleted: ${failingFileIds.toString()}`);
+		}
 	}
 
-	private async deleteFile(file: File) {
-		if (!file.isDirectory) {
-			await this.fileStorageAdapter.deleteFile(file);
-		}
-		await this.filesRepo.delete(file);
+	private async deleteFile(file: File): Promise<{ fileId: string; success: boolean }> {
+		try {
+			if (!file.isDirectory) {
+				await this.fileStorageAdapter.deleteFile(file);
+			}
+			await this.filesRepo.delete(file);
 
-		console.log(file.name + 'deleted');
+			return { fileId: file.id, success: true };
+		} catch (error) {
+			this.logger.error(error);
+
+			return { fileId: file.id, success: false };
+		}
 	}
 }
