98389471ba396629de72326530da5fda6949b7d1
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2023-05-15 12:44:58 +0200

    typo

--- apps/server/src/modules/authorization/types/rule.interface.ts
+++ apps/server/src/modules/authorization/types/rule.interface.ts
@@ -1,6 +1,6 @@
 import { BaseDO, User } from '@shared/domain';
-import { AuthorizableObject } from '@shared/domain/domain-object';
-import { AuthorizationContext } from './authorization-context.interface'; // fix import when it is avaible
+import { AuthorizableObject } from '@shared/domain/domain-object'; // fix import when it is avaible
+import { AuthorizationContext } from './authorization-context.interface';
 
 export interface Rule<T = AuthorizableObject | BaseDO> {
 	isApplicable(user: User, object: T, context?: AuthorizationContext): boolean;
