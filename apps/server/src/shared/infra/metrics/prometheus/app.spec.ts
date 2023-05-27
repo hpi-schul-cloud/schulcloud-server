ec137a57567dc65e51de6da1f53298fef09b1da8
Bartosz Nowicki <bartosz.nowicki@gca.pass-consulting.com>
2023-05-27 15:12:51 +0200

    add unit tests for the createPrometheusMetricsApp function

---
+++ apps/server/src/shared/infra/metrics/prometheus/app.spec.ts
@@ -0,0 +1,26 @@
+import request from 'supertest';
+
+import { createPrometheusMetricsApp } from './app';
+
+describe('createPrometheusMetricsApp', () => {
+	const testMetricsRoute = '/prometheus-metrics';
+	const exampleDefaultMetricName = 'process_heap_bytes';
+	const exampleMetricsRouteMetric =
+		`sc_api_response_time_in_seconds_count{` +
+		`method="GET",` +
+		`base_url="",full_path="${testMetricsRoute}",route_path="${testMetricsRoute}",` +
+		`status_code="200"}`;
+
+	describe('should create an app that should', () => {
+		it('collect all the available metrics and expose them on given route', async () => {
+			const app = createPrometheusMetricsApp(testMetricsRoute, true, true);
+
+			let response = await request(app).get(testMetricsRoute);
+			response = await request(app).get(testMetricsRoute);
+
+			expect(response.statusCode).toBe(200);
+			expect(response.text).toContain(exampleDefaultMetricName);
+			expect(response.text).toContain(exampleMetricsRouteMetric);
+		});
+	});
+});
