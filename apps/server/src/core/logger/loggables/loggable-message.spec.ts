7b6ac69b25cbed810066a6ff628959998d420b0f
Bartosz Nowicki <bartosz.nowicki@gca.pass-consulting.com>
2023-05-29 01:45:02 +0200

    modify getLogMessage() method unit test

--- apps/server/src/core/logger/loggables/loggable-message.spec.ts
+++ apps/server/src/core/logger/loggables/loggable-message.spec.ts
@@ -4,7 +4,7 @@ describe('LoggableMessage', () => {
 	describe('getLogMessage', () => {
 		it('should return loggable message with given content', () => {
 			const testMessage = "this is some test message I'd like to log";
-			const testLoggableMessage = new LoggableMessage(testMessage);
+			const testLoggableMessage = new LoggableMessage(testMessage).getLogMessage();
 
 			expect(testLoggableMessage).toHaveProperty('message', testMessage);
 		});
