7942a7e90ab1b393d2a39f4188eefba27dfc19a1
Marvin Rode <marvin.rode@capgemini.com>
2023-06-02 13:18:47 +0200

    Testing for files in UC

---
+++ apps/server/src/modules/h5p-editor/uc/h5p-files.uc.spec.ts
@@ -0,0 +1,294 @@
+import { DeepMocked, createMock } from '@golevelup/ts-jest';
+import { Test, TestingModule } from '@nestjs/testing';
+import { setupEntities } from '@shared/testing';
+import { Request } from 'express';
+import { Readable } from 'stream';
+import { ContentStorage } from '../contentStorage/contentStorage';
+import { H5PEditorTestModule } from '../h5p-editor-test.module';
+import { LibraryStorage } from '../libraryStorage/libraryStorage';
+import { TemporaryFileStorage } from '../temporary-file-storage/temporary-file-storage';
+import { H5PEditorUc } from './h5p.uc';
+
+describe('H5P Files', () => {
+	let module: TestingModule;
+	let uc: H5PEditorUc;
+	let contentStorage: DeepMocked<ContentStorage>;
+	let libraryStorage: DeepMocked<LibraryStorage>;
+	let temporaryStorage: DeepMocked<TemporaryFileStorage>;
+
+	beforeAll(async () => {
+		module = await Test.createTestingModule({
+			imports: [H5PEditorTestModule],
+		})
+			.overrideProvider(ContentStorage)
+			.useValue(createMock<ContentStorage>())
+			.overrideProvider(LibraryStorage)
+			.useValue(createMock<LibraryStorage>())
+			.overrideProvider(TemporaryFileStorage)
+			.useValue(createMock<TemporaryFileStorage>())
+			.compile();
+
+		uc = module.get(H5PEditorUc);
+		contentStorage = module.get(ContentStorage);
+		libraryStorage = module.get(LibraryStorage);
+		temporaryStorage = module.get(TemporaryFileStorage);
+		await setupEntities();
+	});
+
+	afterEach(() => {
+		jest.resetAllMocks();
+	});
+
+	afterAll(async () => {
+		await module.close();
+	});
+
+	describe('when getting content file', () => {
+		const setup = (
+			contentId: string,
+			filename: string,
+			content: string,
+			rangeCallbackReturnValue?: { start: number; end: number }[] | -1 | -2
+		) => {
+			const fileDate = new Date();
+
+			const readableContent = Readable.from(content);
+			const contentLength = content.length;
+
+			let contentRange: { start: number; end: number } | undefined;
+			if (rangeCallbackReturnValue && rangeCallbackReturnValue !== -1 && rangeCallbackReturnValue !== -2) {
+				contentRange = rangeCallbackReturnValue[0];
+			}
+
+			contentStorage.getFileStats.mockResolvedValueOnce({ birthtime: fileDate, size: contentLength });
+			contentStorage.getFileStream.mockResolvedValueOnce(readableContent);
+
+			const requestMock = {
+				// eslint-disable-next-line @typescript-eslint/no-unused-vars
+				range: (size: number) => rangeCallbackReturnValue,
+			} as Request;
+
+			return { contentId, filename, requestMock, contentRange, contentLength, readableContent };
+		};
+
+		it('should call ContentStorage and return the result', async () => {
+			const { contentId, filename, requestMock, contentLength, contentRange, readableContent } = setup(
+				'DummyId',
+				'dummy-file.jpg',
+				'File Content'
+			);
+
+			const result = await uc.getContentFile(contentId, filename, requestMock);
+
+			expect(result).toStrictEqual({
+				data: readableContent,
+				contentType: 'image/jpeg',
+				contentLength,
+				contentRange,
+			});
+
+			expect(contentStorage.getFileStats).toHaveBeenCalledWith(contentId, filename, expect.anything()); // Todo: User
+			expect(contentStorage.getFileStream).toHaveBeenCalledWith(
+				contentId,
+				filename,
+				expect.anything(), // Todo: User
+				contentRange?.start,
+				contentRange?.end
+			);
+		});
+
+		it('should accept ranges', async () => {
+			const content = 'File Content';
+
+			const { contentId, filename, requestMock, contentLength, contentRange, readableContent } = setup(
+				'DummyId',
+				'dummy-file.jpg',
+				content,
+				[{ start: 0, end: content.length }]
+			);
+
+			const result = await uc.getContentFile(contentId, filename, requestMock);
+
+			expect(result).toStrictEqual({
+				data: readableContent,
+				contentType: 'image/jpeg',
+				contentLength,
+				contentRange,
+			});
+
+			expect(contentStorage.getFileStats).toHaveBeenCalledWith(contentId, filename, expect.anything()); // Todo: User
+			expect(contentStorage.getFileStream).toHaveBeenCalledWith(
+				contentId,
+				filename,
+				expect.anything(), // Todo: User
+				contentRange?.start,
+				contentRange?.end
+			);
+		});
+
+		it('should fail on invalid ranges', async () => {
+			const { contentId, filename, requestMock } = setup('DummyId', 'dummy-file.jpg', 'File Content', -2);
+			const result = uc.getContentFile(contentId, filename, requestMock);
+			await expect(result).rejects.toThrow();
+		});
+
+		it('should fail on unsatisfiable ranges', async () => {
+			const { contentId, filename, requestMock } = setup('DummyId', 'dummy-file.jpg', 'File Content', -1);
+			const result = uc.getContentFile(contentId, filename, requestMock);
+			await expect(result).rejects.toThrow();
+		});
+
+		it('should fail on multipart ranges', async () => {
+			const { contentId, filename, requestMock } = setup('DummyId', 'dummy-file.jpg', 'File Content', [
+				{ start: 0, end: 5 },
+				{ start: 8, end: 12 },
+			]);
+			const result = uc.getContentFile(contentId, filename, requestMock);
+			await expect(result).rejects.toThrow();
+		});
+	});
+
+	describe('when getting library file', () => {
+		const setup = (ubername: string, filename: string, content: string) => {
+			const fileDate = new Date();
+
+			const readableContent = Readable.from(content);
+			const contentLength = content.length;
+			libraryStorage.getFileStats.mockResolvedValueOnce({ birthtime: fileDate, size: contentLength });
+			libraryStorage.getFileStream.mockResolvedValueOnce(readableContent);
+
+			return { ubername, filename, contentLength, readableContent };
+		};
+
+		it('should call LibraryStorage and return the result', async () => {
+			const { ubername, filename, contentLength, readableContent } = setup(
+				'H5P.Example-1.0',
+				'dummy-file.jpg',
+				'File Content'
+			);
+
+			const result = await uc.getLibraryFile(ubername, filename);
+
+			expect(result).toStrictEqual({
+				data: readableContent,
+				contentType: 'image/jpeg',
+				contentLength,
+			});
+
+			expect(libraryStorage.getFileStats).toHaveBeenCalledWith(
+				{ machineName: 'H5P.Example', majorVersion: 1, minorVersion: 0 },
+				'dummy-file.jpg'
+			);
+			expect(libraryStorage.getFileStream).toHaveBeenCalledWith(
+				{ machineName: 'H5P.Example', majorVersion: 1, minorVersion: 0 },
+				'dummy-file.jpg'
+			);
+		});
+
+		it('should fail on invalid ubername', async () => {
+			const { ubername, filename } = setup('InvalidName', 'dummy-file.jpg', 'File Content');
+
+			const result = uc.getLibraryFile(ubername, filename);
+
+			await expect(result).rejects.toThrow('invalid-ubername-pattern');
+		});
+	});
+
+	describe('when getting temporary file', () => {
+		const setup = (
+			filename: string,
+			content: string,
+			rangeCallbackReturnValue?: { start: number; end: number }[] | -1 | -2
+		) => {
+			const fileDate = new Date();
+
+			const readableContent = Readable.from(content);
+			const contentLength = content.length;
+
+			let contentRange: { start: number; end: number } | undefined;
+			if (rangeCallbackReturnValue && rangeCallbackReturnValue !== -1 && rangeCallbackReturnValue !== -2) {
+				contentRange = rangeCallbackReturnValue[0];
+			}
+
+			temporaryStorage.getFileStats.mockResolvedValueOnce({ birthtime: fileDate, size: contentLength });
+			temporaryStorage.getFileStream.mockResolvedValueOnce(readableContent);
+
+			const requestMock = {
+				// eslint-disable-next-line @typescript-eslint/no-unused-vars
+				range: (size: number) => rangeCallbackReturnValue,
+			} as Request;
+
+			return { filename, requestMock, contentRange, contentLength, readableContent };
+		};
+
+		it('should call ContentStorage and return the result', async () => {
+			const { filename, requestMock, contentLength, contentRange, readableContent } = setup(
+				'dummy-file.jpg',
+				'File Content'
+			);
+
+			const result = await uc.getTemporaryFile(filename, requestMock);
+
+			expect(result).toStrictEqual({
+				data: readableContent,
+				contentType: 'image/jpeg',
+				contentLength,
+				contentRange,
+			});
+
+			expect(temporaryStorage.getFileStats).toHaveBeenCalledWith(filename, expect.anything()); // Todo: User
+			expect(temporaryStorage.getFileStream).toHaveBeenCalledWith(
+				filename,
+				expect.anything(), // Todo: User
+				contentRange?.start,
+				contentRange?.end
+			);
+		});
+
+		it('should accept ranges', async () => {
+			const content = 'File Content';
+
+			const { filename, requestMock, contentLength, contentRange, readableContent } = setup('dummy-file.jpg', content, [
+				{ start: 0, end: content.length },
+			]);
+
+			const result = await uc.getTemporaryFile(filename, requestMock);
+
+			expect(result).toStrictEqual({
+				data: readableContent,
+				contentType: 'image/jpeg',
+				contentLength,
+				contentRange,
+			});
+
+			expect(temporaryStorage.getFileStats).toHaveBeenCalledWith(filename, expect.anything()); // Todo: User
+			expect(temporaryStorage.getFileStream).toHaveBeenCalledWith(
+				filename,
+				expect.anything(), // Todo: User
+				contentRange?.start,
+				contentRange?.end
+			);
+		});
+
+		it('should fail on invalid ranges', async () => {
+			const { filename, requestMock } = setup('dummy-file.jpg', 'File Content', -2);
+			const result = uc.getTemporaryFile(filename, requestMock);
+			await expect(result).rejects.toThrow();
+		});
+
+		it('should fail on unsatisfiable ranges', async () => {
+			const { filename, requestMock } = setup('dummy-file.jpg', 'File Content', -2);
+			const result = uc.getTemporaryFile(filename, requestMock);
+			await expect(result).rejects.toThrow();
+		});
+
+		it('should fail on multipart ranges', async () => {
+			const { filename, requestMock } = setup('dummy-file.jpg', 'File Content', [
+				{ start: 0, end: 5 },
+				{ start: 8, end: 12 },
+			]);
+			const result = uc.getTemporaryFile(filename, requestMock);
+			await expect(result).rejects.toThrow();
+		});
+	});
+});
