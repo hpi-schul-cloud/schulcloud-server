b5a8c4c5350904504e23c703d85c3575303ca1eb
hoeppner-dataport <106819770+hoeppner-dataport@users.noreply.github.com>
2023-06-06 16:06:17 +0200

    BC-3573 - Add columnBoard per course (#4154)
    Create one ColumnBoard per course
    The existing CourseDetailPage will contain one, automatically created (when opening a CourseDetailPage for the first time) columnBoard for each Course.
    
    * new feature toggle FEATURE_COLUMN_BOARD_ENABLED
    ** is set to false by default
    ** is set to true for all development environments (see dof-PR) 
    
    ---------
    
    Co-authored-by: Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
    Co-authored-by: Thomas Feldtkeller <thomas.feldtkeller@dataport.de>

---
+++ apps/server/src/modules/learnroom/controller/dto/single-column-board/board-column-board.response.ts
@@ -0,0 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { DecodeHtmlEntities } from '@shared/controller';
+
+export class BoardColumnBoardResponse {
+	constructor({ id, columnBoardId, title, published, createdAt, updatedAt }: BoardColumnBoardResponse) {
+		this.id = id;
+		this.columnBoardId = columnBoardId;
+		this.title = title;
+		this.published = published;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+	}
+
+	@ApiProperty()
+	id: string;
+
+	@ApiProperty()
+	@DecodeHtmlEntities()
+	title: string;
+
+	@ApiProperty()
+	published: boolean;
+
+	@ApiProperty()
+	createdAt: Date;
+
+	@ApiProperty()
+	updatedAt: Date;
+
+	@ApiProperty()
+	columnBoardId: string;
+}
--- apps/server/src/modules/learnroom/service/rooms.service.ts
+++ apps/server/src/modules/learnroom/service/rooms.service.ts
@@ -1,22 +1,67 @@
 import { Injectable } from '@nestjs/common';
-import { Board, EntityId } from '@shared/domain';
+import {
+	Board,
+	BoardExternalReference,
+	BoardExternalReferenceType,
+	ContentElementType,
+	EntityId,
+	InputFormat,
+} from '@shared/domain';
 import { BoardRepo, LessonRepo } from '@shared/repo';
+import { CardService, ColumnBoardService, ColumnService, ContentElementService } from '@src/modules/board';
+import { RichTextContentBody } from '@src/modules/board/controller/dto';
 import { TaskService } from '@src/modules/task/service';
+import { ColumnBoardTargetService } from './column-board-target.service';
 
 @Injectable()
 export class RoomsService {
 	constructor(
 		private readonly taskService: TaskService,
 		private readonly lessonRepo: LessonRepo,
-		private readonly boardRepo: BoardRepo
+		private readonly boardRepo: BoardRepo,
+		private readonly columnBoardService: ColumnBoardService,
+		private readonly columnService: ColumnService,
+		private readonly cardService: CardService,
+		private readonly contentElementService: ContentElementService,
+		private readonly columnBoardTargetService: ColumnBoardTargetService
 	) {}
 
 	async updateBoard(board: Board, roomId: EntityId, userId: EntityId): Promise<Board> {
 		const [courseLessons] = await this.lessonRepo.findAllByCourseIds([roomId]);
 		const [courseTasks] = await this.taskService.findBySingleParent(userId, roomId);
-		board.syncTasksFromList(courseTasks);
-		board.syncLessonsFromList(courseLessons);
+
+		const courseReference = {
+			type: BoardExternalReferenceType.Course,
+			id: roomId,
+		};
+
+		const columnBoardIds = await this.columnBoardService.findIdsByExternalReference(courseReference);
+
+		if (columnBoardIds.length === 0) {
+			const columnBoard = await this.createWelcomeColumnBoard(courseReference);
+			columnBoardIds.push(columnBoard.id);
+		}
+
+		const courseColumnBoardTargets = await this.columnBoardTargetService.findOrCreateTargets(columnBoardIds);
+
+		const boardElementTargets = [...courseLessons, ...courseTasks, ...courseColumnBoardTargets];
+
+		board.syncBoardElementReferences(boardElementTargets);
+
 		await this.boardRepo.save(board);
 		return board;
+	}
+
+	private async createWelcomeColumnBoard(courseReference: BoardExternalReference) {
+		const columnBoard = await this.columnBoardService.create(courseReference);
+		const column = await this.columnService.create(columnBoard);
+		const card = await this.cardService.create(column);
+		await this.cardService.updateTitle(card, 'Willkommen');
+		const text = await this.contentElementService.create(card, ContentElementType.RICH_TEXT);
+		const content = new RichTextContentBody();
+		content.inputFormat = InputFormat.RICH_TEXT_CK5;
+		content.text = '<p>Dieses Board ist ein neues Feature und kann hier getestet werden</p>';
+		await this.contentElementService.update(text, content);
+		return columnBoard;
 	}
 }
