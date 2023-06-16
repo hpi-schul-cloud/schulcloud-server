a0e2adfb81a87bf630ff509d558f074026d7e6e4
Mrika Llabani <mrika.llabani@capgemini.com>
2023-06-16 08:40:21 +0200

    N21-857 uc test

---
+++ apps/server/src/modules/user-login-migration/uc/restart-user-login-migration.uc.spec.ts
@@ -0,0 +1,121 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { Logger } from '@src/core/logger';
+import { SchoolDO, UserLoginMigrationDO } from '@shared/domain';
+import { schoolDOFactory } from '@shared/testing';
+import { UserLoginMigrationService, RestartUserLoginMigrationValidationService } from '../service';
+import { RestartUserLoginMigrationUc } from './restart-user-login-migration.uc';
+import { StartUserLoginMigrationError } from '../error';
+
+describe('RestartUserLoginMigrationUc', () => {
+	let module: TestingModule;
+	let uc: RestartUserLoginMigrationUc;
+
+	let userLoginMigrationService: DeepMocked<UserLoginMigrationService>;
+	let restartUserLoginMigrationValidationService: DeepMocked<RestartUserLoginMigrationValidationService>;
+
+	beforeAll(async () => {
+		module = await Test.createTestingModule({
+			providers: [
+				RestartUserLoginMigrationUc,
+				{
+					provide: UserLoginMigrationService,
+					useValue: createMock<UserLoginMigrationService>(),
+				},
+				{
+					provide: RestartUserLoginMigrationValidationService,
+					useValue: createMock<RestartUserLoginMigrationValidationService>(),
+				},
+				{
+					provide: Logger,
+					useValue: createMock<Logger>(),
+				},
+			],
+		}).compile();
+
+		uc = module.get(RestartUserLoginMigrationUc);
+		userLoginMigrationService = module.get(UserLoginMigrationService);
+		restartUserLoginMigrationValidationService = module.get(RestartUserLoginMigrationValidationService);
+	});
+
+	afterAll(async () => {
+		await module.close();
+	});
+
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
+
+	describe('restartMigration', () => {
+		describe('when admin restarted migration successfully', () => {
+			const setup = () => {
+				const userId = 'userId';
+
+				const migration: UserLoginMigrationDO = new UserLoginMigrationDO({
+					schoolId: 'schoolId',
+					targetSystemId: 'targetSystemId',
+					startedAt: new Date(),
+					closedAt: new Date(),
+					finishedAt: new Date(),
+				});
+
+				const school: SchoolDO = schoolDOFactory.buildWithId();
+
+				restartUserLoginMigrationValidationService.checkPreconditions.mockResolvedValue(Promise.resolve());
+				userLoginMigrationService.restartMigration.mockResolvedValue(migration);
+
+				return { userId, migration, schoolId: school.id as string };
+			};
+
+			it('should check preconditions', async () => {
+				const { userId, schoolId } = setup();
+
+				await uc.restartMigration(userId, schoolId);
+
+				expect(restartUserLoginMigrationValidationService.checkPreconditions).toHaveBeenCalledWith(userId, schoolId);
+			});
+
+			it('should restart the migration ', async () => {
+				const { userId, schoolId } = setup();
+
+				await uc.restartMigration(userId, schoolId);
+
+				expect(userLoginMigrationService.restartMigration).toHaveBeenCalledWith(schoolId);
+			});
+
+			it('should return a UserLoginMigrationDO', async () => {
+				const { userId, schoolId, migration } = setup();
+
+				const result: UserLoginMigrationDO = await uc.restartMigration(userId, schoolId);
+
+				expect(result).toEqual(migration);
+			});
+		});
+
+		describe('when preconditions are not met', () => {
+			const setup = () => {
+				const userId = 'userId';
+
+				const migration: UserLoginMigrationDO = new UserLoginMigrationDO({
+					schoolId: 'schoolId',
+					targetSystemId: 'targetSystemId',
+					startedAt: new Date(),
+				});
+
+				const school: SchoolDO = schoolDOFactory.buildWithId();
+				const error: StartUserLoginMigrationError = new StartUserLoginMigrationError('');
+
+				restartUserLoginMigrationValidationService.checkPreconditions.mockRejectedValue(error);
+				userLoginMigrationService.restartMigration.mockResolvedValue(migration);
+
+				return { userId, migration, schoolId: school.id as string };
+			};
+
+			it('should throw ForbiddenException ', async () => {
+				const { userId, schoolId } = setup();
+
+				await expect(uc.restartMigration(userId, schoolId)).rejects.toThrow(new StartUserLoginMigrationError(''));
+			});
+		});
+	});
+});
