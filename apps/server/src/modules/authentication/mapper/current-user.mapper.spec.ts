20f5963a99d8ef4f7af8481cbf0e9f874533d2b3
Marvin Öhlerking <marvin.oehlerking@capgemini.com>
2023-06-19 13:35:50 +0200

    unused import

--- apps/server/src/modules/authentication/mapper/current-user.mapper.spec.ts
+++ apps/server/src/modules/authentication/mapper/current-user.mapper.spec.ts
@@ -1,5 +1,5 @@
 import { ValidationError } from '@shared/common';
-import { Permission, RoleName, RoleReference } from '@shared/domain';
+import { Permission, RoleName } from '@shared/domain';
 import { UserDO } from '@shared/domain/domainobject/user.do';
 import { roleFactory, schoolFactory, setupEntities, userDoFactory, userFactory } from '@shared/testing';
 import { ICurrentUser } from '../interface';
