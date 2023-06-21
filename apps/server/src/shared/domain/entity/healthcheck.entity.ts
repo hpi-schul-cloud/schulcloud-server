9f657bdfff0b5eb6b18fb1a9a6f9fa5ca937dde9
Bartosz Nowicki <bartosz.nowicki@gca.pass-consulting.com>
2023-06-22 00:27:11 +0200

    add interface for the healthcheck props

--- apps/server/src/shared/domain/entity/healthcheck.entity.ts
+++ apps/server/src/shared/domain/entity/healthcheck.entity.ts
@@ -1,12 +1,24 @@
 import { Entity, PrimaryKeyType, PrimaryKey, Property } from '@mikro-orm/core';
 
+import { EntityId } from '@shared/domain';
+
+export interface HealthcheckProps {
+	id: EntityId;
+	updatedAt: Date;
+}
+
 @Entity({ tableName: 'healthchecks' })
 export class Healthcheck {
 	[PrimaryKeyType]?: string;
 
 	@PrimaryKey({ name: '_id' })
-	id!: string;
+	id!: EntityId;
 
 	@Property()
 	updatedAt!: Date;
+
+	constructor(props: HealthcheckProps) {
+		this.id = props.id;
+		this.updatedAt = props.updatedAt;
+	}
 }
