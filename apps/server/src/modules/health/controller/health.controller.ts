2407d2a26d99fe20b6a90d7eebd39c4e9be9a0cd
Bartosz Nowicki <bartosz.nowicki@gca.pass-consulting.com>
2023-06-24 22:31:05 +0200

    add health module controller

---
+++ apps/server/src/modules/health/controller/health.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
+import { Response } from 'express';
+
+import { HealthUc } from '../uc';
+import { HealthStatusResponse, HealthStatusResponseMapper } from './dto';
+
+@Controller('health')
+export class HealthController {
+	constructor(private readonly healthUc: HealthUc) {}
+
+	private readonly contentTypeApplicationHealthJSON = 'application/health+json';
+
+	@Get('self')
+	getSelfHealth(@Res({ passthrough: true }) res: Response): HealthStatusResponse {
+		res.contentType(this.contentTypeApplicationHealthJSON);
+
+		const healthStatus = this.healthUc.checkSelfHealth();
+
+		return HealthStatusResponseMapper.mapToResponse(healthStatus);
+	}
+
+	@Get()
+	async getOverallHealth(@Res({ passthrough: true }) res: Response): Promise<HealthStatusResponse> {
+		res.contentType(this.contentTypeApplicationHealthJSON);
+
+		const healthStatus = await this.healthUc.checkOverallHealth();
+
+		if (healthStatus.isPassed()) {
+			res.status(HttpStatus.OK);
+		} else {
+			res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		return HealthStatusResponseMapper.mapToResponse(healthStatus);
+	}
+}
