5d83fd0c1f11c550f107a419ee97839f05dc4437
Bartosz Nowicki <bartosz.nowicki@gca.pass-consulting.com>
2023-06-24 13:23:17 +0200

    add HEALTHCHECKS_EXCLUDE_MONGODB_READ_OP_TIME_CHECK env variable that allows for excluding MongoDB read operation time check, add internal config class for the health module

---
+++ apps/server/src/modules/health/health.config.spec.ts
@@ -0,0 +1,37 @@
+import { IConfig } from '@hpi-schul-cloud/commons/lib/interfaces/IConfig';
+import { Configuration } from '@hpi-schul-cloud/commons';
+
+import { HealthConfig } from './health.config';
+
+describe(HealthConfig.name, () => {
+	describe('singleton instance', () => {
+		let configBefore: IConfig;
+
+		beforeAll(() => {
+			configBefore = Configuration.toObject({ plainSecrets: true });
+		});
+
+		beforeEach(() => {
+			Configuration.reset(configBefore);
+		});
+
+		afterAll(() => {
+			Configuration.reset(configBefore);
+		});
+
+		describe('should have correct default value for the', () => {
+			it("'excludeMongoDBReadOpTimeCheck' toggle", () => {
+				expect(HealthConfig.instance.excludeMongoDBReadOpTimeCheck).toBe(false);
+			});
+		});
+
+		describe('should have correct value loaded from the configuration for the', () => {
+			it("'excludeMongoDBReadOpTimeCheck' toggle", () => {
+				Configuration.set('HEALTHCHECKS_EXCLUDE_MONGODB_READ_OP_TIME_CHECK', true);
+				HealthConfig.reload();
+
+				expect(HealthConfig.instance.excludeMongoDBReadOpTimeCheck).toBe(true);
+			});
+		});
+	});
+});
---
+++ apps/server/src/modules/health/health.config.ts
@@ -0,0 +1,29 @@
+import { Configuration } from '@hpi-schul-cloud/commons';
+
+export class HealthConfig {
+	private static _instance: HealthConfig;
+
+	private readonly _exclude_mongodb_read_op_time_check: boolean;
+
+	get excludeMongoDBReadOpTimeCheck(): boolean {
+		return this._exclude_mongodb_read_op_time_check;
+	}
+
+	private constructor() {
+		this._exclude_mongodb_read_op_time_check = Configuration.get(
+			'HEALTHCHECKS_EXCLUDE_MONGODB_READ_OP_TIME_CHECK'
+		) as boolean;
+	}
+
+	public static get instance() {
+		if (this._instance === undefined) {
+			this._instance = new this();
+		}
+
+		return this._instance;
+	}
+
+	public static reload() {
+		this._instance = new this();
+	}
+}
