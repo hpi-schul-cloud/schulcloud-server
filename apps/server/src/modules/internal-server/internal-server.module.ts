1dcf5edf74feaabe388cf617b2ecacc528feaebb
Bartosz Nowicki <bartosz.nowicki@gca.pass-consulting.com>
2023-06-24 22:41:52 +0200

    add internal server module

---
+++ apps/server/src/modules/internal-server/internal-server.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+
+import { DB_URL, DB_USERNAME, DB_PASSWORD } from '@src/config';
+import { HealthApiModule, HealthEntities } from '@src/modules/health';
+
+@Module({
+	imports: [
+		MikroOrmModule.forRoot({
+			type: 'mongo',
+			clientUrl: DB_URL,
+			password: DB_PASSWORD,
+			user: DB_USERNAME,
+			entities: [...HealthEntities],
+			// debug: true, // use it only for the local queries debugging
+		}),
+		HealthApiModule,
+	],
+})
+export class InternalServerModule {}
