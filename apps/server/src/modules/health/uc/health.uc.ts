b3c6c70388d675ecefcf398bdafc576764c6f7bf
Bartosz Nowicki <bartosz.nowicki@gca.pass-consulting.com>
2023-06-27 00:48:00 +0200

    update health module use case that includes db healthcheck to: 1) add hostname info to the healthcheck ID, 2) contain modified MongoDB healthcheck operation name (as it will be an upsert, not find operation from now on)

--- apps/server/src/modules/health/uc/health.uc.ts
+++ apps/server/src/modules/health/uc/health.uc.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 
-import { Configuration } from '@hpi-schul-cloud/commons';
 import { HealthService } from '../service';
 import { HealthConfig } from '../health.config';
 import { HealthStatuses, HealthStatusCheck, HealthStatus } from '../domain';
@@ -9,7 +8,7 @@ const selfOnlyHealthDescription = 'Service health status (self-only)';
 
 const overallHealthDescription = 'Service health status (overall)';
 
-const mongoDBReadOperationTime = 'mongoDB:readOperationTime';
+const mongoDBUpsertOperationTime = 'mongoDB:upsertOperationTime';
 
 const datastoreComponentType = 'datastore';
 
@@ -28,15 +27,7 @@ function hasMessage(error: unknown): error is { message: string } {
 
 @Injectable()
 export class HealthUc {
-	private readonly mongoDBReadOpTimeHealthcheckEnabled: boolean = true;
-
-	constructor(private readonly healthService: HealthService) {
-		if (Configuration.has('HEALTHCHECKS_MONGODB_READ_OP_TIME_ENABLED')) {
-			this.mongoDBReadOpTimeHealthcheckEnabled = Configuration.get(
-				'HEALTHCHECKS_MONGODB_READ_OP_TIME_ENABLED'
-			) as boolean;
-		}
-	}
+	constructor(private readonly healthService: HealthService) {}
 
 	checkSelfHealth(): HealthStatus {
 		// This health check verifies just the correct module setup and doesn't include
@@ -53,12 +44,11 @@ export class HealthUc {
 		// The below check allows for turning off the MongoDB dependency on the healthcheck -
 		// it shouldn't be typically used, but if this healthcheck will be used e.g. in the k8s
 		// liveness or readiness probes and, for any reason, there would be a need to stop
-		// including MongoDB read operation time check in the overall API health checks, the
-		// HEALTHCHECKS_EXCLUDE_MONGODB config var can be set to 'true' to disable it. This way,
-		// as currently only this single MongoDB read operation time check is included in the
-		// overall API health checks, the whole health check will not perform any additional
-		// checks on any of the 3rd party services and thus will behave like the self-only API
-		// health check.
+		// including MongoDB check in the overall API health checks, the HEALTHCHECKS_EXCLUDE_MONGODB
+		// config var can be set to 'true' to disable it. This way, as currently only this single
+		// MongoDB check is included in the overall API health checks, the whole health check will
+		// not perform any additional checks on any of the 3rd party services and thus will behave
+		// like the self-only API health check.
 		if (HealthConfig.instance.excludeMongoDB) {
 			return new HealthStatus({
 				status: HealthStatuses.STATUS_PASS,
@@ -66,18 +56,22 @@ export class HealthUc {
 			});
 		}
 
-		const readOperationDate = new Date();
+		const upsertOperationDate = new Date();
 		const startTime = performance.now();
 
 		try {
-			// The data returned by the find operation is currently not used so it's not
-			// saved into any variable - we just want to check if the find operation has
-			// been performed successfully (in a sense that no error occurred).
-			await this.healthService.upsertHealthcheckById('api-db-healthcheck');
+			let healthcheckID = 'db-healthcheck';
+
+			// If hostname is available in the health module
+			// config, append it to the healthcheck ID.
+			if (HealthConfig.instance.hostname !== '') {
+				healthcheckID += `-${HealthConfig.instance.hostname}`;
+			}
+
+			await this.healthService.upsertHealthcheckById(healthcheckID);
 		} catch (error) {
-			// If any error occurred in the find operation execution on the database,
-			// it should be indicated as a MongoDB read operation time check failure
-			// (and thus the whole healthcheck should fail).
+			// If any error occurred in the database operation execution it should be indicated
+			// as a MongoDB check failure (and thus the whole healthcheck should fail).
 
 			const endTime = performance.now();
 
@@ -91,16 +85,16 @@ export class HealthUc {
 
 			return new HealthStatus({
 				status: HealthStatuses.STATUS_FAIL,
-				output: `'${mongoDBReadOperationTime}' check error: ${errorMessage}`,
+				output: `'${mongoDBUpsertOperationTime}' check error: ${errorMessage}`,
 				description: overallHealthDescription,
 				checks: {
-					[mongoDBReadOperationTime]: [
+					[mongoDBUpsertOperationTime]: [
 						new HealthStatusCheck({
 							componentType: datastoreComponentType,
 							observedValue: endTime - startTime,
 							observedUnit: observedUnitMs,
 							status: HealthStatuses.STATUS_FAIL,
-							time: readOperationDate,
+							time: upsertOperationDate,
 							output: errorMessage,
 						}),
 					],
@@ -110,22 +104,19 @@ export class HealthUc {
 
 		const endTime = performance.now();
 
-		// Prepare the health status as passed, whether the healthcheck document was present
-		// in the database or not. The only case it should fail is the database connectivity
-		// error as a plain 'find' operation is used within the healthchecks repository internally
-		// (not 'find or fail') so the not found (null) document is also acceptable in case of
-		// a read operation time check.
+		// Prepare the passed health status with
+		// the performed upsert operation time.
 		return new HealthStatus({
 			status: HealthStatuses.STATUS_PASS,
 			description: overallHealthDescription,
 			checks: {
-				[mongoDBReadOperationTime]: [
+				[mongoDBUpsertOperationTime]: [
 					new HealthStatusCheck({
 						componentType: datastoreComponentType,
 						observedValue: endTime - startTime,
 						observedUnit: observedUnitMs,
 						status: HealthStatuses.STATUS_PASS,
-						time: readOperationDate,
+						time: upsertOperationDate,
 					}),
 				],
 			},
