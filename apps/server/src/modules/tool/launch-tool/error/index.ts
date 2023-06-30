c9846fb025263d530be196b6b2ae11da16829cd0
Arne Gnisa <arne.gnisa@capgemini.com>
2023-06-30 13:23:24 +0200

    N21-1022 adds error when launching outdated tools

---
+++ apps/server/src/modules/tool/launch-tool/error/index.ts
@@ -0,0 +1,1 @@
+export * from './tool-stauts-outdated.loggable-exception';
---
+++ apps/server/src/modules/tool/launch-tool/error/tool-stauts-outdated.loggable-exception.ts
@@ -0,0 +1,21 @@
+import { BadRequestException } from '@nestjs/common';
+import { EntityId } from '@shared/domain';
+import { ErrorLogMessage, Loggable, LogMessage, ValidationErrorLogMessage } from '@src/core/logger';
+
+export class ToolStatusOutdatedLoggableException extends BadRequestException implements Loggable {
+	constructor(private readonly userId: EntityId, private readonly toolId: EntityId) {
+		super({});
+	}
+
+	getLogMessage(): LogMessage | ErrorLogMessage | ValidationErrorLogMessage {
+		return {
+			type: 'TOOL_STATUS_OUTDATED',
+			message: 'The status of the tool is outdated and cannot be launched by the user.',
+			stack: this.stack,
+			data: {
+				userId: this.userId,
+				toolId: this.toolId,
+			},
+		};
+	}
+}
