258e589835c13bf7afdc27a35cfca2d349ff5f86
wolfganggreschus <wolfgang@greschus.de>
2023-07-10 11:48:26 +0200

    removed unneccessary comment

--- apps/server/src/modules/board/controller/dto/card/card-list.response.ts
+++ apps/server/src/modules/board/controller/dto/card/card-list.response.ts
@@ -1,7 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { CardResponse } from './card.response';
 
-// TODO change this to ListCardResponse
 export class CardListResponse {
 	constructor({ data }: CardListResponse) {
 		this.data = data;
