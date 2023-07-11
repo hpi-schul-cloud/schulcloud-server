dff661f2573fd9a83926f32eab0650677830a3b1
Arne Gnisa <arne.gnisa@capgemini.com>
2023-07-11 14:22:47 +0200

    N21-1016 implemented for both oauth redirect ways

--- apps/server/src/modules/oauth/error/user-not-found-in-unprovisioned-school.loggable-exception.ts
+++ apps/server/src/modules/oauth/error/user-not-found-in-unprovisioned-school.loggable-exception.ts
@@ -3,12 +3,11 @@ import { OAuthSSOError } from './oauth-sso.error';
 
 export class UserNotFoundInUnprovisionedSchoolLoggableException extends OAuthSSOError implements Loggable {
 	constructor(private readonly externalUserId: string, private readonly systemId: string) {
-		super(undefined, 'sso_user_notfound_in_unprovisioned_school');
+		super('Provisioning of user with externalId: externalUserId failed', 'sso_user_notfound_in_unprovisioned_school');
 	}
 
 	getLogMessage(): LogMessage | ErrorLogMessage | ValidationErrorLogMessage {
 		return {
-			type: 'USER_NOT_FOUND_IN_UNPROVISIONED_SCHOOL',
 			message: 'The user could not be found because the school was not provisioned.',
 			stack: this.stack,
 			data: {
