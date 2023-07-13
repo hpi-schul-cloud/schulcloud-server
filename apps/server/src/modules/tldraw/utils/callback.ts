9a5bdb2b8fadae2d6149e6443491c022666d3a8a
Błażej Szczepanowski <blazej1679@gmail.com>
2023-07-14 00:34:57 +0200

    Change response callback to axios

--- apps/server/src/modules/tldraw/utils/callback.ts
+++ apps/server/src/modules/tldraw/utils/callback.ts
@@ -1,6 +1,8 @@
 import * as http from "http";
+import axios, { AxiosResponse } from 'axios';
+import lodash from "lodash";
 import { Configuration } from '@hpi-schul-cloud/commons';
-
+import { Timeout, GeneralError } from '@feathersjs/errors';
 const CALLBACK_URL = Configuration.get('FEATURE_TLDRAW_CALLBACK_URL') as string ? new URL(Configuration.get('FEATURE_TLDRAW_CALLBACK_URL')) : null;
 const CALLBACK_TIMEOUT = Configuration.get('FEATURE_TLDRAW_CALLBACK_TIMEOUT') ?? 5000;
 const CALLBACK_OBJECTS = Configuration.get('FEATURE_TLDRAW_CALLBACK_OBJ') ? JSON.parse(Configuration.get('FEATURE_TLDRAW_CALLBACK_OBJ')) : {};
@@ -36,6 +38,8 @@ export const callbackHandler = (update, origin, doc) => {
  */
 const callbackRequest = (url, timeout, data) => {
 	data = JSON.stringify(data);
+	const axiosClient = axios.create();
+
 	const options = {
 		hostname: url.hostname,
 		port: url.port,
@@ -47,17 +51,51 @@ const callbackRequest = (url, timeout, data) => {
 			'Content-Length': data.length
 		}
 	};
-	const req = http.request(options);
-	req.on('timeout', () => {
-		console.warn('Callback request timed out.');
-		req.abort();
-	})
-	req.on('error', (e) => {
-		console.error('Callback request error.', e);
-		req.abort();
-	})
-	req.write(data);
-	req.end();
+
+	const requestOptions = lodash.merge({ ...url, ...options });
+	axiosClient.request(requestOptions).then(transformResponse).catch(transformErrorResponse);
+}
+
+/**
+ * Take axios response and convert into internal response format
+ * @private
+ * @param config
+ * @param data
+ * @param headers
+ * @param request
+ * @param status
+ * @param statusText
+ */
+function transformResponse({ config, data, headers, request, status, statusText }: AxiosResponse) {
+	// return internal response format
+	return {
+		config,
+		data,
+		headers,
+		request,
+		status,
+		statusText,
+	};
+}
+
+/**
+ * Transform axios response to internal format
+ * @private
+ */
+function transformErrorResponse(error) {
+	if (error.response) {
+		// we got a non-200 response which can be responded but will still be thrown
+		throw this.transformResponse(error.response);
+	}
+	if (error.request) {
+		// The request was made but no response was received
+		// `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+		// http.ClientRequest in node.js
+		throw new Timeout('external api request timeout');
+	} else {
+		// Something happened in setting up the request that triggered an Error
+		throw new GeneralError('external api reuqest error', error);
+	}
 }
 
 /**
