fbcc720cc84ea2f67c094d403ee0f3e39dcddfa0
Malte Berg <malte.berg@capgemini.com>
2023-07-19 14:13:08 +0200

    change custom-parameter-type

--- apps/server/src/shared/domain/entity/tools/external-tool/custom-parameter/custom-parameter-type.enum.ts
+++ apps/server/src/shared/domain/entity/tools/external-tool/custom-parameter/custom-parameter-type.enum.ts
@@ -2,7 +2,7 @@ export enum CustomParameterType {
 	STRING = 'string',
 	NUMBER = 'number',
 	BOOLEAN = 'boolean',
-	AUTO_COURSEID = 'auto_courseid',
-	AUTO_COURSENAME = 'auto_coursename',
+	AUTO_CONTEXTID = 'auto_contextid',
+	AUTO_CONTEXTNAME = 'auto_contextname',
 	AUTO_SCHOOLID = 'auto_schoolid',
 }
