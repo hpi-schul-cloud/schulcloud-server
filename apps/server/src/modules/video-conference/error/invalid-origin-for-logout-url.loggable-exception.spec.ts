afc9dbe1efff672860c291e690998301ebefbcee
Marvin Öhlerking <marvin.oehlerking@capgemini.com>
2023-07-24 14:17:47 +0200

    add logoutUrl as parameter to start meeting

---
+++ apps/server/src/modules/video-conference/error/invalid-origin-for-logout-url.loggable-exception.spec.ts
@@ -0,0 +1,29 @@
+import { InvalidOriginForLogoutUrlLoggableException } from './invalid-origin-for-logout-url.loggable-exception';
+
+describe('InvalidOriginForLogoutUrlLoggableException', () => {
+	describe('getLogMessage', () => {
+		const setup = () => {
+			const exception = new InvalidOriginForLogoutUrlLoggableException('logoutUrl', 'origin');
+
+			return {
+				exception,
+			};
+		};
+
+		it('should log the correct message', () => {
+			const { exception } = setup();
+
+			const result = exception.getLogMessage();
+
+			expect(result).toEqual({
+				type: 'INVALID_ORIGIN_FOR_LOGOUT_URL',
+				message: expect.any(String),
+				stack: expect.any(String),
+				data: {
+					received: 'logoutUrl',
+					expected: 'origin',
+				},
+			});
+		});
+	});
+});
