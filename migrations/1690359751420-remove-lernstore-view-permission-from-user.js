13ec33a20f08c4962ab46dbc4f7c715b77713882
davwas <daw.wasik@gmail.com>
2023-07-26 12:17:51 +0200

    add new migration to remove LERNSTORE_VIEW permission from user role

---
+++ migrations/1690359751420-remove-lernstore-view-permission-from-user.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+// eslint-disable-next-line no-unused-vars
+const { alert, error } = require('../src/logger');
+
+const { connect, close } = require('../src/utils/database');
+
+const Roles = mongoose.model(
+	'roles1690359751420',
+	new mongoose.Schema(
+		{
+			name: { type: String, required: true },
+			permissions: [{ type: String }],
+		},
+		{
+			timestamps: true,
+		}
+	),
+	'roles'
+);
+
+// TODO update seed data and remove this line
+
+module.exports = {
+	up: async function up() {
+		await connect();
+
+		await Roles.updateOne(
+			{ name: 'user' },
+			{
+				$pull: {
+					permissions: {
+						$in: ['LERNSTORE_VIEW'],
+					},
+				},
+			}
+		).exec();
+		alert(`Permission LERNSTORE_VIEW removed from the user role`);
+
+		await close();
+	},
+
+	down: async function down() {
+		await connect();
+
+		await Roles.updateOne(
+			{ name: 'user' },
+			{
+				$addToSet: {
+					permissions: {
+						$each: ['LERNSTORE_VIEW'],
+					},
+				},
+			}
+		).exec();
+		alert(`Permission LERNSTORE_VIEW added to the user role`);
+
+		await close();
+	},
+};
