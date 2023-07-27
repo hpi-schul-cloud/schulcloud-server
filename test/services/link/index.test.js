45608002bf52307ed8b3878dc96316daaceebc7d
Arne Gnisa <arne.gnisa@capgemini.com>
2023-07-27 11:52:50 +0200

    N21-906 test fixing failing feathers test with freeport 2

--- test/services/link/index.test.js
+++ test/services/link/index.test.js
@@ -9,21 +9,24 @@ chai.use(chaiHttp);
 
 describe('link service', () => {
 	let app;
-	let freePort;
+	let port;
 	let server;
 	let service;
 
 	before(async () => {
-		freeport(async (err, port) => {
-			if (err) {
-				logger.warning('freeport:', err);
-			}
-
-			app = await appPromise();
-			server = await app.listen(port);
-			freePort = port;
-			service = app.service('link');
+		port = await new Promise((resolve, reject) => {
+			freeport((err, freePort) => {
+				if (err) {
+					logger.error('freeport:', err);
+					reject(err);
+				}
+				resolve(freePort);
+			});
 		});
+
+		app = await appPromise();
+		server = await app.listen(port);
+		service = app.service('link');
 	});
 
 	after(async () => {
@@ -35,10 +38,12 @@ describe('link service', () => {
 	});
 
 	it(`generates a link that has the correct target set`, async () => {
-		const url = `localhost:${freePort}/`;
+		const url = `http://localhost:${port}/`;
+
 		const data = await service.create({ target: url });
 		chai.expect(data._id).to.have.lengthOf(service.Model.linkLength);
 		chai.expect(data.target).to.equal(url);
+
 		const result = await chai.request(app).get(`/link/${data._id}`);
 		chai.expect(result.redirects[0]).to.equal(url);
 	});
