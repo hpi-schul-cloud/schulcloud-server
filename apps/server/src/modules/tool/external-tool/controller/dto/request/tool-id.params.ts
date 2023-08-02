73ac60f95af92e20d17be982f24c45e53cc1e7b7
Malte Berg <malte.berg@capgemini.com>
2023-08-02 14:54:43 +0200

    adjust ToolIdParams

--- apps/server/src/modules/tool/external-tool/controller/dto/request/tool-id.params.ts
+++ apps/server/src/modules/tool/external-tool/controller/dto/request/tool-id.params.ts
@@ -4,5 +4,5 @@ import { ApiProperty } from '@nestjs/swagger';
 export class ToolIdParams {
 	@IsMongoId()
 	@ApiProperty({ nullable: false, required: true })
-	toolId!: string;
+	externalToolId!: string;
 }
