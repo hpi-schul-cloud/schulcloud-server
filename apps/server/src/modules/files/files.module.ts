d65fd35ad246ebc9bb8d57b09e5969f3aa445d93
Bartosz Nowicki <bartosz.nowicki@gca.pass-consulting.com>
2023-08-04 21:19:31 +0200

    add barrel file for the files deletion console job

--- apps/server/src/modules/files/files.module.ts
+++ apps/server/src/modules/files/files.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { StorageProviderRepo } from '@shared/repo/storageprovider/storageprovider.repo';
 import { LoggerModule } from '@src/core/logger';
 
-import { DeleteFilesConsole } from './job/delete-files.console';
+import { DeleteFilesConsole } from './job';
 import { DeleteFilesUc } from './uc';
 import { FilesRepo } from './repo';
 
