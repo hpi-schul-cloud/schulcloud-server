9ad1a3930ec28c1ee47e9a701fc6e845726833d6
Igor Richter <igor.richter@capgemini.com>
2023-08-07 12:02:59 +0200

    request changes 2

--- apps/server/src/modules/tool/common/common-tool.module.ts
+++ apps/server/src/modules/tool/common/common-tool.module.ts
@@ -3,11 +3,24 @@ import { ContextExternalToolRepo, SchoolExternalToolRepo } from '@shared/repo';
 import { LoggerModule } from '@src/core/logger';
 import { CommonToolService, CommonToolValidationService } from './service';
 import { AuthorizationModule } from '../../authorization';
+import { ToolPermissionHelper } from './uc/tool-permission-helper';
 
 @Module({
 	imports: [LoggerModule, AuthorizationModule],
 	// TODO: make deletion of entities cascading, adjust ExternalToolService.deleteExternalTool and remove the repos from here
-	providers: [CommonToolService, CommonToolValidationService, SchoolExternalToolRepo, ContextExternalToolRepo],
-	exports: [CommonToolService, CommonToolValidationService, SchoolExternalToolRepo, ContextExternalToolRepo],
+	providers: [
+		ToolPermissionHelper,
+		CommonToolService,
+		CommonToolValidationService,
+		SchoolExternalToolRepo,
+		ContextExternalToolRepo,
+	],
+	exports: [
+		ToolPermissionHelper,
+		CommonToolService,
+		CommonToolValidationService,
+		SchoolExternalToolRepo,
+		ContextExternalToolRepo,
+	],
 })
 export class CommonToolModule {}
--- apps/server/src/modules/tool/common/uc/tool-permission-helper.ts
+++ apps/server/src/modules/tool/common/uc/tool-permission-helper.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { EntityId } from '@shared/domain';
-import { AuthorizableReferenceType, AuthorizationContext, AuthorizationService } from '@src/modules';
+import { EntityId, User } from '@shared/domain';
+import { AuthorizationContext, AuthorizationService } from '@src/modules';
 import { ContextExternalTool } from '../../context-external-tool/domain';
-import { ContextTypeMapper } from '../mapper';
 
 @Injectable()
 export class ToolPermissionHelper {
@@ -13,20 +12,7 @@ export class ToolPermissionHelper {
 		contextExternalTool: ContextExternalTool,
 		context: AuthorizationContext
 	): Promise<void> {
-		if (contextExternalTool.id) {
-			await this.authorizationService.checkPermissionByReferences(
-				userId,
-				AuthorizableReferenceType.ContextExternalToolEntity,
-				contextExternalTool.id,
-				context
-			);
-		}
-
-		await this.authorizationService.checkPermissionByReferences(
-			userId,
-			ContextTypeMapper.mapContextTypeToAllowedAuthorizationEntityType(contextExternalTool.contextRef.type),
-			contextExternalTool.contextRef.id,
-			context
-		);
+		const user: User = await this.authorizationService.getUserWithPermissions(userId);
+		this.authorizationService.checkPermission(user, contextExternalTool, context);
 	}
 }
