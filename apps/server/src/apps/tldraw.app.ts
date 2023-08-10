b83e0957a42167e6058275df022ff38ffc9e5784
davwas <daw.wasik@gmail.com>
2023-08-10 20:11:30 +0200

    fix tldraw app start

--- apps/server/src/apps/tldraw.app.ts
+++ apps/server/src/apps/tldraw.app.ts
@@ -3,39 +3,31 @@
 import { NestFactory } from '@nestjs/core';
 import { install as sourceMapInstall } from 'source-map-support';
 import { TldrawModule } from '@src/modules/tldraw';
-import { LegacyLogger } from '@src/core/logger';
+import { Logger } from '@src/core/logger';
 import * as WebSocket from 'ws';
 import { WsAdapter } from '@nestjs/platform-ws';
-import express from 'express';
-import { ExpressAdapter } from '@nestjs/platform-express';
 import { enableOpenApiDocs } from '@shared/controller/swagger';
+import { AppStartLoggable } from '@src/apps/helpers/app-start-loggable';
 
 async function bootstrap() {
 	sourceMapInstall();
-	const nestExpress = express();
 
-	const nestExpressAdapter = new ExpressAdapter(nestExpress);
-	const nestApp = await NestFactory.create(TldrawModule, nestExpressAdapter);
-
+	const nestApp = await NestFactory.create(TldrawModule);
 	const wss = new WebSocket.Server({ noServer: true });
-	nestApp.useLogger(await nestApp.resolve(LegacyLogger));
 	nestApp.useWebSocketAdapter(new WsAdapter(wss));
 	nestApp.enableCors();
 	enableOpenApiDocs(nestApp, 'docs');
-	await nestApp.init();
 
-	const rootExpress = express();
-	const basePath = '/';
-	const port = 4449;
+	const logger = await nestApp.resolve(Logger);
+	const port = 3344;
+	await nestApp.listen(port);
 
-	rootExpress.use(basePath, nestExpress);
-	rootExpress.listen(port);
-
-	console.log('##########################################');
-	console.log(`### Tldraw Server            			###`);
-	console.log(`### App Port: ${port}                  ###`);
-	console.log(`### WebSocket Gateway Port: ${3345}    ###`);
-	console.log('##########################################');
+	logger.info(
+		new AppStartLoggable({
+			appName: 'Tldraw server app',
+			port,
+		})
+	);
 }
 
 void bootstrap();
