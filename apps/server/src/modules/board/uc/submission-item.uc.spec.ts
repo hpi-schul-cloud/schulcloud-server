4b513d9ea937284cbfa32801fc2b0b556bc7bd32
Omar Ezzat <omar.ezzat@dataport.de>
2023-08-14 13:58:01 +0200

    wip

--- apps/server/src/modules/board/uc/submission-item.uc.spec.ts
+++ apps/server/src/modules/board/uc/submission-item.uc.spec.ts
@@ -1,12 +1,11 @@
 import { createMock, DeepMocked } from '@golevelup/ts-jest';
 import { Test, TestingModule } from '@nestjs/testing';
 import { BoardDoAuthorizable } from '@shared/domain';
-import { setupEntities } from '@shared/testing';
+import { setupEntities, submissionContainerElementFactory, submissionItemFactory, userFactory } from '@shared/testing';
 import { Logger } from '@src/core/logger';
 import { AuthorizationService } from '@src/modules/authorization';
 import { ObjectId } from 'bson';
 import { BoardDoAuthorizableService, ContentElementService } from '../service';
-import { ElementUc } from './element.uc';
 import { SubmissionItemUc } from './submission-item.uc';
 
 describe(SubmissionItemUc.name, () => {
@@ -19,7 +18,7 @@ describe(SubmissionItemUc.name, () => {
 	beforeAll(async () => {
 		module = await Test.createTestingModule({
 			providers: [
-				ElementUc,
+				SubmissionItemUc,
 				{
 					provide: AuthorizationService,
 					useValue: createMock<AuthorizationService>(),
@@ -55,8 +54,31 @@ describe(SubmissionItemUc.name, () => {
 	});
 
 	describe('findSubmissionItems', () => {
-		const setup = () => {
-			// TODO
-		};
+		describe('with two students having submission items', () => {
+			const setup = () => {
+				const user1 = userFactory.build();
+				const user2 = userFactory.build();
+				const submissionItemEl1 = submissionItemFactory.build({
+					userId: user1.id,
+				});
+				const submissionItemEl2 = submissionItemFactory.build({
+					userId: user2.id,
+				});
+				const submissionContainerEl = submissionContainerElementFactory.build({
+					children: [submissionItemEl1, submissionItemEl2],
+				});
+
+				const elementSpy = elementService.findById.mockResolvedValue(submissionContainerEl);
+
+				return { submissionContainerEl, submissionItemEl1, user1, elementSpy };
+			};
+
+			it('student1 only gets their own submission item', async () => {
+				const { user1, submissionContainerEl, submissionItemEl1 } = setup();
+				const items = await uc.findSubmissionItems(user1.id, submissionContainerEl.id);
+				expect(items.length).toBe(1);
+				expect(items[0]).toStrictEqual(submissionItemEl1);
+			});
+		});
 	});
 });
