e31ecf616696b4e8eced998b3686790d2dc9b950
Bartosz Nowicki <bartosz.nowicki@gca.pass-consulting.com>
2023-08-18 12:37:23 +0200

    remove unused user file factory reference

--- apps/server/src/shared/domain/entity/submission.entity.spec.ts
+++ apps/server/src/shared/domain/entity/submission.entity.spec.ts
@@ -2,7 +2,6 @@ import { ObjectId } from '@mikro-orm/mongodb';
 import { InternalServerErrorException } from '@nestjs/common';
 import {
 	courseGroupFactory,
-	userFileFactory,
 	schoolFactory,
 	setupEntities,
 	submissionFactory,
@@ -25,10 +24,9 @@ describe('Submission entity', () => {
 			const school = schoolFactory.build();
 			const student = userFactory.build();
 			const task = taskFactory.buildWithId();
-			const file = userFileFactory.buildWithId();
 			const teamMember = userFactory.build();
 
-			return { school, student, task, file, teamMember };
+			return { school, student, task, teamMember };
 		};
 
 		describe('when required pros are set', () => {
