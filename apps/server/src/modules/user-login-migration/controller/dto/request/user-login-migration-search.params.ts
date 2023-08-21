0d048c32f1a726e5c329d5c1e97b932eaa062c5b
Arne Gnisa <arne.gnisa@capgemini.com>
2023-08-21 13:06:45 +0200

    N21-844 fixes linter

--- apps/server/src/modules/user-login-migration/controller/dto/request/user-login-migration-search.params.ts
+++ apps/server/src/modules/user-login-migration/controller/dto/request/user-login-migration-search.params.ts
@@ -1,5 +1,5 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsBoolean, IsOptional, IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class UserLoginMigrationSearchParams {
 	@ApiPropertyOptional()
