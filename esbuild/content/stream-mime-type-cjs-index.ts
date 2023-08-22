23dadae8832bd3fb1a87d6c655a81848b3042927
SevenWaysDP <sergej.hoffmann@dataport.de>
2023-08-22 11:16:31 +0200

    BC-4750 - add types

--- esbuild/content/stream-mime-type-cjs-index.ts
+++ esbuild/content/stream-mime-type-cjs-index.ts
@@ -1,3 +1,41 @@
-import * as StreamMimeType from 'stream-mime-type';
-
-export default StreamMimeType;
+export interface GetMimeTypeOptionsStrict {
+	strict: true;
+	filename?: string;
+}
+export interface GetMimeTypeOptionsNoStrict {
+	strict?: false;
+	filename?: string;
+}
+export type GetMimeTypeOptions = GetMimeTypeOptionsStrict | GetMimeTypeOptionsNoStrict;
+export interface GetMimeTypeResultStrict {
+	mime: string | undefined;
+	stream: undefined;
+}
+export interface GetMimeTypeResult {
+	mime: string;
+	stream: undefined;
+}
+export interface GetMimeTypeResultStrictWithStream {
+	mime: string | undefined;
+	stream: NodeJS.ReadableStream;
+}
+export interface GetMimeTypeResultWithStream {
+	mime: string;
+	stream: NodeJS.ReadableStream;
+}
+export declare function getMimeType(
+	data: NodeJS.ReadableStream,
+	options: GetMimeTypeOptionsStrict
+): Promise<GetMimeTypeResultStrictWithStream>;
+export declare function getMimeType(
+	data: NodeJS.ReadableStream,
+	options?: GetMimeTypeOptionsNoStrict
+): Promise<GetMimeTypeResultWithStream>;
+export declare function getMimeType(
+	data: number | Uint8Array,
+	options: GetMimeTypeOptionsStrict
+): Promise<GetMimeTypeResultStrict>;
+export declare function getMimeType(
+	data: number | Uint8Array,
+	options?: GetMimeTypeOptionsNoStrict
+): Promise<GetMimeTypeResult>;
