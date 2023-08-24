efa2ad6203d5bd35abb67ead2d1cd24780ab85dc
Igor Richter <igor.richter@capgemini.com>
2023-08-24 14:15:50 +0200

    schoolfeature name fix

--- src/services/school/model.js
+++ src/services/school/model.js
@@ -20,7 +20,7 @@ const SCHOOL_FEATURES = {
 	LDAP_UNIVENTION_MIGRATION: 'ldapUniventionMigrationSchool',
 	OAUTH_PROVISIONING_ENABLED: 'oauthProvisioningEnabled',
 	SHOW_OUTDATED_USERS: 'showOutdatedUsers',
-	ENABLE_LDAP_SYNC_DURING_MIGRATION: 'enableSyncDuringMigration',
+	ENABLE_LDAP_SYNC_DURING_MIGRATION: 'enableLdapSyncDuringMigration',
 };
 
 const SCHOOL_OF_DELETED_USERS = { name: 'graveyard school (tombstone users only)', purpose: 'tombstone' };
