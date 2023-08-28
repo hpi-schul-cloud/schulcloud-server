0eb19714df52b71f2f670ce0ceb0e69e91836eb7
Bartosz Nowicki <bartosz.nowicki@gca.pass-consulting.com>
2023-08-28 20:31:26 +0200

    move all consts definition to the top of the test suite

--- apps/server/src/modules/files/repo/files.repo.spec.ts
+++ apps/server/src/modules/files/repo/files.repo.spec.ts
@@ -16,85 +16,6 @@ describe(FilesRepo.name, () => {
 	let em: EntityManager;
 	let module: TestingModule;
 
-	beforeAll(async () => {
-		module = await Test.createTestingModule({
-			imports: [
-				MongoMemoryDatabaseModule.forRoot({
-					entities: [FileEntity, StorageProviderEntity],
-				}),
-			],
-			providers: [FilesRepo],
-		}).compile();
-
-		repo = module.get(FilesRepo);
-		em = module.get(EntityManager);
-	});
-
-	beforeEach(async () => {
-		await em.nativeDelete(FileEntity, {});
-	});
-
-	afterAll(async () => {
-		await module.close();
-	});
-
-	describe('defined', () => {
-		it('repo should be defined', () => {
-			expect(repo).toBeDefined();
-		});
-
-		it('entity manager should be defined', () => {
-			expect(em).toBeDefined();
-		});
-
-		it('should implement entityName getter', () => {
-			expect(repo.entityName).toBe(FileEntity);
-		});
-	});
-
-	describe('findForCleanup', () => {
-		it('should return files marked for deletion according to given params', async () => {
-			const file: FileEntity = userFileFactory.build({ deletedAt: new Date() });
-
-			await em.persistAndFlush(file);
-			em.clear();
-
-			const thresholdDate = new Date();
-
-			const result = await repo.findForCleanup(thresholdDate, 3, 0);
-
-			expect(result.length).toEqual(1);
-			expect(result[0].id).toEqual(file.id);
-		});
-
-		it('should not return files which are not marked for deletion', async () => {
-			const file = userFileFactory.build({ deletedAt: undefined });
-
-			await em.persistAndFlush(file);
-			em.clear();
-
-			const thresholdDate = new Date();
-			const result = await repo.findForCleanup(thresholdDate, 3, 0);
-
-			expect(result.length).toEqual(0);
-		});
-
-		it('should not return files where deletedAt is after threshold', async () => {
-			const thresholdDate = new Date();
-			const file = userFileFactory.build({ deletedAt: new Date(thresholdDate.getTime() + 10) });
-
-			await em.persistAndFlush(file);
-			em.clear();
-
-			// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-			expect(file.deletedAt!.getTime()).toBeGreaterThan(thresholdDate.getTime());
-
-			const result = await repo.findForCleanup(thresholdDate, 3, 0);
-
-			expect(result.length).toEqual(0);
-		});
-	});
-
 	const storageProvider = storageProviderFactory.buildWithId();
 	const mainUserId = new ObjectId().toHexString();
 	const otherUserId = new ObjectId().toHexString();
@@ -309,6 +230,85 @@ describe(FilesRepo.name, () => {
 			expectedMainUserFileProps,
 		};
 	};
+
+	beforeAll(async () => {
+		module = await Test.createTestingModule({
+			imports: [
+				MongoMemoryDatabaseModule.forRoot({
+					entities: [FileEntity, StorageProviderEntity],
+				}),
+			],
+			providers: [FilesRepo],
+		}).compile();
+
+		repo = module.get(FilesRepo);
+		em = module.get(EntityManager);
+	});
+
+	beforeEach(async () => {
+		await em.nativeDelete(FileEntity, {});
+	});
+
+	afterAll(async () => {
+		await module.close();
+	});
+
+	describe('defined', () => {
+		it('repo should be defined', () => {
+			expect(repo).toBeDefined();
+		});
+
+		it('entity manager should be defined', () => {
+			expect(em).toBeDefined();
+		});
+
+		it('should implement entityName getter', () => {
+			expect(repo.entityName).toBe(FileEntity);
+		});
+	});
+
+	describe('findForCleanup', () => {
+		it('should return files marked for deletion according to given params', async () => {
+			const file: FileEntity = userFileFactory.build({ deletedAt: new Date() });
+
+			await em.persistAndFlush(file);
+			em.clear();
+
+			const thresholdDate = new Date();
+
+			const result = await repo.findForCleanup(thresholdDate, 3, 0);
+
+			expect(result.length).toEqual(1);
+			expect(result[0].id).toEqual(file.id);
+		});
+
+		it('should not return files which are not marked for deletion', async () => {
+			const file = userFileFactory.build({ deletedAt: undefined });
+
+			await em.persistAndFlush(file);
+			em.clear();
+
+			const thresholdDate = new Date();
+			const result = await repo.findForCleanup(thresholdDate, 3, 0);
+
+			expect(result.length).toEqual(0);
+		});
+
+		it('should not return files where deletedAt is after threshold', async () => {
+			const thresholdDate = new Date();
+			const file = userFileFactory.build({ deletedAt: new Date(thresholdDate.getTime() + 10) });
+
+			await em.persistAndFlush(file);
+			em.clear();
+
+			// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+			expect(file.deletedAt!.getTime()).toBeGreaterThan(thresholdDate.getTime());
+
+			const result = await repo.findForCleanup(thresholdDate, 3, 0);
+
+			expect(result.length).toEqual(0);
+		});
+	});
 
 	describe('findByOwnerUserId', () => {
 		it('should return proper files that are owned by the user with given userId', async () => {
