27d19314d97f43ea13d16b816b076894461ad4ac
Marvin Rode <marvin.rode@capgemini.com>
2023-08-29 10:23:40 +0200

    get player tests

--- apps/server/src/modules/h5p-editor/uc/h5p-get-player.uc.spec.ts
+++ apps/server/src/modules/h5p-editor/uc/h5p-get-player.uc.spec.ts
@@ -1,30 +1,37 @@
 import { DeepMocked, createMock } from '@golevelup/ts-jest';
-import { H5PEditor, H5PPlayer } from '@lumieducation/h5p-server';
+import { H5PEditor, H5PPlayer, IPlayerModel } from '@lumieducation/h5p-server';
 import { Test, TestingModule } from '@nestjs/testing';
-import { setupEntities } from '@shared/testing';
-import { UserService } from '@src/modules';
+import { h5pContentFactory, setupEntities } from '@shared/testing';
+import { AuthorizationContextBuilder, AuthorizationService, UserService } from '@src/modules';
 import { ICurrentUser } from '@src/modules/authentication';
+import { ForbiddenException, NotFoundException } from '@nestjs/common';
+import { H5PContentRepo } from '../repo';
 import { H5PAjaxEndpointService, LibraryStorage } from '../service';
 import { H5PEditorUc } from './h5p.uc';
 
-const setup = () => {
-	const contentId = '123456789';
-	const notExistingContentId = '0000';
-	const currentUser: ICurrentUser = {
-		userId: '123',
-		roles: [],
-		schoolId: '',
-		accountId: '',
-	};
-	const htmlString = 'htmlString';
+const createParams = () => {
+	const content = h5pContentFactory.build();
 
-	return { contentId, notExistingContentId, currentUser, htmlString };
+	const mockCurrentUser: ICurrentUser = {
+		accountId: 'mockAccountId',
+		roles: ['student'],
+		schoolId: 'mockSchoolId',
+		userId: 'mockUserId',
+	};
+
+	const playerResponseMock = expect.objectContaining({
+		contentId: content.id,
+	}) as IPlayerModel;
+
+	return { content, mockCurrentUser, playerResponseMock };
 };
 
 describe('get H5P player', () => {
 	let module: TestingModule;
 	let uc: H5PEditorUc;
 	let h5pPlayer: DeepMocked<H5PPlayer>;
+	let h5pContentRepo: DeepMocked<H5PContentRepo>;
+	let authorizationService: DeepMocked<AuthorizationService>;
 
 	beforeAll(async () => {
 		module = await Test.createTestingModule({
@@ -47,11 +54,21 @@ describe('get H5P player', () => {
 					provide: UserService,
 					useValue: createMock<UserService>(),
 				},
+				{
+					provide: AuthorizationService,
+					useValue: createMock<AuthorizationService>(),
+				},
+				{
+					provide: H5PContentRepo,
+					useValue: createMock<H5PContentRepo>(),
+				},
 			],
 		}).compile();
 
 		uc = module.get(H5PEditorUc);
 		h5pPlayer = module.get(H5PPlayer);
+		h5pContentRepo = module.get(H5PContentRepo);
+		authorizationService = module.get(AuthorizationService);
 		await setupEntities();
 	});
 
@@ -63,23 +80,116 @@ describe('get H5P player', () => {
 		await module.close();
 	});
 
-	describe('when contentId is given', () => {
-		it('should render h5p player', async () => {
-			const { contentId, currentUser, htmlString } = setup();
-			h5pPlayer.render.mockResolvedValueOnce(htmlString);
-			const result = await uc.getH5pPlayer(currentUser, contentId);
+	describe('getH5pPlayer is called', () => {
+		describe('WHEN user is authorized and service executes successfully', () => {
+			const setup = () => {
+				const { content, mockCurrentUser, playerResponseMock } = createParams();
 
-			expect(result).toEqual('htmlString');
+				const expectedResult = playerResponseMock;
+
+				h5pContentRepo.findById.mockResolvedValueOnce(content);
+				h5pPlayer.render.mockResolvedValueOnce(expectedResult);
+				authorizationService.checkPermissionByReferences.mockResolvedValueOnce();
+
+				return { content, mockCurrentUser, expectedResult };
+			};
+
+			it('should call authorizationService.checkPermissionByReferences', async () => {
+				const { content, mockCurrentUser } = setup();
+
+				await uc.getH5pPlayer(mockCurrentUser, content.id);
+
+				expect(authorizationService.checkPermissionByReferences).toBeCalledWith(
+					mockCurrentUser.userId,
+					content.parentType,
+					content.parentId,
+					AuthorizationContextBuilder.read([])
+				);
+			});
+
+			it('should call service with correct params', async () => {
+				const { content, mockCurrentUser } = setup();
+
+				await uc.getH5pPlayer(mockCurrentUser, content.id);
+
+				expect(h5pPlayer.render).toHaveBeenCalledWith(
+					content.id,
+					expect.objectContaining({
+						id: mockCurrentUser.userId,
+					})
+				);
+			});
+
+			it('should return results of service', async () => {
+				const { content, mockCurrentUser, expectedResult } = setup();
+
+				const result = await uc.getH5pPlayer(mockCurrentUser, content.id);
+
+				expect(result).toEqual(expectedResult);
+			});
 		});
 	});
 
-	describe('when contentId does not exist', () => {
-		it('should throw an error', async () => {
-			const { notExistingContentId, currentUser } = setup();
-			h5pPlayer.render.mockRejectedValueOnce(new Error('Could not get H5P player'));
-			const result = uc.getH5pPlayer(currentUser, notExistingContentId);
+	describe('WHEN content does not exist', () => {
+		const setup = () => {
+			const { content, mockCurrentUser, playerResponseMock } = createParams();
 
-			await expect(result).rejects.toThrow();
+			h5pContentRepo.findById.mockRejectedValueOnce(new NotFoundException());
+
+			return { content, mockCurrentUser, playerResponseMock };
+		};
+
+		it('should throw NotFoundException', async () => {
+			const { content, mockCurrentUser } = setup();
+
+			const getPlayerPromise = uc.getH5pPlayer(mockCurrentUser, content.id);
+
+			await expect(getPlayerPromise).rejects.toThrow(new NotFoundException());
+
+			expect(h5pPlayer.render).toHaveBeenCalledTimes(0);
+		});
+	});
+
+	describe('WHEN user is not authorized', () => {
+		const setup = () => {
+			const { content, mockCurrentUser, playerResponseMock } = createParams();
+
+			h5pContentRepo.findById.mockResolvedValueOnce(content);
+			authorizationService.checkPermissionByReferences.mockRejectedValueOnce(new ForbiddenException());
+
+			return { content, mockCurrentUser, playerResponseMock };
+		};
+
+		it('should throw forbidden error', async () => {
+			const { content, mockCurrentUser } = setup();
+
+			const getPlayerPromise = uc.getH5pPlayer(mockCurrentUser, content.id);
+
+			await expect(getPlayerPromise).rejects.toThrow(new ForbiddenException());
+
+			expect(h5pPlayer.render).toHaveBeenCalledTimes(0);
+		});
+	});
+
+	describe('WHEN service throws error', () => {
+		const setup = () => {
+			const { content, mockCurrentUser, playerResponseMock } = createParams();
+
+			const error = new Error('test');
+
+			h5pContentRepo.findById.mockResolvedValueOnce(content);
+			authorizationService.checkPermissionByReferences.mockResolvedValueOnce();
+			h5pPlayer.render.mockRejectedValueOnce(error);
+
+			return { error, content, mockCurrentUser, playerResponseMock };
+		};
+
+		it('should return error of service', async () => {
+			const { error, content, mockCurrentUser } = setup();
+
+			const getPlayerPromise = uc.getH5pPlayer(mockCurrentUser, content.id);
+
+			await expect(getPlayerPromise).rejects.toThrow(error);
 		});
 	});
 });
