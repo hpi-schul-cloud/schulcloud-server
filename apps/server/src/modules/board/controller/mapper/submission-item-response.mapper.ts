550d13f509d5cb96c74c4ac941687f4d20f2d2d7
MartinSchuhmacher <mschuhmacher@gmx.de>
2023-09-04 13:37:00 +0200

    adjusted structure for user in submission response

--- apps/server/src/modules/board/controller/mapper/submission-item-response.mapper.ts
+++ apps/server/src/modules/board/controller/mapper/submission-item-response.mapper.ts
@@ -1,5 +1,5 @@
 import { SubmissionItem } from '@shared/domain';
-import { SubmissionItemResponse, TimestampsResponse } from '../dto';
+import { SubmissionItemResponse, TimestampsResponse, UserDataResponse } from '../dto';
 
 export class SubmissionItemResponseMapper {
 	private static instance: SubmissionItemResponseMapper;
@@ -14,13 +14,17 @@ export class SubmissionItemResponseMapper {
 
 	public mapToResponse(submissionItem: SubmissionItem): SubmissionItemResponse {
 		const result = new SubmissionItemResponse({
+			completed: submissionItem.completed,
 			id: submissionItem.id,
 			timestamps: new TimestampsResponse({
 				lastUpdatedAt: submissionItem.updatedAt,
 				createdAt: submissionItem.createdAt,
 			}),
-			completed: submissionItem.completed,
-			userId: submissionItem.userId,
+			userData: new UserDataResponse({
+				firstName: 'John',
+				lastName: 'Mr Doe',
+				userId: submissionItem.userId,
+			}),
 		});
 
 		return result;
