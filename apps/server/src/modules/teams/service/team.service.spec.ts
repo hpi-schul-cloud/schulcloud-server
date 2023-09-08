d0957778f7cf71a2bc5a38e41a92fe54e4c2d60f
WojciechGrancow <Wojciech.Grancow@gca.pass-consulting.com>
2023-09-08 10:37:55 +0200

    changes after review

--- apps/server/src/modules/teams/service/team.service.spec.ts
+++ apps/server/src/modules/teams/service/team.service.spec.ts
@@ -1,9 +1,7 @@
 import { DeepMocked, createMock } from '@golevelup/ts-jest';
-import { InternalServerErrorException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TeamsRepo } from '@shared/repo';
-import { setupEntities, teamFactory, teamUserFactory, userDoFactory } from '@shared/testing';
-import { EntityId, UserDO } from '@shared/domain';
+import { setupEntities, teamFactory, teamUserFactory } from '@shared/testing';
 import { TeamService } from './team.service';
 
 describe('TeamService', () => {
@@ -38,23 +36,6 @@ describe('TeamService', () => {
 	});
 
 	describe('findUserDataFromTeams', () => {
-		describe('when user is missing', () => {
-			const setup = () => {
-				const user: UserDO = userDoFactory.build({ id: undefined });
-				const userId = user.id as EntityId;
-
-				return {
-					userId,
-				};
-			};
-
-			it('should throw an error', async () => {
-				const { userId } = setup();
-
-				await expect(service.findUserDataFromTeams(userId)).rejects.toThrowError(InternalServerErrorException);
-			});
-		});
-
 		describe('when finding by userId', () => {
 			const setup = () => {
 				const teamUser = teamUserFactory.buildWithId();
@@ -87,23 +68,6 @@ describe('TeamService', () => {
 	});
 
 	describe('deleteUserDataFromTeams', () => {
-		describe('when user is missing', () => {
-			const setup = () => {
-				const user: UserDO = userDoFactory.build({ id: undefined });
-				const userId = user.id as EntityId;
-
-				return {
-					userId,
-				};
-			};
-
-			it('should throw an error', async () => {
-				const { userId } = setup();
-
-				await expect(service.deleteUserDataFromTeams(userId)).rejects.toThrowError(InternalServerErrorException);
-			});
-		});
-
 		describe('when deleting by userId', () => {
 			const setup = () => {
 				const teamUser = teamUserFactory.buildWithId();
--- apps/server/src/modules/teams/service/team.service.ts
+++ apps/server/src/modules/teams/service/team.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { EntityId, TeamEntity } from '@shared/domain';
 import { TeamsRepo } from '@shared/repo';
 
@@ -7,20 +7,12 @@ export class TeamService {
 	constructor(private readonly teamsRepo: TeamsRepo) {}
 
 	public async findUserDataFromTeams(userId: EntityId): Promise<TeamEntity[]> {
-		if (!userId) {
-			throw new InternalServerErrorException('User id is missing');
-		}
-
 		const teams = await this.teamsRepo.findByUserId(userId);
 
 		return teams;
 	}
 
 	public async deleteUserDataFromTeams(userId: EntityId): Promise<number> {
-		if (!userId) {
-			throw new InternalServerErrorException('User id is missing');
-		}
-
 		const teams = await this.teamsRepo.findByUserId(userId);
 
 		const updatedTeams: TeamEntity[] = teams.map((team: TeamEntity) => {
