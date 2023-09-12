ca9ff6e6b52db0886290d856a65ae10e4cc9bc79
hoeppner-dataport <marcus.hoeppner@dataport.de>
2023-09-12 13:26:28 +0200

    chore: increase coverage for course.service

--- apps/server/src/modules/learnroom/service/course.service.spec.ts
+++ apps/server/src/modules/learnroom/service/course.service.spec.ts
@@ -1,7 +1,8 @@
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
 import { Test, TestingModule } from '@nestjs/testing';
 import { Course } from '@shared/domain';
 import { CourseRepo } from '@shared/repo';
-import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { CourseCreateDto } from '../types';
 import { CourseService } from './course.service';
 
 describe('CourseService', () => {
@@ -31,12 +32,27 @@ describe('CourseService', () => {
 		courseRepo.findById.mockClear();
 	});
 
+	describe('createCourse', () => {
+		it('should call createCourse from course repository', async () => {
+			const courseCreateDto: CourseCreateDto = {
+				name: 'new course',
+				schoolId: '1234aschoolid',
+			};
+
+			await expect(courseService.createCourse(courseCreateDto)).resolves.not.toThrow();
+
+			expect(courseRepo.createCourse).toBeCalledTimes(1);
+			expect(courseRepo.createCourse).toBeCalledWith(expect.objectContaining(courseCreateDto));
+		});
+	});
+
 	describe('findById', () => {
 		it('should call findById from course repository', async () => {
 			const courseId = 'courseId';
 			courseRepo.findById.mockResolvedValueOnce({} as Course);
 
 			await expect(courseService.findById(courseId)).resolves.not.toThrow();
+
 			expect(courseRepo.findById).toBeCalledTimes(1);
 			expect(courseRepo.findById).toBeCalledWith(courseId);
 		});
