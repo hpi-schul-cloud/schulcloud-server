8271fbc9aa3e7be8d6d3260ff7c8279d6eba4832
stekrause <stephan.krause.ext@capgemini.com>
2023-09-15 07:39:24 +0200

    start h5p library management as a microservice

--- apps/server/src/apps/h5p-library-management.app.ts
+++ apps/server/src/apps/h5p-library-management.app.ts
@@ -13,16 +13,23 @@ import { H5PLibraryManagementService } from '@src/modules/h5p-library-management
 async function bootstrap() {
 	sourceMapInstall();
 
-	const nestApp = await NestFactory.createApplicationContext(H5PLibraryManagementModule);
+	const nestApp = await NestFactory.createMicroservice(H5PLibraryManagementModule);
 
 	// WinstonLogger
 	nestApp.useLogger(await nestApp.resolve(LegacyLogger));
 
 	await nestApp.init();
 
+	console.log('#########################################');
+	console.log(`##### Start H5P Library Management ######`);
+	console.log('#########################################');
+
 	await nestApp.get(H5PLibraryManagementService).run();
 	// await app.get(H5PLibraryManagementService).run();
 	// TODO: properly close app (there is some issue with the logger)
+	console.log('#########################################');
+	console.log(`##### Close H5P Library Management ######`);
+	console.log('#########################################');
 	await nestApp.close();
 }
 void bootstrap();
