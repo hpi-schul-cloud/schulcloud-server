6088d6183d7f4e6e0293714ba7a1903f259b3ad3
dyedwiper <maximilian.becker@dataport.de>
2023-09-18 11:31:10 +0200

    Rename api module file

--- apps/server/src/modules/server/server.module.ts
+++ apps/server/src/modules/server/server.module.ts
@@ -23,7 +23,7 @@ import { NewsModule } from '@src/modules/news';
 import { OauthProviderApiModule } from '@src/modules/oauth-provider';
 import { OauthApiModule } from '@src/modules/oauth/oauth-api.module';
 import { RocketChatModule } from '@src/modules/rocketchat';
-import { LegacySchoolApiModule } from '@src/modules/school-migration/school-migration-api.module';
+import { LegacySchoolApiModule } from '@src/modules/school-migration/legacy-school-api.module';
 import { SharingApiModule } from '@src/modules/sharing/sharing.module';
 import { SystemApiModule } from '@src/modules/system/system-api.module';
 import { TaskApiModule } from '@src/modules/task/task-api.module';
