2dc05e33540184c8cd70a72423d50c7b0cb757a1
Marvin Öhlerking <marvin.oehlerking@capgemini.com>
2023-09-18 09:14:35 +0200

    api property

--- apps/server/src/modules/group/controller/dto/response/class-info.response.ts
+++ apps/server/src/modules/group/controller/dto/response/class-info.response.ts
@@ -1,8 +1,13 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
 export class ClassInfoResponse {
+	@ApiProperty()
 	name: string;
 
+	@ApiPropertyOptional()
 	externalSourceName?: string;
 
+	@ApiProperty({ isArray: true })
 	teachers: string[];
 
 	constructor(props: ClassInfoResponse) {
