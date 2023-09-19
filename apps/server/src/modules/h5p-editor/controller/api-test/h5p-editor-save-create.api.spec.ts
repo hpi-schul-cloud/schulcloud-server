aeb63ec035d0a298ed8f1a421edb77a2cbae70c3
stekrause <stephan.krause.ext@capgemini.com>
2023-09-19 15:49:46 +0200

    refactor h5p api tests

--- apps/server/src/modules/h5p-editor/controller/api-test/h5p-editor-save-create.api.spec.ts
+++ apps/server/src/modules/h5p-editor/controller/api-test/h5p-editor-save-create.api.spec.ts
@@ -13,6 +13,7 @@ import request from 'supertest';
 import { H5PEditorTestModule } from '../../h5p-editor-test.module';
 import { H5P_CONTENT_S3_CONNECTION, H5P_LIBRARIES_S3_CONNECTION } from '../../h5p-editor.config';
 import { H5PEditorUc } from '../../uc/h5p.uc';
+import { AuthorizationService } from '@src/modules/authorization';
 
 const setup = () => {
 	const contentId = new ObjectId(0);
@@ -69,6 +70,7 @@ describe('H5PEditor Controller (api)', () => {
 	let em: EntityManager;
 	let currentUser: ICurrentUser;
 	let h5PEditorUc: DeepMocked<H5PEditorUc>;
+	let authorizationService: DeepMocked<AuthorizationService>;
 
 	beforeAll(async () => {
 		const module = await Test.createTestingModule({
@@ -88,11 +90,14 @@ describe('H5PEditor Controller (api)', () => {
 			.useValue(createMock<S3ClientAdapter>())
 			.overrideProvider(H5PEditorUc)
 			.useValue(createMock<H5PEditorUc>())
+			.overrideProvider(AuthorizationService)
+			.useValue(createMock<H5PEditorUc>())
 			.compile();
 
 		app = module.createNestApplication();
 		await app.init();
 		h5PEditorUc = module.get(H5PEditorUc);
+		authorizationService = module.get(AuthorizationService);
 
 		api = new API(app);
 		em = module.get(EntityManager);
@@ -120,6 +125,7 @@ describe('H5PEditor Controller (api)', () => {
 			it('should return 201 status', async () => {
 				const { id, metadata } = setup();
 				const result1 = { id, metadata };
+				authorizationService.checkPermissionByReferences.mockResolvedValueOnce();
 				h5PEditorUc.createH5pContentGetMetadata.mockResolvedValueOnce(result1);
 				const response = await api.create();
 				expect(response.status).toEqual(201);
@@ -144,6 +150,7 @@ describe('H5PEditor Controller (api)', () => {
 			it('should return 201 status', async () => {
 				const { contentId, id, metadata } = setup();
 				const result1 = { id, metadata };
+				authorizationService.checkPermissionByReferences.mockResolvedValueOnce();
 				h5PEditorUc.saveH5pContentGetMetadata.mockResolvedValueOnce(result1);
 				const response = await api.save(contentId.toString());
 				expect(response.status).toEqual(201);
@@ -152,6 +159,7 @@ describe('H5PEditor Controller (api)', () => {
 		describe('with bad request params', () => {
 			it('should return 500 status', async () => {
 				const { notExistingContentId } = setup();
+				authorizationService.checkPermissionByReferences.mockResolvedValueOnce();
 				h5PEditorUc.saveH5pContentGetMetadata.mockRejectedValueOnce(new Error('Could not save H5P content'));
 				const response = await api.save(notExistingContentId.toString());
 				expect(response.status).toEqual(500);
