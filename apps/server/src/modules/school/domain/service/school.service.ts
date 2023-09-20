fea58452604e047b6a2f57d587285fc20ee5b81f
dyedwiper <maximilian.becker@dataport.de>
2023-09-20 10:09:19 +0200

    Rename injection token

--- apps/server/src/modules/school/domain/service/school.service.ts
+++ apps/server/src/modules/school/domain/service/school.service.ts
@@ -1,10 +1,10 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { SchoolRepo } from '../interface';
+import { SchoolRepo, SCHOOL_REPO } from '../interface';
 import { School } from '../school';
 
 @Injectable()
 export class SchoolService {
-	constructor(@Inject(SchoolRepo) private readonly schoolRepo: SchoolRepo) {}
+	constructor(@Inject(SCHOOL_REPO) private readonly schoolRepo: SchoolRepo) {}
 
 	public async getAllSchools(): Promise<School[]> {
 		const schools = await this.schoolRepo.getAllSchools();
--- apps/server/src/modules/school/school.module.ts
+++ apps/server/src/modules/school/school.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { SchoolRepo } from './domain';
+import { SCHOOL_REPO } from './domain';
 import { SchoolService } from './domain/service/school.service';
 import { MikroOrmSchoolRepo } from './repo/school.repo';
 
 @Module({
-	providers: [SchoolService, { provide: SchoolRepo, useClass: MikroOrmSchoolRepo }],
+	providers: [SchoolService, { provide: SCHOOL_REPO, useClass: MikroOrmSchoolRepo }],
 	exports: [SchoolService],
 })
 export class SchoolModule {}
