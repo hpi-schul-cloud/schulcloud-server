de76f17daee0325db75e5af50e46679dd3b87c30
dyedwiper <maximilian.becker@dataport.de>
2023-09-20 10:09:19 +0200

    Rename injection token

--- apps/server/src/modules/school/domain/interface/school-repo.interface.ts
+++ apps/server/src/modules/school/domain/interface/school-repo.interface.ts
@@ -4,5 +4,6 @@ export interface SchoolRepo {
 	getAllSchools(): Promise<School[]>;
 }
 
-// https://stackoverflow.com/a/70088972/11854580
-export const SchoolRepo = Symbol('SchoolRepo');
+// TODO: How to name the injection token? Should it be a string or a symbol? There are different approaches in our code. Shall we be consistent about it?
+// It is even possible to give it the same name as the interface: https://stackoverflow.com/a/70088972/11854580 But that's probably confusing.
+export const SCHOOL_REPO = 'SCHOOL_REPO';
