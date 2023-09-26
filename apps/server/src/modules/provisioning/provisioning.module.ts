1d4a0773e71dc18bb6bfde43328e96023139bfce
Wolfgang <wolfgang@greschus.de>
2023-09-26 11:11:23 +0200

    changed path

--- apps/server/src/modules/provisioning/provisioning.module.ts
+++ apps/server/src/modules/provisioning/provisioning.module.ts
@@ -2,16 +2,16 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { LoggerModule } from '@src/core/logger';
 import { AccountModule } from '@src/modules/account/account.module';
+import { FederalStateModule } from '@src/modules/federal-state';
+import { GroupModule } from '@src/modules/group';
 import { RoleModule } from '@src/modules/role';
 import { SchoolModule } from '@src/modules/school/school.module';
 import { SystemModule } from '@src/modules/system/system.module';
 import { UserModule } from '@src/modules/user';
-import { GroupModule } from '@src/modules/group';
 import { ProvisioningService } from './service/provisioning.service';
 import { IservProvisioningStrategy, OidcMockProvisioningStrategy, SanisProvisioningStrategy } from './strategy';
 import { OidcProvisioningService } from './strategy/oidc/service/oidc-provisioning.service';
-import { SanisResponseMapper } from './strategy/sanis/sanis-response.mapper';
-import { FederalStateModule } from '../federal-state';
+import { SanisResponseMapper } from './strategy/sanis/sanis-response.mappers';
 
 @Module({
 	imports: [
