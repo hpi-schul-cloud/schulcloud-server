a1a8114b58c18efbae803c05efd1da25659a7136
hoeppner-dataport <marcus.hoeppner@dataport.de>
2023-09-27 13:58:12 +0200

    chore: add tests for content-element-update.visitor

---
+++ apps/server/src/shared/testing/factory/domainobject/board/link-element.do.factory.ts
@@ -0,0 +1,14 @@
+/* istanbul ignore file */
+import { LinkElement, LinkElementProps } from '@shared/domain';
+import { ObjectId } from 'bson';
+import { BaseFactory } from '../../base.factory';
+
+export const linkElementFactory = BaseFactory.define<LinkElement, LinkElementProps>(LinkElement, ({ sequence }) => {
+	return {
+		id: new ObjectId().toHexString(),
+		url: `https://www.example.com/link/${sequence}`,
+		children: [],
+		createdAt: new Date(),
+		updatedAt: new Date(),
+	};
+});
