b668fbf5fbd4ddf57ae7fec4ab7779064475e3af
stekrause <stephan.krause.ext@capgemini.com>
2023-09-29 12:11:48 +0200

    update default schema

--- apps/server/src/modules/h5p-library-management/h5p-library-management.config.ts
+++ apps/server/src/modules/h5p-library-management/h5p-library-management.config.ts
@@ -25,7 +25,7 @@ export const s3ConfigContent: S3Config = {
 export const s3ConfigLibraries: S3Config = {
 	connectionName: H5P_LIBRARIES_S3_CONNECTION,
 	endpoint: Configuration.get('H5P_Library__S3_ENDPOINT') as string,
-	region: Configuration.get('H5P_Editor__S3_REGION') as string,
+	region: Configuration.get('H5P_EDITOR__S3_REGION') as string,
 	bucket: Configuration.get('H5P_Library__S3_BUCKET_LIBRARIES') as string,
 	accessKeyId: Configuration.get('H5P_Library__S3_ACCESS_KEY_ID') as string,
 	secretAccessKey: Configuration.get('H5P_Library__S3_SECRET_ACCESS_KEY') as string,
