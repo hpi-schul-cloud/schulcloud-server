785d702d39b56b6c263c693a1539702b39ad8405
Andre Blome <andre-david.blome@capgemini.com>
2023-09-29 21:44:55 +0200

    h5p library management: fix merge leftover, bug, remove s3 config changes which go into another branch

--- apps/server/src/modules/h5p-library-management/h5p-library-management.module.ts
+++ apps/server/src/modules/h5p-library-management/h5p-library-management.module.ts
@@ -2,7 +2,7 @@ import { Dictionary, IPrimaryKey } from '@mikro-orm/core';
 import { MikroOrmModule, MikroOrmModuleSyncOptions } from '@mikro-orm/nestjs';
 import { Module, NotFoundException } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { Account, Role, School, SchoolYear, System, User } from '@shared/domain';
+import { ALL_ENTITIES } from '@shared/domain';
 import { RabbitMQWrapperModule } from '@shared/infra/rabbitmq';
 
 import { S3ClientModule } from '@shared/infra/s3-client';
@@ -11,19 +11,11 @@ import { CoreModule } from '@src/core';
 import { Logger } from '@src/core/logger';
 import { UserModule } from '..';
 
-<<<<<<< HEAD
-import { ContentStorage, LibraryStorage } from '../h5p-editor/service';
-import { config, s3ConfigContent, s3ConfigLibraries } from '../h5p-editor/h5p-editor.config';
+import { config, s3ConfigContent, s3ConfigLibraries } from './h5p-library-management.config';
 import { H5PContentRepo, LibraryRepo } from '../h5p-editor/repo';
-import { createS3ClientAdapter } from '../h5p-editor';
-=======
-import { config, s3ConfigContent } from '../h5p-editor/h5p-editor.config';
-import { LibraryRepo } from '../h5p-editor/repo';
-import { LibraryStorage } from '../h5p-editor/service';
-import { s3ConfigLibraries } from './h5p-library-management.config';
->>>>>>> b668fbf5fbd4ddf57ae7fec4ab7779064475e3af
+import { ContentStorage, LibraryStorage } from '../h5p-editor/service';
 
-import { InstalledLibrary } from '../h5p-editor/entity';
+import { H5PContent, InstalledLibrary } from '../h5p-editor/entity';
 import { H5PLibraryManagementService } from './service/h5p-library-management.service';
 
 const defaultMikroOrmOptions: MikroOrmModuleSyncOptions = {
@@ -44,7 +36,7 @@ const imports = [
 		password: DB_PASSWORD,
 		user: DB_USERNAME,
 		allowGlobalContext: true,
-		entities: [User, Account, Role, School, System, SchoolYear, InstalledLibrary],
+		entities: [...ALL_ENTITIES, H5PContent, InstalledLibrary],
 	}),
 	ConfigModule.forRoot(createConfigModuleOptions(config)),
 	S3ClientModule.register([s3ConfigContent, s3ConfigLibraries]),
@@ -52,32 +44,7 @@ const imports = [
 
 const controllers = [];
 
-const providers = [
-	Logger,
-	H5PLibraryManagementService,
-	ContentStorage,
-	H5PContentRepo,
-	LibraryRepo,
-	LibraryStorage,
-	{
-		provide: 'S3Config_Content',
-		useValue: s3ConfigContent,
-	},
-	{
-		provide: 'S3Config_Libraries',
-		useValue: s3ConfigLibraries,
-	},
-	{
-		provide: 'S3ClientAdapter_Content',
-		useFactory: createS3ClientAdapter,
-		inject: ['S3Config_Content', LegacyLogger],
-	},
-	{
-		provide: 'S3ClientAdapter_Libraries',
-		useFactory: createS3ClientAdapter,
-		inject: ['S3Config_Libraries', LegacyLogger],
-	},
-];
+const providers = [Logger, H5PLibraryManagementService, ContentStorage, H5PContentRepo, LibraryRepo, LibraryStorage];
 
 @Module({
 	imports,
