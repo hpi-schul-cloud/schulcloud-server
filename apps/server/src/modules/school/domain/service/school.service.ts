bdbcc762e7e0976551a75a83f2f6d2db50a10fb9
dyedwiper <maximilian.becker@dataport.de>
2023-10-02 16:59:01 +0200

    Add default sort order

--- apps/server/src/modules/school/domain/service/school.service.ts
+++ apps/server/src/modules/school/domain/service/school.service.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { EntityId, IPagination } from '@shared/domain';
+import { EntityId, IPagination, SortOrder } from '@shared/domain';
 import { SchoolRepo, SCHOOL_REPO } from '../interface';
 import { School } from '../do';
 import { SchoolQuery } from '../type';
@@ -9,7 +9,9 @@ export class SchoolService {
 	constructor(@Inject(SCHOOL_REPO) private readonly schoolRepo: SchoolRepo) {}
 
 	public async getAllSchools(query: SchoolQuery, pagination: IPagination): Promise<School[]> {
-		const schools = await this.schoolRepo.getAllSchools(query, { pagination });
+		// TODO: Is this really the right place to apply the default sort order?
+		const order = { name: SortOrder.asc };
+		const schools = await this.schoolRepo.getAllSchools(query, { pagination, order });
 
 		return schools;
 	}
