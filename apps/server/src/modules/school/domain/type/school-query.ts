b96d9c83ccc9367ac8e7d3abba03a7701c0f8ece
dyedwiper <maximilian.becker@dataport.de>
2023-10-04 15:49:08 +0200

    Add comment

--- apps/server/src/modules/school/domain/type/school-query.ts
+++ apps/server/src/modules/school/domain/type/school-query.ts
@@ -1,5 +1,7 @@
 import { EntityId } from '@shared/domain';
 
+// TODO: I don't really like it, that this is in the type-folder, because the other types describe things coming from the database,
+// while this query comes from the API. Maybe make a separate query-folder, but not sure.
 export interface SchoolQuery {
 	federalStateId?: EntityId;
 }
