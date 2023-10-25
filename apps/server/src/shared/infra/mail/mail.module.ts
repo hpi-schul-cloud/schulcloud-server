7b24e77ac8ac8fe466f3fd687c279ea1ba507e7d
Szymon Szafoni <szymon.szafoni@gca.pass-consulting.com>
2023-10-25 10:16:13 +0200

    add parameter

--- apps/server/src/shared/infra/mail/mail.module.ts
+++ apps/server/src/shared/infra/mail/mail.module.ts
@@ -1,6 +1,7 @@
 import { Module, DynamicModule } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { MailService } from './mail.service';
+import { IMailConfig } from './interfaces/mail-config';
 
 interface MailModuleOptions {
 	exchange: string;
@@ -18,7 +19,7 @@ export class MailModule {
 					provide: 'MAIL_SERVICE_OPTIONS',
 					useValue: { exchange: options.exchange, routingKey: options.routingKey },
 				},
-				ConfigService,
+				ConfigService<IMailConfig, true>,
 			],
 			exports: [MailService],
 		};
