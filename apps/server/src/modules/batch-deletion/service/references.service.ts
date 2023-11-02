5d764d00fbd3dc6460fc7f91b4bf3064bc5e53ea
Bartosz Nowicki <116367402+bn-pass@users.noreply.github.com>
2023-11-02 16:32:44 +0100

    add references service that'll load all the references to the data we want to delete

---
+++ apps/server/src/modules/batch-deletion/service/references.service.ts
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ReferencesService {
+	static loadFromTxtFile(filePath: string): string[] {
+		let fileContent = fs.readFileSync(filePath).toString();
+
+		// Replace all the CRLF occurrences with just a LF.
+		fileContent = fileContent.replace(/\r\n/g, '\n');
+
+		// Split the whole file content by a line feed (LF) char (\n).
+		const fileLines = fileContent.split('\n');
+
+		const references: string[] = [];
+
+		// Iterate over all the file lines and if it contains a valid id (which is
+		// basically any non-empty string), add it to the loaded references array.
+		fileLines.forEach((fileLine) => {
+			const reference = fileLine.trim();
+
+			if (reference && reference.length > 0) {
+				references.push(reference);
+			}
+		});
+
+		return references;
+	}
+}
