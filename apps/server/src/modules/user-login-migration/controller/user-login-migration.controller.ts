476d4cb98566a43d794ef27d61094643b22ec39f
Marvin Öhlerking <marvin.oehlerking@capgemini.com>
2023-11-03 11:57:38 +0100

    refactor api spec

--- apps/server/src/modules/user-login-migration/controller/user-login-migration.controller.ts
+++ apps/server/src/modules/user-login-migration/controller/user-login-migration.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { Authenticate, CurrentUser, ICurrentUser, JWT } from '@modules/authentication';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Put, Query } from '@nestjs/common';
 import {
 	ApiForbiddenResponse,
 	ApiInternalServerErrorResponse,
@@ -11,7 +12,6 @@ import {
 	ApiUnprocessableEntityResponse,
 } from '@nestjs/swagger';
 import { Page, UserLoginMigrationDO } from '@shared/domain';
-import { Authenticate, CurrentUser, ICurrentUser, JWT } from '@modules/authentication';
 import {
 	SchoolNumberMissingLoggableException,
 	UserLoginMigrationAlreadyClosedLoggableException,
@@ -181,6 +181,7 @@ export class UserLoginMigrationController {
 	}
 
 	@Post('close')
+	@HttpCode(HttpStatus.OK)
 	@ApiUnprocessableEntityResponse({
 		description: 'User login migration is already closed and cannot be modified. Restart is possible.',
 		type: UserLoginMigrationAlreadyClosedLoggableException,
