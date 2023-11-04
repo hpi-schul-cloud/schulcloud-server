f2ac1bc88f431f8dfaee9281f4c133feceb1af4d
Bartosz Nowicki <116367402+bn-pass@users.noreply.github.com>
2023-11-04 15:13:26 +0100

    add exporting DeletionClientConfig

--- apps/server/src/modules/deletion/client/index.ts
+++ apps/server/src/modules/deletion/client/index.ts
@@ -1,3 +1,4 @@
 export * from './interface/deletion-request-input.interface';
 export * from './interface/deletion-request-output.interface';
+export * from './deletion-client.config';
 export * from './deletion.client';
