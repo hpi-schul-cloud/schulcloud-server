6c66cf663fcc79b9c301a743718327388fa940f7
Bartosz Nowicki <116367402+bn-pass@users.noreply.github.com>
2023-11-07 00:52:17 +0100

    add batch deletion service implementation

--- apps/server/src/modules/deletion/services/batch-deletion.service.ts
+++ apps/server/src/modules/deletion/services/batch-deletion.service.ts
@@ -6,10 +6,14 @@ import { QueueDeletionRequestInput, QueueDeletionRequestOutput } from './interfa
 export class BatchDeletionService {
 	constructor(private readonly deletionClient: DeletionClient) {}
 
-	queueDeletionRequests(inputs: QueueDeletionRequestInput[]): QueueDeletionRequestOutput[] {
+	async queueDeletionRequests(inputs: QueueDeletionRequestInput[]): Promise<QueueDeletionRequestOutput[]> {
 		const outputs: QueueDeletionRequestOutput[] = [];
 
-		inputs.forEach(async (input) => {
+		// For every provided deletion request input, try to queue it via deletion client.
+		// In any case, add the result of the trial to the outputs - it will be either a valid
+		// response in a form of a requestId + deletionPlannedAt values pair or some error
+		// returned from the client. In any case, every input should be processed.
+		for (const input of inputs) {
 			const deletionRequestInput = {
 				targetRef: {
 					domain: input.targetRefDomain,
@@ -21,14 +25,17 @@ export class BatchDeletionService {
 			try {
 				const deletionRequestOutput = await this.deletionClient.queueDeletionRequest(deletionRequestInput);
 
+				// In case of a successful client response, add the
+				// requestId + deletionPlannedAt values pair to the outputs.
 				outputs.push({
 					requestId: deletionRequestOutput.requestId,
 					deletionPlannedAt: deletionRequestOutput.deletionPlannedAt,
 				});
 			} catch (err) {
-				outputs.push({ error: err });
+				// In case of a failure client response, add the full error message to the outputs.
+				outputs.push({ error: err.toString() });
 			}
-		});
+		}
 
 		return outputs;
 	}
