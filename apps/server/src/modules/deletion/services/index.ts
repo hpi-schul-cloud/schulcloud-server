da9c2a671c2f15c1ac930aaebc252e672c322601
Bartosz Nowicki <116367402+bn-pass@users.noreply.github.com>
2023-11-08 00:34:08 +0100

    remove repeated imports

--- apps/server/src/modules/deletion/services/index.ts
+++ apps/server/src/modules/deletion/services/index.ts
@@ -1,6 +1,5 @@
 export * from './interface';
 export * from './builder';
-export * from './deletion-request.service';
 export * from './references.service';
 export * from './batch-deletion.service';
 export * from './deletion-request.service';
