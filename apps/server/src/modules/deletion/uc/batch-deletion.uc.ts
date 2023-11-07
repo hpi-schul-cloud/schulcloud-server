1c8cae936e5c112ad8995745e81280556507023d
Bartosz Nowicki <116367402+bn-pass@users.noreply.github.com>
2023-11-07 01:08:27 +0100

    add UC for the batch deletion

--- apps/server/src/modules/deletion/uc/batch-deletion.uc.ts
+++ apps/server/src/modules/deletion/uc/batch-deletion.uc.ts
@@ -4,39 +4,66 @@ import { BatchDeletionSummary } from './interface';
 
 @Injectable()
 export class BatchDeletionUc {
-	constructor(
-		private readonly referencesService: ReferencesService,
-		private readonly batchDeletionService: BatchDeletionService
-	) {}
+	constructor(private readonly batchDeletionService: BatchDeletionService) {}
 
-	deleteRefsFromTxtFile(filePath: string): BatchDeletionSummary {
-		const refsFromTxtFile = ReferencesService.loadFromTxtFile(filePath);
+	async deleteRefsFromTxtFile(
+		refsFilePath: string,
+		targetRefDomain: string = 'user',
+		deleteInMinutes: number = 43200 // 43200 minutes = 720 hours = 30 days
+	): Promise<BatchDeletionSummary> {
+		// First, load all the references from the provided text file (with given path).
+		const refsFromTxtFile = ReferencesService.loadFromTxtFile(refsFilePath);
 
 		const inputs: QueueDeletionRequestInput[] = [];
 
+		// For each reference found in a given file, add it to the inputs
+		// array (with added targetRefDomain and deleteInMinutes fields).
 		refsFromTxtFile.forEach((ref) =>
 			inputs.push({
-				targetRefDomain: 'user',
 				targetRefId: ref,
-				deleteInMinutes: 60,
+				targetRefDomain: targetRefDomain,
+				deleteInMinutes: deleteInMinutes,
 			})
 		);
 
-		const outputs = this.batchDeletionService.queueDeletionRequests(inputs);
+		const outputs = await this.batchDeletionService.queueDeletionRequests(inputs);
+
+		// Throw an error if the returned outputs number doesn't match the returned inputs number.
+		if (outputs.length !== inputs.length) {
+			throw new Error(
+				'invalid result from the batch deletion service - expected to ' +
+					'receive the same amount of outputs as the provided inputs, ' +
+					`instead received ${outputs.length} outputs for ${inputs.length} inputs`
+			);
+		}
 
 		const summary: BatchDeletionSummary = {
+			overallStatus: 'failure',
 			successCount: 0,
 			failureCount: 0,
-			details: outputs,
+			details: [],
 		};
 
-		outputs.forEach((output) => {
-			if (output.error !== undefined) {
+		// Go through every received output and, in case of an error presence increase
+		// a failure count or, in case of no error, increase a success count.
+		for (let i = 0; i < outputs.length; i++) {
+			if (outputs[i].error) {
 				summary.failureCount += 1;
 			} else {
 				summary.successCount += 1;
 			}
-		});
+
+			// Also add all the processed inputs and outputs details to the overall summary.
+			summary.details.push({
+				input: inputs[i],
+				output: outputs[i],
+			});
+		}
+
+		// If no failure has been spotted, assume an overall success.
+		if (summary.failureCount === 0) {
+			summary.overallStatus = 'success';
+		}
 
 		return summary;
 	}
--- apps/server/src/modules/deletion/uc/interface/batch-deletion-summary.interface.ts
+++ apps/server/src/modules/deletion/uc/interface/batch-deletion-summary.interface.ts
@@ -1,7 +1,13 @@
-import { QueueDeletionRequestOutput } from '../../services';
+import { QueueDeletionRequestInput, QueueDeletionRequestOutput } from '../../services';
+
+export interface BatchDeletionSummaryDetail {
+	input: QueueDeletionRequestInput;
+	output: QueueDeletionRequestOutput;
+}
 
 export interface BatchDeletionSummary {
+	overallStatus: string;
 	successCount: number;
 	failureCount: number;
-	details: QueueDeletionRequestOutput[];
+	details: BatchDeletionSummaryDetail[];
 }
