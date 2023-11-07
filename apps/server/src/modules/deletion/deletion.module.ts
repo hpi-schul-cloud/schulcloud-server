4795bc620f0edcc77320c65f6086a615a6d7fae0
WojciechGrancow <116577704+WojciechGrancow@users.noreply.github.com>
2023-11-07 10:38:35 +0100

    Update apps/server/src/modules/deletion/deletion.module.ts
    Co-authored-by: Sergej Hoffmann <97111299+SevenWaysDP@users.noreply.github.com>

--- apps/server/src/modules/deletion/deletion.module.ts
+++ apps/server/src/modules/deletion/deletion.module.ts
@@ -4,7 +4,8 @@ import { DeletionRequestService } from './services/deletion-request.service';
 import { DeletionRequestRepo } from './repo/deletion-request.repo';
 
 @Module({
-	providers: [Logger, DeletionRequestService, DeletionRequestRepo],
+        imports: [LoggerModule],
+	providers: [DeletionRequestService, DeletionRequestRepo],
 	exports: [DeletionRequestService],
 })
 export class DeletionModule {}
