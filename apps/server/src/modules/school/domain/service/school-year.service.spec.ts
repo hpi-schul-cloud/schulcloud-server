94a7438a1a9bc9dc592596f23270dc7fa19be16b
dyedwiper <maximilian.becker@dataport.de>
2023-11-08 15:48:46 +0100

    Add test for school-year service

---
+++ apps/server/src/modules/school/domain/service/school-year.service.spec.ts
@@ -0,0 +1,36 @@
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { Test, TestingModule } from '@nestjs/testing';
+import { schoolYearFactory } from '../../testing/school-year.factory';
+import { SchoolYearRepo, SCHOOL_YEAR_REPO } from '../interface';
+import { SchoolYearService } from './school-year.service';
+
+describe('SchoolYearService', () => {
+	let service: SchoolYearService;
+	let schoolYearRepo: DeepMocked<SchoolYearRepo>;
+
+	beforeAll(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [
+				SchoolYearService,
+				{
+					provide: SCHOOL_YEAR_REPO,
+					useValue: createMock<SchoolYearRepo>(),
+				},
+			],
+		}).compile();
+
+		service = module.get(SchoolYearService);
+		schoolYearRepo = module.get(SCHOOL_YEAR_REPO);
+	});
+
+	describe('getAllSchoolYears', () => {
+		it('should return all school years', async () => {
+			const schoolYears = schoolYearFactory.buildList(3);
+			schoolYearRepo.getAllSchoolYears.mockResolvedValueOnce(schoolYears);
+
+			const result = await service.getAllSchoolYears();
+
+			expect(result).toEqual(schoolYears);
+		});
+	});
+});
