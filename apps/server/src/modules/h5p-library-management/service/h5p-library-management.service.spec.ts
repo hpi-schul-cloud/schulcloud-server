477bd25368cad2bf86f71ca8922f6e6b339af1c9
Caspar Neumann <caspar.neumann@capgemini.com>
2023-11-08 13:44:42 +0100

    implement tests for h5p-library-managment

--- apps/server/src/modules/h5p-library-management/service/h5p-library-management.service.spec.ts
+++ apps/server/src/modules/h5p-library-management/service/h5p-library-management.service.spec.ts
@@ -1,18 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Test, TestingModule } from '@nestjs/testing';
-import { createMock } from '@golevelup/ts-jest';
-import { LibraryStorage } from '@src/modules/h5p-editor/service/libraryStorage.service';
-import { ContentStorage } from '@src/modules/h5p-editor/service/contentStorage.service';
-import ContentTypeCache from '@lumieducation/h5p-server/build/src/ContentTypeCache';
-import LibraryAdministration from '@lumieducation/h5p-server/build/src/LibraryAdministration';
+import { DeepMocked, createMock } from '@golevelup/ts-jest';
+import { ContentStorage, LibraryStorage } from '@src/modules/h5p-editor/service';
+import { LibraryAdministration, ContentTypeCache } from '@lumieducation/h5p-server';
+import { IHubContentType } from '@lumieducation/h5p-server/build/src/types';
 import { H5PLibraryManagementService } from './h5p-library-management.service';
+
+jest.mock('@lumieducation/h5p-server', () => {
+	return {
+		...jest.requireActual('@lumieducation/h5p-server'),
+		LibraryAdministration: jest.fn().mockImplementation(() => {
+			return {
+				getLibraries: jest.fn().mockResolvedValue([
+					{ machineName: 'a', dependentsCount: 0 },
+					{ machineName: 'b', dependentsCount: 1 },
+					{ machineName: 'c', dependentsCount: 0 },
+				]),
+			};
+		}),
+	};
+});
 
 describe('H5PLibraryManagementService', () => {
 	let module: TestingModule;
 	let service: H5PLibraryManagementService;
-	let libraryStorageMock: jest.Mocked<LibraryStorage>;
-	let libraryAdministrationMock: jest.Mocked<LibraryAdministration>;
-	let contentTypeCacheMock: jest.Mocked<ContentTypeCache>;
+	let libraryStorageMock: DeepMocked<LibraryStorage>;
 
 	beforeAll(async () => {
 		module = await Test.createTestingModule({
@@ -26,35 +39,65 @@ describe('H5PLibraryManagementService', () => {
 					provide: ContentStorage,
 					useValue: createMock<ContentStorage>(),
 				},
-				{
-					provide: LibraryAdministration,
-					useValue: createMock<LibraryAdministration>(),
-				},
 			],
 		}).compile();
 
 		service = module.get(H5PLibraryManagementService);
 		libraryStorageMock = module.get(LibraryStorage);
-		libraryAdministrationMock = module.get(LibraryAdministration);
 	});
 
 	afterAll(async () => {
 		await module.close();
 	});
 
-	beforeEach(() => {
-		jest.resetAllMocks();
-	});
-
 	describe('uninstallUnwantedLibraries', () => {
-		it('should delete libraries not in the wanted list and with no dependents', async () => {});
+		it('should delete libraries not in the wanted list and with no dependents', async () => {
+			libraryStorageMock.deleteLibrary = jest.fn().mockResolvedValue({});
+			await service.uninstallUnwantedLibraries(['a', 'b']);
+			expect(libraryStorageMock.deleteLibrary).toHaveBeenCalledWith({ machineName: 'c', dependentsCount: 0 });
+			expect(libraryStorageMock.deleteLibrary).not.toHaveBeenCalledWith({ machineName: 'a', dependentsCount: 0 });
+		});
 
-		it('should not delete libraries with dependents', async () => {});
+		it('should not delete libraries with dependents', async () => {
+			libraryStorageMock.deleteLibrary = jest.fn().mockResolvedValue({});
+			const wantedLibraries = ['a', 'b'];
+			await service.uninstallUnwantedLibraries(wantedLibraries);
+			expect(libraryStorageMock.deleteLibrary).not.toHaveBeenCalledWith({ machineName: 'b', dependentsCount: 1 });
+		});
 	});
 
 	describe('installLibraries', () => {
-		it('should install all libraries in the list', async () => {});
+		it('should install all libraries in the list', async () => {
+			const wantedLibraries = ['a', 'b', 'c'];
+			const installContentTypeSpy = jest.spyOn(service.contentTypeRepo, 'installContentType').mockResolvedValue([]);
+			await service.installLibraries(wantedLibraries);
+			for (const libName of wantedLibraries) {
+				expect(installContentTypeSpy).toHaveBeenCalledWith(libName, expect.anything());
+			}
+		});
 
-		it('should throw an error if the library does not exist', async () => {});
+		it('should throw an error if the library does not exist', async () => {
+			const nonExistentLibrary = 'nonExistentLibrary';
+			jest.spyOn(service.contentTypeCache, 'get').mockResolvedValue(undefined as unknown as Promise<IHubContentType[]>);
+			await expect(service.installLibraries([nonExistentLibrary])).rejects.toThrow('this library does not exist');
+		});
+	});
+
+	describe('run', () => {
+		it('should trigger uninstallUnwantedLibraries and installLibraries', async () => {
+			const uninstallSpy = jest.spyOn(service, 'uninstallUnwantedLibraries');
+			const installSpy = jest.spyOn(service, 'installLibraries');
+
+			uninstallSpy.mockResolvedValue(undefined);
+			installSpy.mockResolvedValue(undefined);
+
+			await service.run();
+
+			expect(uninstallSpy).toHaveBeenCalledWith(service.libraryWishList);
+			expect(installSpy).toHaveBeenCalledWith(service.libraryWishList);
+
+			uninstallSpy.mockRestore();
+			installSpy.mockRestore();
+		});
 	});
 });
