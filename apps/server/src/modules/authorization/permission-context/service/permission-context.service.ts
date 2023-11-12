76d74e838c46148dd05643d2f27e07d1cca5dc65
Omar Ezzat <omar.ezzat@dataport.de>
2023-11-12 17:55:13 +0100

    add backbone for PermissionContextService

--- apps/server/src/modules/authorization/permission-context/service/permission-context.service.ts
+++ apps/server/src/modules/authorization/permission-context/service/permission-context.service.ts
@@ -1,3 +1,15 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { BaseDO, EntityId, User, PermissionContextEntity } from '@shared/domain';
+import { UserRepo } from '@shared/repo';
+import { ObjectId } from 'bson';
+
+
 @Injectable()
 export class PermissionContextService {
+	constructor(private readonly userRepo: UserRepo) {}
+
+    public resolvePermissions(user: User, contextReference: ObjectId): string[] {
+
+        throw "Not implemented";
+    }
 }
---
+++ apps/server/src/shared/domain/entity/permission-context.entity.ts
@@ -0,0 +1,59 @@
+import { Entity, Index, ManyToOne, Property } from '@mikro-orm/core';
+import { ObjectId } from 'bson';
+import { BaseEntityWithTimestamps } from './base.entity';
+import { Role } from './role.entity';
+import { Permission } from '../interface';
+
+export interface IPermissionContextProperties {
+	contextReference: ObjectId;
+	role: Role | null;
+	include_permissions: Permission[];
+	exclude_permissions: Permission[];
+	parent_context: PermissionContextEntity | null;
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+// TODO: add test
+@Entity({ tableName: 'permission-context' })
+export class PermissionContextEntity extends BaseEntityWithTimestamps {
+	@Property()
+	@Index()
+	contextReference: ObjectId;
+
+	@Property()
+	@Index()
+	@ManyToOne(() => Role, { nullable: true })
+	role: Role | null;
+
+	@Property()
+	included_permissions: Permission[] = [];
+
+	@Property()
+	excluded_permissions: Permission[] = [];
+
+	@Property()
+	@Index()
+	parent_context: PermissionContextEntity | null;
+
+	constructor(props: IPermissionContextProperties) {
+		super();
+		this.contextReference = props.contextReference;
+		this.role = props.role;
+		this.included_permissions = props.include_permissions;
+		this.excluded_permissions = props.exclude_permissions;
+		this.parent_context = props.parent_context;
+	}
+
+	public resolvedPermissions(): Permission[] {
+		const parentContextPermissions: Permission[] = this.parent_context?.resolvedPermissions() ?? [];
+		const rolePermissions = this.role?.resolvePermissions() ?? [];
+
+		const finalPermissions = parentContextPermissions
+			.concat(rolePermissions as Permission[])
+			.concat(this.included_permissions)
+			.filter((permission) => !this.excluded_permissions.includes(permission));
+
+		return finalPermissions;
+	}
+}
