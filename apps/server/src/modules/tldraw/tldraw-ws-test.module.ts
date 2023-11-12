a97b0a1cc49d2e7e2d53dbe342212f35298483a4
Tomasz Wiaderek <Tomasz.Wiaderek@gca.pass-consulting.com>
2023-11-12 15:42:42 +0100

    setup redis

--- apps/server/src/modules/tldraw/tldraw-ws-test.module.ts
+++ apps/server/src/modules/tldraw/tldraw-ws-test.module.ts
@@ -10,7 +10,7 @@ import { config } from './config';
 import { TldrawWs } from './controller';
 
 const imports = [CoreModule, ConfigModule.forRoot(createConfigModuleOptions(config))];
-const providers = [TldrawWs, TldrawBoardRepo, TldrawWsService];
+const providers = [TldrawWs, TldrawBoardRepo, TldrawWsService, IoredisModule];
 @Module({
 	imports,
 	providers,
@@ -19,7 +19,7 @@ export class TldrawWsTestModule {
 	static forRoot(options?: MongoDatabaseModuleOptions): DynamicModule {
 		return {
 			module: TldrawWsTestModule,
-			imports: [...imports, MongoMemoryDatabaseModule.forRoot({ ...options }), IoredisModule],
+			imports: [...imports, MongoMemoryDatabaseModule.forRoot({ ...options })],
 			providers,
 		};
 	}
