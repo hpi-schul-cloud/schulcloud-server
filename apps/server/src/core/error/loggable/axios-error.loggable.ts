6c0af3c44ca2b95f84531bdfcf62f54a5565dadc
Arne Gnisa <arne.gnisa@capgemini.com>
2023-11-13 08:59:04 +0100

    N21-1456 review changes 2

--- apps/server/src/core/error/loggable/axios-error.loggable.ts
+++ apps/server/src/core/error/loggable/axios-error.loggable.ts
@@ -1,10 +1,12 @@
-import { HttpException } from '@nestjs/common';
+import { HttpException, HttpStatus } from '@nestjs/common';
 import { ErrorLogMessage, Loggable, LogMessage, ValidationErrorLogMessage } from '@src/core/logger';
 import { AxiosError } from 'axios';
 
 export class AxiosErrorLoggable extends HttpException implements Loggable {
 	constructor(private readonly axiosError: AxiosError, protected readonly type: string) {
-		super(axiosError.response?.data as string, axiosError.status ?? 500, { cause: axiosError.cause });
+		super(JSON.stringify(axiosError.response?.data), axiosError.status ?? HttpStatus.INTERNAL_SERVER_ERROR, {
+			cause: axiosError.cause,
+		});
 	}
 
 	getLogMessage(): LogMessage | ErrorLogMessage | ValidationErrorLogMessage {
