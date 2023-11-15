ba8a8b9f2b3d99d69ccac96e37a58a52ea2e0bb0
Bartosz Nowicki <116367402+bn-pass@users.noreply.github.com>
2023-11-15 01:59:47 +0100

    add an interface for the deletion execution trigger result, add builder for it as well

--- apps/server/src/modules/deletion/uc/builder/index.ts
+++ apps/server/src/modules/deletion/uc/builder/index.ts
@@ -1,2 +1,3 @@
 export * from './batch-deletion-summary-detail.builder';
 export * from './batch-deletion-summary.builder';
+export * from './deletion-execution-trigger-result.builder';
--- apps/server/src/modules/deletion/uc/interface/index.ts
+++ apps/server/src/modules/deletion/uc/interface/index.ts
@@ -1,4 +1,6 @@
 export * from './batch-deletion-summary-overall-status.enum';
 export * from './batch-deletion-summary-detail.interface';
 export * from './batch-deletion-summary.interface';
+export * from './deletion-execution-trigger-status.enum';
+export * from './deletion-execution-trigger-result';
 export * from './interfaces';
