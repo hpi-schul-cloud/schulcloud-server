8a6be9ad3d67454cdfa3a5e704268e8585009916
Mrika Llabani <mrika.llabani@capgemini.com>
2023-11-16 06:56:56 +0100

    N21-1319 clean barrelfile

--- apps/server/src/modules/tool/external-tool/loggable/index.ts
+++ apps/server/src/modules/tool/external-tool/loggable/index.ts
@@ -3,4 +3,3 @@ export * from './external-tool-logo-size-exceeded-loggable-exception';
 export * from './external-tool-logo-fetched-loggable';
 export * from './external-tool-logo-fetch-failed-loggable-exception';
 export * from './external-tool-logo-wrong-file-type-loggable-exception';
-export * from './external-tool-metadata-loggable';
