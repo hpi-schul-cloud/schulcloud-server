7046e3c07d3ceaa13964921ec0b218e0c76556a1
Caspar Neumann <caspar.neumann@capgemini.com>
2023-11-17 14:16:04 +0100

    Revert "review comment: solve use of config.get"
    This reverts commit 8045a528dfd709bd0669ec3f560d20bb6c6a6a5f.

--- apps/server/src/modules/h5p-library-management/h5p-library-management.config.ts
+++
@@ -1,3 +0,0 @@
-export interface H5pLibraryManagementConfig {
-	libraryListPath: string;
-}
--- apps/server/src/modules/h5p-library-management/service/h5p-library-management.service.ts
+++ apps/server/src/modules/h5p-library-management/service/h5p-library-management.service.ts
@@ -12,10 +12,9 @@ import ContentManager from '@lumieducation/h5p-server/build/src/ContentManager';
 import ContentTypeInformationRepository from '@lumieducation/h5p-server/build/src/ContentTypeInformationRepository';
 import { Injectable } from '@nestjs/common';
 import { ContentStorage, LibraryStorage } from '@src/modules/h5p-editor';
+import { Configuration } from '@hpi-schul-cloud/commons';
 import { readFileSync } from 'fs';
 import { parse } from 'yaml';
-import { ConfigService } from '@nestjs/config';
-import { H5pLibraryManagementConfig } from '../h5p-library-management.config';
 
 const h5pConfig = new H5PConfig(undefined, {
 	baseUrl: '/api/v3/h5p-editor',
@@ -35,13 +34,7 @@ export class H5PLibraryManagementService {
 
 	libraryWishList: string[];
 
-	libraryListPath: string;
-
-	constructor(
-		private readonly libraryStorage: LibraryStorage,
-		private readonly contentStorage: ContentStorage,
-		private configService: ConfigService
-	) {
+	constructor(private readonly libraryStorage: LibraryStorage, private readonly contentStorage: ContentStorage) {
 		const kvCache = new cacheImplementations.CachedKeyValueStorage('kvcache');
 		this.contentTypeCache = new ContentTypeCache(h5pConfig, kvCache);
 		this.libraryManager = new LibraryManager(
@@ -56,12 +49,7 @@ export class H5PLibraryManagementService {
 		this.contentTypeRepo = new ContentTypeInformationRepository(this.contentTypeCache, this.libraryManager, h5pConfig);
 		const contentManager = new ContentManager(this.contentStorage);
 		this.libraryAdministration = new LibraryAdministration(this.libraryManager, contentManager);
-		const config = this.configService.get<H5pLibraryManagementConfig>(
-			'H5P_EDITOR__LIBRARY_LIST_PATH'
-		) as H5pLibraryManagementConfig;
-		this.libraryListPath = config.libraryListPath;
-
-		const librariesYamlContent = readFileSync(this.libraryListPath, {
+		const librariesYamlContent = readFileSync(Configuration.get('H5P_EDITOR__LIBRARY_LIST_PATH') as string, {
 			encoding: 'utf-8',
 		});
 		this.libraryWishList = (parse(librariesYamlContent) as { h5p_libraries: string[] }).h5p_libraries;
