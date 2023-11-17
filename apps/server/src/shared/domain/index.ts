a1cef1b4a728a626a166ece31f97fee907d8d5db
wolfganggreschus <wolfgang@greschus.de>
2023-11-17 13:45:42 +0100

    initial commit

--- apps/server/src/shared/domain/index.ts
+++ apps/server/src/shared/domain/index.ts
@@ -4,3 +4,9 @@ export * from './interface';
 export * from './service';
 export * from './types';
 // export * from './domain-object'; ~ Do not work for the moment, first import exports must be cleanup.
+/*
+
+    Import external dependencies directly, not from here.
+    Avoid loading all external references for testing.
+    Do not re-export here.
+    */
