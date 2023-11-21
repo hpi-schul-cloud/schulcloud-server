8ccf2d6291d324f02c047eccc8ccd54deb793644
Szymon Szafoni <szymon.szafoni@gca.pass-consulting.com>
2023-11-21 15:53:42 +0100

    fix lint

--- apps/server/src/modules/lesson/index.ts
+++ apps/server/src/modules/lesson/index.ts
@@ -1,6 +1,4 @@
 export * from './lesson.module';
-export * from './service/lesson-copy.service';
-export * from './service/lesson.service';
 export * from './types/lesson-copy-parent.params';
 export * from './types/lesson-copy.params';
 export * from './service';
