d5e620d3e1cb4e68d20dd60903c37afd04c1e92a
virgilchiriac <virgil.chiriac@dataport.de>
2023-11-22 11:38:15 +0100

    updateMany repo helper - fixes classes.repo.integration.test
    TODO Check this https://mongoosejs.com/docs/6.x/docs/api/query.html#query_Query-updateMany
    the given example references nModified, so why must we change it to modifiedCount?

--- src/components/helper/repo.helper.js
+++ src/components/helper/repo.helper.js
@@ -1,17 +1,21 @@
 const { error } = require('../../logger');
 
 /**
- * Converts an mongoose update many result to an internal TO
+ * TODO Check this https://mongoosejs.com/docs/6.x/docs/api/query.html#query_Query-updateMany
+ * the given example references nModified, so why must we change it to modifiedCount?
+ *
+ *
+ * * Converts an mongoose update many result to an internal TO
  * @param {*} param0
  * @param {1|0} param0.ok 0 for error
  * @param {Integer} param0.n Number of elements matched the given filter
- * @param {Integer} param0.nModified Number of elements updated
+ * @param {Integer} param0.modifiedCount Number of elements updated
  */
-const updateManyResult = ({ ok, n, nModified }) => {
+const updateManyResult = ({ ok, n, modifiedCount }) => {
 	if (ok !== 1) {
-		error('mongoose updateMany has failed', { ok, n, nModified });
+		error('mongoose updateMany has failed', { ok, n, modifiedCount });
 	}
-	return { success: ok === 1, modifiedDocuments: nModified };
+	return { success: ok === 1, modifiedDocuments: modifiedCount };
 };
 
 /**
