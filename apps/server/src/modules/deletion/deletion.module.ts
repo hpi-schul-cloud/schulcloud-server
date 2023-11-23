994262f29211d306d724d40b85db90a6ce6655ad
Szymon Szafoni <szymon.szafoni@gca.pass-consulting.com>
2023-11-23 20:46:37 +0100

    fix providers in exports in deletion module

--- apps/server/src/modules/deletion/deletion.module.ts
+++ apps/server/src/modules/deletion/deletion.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { CourseGroupRepo } from '@shared/repo';
 import { Configuration } from '@hpi-schul-cloud/commons';
 import { LoggerModule } from '@src/core/logger';
 import { AuthenticationModule } from '@modules/authentication/authentication.module';
@@ -12,6 +11,7 @@ import { PseudonymModule } from '@modules/pseudonym';
 import { TeamsModule } from '@modules/teams';
 import { UserModule } from '@modules/user';
 import { RocketChatModule } from '@modules/rocketchat';
+import { FilesModule } from '@modules/files';
 import { DeletionRequestService } from './services/deletion-request.service';
 import { DeletionRequestRepo } from './repo/deletion-request.repo';
 import { DeletionRequestsController } from './controller/deletion-requests.controller';
@@ -19,12 +19,8 @@ import { DeletionExecutionsController } from './controller/deletion-executions.c
 import { XApiKeyConfig } from '../authentication/config/x-api-key.config';
 import { DeletionRequestUc } from './uc/deletion-request.uc';
 import { DeletionLogService } from './services/deletion-log.service';
-import { CourseGroupService } from '../learnroom/service';
-import { FilesService } from '../files/service';
-import { RocketChatUserService } from '../rocketchat-user';
+import { RocketChatUserModule } from '../rocketchat-user';
 import { DeletionLogRepo } from './repo';
-import { FilesRepo } from '../files/repo';
-import { RocketChatUserRepo } from '../rocketchat-user/repo';
 
 @Module({
 	imports: [
@@ -44,22 +40,18 @@ import { RocketChatUserRepo } from '../rocketchat-user/repo';
 			adminUser: Configuration.get('ROCKET_CHAT_ADMIN_USER') as string,
 			adminPassword: Configuration.get('ROCKET_CHAT_ADMIN_PASSWORD') as string,
 		}),
+		RocketChatUserModule,
+		FilesModule,
 	],
 	controllers: [DeletionRequestsController, DeletionExecutionsController],
 	providers: [
-		DeletionRequestService,
 		DeletionRequestRepo,
+		DeletionLogRepo,
 		ConfigService<XApiKeyConfig, true>,
 		DeletionRequestUc,
 		DeletionLogService,
-		CourseGroupService,
-		FilesService,
-		DeletionLogRepo,
-		CourseGroupRepo,
-		FilesRepo,
-		RocketChatUserService,
-		RocketChatUserRepo,
+		DeletionRequestService,
 	],
-	exports: [DeletionRequestService],
+	exports: [DeletionRequestService, DeletionLogService],
 })
 export class DeletionModule {}
