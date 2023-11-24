9e1e044fef25b135e867580f23b91a2a8cd7e125
Tomasz Wiaderek <Tomasz.Wiaderek@gca.pass-consulting.com>
2023-11-24 18:21:54 +0100

    remove unused code

--- apps/server/src/modules/tldraw/repo/utils.ts
+++ apps/server/src/modules/tldraw/repo/utils.ts
@@ -4,18 +4,11 @@ import * as binary from 'lib0/binary';
 import * as encoding from 'lib0/encoding';
 import { Buffer } from 'buffer';
 import { applyUpdate, Doc, encodeStateAsUpdate, encodeStateVector } from 'yjs';
-import * as decoding from 'lib0/decoding';
 
 const MAX_DOCUMENT_SIZE = 15000000;
 
 /**
  * Remove all documents from db with Clock between $from and $to
- *
- * @param {any} db
- * @param {string} docName
- * @param {number} from Greater than or equal
- * @param {number} to lower than (not equal)
- * @return {Promise<void>}
  */
 export const clearUpdatesRange = async (db: MongodbAdapter, docName: string, from: number, to: number) =>
 	db.del({
@@ -28,9 +21,6 @@ export const clearUpdatesRange = async (db: MongodbAdapter, docName: string, fro
 
 /**
  * Create a unique key for a update message.
- * @param {string} docName
- * @param {number} [clock] must be unique
- * @return {Object} [opts.version, opts.docName, opts.action, opts.clock]
  */
 export const createDocumentUpdateKey = (docName: string, clock?: number) => {
 	if (clock !== undefined) {
@@ -48,11 +38,6 @@ export const createDocumentUpdateKey = (docName: string, clock?: number) => {
 	};
 };
 
-/**
- * We have a separate state vector key so we can iterate efficiently over all documents
- * @param {string} docName
- * @return {Object} [opts.docName, opts.version]
- */
 export const createDocumentStateVectorKey = (docName: string) => {
 	return {
 		docName,
@@ -60,56 +45,27 @@ export const createDocumentStateVectorKey = (docName: string) => {
 	};
 };
 
-/**
- * @param {string} docName
- * @param {string} metaKey
- * @return {Object} [opts.docName, opts.version, opts.docType, opts.metaKey]
- */
-export const createDocumentMetaKey = (docName: string, metaKey: string) => {
-	return {
-		version: 'v1',
-		docName,
-		metaKey: `meta_${metaKey}`,
-	};
-};
-
-/**
- * @param {any} db
- * @param {object} query
- * @param {object} opts
- * @return {Promise<any[]>}
- */
 export const getMongoBulkData = (db: MongodbAdapter, query: object, opts: object) => db.readAsCursor(query, opts);
 
 /**
  * Convert the mongo document array to an array of values (as buffers)
- *
- * @param {any[]} docs
- * @return {Buffer[]}
  */
 const convertMongoUpdates = (docs: TldrawDrawing[]) => {
 	if (!Array.isArray(docs) || !docs.length) return [];
 
 	const updates: Buffer[] = [];
-	// eslint-disable-next-line no-plusplus
 	for (let i = 0; i < docs.length; i++) {
 		const doc = docs[i];
 		if (!doc.part) {
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-ignore
 			updates.push(doc.value.buffer);
 		} else if (doc.part === 1) {
 			// merge the docs together that got split because of mongodb size limits
 			const parts = [Buffer.from(doc.value.buffer)];
 			let j;
 			let currentPartId: number | undefined = doc.part;
-			// eslint-disable-next-line no-plusplus
 			for (j = i + 1; j < docs.length; j++) {
-				// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 				const part = docs[j];
 				if (part.clock === doc.clock) {
-					// eslint-disable-next-line no-unsafe-optional-chaining,@typescript-eslint/ban-ts-comment
-					// @ts-ignore
 					if (currentPartId !== part.part - 1) {
 						throw new Error('Couldnt merge updates together because a part is missing!');
 					}
@@ -127,22 +83,12 @@ const convertMongoUpdates = (docs: TldrawDrawing[]) => {
 
 /**
  * Get all document updates for a specific document.
- *
- * @param {any} db
- * @param {string} docName
- * @param {any} [opts]
- * @return {Promise<any[]>}
  */
 export const getMongoUpdates = async (db: MongodbAdapter, docName: string, opts = {}) => {
 	const docs = await getMongoBulkData(db, createDocumentUpdateKey(docName), opts);
 	return convertMongoUpdates(docs);
 };
 
-/**
- * @param {any} db
- * @param {string} docName
- * @return {Promise<number>} Returns -1 if this document doesn't exist yet
- */
 export const getCurrentUpdateClock = (db: MongodbAdapter, docName: string) =>
 	getMongoBulkData(
 		db,
@@ -161,13 +107,6 @@ export const getCurrentUpdateClock = (db: MongodbAdapter, docName: string) =>
 		return updates[0].clock;
 	});
 
-/**
- * @param {any} db
- * @param {string} docName
- * @param {Uint8Array} sv state vector
- * @param {number} clock current clock of the document so we can determine
- * when this statevector was created
- */
 export const writeStateVector = async (db: MongodbAdapter, docName: string, sv: Uint8Array, clock: number) => {
 	const encoder = encoding.createEncoder();
 	encoding.writeVarUint(encoder, clock);
@@ -177,15 +116,7 @@ export const writeStateVector = async (db: MongodbAdapter, docName: string, sv: 
 	});
 };
 
-/**
- * @param {any} db
- * @param {string} docName
- * @param {Uint8Array} update
- * @return {Promise<number>} Returns the clock of the stored update
- */
 export const storeUpdate = async (db: MongodbAdapter, docName: string, update: Uint8Array) => {
-	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-	// @ts-ignore
 	const clock: number = await getCurrentUpdateClock(db, docName);
 	if (clock === -1) {
 		// make sure that a state vector is always written, so we can search for available documents
@@ -196,7 +127,6 @@ export const storeUpdate = async (db: MongodbAdapter, docName: string, update: U
 	}
 
 	const value = Buffer.from(update);
-	// mongodb has a maximum document size of 16MB;
 	//  if our buffer exceeds it, we store the update in multiple documents
 	if (value.length <= MAX_DOCUMENT_SIZE) {
 		await db.put(createDocumentUpdateKey(docName, clock + 1), {
@@ -206,7 +136,6 @@ export const storeUpdate = async (db: MongodbAdapter, docName: string, update: U
 		const totalChunks = Math.ceil(value.length / MAX_DOCUMENT_SIZE);
 
 		const putPromises: Promise<any>[] = [];
-		// eslint-disable-next-line no-plusplus
 		for (let i = 0; i < totalChunks; i++) {
 			const start = i * MAX_DOCUMENT_SIZE;
 			const end = Math.min(start + MAX_DOCUMENT_SIZE, value.length);
@@ -224,61 +153,19 @@ export const storeUpdate = async (db: MongodbAdapter, docName: string, update: U
 /**
  * For now this is a helper method that creates a Y.Doc and then re-encodes a document update.
  * In the future this will be handled by Yjs without creating a Y.Doc (constant memory consumption).
- *
- * @param {Array<Uint8Array>} updates
- * @return {{update:Uint8Array, sv: Uint8Array}}
  */
 export const mergeUpdates = (updates: Array<Uint8Array>) => {
 	const ydoc = new Doc();
 	ydoc.transact(() => {
-		// eslint-disable-next-line no-plusplus
-		for (let i = 0; i < updates.length; i++) {
-			applyUpdate(ydoc, updates[i]);
+		for (const element of updates) {
+			applyUpdate(ydoc, element);
 		}
 	});
 	return { update: encodeStateAsUpdate(ydoc), sv: encodeStateVector(ydoc) };
 };
 
-export const decodeMongodbStateVector = (buf) => {
-	let decoder;
-	if (Buffer.isBuffer(buf)) {
-		decoder = decoding.createDecoder(buf);
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-	} else if (Buffer.isBuffer(buf?.buffer)) {
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-member-access
-		decoder = decoding.createDecoder(buf.buffer);
-	} else {
-		throw new Error('No buffer provided at decodeMongodbStateVector()');
-	}
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-	const clock = decoding.readVarUint(decoder);
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-	const sv = decoding.readVarUint8Array(decoder);
-	return { sv, clock };
-};
-
-/**
- * @param {any} db
- * @param {string} docName
- */
-export const readStateVector = async (db: MongodbAdapter, docName: string) => {
-	const doc = await db.get({ ...createDocumentStateVectorKey(docName) });
-	if (!doc?.value) {
-		// no state vector created yet or no document exists
-		return { sv: null, clock: -1 };
-	}
-	return decodeMongodbStateVector(doc.value);
-};
-
-export const getAllSVDocs = async (db: MongodbAdapter) => db.readAsCursor({ version: 'v1_sv' });
-
 /**
  * Merge all MongoDB documents of the same yjs document together.
- * @param {any} db
- * @param {string} docName
- * @param {Uint8Array} stateAsUpdate
- * @param {Uint8Array} stateVector
- * @return {Promise<number>} returns the clock of the flushed doc
  */
 export const flushDocument = async (
 	db: MongodbAdapter,
--- apps/server/src/modules/tldraw/repo/y-mongodb.ts
+++ apps/server/src/modules/tldraw/repo/y-mongodb.ts
@@ -31,19 +31,15 @@ export class YMongodb {
 				this.tr[docName] = promise.resolve();
 			}
 
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 			const currTr = this.tr[docName];
 			let nextTr = null;
 
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-ignore
+
 			nextTr = (async () => {
 				await currTr;
 
 				let res = null;
 				try {
-					// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/ban-ts-comment
-					// @ts-ignore
 					res = await f(this.adapter);
 				} catch (err) {
 					// eslint-disable-next-line no-console
@@ -60,7 +56,6 @@ export class YMongodb {
 
 			this.tr[docName] = nextTr;
 
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-return
 			return this.tr[docName];
 		};
 	}
@@ -78,16 +73,12 @@ export class YMongodb {
 	}
 
 	getYDoc(docName: string): Promise<Doc> {
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-return
 		return this._transact(docName, async (db: MongodbAdapter): Promise<Doc> => {
 			const updates = await U.getMongoUpdates(db, docName);
 			const ydoc = new Doc();
 			ydoc.transact(() => {
-				// eslint-disable-next-line no-plusplus
 				for (let i = 0; i < updates.length; i++) {
 					applyUpdate(ydoc, updates[i]);
-					// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-					// @ts-ignore
 					updates[i] = null;
 				}
 			});
@@ -98,157 +89,11 @@ export class YMongodb {
 		});
 	}
 
-	/**
-	 * Store a single document update to the database.
-	 *
-	 * @param {string} docName
-	 * @param {Uint8Array} update
-	 * @return {Promise<number>} Returns the clock of the stored update
-	 */
 	storeUpdate(docName: string, update: Uint8Array): Promise<number> {
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-return
 		return this._transact(docName, (db: MongodbAdapter) => U.storeUpdate(db, docName, update));
 	}
 
-	/**
-	 * The state vector (describing the state of the persisted document - see https://github.com/yjs/yjs#Document-Updates) is maintained in a separate field and constantly updated.
-	 *
-	 * This allows you to sync changes without actually creating a Yjs document.
-	 *
-	 * @param {string} docName
-	 * @return {Promise<Uint8Array>}
-	 */
-	getStateVector(docName: string) {
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-return,@typescript-eslint/no-unsafe-call
-		return this._transact(docName, async (db: MongodbAdapter) => {
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-			const { clock, sv } = await U.readStateVector(db, docName);
-			let curClock = -1;
-			if (sv !== null) {
-				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-				// @ts-ignore
-				curClock = await U.getCurrentUpdateClock(db, docName);
-			}
-			if (sv !== null && clock === curClock) {
-				return sv;
-			}
-			// current state vector is outdated
-			const updates = await U.getMongoUpdates(db, docName);
-			const { update, sv: newSv } = U.mergeUpdates(updates);
-			await U.flushDocument(db, docName, update, newSv);
-			return newSv;
-		});
-	}
-
-	/**
-	 * Get the differences directly from the database.
-	 * The same as Y.encodeStateAsUpdate(ydoc, stateVector).
-	 * @param {string} docName
-	 * @param {Uint8Array} stateVector
-	 */
-	async getDiff(docName: string, stateVector: Uint8Array) {
-		const ydoc = await this.getYDoc(docName);
-		return encodeStateAsUpdate(ydoc, stateVector);
-	}
-
-	/**
-	 * Persist some meta information in the database and associate it
-	 * with a document. It is up to you what you store here.
-	 * You could, for example, store credentials here.
-	 *
-	 * @param {string} docName
-	 * @param {string} metaKey
-	 * @param {any} value
-	 * @return {Promise<void>}
-	 */
-	setMeta(docName: string, metaKey: string, value: object) {
-		/*	Unlike y-leveldb, we simply store the value here without encoding
-	 		 it in a buffer beforehand. */
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-return
-		return this._transact(docName, async (db: MongodbAdapter) => {
-			await db.put(U.createDocumentMetaKey(docName, metaKey), { value });
-		});
-	}
-
-	/**
-	 * Retrieve a store meta value from the database. Returns undefined if the
-	 * metaKey doesn't exist.
-	 *
-	 * @param {string} docName
-	 * @param {string} metaKey
-	 * @return {Promise<any>}
-	 */
-	getMeta(docName: string, metaKey: string) {
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-return
-		return this._transact(docName, async (db: MongodbAdapter) => {
-			const res = await db.get({
-				...U.createDocumentMetaKey(docName, metaKey),
-			});
-			if (!res?.value) {
-				return undefined;
-			}
-			return res.value;
-		});
-	}
-
-	/**
-	 * Delete a store meta value.
-	 *
-	 * @param {string} docName
-	 * @param {string} metaKey
-	 * @return {Promise<any>}
-	 */
-	delMeta(docName: string, metaKey: string) {
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-return,@typescript-eslint/no-unsafe-call
-		return this._transact(docName, (db: MongodbAdapter) =>
-			db.del({
-				...U.createDocumentMetaKey(docName, metaKey),
-			})
-		);
-	}
-
-	/**
-	 * Retrieve the names of all stored documents.
-	 *
-	 * @return {Promise<string[]>}
-	 */
-	getAllDocNames() {
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-return,@typescript-eslint/no-unsafe-call
-		return this._transact('global', async (db: MongodbAdapter) => {
-			const docs = await U.getAllSVDocs(db);
-			return docs.map((doc) => doc.docName);
-		});
-	}
-
-	/**
-	 * Retrieve the state vectors of all stored documents.
-	 * You can use this to sync two y-leveldb instances.
-	 * !Note: The state vectors might be outdated if the associated document
-	 * is not yet flushed. So use with caution.
-	 * @return {Promise<{ name: string, sv: Uint8Array, clock: number }[]>}
-	 * @todo may not work?
-	 */
-	getAllDocStateVectors() {
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-return,@typescript-eslint/no-unsafe-call
-		return this._transact('global', async (db: MongodbAdapter) => {
-			const docs = await U.getAllSVDocs(db);
-			return docs.map((doc) => {
-				const { sv, clock } = U.decodeMongodbStateVector(doc.value);
-				return { name: doc.docName, sv, clock };
-			});
-		});
-	}
-
-	/**
-	 * Internally y-mongodb stores incremental updates. You can merge all document
-	 * updates to a single entry. You probably never have to use this.
-	 * It is done automatically every $options.flushsize (default 400) transactions.
-	 *
-	 * @param {string} docName
-	 * @return {Promise<void>}
-	 */
 	flushDocument(docName: string) {
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-return,@typescript-eslint/no-unsafe-call
 		return this._transact(docName, async (db: MongodbAdapter) => {
 			const updates = await U.getMongoUpdates(db, docName);
 			const { update, sv } = U.mergeUpdates(updates);
