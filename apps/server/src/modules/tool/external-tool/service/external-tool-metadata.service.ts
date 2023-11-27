537571d8c19e4e8e53b3958b99fd53046a32bcdc
Marvin Öhlerking <103562092+MarvinOehlerkingCap@users.noreply.github.com>
2023-11-27 09:13:39 +0100

    N21-1512 Fix SchoolExternalTool metadata (#4580)

--- apps/server/src/modules/tool/external-tool/service/external-tool-metadata.service.ts
+++ apps/server/src/modules/tool/external-tool/service/external-tool-metadata.service.ts
@@ -2,10 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { EntityId } from '@shared/domain';
 import { ContextExternalToolRepo, SchoolExternalToolRepo } from '@shared/repo';
 import { ToolContextType } from '../../common/enum';
+import { ToolContextMapper } from '../../common/mapper/tool-context.mapper';
 import { ContextExternalToolType } from '../../context-external-tool/entity';
 import { SchoolExternalTool } from '../../school-external-tool/domain';
 import { ExternalToolMetadata } from '../domain';
-import { ToolContextMapper } from '../../common/mapper/tool-context.mapper';
 
 @Injectable()
 export class ExternalToolMetadataService {
@@ -40,11 +40,11 @@ export class ExternalToolMetadataService {
 			);
 		}
 
-		const externaltoolMetadata: ExternalToolMetadata = new ExternalToolMetadata({
+		const externalToolMetadata: ExternalToolMetadata = new ExternalToolMetadata({
 			schoolExternalToolCount: schoolExternalTools.length,
 			contextExternalToolCountPerContext: contextExternalToolCount,
 		});
 
-		return externaltoolMetadata;
+		return externalToolMetadata;
 	}
 }
