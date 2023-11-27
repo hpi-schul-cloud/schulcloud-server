5e17fe9558fe0b936e8397d96c802ff37b68b8c3
wolfganggreschus <wolfgang@greschus.de>
2023-11-27 20:17:23 +0100

    removed comments

--- apps/server/src/shared/domain/index.ts
+++ apps/server/src/shared/domain/index.ts
@@ -1,9 +1,3 @@
-// export * from './domainobject';
-// export * from './entity';
-// export * from './interface';
-// export * from './service';
-// export * from './types';
-// export * from './domain-object'; ~ Do not work for the moment, first import exports must be cleanup.
 /*
 
     Import external dependencies directly, not from here.
