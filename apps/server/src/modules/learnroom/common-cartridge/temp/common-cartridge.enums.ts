f911c5a9bdeaa0d1e112bf8647db0e422f4e2779
Simone Radtke <simone.radtke@capgemini.com>
2023-11-28 13:50:21 +0100

    EW-539 Add resource type to enums

--- apps/server/src/modules/learnroom/common-cartridge/temp/common-cartridge.enums.ts
+++ apps/server/src/modules/learnroom/common-cartridge/temp/common-cartridge.enums.ts
@@ -4,3 +4,10 @@ export enum CommonCartridgeVersion {
 	V_1_2 = '1.2',
 	V_1_3 = '1.3',
 }
+
+export enum CommonCartridgeResourceType {
+	LTI = 'imsbasiclti_xmlv1p0',
+	WEB_CONTENT = 'webcontent',
+	WEB_LINK_V_1_1 = 'imswl_xmlv1p1',
+	WEB_LINK_V_1_3 = 'imswl_xmlv1p3',
+}
