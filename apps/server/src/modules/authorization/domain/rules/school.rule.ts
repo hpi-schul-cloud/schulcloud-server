d32bea8b435a57c04ab3ea6791fea02a0e911f9a
dyedwiper <maximilian.becker@dataport.de>
2023-11-29 12:19:58 +0100

    Remove param type BaseDo from school rule

--- apps/server/src/modules/authorization/domain/rules/school.rule.ts
+++ apps/server/src/modules/authorization/domain/rules/school.rule.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { AuthorizableObject } from '@shared/domain/domain-object';
-import { BaseDO } from '@shared/domain/domainobject';
 import { User } from '@shared/domain/entity';
 import { School } from '@src/modules/school/domain/do';
 import { AuthorizationHelper } from '../service/authorization.helper';
@@ -10,7 +9,7 @@ import { AuthorizationContext, Rule } from '../type';
 export class SchoolRule implements Rule {
 	constructor(private readonly authorizationHelper: AuthorizationHelper) {}
 
-	public isApplicable(user: User, object: AuthorizableObject | BaseDO): boolean {
+	public isApplicable(user: User, object: AuthorizableObject): boolean {
 		const isApplicable = object instanceof School;
 
 		return isApplicable;
