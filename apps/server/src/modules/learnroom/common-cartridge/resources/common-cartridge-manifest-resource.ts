f37f4829082c5c8b113e8963483707da8087e101
Simone Radtke <simone.radtke@capgemini.com>
2023-12-04 08:42:33 +0100

    EW-539 Correct namespaces by versions

--- apps/server/src/modules/learnroom/common-cartridge/resources/common-cartridge-manifest-resource.ts
+++ apps/server/src/modules/learnroom/common-cartridge/resources/common-cartridge-manifest-resource.ts
@@ -49,6 +49,18 @@ export class CommonCartridgeManifestResource implements CommonCartridgeResource 
 			case CommonCartridgeVersion.V_1_1_0:
 				return {
 					identifier: this.props.identifier,
+					xmlns: 'http://www.imsglobal.org/xsd/imsccv1p1/imscp_v1p1',
+					'xmlns:mnf': 'http://ltsc.ieee.org/xsd/imsccv1p1/LOM/manifest',
+					'xmlns:res': 'http://ltsc.ieee.org/xsd/imsccv1p1/LOM/resource',
+					'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
+					'xsi:schemaLocation':
+						'http://ltsc.ieee.org/xsd/imsccv1p1/LOM/resource http://www.imsglobal.org/profile/cc/ccv1p1/LOM/ccv1p1_lomresource_v1p0.xsd ' +
+						'http://www.imsglobal.org/xsd/imsccv1p1/imscp_v1p1 http://www.imsglobal.org/profile/cc/ccv1p1/ccv1p1_imscp_v1p2_v1p0.xsd ' +
+						'http://ltsc.ieee.org/xsd/imsccv1p1/LOM/manifest http://www.imsglobal.org/profile/cc/ccv1p1/LOM/ccv1p1_lommanifest_v1p0.xsd ',
+				};
+			case CommonCartridgeVersion.V_1_3_0:
+				return {
+					identifier: this.props.identifier,
 					xmlns: 'http://www.imsglobal.org/xsd/imsccv1p3/imscp_v1p1',
 					'xmlns:mnf': 'http://ltsc.ieee.org/xsd/imsccv1p3/LOM/manifest',
 					'xmlns:res': 'http://ltsc.ieee.org/xsd/imsccv1p3/LOM/resource',
@@ -59,18 +71,6 @@ export class CommonCartridgeManifestResource implements CommonCartridgeResource 
 						'http://www.imsglobal.org/xsd/imsccv1p3/imscp_v1p1 http://www.imsglobal.org/profile/cc/ccv1p3/ccv1p3_imscp_v1p2_v1p0.xsd ' +
 						'http://ltsc.ieee.org/xsd/imsccv1p3/LOM/manifest http://www.imsglobal.org/profile/cc/ccv1p3/LOM/ccv1p3_lommanifest_v1p0.xsd ' +
 						'http://www.imsglobal.org/xsd/imsccv1p3/imscp_extensionv1p2 http://www.imsglobal.org/profile/cc/ccv1p3/ccv1p3_cpextensionv1p2_v1p0.xsd',
-				};
-			case CommonCartridgeVersion.V_1_3_0:
-				return {
-					identifier: this.props.identifier,
-					xmlns: 'http://www.imsglobal.org/xsd/imsccv1p1/imscp_v1p1',
-					'xmlns:mnf': 'http://ltsc.ieee.org/xsd/imsccv1p1/LOM/manifest',
-					'xmlns:res': 'http://ltsc.ieee.org/xsd/imsccv1p1/LOM/resource',
-					'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
-					'xsi:schemaLocation':
-						'http://ltsc.ieee.org/xsd/imsccv1p1/LOM/resource http://www.imsglobal.org/profile/cc/ccv1p1/LOM/ccv1p1_lomresource_v1p0.xsd ' +
-						'http://www.imsglobal.org/xsd/imsccv1p1/imscp_v1p1 http://www.imsglobal.org/profile/cc/ccv1p1/ccv1p1_imscp_v1p2_v1p0.xsd ' +
-						'http://ltsc.ieee.org/xsd/imsccv1p1/LOM/manifest http://www.imsglobal.org/profile/cc/ccv1p1/LOM/ccv1p1_lommanifest_v1p0.xsd ',
 				};
 			default:
 				throw createVersionNotSupportedError(this.props.version);
