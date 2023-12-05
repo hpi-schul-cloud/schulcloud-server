a7509c7b9ee362e6e680d56942393c1e1667ffd5
aimee-889 <aimee.weiss@capgemini.com>
2023-12-05 15:38:43 +0000

    change var name and add them to schema

--- apps/server/src/modules/h5p-editor/h5p-editor.config.ts
+++ apps/server/src/modules/h5p-editor/h5p-editor.config.ts
@@ -27,8 +27,8 @@ export const s3ConfigLibraries: S3Config = {
 	endpoint: Configuration.get('H5P_EDITOR__S3_ENDPOINT') as string,
 	region: Configuration.get('H5P_EDITOR__S3_REGION') as string,
 	bucket: Configuration.get('H5P_EDITOR__S3_BUCKET_LIBRARIES') as string,
-	accessKeyId: Configuration.get('H5P_LIBRARY__S3_ACCESS_KEY_ID') as string,
-	secretAccessKey: Configuration.get('H5P_LIBRARY__S3_SECRET_ACCESS_KEY') as string,
+	accessKeyId: Configuration.get('H5P_EDITOR__LIBRARIES_S3_ACCESS_KEY_ID') as string,
+	secretAccessKey: Configuration.get('H5P_EDITOR__LIBRARIES_S3_SECRET_ACCESS_KEY') as string,
 };
 
 export const config = () => h5pEditorConfig;
