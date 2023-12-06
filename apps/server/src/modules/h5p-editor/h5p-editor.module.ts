f459d427793d27c5e5e0ecb85f4ee8a825718198
Constantin Bergatt <constantin.bergatt@dataport.de>
2023-12-06 11:16:30 +0100

    BC-5838 - remove `ALL_ENTITIES` from `h5p-editor` module

--- apps/server/src/modules/h5p-editor/h5p-editor.module.ts
+++ apps/server/src/modules/h5p-editor/h5p-editor.module.ts
@@ -4,11 +4,64 @@ import { Dictionary, IPrimaryKey } from '@mikro-orm/core';
 import { MikroOrmModule, MikroOrmModuleSyncOptions } from '@mikro-orm/nestjs';
 import { AuthenticationModule } from '@modules/authentication';
 import { AuthorizationReferenceModule } from '@modules/authorization/authorization-reference.module';
+import { ClassEntity } from '@modules/class/entity';
+import { GroupEntity } from '@modules/group/entity';
+import { ExternalToolPseudonymEntity, PseudonymEntity } from '@modules/pseudonym/entity';
+import { RegistrationPinEntity } from '@modules/registration-pin/entity';
+import { ShareToken } from '@modules/sharing/entity/share-token.entity';
+import { ContextExternalToolEntity } from '@modules/tool/context-external-tool/entity';
+import { ExternalToolEntity } from '@modules/tool/external-tool/entity';
+import { SchoolExternalToolEntity } from '@modules/tool/school-external-tool/entity';
 import { UserModule } from '@modules/user';
 import { Module, NotFoundException } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ALL_ENTITIES } from '@shared/domain/entity';
-import { DB_PASSWORD, DB_URL, DB_USERNAME, createConfigModuleOptions } from '@src/config';
+import {
+	Account,
+	Board,
+	BoardElement,
+	BoardNode,
+	CardNode,
+	ColumnboardBoardElement,
+	ColumnBoardNode,
+	ColumnBoardTarget,
+	ColumnNode,
+	Course,
+	CourseGroup,
+	CourseNews,
+	DashboardGridElementModel,
+	DashboardModelEntity,
+	ExternalToolElementNodeEntity,
+	FederalStateEntity,
+	FileElementNode,
+	ImportUser,
+	LessonBoardElement,
+	LessonEntity,
+	LinkElementNode,
+	LtiTool,
+	Material,
+	News,
+	RichTextElementNode,
+	Role,
+	SchoolEntity,
+	SchoolNews,
+	SchoolRolePermission,
+	SchoolRoles,
+	SchoolYearEntity,
+	StorageProviderEntity,
+	Submission,
+	SubmissionContainerElementNode,
+	SubmissionItemNode,
+	SystemEntity,
+	Task,
+	TaskBoardElement,
+	TeamEntity,
+	TeamNews,
+	TeamUserEntity,
+	User,
+	UserLoginMigrationEntity,
+	VideoConference,
+} from '@shared/domain/entity';
+import { createConfigModuleOptions, DB_PASSWORD, DB_URL, DB_USERNAME } from '@src/config';
 import { CoreModule } from '@src/core';
 import { Logger } from '@src/core/logger';
 import { H5PEditorController } from './controller/h5p-editor.controller';
@@ -18,6 +71,65 @@ import { H5PAjaxEndpointProvider, H5PEditorProvider, H5PPlayerProvider } from '.
 import { H5PContentRepo, LibraryRepo, TemporaryFileRepo } from './repo';
 import { ContentStorage, LibraryStorage, TemporaryFileStorage } from './service';
 import { H5PEditorUc } from './uc/h5p.uc';
+
+const entities = [
+	Account,
+	Board,
+	BoardElement,
+	BoardNode,
+	CardNode,
+	ColumnboardBoardElement,
+	ColumnBoardNode,
+	ColumnBoardTarget,
+	ColumnNode,
+	ClassEntity,
+	FileElementNode,
+	LinkElementNode,
+	RichTextElementNode,
+	SubmissionContainerElementNode,
+	SubmissionItemNode,
+	ExternalToolElementNodeEntity,
+	Course,
+	ContextExternalToolEntity,
+	CourseGroup,
+	CourseNews,
+	DashboardGridElementModel,
+	DashboardModelEntity,
+	ExternalToolEntity,
+	FederalStateEntity,
+	ImportUser,
+	LessonEntity,
+	LessonBoardElement,
+	LtiTool,
+	Material,
+	News,
+	PseudonymEntity,
+	ExternalToolPseudonymEntity,
+	Role,
+	SchoolEntity,
+	SchoolExternalToolEntity,
+	SchoolNews,
+	SchoolRolePermission,
+	SchoolRoles,
+	SchoolYearEntity,
+	ShareToken,
+	StorageProviderEntity,
+	Submission,
+	SystemEntity,
+	Task,
+	TaskBoardElement,
+	TeamEntity,
+	TeamNews,
+	TeamUserEntity,
+	User,
+	UserLoginMigrationEntity,
+	VideoConference,
+	GroupEntity,
+	RegistrationPinEntity,
+	H5PContent,
+	H5pEditorTempFile,
+	InstalledLibrary,
+];
 
 const defaultMikroOrmOptions: MikroOrmModuleSyncOptions = {
 	findOneOrFailHandler: (entityName: string, where: Dictionary | IPrimaryKey) =>
@@ -38,9 +150,8 @@ const imports = [
 		clientUrl: DB_URL,
 		password: DB_PASSWORD,
 		user: DB_USERNAME,
-		// Needs ALL_ENTITIES for authorization
 		allowGlobalContext: true,
-		entities: [...ALL_ENTITIES, H5PContent, H5pEditorTempFile, InstalledLibrary],
+		entities,
 	}),
 	ConfigModule.forRoot(createConfigModuleOptions(config)),
 	S3ClientModule.register([s3ConfigContent, s3ConfigLibraries]),
