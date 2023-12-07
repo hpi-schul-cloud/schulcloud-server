dde6ff3ca9f6053cf31375384c50e8edd5bf0a06
Constantin Bergatt <constantin.bergatt@dataport.de>
2023-12-07 10:02:30 +0100

    BC-5838 - fix `keycloak-migration.service.spec.ts` unit tests

--- apps/server/src/infra/identity-management/keycloak-configuration/service/keycloak-migration.service.spec.ts
+++ apps/server/src/infra/identity-management/keycloak-configuration/service/keycloak-migration.service.spec.ts
@@ -1,13 +1,12 @@
 import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import KeycloakAdminClient from '@keycloak/keycloak-admin-client-cjs/keycloak-admin-client-cjs-index';
+import UserRepresentation from '@keycloak/keycloak-admin-client/lib/defs/userRepresentation';
+import { Users } from '@keycloak/keycloak-admin-client/lib/resources/users';
+import { AccountDto, AccountService } from '@modules/account';
 import { Test, TestingModule } from '@nestjs/testing';
 import { LegacyLogger } from '@src/core/logger';
-import { AccountService } from '@modules/account/services/account.service';
-import { AccountDto } from '@modules/account/services/dto/account.dto';
-import KeycloakAdminClient from '@keycloak/keycloak-admin-client-cjs/keycloak-admin-client-cjs-index';
-import { Users } from '@keycloak/keycloak-admin-client/lib/resources/users';
-import UserRepresentation from '@keycloak/keycloak-admin-client/lib/defs/userRepresentation';
-import { KeycloakMigrationService } from './keycloak-migration.service';
 import { KeycloakAdministrationService } from '../../keycloak-administration/service/keycloak-administration.service';
+import { KeycloakMigrationService } from './keycloak-migration.service';
 
 describe('KeycloakMigrationService', () => {
 	let module: TestingModule;
