afae92471b59481d38b3a2c84e5365493a5893fb
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2023-12-07 16:08:25 +0100

    change test structure

--- apps/server/src/modules/school/domain/service/school-year.service.spec.ts
+++ apps/server/src/modules/school/domain/service/school-year.service.spec.ts
@@ -1,5 +1,6 @@
 import { createMock, DeepMocked } from '@golevelup/ts-jest';
 import { Test, TestingModule } from '@nestjs/testing';
+import { InternalServerErrorException } from '@nestjs/common';
 import { schoolYearFactory } from '../testing';
 import { SchoolYearRepo, SCHOOL_YEAR_REPO } from '../interface';
 import { SchoolYearService } from './school-year.service';
@@ -24,13 +25,53 @@ describe('SchoolYearService', () => {
 	});
 
 	describe('getAllSchoolYears', () => {
-		it('should return all school years', async () => {
-			const schoolYears = schoolYearFactory.buildList(3);
-			schoolYearRepo.getAllSchoolYears.mockResolvedValueOnce(schoolYears);
+		describe('when multiple school years exists', () => {
+			const setup = () => {
+				const schoolYears = schoolYearFactory.buildList(3);
+				schoolYearRepo.getAllSchoolYears.mockResolvedValueOnce(schoolYears);
 
-			const result = await service.getAllSchoolYears();
+				return { schoolYears };
+			};
 
-			expect(result).toEqual(schoolYears);
+			it('should return all school years', async () => {
+				const { schoolYears } = setup();
+
+				const result = await service.getAllSchoolYears();
+
+				expect(result).toEqual(schoolYears);
+			});
+		});
+
+		describe('when no school years exists', () => {
+			const setup = () => {
+				const schoolYears = [];
+				schoolYearRepo.getAllSchoolYears.mockResolvedValueOnce(schoolYears);
+
+				return { schoolYears };
+			};
+
+			it('should return all school years', async () => {
+				const { schoolYears } = setup();
+
+				const result = await service.getAllSchoolYears();
+
+				expect(result).toEqual(schoolYears);
+			});
+		});
+
+		describe('when repo throw an error', () => {
+			const setup = () => {
+				const error = new InternalServerErrorException('test');
+				schoolYearRepo.getAllSchoolYears.mockRejectedValueOnce(error);
+
+				return { error };
+			};
+
+			it('should return all school years', async () => {
+				const { error } = setup();
+
+				await expect(() => service.getAllSchoolYears()).rejects.toThrowError(error);
+			});
 		});
 	});
 });
