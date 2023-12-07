04eb53616a808c4e6e9bd32dcfa1ad69cd8a55cd
Constantin Bergatt <constantin.bergatt@dataport.de>
2023-12-07 10:19:51 +0100

    BC-5838 - remove `ALL_ENTITIES` from `setup.nest.services.js`

--- test/utils/setup.nest.services.js
+++ test/utils/setup.nest.services.js
@@ -12,11 +12,121 @@ const {
 	AccountValidationService,
 } = require('../../dist/apps/server/modules/account/services/account.validation.service');
 const { DB_PASSWORD, DB_URL, DB_USERNAME } = require('../../dist/apps/server/config/database.config');
-const { ALL_ENTITIES } = require('../../dist/apps/server/shared/domain/entity/all-entities');
 const { TeamService } = require('../../dist/apps/server/modules/teams/service/team.service');
 const { TeamsApiModule } = require('../../dist/apps/server/modules/teams/teams-api.module');
 const { AuthorizationModule } = require('../../dist/apps/server/modules/authorization');
 const { SystemRule } = require('../../dist/apps/server/modules/authorization');
+
+const { ClassEntity } = require('../../dist/apps/server/modules/class/entity');
+const { GroupEntity } = require('../../dist/apps/server/modules/group/entity');
+const { ExternalToolPseudonymEntity, PseudonymEntity } = require('../../dist/apps/server/modules/pseudonym/entity');
+const { RegistrationPinEntity } = require('../../dist/apps/server/modules/registration-pin/entity');
+const { ShareToken } = require('../../dist/apps/server/modules/sharing/entity/share-token.entity');
+const { ContextExternalToolEntity } = require('../../dist/apps/server/modules/tool/context-external-tool/entity');
+const { ExternalToolEntity } = require('../../dist/apps/server/modules/tool/external-tool/entity');
+const { SchoolExternalToolEntity } = require('../../dist/apps/server/modules/tool/school-external-tool/entity');
+const {
+	Account,
+	BoardNode,
+	CardNode,
+	ColumnBoardNode,
+	ColumnNode,
+	ExternalToolElementNodeEntity,
+	FileElementNode,
+	LinkElementNode,
+	RichTextElementNode,
+	SubmissionContainerElementNode,
+	SubmissionItemNode,
+	Course,
+	CourseGroup,
+	DashboardGridElementModel,
+	DashboardModelEntity,
+	FederalStateEntity,
+	ImportUser,
+	Board,
+	BoardElement,
+	ColumnboardBoardElement,
+	ColumnBoardTarget,
+	LessonBoardElement,
+	TaskBoardElement,
+	LessonEntity,
+	LtiTool,
+	Material,
+	CourseNews,
+	News,
+	SchoolNews,
+	TeamNews,
+	Role,
+	SchoolEntity,
+	SchoolRolePermission,
+	SchoolRoles,
+	SchoolYearEntity,
+	StorageProviderEntity,
+	Submission,
+	SystemEntity,
+	Task,
+	TeamEntity,
+	TeamUserEntity,
+	UserLoginMigrationEntity,
+	User,
+	VideoConference,
+} = require('../../dist/apps/server/shared/domain/entity');
+
+const entities = [
+	Account,
+	Board,
+	BoardElement,
+	BoardNode,
+	CardNode,
+	ColumnboardBoardElement,
+	ColumnBoardNode,
+	ColumnBoardTarget,
+	ColumnNode,
+	ClassEntity,
+	FileElementNode,
+	LinkElementNode,
+	RichTextElementNode,
+	SubmissionContainerElementNode,
+	SubmissionItemNode,
+	ExternalToolElementNodeEntity,
+	Course,
+	ContextExternalToolEntity,
+	CourseGroup,
+	CourseNews,
+	DashboardGridElementModel,
+	DashboardModelEntity,
+	ExternalToolEntity,
+	FederalStateEntity,
+	ImportUser,
+	LessonEntity,
+	LessonBoardElement,
+	LtiTool,
+	Material,
+	News,
+	PseudonymEntity,
+	ExternalToolPseudonymEntity,
+	Role,
+	SchoolEntity,
+	SchoolExternalToolEntity,
+	SchoolNews,
+	SchoolRolePermission,
+	SchoolRoles,
+	SchoolYearEntity,
+	ShareToken,
+	StorageProviderEntity,
+	Submission,
+	SystemEntity,
+	Task,
+	TaskBoardElement,
+	TeamEntity,
+	TeamNews,
+	TeamUserEntity,
+	User,
+	UserLoginMigrationEntity,
+	VideoConference,
+	GroupEntity,
+	RegistrationPinEntity,
+];
 
 const setupNestServices = async (app) => {
 	const module = await Test.createTestingModule({
@@ -26,7 +136,7 @@ const setupNestServices = async (app) => {
 				clientUrl: DB_URL,
 				password: DB_PASSWORD,
 				user: DB_USERNAME,
-				entities: ALL_ENTITIES,
+				entities,
 				allowGlobalContext: true,
 				// debug: true, // use it for locally debugging of querys
 			}),
