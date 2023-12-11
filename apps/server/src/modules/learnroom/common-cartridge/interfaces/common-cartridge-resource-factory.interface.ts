3d6faf0cbea2d63b4e75bce188f9bf532c701d3e
psachmann <patrick.sachmann@capgemini.com>
2023-12-11 11:25:08 +0100

    EW-539 removing old code

--- apps/server/src/modules/learnroom/common-cartridge/interfaces/common-cartridge-resource-factory.interface.ts
+++
@@ -1,8 +0,0 @@
-import { CommonCartridgeVersion } from '../common-cartridge.enums';
-import { CommonCartridgeResource } from './common-cartridge-resource.interface';
-
-export type CommonCartridgeResourceProps = { version: CommonCartridgeVersion };
-
-export abstract class CommonCartridgeResourceFactory {
-	abstract createResource(props: CommonCartridgeResourceProps): CommonCartridgeResource;
-}
