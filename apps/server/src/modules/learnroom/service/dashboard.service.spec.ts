eb5ec1327affa26e9b216de69c95bc85f097a511
WojciechGrancow <116577704+WojciechGrancow@users.noreply.github.com>
2023-12-12 13:44:34 +0100

    Update apps/server/src/modules/learnroom/service/dashboard.service.spec.ts
    Co-authored-by: Bartosz Nowicki <116367402+bn-pass@users.noreply.github.com>

--- apps/server/src/modules/learnroom/service/dashboard.service.spec.ts
+++ apps/server/src/modules/learnroom/service/dashboard.service.spec.ts
@@ -2,7 +2,7 @@ import { createMock, DeepMocked } from '@golevelup/ts-jest';
 import { Test, TestingModule } from '@nestjs/testing';
 import { DashboardElementRepo, IDashboardRepo, UserRepo } from '@shared/repo';
 import { setupEntities, userFactory } from '@shared/testing';
-import { EntityId, LearnroomMetadata, LearnroomTypes } from '@shared/domain/types';
+import { LearnroomMetadata, LearnroomTypes } from '@shared/domain/types';
 import { DashboardEntity, GridElement } from '@shared/domain/entity';
 import { DashboardService } from '.';
 
