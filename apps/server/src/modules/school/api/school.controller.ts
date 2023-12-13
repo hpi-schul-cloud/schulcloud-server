0cc026307cea3789595e51263103e1204b37ec3c
dyedwiper <maximilian.becker@dataport.de>
2023-12-13 11:30:25 +0100

    Remove comments

--- apps/server/src/modules/school/api/school.controller.ts
+++ apps/server/src/modules/school/api/school.controller.ts
@@ -12,9 +12,6 @@ import { SchoolUc } from './school.uc';
 export class SchoolController {
 	constructor(private readonly schoolUc: SchoolUc) {}
 
-	// TODO: I suggest to add /id to the route. Without it, this controller action needs to be below the other one,
-	// because any route parameter would be catched by /:schoolId, e.g. "list-for-external-invite".
-	// It seems safer to not need to worry about the order of the actions.
 	@Get('/id/:schoolId')
 	public async getSchoolById(
 		@Param() urlParams: SchoolUrlParams,
@@ -25,7 +22,6 @@ export class SchoolController {
 		return res;
 	}
 
-	// TODO: Do we have a convention for the casing of routes?
 	@Get('/list-for-external-invite')
 	public async getSchoolListForExternalInvite(
 		@Query() query: SchoolQueryParams,
