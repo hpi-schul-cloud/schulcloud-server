0fdeeabe8cfdb9e3622c4f9041a03baec389e2ea
psachmann <patrick.sachmann@capgemini.com>
2023-12-20 10:31:28 +0100

    Fixing test coverage for resource factories

--- apps/server/src/modules/common-cartridge/resources/v1.1.0/common-cartridge-resource-factory.spec.ts
+++ apps/server/src/modules/common-cartridge/resources/v1.1.0/common-cartridge-resource-factory.spec.ts
@@ -1,28 +1,42 @@
-import { faker } from '@faker-js/faker';
+import { InternalServerErrorException } from '@nestjs/common';
 import {
 	CommonCartridgeIntendedUseType,
 	CommonCartridgeResourceType,
 	CommonCartridgeVersion,
 } from '../../common-cartridge.enums';
+import {
+	CommonCartridgeManifestResourcePropsV110,
+	CommonCartridgeManifestResourceV110,
+} from './common-cartridge-manifest-resource';
 import { CommonCartridgeResourceFactoryV110 } from './common-cartridge-resource-factory';
-import { CommonCartridgeWebContentResourceV110 } from './common-cartridge-web-content-resource';
-import { CommonCartridgeWebLinkResourceV110 } from './common-cartridge-web-link-resource';
+import {
+	CommonCartridgeWebContentResourcePropsV110,
+	CommonCartridgeWebContentResourceV110,
+} from './common-cartridge-web-content-resource';
+import {
+	CommonCartridgeWebLinkResourcePropsV110,
+	CommonCartridgeWebLinkResourceV110,
+} from './common-cartridge-web-link-resource';
 
 describe('CommonCartridgeResourceFactoryV110', () => {
 	describe('createResource', () => {
 		describe('when creating resources from props', () => {
+			it('should return manifest resource', () => {
+				const result = CommonCartridgeResourceFactoryV110.createResource({
+					type: CommonCartridgeResourceType.MANIFEST,
+					version: CommonCartridgeVersion.V_1_1_0,
+				} as CommonCartridgeManifestResourcePropsV110);
+
+				expect(result).toBeInstanceOf(CommonCartridgeManifestResourceV110);
+			});
+
 			it('should return web content resource', () => {
 				const result = CommonCartridgeResourceFactoryV110.createResource({
 					type: CommonCartridgeResourceType.WEB_CONTENT,
 					version: CommonCartridgeVersion.V_1_1_0,
-					folder: faker.string.uuid(),
-					identifier: faker.string.uuid(),
-					title: faker.lorem.words(),
-					html: faker.lorem.paragraph(),
 					intendedUse: CommonCartridgeIntendedUseType.UNSPECIFIED,
-				});
+				} as CommonCartridgeWebContentResourcePropsV110);
 
-				expect(result).toBeDefined();
 				expect(result).toBeInstanceOf(CommonCartridgeWebContentResourceV110);
 			});
 
@@ -30,16 +44,17 @@ describe('CommonCartridgeResourceFactoryV110', () => {
 				const result = CommonCartridgeResourceFactoryV110.createResource({
 					type: CommonCartridgeResourceType.WEB_LINK,
 					version: CommonCartridgeVersion.V_1_1_0,
-					folder: faker.string.uuid(),
-					identifier: faker.string.uuid(),
-					title: faker.lorem.words(),
-					url: faker.internet.url(),
-					target: faker.lorem.word(),
-					windowFeatures: faker.lorem.words(),
-				});
+				} as CommonCartridgeWebLinkResourcePropsV110);
 
-				expect(result).toBeDefined();
 				expect(result).toBeInstanceOf(CommonCartridgeWebLinkResourceV110);
+			});
+		});
+
+		describe('when resource type is not supported', () => {
+			it('should throw error', () => {
+				expect(() =>
+					CommonCartridgeResourceFactoryV110.createResource({} as CommonCartridgeWebLinkResourcePropsV110)
+				).toThrow(InternalServerErrorException);
 			});
 		});
 	});
--- apps/server/src/modules/common-cartridge/resources/v1.3.0/common-cartridge-resource-factory.spec.ts
+++ apps/server/src/modules/common-cartridge/resources/v1.3.0/common-cartridge-resource-factory.spec.ts
@@ -1,26 +1,41 @@
-import { faker } from '@faker-js/faker';
+import { InternalServerErrorException } from '@nestjs/common';
 import {
 	CommonCartridgeIntendedUseType,
 	CommonCartridgeResourceType,
 	CommonCartridgeVersion,
 } from '../../common-cartridge.enums';
+import {
+	CommonCartridgeManifestResourcePropsV130,
+	CommonCartridgeManifestResourceV130,
+} from './common-cartridge-manifest-resource';
 import { CommonCartridgeResourceFactoryV130 } from './common-cartridge-resource-factory';
-import { CommonCartridgeWebContentResourceV130 } from './common-cartridge-web-content-resource';
-import { CommonCartridgeWebLinkResourceV130 } from './common-cartridge-web-link-resource';
+import {
+	CommonCartridgeWebContentResourcePropsV130,
+	CommonCartridgeWebContentResourceV130,
+} from './common-cartridge-web-content-resource';
+import {
+	CommonCartridgeWebLinkResourcePropsV130,
+	CommonCartridgeWebLinkResourceV130,
+} from './common-cartridge-web-link-resource';
 
 describe('CommonCartridgeResourceFactoryV130', () => {
 	describe('createResource', () => {
 		describe('when creating resources from props', () => {
+			it('should return manifest resource', () => {
+				const result = CommonCartridgeResourceFactoryV130.createResource({
+					type: CommonCartridgeResourceType.MANIFEST,
+					version: CommonCartridgeVersion.V_1_3_0,
+				} as CommonCartridgeManifestResourcePropsV130);
+
+				expect(result).toBeInstanceOf(CommonCartridgeManifestResourceV130);
+			});
+
 			it('shoul return web content resource', () => {
 				const result = CommonCartridgeResourceFactoryV130.createResource({
 					type: CommonCartridgeResourceType.WEB_CONTENT,
 					version: CommonCartridgeVersion.V_1_3_0,
-					folder: faker.string.uuid(),
-					identifier: faker.string.uuid(),
-					title: faker.lorem.words(),
-					html: faker.lorem.paragraph(),
 					intendedUse: CommonCartridgeIntendedUseType.UNSPECIFIED,
-				});
+				} as CommonCartridgeWebContentResourcePropsV130);
 
 				expect(result).toBeInstanceOf(CommonCartridgeWebContentResourceV130);
 			});
@@ -29,15 +44,17 @@ describe('CommonCartridgeResourceFactoryV130', () => {
 				const result = CommonCartridgeResourceFactoryV130.createResource({
 					type: CommonCartridgeResourceType.WEB_LINK,
 					version: CommonCartridgeVersion.V_1_3_0,
-					folder: faker.string.uuid(),
-					identifier: faker.string.uuid(),
-					title: faker.lorem.words(),
-					url: faker.internet.url(),
-					target: faker.lorem.word(),
-					windowFeatures: faker.lorem.words(),
-				});
+				} as CommonCartridgeWebLinkResourcePropsV130);
 
 				expect(result).toBeInstanceOf(CommonCartridgeWebLinkResourceV130);
+			});
+		});
+
+		describe('when resource type is not supported', () => {
+			it('should throw error', () => {
+				expect(() =>
+					CommonCartridgeResourceFactoryV130.createResource({} as CommonCartridgeWebLinkResourcePropsV130)
+				).toThrow(InternalServerErrorException);
 			});
 		});
 	});
