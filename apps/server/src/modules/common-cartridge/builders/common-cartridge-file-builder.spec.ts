e2aad01e7f40246a89e41cd0c71581862f4320de
psachmann <patrick.sachmann@capgemini.com>
2023-12-21 11:22:39 +0100

    Fixing test coverage

--- apps/server/src/modules/common-cartridge/builders/common-cartridge-file-builder.spec.ts
+++ apps/server/src/modules/common-cartridge/builders/common-cartridge-file-builder.spec.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker';
 import AdmZip from 'adm-zip';
 import {
 	CommonCartridgeElementType,
@@ -7,100 +8,84 @@ import {
 } from '../common-cartridge.enums';
 import { CommonCartridgeElementProps } from '../elements/common-cartridge-element-factory';
 import { CommonCartridgeResourceProps } from '../resources/common-cartridge-resource-factory';
-import { OmitVersion } from '../utils';
-import { CommonCartridgeFileBuilder, CommonCartridgeFileBuilderProps } from './common-cartridge-file-builder';
+import { CommonCartridgeFileBuilder } from './common-cartridge-file-builder';
 import { CommonCartridgeOrganizationBuilderOptions } from './common-cartridge-organization-builder';
 
 describe('CommonCartridgeFileBuilder', () => {
-	let sut: CommonCartridgeFileBuilder;
-	let archive: AdmZip;
-
 	const getFileContentAsString = (zip: AdmZip, path: string): string | undefined =>
 		zip.getEntry(path)?.getData().toString();
-	const fileBuilderOptions: CommonCartridgeFileBuilderProps = {
-		version: CommonCartridgeVersion.V_1_1_0,
-		identifier: 'manifest-identifier',
-	};
-	const metadataProps: CommonCartridgeElementProps = {
-		type: CommonCartridgeElementType.METADATA,
-		title: 'metadata-title',
-		creationDate: new Date(),
-		copyrightOwners: ['John Doe', 'Jane Doe'],
-	};
-	const organizationOptions: OmitVersion<CommonCartridgeOrganizationBuilderOptions> = {
-		title: 'organization-title',
-		identifier: 'organization-identifier',
-	};
-	const subOrganizationOptions: OmitVersion<CommonCartridgeOrganizationBuilderOptions> = {
-		title: 'sub-organization-title',
-		identifier: 'sub-organization-identifier',
-	};
-	const subSubOrganizationOptions: OmitVersion<CommonCartridgeOrganizationBuilderOptions> = {
-		title: 'sub-sub-organization-title',
-		identifier: 'sub-sub-organization-identifier',
-	};
-	const resource1Props: CommonCartridgeResourceProps = {
-		type: CommonCartridgeResourceType.WEB_CONTENT,
-		identifier: 'resource-1-identifier',
-		title: 'resource-1-title',
-		html: '<p>resource-1-html</p>',
-		intendedUse: CommonCartridgeIntendedUseType.UNSPECIFIED,
-	};
-	const resource2Props: CommonCartridgeResourceProps = {
-		type: CommonCartridgeResourceType.WEB_CONTENT,
-		identifier: 'resource-2-identifier',
-		title: 'resource-2-title',
-		html: '<p>resource-2-html</p>',
-		intendedUse: CommonCartridgeIntendedUseType.UNSPECIFIED,
-	};
-	const resource3Props: CommonCartridgeResourceProps = {
-		type: CommonCartridgeResourceType.WEB_CONTENT,
-		identifier: 'resource-3-identifier',
-		title: 'resource-3-title',
-		html: '<p>resource-3-html</p>',
-		intendedUse: CommonCartridgeIntendedUseType.UNSPECIFIED,
-	};
 
 	describe('build', () => {
 		describe('when a common cartridge archive has been created', () => {
-			beforeAll(async () => {
-				sut = new CommonCartridgeFileBuilder(fileBuilderOptions);
-				sut.addMetadata(metadataProps)
+			const setup = async () => {
+				const metadataProps: CommonCartridgeElementProps = {
+					type: CommonCartridgeElementType.METADATA,
+					title: faker.lorem.words(),
+					creationDate: new Date(),
+					copyrightOwners: ['John Doe', 'Jane Doe'],
+				};
+				const organizationOptions: CommonCartridgeOrganizationBuilderOptions = {
+					identifier: faker.string.uuid(),
+					title: faker.lorem.words(),
+				};
+				const resourceProps: CommonCartridgeResourceProps = {
+					type: CommonCartridgeResourceType.WEB_CONTENT,
+					identifier: faker.string.uuid(),
+					title: faker.lorem.words(),
+					html: faker.lorem.paragraphs(),
+					intendedUse: CommonCartridgeIntendedUseType.UNSPECIFIED,
+				};
+				const builder = new CommonCartridgeFileBuilder({
+					version: CommonCartridgeVersion.V_1_1_0,
+					identifier: faker.string.uuid(),
+				});
+
+				builder
+					.addMetadata(metadataProps)
 					.addOrganization(organizationOptions)
-					.addResource(resource1Props)
-					.addSubOrganization(subOrganizationOptions)
-					.addResource(resource2Props)
-					.addSubOrganization(subSubOrganizationOptions)
-					.addResource(resource3Props);
+					.addResource(resourceProps)
+					.addSubOrganization(organizationOptions)
+					.addResource(resourceProps)
+					.addSubOrganization(organizationOptions)
+					.addResource(resourceProps);
 
-				archive = new AdmZip(await sut.build());
-			});
+				const archive = new AdmZip(await builder.build());
 
-			it('should have a imsmanifest.xml in archive root', () => {
+				return { archive, metadataProps, organizationOptions, resourceProps };
+			};
+
+			it('should have a imsmanifest.xml in archive root', async () => {
+				const { archive } = await setup();
 				const manifest = getFileContentAsString(archive, 'imsmanifest.xml');
 
 				expect(manifest).toBeDefined();
 			});
 
-			it('should have included the resource in organization folder', () => {
-				const resource = getFileContentAsString(archive, 'organization-identifier/resource-1-identifier.html');
-
-				expect(resource).toBeDefined();
-			});
-
-			it('should have included the resource in sub-organization folder', () => {
+			it('should have included the resource in organization folder', async () => {
+				const { archive, organizationOptions, resourceProps } = await setup();
 				const resource = getFileContentAsString(
 					archive,
-					'organization-identifier/sub-organization-identifier/resource-2-identifier.html'
+					`${organizationOptions.identifier}/${resourceProps.identifier}.html`
 				);
 
 				expect(resource).toBeDefined();
 			});
 
-			it('should have included the resource in sub-sub-organization folder', () => {
+			it('should have included the resource in sub-organization folder', async () => {
+				const { archive, organizationOptions, resourceProps } = await setup();
 				const resource = getFileContentAsString(
 					archive,
-					'organization-identifier/sub-organization-identifier/sub-sub-organization-identifier/resource-3-identifier.html'
+					`${organizationOptions.identifier}/${organizationOptions.identifier}/${resourceProps.identifier}.html`
+				);
+
+				expect(resource).toBeDefined();
+			});
+
+			it('should have included the resource in sub-sub-organization folder', async () => {
+				const { archive, organizationOptions, resourceProps } = await setup();
+				const resource = getFileContentAsString(
+					archive,
+					`${organizationOptions.identifier}/${organizationOptions.identifier}/${organizationOptions.identifier}/${resourceProps.identifier}.html`
 				);
 
 				expect(resource).toBeDefined();
--- apps/server/src/modules/common-cartridge/builders/common-cartridge-organization-builder.spec.ts
+++ apps/server/src/modules/common-cartridge/builders/common-cartridge-organization-builder.spec.ts
@@ -4,8 +4,7 @@ import {
 	CommonCartridgeResourceType,
 	CommonCartridgeVersion,
 } from '../common-cartridge.enums';
-import { CommonCartridgeOrganizationElementV110 } from '../elements/v1.1.0/common-cartridge-organization-element';
-import { CommonCartridgeOrganizationElementV130 } from '../elements/v1.3.0/common-cartridge-organization-element';
+import { CommonCartridgeElement } from '../interfaces/common-cartridge-element.interface';
 import { CommonCartridgeResource } from '../interfaces/common-cartridge-resource.interface';
 import { CommonCartridgeResourceProps } from '../resources/common-cartridge-resource-factory';
 import {
@@ -15,7 +14,7 @@ import {
 
 describe('CommonCartridgeOrganizationBuilder', () => {
 	describe('build', () => {
-		describe('when using Common Cartridge version 1.1.0', () => {
+		describe('when building a Common Cartridge organization', () => {
 			const setup = () => {
 				const resources = new Array<CommonCartridgeResource>();
 				const organizationOptions: CommonCartridgeOrganizationBuilderOptions = {
@@ -45,55 +44,11 @@ describe('CommonCartridgeOrganizationBuilder', () => {
 				return { sut, resources };
 			};
 
-			it('should return a common cartridge organization element', () => {
+			it('should return a common cartridge element', () => {
 				const { sut } = setup();
 				const element = sut.build();
 
-				expect(element).toBeInstanceOf(CommonCartridgeOrganizationElementV110);
-			});
-
-			it('should add 3 resources', () => {
-				const { resources } = setup();
-
-				expect(resources).toHaveLength(3);
-			});
-		});
-
-		describe('when using Common Cartridge version 1.3.0', () => {
-			const setup = () => {
-				const resources = new Array<CommonCartridgeResource>();
-				const organizationOptions: CommonCartridgeOrganizationBuilderOptions = {
-					identifier: faker.string.uuid(),
-					title: faker.lorem.words(),
-				};
-				const resourceProps: CommonCartridgeResourceProps = {
-					type: CommonCartridgeResourceType.WEB_CONTENT,
-					identifier: faker.string.uuid(),
-					title: faker.lorem.words(),
-					html: faker.lorem.paragraphs(),
-					intendedUse: CommonCartridgeIntendedUseType.UNSPECIFIED,
-				};
-				const sut = new CommonCartridgeOrganizationBuilder(
-					{
-						...organizationOptions,
-						version: CommonCartridgeVersion.V_1_3_0,
-					},
-					(resource) => resources.push(resource)
-				)
-					.addResource(resourceProps)
-					.addSubOrganization(organizationOptions)
-					.addResource(resourceProps)
-					.addSubOrganization(organizationOptions)
-					.addResource(resourceProps);
-
-				return { sut, resources };
-			};
-
-			it('should return a common cartridge organization element', () => {
-				const { sut } = setup();
-				const element = sut.build();
-
-				expect(element).toBeInstanceOf(CommonCartridgeOrganizationElementV130);
+				expect(element).toBeInstanceOf(CommonCartridgeElement);
 			});
 
 			it('should add 3 resources', () => {
