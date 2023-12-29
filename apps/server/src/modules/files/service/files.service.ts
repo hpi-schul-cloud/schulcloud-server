59740dbd01edc5e36c2a1233fbf8922ba4d073a1
WojciechGrancow <Wojciech.Grancow@gca.pass-consulting.com>
2023-12-29 13:35:34 +0100

    add logger to FilesModule for removePermision and markFordeletion

--- apps/server/src/modules/files/service/files.service.ts
+++ apps/server/src/modules/files/service/files.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { EntityId } from '@shared/domain/types';
+import { LegacyLogger } from '@src/core/logger';
 import { FileEntity } from '../entity';
 import { FilesRepo } from '../repo';
 
 @Injectable()
 export class FilesService {
-	constructor(private readonly repo: FilesRepo) {}
+	constructor(private readonly repo: FilesRepo, private readonly logger: LegacyLogger) {
+		this.logger.setContext(FilesService.name);
+	}
 
 	async findFilesAccessibleByUser(userId: EntityId): Promise<FileEntity[]> {
 		return this.repo.findByPermissionRefId(userId);
 	}
 
 	async removeUserPermissionsToAnyFiles(userId: EntityId): Promise<number> {
+		this.logger.log({ action: 'Deleting Permissions To Any Files for user ', userId });
 		const entities = await this.repo.findByPermissionRefId(userId);
 
 		if (entities.length === 0) {
@@ -22,6 +26,8 @@ export class FilesService {
 
 		await this.repo.save(entities);
 
+		this.logger.log({ action: 'Deleted Permissions To Any Files for user ', userId });
+
 		return entities.length;
 	}
 
@@ -30,6 +36,7 @@ export class FilesService {
 	}
 
 	async markFilesOwnedByUserForDeletion(userId: EntityId): Promise<number> {
+		this.logger.log({ action: 'Marking Files For Deletion Owned By user ', userId });
 		const entities = await this.repo.findByOwnerUserId(userId);
 
 		if (entities.length === 0) {
@@ -39,6 +46,8 @@ export class FilesService {
 		entities.forEach((entity) => entity.markForDeletion());
 
 		await this.repo.save(entities);
+
+		this.logger.log({ action: 'Marked Files For Deletion Owned By user ', userId });
 
 		return entities.length;
 	}
