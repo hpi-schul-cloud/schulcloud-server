64830b31a780f84c6122b2617b377943c6850933
WojciechGrancow <Wojciech.Grancow@gca.pass-consulting.com>
2023-12-29 09:34:26 +0100

    add logger to registrationPin deleteService

--- apps/server/src/modules/registration-pin/service/registration-pin.service.ts
+++ apps/server/src/modules/registration-pin/service/registration-pin.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { LegacyLogger } from '@src/core/logger';
 import { RegistrationPinRepo } from '../repo';
 
 @Injectable()
 export class RegistrationPinService {
-	constructor(private readonly registrationPinRepo: RegistrationPinRepo) {}
+	constructor(private readonly registrationPinRepo: RegistrationPinRepo, private readonly logger: LegacyLogger) {
+		this.logger.setContext(RegistrationPinService.name);
+	}
 
 	async deleteRegistrationPinByEmail(email: string): Promise<number> {
-		return this.registrationPinRepo.deleteRegistrationPinByEmail(email);
+		this.logger.log({ action: 'Deleting registrationPin for ', email });
+		const result = this.registrationPinRepo.deleteRegistrationPinByEmail(email);
+		this.logger.log({ action: 'Deleted registrationPin for ', email });
+
+		return result;
 	}
 }
