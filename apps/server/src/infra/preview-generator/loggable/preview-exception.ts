99d220d635984c9871232159659ebae647fca2d4
SevenWaysDP <sergej.hoffmann@dataport.de>
2024-01-04 16:18:06 +0100

    BC-6041 - fix bugs

---
+++ apps/server/src/infra/preview-generator/loggable/preview-exception.ts
@@ -0,0 +1,26 @@
+import { InternalServerErrorException } from '@nestjs/common';
+import { ErrorLogMessage, Loggable } from '@src/core/logger';
+import { PreviewFileOptions } from '../interface';
+
+export class PreviewNotPossibleException extends InternalServerErrorException implements Loggable {
+	constructor(private readonly payload: PreviewFileOptions, private readonly error?: Error) {
+		super('CREATE_PREVIEW_NOT_POSSIBLE');
+	}
+
+	getLogMessage(): ErrorLogMessage {
+		const { originFilePath, previewFilePath, previewOptions } = this.payload;
+		const message: ErrorLogMessage = {
+			type: InternalServerErrorException.name,
+			stack: this.stack,
+			error: this.error,
+			data: {
+				originFilePath,
+				previewFilePath,
+				format: previewOptions.format,
+				width: previewOptions.width,
+			},
+		};
+
+		return message;
+	}
+}
--- apps/server/src/infra/preview-generator/preview-generator.service.ts
+++ apps/server/src/infra/preview-generator/preview-generator.service.ts
@@ -1,11 +1,11 @@
 import { GetFile, S3ClientAdapter } from '@infra/s3-client';
-import { Injectable, InternalServerErrorException, UnprocessableEntityException } from '@nestjs/common';
-import { ErrorUtils } from '@src/core/error/utils';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { Logger } from '@src/core/logger';
 import m, { subClass } from 'gm';
 import { PassThrough } from 'stream';
 import { PreviewFileOptions, PreviewInputMimeTypes, PreviewOptions, PreviewResponseMessage } from './interface';
 import { PreviewActionsLoggable } from './loggable/preview-actions.loggable';
+import { PreviewNotPossibleException } from './loggable/preview-exception';
 import { PreviewGeneratorBuilder } from './preview-generator.builder';
 
 @Injectable()
@@ -17,24 +17,28 @@ export class PreviewGeneratorService {
 	}
 
 	public async generatePreview(params: PreviewFileOptions): Promise<PreviewResponseMessage> {
-		this.logger.info(new PreviewActionsLoggable('PreviewGeneratorService.generatePreview:start', params));
-		const { originFilePath, previewFilePath, previewOptions } = params;
+		try {
+			this.logger.info(new PreviewActionsLoggable('PreviewGeneratorService.generatePreview:start', params));
+			const { originFilePath, previewFilePath, previewOptions } = params;
 
-		const original = await this.downloadOriginFile(originFilePath);
+			const original = await this.downloadOriginFile(originFilePath);
 
-		this.checkIfPreviewPossible(original, params);
+			this.checkIfPreviewPossible(original, params);
 
-		const preview = await this.resizeAndConvert(original, previewOptions);
-		const file = PreviewGeneratorBuilder.buildFile(preview, params.previewOptions);
+			const preview = await this.resizeAndConvert(original, previewOptions);
+			const file = PreviewGeneratorBuilder.buildFile(preview, params.previewOptions);
 
-		await this.storageClient.create(previewFilePath, file);
+			await this.storageClient.create(previewFilePath, file);
 
-		this.logger.info(new PreviewActionsLoggable('PreviewGeneratorService.generatePreview:end', params));
+			this.logger.info(new PreviewActionsLoggable('PreviewGeneratorService.generatePreview:end', params));
 
-		return {
-			previewFilePath,
-			status: true,
-		};
+			return {
+				previewFilePath,
+				status: true,
+			};
+		} catch (error) {
+			throw new PreviewNotPossibleException(params, error as Error);
+		}
 	}
 
 	private checkIfPreviewPossible(original: GetFile, params: PreviewFileOptions): void | UnprocessableEntityException {
@@ -43,7 +47,7 @@ export class PreviewGeneratorService {
 
 		if (!isPreviewPossible) {
 			this.logger.warning(new PreviewActionsLoggable('PreviewGeneratorService.previewNotPossible', params));
-			throw new UnprocessableEntityException();
+			throw new UnprocessableEntityException('Unsupported file type for preview generation');
 		}
 	}
 
@@ -73,28 +77,20 @@ export class PreviewGeneratorService {
 		return this.convert(preview, format);
 	}
 
-	private convert(preview: m.State, format: string) {
+	private convert(preview: m.State, format: string): Promise<PassThrough> {
 		const promise = new Promise<PassThrough>((resolve, reject) => {
 			preview.stream(format, (err, stdout, stderr) => {
 				if (err) {
-					stderr.emit('error', err);
+					reject(err);
 				}
 
+				const throughStream = new PassThrough();
+				stdout.pipe(throughStream);
+				stdout.on('data', () => {
+					resolve(throughStream);
+				});
+
 				const errorChunks: Array<Uint8Array> = [];
-
-				stdout.on('error', (error) => {
-					stderr.emit('error', error);
-				});
-
-				stderr.on('error', (error) => {
-					reject(
-						new InternalServerErrorException(
-							'CREATE_PREVIEW_NOT_POSSIBLE',
-							ErrorUtils.createHttpExceptionOptions(error)
-						)
-					);
-				});
-
 				stderr.on('data', (chunk: Uint8Array) => {
 					errorChunks.push(chunk);
 				});
@@ -104,13 +100,8 @@ export class PreviewGeneratorService {
 					Buffer.concat(errorChunks).forEach((chunk) => {
 						errorMessage += String.fromCharCode(chunk);
 					});
-					if (errorMessage !== '') {
-						stderr.emit('error', errorMessage);
-					} else {
-						const throughStream = new PassThrough();
-						stdout.pipe(throughStream);
-						resolve(throughStream);
-					}
+
+					reject(new Error(errorMessage));
 				});
 			});
 		});
