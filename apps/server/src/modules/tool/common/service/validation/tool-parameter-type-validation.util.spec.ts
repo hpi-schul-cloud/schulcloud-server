b94cb3f2b7a0681190ff3646d5b509bf370c95c2
Igor Richter <igor.richter@capgemini.com>
2024-01-05 17:03:48 +0100

    coverage up

--- apps/server/src/modules/tool/common/service/validation/tool-parameter-type-validation.util.spec.ts
+++ apps/server/src/modules/tool/common/service/validation/tool-parameter-type-validation.util.spec.ts
@@ -52,10 +52,43 @@ describe(ToolParameterTypeValidationUtil.name, () => {
 			});
 		});
 
-		describe('when the type is an auto type', () => {
+		describe('when the type is AUTO_CONTEXTNAME', () => {
 			it('should return false', () => {
 				const result: boolean = ToolParameterTypeValidationUtil.isValueValidForType(
 					CustomParameterType.AUTO_CONTEXTNAME,
+					'any value'
+				);
+
+				expect(result).toEqual(false);
+			});
+		});
+
+		describe('when the type is AUTO_CONTEXTID', () => {
+			it('should return false', () => {
+				const result: boolean = ToolParameterTypeValidationUtil.isValueValidForType(
+					CustomParameterType.AUTO_CONTEXTID,
+					'any value'
+				);
+
+				expect(result).toEqual(false);
+			});
+		});
+
+		describe('when the type is AUTO_SCHOOLID', () => {
+			it('should return false', () => {
+				const result: boolean = ToolParameterTypeValidationUtil.isValueValidForType(
+					CustomParameterType.AUTO_SCHOOLID,
+					'any value'
+				);
+
+				expect(result).toEqual(false);
+			});
+		});
+
+		describe('when the type is AUTO_SCHOOLNUMBER', () => {
+			it('should return false', () => {
+				const result: boolean = ToolParameterTypeValidationUtil.isValueValidForType(
+					CustomParameterType.AUTO_SCHOOLNUMBER,
 					'any value'
 				);
 
