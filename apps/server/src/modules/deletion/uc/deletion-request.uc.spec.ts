0f929d44609d20b78a6d67564a8c418fbd06b6e7
WojciechGrancow <Wojciech.Grancow@gca.pass-consulting.com>
2024-01-21 16:30:56 +0100

    update test cases

--- apps/server/src/modules/deletion/uc/deletion-request.uc.spec.ts
+++ apps/server/src/modules/deletion/uc/deletion-request.uc.spec.ts
@@ -383,16 +383,6 @@ describe(DeletionRequestUc.name, () => {
 				expect(teamService.deleteUserDataFromTeams).toHaveBeenCalledWith(deletionRequestToExecute.targetRefId);
 			});
 
-			it('should call userService.deleteUsers to delete user in user module', async () => {
-				const { deletionRequestToExecute } = setup();
-
-				deletionRequestService.findAllItemsToExecute.mockResolvedValueOnce([deletionRequestToExecute]);
-
-				await uc.executeDeletionRequests();
-
-				expect(userService.deleteUser).toHaveBeenCalledWith(deletionRequestToExecute.targetRefId);
-			});
-
 			it('should call rocketChatUserService.findByUserId to find rocketChatUser in rocketChatUser module', async () => {
 				const { deletionRequestToExecute } = setup();
 
@@ -472,7 +462,7 @@ describe(DeletionRequestUc.name, () => {
 
 				await uc.executeDeletionRequests();
 
-				expect(deletionLogService.createDeletionLog).toHaveBeenCalledTimes(13);
+				expect(deletionLogService.createDeletionLog).toHaveBeenCalledTimes(12);
 			});
 		});
 
