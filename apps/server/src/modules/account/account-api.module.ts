d82c7dbbbba4f0717fa95785a3613db1ebe72ab5
Maximilian Kreuzkam <maximilian.kreuzkam@capgemini.com>
2024-01-22 15:12:40 +0100

    Restructure UC and move business logic to service. (without tests)

--- apps/server/src/modules/account/account-api.module.ts
+++ apps/server/src/modules/account/account-api.module.ts
@@ -5,9 +5,10 @@ import { LoggerModule } from '../../core/logger/logger.module';
 import { AccountModule } from './account.module';
 import { AccountController } from './controller/account.controller';
 import { AccountUc } from './uc/account.uc';
+import { AuthorizationModule } from '../authorization';
 
 @Module({
-	imports: [AccountModule, LoggerModule],
+	imports: [AccountModule, LoggerModule, AuthorizationModule],
 	providers: [UserRepo, PermissionService, AccountUc],
 	controllers: [AccountController],
 	exports: [],
--- apps/server/src/modules/account/services/account.service.ts
+++ apps/server/src/modules/account/services/account.service.ts
@@ -1,22 +1,30 @@
 import { ObjectId } from '@mikro-orm/mongodb';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { ValidationError } from '@shared/common';
-import { Counted } from '@shared/domain/types';
-import { isEmail, validateOrReject } from 'class-validator';
+import { AuthorizationError, EntityNotFoundError, ForbiddenOperationError, ValidationError } from '@shared/common';
+import { User } from '@shared/domain/entity';
+import { Counted, EntityId } from '@shared/domain/types';
+import { UserRepo } from '@shared/repo/user/user.repo';
 import { LegacyLogger } from '@src/core/logger';
+import { isEmail, validateOrReject } from 'class-validator';
+import { AccountConfig } from '../account-config';
+import { Account, AccountSave } from '../domain';
+import { AccountEntity } from '../entity/account.entity';
 import { AccountServiceDb } from './account-db.service';
 import { AccountServiceIdm } from './account-idm.service';
 import { AbstractAccountService } from './account.service.abstract';
 import { AccountValidationService } from './account.validation.service';
-import { Account, AccountSave } from '../domain';
-import { AccountConfig } from '../account-config';
+import { AccountByIdBodyParams, PatchMyAccountParams } from '../controller/dto';
 
 /* TODO: extract a service that contains all things required by feathers,
 which is responsible for the additionally required validation 
 
 it should be clearly visible which functions are only needed for feathers, and easy to remove them */
 
+type UserPreferences = {
+	// first login completed
+	firstLogin: boolean;
+};
 @Injectable()
 export class AccountService extends AbstractAccountService {
 	private readonly accountImpl: AbstractAccountService;
@@ -26,7 +34,8 @@ export class AccountService extends AbstractAccountService {
 		private readonly accountIdm: AccountServiceIdm,
 		private readonly configService: ConfigService<AccountConfig, true>,
 		private readonly accountValidationService: AccountValidationService,
-		private readonly logger: LegacyLogger
+		private readonly logger: LegacyLogger,
+		private readonly userRepo: UserRepo
 	) {
 		super();
 		this.logger.setContext(AccountService.name);
@@ -34,6 +43,147 @@ export class AccountService extends AbstractAccountService {
 			this.accountImpl = accountIdm;
 		} else {
 			this.accountImpl = accountDb;
+		}
+	}
+
+	public async updateMyAccount(user: User, account: Account, params: PatchMyAccountParams) {
+		if (account.systemId) {
+			throw new ForbiddenOperationError('External account details can not be changed.');
+		}
+
+		if (!params.passwordOld || !(await this.validatePassword(account, params.passwordOld))) {
+			throw new AuthorizationError('Dein Passwort ist nicht korrekt!');
+		}
+
+		let updateUser = false;
+		let updateAccount = false;
+		if (params.passwordNew) {
+			account.password = params.passwordNew;
+			updateAccount = true;
+		} else {
+			account.password = undefined;
+		}
+
+		if (params.email && user.email !== params.email) {
+			const newMail = params.email.toLowerCase();
+			await this.checkUniqueEmail(account, user, newMail);
+			user.email = newMail;
+			account.username = newMail;
+			updateUser = true;
+			updateAccount = true;
+		}
+
+		if (params.firstName && user.firstName !== params.firstName) {
+			user.firstName = params.firstName;
+			updateUser = true;
+		}
+
+		if (params.lastName && user.lastName !== params.lastName) {
+			user.lastName = params.lastName;
+			updateUser = true;
+		}
+
+		if (updateUser) {
+			try {
+				await this.userRepo.save(user);
+			} catch (err) {
+				throw new EntityNotFoundError(User.name);
+			}
+		}
+		if (updateAccount) {
+			try {
+				await this.save(account);
+			} catch (err) {
+				throw new EntityNotFoundError(AccountEntity.name);
+			}
+		}
+	}
+
+	public async updateAccountById(
+		targetUser: User,
+		targetAccount: Account,
+		body: AccountByIdBodyParams
+	): Promise<Account> {
+		let updateUser = false;
+		let updateAccount = false;
+
+		if (body.password !== undefined) {
+			targetAccount.password = body.password;
+			targetUser.forcePasswordChange = true;
+			updateUser = true;
+			updateAccount = true;
+		}
+		if (body.username !== undefined) {
+			const newMail = body.username.toLowerCase();
+			await this.checkUniqueEmail(targetAccount, targetUser, newMail);
+			targetUser.email = newMail;
+			targetAccount.username = newMail;
+			updateUser = true;
+			updateAccount = true;
+		}
+		if (body.activated !== undefined) {
+			targetAccount.activated = body.activated;
+			updateAccount = true;
+		}
+
+		if (updateUser) {
+			try {
+				await this.userRepo.save(targetUser);
+			} catch (err) {
+				throw new EntityNotFoundError(User.name);
+			}
+		}
+		if (updateAccount) {
+			try {
+				await this.save(targetAccount);
+			} catch (err) {
+				throw new EntityNotFoundError(AccountEntity.name);
+			}
+		}
+
+		return targetAccount;
+	}
+
+	public async replaceMyTemporaryPassword(userId: EntityId, password: string, confirmPassword: string): Promise<void> {
+		if (password !== confirmPassword) {
+			throw new ForbiddenOperationError('Password and confirm password do not match.');
+		}
+
+		let user: User;
+		try {
+			user = await this.userRepo.findById(userId);
+		} catch (err) {
+			throw new EntityNotFoundError(User.name);
+		}
+
+		const userPreferences = <UserPreferences>user.preferences;
+		const firstLoginPassed = userPreferences ? userPreferences.firstLogin : false;
+
+		if (!user.forcePasswordChange && firstLoginPassed) {
+			throw new ForbiddenOperationError('The password is not temporary, hence can not be changed.');
+		} // Password change was forces or this is a first logon for the user
+
+		const account: Account = await this.findByUserIdOrFail(userId);
+
+		if (account.systemId) {
+			throw new ForbiddenOperationError('External account details can not be changed.');
+		}
+
+		if (await this.validatePassword(account, password)) {
+			throw new ForbiddenOperationError('New password can not be same as old password.');
+		}
+
+		try {
+			account.password = password;
+			await this.save(account);
+		} catch (err) {
+			throw new EntityNotFoundError(AccountEntity.name);
+		}
+		try {
+			user.forcePasswordChange = false;
+			await this.userRepo.save(user);
+		} catch (err) {
+			throw new EntityNotFoundError(User.name);
 		}
 	}
 
@@ -201,5 +351,11 @@ export class AccountService extends AbstractAccountService {
 			}
 		}
 		return null;
+	}
+
+	private async checkUniqueEmail(account: Account, user: User, email: string): Promise<void> {
+		if (!(await this.accountValidationService.isUniqueEmail(email, user.id, account.id, account.systemId))) {
+			throw new ValidationError(`The email address is already in use!`);
+		}
 	}
 }
