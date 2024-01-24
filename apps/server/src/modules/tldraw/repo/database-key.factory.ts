0335c47a0e098c1ac1a8c037d357266f3a8bf1b6
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2024-01-24 16:36:08 +0100

    Fix code style

--- apps/server/src/modules/tldraw/repo/database-key.factory.ts
+++ apps/server/src/modules/tldraw/repo/database-key.factory.ts
@@ -1,39 +1,39 @@
 enum DatabaseAction {
-    UPDATE = 'update'
+	UPDATE = 'update'
 }
 
 enum Version {
-    V1_SV = 'v1_sv',
-    V1 = 'v1'
+	V1_SV = 'v1_sv',
+	V1 = 'v1'
 }
 
 interface UniqueDatabaseKey {
-    version: Version;
-    action?: DatabaseAction;
-    docName: string;
-    clock?: number;
+	version: Version;
+	action?: DatabaseAction;
+	docName: string;
+	clock?: number;
 }
 
 export class DatabaseKeyFactory {
-    static createForUpdate(docName: string, clock?: number): UniqueDatabaseKey {
-        const uniqueKey = {
-            docName,
-            version: Version.V1,
-            action: DatabaseAction.UPDATE,
-            // TODO: Do it work when key is exists, but value is undefined?
-            clock
-        }
+	static createForUpdate(docName: string, clock?: number): UniqueDatabaseKey {
+		const uniqueKey = {
+			docName,
+			version: Version.V1,
+			action: DatabaseAction.UPDATE,
+			// TODO: Do it work when key is exists, but value is undefined?
+			clock
+		}
 
-        return uniqueKey;
-    }
+		return uniqueKey;
+	}
 
-    // TODO: Copy paste from source code, but i think naming must be changed.
-    static createForStateVector(docName: string): UniqueDatabaseKey {
-        const uniqueKey = {
-            docName,
-            version: Version.V1_SV,
-        }
+	// TODO: Copy paste from source code, but i think naming must be changed.
+	static createForStateVector(docName: string): UniqueDatabaseKey {
+		const uniqueKey = {
+			docName,
+			version: Version.V1_SV,
+		}
 
-        return uniqueKey;
-    }
+		return uniqueKey;
+	}
 }
