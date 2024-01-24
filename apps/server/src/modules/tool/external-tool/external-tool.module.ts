1aa3af78d16b6f2cbec3f56670172874af109b13
Malte Berg <malte.berg@capgemini.com>
2024-01-24 13:06:07 +0100

    add pdfModule

--- apps/server/src/modules/tool/external-tool/external-tool.module.ts
+++ apps/server/src/modules/tool/external-tool/external-tool.module.ts
@@ -4,6 +4,7 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { ExternalToolRepo } from '@shared/repo';
 import { LoggerModule } from '@src/core/logger';
+import { PDFModule } from '@pyxlab/nestjs-pdf';
 import { CommonToolModule } from '../common';
 import { ToolContextMapper } from '../common/mapper/tool-context.mapper';
 import { ToolConfigModule } from '../tool-config.module';
@@ -19,7 +20,20 @@ import {
 } from './service';
 
 @Module({
-	imports: [CommonToolModule, ToolConfigModule, LoggerModule, OauthProviderServiceModule, EncryptionModule, HttpModule],
+	imports: [
+		CommonToolModule,
+		ToolConfigModule,
+		LoggerModule,
+		OauthProviderServiceModule,
+		EncryptionModule,
+		HttpModule,
+		PDFModule.register({
+			view: {
+				root: 'apps/server/src/modules/tool/external-tool/mustache-template/',
+				engine: 'mustache',
+			},
+		}),
+	],
 	providers: [
 		ExternalToolService,
 		ExternalToolServiceMapper,
