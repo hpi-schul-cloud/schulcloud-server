61f51ab37c8b47a21e170820beb0bcdbb8c07dcc
Arne Gnisa <arne.gnisa@capgemini.com>
2024-01-25 16:18:57 +0100

    N21-1630 adds loggable tests again

--- apps/server/src/modules/user-import/loggable/index.ts
+++ apps/server/src/modules/user-import/loggable/index.ts
@@ -5,5 +5,5 @@ export * from './school-id-does-not-match-with-user-school-id.loggable';
 export * from './migration-is-not-completed.loggable';
 export * from './migration-may-be-completed.loggable';
 export { UserImportConfigurationFailureLoggableException } from './user-import-configuration-failure-loggable-exception';
-export { UserImportFetchingFailureLoggableException } from './user-import-fetching-failure-loggable-exception';
+export { UserImportPopulateFailureLoggableException } from './user-import-populate-failure-loggable-exception';
 export { UserImportSchoolExternalIdMissingLoggableException } from './user-import-school-external-id-missing-loggable-exception';
--- apps/server/src/modules/user-import/loggable/user-import-populate-failure-loggable-exception.ts
+++ apps/server/src/modules/user-import/loggable/user-import-populate-failure-loggable-exception.ts
@@ -2,11 +2,11 @@ import { HttpStatus } from '@nestjs/common';
 import { BusinessError } from '@shared/common';
 import { ErrorLogMessage, Loggable, LogMessage, ValidationErrorLogMessage } from '@src/core/logger';
 
-export class UserImportFetchingFailureLoggableException extends BusinessError implements Loggable {
+export class UserImportPopulateFailureLoggableException extends BusinessError implements Loggable {
 	constructor(private readonly url: string) {
 		super(
 			{
-				type: 'USER_IMPORT_FETCHING_FAILURE',
+				type: 'USER_IMPORT_POPULATE_FAILURE',
 				title: 'Fetching import user failed.',
 				defaultMessage: 'While fetching import users an error occurred.',
 			},
@@ -16,8 +16,8 @@ export class UserImportFetchingFailureLoggableException extends BusinessError im
 
 	getLogMessage(): LogMessage | ErrorLogMessage | ValidationErrorLogMessage {
 		return {
-			type: 'USER_IMPORT_FETCHING_FAILURE',
-			message: 'While fetching import users an error occurred.',
+			type: 'USER_IMPORT_POPULATE_FAILURE',
+			message: 'While populate import users an error occurred.',
 			stack: this.stack,
 			data: {
 				url: this.url,
