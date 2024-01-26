fd5f8972933e6be8fdd69e7d5dc609e8f3d57ed9
Thomas Feldtkeller <thomas.feldtkeller@dataport.de>
2024-01-26 15:27:04 +0100

    trying to ignore unreachable code for test coverage

--- apps/server/src/modules/legacy-school/loggable/admin-api-school-create-no-id-error.loggable.ts
+++ apps/server/src/modules/legacy-school/loggable/admin-api-school-create-no-id-error.loggable.ts
@@ -1,9 +1,9 @@
-/* istanbul ignore file */
 import { InternalServerErrorException } from '@nestjs/common';
 import { ErrorLogMessage, Loggable, LogMessage, ValidationErrorLogMessage } from '@src/core/logger';
 
 export class AdminApiSchoolCreateNoIdErrorLoggable extends InternalServerErrorException implements Loggable {
 	getLogMessage(): LogMessage | ErrorLogMessage | ValidationErrorLogMessage {
+		/* istanbul ignore next */
 		return {
 			type: 'ADMIN_API_CREATED_SCHOOL_HAS_NO_ID',
 			message:
