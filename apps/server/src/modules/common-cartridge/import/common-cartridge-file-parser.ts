fb1b1a04058e90ea189da024b1bb8d01f61b0581
psachmann <patrick.sachmann@capgemini.com>
2024-01-29 14:28:29 +0100

    EW-694 some renaming

--- apps/server/src/modules/common-cartridge/import/common-cartridge-file-parser.ts
+++ apps/server/src/modules/common-cartridge/import/common-cartridge-file-parser.ts
@@ -3,12 +3,16 @@ import { CommonCartridgeManifestParser } from './common-cartridge-manifest-parse
 import { CommonCartridgeManifestNotFoundException } from './utils/common-cartridge-manifest-not-found.exception';
 
 export class CommonCartridgeFileParser {
-	public readonly manifestParser: CommonCartridgeManifestParser;
+	private readonly manifestParser: CommonCartridgeManifestParser;
 
 	public constructor(file: Buffer) {
 		const archive = new AdmZip(file);
 
 		this.manifestParser = new CommonCartridgeManifestParser(this.getManifestFileAsString(archive));
+	}
+
+	public get manifest(): CommonCartridgeManifestParser {
+		return this.manifestParser;
 	}
 
 	private getManifestFileAsString(archive: AdmZip): string | never {
