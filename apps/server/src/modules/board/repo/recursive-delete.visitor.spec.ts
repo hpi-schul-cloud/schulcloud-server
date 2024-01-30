4743fc0791c0bc91bd97daddbe5b1433f3e82f28
blazejpass <blazej.szczepanowski@gca.pass-consulting.com>
2024-01-30 10:25:52 +0100

    Fix lint

--- apps/server/src/modules/board/repo/recursive-delete.visitor.spec.ts
+++ apps/server/src/modules/board/repo/recursive-delete.visitor.spec.ts
@@ -16,7 +16,7 @@ import {
 	submissionContainerElementFactory,
 	submissionItemFactory,
 } from '@shared/testing';
-import { DrawingElementAdapterService } from '../tldraw-client/service/drawing-element-adapter.service';
+import { DrawingElementAdapterService } from '@modules/tldraw-client/service';
 import { RecursiveDeleteVisitor } from './recursive-delete.vistor';
 
 describe(RecursiveDeleteVisitor.name, () => {
