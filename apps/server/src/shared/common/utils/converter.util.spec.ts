4a328acaac5ec303ad5517986997b59c700a1a99
Marvin Öhlerking <marvin.oehlerking@capgemini.com>
2024-01-31 16:18:28 +0100

    Revert "change xml parser package"
    This reverts commit d97baa7223230b23713d1868516d75dda2a280e3.

--- apps/server/src/shared/common/utils/converter.util.spec.ts
+++ apps/server/src/shared/common/utils/converter.util.spec.ts
@@ -1,34 +1,22 @@
 import { ConverterUtil } from '@shared/common';
 
+class TestObject {
+	test: string;
+
+	constructor(test: string) {
+		this.test = test;
+	}
+}
 describe('ConverterUtil', () => {
 	let service: ConverterUtil;
-
 	beforeAll(() => {
 		service = new ConverterUtil();
 	});
-
 	describe('xml2Object', () => {
 		it('should map correctly to TestObject', () => {
-			const testXml = '<test><array n="nr1">1</array><array n="nr2">2</array><str>test1</str>test2</test>';
-
-			const result = service.xml2object(testXml);
-
-			expect(result).toEqual({
-				test: {
-					array: [
-						{
-							n: 'nr1',
-							value: 1,
-						},
-						{
-							n: 'nr2',
-							value: 2,
-						},
-					],
-					str: 'test1',
-					value: 'test2',
-				},
-			});
+			const test = '<test>test</test>';
+			const ret = service.xml2object<TestObject>(test);
+			expect(ret.test).toEqual('test');
 		});
 	});
 });
--- apps/server/src/shared/common/utils/converter.util.ts
+++ apps/server/src/shared/common/utils/converter.util.ts
@@ -1,20 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { X2jOptions, XMLParser } from 'fast-xml-parser';
+import xml2json from '@hendt/xml2json';
 
 /**
  * This class encapsulates
  */
 @Injectable()
 export class ConverterUtil {
-	xml2object<T>(xml: string, options?: X2jOptions): T {
-		const parser: XMLParser = new XMLParser(
-			options || {
-				ignoreAttributes: false,
-				attributeNamePrefix: '',
-				textNodeName: 'value',
-			}
-		);
-
-		return parser.parse(xml) as T;
+	xml2object<T>(xml: string): T {
+		return xml2json(xml) as T;
 	}
 }
