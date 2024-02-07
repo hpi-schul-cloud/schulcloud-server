95b5d8578527d80c377707a73e880e04f69bf674
Simone Radtke <simone.radtke@capgemini.com>
2024-02-07 14:25:18 +0100

    EW-539 Adjust test for cc element factory

--- apps/server/src/modules/common-cartridge/elements/common-cartridge-element-factory.spec.ts
+++ apps/server/src/modules/common-cartridge/elements/common-cartridge-element-factory.spec.ts
@@ -1,33 +1,31 @@
 import { InternalServerErrorException } from '@nestjs/common';
+import {
+	createCommonCartridgeMetadataElementPropsV110,
+	createCommonCartridgeMetadataElementPropsV130,
+} from '@shared/testing/factory/common-cartridge-element-props.factory';
 import { CommonCartridgeElementType, CommonCartridgeVersion } from '../common-cartridge.enums';
 import { CommonCartridgeElementFactory } from './common-cartridge-element-factory';
 import {
 	CommonCartridgeMetadataElementPropsV110,
 	CommonCartridgeMetadataElementV110,
 } from './v1.1.0/common-cartridge-metadata-element';
-import {
-	CommonCartridgeMetadataElementPropsV130,
-	CommonCartridgeMetadataElementV130,
-} from './v1.3.0/common-cartridge-metadata-element';
+import { CommonCartridgeMetadataElementV130 } from './v1.3.0/common-cartridge-metadata-element';
 
 describe('CommonCartridgeElementFactory', () => {
 	describe('createElement', () => {
 		describe('when Common Cartridge versions is supported', () => {
+			const propsV110 = createCommonCartridgeMetadataElementPropsV110();
+			const propsV130 = createCommonCartridgeMetadataElementPropsV130();
+
 			it('should return v1.1.0 element', () => {
-				const result = CommonCartridgeElementFactory.createElement({
-					version: CommonCartridgeVersion.V_1_1_0,
-					type: CommonCartridgeElementType.METADATA,
-				} as CommonCartridgeMetadataElementPropsV110);
+				const result = CommonCartridgeElementFactory.createElement(propsV110);
 
 				expect(result).toBeDefined();
 				expect(result).toBeInstanceOf(CommonCartridgeMetadataElementV110);
 			});
 
 			it('should return v1.3.0 element', () => {
-				const result = CommonCartridgeElementFactory.createElement({
-					version: CommonCartridgeVersion.V_1_3_0,
-					type: CommonCartridgeElementType.METADATA,
-				} as CommonCartridgeMetadataElementPropsV130);
+				const result = CommonCartridgeElementFactory.createElement(propsV130);
 
 				expect(result).toBeDefined();
 				expect(result).toBeInstanceOf(CommonCartridgeMetadataElementV130);
@@ -35,13 +33,13 @@ describe('CommonCartridgeElementFactory', () => {
 		});
 
 		describe('when versions is not supported', () => {
-			it('should throw InternalServerErrorException', () => {
-				const notSupportedVersions = [
-					CommonCartridgeVersion.V_1_0_0,
-					CommonCartridgeVersion.V_1_2_0,
-					CommonCartridgeVersion.V_1_4_0,
-				];
+			const notSupportedVersions = [
+				CommonCartridgeVersion.V_1_0_0,
+				CommonCartridgeVersion.V_1_2_0,
+				CommonCartridgeVersion.V_1_4_0,
+			];
 
+			it('should throw InternalServerErrorException', () => {
 				notSupportedVersions.forEach((version) => {
 					expect(() =>
 						CommonCartridgeElementFactory.createElement({
