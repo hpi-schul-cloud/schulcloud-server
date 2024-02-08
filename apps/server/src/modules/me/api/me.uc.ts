94852df1ecb8c4ec30340f63d2039ece43a886f0
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2024-02-08 14:25:16 +0100

    Add TODO: to comment

--- apps/server/src/modules/me/api/me.uc.ts
+++ apps/server/src/modules/me/api/me.uc.ts
@@ -12,7 +12,7 @@ export class MeUc {
 	public async getMe(userId: EntityId, schoolId: EntityId, accountId: EntityId): Promise<MeResponse> {
 		const [school, user] = await Promise.all([
 			this.schoolService.getSchoolById(schoolId),
-			this.userService.getUserEntityWithRoles(userId), // need to replace is valid user domain object avaible
+			this.userService.getUserEntityWithRoles(userId), // TODO: need to replace is valid user domain object avaible
 		]);
 
 		const dto = MeResponseMapper.mapToResponse(school, user, accountId);
