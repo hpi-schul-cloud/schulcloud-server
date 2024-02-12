438a0a26d4a2fe2c6328fde13c33254c5b295080
Wojciech Grancow <wojciech.grancow@gmail.com>
2024-02-12 10:00:58 +0100

    changes in reconcilation UC

--- apps/server/src/modules/deletion/uc/deletion-reconciliation.uc.ts
+++ apps/server/src/modules/deletion/uc/deletion-reconciliation.uc.ts
@@ -2,11 +2,13 @@ import { UserService } from '@modules/user';
 import { Injectable } from '@nestjs/common';
 import { LegacyLogger } from '@src/core/logger';
 import { SanisResponse, SchulconnexRestClient } from '@src/infra/schulconnex-client';
+import { SynchronizationService } from '..';
 
 @Injectable()
 export class DeletionReconciliationUc {
 	constructor(
 		private readonly schulconnexRestClient: SchulconnexRestClient,
+		private readonly synchronizationService: SynchronizationService,
 		private readonly userService: UserService,
 		private readonly logger: LegacyLogger
 	) {
@@ -15,6 +17,8 @@ export class DeletionReconciliationUc {
 
 	async findUsersToDelete(): Promise<void> {
 		this.logger.debug({ action: 'findUsersToDelete' });
+
+		await this.synchronizationService.createSynchronization();
 
 		const usersToCheck: SanisResponse[] = await this.schulconnexRestClient.getPersonenInfo({});
 
