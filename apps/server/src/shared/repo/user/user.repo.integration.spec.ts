0a43ef436f52b0e05638540d21974db83f1823b2
virgilchiriac <virgil.chiriac@dataport.de>
2024-02-12 14:16:15 +0100

    BC-6386 - refactor user.repo integration test

--- apps/server/src/shared/repo/user/user.repo.integration.spec.ts
+++ apps/server/src/shared/repo/user/user.repo.integration.spec.ts
@@ -441,47 +441,47 @@ describe('user repo', () => {
 				await em.persistAndFlush([user1, user2, user3]);
 				em.clear();
 
-				const expectedUser2 = {
-					firstName: user2.firstName,
-					lastName: user2.lastName,
-					email: user2.email,
-					roles: user2.roles,
-					school: user2.school,
-				};
-
-				const expectedUser3 = {
-					firstName: user3.firstName,
-					lastName: user3.lastName,
-					email: user3.email,
-					roles: user3.roles,
-					school: user3.school,
-				};
-
-				const expectedResult = user1.id;
-
 				return {
-					expectedResult,
-					expectedUser2,
-					expectedUser3,
 					user1,
 					user2,
 					user3,
 				};
 			};
 			it('should delete user', async () => {
-				const { expectedResult, expectedUser2, expectedUser3, user1, user2, user3 } = await setup();
-				const deleteResult = await repo.deleteUser(user1.id);
-				expect(deleteResult).toEqual(expectedResult);
+				const { user1 } = await setup();
+				await repo.deleteUser(user1.id);
 
 				const result1 = await em.find(User, { id: user1.id });
 				expect(result1).toHaveLength(0);
+			});
 
-				const result2 = await repo.findById(user2.id);
-				expect(result2).toMatchObject(expectedUser2);
+			it('should return the id of the deleted user', async () => {
+				const { user1 } = await setup();
+				const result = await repo.deleteUser(user1.id);
+				expect(result).toEqual(user1.id);
+			});
 
-				const result3 = await repo.findById(user3.id);
+			it('should return null if user was already deleted', async () => {
+				const { user1 } = await setup();
+				await repo.deleteUser(user1.id);
+				const secondDelete = await repo.deleteUser(user1.id);
+				expect(secondDelete).toBeNull();
+			});
 
-				expect(result3).toMatchObject(expectedUser3);
+			it('should not affect other users', async () => {
+				const { user1, user2, user3 } = await setup();
+				await repo.deleteUser(user1.id);
+
+				const emUser2 = await em.find(User, { id: user2.id });
+				const emUser3 = await em.find(User, { id: user3.id });
+				expect(emUser2).toHaveLength(1);
+				expect(emUser3).toHaveLength(1);
+
+				const resultUser2 = await repo.findById(user2.id);
+				const resultUser3 = await repo.findById(user3.id);
+
+				expect(resultUser2.id).toEqual(user2.id);
+				expect(resultUser3.id).toEqual(user3.id);
 			});
 		});
 	});
