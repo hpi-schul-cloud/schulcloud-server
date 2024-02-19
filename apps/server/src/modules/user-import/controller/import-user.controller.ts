db97fe691e79b53ee124289b02fee750f367d8fc
Marvin Öhlerking <marvin.oehlerking@capgemini.com>
2024-02-19 16:04:47 +0100

    fix timeout

--- apps/server/src/modules/user-import/controller/import-user.controller.ts
+++ apps/server/src/modules/user-import/controller/import-user.controller.ts
@@ -1,4 +1,5 @@
 import { Authenticate, CurrentUser, ICurrentUser } from '@modules/authentication';
+import { serverConfig } from '@modules/server';
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import {
 	ApiBadRequestResponse,
@@ -121,7 +122,7 @@ export class ImportUserController {
 		await this.userImportUc.endSchoolInMaintenance(currentUser.userId);
 	}
 
-	@RequestTimeout(120000)
+	@RequestTimeout(serverConfig().SCHULCONNEX_CLIENT__PERSONEN_INFO_TIMEOUT_IN_MS)
 	@Post('populate-import-users')
 	@ApiOperation({
 		summary: 'Populates import users',
