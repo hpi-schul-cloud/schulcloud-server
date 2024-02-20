1220958128e45a3794e26f859d02c511b2c8b36b
psachmann <patrick.sachmann@capgemini.com>
2024-02-20 15:41:32 +0100

    EW-694 working on coverage

--- apps/server/src/modules/common-cartridge/import/common-cartridge-manifest-parser.spec.ts
+++ apps/server/src/modules/common-cartridge/import/common-cartridge-manifest-parser.spec.ts
@@ -3,7 +3,13 @@ import { readFile } from 'fs/promises';
 import { CommonCartridgeManifestParser } from './common-cartridge-manifest-parser';
 
 describe('CommonCartridgeManifestParser', () => {
-	const setup = async () => {
+	const setupFile = async (loadFile: boolean) => {
+		if (!loadFile) {
+			const sut = new CommonCartridgeManifestParser('<manifest></manifest>');
+
+			return { sut };
+		}
+
 		const buffer = await readFile('./apps/server/test/assets/common-cartridge/us_history_since_1877.imscc');
 		const archive = new AdmZip(buffer);
 		const sut = new CommonCartridgeManifestParser(archive.readAsText('imsmanifest.xml'));
@@ -13,6 +19,8 @@ describe('CommonCartridgeManifestParser', () => {
 
 	describe('getSchema', () => {
 		describe('when schema is present', () => {
+			const setup = async () => setupFile(true);
+
 			it('should return the schema', async () => {
 				const { sut } = await setup();
 				const result = sut.getSchema();
@@ -20,10 +28,23 @@ describe('CommonCartridgeManifestParser', () => {
 				expect(result).toBe('IMS Common Cartridge');
 			});
 		});
+
+		describe('when schema is not present', () => {
+			const setup = async () => setupFile(false);
+
+			it('should return null', async () => {
+				const { sut } = await setup();
+				const result = sut.getSchema();
+
+				expect(result).toBeNull();
+			});
+		});
 	});
 
 	describe('getVersion', () => {
 		describe('when version is present', () => {
+			const setup = async () => setupFile(true);
+
 			it('should return the version', async () => {
 				const { sut } = await setup();
 				const result = sut.getVersion();
@@ -31,15 +52,39 @@ describe('CommonCartridgeManifestParser', () => {
 				expect(result).toBe('1.3.0');
 			});
 		});
+
+		describe('when version is not present', () => {
+			const setup = async () => setupFile(false);
+
+			it('should return null', async () => {
+				const { sut } = await setup();
+				const result = sut.getVersion();
+
+				expect(result).toBeNull();
+			});
+		});
 	});
 
 	describe('getTitle', () => {
 		describe('when title is present', () => {
+			const setup = async () => setupFile(true);
+
 			it('should return the title', async () => {
 				const { sut } = await setup();
 				const result = sut.getTitle();
 
 				expect(result).toBe('201510-AMH-2020-70C-12218-US History Since 1877');
+			});
+		});
+
+		describe('when title is not present', () => {
+			const setup = async () => setupFile(false);
+
+			it('should return null', async () => {
+				const { sut } = await setup();
+				const result = sut.getTitle();
+
+				expect(result).toBeNull();
 			});
 		});
 	});
