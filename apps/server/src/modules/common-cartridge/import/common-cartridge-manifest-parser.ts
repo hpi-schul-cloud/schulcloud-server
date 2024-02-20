fec4d919dd58c55adca8337f7bce732aa10cad97
psachmann <patrick.sachmann@capgemini.com>
2024-02-20 15:34:28 +0100

    EW-694 fixing tests

--- apps/server/src/modules/common-cartridge/import/common-cartridge-manifest-parser.ts
+++ apps/server/src/modules/common-cartridge/import/common-cartridge-manifest-parser.ts
@@ -4,24 +4,24 @@ export class CommonCartridgeManifestParser {
 	private readonly doc: Document;
 
 	public constructor(manifest: string) {
-		this.doc = new JSDOM(manifest).window.document;
+		this.doc = new JSDOM(manifest, { contentType: 'text/xml' }).window.document;
 	}
 
-	public getSchema(): string | undefined {
-		const schema = this.doc.querySelector('manifest > metadata > schema');
+	public getSchema(): string | null {
+		const result = this.doc.querySelector('manifest > metadata > schema');
 
-		return schema?.textContent || undefined;
+		return result?.textContent || null;
 	}
 
-	public getVersion(): string | undefined {
-		const version = this.doc.querySelector('manifest > metadata > schemaversion');
+	public getVersion(): string | null {
+		const result = this.doc.querySelector('manifest > metadata > schemaversion');
 
-		return version?.textContent || undefined;
+		return result?.textContent || null;
 	}
 
-	public getTitle(): string | undefined {
-		const title = this.doc.querySelector('manifest > metadata > lom > general > title > string');
+	public getTitle(): string | null {
+		const result = this.doc.querySelector('manifest > metadata > lom > general > title > string');
 
-		return title?.textContent || undefined;
+		return result?.textContent || null;
 	}
 }
