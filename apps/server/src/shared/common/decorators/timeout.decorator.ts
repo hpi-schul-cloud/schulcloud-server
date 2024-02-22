feacf27e8fd0c92e213cda1a0eac2c39ee9375ea
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2024-02-22 19:07:45 +0100

    Switch the way of configuration in timeout interceptor to avoid invalid imports from server module.

--- apps/server/src/shared/common/decorators/timeout.decorator.ts
+++ apps/server/src/shared/common/decorators/timeout.decorator.ts
@@ -1,5 +1,5 @@
 import { applyDecorators, SetMetadata } from '@nestjs/common';
 
-export function RequestTimeout(ms: number) {
-	return applyDecorators(SetMetadata('timeout', ms));
+export function RequestTimeout(requestTimeoutEnvirementName: string) {
+	return applyDecorators(SetMetadata('requestTimeoutEnvirementName', requestTimeoutEnvirementName));
 }
--- apps/server/src/shared/common/interceptor/timeout.interceptor.ts
+++ apps/server/src/shared/common/interceptor/timeout.interceptor.ts
@@ -1,7 +1,9 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor, RequestTimeoutException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Reflector } from '@nestjs/core';
 import { Observable, throwError, TimeoutError } from 'rxjs';
 import { catchError, timeout } from 'rxjs/operators';
+import { InterceptorConfig } from './interfaces';
 
 /**
  * This interceptor leaves the request execution after a given timeout in ms.
@@ -9,14 +11,19 @@ import { catchError, timeout } from 'rxjs/operators';
  */
 @Injectable()
 export class TimeoutInterceptor implements NestInterceptor {
-	constructor(private readonly requestTimeout: number) {}
+	constructor(private readonly configService: ConfigService<InterceptorConfig, true>) {}
 
 	intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
 		const reflector = new Reflector();
-		const timeoutValue =
-			reflector.get<number>('timeout', context.getHandler()) || reflector.get<number>('timeout', context.getClass());
+		const requestTimeoutEnvirementName =
+			reflector.get<keyof InterceptorConfig>('requestTimeoutEnvirementName', context.getHandler()) ||
+			reflector.get<keyof InterceptorConfig>('requestTimeoutEnvirementName', context.getClass());
+
+		// type of requestTimeoutEnvirementName is always invalid and can be different
+		const timeoutMS = this.configService.getOrThrow<number>(requestTimeoutEnvirementName || 'INCOMING_REQUEST_TIMEOUT');
+
 		return next.handle().pipe(
-			timeout(timeoutValue || this.requestTimeout),
+			timeout(timeoutMS),
 			catchError((err: Error) => {
 				if (err instanceof TimeoutError) {
 					return throwError(() => new RequestTimeoutException());
