32b05a3eebacfbf486ca144b778887bae3fa8940
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2024-02-26 08:54:33 +0100

    Update apps/server/src/shared/common/decorators/timeout.decorator.ts
    Co-authored-by: bergatco <129839305+bergatco@users.noreply.github.com>

--- apps/server/src/shared/common/decorators/timeout.decorator.ts
+++ apps/server/src/shared/common/decorators/timeout.decorator.ts
@@ -1,5 +1,5 @@
 import { applyDecorators, SetMetadata } from '@nestjs/common';
 
-export function RequestTimeout(requestTimeoutEnvirementName: string) {
-	return applyDecorators(SetMetadata('requestTimeoutEnvirementName', requestTimeoutEnvirementName));
+export function RequestTimeout(requestTimeoutEnvironmentName: string) {
+	return applyDecorators(SetMetadata('requestTimeoutEnvironmentName', requestTimeoutEnvironmentName));
 }
