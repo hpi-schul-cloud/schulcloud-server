b4cfa4c60603dbb036e547939a92bd857b46ea99
Tomasz Wiaderek <Tomasz.Wiaderek@gca.pass-consulting.com>
2024-03-04 12:45:55 +0100

    remove class constructor

--- apps/server/src/modules/user/legacy/controller/dto/class.response.ts
+++ apps/server/src/modules/user/legacy/controller/dto/class.response.ts
@@ -1,13 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class ClassResponse {
-	constructor({ name, gradeLevel }: ClassResponse) {
-		this.name = name;
-		this.gradeLevel = gradeLevel;
-	}
-
 	@ApiProperty()
-	name: string;
+	name?: string;
 
 	@ApiProperty()
 	gradeLevel?: number;
