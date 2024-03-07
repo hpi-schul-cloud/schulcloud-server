ce7e1dd11d1d14d6e632fa51a30eef09b93b8acf
Simone Radtke <simone.radtke@capgemini.com>
2024-03-07 15:52:28 +0100

    EW-539 Fix circular dependency

--- apps/server/src/modules/tool/school-external-tool/school-external-tool.module.ts
+++ apps/server/src/modules/tool/school-external-tool/school-external-tool.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { CommonToolModule } from '../common';
 import { ExternalToolModule } from '../external-tool';
 import { ToolConfigModule } from '../tool-config.module';
 import { SchoolExternalToolService, SchoolExternalToolValidationService } from './service';
 
 @Module({
-	imports: [CommonToolModule, ExternalToolModule, ToolConfigModule],
+	imports: [forwardRef(() => CommonToolModule), forwardRef(() => ExternalToolModule), ToolConfigModule],
 	providers: [SchoolExternalToolService, SchoolExternalToolValidationService],
 	exports: [SchoolExternalToolService, SchoolExternalToolValidationService],
 })
