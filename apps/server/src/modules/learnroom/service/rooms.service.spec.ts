4737d070dfe8450f870a4e5805eeb523336f0f87
Max Bischof <maximilian.bischof@dataport.de>
2024-03-08 13:13:51 +0100

    Remove welcome column board and BoardManagementUC

--- apps/server/src/modules/learnroom/service/rooms.service.spec.ts
+++ apps/server/src/modules/learnroom/service/rooms.service.spec.ts
@@ -1,15 +1,20 @@
 import { createMock, DeepMocked } from '@golevelup/ts-jest';
 import { Configuration } from '@hpi-schul-cloud/commons/lib';
 import { IConfig } from '@hpi-schul-cloud/commons/lib/interfaces/IConfig';
-import { ObjectId } from '@mikro-orm/mongodb';
 import { CardService, ColumnBoardService, ColumnService, ContentElementService } from '@modules/board';
 import { LessonService } from '@modules/lesson';
 import { TaskService } from '@modules/task';
 import { Test, TestingModule } from '@nestjs/testing';
-import { BoardExternalReference, BoardExternalReferenceType } from '@shared/domain/domainobject';
-import { EntityId } from '@shared/domain/types';
 import { BoardRepo } from '@shared/repo';
-import { boardFactory, courseFactory, lessonFactory, setupEntities, taskFactory, userFactory } from '@shared/testing';
+import {
+	boardFactory,
+	columnBoardTargetFactory,
+	courseFactory,
+	lessonFactory,
+	setupEntities,
+	taskFactory,
+	userFactory,
+} from '@shared/testing';
 import { ColumnBoardTargetService } from './column-board-target.service';
 import { RoomsService } from './rooms.service';
 
@@ -81,18 +86,26 @@ describe('rooms service', () => {
 	});
 
 	describe('updateBoard', () => {
-		describe('for lessons and tasks', () => {
+		describe('for lessons, tasks and column boards', () => {
 			const setup = () => {
 				const user = userFactory.buildWithId();
 				const room = courseFactory.buildWithId({ students: [user] });
 				const tasks = taskFactory.buildList(3, { course: room });
 				const lessons = lessonFactory.buildList(3, { course: room });
 				const board = boardFactory.buildWithId({ course: room });
+				const columnBoardTarget = columnBoardTargetFactory.build();
+				const columnBoardTargetId = 'testid';
 
 				board.syncBoardElementReferences([...tasks, ...lessons]);
 
 				const tasksSpy = taskService.findBySingleParent.mockResolvedValue([tasks, 3]);
 				const lessonsSpy = lessonService.findByCourseIds.mockResolvedValue([lessons, 3]);
+				const columnBoardIdsSpy = columnBoardService.findIdsByExternalReference.mockResolvedValue([
+					columnBoardTargetId,
+				]);
+				const columnBoardTargetsSpy = columnBoardTargetService.findOrCreateTargets.mockResolvedValue([
+					columnBoardTarget,
+				]);
 				const syncBoardElementReferencesSpy = jest.spyOn(board, 'syncBoardElementReferences');
 				const saveSpy = boardRepo.save.mockResolvedValue();
 
@@ -102,10 +115,13 @@ describe('rooms service', () => {
 					room,
 					tasks,
 					lessons,
+					columnBoardTarget,
 					lessonsSpy,
 					tasksSpy,
 					syncBoardElementReferencesSpy,
 					saveSpy,
+					columnBoardIdsSpy,
+					columnBoardTargetsSpy,
 				};
 			};
 
@@ -121,155 +137,28 @@ describe('rooms service', () => {
 				expect(tasksSpy).toHaveBeenCalledWith(user.id, room.id);
 			});
 
-			it('should sync boards lessons with fetched tasks and lessons', async () => {
-				const { board, room, user, tasks, lessons, syncBoardElementReferencesSpy } = setup();
+			it('should fetch all column boards', async () => {
+				const { board, room, user } = setup();
+
 				await roomsService.updateBoard(board, room.id, user.id);
-				expect(syncBoardElementReferencesSpy).toHaveBeenCalledWith([...lessons, ...tasks]);
+
+				expect(columnBoardService.findIdsByExternalReference).toHaveBeenCalledWith({
+					type: 'course',
+					id: room.id,
+				});
+				expect(columnBoardTargetService.findOrCreateTargets).toHaveBeenCalledWith(['testid']);
+			});
+
+			it('should sync boards lessons with fetched tasks and lessons', async () => {
+				const { board, room, user, tasks, lessons, columnBoardTarget, syncBoardElementReferencesSpy } = setup();
+				await roomsService.updateBoard(board, room.id, user.id);
+				expect(syncBoardElementReferencesSpy).toHaveBeenCalledWith([...lessons, ...tasks, columnBoardTarget]);
 			});
 
 			it('should persist board', async () => {
 				const { board, room, user, saveSpy } = setup();
 				await roomsService.updateBoard(board, room.id, user.id);
 				expect(saveSpy).toHaveBeenCalledWith(board);
-			});
-		});
-
-		describe('for column boards', () => {
-			const setup = () => {
-				lessonService.findByCourseIds.mockResolvedValueOnce([[], 0]);
-				taskService.findBySingleParent.mockResolvedValueOnce([[], 0]);
-
-				const user = userFactory.buildWithId();
-				const course1 = courseFactory.buildWithId({ students: [user] });
-				const course2 = courseFactory.buildWithId({ students: [user] });
-				const boardWithoutColumnBoard = boardFactory.build({ course: course1 });
-				const boardWithColumnBoard = boardFactory.build({ course: course2 });
-				const columnBoardId = new ObjectId().toHexString();
-
-				jest.spyOn(boardWithoutColumnBoard, 'syncBoardElementReferences').mockImplementation();
-				jest.spyOn(boardWithColumnBoard, 'syncBoardElementReferences').mockImplementation();
-
-				columnBoardService.findIdsByExternalReference.mockImplementation(
-					async (courseReference: BoardExternalReference): Promise<EntityId[]> => {
-						if (courseReference.id === boardWithColumnBoard.course.id) {
-							return Promise.resolve([columnBoardId]);
-						}
-						return Promise.resolve([]);
-					}
-				);
-
-				return { user, boardWithoutColumnBoard, boardWithColumnBoard, columnBoardId };
-			};
-
-			describe('when ColumnBoard-feature is enabled', () => {
-				const setupWithEnvVariables = () => {
-					Configuration.set('FEATURE_COLUMN_BOARD_ENABLED', 'true');
-
-					return setup();
-				};
-
-				describe('when no column board exists for the board', () => {
-					it('should create one', async () => {
-						const { user, boardWithoutColumnBoard: board } = setupWithEnvVariables();
-
-						await roomsService.updateBoard(board, board.course.id, user.id);
-
-						expect(columnBoardService.createWelcomeColumnBoard).toBeCalledWith<BoardExternalReference[]>({
-							type: BoardExternalReferenceType.Course,
-							id: board.course.id,
-						});
-					});
-				});
-
-				describe('when a colum board exists for the board', () => {
-					it('should not create one', async () => {
-						const { user, boardWithColumnBoard: board } = setupWithEnvVariables();
-
-						await roomsService.updateBoard(board, board.course.id, user.id);
-
-						expect(columnBoardService.createWelcomeColumnBoard).not.toBeCalledWith(
-							expect.objectContaining({ id: board.course.id })
-						);
-					});
-				});
-
-				it('should use the service to find or create targets', async () => {
-					const { user, boardWithColumnBoard: board, columnBoardId } = setupWithEnvVariables();
-
-					await roomsService.updateBoard(board, board.course.id, user.id);
-
-					expect(columnBoardTargetService.findOrCreateTargets).toBeCalledWith([columnBoardId]);
-				});
-			});
-
-			describe('when ColumnBoard-feature and COLUMN_BOARD_HELP_LINK is enabled', () => {
-				const setupWithEnvVariables = () => {
-					Configuration.set('FEATURE_COLUMN_BOARD_ENABLED', 'true');
-					Configuration.set('COLUMN_BOARD_HELP_LINK', 'www.google.com');
-
-					return setup();
-				};
-
-				describe('when no column board exists for the board', () => {
-					it('should create one', async () => {
-						const { user, boardWithoutColumnBoard: board } = setupWithEnvVariables();
-
-						await roomsService.updateBoard(board, board.course.id, user.id);
-
-						expect(columnBoardService.createWelcomeColumnBoard).toBeCalledWith<BoardExternalReference[]>({
-							type: BoardExternalReferenceType.Course,
-							id: board.course.id,
-						});
-					});
-				});
-			});
-
-			describe('when ColumnBoard-feature and COLUMN_BOARD_FEEDBACK_LINK is enabled', () => {
-				const setupWithEnvVariables = () => {
-					Configuration.set('FEATURE_COLUMN_BOARD_ENABLED', 'true');
-					Configuration.set('COLUMN_BOARD_FEEDBACK_LINK', 'www.twitter.com');
-
-					return setup();
-				};
-
-				describe('when no column board exists for the board', () => {
-					it('should create one', async () => {
-						const { user, boardWithoutColumnBoard: board } = setupWithEnvVariables();
-
-						await roomsService.updateBoard(board, board.course.id, user.id);
-
-						expect(columnBoardService.createWelcomeColumnBoard).toBeCalledWith<BoardExternalReference[]>({
-							type: BoardExternalReferenceType.Course,
-							id: board.course.id,
-						});
-					});
-				});
-			});
-
-			describe('when ColumnBoard-feature is disabled', () => {
-				const setupWithEnvVariables = () => {
-					Configuration.set('FEATURE_COLUMN_BOARD_ENABLED', 'false');
-
-					return setup();
-				};
-
-				describe('when no column board exists for the board', () => {
-					it('should NOT create one', async () => {
-						const { user, boardWithoutColumnBoard: board } = setupWithEnvVariables();
-
-						await roomsService.updateBoard(board, board.course.id, user.id);
-
-						expect(columnBoardService.createWelcomeColumnBoard).not.toBeCalled();
-					});
-				});
-
-				it('should NOT use the service to find or create targets', async () => {
-					const { user, boardWithColumnBoard: board } = setupWithEnvVariables();
-
-					await roomsService.updateBoard(board, board.course.id, user.id);
-
-					expect(columnBoardTargetService.findOrCreateTargets).not.toBeCalled();
-				});
 			});
 		});
 	});
--- apps/server/src/modules/management/management.module.ts
+++ apps/server/src/modules/management/management.module.ts
@@ -1,19 +1,17 @@
 import { Configuration } from '@hpi-schul-cloud/commons/lib';
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { ConsoleWriterService } from '@infra/console';
 import { DatabaseManagementModule, DatabaseManagementService } from '@infra/database';
 import { EncryptionModule } from '@infra/encryption';
 import { FileSystemModule } from '@infra/file-system';
 import { KeycloakConfigurationModule } from '@infra/identity-management/keycloak-configuration/keycloak-configuration.module';
+import { serverConfig } from '@modules/server';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { createConfigModuleOptions } from '@src/config';
 import { LoggerModule } from '@src/core/logger';
-import { serverConfig } from '@modules/server';
-import { BoardManagementConsole } from './console/board-management.console';
 import { DatabaseManagementConsole } from './console/database-management.console';
 import { DatabaseManagementController } from './controller/database-management.controller';
 import { BsonConverter } from './converter/bson.converter';
-import { BoardManagementUc } from './uc/board-management.uc';
 import { DatabaseManagementUc } from './uc/database-management.uc';
 
 const baseImports = [
@@ -36,8 +34,6 @@ const providers = [
 	DatabaseManagementConsole,
 	// infra services
 	ConsoleWriterService,
-	BoardManagementConsole,
-	BoardManagementUc,
 ];
 
 const controllers = [DatabaseManagementController];
