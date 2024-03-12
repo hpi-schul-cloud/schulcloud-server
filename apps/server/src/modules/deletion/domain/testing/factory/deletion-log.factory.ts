c210e96df8e43f615ebede1531b9dd6fbd2ad3af
WojciechGrancow <Wojciech.Grancow@gca.pass-consulting.com>
2024-03-12 11:30:07 +0100

    fix after review

--- apps/server/src/modules/deletion/domain/testing/factory/deletion-log.factory.ts
+++ apps/server/src/modules/deletion/domain/testing/factory/deletion-log.factory.ts
@@ -1,21 +1,10 @@
 import { DoBaseFactory } from '@shared/testing';
 import { ObjectId } from '@mikro-orm/mongodb';
-import { DeepPartial } from 'fishery';
 import { DomainOperationReportBuilder, DomainDeletionReportBuilder } from '../../../builder';
 import { DomainName, OperationType } from '../../../types';
 import { DeletionLog, DeletionLogProps } from '../../deletion-log.do';
 
-class DeletionLogFactory extends DoBaseFactory<DeletionLog, DeletionLogProps> {
-	withDeletionRequestId(id: string): this {
-		const params: DeepPartial<DeletionLogProps> = {
-			deletionRequestId: id,
-		};
-
-		return this.params(params);
-	}
-}
-
-export const deletionLogFactory = DeletionLogFactory.define(DeletionLog, () => {
+export const deletionLogFactory = DoBaseFactory.define<DeletionLog, DeletionLogProps>(DeletionLog, () => {
 	return {
 		id: new ObjectId().toHexString(),
 		domain: DomainName.USER,
