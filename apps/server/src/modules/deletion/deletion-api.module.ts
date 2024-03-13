c6a8863941126e9ed96a76528d7cc1c8f36484a8
WojciechGrancow <Wojciech.Grancow@gca.pass-consulting.com>
2024-03-13 10:10:00 +0100

    correct imports

--- apps/server/src/modules/deletion/deletion-api.module.ts
+++ apps/server/src/modules/deletion/deletion-api.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { LoggerModule } from '@src/core/logger';
 import { CqrsModule } from '@nestjs/cqrs';
+import { AuthenticationModule } from '@modules/authentication';
 import { DeletionRequestsController } from './controller/deletion-requests.controller';
 import { DeletionExecutionsController } from './controller/deletion-executions.controller';
 import { DeletionRequestUc } from './uc';
 import { DeletionModule } from '.';
 
 @Module({
-	imports: [CqrsModule, DeletionModule, LoggerModule],
+	imports: [CqrsModule, DeletionModule, LoggerModule, AuthenticationModule],
 	controllers: [DeletionRequestsController, DeletionExecutionsController],
 	providers: [DeletionRequestUc],
 })
