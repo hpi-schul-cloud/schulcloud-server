2512d6f40d00c5179dfe4d678045c54d8f44fa92
Tomasz Wiaderek <Tomasz.Wiaderek@gca.pass-consulting.com>
2024-03-13 12:15:27 +0100

    add unit test for search-query-helper

--- apps/server/src/modules/user/legacy/repo/helper/search-query-helper.spec.ts
+++ apps/server/src/modules/user/legacy/repo/helper/search-query-helper.spec.ts
@@ -1,14 +1,14 @@
 import { ObjectId } from 'bson';
 import { UserSearchQuery } from '../../interfaces';
 import { SearchQueryHelper } from '.';
-import { UsersSearchQueryParams } from '../../controller/dto';
+import {RangeType, UsersSearchQueryParams} from '../../controller/dto';
 
 describe('Search query helper', () => {
 	afterEach(() => {
 		jest.resetAllMocks();
 	});
 
-	describe('when searching by searchQuery', () => {
+	describe('when search parameters exists', () => {
 		const setup = () => {
 			const exampleId = '5fa31aacb229544f2c697b48';
 
@@ -48,7 +48,7 @@ describe('Search query helper', () => {
 			};
 		};
 
-		it('should fill searchQuery and searchFilterGate', () => {
+		it('setSearchParametersIfExist should fill searchQuery and searchFilterGate', () => {
 			const { queryParams, query } = setup();
 
 			SearchQueryHelper.setSearchParametersIfExist(query, queryParams);
@@ -56,6 +56,67 @@ describe('Search query helper', () => {
 			expect(query.searchQuery).toEqual('test t te tes est');
 			expect(query.searchFilterGate).toEqual(9);
 			expect(query.sort).toEqual({ firstName: 1, sortBySearchQueryResult: 1 });
+		});
+	});
+
+	describe('when date parameters exists', () => {
+		const setup = () => {
+			const exampleId = '5fa31aacb229544f2c697b48';
+
+			const dateParam: Record<RangeType, Date> = {
+				$gt: new Date('2024-02-08T23:00:00Z'),
+				$gte: new Date('2024-02-08T23:00:00Z'),
+				$lt: new Date('2024-02-08T23:00:00Z'),
+				$lte: new Date('2024-02-08T23:00:00Z'),
+			};
+
+			const queryParams: UsersSearchQueryParams = {
+				$skip: 0,
+				$limit: 5,
+				$sort: { firstName: 1 },
+				createdAt: dateParam,
+				lastLoginSystemChange: dateParam,
+				outdatedSince: dateParam,
+			};
+
+			const query: UserSearchQuery = {
+				skip: 0,
+				limit: 5,
+				sort: { firstName: 1 },
+				schoolId: new ObjectId(exampleId),
+				schoolYearId: new ObjectId(exampleId),
+				roles: new ObjectId(exampleId),
+				select: [
+					'consentStatus',
+					'consent',
+					'classes',
+					'firstName',
+					'lastName',
+					'email',
+					'createdAt',
+					'importHash',
+					'birthday',
+					'preferences.registrationMailSend',
+					'lastLoginSystemChange',
+					'outdatedSince',
+				],
+			};
+
+			return {
+				queryParams,
+				query,
+				dateParam,
+			};
+		};
+
+		it('setDateParametersIfExists should fill date params', () => {
+			const { queryParams, query, dateParam } = setup();
+
+			SearchQueryHelper.setDateParametersIfExists(query, queryParams);
+
+			expect(query.createdAt).toEqual(dateParam);
+			expect(query.lastLoginSystemChange).toEqual(dateParam);
+			expect(query.outdatedSince).toEqual(dateParam);
 		});
 	});
 });
