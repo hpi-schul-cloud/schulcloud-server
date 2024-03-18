9ed26a3e020a2991307c4a15be2228e0e06ea982
dyedwiper <maximilian.becker@dataport.de>
2024-03-18 14:06:03 +0100

    Use role enum in migration

--- apps/server/src/migrations/mikro-orm/Migration20240315140224.ts
+++ apps/server/src/migrations/mikro-orm/Migration20240315140224.ts
@@ -2,13 +2,14 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Migration } from '@mikro-orm/migrations-mongodb';
+import { RoleName } from '@shared/domain/interface';
 import { FileRecordParentType } from '@src/modules/files-storage/entity';
 
 export class Migration20240315140224 extends Migration {
 	async up(): Promise<void> {
 		console.log('Start updating parentType of fileRecords if creator is teacher.');
 
-		const teacherRole = (await this.driver.findOne('roles', { name: 'teacher' })) as any;
+		const teacherRole = (await this.driver.findOne('roles', { name: RoleName.TEACHER })) as any;
 		const teachers = await this.driver.aggregate('users', [
 			{ $match: { roles: teacherRole._id } },
 			{ $project: { _id: 1 } },
