adb9b1b24d6ba5820599f6a55c8ade58d341bffa
SevenWaysDP <sergej.hoffmann@dataport.de>
2024-03-19 13:28:53 +0100

    BC-5335 code review repo.inteface

--- apps/server/src/modules/system/domain/interface/system.repo.interface.ts
+++ apps/server/src/modules/system/domain/interface/system.repo.interface.ts
@@ -1,7 +1,9 @@
+import { SystemEntity } from '@shared/domain/entity';
 import { EntityId } from '@shared/domain/types/entity-id';
+import { BaseDomainObjectRepo } from '@shared/repo/base-domain-object.repo';
 import { System } from '../system.do';
 
-export interface SystemRepo {
+export interface SystemRepo extends BaseDomainObjectRepo<System, SystemEntity> {
 	getSystemsByIds(systemIds: EntityId[]): Promise<System[]>;
 
 	getSystemById(systemId: EntityId): Promise<System | null>;
@@ -9,8 +11,6 @@ export interface SystemRepo {
 	findAllForLdapLogin(): Promise<System[]>;
 
 	delete(domainObject: System): Promise<void>;
-
-	save(domainObject: System): Promise<System>;
 }
 
 export const SYSTEM_REPO = 'SYSTEM_REPO';
