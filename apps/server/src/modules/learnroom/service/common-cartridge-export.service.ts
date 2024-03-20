0f6f84c1e244fba19dd06a789f6f6831b664bfdc
Patrick Sachmann <patrick.sachmann@capgemini.com>
2024-03-20 13:11:11 +0100

    EW-796 adding column boards to cc export

--- apps/server/src/modules/learnroom/service/common-cartridge-export.service.ts
+++ apps/server/src/modules/learnroom/service/common-cartridge-export.service.ts
@@ -6,8 +6,10 @@ import {
 import { LessonService } from '@modules/lesson';
 import { TaskService } from '@modules/task';
 import { Injectable } from '@nestjs/common';
+import { BoardExternalReferenceType } from '@shared/domain/domainobject';
 import { ComponentProperties } from '@shared/domain/entity';
 import { EntityId } from '@shared/domain/types';
+import { ColumnBoardService } from '@src/modules/board';
 import { createIdentifier } from '@src/modules/common-cartridge/export/utils';
 import { CommonCartridgeMapper } from '../mapper/common-cartridge.mapper';
 import { CourseService } from './course.service';
@@ -18,6 +20,7 @@ export class CommonCartridgeExportService {
 		private readonly courseService: CourseService,
 		private readonly lessonService: LessonService,
 		private readonly taskService: TaskService,
+		private readonly columnBoardService: ColumnBoardService,
 		private readonly commonCartridgeMapper: CommonCartridgeMapper
 	) {}
 
@@ -29,6 +32,7 @@ export class CommonCartridgeExportService {
 
 		await this.addLessons(builder, courseId, version);
 		await this.addTasks(builder, courseId, userId, version);
+		await this.addColumnBoards(builder, courseId);
 
 		return builder.build();
 	}
@@ -68,6 +72,22 @@ export class CommonCartridgeExportService {
 		tasks.forEach((task) => {
 			organization.addResource(this.commonCartridgeMapper.mapTaskToResource(task, version));
 		});
+	}
+
+	private async addColumnBoards(builder: CommonCartridgeFileBuilder, courseId: EntityId): Promise<void> {
+		const columnBoardIds = await this.columnBoardService.findIdsByExternalReference({
+			type: BoardExternalReferenceType.Course,
+			id: courseId,
+		});
+
+		for await (const columnBoardId of columnBoardIds) {
+			const columnBoard = await this.columnBoardService.findById(columnBoardId);
+
+			builder.addOrganization({
+				title: columnBoard.title,
+				identifier: createIdentifier(columnBoard.id),
+			});
+		}
 	}
 
 	private addComponentToOrganization(
