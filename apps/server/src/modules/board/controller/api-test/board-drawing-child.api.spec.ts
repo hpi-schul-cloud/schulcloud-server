377bb4f42fbeba013e271e7f33917fd325b27c51
blazejpass <blazej.szczepanowski@gca.pass-consulting.com>
2024-03-21 14:28:58 +0100

    Remove old client API implementation for tests

--- apps/server/src/modules/board/controller/api-test/board-drawing-child.api.spec.ts
+++ apps/server/src/modules/board/controller/api-test/board-drawing-child.api.spec.ts
@@ -1,10 +1,7 @@
 import { EntityManager, ObjectId } from '@mikro-orm/mongodb';
-import { ICurrentUser } from '@modules/authentication';
-import { JwtAuthGuard } from '@modules/authentication/guard/jwt-auth.guard';
 import { ServerTestModule } from '@modules/server/server.module';
-import { ExecutionContext, INestApplication } from '@nestjs/common';
+import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { ApiValidationError } from '@shared/common';
 import { BoardExternalReferenceType } from '@shared/domain/domainobject';
 import {
 	cardNodeFactory,
@@ -12,59 +9,27 @@ import {
 	columnBoardNodeFactory,
 	columnNodeFactory,
 	courseFactory,
-	mapUserToCurrentUser,
-	userFactory,
+	TestApiClient,
+	UserAndAccountTestFactory,
 } from '@shared/testing';
-import { Request } from 'express';
-import request from 'supertest';
 import { drawingElementNodeFactory } from '@shared/testing/factory/boardnode/drawing-element-node.factory';
 
 const baseRouteName = '/boards';
 
-class API {
-	app: INestApplication;
-
-	constructor(app: INestApplication) {
-		this.app = app;
-	}
-
-	async get(id: string) {
-		const response = await request(this.app.getHttpServer())
-			.get(`${baseRouteName}/${id}/hasDrawingChild`)
-			.set('Accept', 'application/json');
-
-		return {
-			result: response.text === 'true',
-			error: response.body as ApiValidationError,
-			status: response.status,
-		};
-	}
-}
-
 describe(`has drawing child (api)`, () => {
 	let app: INestApplication;
 	let em: EntityManager;
-	let currentUser: ICurrentUser;
-	let api: API;
+	let testApiClient: TestApiClient;
 
 	beforeAll(async () => {
 		const module: TestingModule = await Test.createTestingModule({
 			imports: [ServerTestModule],
-		})
-			.overrideGuard(JwtAuthGuard)
-			.useValue({
-				canActivate(context: ExecutionContext) {
-					const req: Request = context.switchToHttp().getRequest();
-					req.user = currentUser;
-					return true;
-				},
-			})
-			.compile();
+		}).compile();
 
 		app = module.createNestApplication();
 		await app.init();
 		em = module.get(EntityManager);
-		api = new API(app);
+		testApiClient = new TestApiClient(app, baseRouteName);
 	});
 
 	afterAll(async () => {
@@ -73,10 +38,11 @@ describe(`has drawing child (api)`, () => {
 
 	const setup = async () => {
 		await cleanupCollections(em);
-		const user = userFactory.build();
-		const course = courseFactory.build({ teachers: [user] });
-		const course2 = courseFactory.build({ teachers: [user] });
-		await em.persistAndFlush([user, course, course2]);
+		const { teacherAccount, teacherUser } = UserAndAccountTestFactory.buildTeacher();
+
+		const course = courseFactory.build({ teachers: [teacherUser] });
+		const course2 = courseFactory.build({ teachers: [teacherUser] });
+		await em.persistAndFlush([teacherAccount, teacherUser, course, course2]);
 
 		const columnBoardNode = columnBoardNodeFactory.buildWithId({
 			context: { id: course.id, type: BoardExternalReferenceType.Course },
@@ -91,43 +57,43 @@ describe(`has drawing child (api)`, () => {
 		await em.persistAndFlush([columnBoardNode, columnNode, cardNode, drawingNode, columnBoardNoChildrenNode]);
 		em.clear();
 
-		currentUser = mapUserToCurrentUser(user);
+		const loggedInClient = await testApiClient.login(teacherAccount);
 
-		return { columnBoardNode, columnBoardNoChildrenNode };
+		return { columnBoardNode, columnBoardNoChildrenNode, loggedInClient };
 	};
 
 	describe('with valid board ids', () => {
 		it('should return status 200', async () => {
-			const { columnBoardNode } = await setup();
+			const { columnBoardNode, loggedInClient } = await setup();
 
-			const response = await api.get(`${columnBoardNode.id}`);
+			const response = await loggedInClient.get(`${columnBoardNode.id}`);
 
 			expect(response.status).toEqual(200);
 		});
 
 		it('should return true as response', async () => {
-			const { columnBoardNode } = await setup();
+			const { columnBoardNode, loggedInClient } = await setup();
 
-			const result = await api.get(columnBoardNode.id);
+			const result = await loggedInClient.get(`${columnBoardNode.id}/hasDrawingChild`);
 
-			expect(result.result).toEqual(true);
+			expect(result.text).toEqual('true');
 		});
 
 		it('should return false as response', async () => {
-			const { columnBoardNoChildrenNode } = await setup();
+			const { columnBoardNoChildrenNode, loggedInClient } = await setup();
 
-			const { result } = await api.get(columnBoardNoChildrenNode.id);
+			const result = await loggedInClient.get(`${columnBoardNoChildrenNode.id}/hasDrawingChild`);
 
-			expect(result).toEqual(false);
+			expect(result.text).toEqual('false');
 		});
 	});
 
 	describe('with invalid board id', () => {
 		it('should return status 404', async () => {
-			await setup();
+			const { loggedInClient } = await setup();
 			const notExistingBoardId = new ObjectId().toString();
 
-			const response = await api.get(notExistingBoardId);
+			const response = await loggedInClient.get(notExistingBoardId);
 
 			expect(response.status).toEqual(404);
 		});
@@ -136,12 +102,12 @@ describe(`has drawing child (api)`, () => {
 	describe('with invalid user', () => {
 		it('should return status 403', async () => {
 			const { columnBoardNode } = await setup();
+			const { teacherAccount, teacherUser } = UserAndAccountTestFactory.buildTeacher();
 
-			const invalidUser = userFactory.build();
-			await em.persistAndFlush([invalidUser]);
-			currentUser = mapUserToCurrentUser(invalidUser);
+			await em.persistAndFlush([teacherAccount, teacherUser]);
 
-			const response = await api.get(`${columnBoardNode.id}`);
+			const loggedInWrongClient = await testApiClient.login(teacherAccount);
+			const response = await loggedInWrongClient.get(`${columnBoardNode.id}/hasDrawingChild`);
 
 			expect(response.status).toEqual(403);
 		});
