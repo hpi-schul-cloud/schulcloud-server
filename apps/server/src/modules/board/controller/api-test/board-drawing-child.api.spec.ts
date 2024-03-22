a1facdf82efc694ab8f015cc83f931aff663d5e4
blazejpass <blazej.szczepanowski@gca.pass-consulting.com>
2024-03-22 12:45:19 +0100

    Fix tests

--- apps/server/src/modules/board/controller/api-test/board-drawing-child.api.spec.ts
+++ apps/server/src/modules/board/controller/api-test/board-drawing-child.api.spec.ts
@@ -36,59 +36,92 @@ describe(`has drawing child (api)`, () => {
 		await app.close();
 	});
 
-	const setup = async () => {
-		await cleanupCollections(em);
-		const { teacherAccount, teacherUser } = UserAndAccountTestFactory.buildTeacher();
-
-		const course = courseFactory.build({ teachers: [teacherUser] });
-		const course2 = courseFactory.build({ teachers: [teacherUser] });
-		await em.persistAndFlush([teacherAccount, teacherUser, course, course2]);
-
-		const columnBoardNode = columnBoardNodeFactory.buildWithId({
-			context: { id: course.id, type: BoardExternalReferenceType.Course },
-		});
-		const columnBoardNoChildrenNode = columnBoardNodeFactory.buildWithId({
-			context: { id: course2.id, type: BoardExternalReferenceType.Course },
-		});
-		const columnNode = columnNodeFactory.buildWithId({ parent: columnBoardNode });
-		const cardNode = cardNodeFactory.buildWithId({ parent: columnNode });
-		const drawingNode = drawingElementNodeFactory.buildWithId({ parent: cardNode });
-
-		await em.persistAndFlush([columnBoardNode, columnNode, cardNode, drawingNode, columnBoardNoChildrenNode]);
-		em.clear();
-
-		const loggedInClient = await testApiClient.login(teacherAccount);
-
-		return { columnBoardNode, columnBoardNoChildrenNode, loggedInClient };
-	};
-
 	describe('with valid board ids', () => {
-		it('should return status 200', async () => {
-			const { columnBoardNode, loggedInClient } = await setup();
+		describe('when drawing is ancestor of a board', () => {
+			const setup = async () => {
+				await cleanupCollections(em);
+				const { teacherAccount, teacherUser } = UserAndAccountTestFactory.buildTeacher();
 
-			const response = await loggedInClient.get(`${columnBoardNode.id}`);
+				const course = courseFactory.build({ teachers: [teacherUser] });
+				const course2 = courseFactory.build({ teachers: [teacherUser] });
+				await em.persistAndFlush([teacherAccount, teacherUser, course, course2]);
 
-			expect(response.status).toEqual(200);
+				const columnBoardNode = columnBoardNodeFactory.buildWithId({
+					context: { id: course.id, type: BoardExternalReferenceType.Course },
+				});
+				const columnBoardNoChildrenNode = columnBoardNodeFactory.buildWithId({
+					context: { id: course2.id, type: BoardExternalReferenceType.Course },
+				});
+				const columnNode = columnNodeFactory.buildWithId({ parent: columnBoardNode });
+				const cardNode = cardNodeFactory.buildWithId({ parent: columnNode });
+				const drawingNode = drawingElementNodeFactory.buildWithId({ parent: cardNode });
+
+				await em.persistAndFlush([columnBoardNode, columnNode, cardNode, drawingNode, columnBoardNoChildrenNode]);
+				em.clear();
+
+				const loggedInClient = await testApiClient.login(teacherAccount);
+
+				return { columnBoardNode, columnBoardNoChildrenNode, loggedInClient };
+			};
+
+			it('should return status 200', async () => {
+				const { columnBoardNode, loggedInClient } = await setup();
+
+				const response = await loggedInClient.get(`${columnBoardNode.id}`);
+
+				expect(response.status).toEqual(200);
+			});
+			it('should return true as response', async () => {
+				const { columnBoardNode, loggedInClient } = await setup();
+
+				const result = await loggedInClient.get(`${columnBoardNode.id}/hasDrawingChild`);
+
+				expect(result.text).toEqual('true');
+			});
 		});
+		describe('when drawing is ancestor of a board', () => {
+			const setup = async () => {
+				await cleanupCollections(em);
+				const { teacherAccount, teacherUser } = UserAndAccountTestFactory.buildTeacher();
 
-		it('should return true as response', async () => {
-			const { columnBoardNode, loggedInClient } = await setup();
+				const course = courseFactory.build({ teachers: [teacherUser] });
+				await em.persistAndFlush([teacherAccount, teacherUser, course]);
 
-			const result = await loggedInClient.get(`${columnBoardNode.id}/hasDrawingChild`);
+				const columnBoardNoChildrenNode = columnBoardNodeFactory.buildWithId({
+					context: { id: course.id, type: BoardExternalReferenceType.Course },
+				});
 
-			expect(result.text).toEqual('true');
-		});
+				await em.persistAndFlush([columnBoardNoChildrenNode]);
+				em.clear();
 
-		it('should return false as response', async () => {
-			const { columnBoardNoChildrenNode, loggedInClient } = await setup();
+				const loggedInClient = await testApiClient.login(teacherAccount);
 
-			const result = await loggedInClient.get(`${columnBoardNoChildrenNode.id}/hasDrawingChild`);
+				return { columnBoardNoChildrenNode, loggedInClient };
+			};
 
-			expect(result.text).toEqual('false');
+			it('should return false as response', async () => {
+				const { columnBoardNoChildrenNode, loggedInClient } = await setup();
+
+				const result = await loggedInClient.get(`${columnBoardNoChildrenNode.id}/hasDrawingChild`);
+
+				expect(result.text).toEqual('false');
+			});
 		});
 	});
 
 	describe('with invalid board id', () => {
+		const setup = async () => {
+			await cleanupCollections(em);
+			const { teacherAccount, teacherUser } = UserAndAccountTestFactory.buildTeacher();
+			await em.persistAndFlush([teacherAccount, teacherUser]);
+
+			em.clear();
+
+			const loggedInClient = await testApiClient.login(teacherAccount);
+
+			return { loggedInClient };
+		};
+
 		it('should return status 404', async () => {
 			const { loggedInClient } = await setup();
 			const notExistingBoardId = new ObjectId().toString();
@@ -99,15 +132,30 @@ describe(`has drawing child (api)`, () => {
 		});
 	});
 
-	describe('with invalid user', () => {
-		it('should return status 403', async () => {
-			const { columnBoardNode } = await setup();
+	describe('with invalid user (no permission)', () => {
+		const setup = async () => {
+			await cleanupCollections(em);
 			const { teacherAccount, teacherUser } = UserAndAccountTestFactory.buildTeacher();
 
-			await em.persistAndFlush([teacherAccount, teacherUser]);
+			const course = courseFactory.build();
+			await em.persistAndFlush([teacherAccount, teacherUser, course]);
 
-			const loggedInWrongClient = await testApiClient.login(teacherAccount);
-			const response = await loggedInWrongClient.get(`${columnBoardNode.id}/hasDrawingChild`);
+			const columnBoardNode = columnBoardNodeFactory.buildWithId({
+				context: { id: course.id, type: BoardExternalReferenceType.Course },
+			});
+
+			await em.persistAndFlush([columnBoardNode]);
+			em.clear();
+
+			const loggedInClient = await testApiClient.login(teacherAccount);
+
+			return { columnBoardNode, loggedInClient };
+		};
+
+		it('should return status 403', async () => {
+			const { columnBoardNode, loggedInClient } = await setup();
+
+			const response = await loggedInClient.get(`${columnBoardNode.id}/hasDrawingChild`);
 
 			expect(response.status).toEqual(403);
 		});
