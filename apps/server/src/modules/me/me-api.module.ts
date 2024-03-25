f84e533f11f4347f15950046f575b38658973dc4
Max <53796487+dyedwiper@users.noreply.github.com>
2024-03-25 14:49:31 +0100

    BC-6945 Fix permission resolution for current user (#4870)

--- apps/server/src/modules/me/me-api.module.ts
+++ apps/server/src/modules/me/me-api.module.ts
@@ -3,10 +3,11 @@ import { SchoolModule } from '@modules/school';
 import { UserModule } from '@modules/user';
 import { AuthenticationModule } from '@modules/authentication';
 import { MeUc, MeController } from './api';
+import { PermissionService } from './domain/permission.service';
 
 @Module({
 	imports: [SchoolModule, UserModule, AuthenticationModule],
 	controllers: [MeController],
-	providers: [MeUc],
+	providers: [MeUc, PermissionService],
 })
 export class MeApiModule {}
