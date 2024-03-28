4a6fb246542d4b06ab868a09c80c54ff52051967
Szymon Szafoni <szymon.szafoni@gca.pass-consulting.com>
2024-03-28 13:09:12 +0100

    add needed modules and providers

--- apps/server/src/modules/deletion-console/deletion-console.module.ts
+++ apps/server/src/modules/deletion-console/deletion-console.module.ts
@@ -3,13 +3,27 @@ import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { ConsoleModule } from 'nestjs-console';
 import { ConsoleWriterModule } from '@infra/console';
-import { createConfigModuleOptions } from '@src/config';
+import { DB_PASSWORD, DB_URL, DB_USERNAME, createConfigModuleOptions } from '@src/config';
 import { DeletionClient } from './deletion-client';
 import { getDeletionClientConfig } from './deletion-client/deletion-client.config';
 import { DeletionQueueConsole } from './deletion-queue.console';
 import { DeletionExecutionConsole } from './deletion-execution.console';
 import { BatchDeletionService } from './services';
 import { BatchDeletionUc, DeletionExecutionUc } from './uc';
+import { UserService } from '../user';
+import { AccountModule } from '../account';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { defaultMikroOrmOptions } from '../server';
+import { ALL_ENTITIES } from '@shared/domain/entity';
+import { FileEntity } from '../files/entity';
+import { RoleRepo, UserRepo } from '@shared/repo';
+import { UserModule } from '../user/user.module';
+import { UserDORepo } from '@shared/repo/user/user-do.repo';
+import { RoleService } from '../role';
+import { RegistrationPinService } from '../registration-pin';
+import { LoggerModule } from '@src/core/logger';
+import { CqrsModule } from '@nestjs/cqrs';
+import { RegistrationPinRepo } from '../registration-pin/repo';
 
 @Module({
 	imports: [
@@ -17,6 +31,18 @@ import { BatchDeletionUc, DeletionExecutionUc } from './uc';
 		ConsoleWriterModule,
 		HttpModule,
 		ConfigModule.forRoot(createConfigModuleOptions(getDeletionClientConfig)),
+		AccountModule,
+		MikroOrmModule.forRoot({
+			...defaultMikroOrmOptions,
+			type: 'mongo',
+			clientUrl: DB_URL,
+			password: DB_PASSWORD,
+			user: DB_USERNAME,
+			entities: [...ALL_ENTITIES, FileEntity],
+			debug: true,
+		}),
+		LoggerModule,
+		CqrsModule,
 	],
 	providers: [
 		DeletionClient,
@@ -25,6 +51,13 @@ import { BatchDeletionUc, DeletionExecutionUc } from './uc';
 		DeletionExecutionUc,
 		DeletionQueueConsole,
 		DeletionExecutionConsole,
+		UserService,
+		UserRepo,
+		UserDORepo,
+		RoleService,
+		RegistrationPinService,
+		RoleRepo,
+		RegistrationPinRepo,
 	],
 })
 export class DeletionConsoleModule {}
