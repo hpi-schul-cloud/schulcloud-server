537b0f77ddc799ad4e75d5dcaa73e5144ad88131
Alexander Weber <alexander.weber@capgemini.com>
2024-04-03 08:12:03 +0200

    add resource get

--- apps/server/src/modules/common-cartridge/import/common-cartridge-file-parser.ts
+++ apps/server/src/modules/common-cartridge/import/common-cartridge-file-parser.ts
@@ -1,4 +1,5 @@
 import AdmZip from 'adm-zip';
+import { CommonCartridgeResourceNotFoundException } from './utils/common-cartridge-resource-not-found.exception';
 import { DEFAULT_FILE_PARSER_OPTIONS } from './common-cartridge-import.types';
 import { CommonCartridgeManifestParser } from './common-cartridge-manifest-parser';
 import { CommonCartridgeManifestNotFoundException } from './utils/common-cartridge-manifest-not-found.exception';
@@ -6,24 +7,38 @@ import { CommonCartridgeManifestNotFoundException } from './utils/common-cartrid
 export class CommonCartridgeFileParser {
 	private readonly manifestParser: CommonCartridgeManifestParser;
 
-	public constructor(file: Buffer, private readonly options = DEFAULT_FILE_PARSER_OPTIONS) {
-		const archive = new AdmZip(file);
+	private readonly archive: AdmZip;
 
-		this.manifestParser = new CommonCartridgeManifestParser(this.getManifestFileAsString(archive), this.options);
+	public constructor(file: Buffer, private readonly options = DEFAULT_FILE_PARSER_OPTIONS) {
+		this.archive = new AdmZip(file);
+
+		this.manifestParser = new CommonCartridgeManifestParser(this.getManifestFileAsString(), this.options);
 	}
 
 	public get manifest(): CommonCartridgeManifestParser {
 		return this.manifestParser;
 	}
 
-	private getManifestFileAsString(archive: AdmZip): string | never {
+	private getManifestFileAsString(): string | never {
 		// imsmanifest.xml is the standard name, but manifest.xml is also valid until v1.3
-		const manifest = archive.getEntry('imsmanifest.xml') || archive.getEntry('manifest.xml');
+		const manifest: AdmZip.IZipEntry | null =
+			this.archive.getEntry('imsmanifest.xml') || this.archive.getEntry('manifest.xml');
 
 		if (manifest) {
-			return archive.readAsText(manifest);
+			return this.archive.readAsText(manifest);
 		}
 
 		throw new CommonCartridgeManifestNotFoundException();
+	}
+
+	public getResourceAsString(filepath: string): string {
+
+		const resource: AdmZip.IZipEntry | null = this.archive.getEntry(filepath);
+
+		if (resource) {
+			return this.archive.readAsText(filepath);
+		}
+
+		throw new CommonCartridgeResourceNotFoundException();
 	}
 }
