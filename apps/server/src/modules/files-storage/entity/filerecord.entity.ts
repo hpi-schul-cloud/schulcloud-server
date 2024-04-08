23eb1abbb01029717d71f48f9c229b7adda046f7
WojciechGrancow <116577704+WojciechGrancow@users.noreply.github.com>
2024-04-08 10:10:58 +0200

    BC-7063 KNL Refactor - deletion threshold and database indexes (#4896)
    * add indexes and additional configuration to deletionModule
    
    * add tests

--- apps/server/src/modules/files-storage/entity/filerecord.entity.ts
+++ apps/server/src/modules/files-storage/entity/filerecord.entity.ts
@@ -133,6 +133,7 @@ export class FileRecord extends BaseEntityWithTimestamps {
 		return this._parentId.toHexString();
 	}
 
+	@Index()
 	@Property({ fieldName: 'creator', nullable: true })
 	_creatorId?: ObjectId;
 
