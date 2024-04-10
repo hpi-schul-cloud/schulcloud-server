892fc0773def84e6571015853f8093fbc891fbef
Patrick Sachmann <patrick.sachmann@capgemini.com>
2024-04-10 16:22:03 +0200

    EW-771 adding cc import utils

---
+++ apps/server/src/modules/common-cartridge/import/utils/common-cartridge-import-utils.spec.ts
@@ -0,0 +1,76 @@
+import AdmZip from 'adm-zip';
+import { CommonCartridgeImportUtils } from './common-cartridge-import-utils';
+
+describe('CommonCartridgeImportUtils', () => {
+	const setupArchive = (manifestName: string) => {
+		const archive = new AdmZip();
+
+		archive.addFile(manifestName, Buffer.from('<manifest></manifest>'));
+
+		return { archive };
+	};
+
+	describe('getManifestFileAsString', () => {
+		describe('when manifest file is named imsmanifest.xml', () => {
+			const setup = () => setupArchive('imsmanifest.xml');
+
+			it('should return manifest file as string', () => {
+				const { archive } = setup();
+
+				const manifest = CommonCartridgeImportUtils.getManifestFileAsString(archive);
+
+				expect(manifest).toBeDefined();
+			});
+		});
+
+		describe('when manifest file is named manifest.xml', () => {
+			const setup = () => setupArchive('manifest.xml');
+
+			it('should return manifest ', () => {
+				const { archive } = setup();
+
+				const manifest = CommonCartridgeImportUtils.getManifestFileAsString(archive);
+
+				expect(manifest).toBeDefined();
+			});
+		});
+
+		describe('when manifest file is not found', () => {
+			const setup = () => setupArchive('not-manifest.xml');
+
+			it('should return undefined', () => {
+				const { archive } = setup();
+
+				const manifest = CommonCartridgeImportUtils.getManifestFileAsString(archive);
+
+				expect(manifest).toBeUndefined();
+			});
+		});
+	});
+
+	describe('hasManifestFile', () => {
+		describe('when manifest file is found', () => {
+			const setup = () => setupArchive('imsmanifest.xml');
+
+			it('should return true', () => {
+				const { archive } = setup();
+
+				const hasManifest = CommonCartridgeImportUtils.hasManifestFile(archive);
+
+				expect(hasManifest).toEqual(true);
+			});
+		});
+
+		describe('when manifest file is not found', () => {
+			const setup = () => setupArchive('not-manifest.xml');
+
+			it('should return false', () => {
+				const { archive } = setup();
+
+				const hasManifest = CommonCartridgeImportUtils.hasManifestFile(archive);
+
+				expect(hasManifest).toEqual(false);
+			});
+		});
+	});
+});
---
+++ apps/server/src/modules/common-cartridge/import/utils/common-cartridge-import-utils.ts
@@ -0,0 +1,18 @@
+import AdmZip from 'adm-zip';
+
+export class CommonCartridgeImportUtils {
+	public static getManifestFileAsString(archive: AdmZip): string | undefined {
+		// imsmanifest.xml is the standard name, but manifest.xml is also valid until v1.3
+		const manifest = archive.getEntry('imsmanifest.xml') || archive.getEntry('manifest.xml');
+
+		if (manifest) {
+			return archive.readAsText(manifest);
+		}
+
+		return undefined;
+	}
+
+	public static hasManifestFile(archive: AdmZip): boolean {
+		return !!CommonCartridgeImportUtils.getManifestFileAsString(archive);
+	}
+}
