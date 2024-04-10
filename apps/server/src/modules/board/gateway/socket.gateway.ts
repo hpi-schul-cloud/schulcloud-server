343d81bb78bc4e9e792d1759d6cd58324f9885d6
hoeppner-dataport <marcus.hoeppner@dataport.de>
2024-04-10 10:50:07 +0200

    chore: remove commented out code

--- apps/server/src/modules/board/gateway/socket.gateway.ts
+++ apps/server/src/modules/board/gateway/socket.gateway.ts
@@ -1,36 +1,10 @@
-import {
-	WebSocketGateway,
-	// OnGatewayConnection,
-	// WebSocketServer,
-	SubscribeMessage,
-	// OnGatewayDisconnect,
-	// OnGatewayInit,
-} from '@nestjs/websockets';
-// import { Server, Socket } from 'socket.io';
+import { WebSocketGateway, SubscribeMessage } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 import { Logger } from '@nestjs/common';
 
 @WebSocketGateway({ cors: true })
-// export class SocketGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
 export class SocketGateway {
 	private readonly logger = new Logger(SocketGateway.name);
-
-	// @WebSocketServer()
-	// io: Server;
-
-	// afterInit() {
-	// 	this.logger.log('Initialized');
-	// }
-
-	// handleConnection(client: Socket) {
-	// 	const { sockets } = this.io.sockets;
-	// 	this.logger.log(`Client id: ${client.id} connected`);
-	// 	this.logger.debug(`Number of connected clients: ${sockets.size}`);
-	// }
-
-	// handleDisconnect(client: Socket) {
-	// 	this.logger.log(`Cliend id:${client.id} disconnected`);
-	// }
 
 	@SubscribeMessage('update-card')
 	handleUpdateCard(client: Socket, data: unknown) {
