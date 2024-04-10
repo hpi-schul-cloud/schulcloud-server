0d036e368028c1304c7ebff5df307955f0cbf0c9
hoeppner-dataport <marcus.hoeppner@dataport.de>
2024-04-10 13:21:16 +0200

    extend handlers

--- apps/server/src/modules/board/gateway/socket.gateway.ts
+++ apps/server/src/modules/board/gateway/socket.gateway.ts
@@ -6,24 +6,35 @@ import { Logger } from '@nestjs/common';
 export class SocketGateway {
 	private readonly logger = new Logger(SocketGateway.name);
 
-	@SubscribeMessage('update-card')
+	@SubscribeMessage('update-card-request')
 	handleUpdateCard(client: Socket, data: unknown) {
 		this.logger.log(`Message received from client id: ${client.id}`);
 		this.logger.debug(`Payload: ${JSON.stringify(data)}`);
-		client.broadcast.emit('update-card', data);
+		client.broadcast.emit('update-card-success', data);
+		client.emit('update-card-success', data);
 	}
 
-	@SubscribeMessage('lock-card')
+	@SubscribeMessage('lock-card-request')
 	handleLockCard(client: Socket, data: unknown) {
 		this.logger.log(`Message received from client id: ${client.id}`);
 		this.logger.debug(`Payload: ${JSON.stringify(data)}`);
-		client.broadcast.emit('lock-card', data);
+		client.broadcast.emit('lock-card-success', data);
+		client.emit('lock-card-success', data);
 	}
 
-	@SubscribeMessage('unlock-card')
+	@SubscribeMessage('unlock-card-request')
 	handleUnlockCard(client: Socket, data: unknown) {
 		this.logger.log(`Message received from client id: ${client.id}`);
 		this.logger.debug(`Payload: ${JSON.stringify(data)}`);
-		client.broadcast.emit('unlock-card', data);
+		client.broadcast.emit('unlock-card-success', data);
+		client.emit('unlock-card-success', data);
+	}
+
+	@SubscribeMessage('delete-card-request')
+	handleDeleteCard(client: Socket, data: unknown) {
+		this.logger.log(`Message received from client id: ${client.id}`);
+		this.logger.debug(`Payload: ${JSON.stringify(data)}`);
+		client.broadcast.emit('delete-card-success', data);
+		client.emit('delete-card-success', data);
 	}
 }
