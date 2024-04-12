ea3ba6d558dced68ddf2d92c401b104b8e1dab8d
Firas Shmit <firas.shmit@capgemini.com>
2024-04-12 10:37:22 +0200

    EW-798 used richtext Factory in tests

--- apps/server/src/modules/learnroom/service/common-cartridge-export.service.spec.ts
+++ apps/server/src/modules/learnroom/service/common-cartridge-export.service.spec.ts
@@ -15,10 +15,10 @@ import {
 	lessonFactory,
 	setupEntities,
 	taskFactory,
+	richTextElementFactory,
 } from '@shared/testing';
 import { ColumnBoardService } from '@src/modules/board';
 import AdmZip from 'adm-zip';
-import { ContentElementFactory, ContentElementType } from '@shared/domain/domainobject';
 import { CommonCartridgeMapper } from '../mapper/common-cartridge.mapper';
 
 describe('CommonCartridgeExportService', () => {
@@ -29,7 +29,6 @@ describe('CommonCartridgeExportService', () => {
 	let taskServiceMock: DeepMocked<TaskService>;
 	let configServiceMock: DeepMocked<ConfigService<LearnroomConfig, true>>;
 	let columnBoardServiceMock: DeepMocked<ColumnBoardService>;
-	const contentElementFactory = new ContentElementFactory();
 
 	const createXmlString = (nodeName: string, value: boolean | number | string): string =>
 		`<${nodeName}>${value.toString()}</${nodeName}>`;
@@ -73,8 +72,8 @@ describe('CommonCartridgeExportService', () => {
 		});
 		const [lesson] = lessons;
 		const taskFromLesson = taskFactory.buildWithId({ course, lesson });
-		const contentElement = contentElementFactory.build(ContentElementType.RICH_TEXT);
-		const card = cardFactory.build({ children: [contentElement] });
+		const textCardElement = richTextElementFactory.build();
+		const card = cardFactory.build({ children: [textCardElement] });
 		const column = columnFactory.build({ children: [card] });
 		const columnBoard = columnBoardFactory.build({ children: [column] });
 
@@ -95,7 +94,7 @@ describe('CommonCartridgeExportService', () => {
 		);
 		const archive = new AdmZip(buffer);
 
-		return { archive, course, lessons, tasks, taskFromLesson, columnBoard, column, card, contentElement };
+		return { archive, course, lessons, tasks, taskFromLesson, columnBoard, column, card, textCardElement };
 	};
 
 	beforeAll(async () => {
@@ -265,10 +264,10 @@ describe('CommonCartridgeExportService', () => {
 			});
 
 			it('should add content element of cards', async () => {
-				const { archive, contentElement } = await setup();
+				const { archive, textCardElement } = await setup();
 				const manifest = getFileContent(archive, 'imsmanifest.xml');
 
-				expect(manifest).toContain(`<resource identifier="i${contentElement.id}"`);
+				expect(manifest).toContain(`<resource identifier="i${textCardElement.id}"`);
 			});
 		});
 
