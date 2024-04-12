2f737b63492005b7b09b407c0b0aabba2f806d7a
Thomas Feldtkeller <thomas.feldtkeller@dataport.de>
2024-04-12 14:26:16 +0200

    add board module to board collab app

--- apps/server/src/modules/board/board-collaboration.module.ts
+++ apps/server/src/modules/board/board-collaboration.module.ts
@@ -1,12 +1,32 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { createConfigModuleOptions } from '@src/config';
+import { createConfigModuleOptions, DB_PASSWORD, DB_URL, DB_USERNAME } from '@src/config';
 import { CoreModule } from '@src/core';
 import { SocketGateway } from './gateway/socket.gateway';
 import { config } from './board-collaboration.config';
+import { ConsoleWriterModule } from '@src/infra/console';
+import { BoardModule } from './board.module';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { ALL_ENTITIES } from '@shared/domain/entity';
+import { defaultMikroOrmOptions } from '../server';
+import { RabbitMQWrapperModule } from '@src/infra/rabbitmq';
 
 @Module({
-	imports: [CoreModule, ConfigModule.forRoot(createConfigModuleOptions(config))],
+	imports: [
+		CoreModule,
+		RabbitMQWrapperModule,
+		ConfigModule.forRoot(createConfigModuleOptions(config)),
+		ConsoleWriterModule,
+		MikroOrmModule.forRoot({
+			...defaultMikroOrmOptions,
+			type: 'mongo',
+			clientUrl: DB_URL, // TODO: check if this needs to be different
+			password: DB_PASSWORD,
+			user: DB_USERNAME,
+			entities: ALL_ENTITIES,
+		}),
+		BoardModule,
+	],
 	providers: [SocketGateway],
 	exports: [],
 })
