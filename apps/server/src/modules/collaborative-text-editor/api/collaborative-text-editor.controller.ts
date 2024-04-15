667ce25285ee4c935f0221563baf5c9d0c334ca0
Max Bischof <maximilian.bischof@dataport.de>
2024-04-15 11:18:52 +0200

    Add url to response

--- apps/server/src/modules/collaborative-text-editor/api/collaborative-text-editor.controller.ts
+++ apps/server/src/modules/collaborative-text-editor/api/collaborative-text-editor.controller.ts
@@ -4,7 +4,9 @@ import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ApiValidationError } from '@shared/common';
 import { Response } from 'express';
 import { CollaborativeTextEditorUc } from './collaborative-text-editor.uc';
+import { CollaborativeTextEditorResponse } from './dto/collaborative-text-editor.response';
 import { GetCollaborativeTextEditorForParentParams } from './dto/get-collaborative-text-editor-for-parent.params';
+import { CollaborativeTextEditorMapper } from './mapper/collaborative-text-editor.mapper';
 
 @ApiTags('CollaborativeTextEditor')
 @Authenticate('jwt')
@@ -13,6 +15,7 @@ export class CollaborativeTextEditorController {
 	constructor(private readonly collaborativeTextEditorUc: CollaborativeTextEditorUc) {}
 
 	@ApiOperation({ summary: 'Redirect to CollaborativeTextEditor' })
+	@ApiResponse({ status: 200, type: CollaborativeTextEditorResponse })
 	@ApiResponse({ status: 400, type: ApiValidationError })
 	@ApiResponse({ status: 403, type: ForbiddenException })
 	@ApiResponse({ status: 404, type: NotFoundException })
@@ -20,15 +23,17 @@ export class CollaborativeTextEditorController {
 	async getCollaborativeTextEditorForParent(
 		@Param() getCollaborativeTextEditorForParentParams: GetCollaborativeTextEditorForParentParams,
 		@CurrentUser() currentUser: ICurrentUser,
-		@Res() res: Response
-	): Promise<void> {
-		const { sessions, url, sessionExpiryDate } =
-			await this.collaborativeTextEditorUc.getCollaborativeTextEditorForParent(
-				currentUser.userId,
-				getCollaborativeTextEditorForParentParams
-			);
+		@Res({ passthrough: true }) res: Response
+	): Promise<CollaborativeTextEditorResponse> {
+		const textEditor = await this.collaborativeTextEditorUc.getCollaborativeTextEditorForParent(
+			currentUser.userId,
+			getCollaborativeTextEditorForParentParams
+		);
 
-		res.cookie('sessionID', sessions.toString(), { expires: sessionExpiryDate });
-		res.redirect(url);
+		res.cookie('sessionID', textEditor.sessions.toString(), { expires: textEditor.sessionExpiryDate });
+
+		const dto = CollaborativeTextEditorMapper.mapCollaborativeTextEditorToResponse(textEditor);
+
+		return dto;
 	}
 }
