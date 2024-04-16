2ba863cede85bd1c6fdbf78644d59b159a2ab8d8
Tomasz Wiaderek <Tomasz.Wiaderek@gca.pass-consulting.com>
2024-04-16 16:51:07 +0200

    fix lint

--- apps/server/src/modules/alert/adapter/dto/component.dto.ts
+++ apps/server/src/modules/alert/adapter/dto/component.dto.ts
@@ -11,7 +11,7 @@ export class ComponentDto {
 		updated_at: Date,
 		deleted_at: Date,
 		enabled: boolean,
-		status_name: string,
+		status_name: string
 	) {
 		this.id = id;
 		this.name = name;
