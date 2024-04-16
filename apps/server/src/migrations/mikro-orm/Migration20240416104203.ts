49930c3dbef624a0ecb0a80af932a6bc1650129b
Igor Richter <igor.richter@capgemini.com>
2024-04-16 14:47:48 +0200

    add migration wizard permissions to admin role

---
+++ apps/server/src/migrations/mikro-orm/Migration20240416104203.ts
@@ -0,0 +1,42 @@
+import { Migration } from '@mikro-orm/migrations-mongodb';
+
+// add migration wizard permissions to admin role
+export class Migration20240416104203 extends Migration {
+	async up(): Promise<void> {
+		const adminRoleUpdate = await this.getCollection('roles').updateOne(
+			{ name: 'administrator' },
+			{
+				$addToSet: {
+					permissions: {
+						$each: ['IMPORT_USER_MIGRATE', 'IMPORT_USER_UPDATE', 'IMPORT_USER_VIEW'],
+					},
+				},
+			}
+		);
+
+		if (adminRoleUpdate.modifiedCount > 0) {
+			console.info(
+				'Permissions IMPORT_USER_MIGRATE, IMPORT_USER_UPDATE, IMPORT_USER_VIEW were added to role administrator.'
+			);
+		}
+	}
+
+	async down(): Promise<void> {
+		const adminRoleUpdate = await this.getCollection('roles').updateOne(
+			{ name: 'administrator' },
+			{
+				$pull: {
+					permissions: {
+						$in: ['IMPORT_USER_MIGRATE', 'IMPORT_USER_UPDATE', 'IMPORT_USER_VIEW'],
+					},
+				},
+			}
+		);
+
+		if (adminRoleUpdate.modifiedCount > 0) {
+			console.info(
+				'Rollback: Removed permissions IMPORT_USER_MIGRATE, IMPORT_USER_UPDATE, IMPORT_USER_VIEW from role administrator.'
+			);
+		}
+	}
+}
