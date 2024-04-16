ce82deaee87ef586784792449f0f44e77f5c583d
Max Bischof <maximilian.bischof@dataport.de>
2024-04-16 10:47:08 +0200

    Add more unit tests

---
+++ apps/server/src/shared/domain/domainobject/board/collaborative-text-editor-element.do.spec.ts
@@ -0,0 +1,62 @@
+import { createMock } from '@golevelup/ts-jest';
+import { collaborativeTextEditorElementFactory } from '@shared/testing';
+import {
+	CollaborativeTextEditorElement,
+	isCollaborativeTextEditorElement,
+} from './collaborative-text-editor-element.do';
+import { BoardCompositeVisitor, BoardCompositeVisitorAsync } from './types';
+
+describe(CollaborativeTextEditorElement.name, () => {
+	describe('isAllowedAsChild', () => {
+		it('should return false', () => {
+			const collaborativeTextEditorElement = collaborativeTextEditorElementFactory.build();
+
+			expect(collaborativeTextEditorElement.isAllowedAsChild()).toBe(false);
+		});
+	});
+
+	describe('when trying to add a child to a CollaborativeTextEditorElement', () => {
+		it('should throw an error ', () => {
+			const collaborativeTextEditorElement = collaborativeTextEditorElementFactory.build();
+			const collaborativeTextEditorElementChild = collaborativeTextEditorElementFactory.build();
+
+			expect(() => collaborativeTextEditorElement.addChild(collaborativeTextEditorElementChild)).toThrow();
+		});
+	});
+
+	describe('accept', () => {
+		it('should call the right visitor method', () => {
+			const visitor = createMock<BoardCompositeVisitor>();
+			const collaborativeTextEditorElement = collaborativeTextEditorElementFactory.build();
+
+			collaborativeTextEditorElement.accept(visitor);
+
+			expect(visitor.visitCollaborativeTextEditorElement).toHaveBeenCalledWith(collaborativeTextEditorElement);
+		});
+	});
+
+	describe('acceptAsync', () => {
+		it('should call the right async visitor method', async () => {
+			const visitor = createMock<BoardCompositeVisitorAsync>();
+			const collaborativeTextEditorElement = collaborativeTextEditorElementFactory.build();
+
+			await collaborativeTextEditorElement.acceptAsync(visitor);
+
+			expect(visitor.visitCollaborativeTextEditorElementAsync).toHaveBeenCalledWith(collaborativeTextEditorElement);
+		});
+	});
+
+	describe('isCollaborativeTextEditorElement', () => {
+		it('should return true', () => {
+			const collaborativeTextEditorElement = collaborativeTextEditorElementFactory.build();
+
+			expect(isCollaborativeTextEditorElement(collaborativeTextEditorElement)).toBe(true);
+		});
+
+		it('should return false', () => {
+			const notCollaborativeTextEditorElement = {};
+
+			expect(isCollaborativeTextEditorElement(notCollaborativeTextEditorElement)).toBe(false);
+		});
+	});
+});
