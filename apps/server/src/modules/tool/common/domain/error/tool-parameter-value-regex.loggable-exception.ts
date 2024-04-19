b38a0ed867c8b68a1a950476c46f928e489ed85b
Malte Berg <malte.berg@capgemini.com>
2024-04-19 14:03:52 +0200

    adjust ToolParameterValueRegexLoggableException

--- apps/server/src/modules/tool/common/domain/error/tool-parameter-value-regex.loggable-exception.ts
+++ apps/server/src/modules/tool/common/domain/error/tool-parameter-value-regex.loggable-exception.ts
@@ -1,18 +1,27 @@
-import { ValidationError } from '@shared/common';
+import { BusinessError } from '@shared/common';
 import { ErrorLogMessage, Loggable, LogMessage, ValidationErrorLogMessage } from '@src/core/logger';
+import { HttpStatus } from '@nestjs/common';
 import { CustomParameter } from '../custom-parameter.do';
 
-export class ToolParameterValueRegexLoggableException extends ValidationError implements Loggable {
+export class ToolParameterValueRegexLoggableException extends BusinessError implements Loggable {
 	constructor(private readonly parameterDeclaration: CustomParameter) {
 		super(
-			`tool_param_value_regex: The given entry for the parameter with name ${parameterDeclaration.name} does not fit the regex.`
+			{
+				type: 'TOOL_PARAMETER_VALUE_REGEX',
+				title: 'Tool parameter value is no regex',
+				defaultMessage: 'The parameter value does not fit the regex.',
+			},
+			HttpStatus.BAD_REQUEST,
+			{
+				parameterDeclaration,
+			}
 		);
 	}
 
 	getLogMessage(): LogMessage | ErrorLogMessage | ValidationErrorLogMessage {
 		return {
-			type: 'TOOL_PARAMETER_VALUE_REGEX',
-			message: 'The parameter value does not fit the regex.',
+			type: this.type,
+			message: this.message,
 			stack: this.stack,
 			data: {
 				parameterName: this.parameterDeclaration.name,
