6ab2fdd7e0e0c4c53d1566fc877d28c0b2afb38e
Malte Berg <malte.berg@capgemini.com>
2024-04-19 13:44:34 +0200

    adjust ToolParameterDuplicateLoggableException

--- apps/server/src/modules/tool/common/domain/error/tool-parameter-duplicate.loggable-exception.ts
+++ apps/server/src/modules/tool/common/domain/error/tool-parameter-duplicate.loggable-exception.ts
@@ -1,15 +1,26 @@
-import { ValidationError } from '@shared/common';
+import { BusinessError } from '@shared/common';
 import { ErrorLogMessage, Loggable, LogMessage, ValidationErrorLogMessage } from '@src/core/logger';
+import { HttpStatus } from '@nestjs/common';
 
-export class ToolParameterDuplicateLoggableException extends ValidationError implements Loggable {
+export class ToolParameterDuplicateLoggableException extends BusinessError implements Loggable {
 	constructor(private readonly parameterName: string) {
-		super(`tool_param_duplicate: The parameter with name ${parameterName} is defined multiple times.`);
+		super(
+			{
+				type: 'TOOL_PARAMETER_DUPLICATE',
+				title: 'Duplicate tool parameter',
+				defaultMessage: 'The parameter is defined multiple times.',
+			},
+			HttpStatus.BAD_REQUEST,
+			{
+				parameterName,
+			}
+		);
 	}
 
 	getLogMessage(): LogMessage | ErrorLogMessage | ValidationErrorLogMessage {
 		return {
-			type: 'TOOL_PARAMETER_DUPLICATE',
-			message: 'The parameter is defined multiple times.',
+			type: this.type,
+			message: this.message,
 			stack: this.stack,
 			data: {
 				parameterName: this.parameterName,
