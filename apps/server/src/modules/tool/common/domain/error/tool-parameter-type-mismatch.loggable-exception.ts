dd4f83e5eb0d4cf07178bb402af1eace40203065
Malte Berg <malte.berg@capgemini.com>
2024-04-19 13:54:49 +0200

    adjust ToolParameterTypeMismatchLoggableException

--- apps/server/src/modules/tool/common/domain/error/tool-parameter-type-mismatch.loggable-exception.ts
+++ apps/server/src/modules/tool/common/domain/error/tool-parameter-type-mismatch.loggable-exception.ts
@@ -1,18 +1,27 @@
-import { ValidationError } from '@shared/common';
+import { BusinessError } from '@shared/common';
 import { ErrorLogMessage, Loggable, LogMessage, ValidationErrorLogMessage } from '@src/core/logger';
+import { HttpStatus } from '@nestjs/common';
 import { CustomParameter } from '../custom-parameter.do';
 
-export class ToolParameterTypeMismatchLoggableException extends ValidationError implements Loggable {
+export class ToolParameterTypeMismatchLoggableException extends BusinessError implements Loggable {
 	constructor(private readonly parameterDeclaration: CustomParameter) {
 		super(
-			`tool_param_type_mismatch: The value of parameter with name ${parameterDeclaration.name} should be of type ${parameterDeclaration.type}.`
+			{
+				type: 'TOOL_PARAMETER_TYPE_MISMATCH',
+				title: 'Parameter type mismatch',
+				defaultMessage: 'The parameter value has the wrong type.',
+			},
+			HttpStatus.BAD_REQUEST,
+			{
+				parameterDeclaration,
+			}
 		);
 	}
 
 	getLogMessage(): LogMessage | ErrorLogMessage | ValidationErrorLogMessage {
 		return {
-			type: 'TOOL_PARAMETER_TYPE_MISMATCH',
-			message: 'The parameter value has the wrong type.',
+			type: this.type,
+			message: this.message,
 			stack: this.stack,
 			data: {
 				parameterName: this.parameterDeclaration.name,
