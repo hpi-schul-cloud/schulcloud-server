215e84782270c32abfa706b2e80d2f0b867d2abe
Malte Berg <malte.berg@capgemini.com>
2024-04-19 14:00:12 +0200

    adjust ToolParameterValueMissingLoggableException

--- apps/server/src/modules/tool/common/domain/error/tool-parameter-value-missing.loggable-exception.ts
+++ apps/server/src/modules/tool/common/domain/error/tool-parameter-value-missing.loggable-exception.ts
@@ -1,16 +1,27 @@
-import { ValidationError } from '@shared/common';
+import { BusinessError } from '@shared/common';
 import { ErrorLogMessage, Loggable, LogMessage, ValidationErrorLogMessage } from '@src/core/logger';
+import { HttpStatus } from '@nestjs/common';
 import { CustomParameter } from '../custom-parameter.do';
 
-export class ToolParameterValueMissingLoggableException extends ValidationError implements Loggable {
+export class ToolParameterValueMissingLoggableException extends BusinessError implements Loggable {
 	constructor(private readonly parameterDeclaration: CustomParameter) {
-		super(`tool_param_value_missing: The parameter with name ${parameterDeclaration.name} has no value`);
+		super(
+			{
+				type: 'TOOL_PARAMETER_VALUE_MISSING',
+				title: 'Missing tool parameter value',
+				defaultMessage: 'The parameter has no value.',
+			},
+			HttpStatus.BAD_REQUEST,
+			{
+				parameterDeclaration,
+			}
+		);
 	}
 
 	getLogMessage(): LogMessage | ErrorLogMessage | ValidationErrorLogMessage {
 		return {
-			type: 'TOOL_PARAMETER_VALUE_MISSING',
-			message: 'The parameter has no value.',
+			type: this.type,
+			message: this.message,
 			stack: this.stack,
 			data: {
 				parameterName: this.parameterDeclaration.name,
