9be19ca877a4f3dd5385aa7a5ba7043930d74578
Malte Berg <malte.berg@capgemini.com>
2024-04-19 13:56:54 +0200

    adjust ToolParameterUnknownLoggableException

--- apps/server/src/modules/tool/common/domain/error/tool-parameter-unknown.loggable-exception.ts
+++ apps/server/src/modules/tool/common/domain/error/tool-parameter-unknown.loggable-exception.ts
@@ -1,16 +1,27 @@
-import { ValidationError } from '@shared/common';
+import { BusinessError } from '@shared/common';
 import { ErrorLogMessage, Loggable, LogMessage, ValidationErrorLogMessage } from '@src/core/logger';
+import { HttpStatus } from '@nestjs/common';
 import { CustomParameterEntry } from '../custom-parameter-entry.do';
 
-export class ToolParameterUnknownLoggableException extends ValidationError implements Loggable {
+export class ToolParameterUnknownLoggableException extends BusinessError implements Loggable {
 	constructor(private readonly parameterEntry: CustomParameterEntry) {
-		super(`tool_param_unknown: The parameter with name ${parameterEntry.name} is not part of this tool.`);
+		super(
+			{
+				type: 'TOOL_PARAMETER_UNKNOWN',
+				title: 'Tool parameter unknown',
+				defaultMessage: 'The parameter is not part of this tool.',
+			},
+			HttpStatus.BAD_REQUEST,
+			{
+				parameterEntry,
+			}
+		);
 	}
 
 	getLogMessage(): LogMessage | ErrorLogMessage | ValidationErrorLogMessage {
 		return {
-			type: 'TOOL_PARAMETER_UNKNOWN',
-			message: 'The parameter is not part of this tool.',
+			type: this.type,
+			message: this.message,
 			stack: this.stack,
 			data: {
 				parameterName: this.parameterEntry.name,
