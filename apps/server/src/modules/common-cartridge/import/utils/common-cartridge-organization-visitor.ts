0a509dd90e22bef12a690f74c9bd59e93a311415
Patrick Sachmann <20001160+psachmann@users.noreply.github.com>
2024-04-19 08:01:19 +0200

    EW-771 Common Cartridge 1.1.0 Web Link Import (#4915)
    * Parsing web link from xml resource file
    * Importing links from Common Cartridge file that are not inlined
    * Removing AdmZip and JSDOM from common-cartridge-file-validation.pipe.ts
    
    ---------
    
    Co-authored-by: Alexander Weber <103171324+alweber-cap@users.noreply.github.com>

--- apps/server/src/modules/common-cartridge/import/utils/common-cartridge-organization-visitor.ts
+++ apps/server/src/modules/common-cartridge/import/utils/common-cartridge-organization-visitor.ts
@@ -1,5 +1,4 @@
-import { randomUUID } from 'crypto';
-import { CommonCartridgeFileParserOptions, OrganizationProps } from '../common-cartridge-import.types';
+import { CommonCartridgeFileParserOptions, CommonCartridgeOrganizationProps } from '../common-cartridge-import.types';
 
 type SearchElement = { depth: number; path: string; element: Element };
 
@@ -22,8 +21,10 @@ type SearchElement = { depth: number; path: string; element: Element };
 export class CommonCartridgeOrganizationVisitor {
 	constructor(private readonly document: Document, private readonly options: CommonCartridgeFileParserOptions) {}
 
-	public findAllOrganizations(): OrganizationProps[] {
-		return this.search().map((element) => this.createOrganizationProps(element.element, element.path, element.depth));
+	public findAllOrganizations(): CommonCartridgeOrganizationProps[] {
+		const organizations = this.search().map((element) => this.createOrganizationProps(element));
+
+		return organizations;
 	}
 
 	private search(): SearchElement[] {
@@ -73,14 +74,36 @@ export class CommonCartridgeOrganizationVisitor {
 		});
 	}
 
+	private createOrganizationProps(element: SearchElement): CommonCartridgeOrganizationProps {
+		const title = this.getElementTitle(element.element);
+		const identifier = this.getElementIdentifier(element.element);
+		const identifierRef = this.getElementIdentifierRef(element.element);
+		const isResource = identifierRef !== '';
+		const resourcePath = isResource ? this.getResourcePath(identifierRef) : '';
+		const resourceType = isResource ? this.getResourceType(identifierRef) : '';
+		const isInlined = isResource && !resourcePath;
+
+		return {
+			path: element.path,
+			pathDepth: element.depth,
+			identifier,
+			identifierRef,
+			title,
+			isResource,
+			isInlined,
+			resourcePath,
+			resourceType,
+		};
+	}
+
 	private getElementIdentifier(element: Element): string {
-		const identifier = element.getAttribute('identifier') ?? `i${randomUUID()}`;
+		const identifier = element.getAttribute('identifier') || '';
 
 		return identifier;
 	}
 
-	private getElementIdentifierRef(element: Element): string | undefined {
-		const identifierRef = element.getAttribute('identifierref') ?? undefined;
+	private getElementIdentifierRef(element: Element): string {
+		const identifierRef = element.getAttribute('identifierref') || '';
 
 		return identifierRef;
 	}
@@ -91,17 +114,21 @@ export class CommonCartridgeOrganizationVisitor {
 		return title;
 	}
 
-	private createOrganizationProps(element: Element, path: string, pathDepth: number): OrganizationProps {
-		const title = this.getElementTitle(element);
-		const identifier = this.getElementIdentifier(element);
-		const identifierRef = this.getElementIdentifierRef(element);
+	private getResourcePath(identifierRef: string): string {
+		const path =
+			this.document
+				.querySelector(`manifest > resources > resource[identifier="${identifierRef}"] > file`)
+				?.getAttribute('href') || '';
 
-		return {
-			path,
-			pathDepth,
-			identifier,
-			identifierRef,
-			title,
-		};
+		return path;
+	}
+
+	private getResourceType(identifierRef: string): string {
+		const type =
+			this.document
+				.querySelector(`manifest > resources > resource[identifier="${identifierRef}"]`)
+				?.getAttribute('type') || '';
+
+		return type;
 	}
 }
