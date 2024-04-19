d9e9003a7afe23aa575990b19081efc0c65ee1f5
Constantin Bergatt <constantin.bergatt@dataport.de>
2024-04-19 09:24:51 +0200

    BC-6781 - remove forwardRef from API module

--- apps/server/src/modules/collaborative-text-editor/collaborative-text-editor-api.module.ts
+++ apps/server/src/modules/collaborative-text-editor/collaborative-text-editor-api.module.ts
@@ -1,12 +1,12 @@
 import { AuthorizationModule } from '@modules/authorization';
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { BoardModule } from '../board';
 import { CollaborativeTextEditorController } from './api/collaborative-text-editor.controller';
 import { CollaborativeTextEditorUc } from './api/collaborative-text-editor.uc';
 import { CollaborativeTextEditorModule } from './collaborative-text-editor.module';
 
 @Module({
-	imports: [CollaborativeTextEditorModule, forwardRef(() => AuthorizationModule), BoardModule],
+	imports: [CollaborativeTextEditorModule, AuthorizationModule, BoardModule],
 	controllers: [CollaborativeTextEditorController],
 	providers: [CollaborativeTextEditorUc],
 })
