d1623547849f3f6b4289326987a6ecd1c2cd3c2e
Malte Berg <malte.berg@capgemini.com>
2024-04-22 13:10:02 +0200

    add toolId to ToolParameterUnknownLoggableException

--- apps/server/src/modules/tool/common/domain/error/tool-parameter-unknown.loggable-exception.ts
+++ apps/server/src/modules/tool/common/domain/error/tool-parameter-unknown.loggable-exception.ts
@@ -1,10 +1,11 @@
 import { BusinessError } from '@shared/common';
 import { ErrorLogMessage, Loggable, LogMessage, ValidationErrorLogMessage } from '@src/core/logger';
 import { HttpStatus } from '@nestjs/common';
+import { EntityId } from '@shared/domain/types';
 import { CustomParameterEntry } from '../custom-parameter-entry.do';
 
 export class ToolParameterUnknownLoggableException extends BusinessError implements Loggable {
-	constructor(private readonly parameterEntry: CustomParameterEntry) {
+	constructor(private readonly toolId: EntityId | undefined, private readonly parameterEntry: CustomParameterEntry) {
 		super(
 			{
 				type: 'TOOL_PARAMETER_UNKNOWN',
@@ -13,6 +14,7 @@ export class ToolParameterUnknownLoggableException extends BusinessError impleme
 			},
 			HttpStatus.BAD_REQUEST,
 			{
+				toolId,
 				parameterEntry,
 			}
 		);
@@ -24,6 +26,7 @@ export class ToolParameterUnknownLoggableException extends BusinessError impleme
 			message: this.message,
 			stack: this.stack,
 			data: {
+				toolId: this.toolId,
 				parameterName: this.parameterEntry.name,
 			},
 		};
