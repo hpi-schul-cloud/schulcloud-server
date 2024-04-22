e3752fd4fae7f10486f829fdd04386fb2d99452b
Igor Richter <igor.richter@capgemini.com>
2024-04-22 13:03:53 +0200

    add instance specific rollout

--- apps/server/src/migrations/mikro-orm/Migration20240416104203.ts
+++ apps/server/src/migrations/mikro-orm/Migration20240416104203.ts
@@ -3,42 +3,48 @@ import { Migration } from '@mikro-orm/migrations-mongodb';
 // add migration wizard permissions to admin role
 export class Migration20240416104203 extends Migration {
 	async up(): Promise<void> {
-		const adminRoleUpdate = await this.driver.nativeUpdate(
-			'roles',
-			{ name: 'administrator' },
-			{
-				$addToSet: {
-					permissions: {
-						$each: ['IMPORT_USER_MIGRATE', 'IMPORT_USER_UPDATE', 'IMPORT_USER_VIEW'],
+		// eslint-disable-next-line no-process-env
+		if (process.env.SC_THEME !== 'thr') {
+			const adminRoleUpdate = await this.driver.nativeUpdate(
+				'roles',
+				{ name: 'administrator' },
+				{
+					$addToSet: {
+						permissions: {
+							$each: ['IMPORT_USER_MIGRATE', 'IMPORT_USER_UPDATE', 'IMPORT_USER_VIEW'],
+						},
 					},
-				},
-			}
-		);
-
-		if (adminRoleUpdate.affectedRows > 0) {
-			console.info(
-				'Permissions IMPORT_USER_MIGRATE, IMPORT_USER_UPDATE, IMPORT_USER_VIEW were added to role administrator.'
+				}
 			);
+
+			if (adminRoleUpdate.affectedRows > 0) {
+				console.info(
+					'Permissions IMPORT_USER_MIGRATE, IMPORT_USER_UPDATE, IMPORT_USER_VIEW were added to role administrator.'
+				);
+			}
 		}
 	}
 
 	async down(): Promise<void> {
-		const adminRoleUpdate = await this.driver.nativeUpdate(
-			'roles',
-			{ name: 'administrator' },
-			{
-				$pull: {
-					permissions: {
-						$in: ['IMPORT_USER_MIGRATE', 'IMPORT_USER_UPDATE', 'IMPORT_USER_VIEW'],
+		// eslint-disable-next-line no-process-env
+		if (process.env.SC_THEME !== 'thr') {
+			const adminRoleUpdate = await this.driver.nativeUpdate(
+				'roles',
+				{ name: 'administrator' },
+				{
+					$pull: {
+						permissions: {
+							$in: ['IMPORT_USER_MIGRATE', 'IMPORT_USER_UPDATE', 'IMPORT_USER_VIEW'],
+						},
 					},
-				},
-			}
-		);
-
-		if (adminRoleUpdate.affectedRows > 0) {
-			console.info(
-				'Rollback: Removed permissions IMPORT_USER_MIGRATE, IMPORT_USER_UPDATE, IMPORT_USER_VIEW from role administrator.'
+				}
 			);
+
+			if (adminRoleUpdate.affectedRows > 0) {
+				console.info(
+					'Rollback: Removed permissions IMPORT_USER_MIGRATE, IMPORT_USER_UPDATE, IMPORT_USER_VIEW from role administrator.'
+				);
+			}
 		}
 	}
 }
