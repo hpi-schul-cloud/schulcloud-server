7047c01968d9d12038f9a145c5688e9fdb81a7e5
Sergej Hoffmann <97111299+SevenWaysDP@users.noreply.github.com>
2024-04-26 10:55:14 +0200

    BC-5661 - refactor RabbitMQ configuration (#4963)

--- apps/server/src/infra/rabbitmq/exchange/files-preview.ts
+++ apps/server/src/infra/rabbitmq/exchange/files-preview.ts
@@ -1,7 +1,3 @@
-import { Configuration } from '@hpi-schul-cloud/commons/lib';
-
-export const FilesPreviewExchange = Configuration.get('FILES_STORAGE__EXCHANGE') as string;
-
 export enum FilesPreviewEvents {
 	'GENERATE_PREVIEW' = 'generate-preview',
 }
