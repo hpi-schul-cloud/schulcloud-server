43fdc242484d9b6db10cfaeff24262638d0ed745
Arne Gnisa <arne.gnisa@capgemini.com>
2024-04-26 11:11:29 +0200

    N21-1880 removoes unused classes and imports

--- apps/server/src/infra/schulconnex-client/loggable/index.ts
+++ apps/server/src/infra/schulconnex-client/loggable/index.ts
@@ -1,2 +1,1 @@
 export { SchulconnexConfigurationMissingLoggable } from './schulconnex-configuration-missing.loggable';
-export { TokenRequestLoggableException } from './token-request-loggable-exception';
