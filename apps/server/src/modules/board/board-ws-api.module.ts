14d9dad7189977711b119fea5868a3fef2c53fd3
hoeppner-dataport <marcus.hoeppner@dataport.de>
2024-05-07 12:17:54 +0200

    chore: add cardUc to module

--- apps/server/src/modules/board/board-ws-api.module.ts
+++ apps/server/src/modules/board/board-ws-api.module.ts
@@ -4,11 +4,11 @@ import { LoggerModule } from '@src/core/logger';
 import { AuthorizationModule } from '../authorization';
 import { BoardModule } from './board.module';
 import { SocketGateway } from './gateway/socket.gateway';
-import { BoardUc, ColumnUc } from './uc';
+import { BoardUc, CardUc, ColumnUc } from './uc';
 
 @Module({
 	imports: [BoardModule, forwardRef(() => AuthorizationModule), LoggerModule],
-	providers: [SocketGateway, ColumnUc, BoardUc, CourseRepo],
+	providers: [SocketGateway, CardUc, ColumnUc, BoardUc, CourseRepo],
 	exports: [],
 })
 export class BoardWsApiModule {}
