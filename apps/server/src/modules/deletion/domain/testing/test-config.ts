d4c09d90617ffa6fd3065837ad58856a4730f37e
WojciechGrancow <Wojciech.Grancow@gca.pass-consulting.com>
2024-05-07 12:44:42 +0200

    fix in test in deletionrequest service

--- apps/server/src/modules/deletion/domain/testing/test-config.ts
+++ apps/server/src/modules/deletion/domain/testing/test-config.ts
@@ -1,7 +1,7 @@
 import { Configuration } from '@hpi-schul-cloud/commons';
 
 const deletionConfig = {
-	MODIFICATION_THRESHOLD_MS: Configuration.get('ADMIN_API__MODIFICATION_THRESHOLD_MS') as number,
+	ADMIN_API__MODIFICATION_THRESHOLD_MS: Configuration.get('ADMIN_API__MODIFICATION_THRESHOLD_MS') as number,
 	MAX_AMOUNT_OF_DELETIONREQUESTS_DO_CONCURENTLY: Configuration.get(
 		'ADMIN_API__MAX_AMOUNT_OF_DELETIONREQUESTS_DO_CONCURENTLY'
 	) as number,
@@ -12,7 +12,7 @@ const config = () => deletionConfig;
 
 export const deletionTestConfig = () => {
 	const conf = config();
-	conf.MODIFICATION_THRESHOLD_MS = 1000;
+	conf.ADMIN_API__MODIFICATION_THRESHOLD_MS = 1000;
 	conf.MAX_AMOUNT_OF_DELETIONREQUESTS_DO_CONCURENTLY = 2;
 	conf.DELETION_DELAY_MILLISECONDS = 100;
 	return conf;
