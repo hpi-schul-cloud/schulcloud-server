c3f494ace0346cca14ca6ef66626d2a83d65db47
Phillip Wirth <phillip.wirth@dataport.de>
2024-05-10 12:36:28 +0200

    improve exception logging

--- apps/server/src/infra/etherpad-client/loggable/etherpad-error-loggable-exception.ts
+++ apps/server/src/infra/etherpad-client/loggable/etherpad-error-loggable-exception.ts
@@ -20,6 +20,7 @@ export class EtherpadErrorLoggableException extends InternalServerErrorException
 			data: {
 				userId,
 				parentId,
+				etherpadMessage: this.exceptionOptions.description,
 			},
 		};
 
