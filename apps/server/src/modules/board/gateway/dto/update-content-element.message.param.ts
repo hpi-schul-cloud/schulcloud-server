4c67cc36c9d22165b176ae2b15993e34dc0b71e0
hoeppner-dataport <marcus.hoeppner@dataport.de>
2024-05-17 16:35:58 +0200

    chore: reduce code duplication

--- apps/server/src/modules/board/gateway/dto/update-content-element.message.param.ts
+++ apps/server/src/modules/board/gateway/dto/update-content-element.message.param.ts
@@ -1,52 +1,7 @@
-import { ApiProperty, getSchemaPath } from '@nestjs/swagger';
-import { ContentElementType } from '@shared/domain/domainobject';
-import { Type } from 'class-transformer';
-import { IsMongoId, ValidateNested } from 'class-validator';
-import {
-	DrawingElementContentBody,
-	ElementContentBody,
-	ExternalToolElementContentBody,
-	FileElementContentBody,
-	LinkElementContentBody,
-	RichTextElementContentBody,
-	SubmissionContainerElementContentBody,
-} from '../../controller/dto';
+import { IsMongoId } from 'class-validator';
+import { UpdateElementContentBodyParams } from '../../controller/dto';
 
-export class UpdateContentElementMessageParams {
+export class UpdateContentElementMessageParams extends UpdateElementContentBodyParams {
 	@IsMongoId()
 	elementId!: string;
-
-	@ValidateNested()
-	@Type(() => ElementContentBody, {
-		discriminator: {
-			property: 'type',
-			subTypes: [
-				{ value: FileElementContentBody, name: ContentElementType.FILE },
-				{ value: LinkElementContentBody, name: ContentElementType.LINK },
-				{ value: RichTextElementContentBody, name: ContentElementType.RICH_TEXT },
-				{ value: SubmissionContainerElementContentBody, name: ContentElementType.SUBMISSION_CONTAINER },
-				{ value: ExternalToolElementContentBody, name: ContentElementType.EXTERNAL_TOOL },
-				{ value: ExternalToolElementContentBody, name: ContentElementType.DRAWING },
-				{ value: DrawingElementContentBody, name: ContentElementType.DRAWING },
-			],
-		},
-		keepDiscriminatorProperty: true,
-	})
-	@ApiProperty({
-		oneOf: [
-			{ $ref: getSchemaPath(FileElementContentBody) },
-			{ $ref: getSchemaPath(LinkElementContentBody) },
-			{ $ref: getSchemaPath(RichTextElementContentBody) },
-			{ $ref: getSchemaPath(SubmissionContainerElementContentBody) },
-			{ $ref: getSchemaPath(ExternalToolElementContentBody) },
-			{ $ref: getSchemaPath(DrawingElementContentBody) },
-		],
-	})
-	data!:
-		| FileElementContentBody
-		| LinkElementContentBody
-		| RichTextElementContentBody
-		| SubmissionContainerElementContentBody
-		| ExternalToolElementContentBody
-		| DrawingElementContentBody;
 }
