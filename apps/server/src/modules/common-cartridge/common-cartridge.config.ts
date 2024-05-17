db7ea91f5326a473e0c7d0b26e4ef86d938987ad
Firas Shmit <firas.shmit@capgemini.com>
2024-05-17 12:25:17 +0200

    EW-834 added a test for config of common cartridge

--- apps/server/src/modules/common-cartridge/common-cartridge.config.ts
+++ apps/server/src/modules/common-cartridge/common-cartridge.config.ts
@@ -1,7 +1,13 @@
 import { Configuration } from '@hpi-schul-cloud/commons';
 
-const commonCartridgeConfig = {
-	NEST_LOG_LEVE: Configuration.get('NEST_LOG_LEVEL') as string,
+export interface CommonCartridgeConfig {
+	NEST_LOG_LEVEL: string;
+}
+
+const commonCartridgeConfig: CommonCartridgeConfig = {
+	NEST_LOG_LEVEL: Configuration.get('NEST_LOG_LEVEL') as string,
 };
 
-export const config = () => commonCartridgeConfig;
+export function config(): CommonCartridgeConfig {
+	return commonCartridgeConfig;
+}
