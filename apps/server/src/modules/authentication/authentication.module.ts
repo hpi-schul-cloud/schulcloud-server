ef1f6dd7c47aeed9827cec85571a75a0c0b7c222
Maximilian Kreuzkam <maximilian.kreuzkam@capgemini.com>
2024-05-17 13:20:11 +0200

    revert moved algorithms check

--- apps/server/src/modules/authentication/authentication.module.ts
+++ apps/server/src/modules/authentication/authentication.module.ts
@@ -21,6 +21,26 @@ import { Oauth2Strategy } from './strategy/oauth2.strategy';
 import { XApiKeyStrategy } from './strategy/x-api-key.strategy';
 import { WsJwtStrategy } from './strategy/ws-jwt.strategy';
 
+// values copied from Algorithm definition. Type does not exist at runtime and can't be checked anymore otherwise
+const algorithms = [
+	'HS256',
+	'HS384',
+	'HS512',
+	'RS256',
+	'RS384',
+	'RS512',
+	'ES256',
+	'ES384',
+	'ES512',
+	'PS256',
+	'PS384',
+	'PS512',
+	'none',
+];
+
+if (!algorithms.includes(jwtConstants.jwtOptions.algorithm)) {
+	throw new Error(`${jwtConstants.jwtOptions.algorithm} is not a valid JWT signing algorithm`);
+}
 const signAlgorithm = jwtConstants.jwtOptions.algorithm as Algorithm;
 
 const signOptions: SignOptions = {
