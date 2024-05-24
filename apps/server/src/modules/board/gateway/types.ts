6b406c2a75bdf6d1257ec58f25019b77bdf1c229
Murat Merdoglu <murat.merdoglu@dataport.de>
2024-05-24 10:14:03 +0200

    move enums into types file

--- apps/server/src/modules/board/gateway/types.ts
+++ apps/server/src/modules/board/gateway/types.ts
@@ -2,3 +2,17 @@ import { ICurrentUser } from '@src/modules/authentication';
 import { Socket as IoSocket } from 'socket.io';
 
 export type Socket = IoSocket & { handshake: { user?: ICurrentUser } };
+
+export enum ErrorTypes {
+	NOT_CREATED = 'notCreated',
+	NOT_LOADED = 'notLoaded',
+	NOT_UPDATED = 'notUpdated',
+	NOT_DELETED = 'notDeleted',
+}
+
+export enum BoardObjectTypes {
+	BOARD = 'board',
+	BOARD_COLUMN = 'boardColumn',
+	BOARD_CARD = 'boardCard',
+	BOARD_ELEMENT = 'boardElement',
+}
