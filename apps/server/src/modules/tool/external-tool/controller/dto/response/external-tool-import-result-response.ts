9eca22f2e882125c968524d2ae604a18398c753f
Marvin Öhlerking <marvin.oehlerking@capgemini.com>
2024-05-24 09:49:48 +0200

    api description

--- apps/server/src/modules/tool/external-tool/controller/dto/response/external-tool-import-result-response.ts
+++ apps/server/src/modules/tool/external-tool/controller/dto/response/external-tool-import-result-response.ts
@@ -1,14 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class ExternalToolImportResultResponse {
+	@ApiProperty({ description: 'Name of the external tool' })
 	toolName: string;
 
+	@ApiProperty({ description: 'Medium id of the external tool' })
 	mediumId?: string;
 
+	@ApiProperty({ description: 'Medium source of the external tool' })
 	mediumSourceId?: string;
 
+	@ApiProperty({ description: 'ObjectId of the created external tool' })
 	toolId?: string;
 
+	@ApiProperty({ description: 'Status message of the error that occurred' })
 	error?: string;
 
 	constructor(props: ExternalToolImportResultResponse) {
@@ -21,7 +26,10 @@ export class ExternalToolImportResultResponse {
 }
 
 export class ExternalToolImportResultListResponse {
-	@ApiProperty({ type: [ExternalToolImportResultResponse] })
+	@ApiProperty({
+		type: [ExternalToolImportResultResponse],
+		description: 'List of operation results for the provided external tools',
+	})
 	results: ExternalToolImportResultResponse[];
 
 	constructor(props: ExternalToolImportResultListResponse) {
