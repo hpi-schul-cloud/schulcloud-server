fe0586d32e71f6de2ba08e3302873850f7dfd588
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2024-05-27 14:01:47 +0200

    Change action to string enum and make it useable over api.

--- apps/server/src/modules/authorization/api/dto/authorization-body.params.ts
+++ apps/server/src/modules/authorization/api/dto/authorization-body.params.ts
@@ -4,9 +4,12 @@ import { IsArray, IsEnum, IsMongoId, ValidateNested } from 'class-validator';
 import { Action, AuthorizableReferenceType, AuthorizationContext } from '../../domain';
 
 class AuthorizationContextParms implements AuthorizationContext {
-	// TODO: Action is not string enum, is 0 = read / 1 = write ...should be changed in enum
 	@IsEnum(Action)
-	@ApiProperty({ description: 'Define for which action the operation should be performend.' })
+	@ApiProperty({
+		description: 'Define for which action the operation should be performend.',
+		enum: Action,
+		example: Action.read,
+	})
 	action!: Action;
 
 	@IsArray()
@@ -15,12 +18,13 @@ class AuthorizationContextParms implements AuthorizationContext {
 		enum: Permission,
 		isArray: true,
 		description: 'Needed user permissions based on user role, that are needed to execute the operation.',
+		example: Permission.USER_UPDATE,
 	})
 	requiredPermissions!: Permission[];
 }
 
 export class AuthorizationBodyParams {
-	@ValidateNested()
+	@ValidateNested({ each: true })
 	@ApiProperty({
 		type: AuthorizationContextParms,
 	})
