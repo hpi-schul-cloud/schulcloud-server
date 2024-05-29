f88304a48c004816b6a9fd7df30195feb591865d
Mrika Llabani <mrika.llabani@capgemini.com>
2024-05-29 08:24:48 +0200

    N21-1971 fix env

--- apps/server/src/modules/user-import/config/user-import-config.ts
+++ apps/server/src/modules/user-import/config/user-import-config.ts
@@ -6,7 +6,6 @@ export interface IUserImportFeatures {
 	userMigrationEnabled: boolean;
 	userMigrationSystemId: string;
 	useWithUserLoginMigration: boolean;
-	userMigrationRequestTimeout: number;
 }
 
 export class UserImportConfiguration {
@@ -14,6 +13,5 @@ export class UserImportConfiguration {
 		userMigrationEnabled: Configuration.get('FEATURE_USER_MIGRATION_ENABLED') as boolean,
 		userMigrationSystemId: Configuration.get('FEATURE_USER_MIGRATION_SYSTEM_ID') as string,
 		useWithUserLoginMigration: Configuration.get('FEATURE_MIGRATION_WIZARD_WITH_USER_LOGIN_MIGRATION') as boolean,
-		userMigrationRequestTimeout: Configuration.get('IMPORTUSER_SAVE_ALL_MATCHES_REQUEST_TIMEOUT_MS') as number,
 	};
 }
