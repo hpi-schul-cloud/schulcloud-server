9abc075c793bb70dd6fa467fc860d448bd0b9856
virgilchiriac <virgil.chiriac@dataport.de>
2024-05-30 07:53:52 +0200

    Update column-board-node.factory.ts

--- apps/server/src/shared/testing/factory/column-board-node.factory.ts
+++ apps/server/src/shared/testing/factory/column-board-node.factory.ts
@@ -1,8 +1,8 @@
 /* istanbul ignore file */
 import { ObjectId } from '@mikro-orm/mongodb';
 import { ColumnBoardNode, ColumnBoardNodeProps } from '@shared/domain/entity';
-import { BoardExternalReferenceType } from '@src/modules/board';
-import { BoardLayout } from '@src/modules/board/domain/types/board-layout.enum';
+import { BoardExternalReferenceType } from '@modules/board/domain/types/board-external-reference';
+import { BoardLayout } from '@modules/board/domain/types/board-layout.enum';
 import { BaseFactory } from './base.factory';
 
 export const columnBoardNodeFactory = BaseFactory.define<ColumnBoardNode, ColumnBoardNodeProps>(
