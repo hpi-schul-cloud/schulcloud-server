b2f235160b63b7cfc3aa7e616cd0c1531f3b9718
dyedwiper <maximilian.becker@dataport.de>
2024-05-30 11:29:42 +0200

    Fix sorting of classes

--- apps/server/src/modules/user/user-management/uc/mapper/user-list.dto.mapper.ts
+++ apps/server/src/modules/user/user-management/uc/mapper/user-list.dto.mapper.ts
@@ -1,15 +1,14 @@
 import { User } from '../../domain/user';
 import { UserListDto } from '../dto/user-list.dto';
-import { UserListQuery } from '../query/user-list.query';
 import { UserDtoMapper } from './user.dto.mapper';
 
 export class UserListDtoMapper {
-	public static mapToDto(users: User[], query: UserListQuery, total: number): UserListDto {
+	public static mapToDto(users: User[], limit: number, offset: number, total: number): UserListDto {
 		const data = UserDtoMapper.mapToDtos(users);
 
 		const dto = new UserListDto({
-			limit: query.limit,
-			offset: query.offset,
+			limit,
+			offset,
 			total,
 			data,
 		});
