de407c6a8795bb617ad7e2bcb93bab7fcd287ff4
dyedwiper <maximilian.becker@dataport.de>
2024-05-30 12:02:42 +0200

    Add TODO comment

--- apps/server/src/modules/user-management/repo/mapper/class.mapper.ts
+++ apps/server/src/modules/user-management/repo/mapper/class.mapper.ts
@@ -3,13 +3,14 @@ import { Class } from '../../domain/class';
 
 export class ClassMapper {
 	public static mapToDo(entity: ClassEntity): Class {
-		// @ts-ignore Needed because of wrong reference types in ClassEntity
+		// TODO: Remove ts-ignore when reference types are fixed in ClassEntity.
+		// @ts-ignore Needed because of wrong reference types in ClassEntity.
 		const userIds = entity.userIds?.map((user) => user._id.toString()) ?? [];
 
 		const c = new Class({
 			id: entity.id,
 			name: entity.name,
-			// @ts-ignore Needed because of wrong reference types in ClassEntity
+			// @ts-ignore Needed because of wrong reference types in ClassEntity.
 			userIds,
 		});
 
