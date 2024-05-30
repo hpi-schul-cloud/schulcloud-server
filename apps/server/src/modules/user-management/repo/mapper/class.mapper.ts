52b03b2723f15ec8cd886bdb278d9e7132ada918
dyedwiper <maximilian.becker@dataport.de>
2024-05-30 16:15:28 +0200

    Remove ts-ignores in ClassMapper

--- apps/server/src/modules/user-management/repo/mapper/class.mapper.ts
+++ apps/server/src/modules/user-management/repo/mapper/class.mapper.ts
@@ -3,11 +3,8 @@ import { Class } from '../../domain/class';
 
 export class ClassMapper {
 	public static mapToDo(entity: ClassEntity): Class {
-		// TODO: Remove ts-ignore when reference types are fixed in ClassEntity.
-		// @ts-ignore Needed because of wrong reference types in ClassEntity.
-		const userIds = entity.userIds?.map((user) => user._id.toString()) ?? [];
-		// @ts-ignore Needed because of wrong reference types in ClassEntity.
-		const teacherIds = entity.teacherIds?.map((teacher) => teacher._id.toString()) ?? [];
+		const userIds = entity.userIds?.map((_id) => _id.toString()) ?? [];
+		const teacherIds = entity.teacherIds?.map((_id) => _id.toString()) ?? [];
 
 		const c = new Class({
 			id: entity.id,
