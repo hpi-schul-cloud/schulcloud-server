0ce6d8640a542782789d2fa37972f3aa4f832b46
Constantin Bergatt <constantin.bergatt@dataport.de>
2024-06-10 15:33:15 +0200

    BC-6453 - update JWT extraction and handling in AuthorizationClientAdapter

--- apps/server/src/infra/authorization-client/authorization-client.adapter.ts
+++ apps/server/src/infra/authorization-client/authorization-client.adapter.ts
@@ -1,6 +1,8 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { REQUEST } from '@nestjs/core';
+import cookie from 'cookie';
 import { Request } from 'express';
+import { ExtractJwt, JwtFromRequestFunction } from 'passport-jwt';
 import { AuthorizationApi, AuthorizationBodyParams } from './authorization-api-client';
 import { AuthorizationErrorLoggableException, AuthorizationForbiddenLoggableException } from './error';
 
@@ -9,17 +11,18 @@ export class AuthorizationClientAdapter {
 	constructor(private readonly authorizationApi: AuthorizationApi, @Inject(REQUEST) private request: Request) {}
 
 	public async checkPermissionByReferences(params: AuthorizationBodyParams): Promise<void> {
-		if (!(await this.hasPermissionByReferences(params))) {
+		const hasPermission = await this.hasPermissionByReferences(params);
+		if (!hasPermission) {
 			throw new AuthorizationForbiddenLoggableException(params);
 		}
 	}
 
 	public async hasPermissionByReferences(params: AuthorizationBodyParams): Promise<boolean> {
-		try {
-			const tokenStr = this.request.headers.authorization || '';
+		const jwt = this.getJWT();
 
+		try {
 			const response = await this.authorizationApi.authorizationReferenceControllerAuthorizeByReference(params, {
-				headers: { authorization: `${tokenStr}` },
+				headers: { authorization: `${jwt}` },
 			});
 
 			const hasPermission = response.data.isAuthorized;
@@ -28,5 +31,29 @@ export class AuthorizationClientAdapter {
 		} catch (error) {
 			throw new AuthorizationErrorLoggableException(error as Error, params);
 		}
+	}
+
+	private getJWT(): string {
+		const getJWT = ExtractJwt.fromExtractors([ExtractJwt.fromAuthHeaderAsBearerToken(), this.fromCookie('jwt')]);
+		const jwt = getJWT(this.request) || this.request.headers.authorization;
+
+		if (!jwt) {
+			throw new UnauthorizedException('Authentication is required.');
+		}
+
+		return jwt;
+	}
+
+	private fromCookie(name: string): JwtFromRequestFunction {
+		return (request: Request) => {
+			let token: string | null = null;
+			const cookies = cookie.parse(request.headers.cookie || '');
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+			if (cookies && cookies[name]) {
+				// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+				token = cookies[name];
+			}
+			return token;
+		};
 	}
 }
