41cef59289413d55501bf5315cda3fc0316020ea
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2024-06-11 12:53:16 +0200

    fix tests regarding existing collections

--- apps/server/src/shared/domain/entity/all-entities.spec.ts
+++ apps/server/src/shared/domain/entity/all-entities.spec.ts
@@ -2,6 +2,7 @@ import { MongoMemoryDatabaseModule } from '@infra/database';
 import { MikroORM } from '@mikro-orm/core';
 import { EntityManager } from '@mikro-orm/mongodb';
 import { Test, TestingModule } from '@nestjs/testing';
+import { createCollections } from '@shared/testing';
 import { ALL_ENTITIES } from '.';
 
 describe('BaseRepo', () => {
@@ -17,6 +18,8 @@ describe('BaseRepo', () => {
 
 		em = module.get(EntityManager);
 		orm = module.get(MikroORM);
+
+		await createCollections(em);
 	});
 
 	afterAll(async () => {
@@ -29,31 +32,8 @@ describe('BaseRepo', () => {
 		});
 	});
 
-	// When we call `ensureIndexes` we get a MikroORM error when the collection already exists.
-	// This is despite the ORM ignoring existing collections. That's why we create all collections
-	// manually for this particular test.
-	// https://github.com/mikro-orm/mikro-orm/blob/fd56714e06e39c2724a3193b8b07279b8fb6c91f/packages/mongodb/src/MongoSchemaGenerator.ts#L30
-	const createCollections = async () => {
-		const collections = new Set();
-		Object.values(em.getMetadata().getAll()).forEach((meta) => {
-			if (meta.collection) {
-				collections.add(meta.collection);
-			}
-		});
-		await Promise.all(
-			Array.from(collections.values()).map(async (collection) => {
-				await em
-					.getDriver()
-					.getConnection()
-					.createCollection(collection as string);
-			})
-		);
-	};
-
 	describe('When entities have index definitions', () => {
 		it('should ensure indexes', async () => {
-			await createCollections();
-
 			await orm.getSchemaGenerator().ensureIndexes();
 		});
 	});
--- apps/server/src/shared/testing/index.ts
+++ apps/server/src/shared/testing/index.ts
@@ -1,5 +1,6 @@
 export * from './factory';
 export * from './setup-entities';
+export * from './create-collections';
 export * from './cleanup-collections';
 export * from './map-user-to-current-user';
 export * from './test-api-client';
