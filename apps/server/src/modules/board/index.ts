dc46eb25e93889d9fb12994202e7b4356a33465f
Simone Radtke <94017602+SimoneRadtke-Cap@users.noreply.github.com>
2024-06-21 14:14:44 +0200

    EW-793 - Refactor Common Cartridge Export (#4918)
    * Adding loggable exceptions
    * Renaming folder and adjust regarding imports
    * Adding common cartridge guard for intended use
    * Restructuring Common Cartridge export mechanism
    
    ---------
    
    Co-authored-by: Patrick Sachmann <20001160+psachmann@users.noreply.github.com>

--- apps/server/src/modules/board/index.ts
+++ apps/server/src/modules/board/index.ts
@@ -1,28 +1,37 @@
-export { BoardModule } from './board.module';
 export { BoardConfig } from './board.config';
+export { BoardModule } from './board.module';
+export { AnyElementContentBody, LinkContentBody, RichTextContentBody } from './controller/dto';
 export {
+	AnyBoardNode,
+	BoardExternalReference,
+	BoardExternalReferenceType,
+	BoardLayout,
 	BoardNode,
 	BoardNodeAuthorizable,
-	BoardExternalReferenceType,
-	BoardExternalReference,
-	BoardLayout,
 	BoardNodeFactory,
-	ColumnBoard,
 	// @modules/authorization/domain/rules/board-node.rule.ts
 	BoardRoles,
-	isDrawingElement,
-	isSubmissionItem,
-	isSubmissionItemContent,
-	SubmissionItem,
-	UserWithBoardRoles,
+	Card,
+	Column,
+	ColumnBoard,
+	ContentElementType,
 	// @modules/tool/tool-launch/service/auto-parameter-strategy/auto-context-name.strategy.ts
 	MediaBoard,
+	SubmissionItem,
+	UserWithBoardRoles,
+	isCard,
+	isColumn,
+	isDrawingElement,
+	isLinkElement,
+	isRichTextElement,
+	isSubmissionItem,
+	isSubmissionItemContent,
 } from './domain';
 
 export {
+	BoardCommonToolService,
 	BoardNodeAuthorizableService,
 	BoardNodeService,
-	BoardCommonToolService,
 	ColumnBoardService,
 	MediaAvailableLineService,
 	MediaBoardService,
