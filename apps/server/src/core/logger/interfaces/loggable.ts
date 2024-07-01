a5641d7cee2db73d4b571687f7365e98b505fbe3
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2024-07-01 12:42:30 +0200

    remove todo

--- apps/server/src/core/logger/interfaces/loggable.ts
+++ apps/server/src/core/logger/interfaces/loggable.ts
@@ -1,6 +1,5 @@
 import { ErrorLogMessage, LogMessage, ValidationErrorLogMessage } from '../types';
 
-// should be in shared or?
 export interface Loggable {
 	getLogMessage(): LogMessage | ErrorLogMessage | ValidationErrorLogMessage;
 }
