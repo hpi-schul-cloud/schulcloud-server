c89c66c3f072401fd553ed75c1d82df834176621
Maximilian Kreuzkam <maximilian.kreuzkam@capgemini.com>
2024-07-11 14:14:01 +0200

    Fix import order.

--- apps/server/src/infra/identity-management/keycloak/service/keycloak-identity-management-oauth.service.spec.ts
+++ apps/server/src/infra/identity-management/keycloak/service/keycloak-identity-management-oauth.service.spec.ts
@@ -5,9 +5,9 @@ import { HttpService } from '@nestjs/axios';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AxiosResponse } from 'axios';
 import { of } from 'rxjs';
+import { Logger } from '@src/core/logger';
 import { KeycloakAdministrationService } from '../../keycloak-administration/service/keycloak-administration.service';
 import { KeycloakIdentityManagementOauthService } from './keycloak-identity-management-oauth.service';
-import { Logger } from '@src/core/logger';
 
 describe('KeycloakIdentityManagementService', () => {
 	let module: TestingModule;
