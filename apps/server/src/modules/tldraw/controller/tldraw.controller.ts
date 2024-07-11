07244ca1e44387dafeaabbee2f92d0a65cfe0ceb
Phillip Wirth <phillip.wirth@dataport.de>
2024-07-11 14:18:37 +0200

    added endpoint to get document statistics

--- apps/server/src/modules/tldraw/controller/tldraw.controller.ts
+++ apps/server/src/modules/tldraw/controller/tldraw.controller.ts
@@ -1,17 +1,26 @@
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { Controller, Delete, ForbiddenException, HttpCode, NotFoundException, Param, UseGuards } from '@nestjs/common';
+import {
+	Controller,
+	Delete,
+	ForbiddenException,
+	Get,
+	HttpCode,
+	NotFoundException,
+	Param,
+	UseGuards
+} from '@nestjs/common';
 import { ApiValidationError } from '@shared/common';
 import { AuthGuard } from '@nestjs/passport';
 import { TldrawService } from '../service';
 import { TldrawDeleteParams } from './tldraw.params';
 
 @ApiTags('Tldraw Document')
-@UseGuards(AuthGuard('api-key'))
 @Controller('tldraw-document')
 export class TldrawController {
 	constructor(private readonly tldrawService: TldrawService) {}
 
 	@ApiOperation({ summary: 'Delete every element of tldraw drawing by its docName.' })
+	@UseGuards(AuthGuard('api-key'))
 	@ApiResponse({ status: 204 })
 	@ApiResponse({ status: 400, type: ApiValidationError })
 	@ApiResponse({ status: 403, type: ForbiddenException })
@@ -20,5 +29,16 @@ export class TldrawController {
 	@Delete(':docName')
 	async deleteByDocName(@Param() urlParams: TldrawDeleteParams) {
 		await this.tldrawService.deleteByDocName(urlParams.docName);
+	}
+
+	@ApiOperation({ summary: 'Delete every element of tldraw drawing by its docName.' })
+	@ApiResponse({ status: 204 })
+	@ApiResponse({ status: 400, type: ApiValidationError })
+	@ApiResponse({ status: 403, type: ForbiddenException })
+	@ApiResponse({ status: 404, type: NotFoundException })
+	@HttpCode(204)
+	@Get(':docName')
+	async getDocStats(@Param() urlParams: TldrawDeleteParams) {
+		await this.tldrawService.documentStatistics(urlParams.docName);
 	}
 }
--- apps/server/src/modules/tldraw/service/tldraw.service.ts
+++ apps/server/src/modules/tldraw/service/tldraw.service.ts
@@ -1,12 +1,38 @@
 import { Injectable } from '@nestjs/common';
-import { TldrawRepo } from '../repo';
+import { TldrawBoardRepo, TldrawRepo } from '../repo';
+import Y from 'yjs';
 
 @Injectable()
 export class TldrawService {
-	constructor(private readonly tldrawRepo: TldrawRepo) {}
+	constructor(private readonly tldrawRepo: TldrawRepo, private readonly tldrawBoardRepo: TldrawBoardRepo) {
+	}
 
 	async deleteByDocName(docName: string): Promise<void> {
 		const drawings = await this.tldrawRepo.findByDocName(docName);
 		await this.tldrawRepo.delete(drawings);
+	}
+
+	async documentStatistics(docName: string) {
+		const doc = await this.tldrawBoardRepo.getDocumentFromDb(docName);
+
+		if (doc?.store?.pendingStructs) {
+			const missingSize = doc.store.pendingStructs.missing.size;
+			const updateLength = doc.store.pendingStructs.update.length;
+			const pendingUpdate = doc.store.pendingStructs.update;
+			const decodedUpdate = Y.decodeUpdateV2(pendingUpdate);
+			const clients: { id: number; count: number }[] = [];
+
+			decodedUpdate.structs.forEach((struct) => {
+				if (!clients.find((client) => client.id === struct.id.client)) {
+					clients.push({ id: struct.id.client, count: 1 });
+				} else {
+					clients.find((client) => client.id === struct.id.client)!.count += 1;
+				}
+			});
+
+			console.log(
+				`Found pendingStructs in doc ${docName}; size of missing: ${missingSize}; size of update: ${updateLength}; number of pending structs: ${decodedUpdate.structs.length}; number of clients in pending structs: ${clients.length}; number of clients in doc ${doc.store.clients.size}`
+			);
+		}
 	}
 }
