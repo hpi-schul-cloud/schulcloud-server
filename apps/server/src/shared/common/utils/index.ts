342fd903d7da5942caaaa3411052894a189a1be2
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2024-07-15 17:31:37 +0200

    Fix barrel file

--- apps/server/src/shared/common/utils/index.ts
+++ apps/server/src/shared/common/utils/index.ts
@@ -1,4 +1,3 @@
-export { GuardAgainst } from './guard-against';
 export { SortHelper } from './sort-helper';
 export { getResolvedValues, isFulfilled } from './promise';
 export { extractJwtFromHeader, JwtExtractor } from './jwt';
