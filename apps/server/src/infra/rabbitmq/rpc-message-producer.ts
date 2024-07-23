b7927e9bb5051d91489c454e97725775b17b9070
SevenWaysDP <sergej.hoffmann@dataport.de>
2024-07-23 17:50:35 +0200

    refactor: add expiration to RPC message requests

--- apps/server/src/infra/rabbitmq/rpc-message-producer.ts
+++ apps/server/src/infra/rabbitmq/rpc-message-producer.ts
@@ -27,11 +27,15 @@ export abstract class RpcMessageProducer {
 	}
 
 	protected createRequest(event: string, payload: unknown) {
+		// expiration should be greater than timeout
+		const expiration = this.timeout > 0 ? this.timeout / 0.5 + this.timeout : undefined;
+
 		return {
 			exchange: this.exchange,
 			routingKey: event,
 			payload,
 			timeout: this.timeout,
+			expiration,
 		};
 	}
 }
