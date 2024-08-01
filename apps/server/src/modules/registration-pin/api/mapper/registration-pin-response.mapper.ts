ba98eb1b66015a1381fa7d5e6453e030ca7001d5
Patrick Sachmann <20001160+psachmann@users.noreply.github.com>
2024-08-01 14:34:20 +0200

    EW-949 adding admin api endpoints for registration pin

---
+++ apps/server/src/modules/registration-pin/api/mapper/registration-pin-response.mapper.ts
@@ -0,0 +1,14 @@
+import { RegistrationPinEntity } from '../../entity';
+import { GetRegistrationPinResponse } from '../dto';
+
+export class RegistrationPinResponseMapper {
+	public static mapRegistrationPinDoToRegistrationPinResponse(
+		registrationPin: RegistrationPinEntity
+	): GetRegistrationPinResponse {
+		return new GetRegistrationPinResponse({
+			username: registrationPin.email,
+			registrationPin: registrationPin.pin,
+			verified: registrationPin.verified,
+		});
+	}
+}
