887d6122271f9eaa8bfe208dd4f76cd2e7c21778
Patrick Sachmann <20001160+psachmann@users.noreply.github.com>
2024-08-06 15:30:25 +0200

    EW-949 adding unit tests

--- apps/server/src/modules/registration-pin/api/admin-api-registration-pin.controller.ts
+++ apps/server/src/modules/registration-pin/api/admin-api-registration-pin.controller.ts
@@ -11,7 +11,7 @@ export class AdminApiRegistrationPinController {
 	constructor(private readonly uc: RegistrationPinUc) {}
 
 	@Get(':email')
-	async createRegistrationPin(@Param('email') email: string): Promise<GetRegistrationPinResponse[]> {
+	async getRegistrationPinsForEmail(@Param('email') email: string): Promise<GetRegistrationPinResponse[]> {
 		const response = await this.uc.findForEmail(email);
 
 		return response;
