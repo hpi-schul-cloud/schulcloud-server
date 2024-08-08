e68d2b1bee5ee7413a1570361a52c64e007b1093
hoeppner-dataport <marcus.hoeppner@dataport.de>
2024-08-08 15:20:00 +0200

    add logging to redisIoAdapter

--- apps/server/src/infra/socketio/redis-ioadapter.ts
+++ apps/server/src/infra/socketio/redis-ioadapter.ts
@@ -5,20 +5,30 @@ import { Redis } from 'ioredis';
 import { ServerOptions } from 'socket.io';
 import { createAdapter } from '@socket.io/redis-adapter';
 import { Configuration } from '@hpi-schul-cloud/commons';
+import { LegacyLogger } from '@src/core/logger';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class RedisIoAdapter extends IoAdapter {
+	constructor(private readonly logger: LegacyLogger) {
+		super();
+	}
+
 	connectToRedis(): ReturnType<typeof createAdapter> | undefined {
 		if (Configuration.has('REDIS_URI')) {
 			try {
-				// const redisUri = Configuration.has('REDIS_URI') ? Configuration.get('REDIS_URI') : 'redis://localhost:6379';
+				const redisUri = Configuration.has('REDIS_URI')
+					? (Configuration.get('REDIS_URI') as string)
+					: 'redis://localhost:6379';
+				this.logger.log(`redisUri: ${redisUri}`);
 				const pubClient = new Redis(Configuration.get('REDIS_URI') as string);
 				const subClient = pubClient.duplicate();
 
 				pubClient.on('error', (err) => {
-					console.error('pubClient error', err);
+					this.logger.error('pubClient error', err);
 				});
 				subClient.on('error', (err) => {
-					console.error('subClient error', err);
+					this.logger.error('subClient error', err);
 				});
 				const adapterConstructor = createAdapter(pubClient, subClient);
 				return adapterConstructor;
