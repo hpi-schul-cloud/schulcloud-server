f0fdcc87080f089b717e6326ade2495ca28cff7c
Max Bischof <maximilian.bischof@dataport.de>
2024-08-15 15:11:00 +0200

    Move comment in auth guard index file

--- apps/server/src/infra/auth-guard/index.ts
+++ apps/server/src/infra/auth-guard/index.ts
@@ -1,10 +1,9 @@
 export { JwtValidationAdapter } from './adapter';
 export { AuthGuardModule } from './auth-guard.module';
-export { XApiKeyConfig } from './config';
+export { XApiKeyConfig, authConfig } from './config';
 export { CurrentUser, JWT, JwtAuthentication } from './decorator';
 // JwtAuthGuard only exported because api tests still overried this guard.
 // Use JwtAuthentication decorator for request validation
-export { authConfig } from './config';
 export { JwtAuthGuard, WsJwtAuthGuard } from './guard';
 export { CreateJwtPayload, ICurrentUser, JwtPayload, StrategyType } from './interface';
 export { CurrentUserMapper } from './mapper';
