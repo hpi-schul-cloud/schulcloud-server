cbf29e3ded480c3c0b104837888d66c65f558d45
Mrika Llabani <mrika.llabani@capgemini.com>
2024-08-19 11:35:37 +0200

    N21-2075 fix permission check

--- apps/server/src/modules/authorization/domain/rules/course.rule.ts
+++ apps/server/src/modules/authorization/domain/rules/course.rule.ts
@@ -1,6 +1,7 @@
 import { Course } from '@modules/learnroom/domain';
 import { Injectable } from '@nestjs/common';
 import { Course as CourseEntity, User } from '@shared/domain/entity';
+import { Permission } from '@shared/domain/interface';
 import { AuthorizationHelper } from '../service/authorization.helper';
 import { Action, AuthorizationContext, Rule } from '../type';
 
@@ -16,14 +17,20 @@ export class CourseRule implements Rule<CourseEntity | Course> {
 
 	public hasPermission(user: User, object: CourseEntity | Course, context: AuthorizationContext): boolean {
 		const { action, requiredPermissions } = context;
-		const hasPermission =
-			this.authorizationHelper.hasAllPermissions(user, requiredPermissions) &&
-			this.authorizationHelper.hasAccessToEntity(
-				user,
-				object,
-				action === Action.read ? ['teachers', 'substitutionTeachers', 'students'] : ['teachers', 'substitutionTeachers']
-			);
 
-		return hasPermission;
+		const hasRequiredPermission = this.authorizationHelper.hasAllPermissions(user, requiredPermissions);
+		const hasAdminPermission = this.authorizationHelper.hasAllPermissions(user, [Permission.COURSE_ADMINISTRATION]);
+
+		const hasAccessToEntity = hasAdminPermission
+			? true
+			: this.authorizationHelper.hasAccessToEntity(
+					user,
+					object,
+					action === Action.read
+						? ['teachers', 'substitutionTeachers', 'students']
+						: ['teachers', 'substitutionTeachers']
+			  );
+
+		return hasAccessToEntity && hasRequiredPermission;
 	}
 }
