8a82540270fa91b6109dd928cf92384ecccb46e2
Maximilian Kreuzkam <maximilian.kreuzkam@capgemini.com>
2024-08-21 11:41:43 +0200

    EW-811: Create getJwt and options methods to Adapter.

--- apps/server/src/infra/board-client/board-client.adapter.ts
+++ apps/server/src/infra/board-client/board-client.adapter.ts
@@ -1,8 +1,28 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { REQUEST } from '@nestjs/core';
+import { extractJwtFromHeader } from '@shared/common';
+import { RawAxiosRequestConfig } from 'axios';
+import { Request } from 'express';
 import { BoardApi } from './board-api-client';
 
 @Injectable()
 export class BoardClientAdapter {
 	constructor(private readonly boardApi: BoardApi, @Inject(REQUEST) private request: Request) {}
+
+	private createOptionParams(): RawAxiosRequestConfig {
+		const jwt = this.getJwt();
+		const options: RawAxiosRequestConfig = { headers: { authorization: `Bearer ${jwt}` } };
+
+		return options;
+	}
+
+	private getJwt(): string {
+		const jwt = extractJwtFromHeader(this.request) ?? this.request.headers.authorization;
+
+		if (!jwt) {
+			throw new UnauthorizedException('No JWT found in request');
+		}
+
+		return jwt;
+	}
 }
