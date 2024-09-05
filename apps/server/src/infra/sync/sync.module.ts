adb653f8a51f64d4284d550bc498274b4958fd3b
Firas Shmit <firas.shmit@capgemini.com>
2024-09-05 16:17:39 +0200

    EW-998 initialize tsp strategy depending on feature flag

--- apps/server/src/infra/sync/sync.module.ts
+++ apps/server/src/infra/sync/sync.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { LoggerModule } from '@src/core/logger';
 import { ConsoleWriterModule } from '@infra/console';
+import { Configuration } from '@hpi-schul-cloud/commons/lib';
 import { SyncConsole } from './console/sync.console';
 import { SyncUc } from './uc/sync.uc';
 import { SyncService } from './service/sync.service';
@@ -8,7 +9,12 @@ import { TspSyncStrategy } from './tsp/tsp-sync.strategy';
 
 @Module({
 	imports: [LoggerModule, ConsoleWriterModule],
-	providers: [SyncConsole, SyncUc, SyncService, TspSyncStrategy],
+	providers: [
+		SyncConsole,
+		SyncUc,
+		SyncService,
+		...((Configuration.get('FEATURE_TSP_SYNC_ENABLED ') as boolean) ? [TspSyncStrategy] : []),
+	],
 	exports: [SyncConsole],
 })
 export class SyncModule {}
