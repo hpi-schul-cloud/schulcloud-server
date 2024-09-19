286f86e30238d6e3b98e13d306c56390941dade8
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2024-09-19 13:24:28 +0200

    code style

--- apps/server/src/modules/tool/external-tool/controller/admin-api-external-tool.controller.ts
+++ apps/server/src/modules/tool/external-tool/controller/admin-api-external-tool.controller.ts
@@ -1,7 +1,6 @@
 import { ApiKeyGuard } from '@infra/auth-guard';
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
-
 import { ExternalToolRequestMapper, ExternalToolResponseMapper } from '../mapper';
 import { AdminApiExternalToolUc } from '../uc';
 import { ExternalToolCreateParams, ExternalToolResponse } from './dto';
