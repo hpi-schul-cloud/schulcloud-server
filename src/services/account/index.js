a56a6f59fe06a5c78f2aa46f53f50a325ad800b6
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2024-09-24 13:44:57 +0200

    Remove featherJS supportJwt Code

--- src/services/account/index.js
+++ src/services/account/index.js
@@ -1,12 +1,10 @@
 const { static: staticContent } = require('@feathersjs/express');
 const path = require('path');
 
-const { supportJWTServiceSetup, jwtTimerServiceSetup } = require('./services');
+const { jwtTimerServiceSetup } = require('./services');
 
 module.exports = (app) => {
 	app.use('/accounts/api', staticContent(path.join(__dirname, '/docs/openapi.yaml')));
 
 	app.configure(jwtTimerServiceSetup);
-
-	app.configure(supportJWTServiceSetup); // TODO: Remove me
 };
