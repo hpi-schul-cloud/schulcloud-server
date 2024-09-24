ff30cd9cd63b76f48287a744efc9d039ff16e3e7
Patrick Sachmann <20001160+psachmann@users.noreply.github.com>
2024-09-24 11:10:09 +0200

    EW-999 adding school change and classes provisioning to tsp sync (#5237)
    * Working on the provisioning function to sync classes and school changes
    * Adding loggables to the provisioning module
    * Refactoring tsp.strategy.ts
    * Adding tsp-provisioning.service.ts

---
+++ apps/server/src/modules/provisioning/dto/external-class.dto.ts
@@ -0,0 +1,10 @@
+export class ExternalClassDto {
+	public readonly externalId: string;
+
+	public readonly name: string;
+
+	constructor(props: Readonly<ExternalClassDto>) {
+		this.externalId = props.externalId;
+		this.name = props.name;
+	}
+}
