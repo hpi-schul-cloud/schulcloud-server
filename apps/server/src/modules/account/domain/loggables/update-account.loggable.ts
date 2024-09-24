98c2e4528f0b667ff92679bad829fb083a047d9c
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2024-09-24 17:02:17 +0200

    Add strict filter for cause error, to not expose details.

--- apps/server/src/modules/account/domain/loggables/update-account.loggable.ts
+++ apps/server/src/modules/account/domain/loggables/update-account.loggable.ts
@@ -2,7 +2,7 @@ import { InternalServerErrorException } from '@nestjs/common';
 import { ErrorLogMessage, Loggable } from '@src/core/logger';
 
 export class UpdateAccountLoggableException extends InternalServerErrorException implements Loggable {
-	constructor(private readonly type: string, private readonly error?: unknown) {
+	constructor(private readonly type: string, private readonly causeErrorMessage: string) {
 		super('ENTITY_NOT_FOUND_EXCEPTION');
 	}
 
@@ -10,8 +10,9 @@ export class UpdateAccountLoggableException extends InternalServerErrorException
 		const message: ErrorLogMessage = {
 			type: this.type,
 			stack: this.stack,
-			error: (this.error as Error) ?? {},
-			data: {},
+			data: {
+				causeErrorMessage: this.causeErrorMessage,
+			},
 		};
 
 		return message;
