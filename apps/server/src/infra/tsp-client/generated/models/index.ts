b4ec8349ee59df2689bea846c1cd2dd10c30db99
Simone Radtke <simone.radtke@capgemini.com>
2024-09-26 14:02:23 +0200

    EW-995 Generate api

--- apps/server/src/infra/tsp-client/generated/models/index.ts
+++ apps/server/src/infra/tsp-client/generated/models/index.ts
@@ -1,1 +1,7 @@
+export * from './robj-export-klasse';
+export * from './robj-export-lehrer';
 export * from './robj-export-lehrer-migration';
+export * from './robj-export-schueler';
+export * from './robj-export-schueler-migration';
+export * from './robj-export-schule';
+export * from './version-response';
