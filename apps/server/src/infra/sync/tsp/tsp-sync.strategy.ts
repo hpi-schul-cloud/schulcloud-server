923e8b3ab3ebf11cb02ddb8961c92bca7e10d503
Maximilian Kreuzkam <maximilian.kreuzkam@capgemini.com>
2024-09-26 11:31:12 +0200

    Remove logging.

--- apps/server/src/infra/sync/tsp/tsp-sync.strategy.ts
+++ apps/server/src/infra/sync/tsp/tsp-sync.strategy.ts
@@ -1,29 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { Logger } from '@src/core/logger';
 import { SyncStrategy } from '../strategy/sync-strategy';
 import { SyncStrategyTarget } from '../sync-strategy.types';
 
 @Injectable()
 export class TspSyncStrategy extends SyncStrategy {
-	constructor(private readonly logger: Logger) {
-		super();
-		this.logger.setContext(TspSyncStrategy.name);
-	}
-
 	getType(): SyncStrategyTarget {
 		return SyncStrategyTarget.TSP;
 	}
 
 	sync(): Promise<void> {
 		// implementation
-		// Just for testing purposes
-		this.logger.info({
-			getLogMessage() {
-				return {
-					message: 'Running TSP sync',
-				};
-			},
-		});
 		return Promise.resolve();
 	}
 }
