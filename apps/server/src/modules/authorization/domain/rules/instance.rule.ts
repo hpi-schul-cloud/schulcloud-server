17ee1bd6c9d17d64ace19fb7bc9fc93dcdddf885
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2024-09-26 08:51:12 +0200

    add comment

--- apps/server/src/modules/authorization/domain/rules/instance.rule.ts
+++ apps/server/src/modules/authorization/domain/rules/instance.rule.ts
@@ -2,7 +2,7 @@ import { Instance } from '@modules/instance';
 import { Injectable } from '@nestjs/common';
 import { User } from '@shared/domain/entity';
 import { RoleName } from '@shared/domain/interface';
-import { Action } from '@src/infra/authorization-client';
+import { Action } from '@infra/authorization-client';
 import { AuthorizationHelper } from '../service/authorization.helper';
 import { AuthorizationContext, Rule } from '../type';
 
@@ -19,6 +19,7 @@ export class InstanceRule implements Rule<Instance> {
 	public hasPermission(user: User, entity: Instance, context: AuthorizationContext): boolean {
 		const hasPermission = this.authorizationHelper.hasAllPermissions(user, context.requiredPermissions);
 
+		// As temporary solution until the user with write access to instance added as group, we must check the role.
 		if (context.action === Action.WRITE) {
 			const hasRole = this.authorizationHelper.hasRole(user, RoleName.SUPERHERO);
 
