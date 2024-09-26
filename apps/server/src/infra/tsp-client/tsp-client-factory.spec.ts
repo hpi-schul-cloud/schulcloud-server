cfe9a250735541ba89cf93776ca15156e43df99f
Patrick Sachmann <20001160+psachmann@users.noreply.github.com>
2024-09-26 14:41:59 +0200

    adding tests for tsp-client-factory.ts
    Co-authored-by: Simone Radtke <SimoneRadtke-Cap@users.noreply.github.com>

--- apps/server/src/infra/tsp-client/tsp-client-factory.spec.ts
+++ apps/server/src/infra/tsp-client/tsp-client-factory.spec.ts
@@ -19,10 +19,22 @@ describe('TspClientFactory', () => {
 					useValue: createMock<ConfigService<ServerConfig, true>>({
 						getOrThrow: (key: string) => {
 							switch (key) {
+								case 'SC_DOMAIN':
+									return faker.internet.domainName();
+								case 'HOST':
+									return faker.internet.url();
+								case 'TSP_API_BASE_URL':
+									return 'https://test2.schulportal-thueringen.de/tip-ms/api/';
+								case 'TSP_API_CLIENT_ID':
+									return faker.string.uuid();
+								case 'TSP_API_CLIENT_SECRET':
+									return faker.string.uuid();
+								case 'TSP_API_SIGNATURE_KEY':
+									return faker.string.uuid();
 								case 'TSP_API_TOKEN_LIFETIME_MS':
 									return faker.number.int();
 								default:
-									return faker.lorem.word();
+									throw new Error(`Unknown key: ${key}`);
 							}
 						},
 					}),
@@ -57,18 +69,21 @@ describe('TspClientFactory', () => {
 		});
 	});
 
-	describe('when using the created client', () => {
+	// TODO: add a working integration test
+	describe.skip('when using the created client', () => {
 		const setup = () => {
 			const client = sut.createExportClient();
+
 			return client;
 		};
 
 		it('should return the migration version', async () => {
 			const client = setup();
 
-			const result = await client.exportLehrerListMigration();
+			const result = await client.version();
 
-			expect(result).toBeDefined();
+			expect(result.status).toBe(200);
+			expect(result.data.version).toBeDefined();
 		});
 	});
 });
