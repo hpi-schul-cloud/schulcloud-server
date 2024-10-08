b19ddb08941805842dcc98e0bc49e131a6d047ef
Steliyan Dinkov <steliyan.dinkov@capgemini.com>
2024-10-08 16:46:01 +0200

    update bad import

--- apps/server/src/modules/user-license/domain/media-source-oauth-config.ts
+++ apps/server/src/modules/user-license/domain/media-source-oauth-config.ts
@@ -1,6 +1,6 @@
 import { AuthorizableObject, DomainObject } from '@shared/domain/domain-object';
+import { EntityId } from '@shared/domain/types';
 import { MediaSourceAuthMethod } from '../enum';
-import { EntityId } from '../../../shared/domain/types';
 
 export interface MediaSourceOauthConfigProps extends AuthorizableObject {
 	id: EntityId;
