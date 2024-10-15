8c89ad5d3d0a55272b25f27f301fa669403d2723
Hussam Kayed <hussam.kayed@capgemini.com>
2024-10-15 14:55:56 +0200

    ew-1009: created adapter, request config and module wrapper

---
+++ apps/server/src/modules/common-cartridge/common-cartridge-client/card-client/card-client.adapter.ts
@@ -0,0 +1,38 @@
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { extractJwtFromHeader } from '@shared/common';
+import { RawAxiosRequestConfig } from 'axios';
+import { Request } from 'express';
+import { REQUEST } from '@nestjs/core';
+import { CardListResponse } from '@src/modules/board/controller/dto';
+import { BoardCardApi, CardResponse } from './cards-api-client';
+
+@Injectable()
+export class CardClientAdapter {
+	constructor(private readonly boardCardApi: BoardCardApi, @Inject(REQUEST) private request: Request) {}
+
+	public async getAllBoardCardsbyIds(cardsIds: Array<string>): Promise<CardResponse> {
+		const options = this.createOptionParams();
+		const getBoardCardsResponse = await this.boardCardApi
+			.cardControllerGetCards(cardsIds, options)
+			.then((response) => response.data);
+
+		return getBoardCardsResponse;
+	}
+
+	private createOptionParams(): RawAxiosRequestConfig {
+		const jwt = this.getJwt();
+		const options: RawAxiosRequestConfig = { headers: { authorization: `Bearer ${jwt}` } };
+
+		return options;
+	}
+
+	private getJwt(): string {
+		const jwt = extractJwtFromHeader(this.request) ?? this.request.headers.authorization;
+
+		if (!jwt) {
+			throw new UnauthorizedException('No JWT found in request');
+		}
+
+		return jwt;
+	}
+}
