82811c06857dc206d6b4f1bcc5a23c2db25117b5
Phillip Wirth <phillip.wirth@dataport.de>
2024-10-22 16:51:21 +0200

    added migration

--- apps/server/src/migrations/mikro-orm/Migration20241022205656.ts
+++ apps/server/src/migrations/mikro-orm/Migration20241022205656.ts
@@ -2,16 +2,36 @@ import { Migration } from '@mikro-orm/migrations-mongodb';
 
 export class Migration20241022205656 extends Migration {
 	async up(): Promise<void> {
-		await this.driver.nativeUpdate('context-external-tools', { contextId: { $type: 'string' } }, [
-			{ $set: { contextId: { $toObjectId: '$contextId' } } },
-		]);
+		await this.getCollection('context-external-tools').updateMany(
+			{
+				contextId: { $type: 'string' },
+			},
+			[
+				{
+					$set: {
+						contextId: {
+							$toObjectId: '$contextId',
+						},
+					},
+				},
+			]
+		);
 	}
 
 	async down(): Promise<void> {
-		await this.driver.nativeUpdate('context-external-tools', { contextId: { $type: 'objectId' } }, [
+		await this.getCollection('context-external-tools').updateMany(
 			{
-				$set: { contextId: { $toString: '$contextId' } },
+				contextId: { $type: 'objectId' },
 			},
-		]);
+			[
+				{
+					$set: {
+						contextId: {
+							$toString: '$contextId',
+						},
+					},
+				},
+			]
+		);
 	}
 }
