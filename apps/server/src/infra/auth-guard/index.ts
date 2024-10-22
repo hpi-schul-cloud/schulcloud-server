9b81974a03b413eff1c35232e5fed146f56c34e8
Max Bischof <maximilian.bischof@dataport.de>
2024-10-22 14:44:24 +0200

    Add WSJwtAuthenticationDecorator

--- apps/server/src/infra/auth-guard/index.ts
+++ apps/server/src/infra/auth-guard/index.ts
@@ -2,7 +2,7 @@ export { JwtValidationAdapter } from './adapter';
 export { AuthGuardConfig } from './auth-guard.config';
 export { AuthGuardModule } from './auth-guard.module';
 export { XApiKeyConfig } from './config';
-export { CurrentUser, JWT, JwtAuthentication, XApiKeyAuthentication } from './decorator';
+export { CurrentUser, JWT, JwtAuthentication, WsJwtAuthentication, XApiKeyAuthentication } from './decorator';
 // JwtAuthGuard only exported because api tests still overried this guard.
 // Use JwtAuthentication decorator for request validation
 export { JwtAuthGuard, WsJwtAuthGuard, XApiKeyGuard } from './guard';
