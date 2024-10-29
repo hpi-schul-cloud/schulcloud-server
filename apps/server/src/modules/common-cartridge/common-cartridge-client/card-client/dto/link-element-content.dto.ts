71e37b752ad1499580e804a2013c06c65fc8c2eb
HKayed <90037037+HKayed@users.noreply.github.com>
2024-10-29 14:28:08 +0100

    EW-1009: Detach card dependency from boards (#5306)
    * ew-1009: Detach card dependency from boards

---
+++ apps/server/src/modules/common-cartridge/common-cartridge-client/card-client/dto/link-element-content.dto.ts
@@ -0,0 +1,16 @@
+export class LinkElementContentDto {
+	url: string;
+
+	title: string;
+
+	description?: string;
+
+	imageUrl?: string;
+
+	constructor(url: string, title: string, description: string, imageUrl: string) {
+		this.url = url;
+		this.title = title;
+		this.description = description;
+		this.imageUrl = imageUrl;
+	}
+}
