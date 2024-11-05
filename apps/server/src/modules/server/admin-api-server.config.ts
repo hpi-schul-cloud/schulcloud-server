baa3940fb6a86b7e22d556705d797951540c661d
Max <53796487+dyedwiper@users.noreply.github.com>
2024-11-05 14:53:19 +0100

    BC-8019 Switch to asymmetric signing of JWT (#5294)
    * Switch to asymmetric signing
    
    * Adjust configs
    
    * Fix tests
    
    * Make secret combination of private and public key in feathers
    
    * Add tests
    
    * Update typ of JWT
    
    * Update authConfig in tests
    
    * Handle new lines in keys
    
    * Clean up strategy tests
    
    * Use JwtModuleOptionsFactory
    
    * Use factory method instead of class
    
    * Remove authConfig
    
    * Remove unnecessary props in AuthGuardConfig
    
    * Add AuthGuardConfig to files-storage
    
    * Change setting of options in authn service
    
    * Add authn test modules
    
    * Fix authn service test
    
    * Change iss and aud of JWT
    
    * Fix reading of config for other apps
    
    * Add JWT_SIGNING_ALGORITHM as env var
    
    * Add check of algorithm in JWT validation
    
    * Add check of issuer and audience in JWT validation
    
    * Remove unnecessary consts
    
    * Skip broken test
    
    * Add SC_Domain to preview-generator-configmap
    
    * Add values to top-level configs where forgotten
    
    * Add factory for strategy options to satisfy SonarCloud
    
    * Use getOrThrow to read values from ConfigService
    
    * Define defaultMikroOrmOptions in for AdminApiServerModule separately to fix dependency problem
    
    * Add JWT_PUBLIC_KEY to configmap of admin-api-server
    
    * Add JWT_PUBLIC_KEY to configmap of preview-generator
    
    ---------
    
    Co-authored-by: Max Bischof <106820326+bischofmax@users.noreply.github.com>

--- apps/server/src/modules/server/admin-api-server.config.ts
+++ apps/server/src/modules/server/admin-api-server.config.ts
@@ -1,11 +1,12 @@
-import { DeletionConfig } from '@modules/deletion';
+import { Configuration } from '@hpi-schul-cloud/commons/lib';
 import { AuthGuardConfig } from '@infra/auth-guard';
+import { DeletionConfig } from '@modules/deletion';
 import { LegacySchoolConfig } from '@modules/legacy-school';
-import { UserConfig } from '@modules/user';
 import { RegistrationPinConfig } from '@modules/registration-pin';
 import { ToolConfig } from '@modules/tool';
-import { Configuration } from '@hpi-schul-cloud/commons/lib';
+import { UserConfig } from '@modules/user';
 import { LanguageType } from '@shared/domain/interface';
+import { Algorithm } from 'jsonwebtoken';
 
 export interface AdminApiServerConfig
 	extends DeletionConfig,
@@ -56,9 +57,11 @@ const config: AdminApiServerConfig = {
 	ADMIN_API__ALLOWED_API_KEYS: (Configuration.get('ADMIN_API__ALLOWED_API_KEYS') as string)
 		.split(',')
 		.map((part) => (part.split(':').pop() ?? '').trim()),
-	JWT_AUD: Configuration.get('JWT_AUD') as string,
-	JWT_LIFETIME: Configuration.get('JWT_LIFETIME') as string,
-	AUTHENTICATION: Configuration.get('AUTHENTICATION') as string,
+
+	// Node's process.env escapes newlines. We need to reverse it for the keys to work.
+	// See: https://stackoverflow.com/questions/30400341/environment-variables-containing-newlines-in-node
+	JWT_PUBLIC_KEY: (Configuration.get('JWT_PUBLIC_KEY') as string).replace(/\\n/g, '\n'),
+	JWT_SIGNING_ALGORITHM: Configuration.get('JWT_SIGNING_ALGORITHM') as Algorithm,
 	LOGIN_BLOCK_TIME: 0,
 	TEACHER_STUDENT_VISIBILITY__IS_CONFIGURABLE: Configuration.get(
 		'TEACHER_STUDENT_VISIBILITY__IS_CONFIGURABLE'
@@ -70,6 +73,7 @@ const config: AdminApiServerConfig = {
 	TEACHER_VISIBILITY_FOR_EXTERNAL_TEAM_INVITATION: Configuration.get(
 		'TEACHER_VISIBILITY_FOR_EXTERNAL_TEAM_INVITATION'
 	) as string,
+	SC_DOMAIN: Configuration.get('SC_DOMAIN') as string,
 };
 
 export const adminApiServerConfig = () => config;
