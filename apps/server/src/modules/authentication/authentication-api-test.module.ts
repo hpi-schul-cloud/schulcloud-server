a3c96c1dc0e73fb2f12d66efc27ae146709fcace
Marvin Öhlerking <marvin.oehlerking@capgemini.com>
2024-11-12 10:47:58 +0100

    fix import

--- apps/server/src/modules/authentication/authentication-api-test.module.ts
+++ apps/server/src/modules/authentication/authentication-api-test.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { LoggerModule } from '../../core/logger';
+import { LoggerModule } from '@src/core/logger';
 import { AuthenticationTestModule } from './authentication-test.module';
 import { LoginController, LogoutController } from './controllers';
 import { LoginUc, LogoutUc } from './uc';
