c2b7542b3d97bebbda3a2801643d46fd22bf459e
Patrick Sachmann <20001160+psachmann@users.noreply.github.com>
2024-11-19 10:37:16 +0100

    some changes

--- apps/server/src/infra/files-storage-client/files-storage-rest-client.adapter.ts
+++ apps/server/src/infra/files-storage-client/files-storage-rest-client.adapter.ts
@@ -1,10 +1,7 @@
-import { Logger } from '@src/core/logger';
 import { FileApi } from './generated';
 
 export class FilesStorageRestClientAdapter {
-	constructor(private readonly api: FileApi, private readonly logger: Logger) {
-		this.logger.setContext(FilesStorageRestClientAdapter.name);
-	}
+	constructor(private readonly api: FileApi) {}
 
 	public async download(fileRecordId: string, fileName: string): Promise<string> {
 		const response = await this.api.download(fileRecordId, fileName, undefined, { responseType: 'blob' });
