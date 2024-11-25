bd256874e9f855d668650625a12bf54b5750fa69
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2024-11-25 18:39:07 +0100

    fix board tests

--- apps/server/src/modules/board/metrics/metrics.service.spec.ts
+++ apps/server/src/modules/board/metrics/metrics.service.spec.ts
@@ -2,7 +2,8 @@ import { DeepMocked, createMock } from '@golevelup/ts-jest';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from '@src/modules/user';
 import { RoleName } from '@shared/domain/interface';
-import { roleFactory, userDoFactory } from '@shared/testing';
+import { roleFactory } from '@shared/testing/factory/role.factory';
+import { userDoFactory } from '@shared/testing/factory/user.do.factory';
 import { MetricsService } from './metrics.service';
 
 describe(MetricsService.name, () => {
