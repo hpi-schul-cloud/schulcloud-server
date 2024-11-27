c521ae5362ecf8cb973f6a4f827054c8b6fdb47e
hoeppner-dataport <106819770+hoeppner-dataport@users.noreply.github.com>
2024-11-27 14:58:56 +0100

    BC-8329 - fix meta tag extractor (#5350)
    Fix security issues related to meta tag extractor service.
    
    * only take max 50.000 characters of the html response to extract meta-tags
    * ensure html is never longer than 50.000 characters
    * enforce https to be the used protocol
    * only consider urls internal that have the correct hostname
    * fix RegEx for mongoDb-Ids
    
    ---------
    
    Co-authored-by: Thomas Feldtkeller <thomas.feldtkeller@dataport.de>

--- apps/server/src/modules/board/domain/link-element.do.spec.ts
+++ apps/server/src/modules/board/domain/link-element.do.spec.ts
@@ -21,6 +21,7 @@ describe('LinkElement', () => {
 			title: 'Example',
 			description: 'Example description',
 			imageUrl: 'https://example.com/image.jpg',
+			originalImageUrl: 'https://example.com/image.jpg',
 		});
 	});
 
@@ -66,6 +67,15 @@ describe('LinkElement', () => {
 	it('should set imageUrl', () => {
 		linkElement.imageUrl = 'https://newurl.com/newimage.jpg';
 		expect(linkElement.imageUrl).toBe('https://newurl.com/newimage.jpg');
+	});
+
+	it('should return originalImageUrl', () => {
+		expect(linkElement.originalImageUrl).toBe('https://example.com/image.jpg');
+	});
+
+	it('should set originalImageUrl', () => {
+		linkElement.originalImageUrl = 'https://newurl.com/newimage.jpg';
+		expect(linkElement.originalImageUrl).toBe('https://newurl.com/newimage.jpg');
 	});
 
 	it('should not have child', () => {
