e42188049c5eade6e658d53aae6cc23eb7d572bf
Thomas Feldtkeller <thomas.feldtkeller@dataport.de>
2024-11-27 14:12:43 +0100

    refactor loggable

--- apps/server/src/modules/user/loggable/addSecondarySchoolToUserError.loggable.ts
+++ apps/server/src/modules/user/loggable/addSecondarySchoolToUserError.loggable.ts
@@ -1,25 +1,26 @@
 import { InternalServerErrorException } from '@nestjs/common';
-import { ErrorLogMessage } from '@shared/common';
+import { ErrorLogMessage, ValidationErrorLogMessage } from '@shared/common';
 import { Loggable } from '@shared/common/loggable';
 import { RoleReference } from '@shared/domain/domainobject';
+import { LogMessage } from '@src/core/logger';
 
 export class AddSecondarySchoolToUsersRoleErrorLoggableException
 	extends InternalServerErrorException
 	implements Loggable
 {
 	constructor(private readonly payload: { roles: RoleReference[] }) {
-		super('could not recognize any of the users roles when adding a secondary school');
+		super();
 	}
 
-	getLogMessage(): ErrorLogMessage {
+	getLogMessage(): LogMessage | ErrorLogMessage | ValidationErrorLogMessage {
 		const { roles } = this.payload;
 		const roleNames = roles.map((role) => role.name).join(', ');
 
-		const message: ErrorLogMessage = {
+		const message = {
 			type: 'INTERNAL_ERROR',
+			message: 'could not recognize any of the users roles when adding a secondary school',
 			stack: this.stack,
 			data: {
-				message: 'could not recognize any of the users roles when adding a secondary school',
 				roleNames,
 			},
 		};
