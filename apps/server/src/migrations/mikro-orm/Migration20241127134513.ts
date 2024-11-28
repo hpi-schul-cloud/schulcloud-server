72db046f3320ffc578917d26b6f4d1a4f91523d5
Marvin Öhlerking <marvin.oehlerking@capgemini.com>
2024-11-28 11:38:44 +0100

    migration script

---
+++ apps/server/src/migrations/mikro-orm/Migration20241127134513.ts
@@ -0,0 +1,30 @@
+import { Migration } from '@mikro-orm/migrations-mongodb';
+import { ObjectId } from '@mikro-orm/mongodb';
+import type { ContextExternalToolEntity } from '@modules/tool/context-external-tool/entity';
+import { ContextExternalToolType } from '@modules/tool/context-external-tool/entity/context-external-tool-type.enum';
+import type { Course } from '@shared/domain/entity';
+
+export class Migration20241127134513 extends Migration {
+	async up(): Promise<void> {
+		let deleteCount = 0;
+		const cursor = this.getCollection<ContextExternalToolEntity>('context-external-tools').find({
+			contextType: ContextExternalToolType.COURSE,
+		});
+
+		for await (const doc of cursor) {
+			const course = await this.getCollection<Course>('courses').findOne({ _id: new ObjectId(doc.contextId) });
+
+			if (course === null) {
+				await this.getCollection('context-external-tools').deleteOne({ _id: doc._id });
+				deleteCount++;
+			}
+		}
+
+		console.info(`Deleted ${deleteCount} context-external-tools without a reference to an existing course context.`);
+	}
+
+	// eslint-disable-next-line @typescript-eslint/require-await
+	async down(): Promise<void> {
+		console.info('This migration cannot be rolled back.');
+	}
+}
