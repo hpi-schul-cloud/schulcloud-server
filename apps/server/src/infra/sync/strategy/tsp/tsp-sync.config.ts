ab93fc84284b48fe743cc338612316771bda2ae6
Hussam Kayed <hussam.kayed@capgemini.com>
2024-11-29 14:26:06 +0100

    EW-1055: modifed config name and import

--- apps/server/src/infra/sync/strategy/tsp/tsp-sync.config.ts
+++ apps/server/src/infra/sync/strategy/tsp/tsp-sync.config.ts
@@ -1,6 +1,6 @@
-import { TspConfig } from '../tsp.config';
+import { SyncConfig } from '../sync.config';
 
-export interface TspSyncConfig extends TspConfig {
+export interface TspSyncConfig extends SyncConfig {
 	TSP_SYNC_SCHOOL_LIMIT: number;
 	TSP_SYNC_SCHOOL_DAYS_TO_FETCH: number;
 	TSP_SYNC_DATA_LIMIT: number;
