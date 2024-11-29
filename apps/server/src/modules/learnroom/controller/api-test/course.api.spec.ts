a050214987e38314a4e623f499617498e8f82803
Patrick Sachmann <20001160+psachmann@users.noreply.github.com>
2024-11-29 13:20:14 +0100

    removing unused test

--- apps/server/src/modules/learnroom/controller/api-test/course.api.spec.ts
+++ apps/server/src/modules/learnroom/controller/api-test/course.api.spec.ts
@@ -1,7 +1,6 @@
-import { faker } from '@faker-js/faker/locale/af_ZA';
 import { EntityManager } from '@mikro-orm/mongodb';
 import { ServerTestModule } from '@modules/server/server.module';
-import { HttpStatus, INestApplication, StreamableFile } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { Course as CourseEntity } from '@shared/domain/entity';
 import { Permission } from '@shared/domain/interface';
@@ -93,42 +92,6 @@ describe('Course Controller (API)', () => {
 			expect(typeof data[0].title).toBe('string');
 			expect(data[0].startDate).toBe(course.startDate);
 			expect(data[0].untilDate).toBe(course.untilDate);
-		});
-	});
-
-	describe('[POST] /courses/:id/export', () => {
-		const setup = async () => {
-			const student1 = createStudent();
-			const student2 = createStudent();
-			const teacher = createTeacher();
-			const substitutionTeacher = createTeacher();
-			const teacherUnknownToCourse = createTeacher();
-			const course = courseFactory.build({
-				teachers: [teacher.user],
-				students: [student1.user, student2.user],
-			});
-
-			await em.persistAndFlush([teacher.account, teacher.user, course]);
-			em.clear();
-
-			const loggedInClient = await testApiClient.login(teacher.account);
-
-			return { course, teacher, teacherUnknownToCourse, substitutionTeacher, student1, loggedInClient };
-		};
-
-		it('should find course export', async () => {
-			const { course, loggedInClient } = await setup();
-
-			const body = { topics: [faker.string.uuid()], tasks: [faker.string.uuid()], columnBoards: [faker.string.uuid()] };
-			const response = await loggedInClient.post(`${course.id}/export?version=1.1.0`, body);
-
-			expect(response.statusCode).toEqual(201);
-			const file = response.body as StreamableFile;
-			expect(file).toBeDefined();
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-			expect(response.header['content-type']).toBe('application/zip');
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-			expect(response.header['content-disposition']).toBe('attachment;');
 		});
 	});
 
