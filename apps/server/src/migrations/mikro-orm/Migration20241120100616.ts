f76d8cf57556ec1357da35bb0b6a80dccbd4b5d5
Igor Richter <igor.richter@capgemini.com>
2024-12-02 11:52:10 +0100

    requested changes

--- apps/server/src/migrations/mikro-orm/Migration20241120100616.ts
+++ apps/server/src/migrations/mikro-orm/Migration20241120100616.ts
@@ -1,13 +1,12 @@
 import { Migration } from '@mikro-orm/migrations-mongodb';
 import { ObjectId } from '@mikro-orm/mongodb';
-import { type BoardNodeEntity } from '@modules/board/repo';
-import { type ContextExternalToolEntity, ContextExternalToolType } from '@modules/tool/context-external-tool/entity';
-import { type SchoolExternalToolEntity } from '@modules/tool/school-external-tool/entity';
-import { type Course } from '@shared/domain/entity';
+import { ContextExternalToolType } from '@modules/tool/context-external-tool/entity';
 
 export class Migration20241120100616 extends Migration {
 	async up(): Promise<void> {
-		const cursor = this.getCollection<ContextExternalToolEntity>('context-external-tools').find({
+		const cursor = this.getCollection<{ contextType: string; contextId: ObjectId; schoolTool: ObjectId }>(
+			'context-external-tools'
+		).find({
 			$or: [{ contextType: ContextExternalToolType.COURSE }, { contextType: ContextExternalToolType.BOARD_ELEMENT }],
 		});
 
@@ -18,31 +17,31 @@ export class Migration20241120100616 extends Migration {
 			if (tool.contextType === 'course') {
 				courseId = tool.contextId;
 			} else if (tool.contextType === 'boardElement') {
-				const element: BoardNodeEntity | null = (await this.driver.findOne('boardnodes', {
+				const element = await this.getCollection<{ path: string }>('boardnodes').findOne({
 					_id: tool.contextId,
-				})) as BoardNodeEntity | null;
+				});
 
 				if (element) {
 					const boardId = new ObjectId(element.path.split(',')[1]);
 
-					const board: BoardNodeEntity | null = (await this.driver.findOne('boardnodes', {
+					const board = await this.getCollection<{ context: ObjectId }>('boardnodes').findOne({
 						_id: boardId,
-					})) as BoardNodeEntity | null;
+					});
 
 					if (board) {
-						courseId = board.context as unknown as ObjectId;
+						courseId = board.context;
 					}
 				}
 			}
 
 			if (courseId) {
-				const course: Course = (await this.driver.findOne('courses', { _id: courseId })) as Course;
+				const course = await this.getCollection<{ schoolId: ObjectId }>('courses').findOne({ _id: courseId });
 
-				const schoolTool: SchoolExternalToolEntity = (await this.driver.findOne('school-external-tools', {
+				const schoolTool = await this.getCollection<{ school: ObjectId }>('school-external-tools').findOne({
 					_id: tool.schoolTool,
-				})) as SchoolExternalToolEntity;
+				});
 
-				if (!course || !schoolTool || course.school.toString() !== schoolTool.school.toString()) {
+				if (!course || !schoolTool || course.schoolId.toString() !== schoolTool.school.toString()) {
 					await this.driver.nativeDelete('context-external-tools', { _id: tool._id });
 					console.info(`deleted context external tool: ${tool._id.toString()}`);
 					numberOfDeletedTools += 1;
