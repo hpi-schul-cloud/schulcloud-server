2437608408919b849af0a1a8fc725c0b3ba5d2bc
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2024-12-02 10:12:11 +0100

    rename comments

--- apps/server/src/migrations/mikro-orm/Migration202410041210124.ts
+++ apps/server/src/migrations/mikro-orm/Migration202410041210124.ts
@@ -2,28 +2,28 @@ import { Migration } from '@mikro-orm/migrations-mongodb';
 
 export class Migration202410041210124 extends Migration {
 	async up(): Promise<void> {
-		// Add ROOMVIEWER role
+		// Add ROOM_VIEWER role
 		await this.getCollection('roles').insertOne({
 			name: 'room_viewer',
 			permissions: ['ROOM_VIEW'],
 		});
-		console.info('Added ROOMVIEWER role with ROOM_VIEW permission');
+		console.info('Added ROOM_VIEWER role with ROOM_VIEW permission');
 
-		// Add ROOMEDITOR role
+		// Add ROOM_EDITOR role
 		await this.getCollection('roles').insertOne({
 			name: 'room_editor',
 			permissions: ['ROOM_VIEW', 'ROOM_EDIT'],
 		});
-		console.info('Added ROOMEDITOR role with ROOM_VIEW and ROOM_EDIT permissions');
+		console.info('Added ROOM_EDITOR role with ROOM_VIEW and ROOM_EDIT permissions');
 	}
 
 	async down(): Promise<void> {
-		// Remove ROOMVIEWER role
+		// Remove ROOM_VIEWER role
 		await this.getCollection('roles').deleteOne({ name: 'room_viewer' });
-		console.info('Rollback: Removed ROOMVIEWER role');
+		console.info('Rollback: Removed ROOM_VIEWER role');
 
-		// Remove ROOMEDITOR role
+		// Remove ROOM_EDITOR role
 		await this.getCollection('roles').deleteOne({ name: 'room_editor' });
-		console.info('Rollback: Removed ROOMEDITOR role');
+		console.info('Rollback: Removed ROOM_EDITOR role');
 	}
 }
