a3436d70c8cdbf835bd407b42681b430635d8a50
SevenWaysDP <sergej.hoffmann@dataport.de>
2024-12-04 12:15:16 +0100

    Update Tldraw client config to log WITH_TLDRAW2 and enable printHierarchy in sc-config

--- apps/server/src/modules/tldraw-client/tldraw-client.config.ts
+++ apps/server/src/modules/tldraw-client/tldraw-client.config.ts
@@ -2,9 +2,14 @@ import { Configuration } from '@hpi-schul-cloud/commons/lib';
 import { TldrawClientConfig } from './interface';
 
 export const getTldrawClientConfig = (): TldrawClientConfig => {
-	return {
+	const WITH_TLDRAW = Configuration.get('WITH_TLDRAW2');
+	const A = Configuration.get('WITH_TLDRAW2');
+	console.log('WITH_TLDRAW2', A, typeof A);
+	const config = {
 		TLDRAW_ADMIN_API_CLIENT_BASE_URL: Configuration.get('TLDRAW_ADMIN_API_CLIENT__BASE_URL') as string,
 		TLDRAW_ADMIN_API_CLIENT_API_KEY: Configuration.get('TLDRAW_ADMIN_API_CLIENT__API_KEY') as string,
-		WITH_TLDRAW2: Configuration.get('WITH_TLDRAW2') as boolean,
+		WITH_TLDRAW2: WITH_TLDRAW as boolean,
 	};
+
+	return config;
 };
