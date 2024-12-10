e0a213c4bac6dee4a7b8657401a6eca2a0128dea
Max Bischof <maximilian.bischof@dataport.de>
2024-12-10 11:19:33 +0100

    Remove tldraw and tldraw client modules

--- apps/server/src/modules/tldraw-client/index.ts
+++
@@ -1,4 +0,0 @@
-export { TldrawClientModule } from './tldraw-client.module';
-export { DrawingElementAdapterService } from './service/drawing-element-adapter.service';
-export { TldrawClientConfig } from './interface';
-export { getTldrawClientConfig } from './tldraw-client.config';
--- apps/server/src/modules/tldraw-client/interface/tldraw-client-config.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface TldrawClientConfig {
-	TLDRAW_ADMIN_API_CLIENT_BASE_URL: string;
-	TLDRAW_ADMIN_API_CLIENT_API_KEY: string;
-}
