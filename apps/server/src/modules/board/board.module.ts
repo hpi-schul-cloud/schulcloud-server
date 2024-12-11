e3a060ef50163236b76ee92c029076625b69bf54
Max Bischof <maximilian.bischof@dataport.de>
2024-12-11 09:29:05 +0100

    Fix typo in env name

--- apps/server/src/modules/board/board.module.ts
+++ apps/server/src/modules/board/board.module.ts
@@ -45,7 +45,7 @@ import {
 		HttpModule,
 		CqrsModule,
 		TldrawClientModule.register({
-			TLDRAW_ADMIN_API_CLIENT_BASE_URL: Configuration.get('TLDRAW_ADMIN_API_CLIENT_BASE_URL') as string,
+			TLDRAW_ADMIN_API_CLIENT_BASE_URL: Configuration.get('TLDRAW_ADMIN_API_CLIENT__BASE_URL') as string,
 			TLDRAW_ADMIN_API_CLIENT_API_KEY: Configuration.get('TLDRAW_ADMIN_API_CLIENT__API_KEY') as string,
 		}),
 		CollaborativeTextEditorModule,
