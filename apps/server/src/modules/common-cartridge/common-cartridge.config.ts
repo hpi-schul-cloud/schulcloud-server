09e6ba93ed6d0cbd02197b702e7919fdd5c34c40
Patrick Sachmann <20001160+psachmann@users.noreply.github.com>
2024-12-11 14:47:04 +0100

    updating config

--- apps/server/src/modules/common-cartridge/common-cartridge.config.ts
+++ apps/server/src/modules/common-cartridge/common-cartridge.config.ts
@@ -4,11 +4,15 @@ export interface CommonCartridgeConfig {
 	NEST_LOG_LEVEL: string;
 	INCOMING_REQUEST_TIMEOUT: number;
 	FEATURE_COMMON_CARTRIDGE_COURSE_IMPORT_MAX_FILE_SIZE: number;
+	JWT_PUBLIC_KEY: string;
+	JWT_SIGNING_ALGORITHM: Algorithm;
 }
 
 const commonCartridgeConfig: CommonCartridgeConfig = {
 	NEST_LOG_LEVEL: Configuration.get('NEST_LOG_LEVEL') as string,
 	INCOMING_REQUEST_TIMEOUT: Configuration.get('FILES_STORAGE__INCOMING_REQUEST_TIMEOUT') as number,
+	JWT_PUBLIC_KEY: (Configuration.get('JWT_PUBLIC_KEY') as string).replace(/\\n/g, '\n'),
+	JWT_SIGNING_ALGORITHM: Configuration.get('JWT_SIGNING_ALGORITHM') as Algorithm,
 	FEATURE_COMMON_CARTRIDGE_COURSE_IMPORT_MAX_FILE_SIZE: Configuration.get(
 		'FEATURE_COMMON_CARTRIDGE_COURSE_IMPORT_MAX_FILE_SIZE'
 	) as number,
