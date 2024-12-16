dd16232d7948b1578adbc0be649f265baebcf3c2
Igor Richter <igor.richter@capgemini.com>
2024-12-16 12:30:58 +0100

    remove unused index files

--- apps/server/src/modules/provisioning/testing/index.ts
+++ apps/server/src/modules/provisioning/testing/index.ts
@@ -1,3 +1,2 @@
-export { externalUserDtoFactory } from './external-user-dto.factory';
 export { externalGroupDtoFactory } from './external-group-dto.factory';
 export { externalGroupUserDtoFactory } from './external-group-user-dto.factory';
