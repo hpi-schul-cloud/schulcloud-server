92381e75b273959d87c38af6d6e5c45ed8db91d1
Hussam Kayed <hussam.kayed@capgemini.com>
2024-12-18 14:36:36 +0100

    EW-830: changed default name of exported course

--- apps/server/src/modules/common-cartridge/import/common-cartridge-file-parser.ts
+++ apps/server/src/modules/common-cartridge/import/common-cartridge-file-parser.ts
@@ -39,6 +39,7 @@ export class CommonCartridgeFileParser {
 	public getTitle(): string | undefined {
 		const title = this.manifestParser.getTitle();
 
+		if (title === '' || title === undefined) return 'Importiertes Spaltenboard';
 		return title;
 	}
 
