e8b10e9c7e7f7dad52a35d151149c45abf42f47e
Gordon Nicholas <gordon.nicholas@capgemini.com>
2024-12-19 17:05:45 +0100

    N21-2103 wip vidis response test coverage

--- apps/server/src/infra/sync/media-licenses/testing/vidis-item.response.factory.ts
+++ apps/server/src/infra/sync/media-licenses/testing/vidis-item.response.factory.ts
@@ -4,8 +4,13 @@ import { VidisItemResponse } from '../response';
 export const vidisItemResponseFactory = Factory.define<VidisItemResponse>(({ sequence }) => {
 	return {
 		offerId: `${sequence}`,
-		offerTitle: 'VIDIS Test Response',
-		offerVersion: 1,
 		schoolActivations: ['00100', '00200', '00300'],
+		educationProviderOrganizationName: 'Test Org',
+		offerDescription: 'Test Description',
+		offerLink: 'https://test-link.com/offer',
+		offerLogo: 'https://test-link.com/offer/logo.svg',
+		offerTitle: 'VIDIS Test',
+		offerLongTitle: 'VIDIS Test Response',
+		offerVersion: 1,
 	};
 });
