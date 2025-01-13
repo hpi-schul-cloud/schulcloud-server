34cd1c015ba6bb78eab1b1b55fc137d20f7bfd3e
Max Bischof <maximilian.bischof@dataport.de>
2025-01-13 09:24:45 +0100

    Remove import from infra and add CreateJwtPayload interface in JwtAuthenticationFactory

--- apps/server/src/shared/testing/factory/jwt-authentication.factory.ts
+++ apps/server/src/shared/testing/factory/jwt-authentication.factory.ts
@@ -1,6 +1,16 @@
 import { Configuration } from '@hpi-schul-cloud/commons/lib';
-import { CreateJwtPayload } from '@src/infra/auth-guard';
 import jwt, { Algorithm } from 'jsonwebtoken';
+
+interface CreateJwtPayload {
+	accountId: string;
+	userId: string;
+	schoolId: string;
+	roles: string[];
+	systemId?: string;
+	support: boolean;
+	supportUserId?: string;
+	isExternalUser: boolean;
+}
 
 const privateKey = Configuration.get('JWT_PRIVATE_KEY') as string;
 const domain = Configuration.get('SC_DOMAIN') as string;
