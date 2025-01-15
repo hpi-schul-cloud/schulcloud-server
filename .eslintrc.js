9168b0cf0c5ced9994aafb5e177c808986555289
Constantin Bergatt <constantin.bergatt@dataport.de>
2025-01-15 11:11:52 +0100

    BC-8373 - revert linter rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -158,8 +158,8 @@ module.exports = {
 							{
 								patterns: [
 									{
-										group: ['@apps/**', '@infra/**', '@modules/**/*.ts', '@shared/**', '!@modules/**/*.app.module.ts'],
-										message: 'apps-modules may NOT import from @apps, @infra, @modules or @shared',
+										group: ['@apps/**', '@infra/**', '@shared/**'],
+										message: 'apps-modules may NOT import from @apps, @infra or @shared',
 									},
 								],
 							},
