590e596dbd3de89a2e00ff8691dc1aed1bc1e100
SevenWaysDP <sergej.hoffmann@dataport.de>
2025-01-15 10:22:52 +0100

    refactor: simplify config service mock in local.strategy.spec.ts and add mockId in collaborative-storage.service.spec.ts

--- apps/server/src/modules/authentication/strategy/local.strategy.spec.ts
+++ apps/server/src/modules/authentication/strategy/local.strategy.spec.ts
@@ -1,7 +1,6 @@
 import { createMock, DeepMocked } from '@golevelup/ts-jest';
 import { IdentityManagementOauthService } from '@infra/identity-management';
 import { Account } from '@modules/account';
-import { ServerConfig } from '@modules/server';
 import { UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { User } from '@shared/domain/entity';
@@ -29,7 +28,7 @@ describe('LocalStrategy', () => {
 		await setupEntities();
 		authenticationServiceMock = createMock<AuthenticationService>();
 		idmOauthServiceMock = createMock<IdentityManagementOauthService>();
-		configServiceMock = createMock<ConfigService<ServerConfig, true>>();
+		configServiceMock = createMock<ConfigService>();
 		userRepoMock = createMock<UserRepo>();
 		strategy = new LocalStrategy(authenticationServiceMock, idmOauthServiceMock, configServiceMock, userRepoMock);
 		mockUser = userFactory.withRoleByName(RoleName.STUDENT).buildWithId();
