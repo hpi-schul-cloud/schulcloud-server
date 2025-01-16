fb8c9a407c78c5efa62d2525f8ebe9ecef0d26ed
SevenWaysDP <sergej.hoffmann@dataport.de>
2025-01-16 14:46:52 +0100

    refactor: replace decode with jwtDecode in Etherpad tests

--- test/services/etherpad/permissions.test.js
+++ test/services/etherpad/permissions.test.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const freeport = require('freeport');
 const { Configuration } = require('@hpi-schul-cloud/commons');
-const decode = require('jwt-decode');
+const { jwtDecode } = require('jwt-decode');
 const logger = require('../../../src/logger');
 const MockServer = require('./MockServer');
 const appPromise = require('../../../src/app');
@@ -80,7 +80,7 @@ describe('Etherpad Permission Check: Teacher', () => {
 			},
 		} = await testObjects.setupUser({ roles: ['teacher'] });
 
-		const jwt = decode(accessToken);
+		const jwt = jwtDecode(accessToken);
 		const course = await testObjects.createTestCourse({ teacherIds: [jwt.userId] });
 
 		const data = {
