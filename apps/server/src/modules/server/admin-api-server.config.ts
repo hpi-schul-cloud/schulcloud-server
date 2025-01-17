2f8d773d87792f1ad5b5eb69d158ebe28fde31da
Steliyan Dinkov <steliyan.dinkov@capgemini.com>
2025-01-17 15:21:57 +0100

    update modules and configs

--- apps/server/src/modules/server/admin-api-server.config.ts
+++ apps/server/src/modules/server/admin-api-server.config.ts
@@ -68,6 +68,7 @@ const config: AdminApiServerConfig = {
 		'TEACHER_VISIBILITY_FOR_EXTERNAL_TEAM_INVITATION'
 	) as string,
 	PUBLIC_BACKEND_URL: Configuration.get('PUBLIC_BACKEND_URL') as string,
+	FEATURE_VIDIS_MEDIA_ACTIVATIONS_ENABLED: Configuration.get('FEATURE_VIDIS_MEDIA_ACTIVATIONS_ENABLED') as boolean,
 };
 
 export const adminApiServerConfig = () => config;
