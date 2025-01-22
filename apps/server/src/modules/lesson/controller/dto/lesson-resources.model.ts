0d12c9151565a20904cdddd930b22236993a3f7d
Hussam Kayed <hussam.kayed@capgemini.com>
2025-01-22 15:38:54 +0100

    EW-1090: Modifying the swagger definitions of resources model

--- apps/server/src/modules/lesson/controller/dto/lesson-resources.model.ts
+++ apps/server/src/modules/lesson/controller/dto/lesson-resources.model.ts
@@ -1,8 +1,15 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class LessonResources {
+    @ApiProperty({description: "client"})
     client: string;
+    @ApiProperty({description: "description"})
     description: string;
+    @ApiProperty({description: "merlinReference"})
     merlinReference?: string;
+    @ApiProperty({description: "title"})
     title: string;
+    @ApiProperty({description: "url"})
     url: string;
 
     constructor(client: string, description: string, title: string, url: string, merlinReference?: string) {
