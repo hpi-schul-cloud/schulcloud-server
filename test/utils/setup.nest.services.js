2835248870162f66beb892084a0a21247cd6b477
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2025-01-24 15:46:59 +0100

    Cleanup code

--- test/utils/setup.nest.services.js
+++ test/utils/setup.nest.services.js
@@ -17,93 +17,6 @@ const { SystemRule, AuthorizationRulesModule } = require('../../dist/apps/server
 const { createConfigModuleOptions } = require('../../dist/apps/server/shared/common/config-module-options');
 const { serverConfig } = require('../../dist/apps/server/modules/server/server.config');
 
-const { AccountEntity } = require('../../dist/apps/server/modules/account/domain/entity/account.entity');
-const { SchoolSystemOptionsEntity } = require('../../dist/apps/server/modules/legacy-school/entity');
-const {
-	SystemEntity,
-	OauthConfigEntity,
-	OidcConfigEntity,
-	LdapConfigEntity,
-	ExternalSourceEmbeddable,
-} = require('../../dist/apps/server/modules/system/entity');
-const {
-	CountyEmbeddable,
-	FederalStateEntity,
-} = require('../../dist/apps/server/shared/domain/entity/federal-state.entity');
-const { Role } = require('../../dist/apps/server/shared/domain/entity/role.entity');
-const {
-	SchoolEntity,
-	SchoolRolePermission,
-	SchoolRoles,
-} = require('../../dist/apps/server/shared/domain/entity/school.entity');
-const { SchoolYearEntity } = require('../../dist/apps/server/shared/domain/entity/schoolyear.entity');
-const { StorageProviderEntity } = require('../../dist/apps/server/shared/domain/entity/storageprovider.entity');
-const { UserLoginMigrationEntity } = require('../../dist/apps/server/shared/domain/entity/user-login-migration.entity');
-const { UserSchoolEmbeddable } = require('../../dist/apps/server/shared/domain/entity/user.entity');
-const {
-	ConsentEntity,
-	UserConsentEntity,
-	ParentConsentEntity,
-} = require('../../dist/apps/server/shared/domain/entity');
-const { UserParentsEntity } = require('../../dist/apps/server/shared/domain/entity/user-parents.entity');
-const { UserSourceOptionsEntity } = require('../../dist/apps/server/shared/domain/entity/user-source-options-entity');
-
-const {
-	DashboardModelEntity,
-	DashboardGridElementModel,
-} = require('../../dist/apps/server/shared/domain/entity/dashboard.model.entity');
-const { Course } = require('../../dist/apps/server/shared/domain/entity/course.entity');
-const { CourseGroup } = require('../../dist/apps/server/shared/domain/entity/coursegroup.entity');
-const { ClassEntity, ClassSourceOptionsEntity } = require('../../dist/apps/server/modules/class/entity');
-const {
-	GroupEntity,
-	GroupUserEmbeddable,
-	GroupValidPeriodEmbeddable,
-} = require('../../dist/apps/server/modules/group/entity');
-
-const {
-	ContextExternalToolService,
-} = require('../../dist/apps/server/modules/tool/context-external-tool/service/context-external-tool.service');
-const { ColumnBoardService } = require('../../dist/apps/server/modules/board/service/column-board.service');
-const {
-	CollaborativeStorageUc,
-} = require('../../dist/apps/server/modules/collaborative-storage/uc/collaborative-storage.uc');
-const { FeathersRosterService } = require('../../dist/apps/server/modules/roster/service/feathers-roster.service');
-const { GroupService } = require('../../dist/apps/server/modules/group/service/group.service');
-const { RocketChatService } = require('../../dist/apps/server/modules/rocketchat/rocket-chat.service');
-
-const ENTITIES = {
-	Role,
-	SchoolYearEntity,
-	SchoolRoles,
-	SchoolRolePermission,
-	UserLoginMigrationEntity,
-	SystemEntity,
-	SchoolEntity,
-	SchoolSystemOptionsEntity,
-	FederalStateEntity,
-	CountyEmbeddable,
-	StorageProviderEntity,
-	ConsentEntity,
-	UserConsentEntity,
-	ParentConsentEntity,
-	UserParentsEntity,
-	UserSourceOptionsEntity,
-	UserSchoolEmbeddable,
-	AccountEntity,
-	OauthConfigEntity,
-	OidcConfigEntity,
-	LdapConfigEntity,
-	ExternalSourceEmbeddable,
-	Course,
-	CourseGroup,
-	ClassEntity,
-	ClassSourceOptionsEntity,
-	GroupEntity,
-	GroupUserEmbeddable,
-	GroupValidPeriodEmbeddable,
-};
-
 const setupNestServices = async (app) => {
 	const module = await Test.createTestingModule({
 		imports: [
@@ -113,13 +26,7 @@ const setupNestServices = async (app) => {
 					clientUrl: DB_URL,
 					password: DB_PASSWORD,
 					user: DB_USERNAME,
-					entities: [
-						// DashboardModelEntity,
-						// DashboardGridElementModel,
-						'dist/apps/server/modules/**/*.entity.js',
-						'dist/apps/server/shared/domain/entity/*.entity.js',
-					],
-					// entitiesTs: ['apps/server/src/modules/**/*.entity.ts', 'apps/server/src/shared/domain/entity/*.entity.ts'],
+					entities: ['dist/apps/server/modules/**/*.entity.js', 'dist/apps/server/shared/domain/entity/*.entity.js'],
 					allowGlobalContext: true,
 					debug: false, // use it for locally debugging of querys
 				})
