dc391de66f68fa41ae0bdabc6217d2d75ec32faa
Constantin Bergatt <constantin.bergatt@dataport.de>
2025-01-24 08:54:24 +0100

    BC-8551 - readd and reduce used entities of `deletion-console.app.module.ts`

--- apps/server/src/modules/deletion-console/deletion-console.app.module.ts
+++ apps/server/src/modules/deletion-console/deletion-console.app.module.ts
@@ -1,11 +1,16 @@
+import { DB_PASSWORD, DB_URL, DB_USERNAME } from '@imports-from-feathers';
 import { ConsoleWriterModule } from '@infra/console';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { AccountModule } from '@modules/account';
 import { UserModule } from '@modules/user';
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { createConfigModuleOptions } from '@shared/common/config-module-options';
+import { defaultMikroOrmOptions } from '@shared/common/defaultMikroOrmOptions';
+import { Role } from '@shared/domain/entity/role.entity';
 import { ConsoleModule } from 'nestjs-console';
+import { FileEntity } from '../files/entity';
 import { DeletionClient } from './deletion-client';
 import { DeletionExecutionConsole } from './deletion-execution.console';
 import { DeletionQueueConsole } from './deletion-queue.console';
@@ -19,6 +24,16 @@ import { BatchDeletionUc, DeletionExecutionUc } from './uc';
 		ConsoleWriterModule,
 		UserModule,
 		ConfigModule.forRoot(createConfigModuleOptions(deletionConsoleConfig)),
+		MikroOrmModule.forRoot({
+			...defaultMikroOrmOptions,
+			type: 'mongo',
+			clientUrl: DB_URL,
+			password: DB_PASSWORD,
+			user: DB_USERNAME,
+			allowGlobalContext: true,
+			entities: [Role, FileEntity],
+			// debug: true, // use it for locally debugging of queries
+		}),
 		AccountModule,
 		HttpModule,
 	],
