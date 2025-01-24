4dd09a39e72b5545b1d0e65e66c60915713c2ad3
Constantin Bergatt <constantin.bergatt@dataport.de>
2025-01-24 08:58:52 +0100

    @bergatco BC-8551 - reduce used entities of `admin-api.server.app.module.ts`

--- apps/server/src/modules/server/admin-api.server.app.module.ts
+++ apps/server/src/modules/server/admin-api.server.app.module.ts
@@ -7,8 +7,8 @@ import { EtherpadClientModule } from '@infra/etherpad-client';
 import { RabbitMQWrapperModule, RabbitMQWrapperTestModule } from '@infra/rabbitmq';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { DeletionApiModule } from '@modules/deletion/deletion-api.module';
-import { FileEntity } from '@modules/files/entity';
 import { LegacySchoolAdminApiModule } from '@modules/legacy-school/legacy-school-admin.api-module';
+import { AdminApiRegistrationPinModule } from '@modules/registration-pin/admin-api-registration-pin.module';
 import { ToolAdminApiModule } from '@modules/tool/tool-admin-api.module';
 import { UserAdminApiModule } from '@modules/user/user-admin-api.module';
 import { DynamicModule, Module } from '@nestjs/common';
@@ -16,120 +16,8 @@ import { ConfigModule } from '@nestjs/config';
 import { CqrsModule } from '@nestjs/cqrs';
 import { createConfigModuleOptions } from '@shared/common/config-module-options';
 import { defaultMikroOrmOptions } from '@shared/common/defaultMikroOrmOptions';
-import { AccountEntity } from '@modules/account/domain/entity/account.entity';
-import { BoardNodeEntity } from '@modules/board/repo/entity';
-import { ClassEntity } from '@modules/class/entity';
-import { DeletionLogEntity } from '@modules/deletion/repo/entity/deletion-log.entity';
-import { DeletionRequestEntity } from '@modules/deletion/repo/entity/deletion-request.entity';
-import { GroupEntity } from '@modules/group/entity';
-import { InstanceEntity } from '@modules/instance';
-import { SchoolSystemOptionsEntity } from '@modules/legacy-school/entity';
-import { MediaSourceEntity } from '@modules/media-source/entity';
-import { OauthSessionTokenEntity } from '@modules/oauth/entity';
-import { ExternalToolPseudonymEntity, PseudonymEntity } from '@modules/pseudonym/entity';
-import { RegistrationPinEntity } from '@modules/registration-pin/entity';
-import { RocketChatUserEntity } from '@modules/rocketchat-user/entity';
-import { RoomMembershipEntity } from '@modules/room-membership/repo/entity/room-membership.entity';
-import { RoomEntity } from '@modules/room/repo/entity';
-import { MediaSchoolLicenseEntity, SchoolLicenseEntity } from '@modules/school-license/entity';
-import { ShareToken } from '@modules/sharing/entity/share-token.entity';
-import { SystemEntity } from '@modules/system/entity/system.entity';
-import { ContextExternalToolEntity, LtiDeepLinkTokenEntity } from '@modules/tool/context-external-tool/entity';
-import { ExternalToolEntity } from '@modules/tool/external-tool/entity';
-import { SchoolExternalToolEntity } from '@modules/tool/school-external-tool/entity';
-import { ImportUser } from '@modules/user-import/entity';
-import { MediaUserLicenseEntity, UserLicenseEntity } from '@modules/user-license/entity';
-import { ColumnBoardNode } from '@shared/domain/entity/column-board-node.entity';
-import { Course } from '@shared/domain/entity/course.entity';
-import { CourseGroup } from '@shared/domain/entity/coursegroup.entity';
-import { DashboardGridElementModel, DashboardModelEntity } from '@shared/domain/entity/dashboard.model.entity';
-import { CountyEmbeddable, FederalStateEntity } from '@shared/domain/entity/federal-state.entity';
-import {
-	ColumnboardBoardElement,
-	LegacyBoard,
-	LegacyBoardElement,
-	LessonBoardElement,
-	TaskBoardElement,
-} from '@shared/domain/entity/legacy-board';
-import { LessonEntity } from '@shared/domain/entity/lesson.entity';
-import { LtiTool } from '@shared/domain/entity/ltitool.entity';
-import { Material } from '@shared/domain/entity/materials.entity';
-import { CourseNews, News, SchoolNews, TeamNews } from '@shared/domain/entity/news.entity';
 import { Role } from '@shared/domain/entity/role.entity';
-import { SchoolEntity, SchoolRolePermission, SchoolRoles } from '@shared/domain/entity/school.entity';
-import { SchoolYearEntity } from '@shared/domain/entity/schoolyear.entity';
-import { StorageProviderEntity } from '@shared/domain/entity/storageprovider.entity';
-import { Submission } from '@shared/domain/entity/submission.entity';
-import { Task } from '@shared/domain/entity/task.entity';
-import { TeamEntity, TeamUserEntity } from '@shared/domain/entity/team.entity';
-import { UserLoginMigrationEntity } from '@shared/domain/entity/user-login-migration.entity';
-import { User } from '@shared/domain/entity/user.entity';
-import { VideoConference } from '@shared/domain/entity/video-conference.entity';
-import { AdminApiRegistrationPinModule } from '@modules/registration-pin/admin-api-registration-pin.module';
 import { adminApiServerConfig } from './admin-api-server.config';
-
-export const ENTITIES = [
-	AccountEntity,
-	LegacyBoard,
-	LegacyBoardElement,
-	BoardNodeEntity,
-	ColumnboardBoardElement,
-	ColumnBoardNode,
-	ClassEntity,
-	DeletionRequestEntity,
-	DeletionLogEntity,
-	ContextExternalToolEntity,
-	CountyEmbeddable,
-	Course,
-	CourseGroup,
-	CourseNews,
-	DashboardGridElementModel,
-	DashboardModelEntity,
-	ExternalToolEntity,
-	FederalStateEntity,
-	ImportUser,
-	LessonEntity,
-	LessonBoardElement,
-	LtiTool,
-	Material,
-	News,
-	PseudonymEntity,
-	ExternalToolPseudonymEntity,
-	RocketChatUserEntity,
-	Role,
-	RoomEntity,
-	RoomMembershipEntity,
-	SchoolEntity,
-	SchoolExternalToolEntity,
-	SchoolNews,
-	SchoolRolePermission,
-	SchoolRoles,
-	SchoolSystemOptionsEntity,
-	SchoolYearEntity,
-	ShareToken,
-	StorageProviderEntity,
-	Submission,
-	SystemEntity,
-	Task,
-	TaskBoardElement,
-	TeamEntity,
-	TeamNews,
-	TeamUserEntity,
-	User,
-	UserLoginMigrationEntity,
-	VideoConference,
-	GroupEntity,
-	RegistrationPinEntity,
-	UserLicenseEntity,
-	MediaUserLicenseEntity,
-	InstanceEntity,
-	MediaSourceEntity,
-	SchoolLicenseEntity,
-	MediaSchoolLicenseEntity,
-	OauthSessionTokenEntity,
-	LtiDeepLinkTokenEntity,
-	FileEntity,
-];
 
 const serverModules = [
 	ConfigModule.forRoot(createConfigModuleOptions(adminApiServerConfig)),
@@ -155,7 +43,7 @@ const serverModules = [
 			clientUrl: DB_URL,
 			password: DB_PASSWORD,
 			user: DB_USERNAME,
-			entities: ENTITIES,
+			entities: [Role],
 			// debug: true, // use it for locally debugging of queries
 		}),
 		CqrsModule,
