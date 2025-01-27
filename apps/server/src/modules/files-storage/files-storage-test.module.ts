db526d675f514ee8aee8c230c8018f94ee32ec33
Constantin Bergatt <constantin.bergatt@dataport.de>
2025-01-27 11:57:43 +0100

    BC-8551 - readd and reduce entities to `files-storage-test.module.ts`

--- apps/server/src/modules/files-storage/files-storage-test.module.ts
+++ apps/server/src/modules/files-storage/files-storage-test.module.ts
@@ -2,14 +2,61 @@ import { CoreModule } from '@core/core.module';
 import { LoggerModule } from '@core/logger';
 import { MongoDatabaseModuleOptions, MongoMemoryDatabaseModule } from '@infra/database';
 import { RabbitMQWrapperTestModule } from '@infra/rabbitmq';
+import { AccountEntity } from '@modules/account/domain/entity/account.entity';
+import { ClassEntity } from '@modules/class/entity';
+import { GroupEntity } from '@modules/group/entity';
+import { SchoolSystemOptionsEntity } from '@modules/legacy-school/entity';
+import { SystemEntity } from '@modules/system/entity';
 import { DynamicModule, Module } from '@nestjs/common';
 import { defaultMikroOrmOptions } from '@shared/common/defaultMikroOrmOptions';
+import {
+	CountyEmbeddable,
+	Course,
+	CourseGroup,
+	FederalStateEntity,
+	LessonEntity,
+	Material,
+	Role,
+	SchoolEntity,
+	SchoolRolePermission,
+	SchoolRoles,
+	SchoolYearEntity,
+	StorageProviderEntity,
+	Submission,
+	Task,
+	User,
+	UserLoginMigrationEntity,
+} from '@shared/domain/entity';
 import { FileRecord } from './entity';
 import { FilesStorageApiModule } from './files-storage-api.app.module';
 
+const ENTITIES = [
+	AccountEntity,
+	ClassEntity,
+	CountyEmbeddable,
+	Course,
+	CourseGroup,
+	FederalStateEntity,
+	GroupEntity,
+	LessonEntity,
+	Material,
+	Role,
+	SchoolEntity,
+	SchoolRoles,
+	SchoolRolePermission,
+	SchoolSystemOptionsEntity,
+	SchoolYearEntity,
+	StorageProviderEntity,
+	SystemEntity,
+	Submission,
+	Task,
+	User,
+	UserLoginMigrationEntity,
+];
+
 const imports = [
 	FilesStorageApiModule,
-	MongoMemoryDatabaseModule.forRoot({ entities: [FileRecord] }),
+	MongoMemoryDatabaseModule.forRoot({ entities: [...ENTITIES, FileRecord] }),
 	RabbitMQWrapperTestModule,
 	CoreModule,
 	LoggerModule,
