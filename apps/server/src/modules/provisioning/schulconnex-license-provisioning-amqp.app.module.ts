e055cd0ad7543e39d17928f70173278305ff2189
Marvin Öhlerking <marvin.oehlerking@capgemini.com>
2025-01-29 10:33:33 +0100

    add mikroorm

--- apps/server/src/modules/provisioning/schulconnex-license-provisioning-amqp.app.module.ts
+++ apps/server/src/modules/provisioning/schulconnex-license-provisioning-amqp.app.module.ts
@@ -1,7 +1,11 @@
 import { CoreModule } from '@core/core.module';
+import { DB_PASSWORD, DB_URL, DB_USERNAME } from '@imports-from-feathers';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { createConfigModuleOptions } from '@shared/common/config-module-options';
+import { defaultMikroOrmOptions } from '@shared/common/defaultMikroOrmOptions';
+import { ALL_ENTITIES } from '@shared/domain/entity';
 import { serverConfig } from '../server';
 import { SchulconnexLicenseProvisioningConsumerModule } from './schulconnex-license-provisioning-consumer.module';
 
@@ -9,6 +13,17 @@ import { SchulconnexLicenseProvisioningConsumerModule } from './schulconnex-lice
 	imports: [
 		ConfigModule.forRoot(createConfigModuleOptions(serverConfig)),
 		CoreModule,
+		MikroOrmModule.forRoot({
+			...defaultMikroOrmOptions,
+			type: 'mongo',
+			// TODO add mongoose options as mongo options (see database.js)
+			clientUrl: DB_URL,
+			password: DB_PASSWORD,
+			user: DB_USERNAME,
+			entities: [...ALL_ENTITIES],
+
+			// debug: true, // use it for locally debugging of querys
+		}),
 		SchulconnexLicenseProvisioningConsumerModule,
 	],
 })
