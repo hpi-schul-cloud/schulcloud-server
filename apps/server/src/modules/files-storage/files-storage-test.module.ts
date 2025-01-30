c6379d75dfd1574f0017cea875951c299c406b02
SevenWaysDP <sergej.hoffmann@dataport.de>
2025-01-30 12:23:24 +0100

    Refactor files-storage module to separate test entities for improved organization and maintainability

--- apps/server/src/modules/files-storage/files-storage-test.module.ts
+++ apps/server/src/modules/files-storage/files-storage-test.module.ts
@@ -1,13 +1,12 @@
 import { MongoMemoryDatabaseModule } from '@infra/database';
 import { RabbitMQWrapperTestModule } from '@infra/rabbitmq';
 import { Module } from '@nestjs/common';
-import { User } from '@shared/domain/entity';
 import { FilesStorageApiModule } from './files-storage-api.app.module';
-import { ENTITIES } from './files-storage.entity.imports';
+import { TEST_ENTITIES } from './files-storage.entity.imports';
 
 const imports = [
 	FilesStorageApiModule,
-	MongoMemoryDatabaseModule.forRoot({ entities: [...ENTITIES, User] }),
+	MongoMemoryDatabaseModule.forRoot({ entities: TEST_ENTITIES }),
 	RabbitMQWrapperTestModule,
 ];
 const controllers = [];
