e3b827afb6d67415bb2f32627fefecdab61f459f
SevenWaysDP <sergej.hoffmann@dataport.de>
2025-01-30 12:20:49 +0100

    Refactor board module to centralize entity imports into a dedicated file for improved organization and maintainability

--- apps/server/src/modules/board/board-collaboration.app.module.ts
+++ apps/server/src/modules/board/board-collaboration.app.module.ts
@@ -4,114 +4,16 @@ import { AuthGuardModule, AuthGuardOptions } from '@infra/auth-guard';
 import { MongoMemoryDatabaseModule } from '@infra/database';
 import { RabbitMQWrapperModule } from '@infra/rabbitmq';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { AccountEntity } from '@modules/account/domain/entity/account.entity';
-import { BoardNodeEntity } from '@modules/board/repo/entity';
-import { ClassEntity } from '@modules/class/entity';
-import { DeletionLogEntity } from '@modules/deletion/repo/entity/deletion-log.entity';
-import { DeletionRequestEntity } from '@modules/deletion/repo/entity/deletion-request.entity';
-import { GroupEntity } from '@modules/group/entity';
-import { InstanceEntity } from '@modules/instance';
-import { SchoolSystemOptionsEntity } from '@modules/legacy-school/entity';
-import { MediaSourceEntity } from '@modules/media-source/entity';
-import { OauthSessionTokenEntity } from '@modules/oauth/entity';
-import { ExternalToolPseudonymEntity, PseudonymEntity } from '@modules/pseudonym/entity';
-import { RegistrationPinEntity } from '@modules/registration-pin/entity';
-import { RocketChatUserEntity } from '@modules/rocketchat-user/entity';
-import { RoomMembershipEntity } from '@modules/room-membership/repo/entity/room-membership.entity';
-import { RoomEntity } from '@modules/room/repo/entity';
-import { MediaSchoolLicenseEntity, SchoolLicenseEntity } from '@modules/school-license/entity';
-import { ShareToken } from '@modules/sharing/entity/share-token.entity';
-import { SystemEntity } from '@modules/system/entity/system.entity';
-import { ContextExternalToolEntity, LtiDeepLinkTokenEntity } from '@modules/tool/context-external-tool/entity';
-import { ExternalToolEntity } from '@modules/tool/external-tool/entity';
-import { SchoolExternalToolEntity } from '@modules/tool/school-external-tool/entity';
-import { ImportUser } from '@modules/user-import/entity';
-import { MediaUserLicenseEntity, UserLicenseEntity } from '@modules/user-license/entity';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { createConfigModuleOptions } from '@shared/common/config-module-options';
 import { defaultMikroOrmOptions } from '@shared/common/defaultMikroOrmOptions';
-import { ColumnBoardNode } from '@shared/domain/entity/column-board-node.entity';
-import { Course } from '@shared/domain/entity/course.entity';
-import { CourseGroup } from '@shared/domain/entity/coursegroup.entity';
-import { DashboardGridElementModel, DashboardModelEntity } from '@shared/domain/entity/dashboard.model.entity';
-import { CountyEmbeddable, FederalStateEntity } from '@shared/domain/entity/federal-state.entity';
-import { LessonEntity } from '@shared/domain/entity/lesson.entity';
-import { LtiTool } from '@shared/domain/entity/ltitool.entity';
-import { Material } from '@shared/domain/entity/materials.entity';
-import { CourseNews, News, SchoolNews, TeamNews } from '@shared/domain/entity/news.entity';
-import { Role } from '@shared/domain/entity/role.entity';
-import { SchoolEntity, SchoolRolePermission, SchoolRoles } from '@shared/domain/entity/school.entity';
-import { SchoolYearEntity } from '@shared/domain/entity/schoolyear.entity';
-import { StorageProviderEntity } from '@shared/domain/entity/storageprovider.entity';
-import { Submission } from '@shared/domain/entity/submission.entity';
-import { Task } from '@shared/domain/entity/task.entity';
-import { TeamEntity, TeamUserEntity } from '@shared/domain/entity/team.entity';
-import { UserLoginMigrationEntity } from '@shared/domain/entity/user-login-migration.entity';
-import { User } from '@shared/domain/entity/user.entity';
-import { VideoConference } from '@shared/domain/entity/video-conference.entity';
 import { AuthorizationModule } from '../authorization';
 import { serverConfig } from '../server';
 import { config } from './board-collaboration.config';
 import { BoardWsApiModule } from './board-ws-api.module';
+import { ENTITIES, TEST_ENTITIES } from './board.entity.imports';
 import { BoardModule } from './board.module';
-
-export const ENTITIES = [
-	AccountEntity,
-	BoardNodeEntity,
-	ClassEntity,
-	ColumnBoardNode,
-	ContextExternalToolEntity,
-	CountyEmbeddable,
-	Course,
-	CourseGroup,
-	CourseNews,
-	DashboardGridElementModel,
-	DashboardModelEntity,
-	DeletionLogEntity,
-	DeletionRequestEntity,
-	ExternalToolEntity,
-	ExternalToolPseudonymEntity,
-	FederalStateEntity,
-	GroupEntity,
-	ImportUser,
-	InstanceEntity,
-	LessonEntity,
-	LtiDeepLinkTokenEntity,
-	LtiTool,
-	Material,
-	MediaSchoolLicenseEntity,
-	MediaSourceEntity,
-	MediaUserLicenseEntity,
-	News,
-	OauthSessionTokenEntity,
-	PseudonymEntity,
-	RegistrationPinEntity,
-	RocketChatUserEntity,
-	Role,
-	RoomEntity,
-	RoomMembershipEntity,
-	SchoolEntity,
-	SchoolExternalToolEntity,
-	SchoolLicenseEntity,
-	SchoolNews,
-	SchoolRolePermission,
-	SchoolRoles,
-	SchoolSystemOptionsEntity,
-	SchoolYearEntity,
-	ShareToken,
-	StorageProviderEntity,
-	Submission,
-	SystemEntity,
-	Task,
-	TeamEntity,
-	TeamNews,
-	TeamUserEntity,
-	User,
-	UserLicenseEntity,
-	UserLoginMigrationEntity,
-	VideoConference,
-];
 
 const imports = [
 	CoreModule,
@@ -150,7 +52,7 @@ const testConfig = () => {
 		ConfigModule.forRoot(createConfigModuleOptions(testConfig)),
 		MongoMemoryDatabaseModule.forRoot({
 			...defaultMikroOrmOptions,
-			entities: ENTITIES,
+			entities: TEST_ENTITIES,
 		}),
 	],
 })
