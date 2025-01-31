9b3ba924c40f3cbb20a3d7b154aad3db8241cf1c
Max Bischof <maximilian.bischof@dataport.de>
2025-01-31 08:48:41 +0100

    Add entities to setupEntities

--- apps/server/src/testing/setup-entities.ts
+++ apps/server/src/testing/setup-entities.ts
@@ -1,11 +1,29 @@
 import { AnyEntity, EntityClass, MikroORM } from '@mikro-orm/core';
+import {
+	ColumnBoardBoardElement,
+	ColumnBoardNode,
+	LegacyBoard,
+	LegacyBoardElement,
+	LessonBoardElement,
+	TaskBoardElement,
+} from '@modules/learnroom/repo';
 import { ALL_ENTITIES } from '@shared/domain/entity';
 
 /**
  * Test-Setup to make all entities available without a database connection.
  * @returns
  */
-export const setupEntities = async (entities: EntityClass<AnyEntity>[] = ALL_ENTITIES): Promise<MikroORM> => {
+export const setupEntities = async (
+	entities: EntityClass<AnyEntity>[] = [
+		...ALL_ENTITIES,
+		ColumnBoardBoardElement,
+		ColumnBoardNode,
+		LegacyBoard,
+		LegacyBoardElement,
+		LessonBoardElement,
+		TaskBoardElement,
+	]
+): Promise<MikroORM> => {
 	const orm = await MikroORM.init({
 		type: 'mongo',
 		dbName: 'dummy',
