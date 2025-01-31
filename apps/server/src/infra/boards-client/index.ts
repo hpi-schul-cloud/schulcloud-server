1de76b69525bf324a2627f7e9567fc4ac60f854f
Patrick Sachmann <20001160+psachmann@users.noreply.github.com>
2025-01-31 12:47:25 +0100

    EW-1071 Common Cartridge microservice creates boards during import (#5435)
    * creating new boards client
    * updating courses client
    * removing old boards client

---
+++ apps/server/src/infra/boards-client/index.ts
@@ -0,0 +1,4 @@
+export { BoardsClientAdapter } from './boards-client.adapter';
+export { BoardsClientConfig } from './boards-client.config';
+export { BoardsClientModule } from './boards-client.module';
+export type * from './generated/models';
