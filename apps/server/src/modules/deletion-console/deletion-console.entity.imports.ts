7690a351c577b89cb9d3ffd6fcb37c4b999c0af7
SevenWaysDP <sergej.hoffmann@dataport.de>
2025-02-04 16:51:03 +0100

    check entities in deletion-console entity imports

--- apps/server/src/modules/deletion-console/deletion-console.entity.imports.ts
+++ apps/server/src/modules/deletion-console/deletion-console.entity.imports.ts
@@ -1,7 +1,6 @@
 import { AccountEntity } from '@modules/account/domain/entity/account.entity';
 import { User } from '@shared/domain/entity';
 import { Role } from '@shared/domain/entity/role.entity';
-import { FileEntity } from '../files/entity';
 
-export const ENTITIES = [Role, FileEntity, User, AccountEntity];
+export const ENTITIES = [Role, User, AccountEntity];
 export const TEST_ENTITIES = [...ENTITIES];
