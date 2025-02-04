edd6a39509c96f05451a5f1890dcd539736a98fe
SevenWaysDP <sergej.hoffmann@dataport.de>
2025-02-04 16:56:38 +0100

    Add missing entity imports to admin-api-server.entity.imports.ts

--- apps/server/src/modules/server/admin-api-server.entity.imports.ts
+++ apps/server/src/modules/server/admin-api-server.entity.imports.ts
@@ -1,18 +1,29 @@
 import { AccountEntity } from '@modules/account/domain/entity/account.entity';
+import { BoardNodeEntity } from '@modules/board/repo';
 import { ClassEntity } from '@modules/class/entity';
 import { DeletionLogEntity, DeletionRequestEntity } from '@modules/deletion/repo/entity';
+import { FileEntity } from '@modules/files/entity';
 import { GroupEntity } from '@modules/group/entity';
-import { ContextExternalToolEntity } from '@modules/tool/context-external-tool/entity';
+import { ExternalToolPseudonymEntity, PseudonymEntity } from '@modules/pseudonym/entity';
+import { RegistrationPinEntity } from '@modules/registration-pin/entity';
+import { RocketChatUserEntity } from '@modules/rocketchat-user/entity';
+import { RoomMembershipEntity } from '@modules/room-membership';
+import { ContextExternalToolEntity, LtiDeepLinkTokenEntity } from '@modules/tool/context-external-tool/entity';
 import { ExternalToolEntity } from '@modules/tool/external-tool/entity';
 import { SchoolExternalToolEntity } from '@modules/tool/school-external-tool/entity';
 import {
 	Course,
 	CourseGroup,
+	CourseNews,
 	FederalStateEntity,
+	News,
 	Role,
 	SchoolEntity,
+	SchoolNews,
 	SchoolYearEntity,
 	StorageProviderEntity,
+	TeamEntity,
+	TeamNews,
 	User,
 } from '@shared/domain/entity';
 
@@ -33,6 +44,19 @@ export const ENTITIES = [
 	ExternalToolEntity,
 	ContextExternalToolEntity,
 	SchoolExternalToolEntity,
+	FileEntity,
+	CourseNews,
+	News,
+	SchoolNews,
+	TeamNews,
+	TeamEntity,
+	PseudonymEntity,
+	ExternalToolPseudonymEntity,
+	RocketChatUserEntity,
+	RegistrationPinEntity,
+	LtiDeepLinkTokenEntity,
+	BoardNodeEntity,
+	RoomMembershipEntity,
 ];
 
 export const TEST_ENTITIES = [...ENTITIES];
