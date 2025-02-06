b6f8c9e2d548fd981725e308e6aad601e47e475f
hoeppner.dataport <marcus.hoeppner@dataport.de>
2025-02-06 13:53:57 +0100

    remove ROOM_LEAVE permission from role RoomViewer

--- apps/server/src/migrations/mikro-orm/Migration20250205101112.ts
+++ apps/server/src/migrations/mikro-orm/Migration20250205101112.ts
@@ -31,21 +31,6 @@ export class Migration20250205101112 extends Migration {
 		if (editorRoleUpdate.modifiedCount > 0) {
 			console.info('Permission ROOM_LEAVE added to role roomeditor.');
 		}
-
-		const viewerRoleUpdate = await this.getCollection('roles').updateOne(
-			{ name: 'roomviewer' },
-			{
-				$addToSet: {
-					permissions: {
-						$each: ['ROOM_LEAVE'],
-					},
-				},
-			}
-		);
-
-		if (viewerRoleUpdate.modifiedCount > 0) {
-			console.info('Permission ROOM_LEAVE added to role roomviewer.');
-		}
 	}
 
 	public async down(): Promise<void> {
@@ -77,21 +62,6 @@ export class Migration20250205101112 extends Migration {
 
 		if (editorRoleUpdate.modifiedCount > 0) {
 			console.info('Rollback: Permission ROOM_LEAVE added to role roomeditor.');
-		}
-
-		const viewerRoleUpdate = await this.getCollection('roles').updateOne(
-			{ name: 'roomviewer' },
-			{
-				$pull: {
-					permissions: {
-						$in: ['ROOM_LEAVE'],
-					},
-				},
-			}
-		);
-
-		if (viewerRoleUpdate.modifiedCount > 0) {
-			console.info('Rollback: Permission ROOM_LEAVE added to role roomviewer.');
 		}
 	}
 }
