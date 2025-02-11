2e9aa723fe76cedede33b0bd282afc25fece5b3a
Max <53796487+dyedwiper@users.noreply.github.com>
2025-02-11 12:41:54 +0100

    BC-8780 Replace request-promise (part 1) (#5501)

--- src/services/calendar/courseCalendar.js
+++ src/services/calendar/courseCalendar.js
@@ -1,4 +1,4 @@
-const request = require('request-promise-native');
+const axios = require('axios');
 
 const { Configuration } = require('@hpi-schul-cloud/commons');
 const hooks = require('./hooks');
@@ -12,19 +12,18 @@ class CourseCalendarService {
 	remove(id, params) {
 		const userId = (params.query || {}).userId || (params.account || {}).userId || params.payload.userId;
 		const options = {
-			uri: `${calendarUri}/scopes/${id}`,
+			url: `${calendarUri}/scopes/${id}`,
+			method: 'DELETE',
 			headers: {
 				Authorization: userId,
 			},
-			json: true,
-			method: 'DELETE',
 			timeout: Configuration.get('REQUEST_OPTION__TIMEOUT_MS'),
-			body: { data: [{ type: 'event' }] },
+			data: { data: [{ type: 'event' }] },
 		};
 
-		return request(options).then((res) => {
+		return axios(options).then((res) => {
 			// calendar returns nothing if event was successfully deleted
-			if (!res) return { message: 'Successful deleted event' };
+			if (!res.data) return { message: 'Successful deleted event' };
 			return res;
 		});
 	}
