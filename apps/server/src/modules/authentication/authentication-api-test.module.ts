3291510d3182b61022cf43e430761bc1cd47b093
Steliyan Dinkov <steliyan.dinkov@capgemini.com>
2025-02-12 09:26:50 +0100

    update module imports - include logger module

--- apps/server/src/modules/authentication/authentication-api-test.module.ts
+++ apps/server/src/modules/authentication/authentication-api-test.module.ts
@@ -1,3 +1,4 @@
+import { LoggerModule } from '@core/logger';
 import { Module } from '@nestjs/common';
 import { AuthenticationTestModule } from './authentication-test.module';
 import { LoginController, LogoutController } from './controllers';
@@ -5,7 +6,7 @@ import { LoginUc, LogoutUc } from './uc';
 
 // This module is for use in api tests of other apps than the core server.
 @Module({
-	imports: [AuthenticationTestModule],
+	imports: [AuthenticationTestModule, LoggerModule],
 	providers: [LoginUc, LogoutUc],
 	controllers: [LoginController, LogoutController],
 })
--- apps/server/src/modules/authentication/authentication-api.module.ts
+++ apps/server/src/modules/authentication/authentication-api.module.ts
@@ -1,5 +1,5 @@
-import { LoggerModule } from '@core/logger';
 import { Module } from '@nestjs/common';
+import { LoggerModule } from '@core/logger';
 import { AuthenticationModule } from './authentication.module';
 import { LoginController, LogoutController } from './controllers';
 import { LoginUc, LogoutUc } from './uc';
