eebccc456780fae20f9567005435b065c0112a9e
mamutmk5 <3045922+mamutmk5@users.noreply.github.com>
2025-02-13 12:01:09 +0100

    BC-8749 - back to work

--- apps/server/src/modules/tool/external-tool/entity/config/oauth2-tool-config.entity.ts
+++ apps/server/src/modules/tool/external-tool/entity/config/oauth2-tool-config.entity.ts
@@ -1,10 +1,10 @@
-import { Embeddable, Property, Unique } from '@mikro-orm/core';
+import { Embeddable, Property } from '@mikro-orm/core';
+import { Embedded, Entity, Index, Property } from '@mikro-orm/core';
 import { ExternalToolConfigEntity } from './external-tool-config.entity';
 import { ToolConfigType } from '../../../common/enum';
 
 @Embeddable({ discriminatorValue: ToolConfigType.OAUTH2 })
 export class Oauth2ToolConfigEntity extends ExternalToolConfigEntity {
-	@Unique({ options: { sparse: true } })
 	@Property()
 	clientId: string;
 
