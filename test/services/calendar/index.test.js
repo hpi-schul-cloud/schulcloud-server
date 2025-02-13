8ec904749259883ecf83dbb05ebcadca26c2cdd4
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2025-02-13 10:03:26 +0100

    Fix tests

--- test/services/calendar/index.test.js
+++ test/services/calendar/index.test.js
@@ -4,35 +4,37 @@ const appPromise = require('../../../src/app');
 
 const { expect } = chai;
 
-const mockRequest = async (options) => {
-	const exampleResponse = {
-		links: {
-			self: 'https://dbildungscloud.de:3000/events',
-		},
-		data: [
-			{
-				type: 'event',
-				id: '0ef82d0a-0e20-465d-ae1e-850dc04d9432',
-				attributes: {
-					summary: 'tttttt',
-					location: 'Paul-Gerhardt-Gymnasium',
-					description: 'sdds',
-					dtstart: '2017-05-19T20:00:00.000Z',
-					dtend: '2017-05-19T20:00:00.000Z',
-					dtstamp: '2017-05-10T06:44:24.174Z',
-					uid: 'e40e1276-4a27-458c-8b64-ae549adadbc2',
-				},
-				relationships: {
-					'scope-ids': ['0000d231816abba584714c9e'],
-				},
+const mockRequest = (options) => {
+	const calendarResponse = {
+		data: {
+			links: {
+				self: 'https://dbildungscloud.de:3000/events',
 			},
-		],
+			data: [
+				{
+					type: 'event',
+					id: '0ef82d0a-0e20-465d-ae1e-850dc04d9432',
+					attributes: {
+						summary: 'tttttt',
+						location: 'Paul-Gerhardt-Gymnasium',
+						description: 'sdds',
+						dtstart: '2017-05-19T20:00:00.000Z',
+						dtend: '2017-05-19T20:00:00.000Z',
+						dtstamp: '2017-05-10T06:44:24.174Z',
+						uid: 'e40e1276-4a27-458c-8b64-ae549adadbc2',
+					},
+					relationships: {
+						'scope-ids': ['0000d231816abba584714c9e'],
+					},
+				},
+			],
+		},
 	};
 
-	return exampleResponse;
+	return calendarResponse;
 };
 
-describe('calendar service', () => {
+describe.only('calendar service', () => {
 	let server;
 	let calendarService;
 
