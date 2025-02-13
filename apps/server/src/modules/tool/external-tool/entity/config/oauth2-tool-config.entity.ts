1b3e916428892f458c33870c6ae4dd81fc4a17b3
mamutmk5 <3045922+mamutmk5@users.noreply.github.com>
2025-02-13 11:06:32 +0100

    BC-8749 - back to old way

--- apps/server/src/modules/tool/external-tool/entity/config/oauth2-tool-config.entity.ts
+++ apps/server/src/modules/tool/external-tool/entity/config/oauth2-tool-config.entity.ts
@@ -1,11 +1,9 @@
-import { Embeddable, Property, Index, Unique } from '@mikro-orm/core';
+import { Embeddable, Property } from '@mikro-orm/core';
 import { ExternalToolConfigEntity } from './external-tool-config.entity';
 import { ToolConfigType } from '../../../common/enum';
 
 @Embeddable({ discriminatorValue: ToolConfigType.OAUTH2 })
 export class Oauth2ToolConfigEntity extends ExternalToolConfigEntity {
-	@Index()
-	@Unique({options: { sparse: true }})
 	@Property()
 	clientId: string;
 
