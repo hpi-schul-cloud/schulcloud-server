df6016d9e39d543afb064198f523d52f42c7c634
Max Bischof <maximilian.bischof@dataport.de>
2025-02-14 14:09:55 +0100

    Fix import

--- apps/server/src/modules/school-license/uc/media-school-license.uc.spec.ts
+++ apps/server/src/modules/school-license/uc/media-school-license.uc.spec.ts
@@ -1,10 +1,10 @@
-import { Permission } from '@aws-sdk/client-s3';
 import { createMock, DeepMocked } from '@golevelup/ts-jest';
 import { AuthorizationService } from '@modules/authorization';
 import { schoolEntityFactory } from '@modules/school/testing';
 import { UnauthorizedException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { User } from '@shared/domain/entity';
+import { Permission } from '@shared/domain/interface';
 import { setupEntities } from '@testing/database';
 import { userFactory } from '@testing/factory/user.factory';
 import { MediaSchoolLicenseService } from '../service';
