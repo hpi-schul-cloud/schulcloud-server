375681843e4e79002e8725e960f8be9767872a26
Firas Shmit <firas.shmit@capgemini.com>
2025-02-21 16:26:31 +0100

    EW-1151 modified constructor of lernstore component

--- apps/server/src/modules/common-cartridge/common-cartridge-client/lesson-client/dto/component-lernstore-props.dto.ts
+++ apps/server/src/modules/common-cartridge/common-cartridge-client/lesson-client/dto/component-lernstore-props.dto.ts
@@ -2,9 +2,12 @@ import { ComponentLernstorePropsImpl } from '../lessons-api-client';
 import { LernstoreResourcesDto } from './lernstore-resources.dto';
 
 export class ComponentLernstorePropsDto {
-	public resources: LernstoreResourcesDto[];
+	public resources: LernstoreResourcesDto[] = [];
 
 	constructor(lernstoreContent: ComponentLernstorePropsImpl) {
+		if (!lernstoreContent) {
+			return;
+		}
 		this.resources = lernstoreContent.resources
 			? lernstoreContent.resources.map((resource) => new LernstoreResourcesDto(resource))
 			: [];
