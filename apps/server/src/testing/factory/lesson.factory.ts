341362baa95085f82012f70ae6b9ded8e692a774
SevenWaysDP <sergej.hoffmann@dataport.de>
2025-02-27 07:25:57 +0100

    Refactor lesson factory to remove unused ComponentProperties import and simplify contents initialization

--- apps/server/src/testing/factory/lesson.factory.ts
+++ apps/server/src/testing/factory/lesson.factory.ts
@@ -1,23 +1,16 @@
 // Remove the eslint-disable after fixing the import issue in EPIC-96
 // eslint-disable-next-line @typescript-eslint/no-restricted-imports
 import { courseEntityFactory } from '@modules/course/testing';
-import { ComponentProperties, LessonEntity, LessonProperties } from '@shared/domain/entity';
+import { LessonEntity, LessonProperties } from '@shared/domain/entity';
 import { BaseFactory } from './base.factory';
 
 class LessonFactory extends BaseFactory<LessonEntity, LessonProperties> {}
 
 export const lessonFactory = LessonFactory.define(LessonEntity, ({ sequence }) => {
-	const contents: ComponentProperties[] = [];
-	if (contents) {
-		contents.forEach((element) => {
-			contents.push(element);
-		});
-	}
-
 	return {
 		name: `lesson #${sequence}`,
 		course: courseEntityFactory.build(),
-		contents,
+		contents: [],
 		hidden: false,
 		materials: [],
 	};
