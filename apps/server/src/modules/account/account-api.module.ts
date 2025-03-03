0f23ff2b45b988f455c55a0851bde355e2a45696
Max Bischof <maximilian.bischof@dataport.de>
2025-03-03 15:13:58 +0100

    Add user module to AccountApiModule

--- apps/server/src/modules/account/account-api.module.ts
+++ apps/server/src/modules/account/account-api.module.ts
@@ -1,12 +1,13 @@
 import { LoggerModule } from '@core/logger';
 import { AuthorizationModule } from '@modules/authorization';
+import { UserModule } from '@modules/user';
 import { Module } from '@nestjs/common';
 import { AccountModule } from './account.module';
 import { AccountController } from './api/account.controller';
 import { AccountUc } from './api/account.uc';
 
 @Module({
-	imports: [AccountModule, LoggerModule, AuthorizationModule],
+	imports: [AccountModule, LoggerModule, AuthorizationModule, UserModule],
 	providers: [AccountUc],
 	controllers: [AccountController],
 	exports: [],
