346686cf273068f8df769edd2fb530af3bdac81a
hoeppner-dataport <106819770+hoeppner-dataport@users.noreply.github.com>
2025-03-06 19:53:24 +0100

    BC-8980 - remove eslint-airbnb (#5549)
    Remove deprecated dependencies that block migration to ESLint v9
    * remove dependency eslint-config-airbnb-base
    * remove dependency eslint-config-airbnb-typescript
    * add additional eslint-configuration holding the configuration of removed dependencies
    * add npm script to dump eslint config
    * adapt eslintignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,7 @@
+const airbnbRules = require('./.eslint-rules-airbnb.js');
+
 module.exports = {
-	extends: ['airbnb-base', 'prettier', 'plugin:promise/recommended'],
+	extends: ['prettier', 'plugin:promise/recommended'],
 	rules: {
 		'prettier/prettier': ['error'],
 		'no-process-env': 'error',
@@ -18,12 +20,6 @@ module.exports = {
 			'error',
 			{
 				allow: ['_id', '_v', '__v'],
-			},
-		],
-		'no-shadow': [
-			'error',
-			{
-				allow: ['err', 'error'],
 			},
 		],
 		'prefer-destructuring': [
@@ -74,10 +70,14 @@ module.exports = {
 			parserOptions: {
 				project: 'apps/server/tsconfig.lint.json',
 				sourceType: 'module',
+				ecmaVersion: 2018,
+				ecmaFeatures: {
+					generators: false,
+					objectLiteralDuplicateProperties: false,
+				},
 			},
 			plugins: ['@typescript-eslint/eslint-plugin', 'import'],
 			extends: [
-				'airbnb-typescript/base',
 				'plugin:@typescript-eslint/recommended',
 				'plugin:@typescript-eslint/recommended-requiring-type-checking',
 				'prettier',
@@ -85,11 +85,9 @@ module.exports = {
 				'plugin:import/typescript',
 			],
 			rules: {
-				'import/no-unresolved': 'off', // better handled by ts resolver
-				'import/no-extraneous-dependencies': 'off', // better handles by ts resolver
+				...airbnbRules,
 				'import/prefer-default-export': 'off',
 				'no-void': ['error', { allowAsStatement: true }],
-				'class-methods-use-this': 'off',
 				'no-param-reassign': 'off',
 				'no-underscore-dangle': 'off',
 				'filename-rules/match': [1, /^([a-z0-9]+-)*[a-z]+(?:\..*)?$/],
@@ -110,7 +108,6 @@ module.exports = {
 						},
 					},
 				],
-				'@typescript-eslint/no-unused-vars': 'error',
 				'@typescript-eslint/no-empty-interface': [
 					'error',
 					{
