1441d683ad3973bbcbbf72a4b73a3c7015ca78dc
Steliyan Dinkov <steliyan.dinkov@capgemini.com>
2025-03-10 14:20:07 +0100

    rm class

--- apps/server/src/modules/tool/external-tool/controller/dto/request/external-tool-metadata.params.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class ExternalToolMetadataParams {
-	@ApiProperty({ nullable: false, required: true })
-	mediumId!: string;
-
-	@ApiProperty({ nullable: false, required: true })
-	mediaSourceId!: string;
-}
