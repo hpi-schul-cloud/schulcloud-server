842744cfa4155f0f3b83726564bc32e776309125
hoeppner.dataport <marcus.hoeppner@dataport.de>
2025-03-10 16:04:33 +0100

    chore: works for newly created users, too

--- apps/server/src/modules/room/api/room.uc.ts
+++ apps/server/src/modules/room/api/room.uc.ts
@@ -122,7 +122,7 @@ export class RoomUc {
 		});
 
 		const userIds = roomMembershipAuthorizable.members.map((member) => member.userId);
-		const users = (await this.userService.findByIds(userIds)).filter((user) => user.deletedAt === undefined);
+		const users = (await this.userService.findByIds(userIds)).filter((user) => !user.deletedAt);
 
 		const memberResponses = users.map((user) => {
 			const member = roomMembershipAuthorizable.members.find((item) => item.userId === user.id);
