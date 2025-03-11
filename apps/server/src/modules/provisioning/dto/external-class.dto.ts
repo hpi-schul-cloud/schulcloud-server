bed76a70f04e3ab9d5ac0ed2469762f96157464c
mkreuzkam-cap <144103168+mkreuzkam-cap@users.noreply.github.com>
2025-03-11 12:53:28 +0100

    EW-1164: Fix class names and student class grouping. (#5580)
    * Fix class names and student class grouping.
    
    * Add tests.

--- apps/server/src/modules/provisioning/dto/external-class.dto.ts
+++ apps/server/src/modules/provisioning/dto/external-class.dto.ts
@@ -3,8 +3,11 @@ export class ExternalClassDto {
 
 	public readonly name?: string;
 
+	public readonly gradeLevel?: number;
+
 	constructor(props: Readonly<ExternalClassDto>) {
 		this.externalId = props.externalId;
 		this.name = props.name;
+		this.gradeLevel = props.gradeLevel;
 	}
 }
