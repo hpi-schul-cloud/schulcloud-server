df612dbf0d0f9cc801767e551fa1c1ecc89bb205
Constantin Bergatt <constantin.bergatt@dataport.de>
2025-03-12 13:12:09 +0100

    BC-9229 - rename `user-do.ts` to `user.do.ts` once again

--- apps/server/src/modules/user/domain/do/index.ts
+++ apps/server/src/modules/user/domain/do/index.ts
@@ -1,5 +1,5 @@
 export { Consent } from './consent';
 export { ParentConsent } from './parent-consent';
 export { UserConsent } from './user-consent';
-export { SecondarySchoolReference, UserDo } from './user-do';
 export { UserSourceOptions } from './user-source-options';
+export { SecondarySchoolReference, UserDo } from './user.do';
