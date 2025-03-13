017339339f0971ccfc3d6d69250c7fae82ca1b00
Steliyan Dinkov <steliyan.dinkov@capgemini.com>
2025-03-13 16:25:10 +0100

    fix module import

--- apps/server/src/modules/media-source-sync/index.ts
+++ apps/server/src/modules/media-source-sync/index.ts
@@ -1,3 +1,4 @@
-export * from './types';
+export { MediaSourceSyncOperationReport, MediaSourceSyncReport } from './interface';
+export * from './media-source-sync.module';
 export { MediaSourceSyncService } from './service';
-export { MediaSourceSyncReport, MediaSourceSyncOperationReport } from './interface';
+export * from './types';
