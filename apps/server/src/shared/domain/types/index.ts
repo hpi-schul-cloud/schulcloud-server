2d1f107714d24137d91148840b0612b251a9a512
Constantin Bergatt <constantin.bergatt@dataport.de>
2025-03-14 10:26:01 +0100

    BC-8561 - move "richt-text.types" to "domain/type" folder of "task" module

--- apps/server/src/shared/domain/types/index.ts
+++ apps/server/src/shared/domain/types/index.ts
@@ -2,7 +2,6 @@ export { Constructor } from './constructor';
 export * from './counted';
 export * from './entity-id';
 export * from './input-format.types';
-export * from './rich-text.types';
 export * from './schulcloud-theme.enum';
 export * from './system.type';
 export * from './value-of';
