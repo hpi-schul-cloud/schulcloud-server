b656ec52e3f9010e4669f15a93eea9cd451b10d7
hoeppner.dataport <marcus.hoeppner@dataport.de>
2025-03-18 11:46:01 +0100

    add new permission ROOM_CONTENT_EDIT

---
+++ apps/server/src/migrations/mikro-orm/Migration20250318105100.ts
@@ -0,0 +1,51 @@
+import { Migration } from '@mikro-orm/migrations-mongodb';
+
+export class Migration20250318105100 extends Migration {
+	private async addPermission(roleName: string, permission: string): Promise<void> {
+		const roleUpdate = await this.getCollection('roles').updateOne(
+			{ name: roleName },
+			{
+				$addToSet: {
+					permissions: {
+						$each: [permission],
+					},
+				},
+			}
+		);
+
+		if (roleUpdate.modifiedCount > 0) {
+			console.info(`Permission ${permission} added to role ${roleName}.`);
+		}
+	}
+
+	private async removePermission(roleName: string, permission: string): Promise<void> {
+		const roleUpdate = await this.getCollection('roles').updateOne(
+			{ name: roleName },
+			{
+				$pull: {
+					permissions: {
+						$in: ['ROOM_LEAVE'],
+					},
+				},
+			}
+		);
+
+		if (roleUpdate.modifiedCount > 0) {
+			console.info(`Permission ${permission} removed from role ${roleName}.`);
+		}
+	}
+
+	public async up(): Promise<void> {
+		await this.removePermission('roomeditor', 'ROOM_EDIT');
+		await this.addPermission('roomeditor', 'ROOM_CONTENT_EDIT');
+		await this.addPermission('roomadmin', 'ROOM_CONTENT_EDIT');
+		await this.addPermission('roomowner', 'ROOM_CONTENT_EDIT');
+	}
+
+	public async down(): Promise<void> {
+		await this.addPermission('roomeditor', 'ROOM_EDIT');
+		await this.removePermission('roomeditor', 'ROOM_CONTENT_EDIT');
+		await this.removePermission('roomadmin', 'ROOM_CONTENT_EDIT');
+		await this.removePermission('roomowner', 'ROOM_CONTENT_EDIT');
+	}
+}
