f359cf71288527a579b9d4cded2d3074b48e93b3
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2025-03-21 20:29:25 +0100

    implement test cli

--- apps/server/src/modules/deletion-console/deletion-execution-cmd.spec.ts
+++ apps/server/src/modules/deletion-console/deletion-execution-cmd.spec.ts
@@ -1,76 +1,133 @@
 import { INestApplicationContext } from '@nestjs/common';
 import { NestApplicationContext } from '@nestjs/core';
 import { Test, TestingModule } from '@nestjs/testing';
-import { AbstractBootstrapConsole, BootstrapConsole, ConsoleService } from 'nestjs-console';
+import { AbstractBootstrapConsole, CommonBootstrapConsoleOptions } from 'nestjs-console';
+import { CommandResponse } from 'nestjs-console/dist/interfaces';
 import { DeletionConsoleTestModule } from './deletion-console.app.module';
 import { DeletionExecutionConsole } from './deletion-execution.console';
+import { DeletionExecutionUc } from './uc';
 
 // This is inspired by the test cases in the nestjs-console library.
 // see e.g:
 // - https://github.com/Pop-Code/nestjs-console/blob/master/test/bootstrap.spec.ts
 // - https://github.com/Pop-Code/nestjs-console/blob/master/test/console.spec.ts
 
-export class TestBootstrapConsole extends AbstractBootstrapConsole<TestingModule> {
-	create(): Promise<TestingModule> {
+interface TestCliOptions extends CommonBootstrapConsoleOptions {
+	cliName: string;
+	commandName?: string;
+	subCommandName?: string;
+}
+
+export class TestBootstrapConsole extends AbstractBootstrapConsole<TestingModule, TestCliOptions> {
+	public create(): Promise<TestingModule> {
 		return Test.createTestingModule({
 			imports: [this.options.module],
 		}).compile();
 	}
+
+	public execute(args: string[] = []): Promise<CommandResponse> {
+		if (this.options.commandName) {
+			args.unshift(this.options.commandName);
+		}
+		if (this.options.cliName) {
+			args.unshift(this.options.cliName);
+		}
+		args.unshift(process.argv0);
+		return this.boot(args);
+	}
 }
 
-describe('DeletionExecutionConsole (e2e)', () => {
-	let app: INestApplicationContext;
-	let bootstrap: BootstrapConsole;
-	let consoleService: ConsoleService;
-	let stdoutMock: jest.Mock;
-	let stderrMock: jest.Mock;
-	let logMock: jest.SpyInstance;
+export const createTestCLI = async (
+	options: TestCliOptions,
+	callback?: (app: INestApplicationContext, bootstrap: TestBootstrapConsole) => Promise<void>
+): Promise<{ app: INestApplicationContext; bootstrap: TestBootstrapConsole }> => {
+	const bootstrap = new TestBootstrapConsole(options);
+	const app = await bootstrap.init();
+	await app.init();
+	if (callback) {
+		await callback(app, bootstrap);
+		await app.close();
+	}
+	return { app, bootstrap };
+};
 
-	beforeEach(async () => {
-		bootstrap = new TestBootstrapConsole({
+describe('DeletionExecutionConsole (e2e)', () => {
+	const setup = async () => {
+		const { app, bootstrap } = await createTestCLI({
 			module: DeletionConsoleTestModule,
 			useDecorators: true,
-		});
-		app = await bootstrap.init();
-		await app.init();
-		consoleService = app.get(ConsoleService);
-		stdoutMock = jest.fn();
-		stderrMock = jest.fn();
-		logMock = jest.spyOn(console, 'log').mockImplementation();
-	});
-
-	afterEach(() => {
-		stdoutMock.mockReset();
-		stderrMock.mockReset();
-		logMock.mockReset();
-		consoleService.resetCli();
-	});
-
-	it('Should init the application context', () => {
-		expect(app).toBeInstanceOf(NestApplicationContext);
-		const options = bootstrap.getOptions();
-		expect(options.contextOptions?.logger).toHaveLength(1);
-		expect(options.contextOptions?.logger).toContain('error');
-	});
-
-	it('should execute a command with multiple args', async () => {
-		const cli = consoleService.getCli('execution');
-		cli?.exitOverride().configureOutput({
-			writeOut: stdoutMock,
-			writeErr: stderrMock,
+			cliName: 'deletion-console',
+			commandName: 'execution',
 		});
 
-		const service = app.get(DeletionExecutionConsole);
-		service.triggerDeletionExecution = service.triggerDeletionExecution.bind(service);
-		const mockHandler = jest.spyOn(service, 'triggerDeletionExecution').mockImplementation();
+		return { app, bootstrap };
+	};
 
-		const args = ['trigger', '--limit', '10', '--runFailed', 'true'];
+	describe('module', () => {
+		it('initialize the cli', async () => {
+			const { app, bootstrap } = await setup();
 
-		await bootstrap.boot([process.argv0, 'deletion-console', 'execution', ...args]);
+			const cli = bootstrap.getService().getCli('execution');
 
-		expect(mockHandler).toHaveBeenCalledTimes(1);
-		expect(mockHandler.mock.calls[0][0]).toEqual({ limit: '10', runFailed: 'true' });
+			expect(cli).toBeDefined();
 
-		mockHandler.mockRestore();
+			await app.close();
+		});
+
+		it('Should init the application context', async () => {
+			const { app, bootstrap } = await setup();
+
+			expect(app).toBeInstanceOf(NestApplicationContext);
+			const options = bootstrap.getOptions();
+			expect(options.contextOptions?.logger).toHaveLength(1);
+			expect(options.contextOptions?.logger).toContain('error');
+
+			await app.close();
+		});
+	});
+
+	describe('execution ', () => {
+		const setup = async (
+			callback: (app: INestApplicationContext, bootstrap: TestBootstrapConsole) => Promise<void>
+		) => {
+			await createTestCLI(
+				{
+					module: DeletionConsoleTestModule,
+					useDecorators: true,
+					cliName: 'deletion-console',
+					commandName: 'execution',
+				},
+				callback
+			);
+		};
+
+		it('should execute a command with multiple args', async () => {
+			await setup(async (app, bootstrap) => {
+				const service = app.get(DeletionExecutionConsole);
+				const mockHandler = jest.spyOn(service, 'triggerDeletionExecution').mockImplementation();
+
+				await bootstrap.execute(['trigger', '--limit', '10', '--runFailed', 'true']);
+
+				expect(mockHandler).toHaveBeenCalledTimes(1);
+				expect(mockHandler.mock.calls[0][0]).toEqual({ limit: '10', runFailed: 'true' });
+
+				mockHandler.mockRestore();
+			});
+		});
+
+		it('should call the use case with the correct arguments', async () => {
+			await setup(async (app, bootstrap) => {
+				const uc = app.get(DeletionExecutionUc);
+				const mockUc = jest.spyOn(uc, 'triggerDeletionExecution').mockImplementation();
+
+				const args = ['trigger', '--limit', '10', '--runFailed', 'true'];
+
+				await bootstrap.boot([process.argv0, 'deletion-console', 'execution', ...args]);
+
+				expect(mockUc).toHaveBeenNthCalledWith(1, 10, true);
+
+				mockUc.mockRestore();
+			});
+		});
 	});
 });
