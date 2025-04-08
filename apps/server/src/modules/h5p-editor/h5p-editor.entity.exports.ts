16e63d3d704c50e021e00d561e35068e308dcae0
Marvin Öhlerking <marvin.oehlerking@capgemini.com>
2025-04-08 14:56:15 +0200

    fix h5p app

--- apps/server/src/modules/h5p-editor/h5p-editor.entity.exports.ts
+++ apps/server/src/modules/h5p-editor/h5p-editor.entity.exports.ts
@@ -1,5 +1,5 @@
 import { User } from '@modules/user/repo';
 import { H5PContent, InstalledLibrary } from './entity';
 
-export const ENTITIES = [H5PContent, InstalledLibrary];
-export const TEST_ENTITIES = [...ENTITIES, User];
+export const ENTITIES = [H5PContent, InstalledLibrary, User];
+export const TEST_ENTITIES = [...ENTITIES];
