b75bbbbde199e947c7018e50b189ef092193f09a
Marvin Öhlerking <marvin.oehlerking@capgemini.com>
2025-04-15 11:02:02 +0200

    fix test

--- apps/server/src/modules/board/domain/h5p-element.do.spec.ts
+++ apps/server/src/modules/board/domain/h5p-element.do.spec.ts
@@ -2,16 +2,23 @@ import { ObjectId } from '@mikro-orm/mongodb';
 import { h5pElementFactory } from '../testing';
 import { H5pElement, isH5pElement } from './h5p-element.do';
 
-describe('H5pElement', () => {
-	let h5pElement: H5pElement;
+describe(H5pElement.name, () => {
+	const setup = () => {
+		const contentId = new ObjectId().toHexString();
 
-	beforeEach(() => {
-		h5pElement = h5pElementFactory.build({
-			contentId: new ObjectId().toHexString(),
+		const h5pElement = h5pElementFactory.build({
+			contentId,
 		});
-	});
+
+		return {
+			h5pElement,
+			contentId,
+		};
+	};
 
 	it('should be instance of H5pElement', () => {
+		const { h5pElement } = setup();
+
 		expect(isH5pElement(h5pElement)).toBe(true);
 	});
 
@@ -19,16 +26,24 @@ describe('H5pElement', () => {
 		expect(isH5pElement({})).toBe(false);
 	});
 
-	it('should return contextExternalToolId', () => {
-		expect(h5pElement.contentId).toBe('test-id');
+	it('should return contentId', () => {
+		const { h5pElement, contentId } = setup();
+
+		expect(h5pElement.contentId).toBe(contentId);
 	});
 
-	it('should set contextExternalToolId', () => {
-		h5pElement.contentId = 'new-id';
-		expect(h5pElement.contentId).toBe('new-id');
+	it('should set contentId', () => {
+		const { h5pElement } = setup();
+
+		const newId = new ObjectId().toHexString();
+		h5pElement.contentId = newId;
+
+		expect(h5pElement.contentId).toBe(newId);
 	});
 
 	it('should not have child', () => {
+		const { h5pElement } = setup();
+
 		expect(h5pElement.canHaveChild()).toBe(false);
 	});
 });
