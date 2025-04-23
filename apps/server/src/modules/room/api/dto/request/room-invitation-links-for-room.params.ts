0a295a4747921f3024a31cdf196da79e6e552097
hoeppner.dataport <marcus.hoeppner@dataport.de>
2025-04-23 14:40:18 +0200

    chore: remove unused param object

--- apps/server/src/modules/room/api/dto/request/room-invitation-links-for-room.params.ts
+++
@@ -1,9 +0,0 @@
-import { ApiPropertyOptional } from '@nestjs/swagger';
-import { PaginationParams } from '@shared/controller/dto';
-import { IsInt } from 'class-validator';
-
-export class RoomInvitationLinksForRoomParams extends PaginationParams {
-	@IsInt()
-	@ApiPropertyOptional({ description: 'Page limit, defaults to 1000.' })
-	override limit?: number = 1000;
-}
