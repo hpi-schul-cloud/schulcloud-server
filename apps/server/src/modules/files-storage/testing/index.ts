13f09ea0a113d8cbfdbf2a9ef2623c8da9b3c809
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2025-04-28 09:25:05 +0200

    fix export

--- apps/server/src/modules/files-storage/testing/index.ts
+++ apps/server/src/modules/files-storage/testing/index.ts
@@ -2,4 +2,4 @@ export * from './file-record-security-check.embeddable.factory';
 export * from './file-record.entity.factory';
 export * from './file-record.test.factory';
 export * from './readable-stream-with-file-type.factory';
-export * from './test-helper';
+export * from './get-file.test.factory';
