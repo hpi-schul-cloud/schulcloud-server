eb05421049fdb4df34bb15b89d2be0353a4bcae6
MajedAlaitwniCap <majed.alaitwni@capgemini.com>
2025-04-30 07:44:51 +0200

    adjust mapper tests

--- apps/server/src/modules/common-cartridge/service/common-cartridge-import.mapper.spec.ts
+++ apps/server/src/modules/common-cartridge/service/common-cartridge-import.mapper.spec.ts
@@ -51,111 +51,109 @@ describe('CommonCartridgeImportMapper', () => {
 		});
 	});
 
-	describe('mapToResourceBody', () => {
-		describe('when resource is provided', () => {
-			describe('when resource is webLink', () => {
-				const setup = () => {
-					const resource: CommonCartridgeWebLinkResourceProps = {
-						type: CommonCartridgeResourceTypeV1P1.WEB_LINK,
+	describe('mapResourceToContentBody', () => {
+		describe('when resource is webLink', () => {
+			const setup = () => {
+				const resource: CommonCartridgeWebLinkResourceProps = {
+					type: CommonCartridgeResourceTypeV1P1.WEB_LINK,
+					url: 'https://example.com',
+					title: '',
+				};
+
+				const cardElementProps: CommonCartridgeOrganizationProps = {
+					resourcePath: 'path/to/resource',
+					path: 'path/to/resource',
+					pathDepth: 1,
+					identifier: 'resource-id',
+					title: 'Resource Title',
+					isResource: true,
+					isInlined: false,
+					resourceType: 'webLink',
+				};
+
+				const result = sut.mapResourceToContentBody(resource, cardElementProps);
+
+				return result;
+			};
+
+			it('should return link body', () => {
+				const result = setup();
+				expect(result).toEqual({
+					type: 'link',
+					content: {
 						url: 'https://example.com',
 						title: '',
-					};
-
-					const cardElementProps: CommonCartridgeOrganizationProps = {
-						resourcePath: 'path/to/resource',
-						path: 'path/to/resource',
-						pathDepth: 1,
-						identifier: 'resource-id',
-						title: 'Resource Title',
-						isResource: true,
-						isInlined: false,
-						resourceType: 'webLink',
-					};
-
-					const result = sut.mapResourceToContentBody(resource, cardElementProps);
-
-					return result;
-				};
-
-				it('should return link body', () => {
-					const result = setup();
-					expect(result).toEqual({
-						type: 'link',
-						content: {
-							url: 'https://example.com',
-							title: '',
-							description: '',
-							imageUrl: '',
-							originalImageUrl: '',
-						},
-					});
+						description: '',
+						imageUrl: '',
+						originalImageUrl: '',
+					},
 				});
 			});
+		});
 
-			describe('when resource is webContent', () => {
-				const setup = () => {
-					const resource: CommonCartridgeWebContentResourceProps = {
-						type: CommonCartridgeResourceTypeV1P1.WEB_CONTENT,
-						html: '<p>Test</p>',
-					};
-
-					const cardElementProps: CommonCartridgeOrganizationProps = {
-						resourcePath: 'path/to/resource.html',
-						path: 'path/to/resource',
-						pathDepth: 1,
-						identifier: 'resource-id',
-						title: 'Resource Title',
-						isResource: true,
-						isInlined: false,
-						resourceType: 'webContent',
-					};
-
-					const result = sut.mapResourceToContentBody(resource, cardElementProps);
-
-					return result;
+		describe('when resource is webContent', () => {
+			const setup = () => {
+				const resource: CommonCartridgeWebContentResourceProps = {
+					type: CommonCartridgeResourceTypeV1P1.WEB_CONTENT,
+					html: '<p>Test</p>',
 				};
 
-				it('should return rich text body', () => {
-					const result = setup();
+				const cardElementProps: CommonCartridgeOrganizationProps = {
+					resourcePath: 'path/to/resource.html',
+					path: 'path/to/resource',
+					pathDepth: 1,
+					identifier: 'resource-id',
+					title: 'Resource Title',
+					isResource: true,
+					isInlined: false,
+					resourceType: 'webContent',
+				};
 
-					expect(result).toEqual({
-						type: 'richText',
-						content: {
-							inputFormat: 'richTextCk4',
-							text: '<p>Test</p>',
-						},
-					});
+				const result = sut.mapResourceToContentBody(resource, cardElementProps);
+
+				return result;
+			};
+
+			it('should return rich text body', () => {
+				const result = setup();
+
+				expect(result).toEqual({
+					type: 'richText',
+					content: {
+						inputFormat: 'richTextCk4',
+						text: '<p>Test</p>',
+					},
 				});
 			});
+		});
 
-			describe('when resource is webContent with non-html path', () => {
-				const setup = () => {
-					const resource: CommonCartridgeWebContentResourceProps = {
-						type: CommonCartridgeResourceTypeV1P1.WEB_CONTENT,
-						html: '<p>Test</p>',
-					};
-
-					const cardElementProps: CommonCartridgeOrganizationProps = {
-						resourcePath: 'path/to/resource.jpg',
-						path: 'path/to/resource',
-						pathDepth: 1,
-						identifier: 'resource-id',
-						title: 'Resource Title',
-						isResource: true,
-						isInlined: false,
-						resourceType: 'webContent',
-					};
-
-					const result = sut.mapResourceToContentBody(resource, cardElementProps);
-
-					return result;
+		describe('when resource is webContent with non-html path', () => {
+			const setup = () => {
+				const resource: CommonCartridgeWebContentResourceProps = {
+					type: CommonCartridgeResourceTypeV1P1.WEB_CONTENT,
+					html: '<p>Test</p>',
 				};
 
-				it('should return undefined for unknown resource type', () => {
-					const result = setup();
+				const cardElementProps: CommonCartridgeOrganizationProps = {
+					resourcePath: 'path/to/resource.jpg',
+					path: 'path/to/resource',
+					pathDepth: 1,
+					identifier: 'resource-id',
+					title: 'Resource Title',
+					isResource: true,
+					isInlined: false,
+					resourceType: 'webContent',
+				};
 
-					expect(result).toEqual(undefined);
-				});
+				const result = sut.mapResourceToContentBody(resource, cardElementProps);
+
+				return result;
+			};
+
+			it('should return undefined for unknown resource type', () => {
+				const result = setup();
+
+				expect(result).toEqual(undefined);
 			});
 		});
 	});
