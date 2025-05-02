393d4b419f069f1c495df090a83333791a9187a6
hoeppner.dataport <marcus.hoeppner@dataport.de>
2025-05-02 07:58:17 +0200

    fix: improve coverage

--- apps/server/src/modules/room/api/dto/response/room-invitation-link.error.ts
+++ apps/server/src/modules/room/api/dto/response/room-invitation-link.error.ts
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 import { ApiProperty } from '@nestjs/swagger';
 import { RoomInvitationLinkValidationError } from '../../type/room-invitation-link-validation-error.enum';
 
