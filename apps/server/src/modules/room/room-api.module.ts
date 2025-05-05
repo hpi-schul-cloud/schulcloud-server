8f84a7bc13b76964c71c1a4630290e060b3a57a3
Thomas Feldtkeller <thomas.feldtkeller@dataport.de>
2025-05-05 15:31:50 +0200

    BC-9031 adjust errors for invitation links (#5663)
    * throw validation error for invalid link
    * introduce specific validation error type to carry school name
    
    ---------
    
    Co-authored-by: hoeppner.dataport <marcus.hoeppner@dataport.de>

--- apps/server/src/modules/room/room-api.module.ts
+++ apps/server/src/modules/room/room-api.module.ts
@@ -6,9 +6,10 @@ import { RoomMembershipModule } from '../room-membership/room-membership.module'
 import { UserModule } from '../user';
 import { RoomController, RoomInvitationLinkController, RoomInvitationLinkUc, RoomUc } from './api';
 import { RoomModule } from './room.module';
+import { SchoolModule } from '@modules/school';
 
 @Module({
-	imports: [RoomModule, AuthorizationModule, LoggerModule, RoomMembershipModule, BoardModule, UserModule],
+	imports: [RoomModule, AuthorizationModule, LoggerModule, RoomMembershipModule, BoardModule, UserModule, SchoolModule],
 	controllers: [RoomController, RoomInvitationLinkController],
 	providers: [RoomUc, RoomInvitationLinkUc],
 })
