58c2ef0254d577e3306b98e7cdad6e3bf2787cc0
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2025-05-05 16:47:46 +0200

    implement step for news

--- apps/server/src/modules/news/news.module.ts
+++ apps/server/src/modules/news/news.module.ts
@@ -1,16 +1,16 @@
 import { LoggerModule } from '@core/logger';
 import { AuthorizationModule } from '@modules/authorization';
 import { forwardRef, Module } from '@nestjs/common';
-import { DeletionModule } from '@modules/deletion';
 import { NewsController, NewsUc, TeamNewsController } from './api';
-import { NewsRepo } from './repo/news.repo';
 import { NewsService } from './domain';
+import { NewsRepo } from './repo/news.repo';
+import { DeleteUserNewsDataStep } from './saga';
 
 // imports from deletion module?
 @Module({
-	imports: [LoggerModule, forwardRef(() => AuthorizationModule), DeletionModule],
+	imports: [LoggerModule, forwardRef(() => AuthorizationModule)],
 	controllers: [NewsController, TeamNewsController],
-	providers: [NewsUc, NewsRepo, NewsService],
+	providers: [NewsUc, NewsRepo, NewsService, DeleteUserNewsDataStep],
 	exports: [NewsUc, NewsService],
 })
 export class NewsModule {}
