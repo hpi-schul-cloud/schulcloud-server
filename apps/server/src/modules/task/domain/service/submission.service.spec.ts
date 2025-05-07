dbf5e328e4968701e5d2aa9b192529c1c21fd90c
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2025-05-07 17:58:01 +0200

    adapt tests for task module

--- apps/server/src/modules/task/domain/service/submission.service.spec.ts
+++ apps/server/src/modules/task/domain/service/submission.service.spec.ts
@@ -2,21 +2,13 @@ import { Logger } from '@core/logger';
 import { createMock, DeepMocked } from '@golevelup/ts-jest';
 import { FileRecordParentType } from '@infra/rabbitmq';
 import { CourseEntity, CourseGroupEntity } from '@modules/course/repo';
-import {
-	DomainDeletionReportBuilder,
-	DomainName,
-	DomainOperationReportBuilder,
-	OperationType,
-	UserDeletionInjectionService,
-} from '@modules/deletion';
+import { UserDeletionInjectionService } from '@modules/deletion';
 import { FileDto, FilesStorageClientAdapterService } from '@modules/files-storage-client';
 import { LessonEntity, Material } from '@modules/lesson/repo';
 import { User } from '@modules/user/repo';
-import { userFactory } from '@modules/user/testing';
 import { Test, TestingModule } from '@nestjs/testing';
 import { Counted } from '@shared/domain/types';
 import { setupEntities } from '@testing/database';
-import { ObjectId } from 'bson';
 import { Submission, SubmissionRepo, Task } from '../../repo';
 import { submissionFactory, taskFactory } from '../../testing';
 import { SubmissionService } from './submission.service';
@@ -232,181 +224,6 @@ describe('Submission Service', () => {
 
 				await expect(service.delete(submission)).rejects.toThrow(error);
 			});
-		});
-	});
-
-	describe('deleteSingleSubmissionsOwnedByUser', () => {
-		describe('when submission with specified userId was not found ', () => {
-			const setup = () => {
-				const submission = submissionFactory.buildWithId();
-
-				submissionRepo.findAllByUserId.mockResolvedValueOnce([[], 0]);
-
-				return { submission };
-			};
-
-			it('should return deletedSubmissions number of 0', async () => {
-				const { submission } = setup();
-
-				const result = await service.deleteSingleSubmissionsOwnedByUser(new ObjectId().toString());
-
-				expect(result.count).toEqual(0);
-				expect(result.refs.length).toEqual(0);
-				expect(submission).toBeDefined();
-			});
-		});
-
-		describe('when submission with specified userId was found ', () => {
-			const setup = () => {
-				const user = userFactory.buildWithId();
-				const submission = submissionFactory.buildWithId({ student: user, teamMembers: [user] });
-
-				submissionRepo.findAllByUserId.mockResolvedValueOnce([[submission], 1]);
-				submissionRepo.delete.mockResolvedValueOnce();
-
-				return { submission, user };
-			};
-
-			it('should return deletedSubmissions number of 1', async () => {
-				const { submission, user } = setup();
-
-				const result = await service.deleteSingleSubmissionsOwnedByUser(user.id);
-
-				expect(result.count).toEqual(1);
-				expect(result.refs.length).toEqual(1);
-				expect(submissionRepo.delete).toBeCalledTimes(1);
-				expect(submissionRepo.delete).toHaveBeenCalledWith([submission]);
-			});
-		});
-	});
-
-	describe('removeUserReferencesFromSubmissions', () => {
-		describe('when submission with specified userId was not found ', () => {
-			const setup = () => {
-				const user1 = userFactory.buildWithId();
-				const user2 = userFactory.buildWithId();
-				const submission = submissionFactory.buildWithId({ student: user1, teamMembers: [user1, user2] });
-
-				submissionRepo.findAllByUserId.mockResolvedValueOnce([[], 0]);
-
-				return { submission, user1, user2 };
-			};
-
-			it('should return updated submission number of 0', async () => {
-				const { submission, user1 } = setup();
-
-				const result = await service.removeUserReferencesFromSubmissions(new ObjectId().toString());
-
-				expect(result.count).toEqual(0);
-				expect(result.refs.length).toEqual(0);
-				expect(submission.student).toEqual(user1);
-				expect(submission.teamMembers.length).toEqual(2);
-			});
-		});
-
-		describe('when submission with specified userId was found ', () => {
-			const setup = () => {
-				const user1 = userFactory.buildWithId();
-				const user2 = userFactory.buildWithId();
-				const submission = submissionFactory.buildWithId({
-					student: user1,
-					teamMembers: [user1, user2],
-				});
-
-				submissionRepo.findAllByUserId.mockResolvedValueOnce([[submission], 1]);
-
-				return { submission, user1, user2 };
-			};
-
-			it('should return updated submission number of 1', async () => {
-				const { user1 } = setup();
-
-				const result = await service.removeUserReferencesFromSubmissions(user1.id);
-
-				expect(result.count).toEqual(1);
-				expect(result.refs.length).toEqual(1);
-			});
-
-			it('should call repo removeUserReference', async () => {
-				const { submission, user1 } = setup();
-
-				await service.removeUserReferencesFromSubmissions(user1.id);
-
-				expect(submissionRepo.removeUserReference).toHaveBeenCalledWith([submission.id]);
-			});
-
-			it('should call repo deleteUserFromTeam', async () => {
-				const { user1 } = setup();
-
-				await service.removeUserReferencesFromSubmissions(user1.id);
-
-				expect(submissionRepo.deleteUserFromGroupSubmissions).toHaveBeenCalledWith(user1.id);
-			});
-		});
-	});
-
-	describe('deleteUserData', () => {
-		const setup = () => {
-			const user1 = userFactory.buildWithId();
-			const user2 = userFactory.buildWithId();
-			const submission1 = submissionFactory.buildWithId({ student: user1, teamMembers: [user1] });
-			const submission2 = submissionFactory.buildWithId({
-				student: user1,
-				teamMembers: [user1, user2],
-			});
-
-			submissionRepo.findAllByUserId.mockResolvedValueOnce([[submission1, submission2], 2]);
-
-			const expectedResultForOwner = DomainOperationReportBuilder.build(OperationType.DELETE, 1, [submission1.id]);
-
-			const expectedResultForUsersPermission = DomainOperationReportBuilder.build(OperationType.DELETE, 1, [
-				submission2.id,
-			]);
-
-			const expectedResult = DomainDeletionReportBuilder.build(DomainName.SUBMISSIONS, [
-				expectedResultForOwner,
-				expectedResultForUsersPermission,
-			]);
-
-			return {
-				user1,
-				expectedResultForOwner,
-				expectedResultForUsersPermission,
-				expectedResult,
-			};
-		};
-
-		it('should call deleteSingleSubmissionsOwnedByUser with userId', async () => {
-			const { user1, expectedResultForOwner } = setup();
-			jest.spyOn(service, 'deleteSingleSubmissionsOwnedByUser').mockResolvedValueOnce(expectedResultForOwner);
-
-			await service.deleteUserData(user1.id);
-
-			expect(service.deleteSingleSubmissionsOwnedByUser).toHaveBeenCalledWith(user1.id);
-		});
-
-		it('should call removeUserReferencesFromSubmissions with userId', async () => {
-			const { user1, expectedResultForUsersPermission } = setup();
-			jest
-				.spyOn(service, 'removeUserReferencesFromSubmissions')
-				.mockResolvedValueOnce(expectedResultForUsersPermission);
-
-			await service.deleteUserData(user1.id);
-
-			expect(service.removeUserReferencesFromSubmissions).toHaveBeenCalledWith(user1.id);
-		});
-
-		it('should return domainOperation object with information about deleted user data', async () => {
-			const { user1, expectedResultForOwner, expectedResultForUsersPermission, expectedResult } = setup();
-
-			jest.spyOn(service, 'deleteSingleSubmissionsOwnedByUser').mockResolvedValueOnce(expectedResultForOwner);
-			jest
-				.spyOn(service, 'removeUserReferencesFromSubmissions')
-				.mockResolvedValueOnce(expectedResultForUsersPermission);
-
-			const result = await service.deleteUserData(user1.id);
-
-			expect(result).toEqual(expectedResult);
 		});
 	});
 });
