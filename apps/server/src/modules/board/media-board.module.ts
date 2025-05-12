cea8c33d31a485fc2ba79cf18affd86beb6ad40c
Thomas Feldtkeller <thomas.feldtkeller@dataport.de>
2025-05-12 11:43:12 +0200

    fix module imports

--- apps/server/src/modules/board/media-board.module.ts
+++ apps/server/src/modules/board/media-board.module.ts
@@ -3,10 +3,9 @@ import { Module } from '@nestjs/common';
 import { MediaBoardNodeFactory } from './domain';
 import { BoardNodeRepo } from './repo';
 import { MediaBoardService, MediaAvailableLineService } from './service';
-import { UserModule } from '@modules/user';
 
 @Module({
-	imports: [ToolModule, UserModule],
+	imports: [ToolModule],
 	providers: [BoardNodeRepo, MediaBoardNodeFactory, MediaBoardService, MediaAvailableLineService],
 	exports: [MediaBoardNodeFactory, MediaBoardService, MediaAvailableLineService],
 })
