e6faf97c6e563bc549dac9bb98c6dce03b6d20f1
Steliyan Dinkov <steliyan.dinkov@capgemini.com>
2025-05-13 17:00:54 +0200

    update SchoolExternalToolMedium

--- apps/server/src/modules/tool/school-external-tool/controller/dto/school-external-tool-medium.response.ts
+++ apps/server/src/modules/tool/school-external-tool/controller/dto/school-external-tool-medium.response.ts
@@ -1,9 +1,20 @@
 import { MediaSourceLicenseType } from '@modules/media-source';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEnum } from 'class-validator';
+import { ExternalToolMediumStatus } from '../../../external-tool/enum';
 
 export class SchoolExternalToolMediumResponse {
+	@ApiProperty({
+		description: 'The type of the medium',
+		required: true,
+		enum: ExternalToolMediumStatus,
+		enumName: 'ExternalToolMediumStatus',
+	})
+	@IsEnum(ExternalToolMediumStatus)
+	public status: ExternalToolMediumStatus;
+
 	@ApiProperty({ description: 'Id of the medium' })
-	public mediumId: string;
+	public mediumId?: string;
 
 	@ApiPropertyOptional({ description: 'The id of the media source' })
 	public mediaSourceId?: string;
@@ -19,6 +30,7 @@ export class SchoolExternalToolMediumResponse {
 	public mediaSourceLicenseType?: MediaSourceLicenseType;
 
 	constructor(props: SchoolExternalToolMediumResponse) {
+		this.status = props.status;
 		this.mediaSourceName = props.mediaSourceName;
 		this.mediumId = props.mediumId;
 		this.mediaSourceId = props.mediaSourceId;
