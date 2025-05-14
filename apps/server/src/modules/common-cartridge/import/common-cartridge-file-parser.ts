2b0387e9f001ea179bb3450336b8edc0dafbb21a
mkreuzkam-cap <144103168+mkreuzkam-cap@users.noreply.github.com>
2025-05-14 12:20:10 +0200

    EW-1212: Refactor/Refurbish CC-Import (#5678)

--- apps/server/src/modules/common-cartridge/import/common-cartridge-file-parser.ts
+++ apps/server/src/modules/common-cartridge/import/common-cartridge-file-parser.ts
@@ -18,7 +18,7 @@ export class CommonCartridgeFileParser {
 
 	private readonly resourceFactory: CommonCartridgeResourceFactory;
 
-	public constructor(file: Buffer, private readonly options = DEFAULT_FILE_PARSER_OPTIONS) {
+	constructor(file: Buffer, public readonly options = DEFAULT_FILE_PARSER_OPTIONS) {
 		this.archive = new AdmZip(file);
 		this.manifestParser = new CommonCartridgeManifestParser(this.getManifestAsDocument(), this.options);
 		this.resourceFactory = new CommonCartridgeResourceFactory(this.archive);
