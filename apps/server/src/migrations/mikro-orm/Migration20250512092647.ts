54cd63f548f7382345de8bf02fa61837ed14948c
Steliyan Dinkov <steliyan.dinkov@capgemini.com>
2025-05-20 09:43:59 +0200

    update migration - add rollback for medium status field

--- apps/server/src/migrations/mikro-orm/Migration20250512092647.ts
+++ apps/server/src/migrations/mikro-orm/Migration20250512092647.ts
@@ -1,18 +1,25 @@
 import { Migration } from '@mikro-orm/migrations-mongodb';
-import { ExternalTool } from '../../modules/tool';
-
+type ExternalToolEntity = {
+	medium: {
+		status: string;
+	};
+};
 export class Migration20250512092647 extends Migration {
 	public async up(): Promise<void> {
-		const externalTools = await this.getCollection<ExternalTool>('external-tools').updateMany(
+		const externalTools = await this.getCollection<ExternalToolEntity>('external-tools').updateMany(
 			{ medium: { $exists: true } },
 			{ $set: { 'medium.status': 'active' } }
 		);
 
-		console.info("Added 'isPreferred' field to 'external-tools'", externalTools.modifiedCount);
+		console.info("Added 'status' field to 'external-tools' medium, set to 'active'", externalTools.modifiedCount);
 	}
 
-	// eslint-disable-next-line @typescript-eslint/require-await, require-await
 	public async down(): Promise<void> {
-		console.info('This migration cannot be rolled back.');
+		const externalTools = await this.getCollection<ExternalToolEntity>('external-tools').updateMany(
+			{ medium: { $exists: true } },
+			{ $unset: { 'medium.status': '' } }
+		);
+
+		console.info("Rollback of add 'status' field to 'external-tools' medium:", externalTools.modifiedCount);
 	}
 }
