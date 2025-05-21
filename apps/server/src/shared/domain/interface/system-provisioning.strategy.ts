aa3eb2956c82f540598460aebd511d98ddbfc9d9
Igor Richter <igor.richter@capgemini.com>
2025-05-21 08:46:52 +0200

    rename sanis system alias to moin.schule

--- apps/server/src/shared/domain/interface/system-provisioning.strategy.ts
+++ apps/server/src/shared/domain/interface/system-provisioning.strategy.ts
@@ -1,5 +1,5 @@
 export enum SystemProvisioningStrategy {
-	SCHULCONNEX_LEGACY = 'sanis',
+	SCHULCONNEX_LEGACY = 'moin.schule',
 	SCHULCONNEX_ASYNC = 'schulconnex-async',
 	OIDC = 'oidc',
 	TSP = 'tsp',
