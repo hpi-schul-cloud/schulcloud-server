a5ff4224939e70468082b3b6d96a05b9dc1fe459
hoeppner-dataport <106819770+hoeppner-dataport@users.noreply.github.com>
2025-05-22 13:16:51 +0200

    BC-9729 - adding teachers from other school to room bug (#5695)
    * discoverable property should be mapped to the do in userDoRepo
    * user rule should respect system setting for discoverability of teachers

--- apps/server/src/modules/authorization-rules/rules/user.rule.ts
+++ apps/server/src/modules/authorization-rules/rules/user.rule.ts
@@ -2,11 +2,13 @@ import { AuthorizationContext, AuthorizationHelper, AuthorizationInjectionServic
 import { UserDo } from '@modules/user';
 import { User } from '@modules/user/repo';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UserRule implements Rule<UserDo> {
 	constructor(
 		private readonly authorizationHelper: AuthorizationHelper,
+		private readonly configService: ConfigService,
 		authorisationInjectionService: AuthorizationInjectionService
 	) {
 		authorisationInjectionService.injectAuthorizationRule(this);
@@ -23,10 +25,29 @@ export class UserRule implements Rule<UserDo> {
 
 		const isHimself = user.id === entity.id;
 		const isUsersSchool = user.school.id === entity.schoolId;
-		const isDiscoverable = entity.discoverable || false;
+		const isDiscoverable = this.determineDiscoverability(entity);
 
 		const isVisible = isHimself || isUsersSchool || isDiscoverable;
 
 		return hasPermission && isVisible;
+	}
+
+	private determineDiscoverability(entity: UserDo): boolean {
+		const discoverabilitySetting = this.configService.get<string>('TEACHER_VISIBILITY_FOR_EXTERNAL_TEAM_INVITATION');
+		if (discoverabilitySetting === 'disabled') {
+			return false;
+		}
+		if (discoverabilitySetting === 'enabled') {
+			return true;
+		}
+
+		if (discoverabilitySetting === 'opt-in') {
+			return entity.discoverable ?? false;
+		}
+		if (discoverabilitySetting === 'opt-out') {
+			return entity.discoverable ?? true;
+		}
+
+		throw new Error('Invalid discoverability setting');
 	}
 }
