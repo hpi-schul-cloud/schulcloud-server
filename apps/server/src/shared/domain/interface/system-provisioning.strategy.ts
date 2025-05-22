496a546f53b6e7fcc7875be73dbb8b6273a1fa18
Igor Richter <igor.richter@capgemini.com>
2025-05-22 09:36:11 +0200

    revert change too legacy provisioning

--- apps/server/src/shared/domain/interface/system-provisioning.strategy.ts
+++ apps/server/src/shared/domain/interface/system-provisioning.strategy.ts
@@ -1,5 +1,5 @@
 export enum SystemProvisioningStrategy {
-	SCHULCONNEX_LEGACY = 'moin.schule',
+	SCHULCONNEX_LEGACY = 'sanis',
 	SCHULCONNEX_ASYNC = 'schulconnex-async',
 	OIDC = 'oidc',
 	TSP = 'tsp',
