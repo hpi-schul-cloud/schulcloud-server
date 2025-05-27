2e7655e6a3832edf9485ec4587df2ad445acfc89
Gordon Nicholas <gordon.nicholas@capgemini.com>
2025-05-27 11:39:10 +0200

    N21-2498 wip restructured h5p editor client

--- apps/server/src/infra/h5p-editor-client/h5p-editor-client.adapter.ts
+++ apps/server/src/infra/h5p-editor-client/h5p-editor-client.adapter.ts
@@ -1,18 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { REQUEST } from '@nestjs/core';
+import { JwtExtractor } from '@shared/common/utils';
 import { AxiosResponse } from 'axios';
-import { type H5PContentParentType, H5PCopyResponse, H5pEditorApi, PostH5PContentCopyParams } from './generated';
+import { Request } from 'express';
+import {
+	Configuration,
+	type H5PContentParentType,
+	H5PCopyResponse,
+	H5pEditorApi,
+	PostH5PContentCopyParams,
+} from './generated';
 
 @Injectable()
 export class H5pEditorClientAdapter {
-	constructor(private readonly h5pEditorApi: H5pEditorApi) {}
+	constructor(@Inject(REQUEST) private request: Request, private readonly configService: ConfigService) {}
+
+	private createH5pEditorApi(): H5pEditorApi {
+		const basePath = this.configService.getOrThrow<string>('H5P_EDITOR__SERVICE_BASE_URL');
+
+		const config = new Configuration({
+			accessToken: JwtExtractor.extractJwtFromRequestOrFail(this.request),
+			basePath: `${basePath}/api/v3`,
+		});
+
+		const h5pEditorApi = new H5pEditorApi(config);
+
+		return h5pEditorApi;
+	}
 
 	public async copyH5PContent(contentId: string, parentId: string, parentType: H5PContentParentType): Promise<string> {
+		const h5pEditorApi = this.createH5pEditorApi();
+
 		const bodyParams: PostH5PContentCopyParams = {
 			parentId,
 			parentType,
 		};
 
-		const response: AxiosResponse<H5PCopyResponse> = await this.h5pEditorApi.h5PEditorControllerCopyH5pContent(
+		const response: AxiosResponse<H5PCopyResponse> = await h5pEditorApi.h5PEditorControllerCopyH5pContent(
 			contentId,
 			bodyParams
 		);
--- apps/server/src/infra/h5p-editor-client/h5p-editor-client.module.ts
+++ apps/server/src/infra/h5p-editor-client/h5p-editor-client.module.ts
@@ -1,32 +1,9 @@
-import { Module, Scope } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { REQUEST } from '@nestjs/core';
-import { JwtExtractor } from '@shared/common/utils';
-import { Request } from 'express';
+import { Module } from '@nestjs/common';
 import { H5pEditorClientAdapter } from './h5p-editor-client.adapter';
-import { H5pEditorApi, Configuration } from './generated';
-import type { H5pEditorClientConfig } from './h5p-editor-client.config';
 
 @Module({
 	imports: [],
-	providers: [
-		H5pEditorClientAdapter,
-		{
-			provide: H5pEditorApi,
-			scope: Scope.REQUEST,
-			useFactory: (configService: ConfigService<H5pEditorClientConfig, true>, request: Request): H5pEditorApi => {
-				const basePath = configService.getOrThrow<string>('H5P_EDITOR__SERVICE_BASE_URL');
-
-				const config = new Configuration({
-					accessToken: JwtExtractor.extractJwtFromRequestOrFail(request),
-					basePath: `${basePath}/api/v3`,
-				});
-
-				return new H5pEditorApi(config);
-			},
-			inject: [ConfigService, REQUEST],
-		},
-	],
+	providers: [H5pEditorClientAdapter],
 	exports: [H5pEditorClientAdapter],
 })
 export class H5pEditorClientModule {}
