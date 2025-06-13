3d0d0b20ea4e119c1593f4bf3e0e46fe7b1e0500
virgilchiriac <virgil.chiriac@dataport.de>
2025-06-13 15:14:10 +0200

    fix linter

--- apps/server/src/modules/deletion/api/controller/api-test/deletion-executions.api.spec.ts
+++ apps/server/src/modules/deletion/api/controller/api-test/deletion-executions.api.spec.ts
@@ -136,7 +136,6 @@ describe(`deletionExecution (api)`, () => {
 
 		describe('when deleting users with full data', () => {
 			const setup = async () => {
-
 				const school = schoolEntityFactory.buildWithId();
 				const { teacherUser, teacherAccount } = UserAndAccountTestFactory.buildTeacher({ school });
 				const { studentUser, studentAccount } = UserAndAccountTestFactory.buildStudent({ school });
