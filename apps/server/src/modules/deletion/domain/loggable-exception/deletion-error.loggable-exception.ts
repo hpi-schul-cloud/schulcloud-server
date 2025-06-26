a04d3414dac1ee8f144b38d3bf787fee64c2270b
wolfganggreschus <wolfgang@greschus.de>
2025-06-26 08:34:24 +0200

    fix: reorder imports and simplify error handling in DeletionErrorLoggableException

--- apps/server/src/modules/deletion/domain/loggable-exception/deletion-error.loggable-exception.ts
+++ apps/server/src/modules/deletion/domain/loggable-exception/deletion-error.loggable-exception.ts
@@ -1,7 +1,7 @@
-import { InternalServerErrorException } from '@nestjs/common';
+import { AxiosErrorLoggable } from '@core/error/loggable';
 import { Loggable } from '@core/logger';
 import { ErrorLogMessage } from '@core/logger/types';
-import { AxiosErrorLoggable } from '@core/error/loggable';
+import { InternalServerErrorException } from '@nestjs/common';
 import { isAxiosError } from 'axios';
 export class DeletionErrorLoggableException extends InternalServerErrorException implements Loggable {
 	constructor(private readonly errorMessage: string, private readonly error?: Error) {
@@ -9,10 +9,9 @@ export class DeletionErrorLoggableException extends InternalServerErrorException
 	}
 
 	public getLogMessage(): ErrorLogMessage {
-		let { error } = this;
-		if (isAxiosError(this.error)) {
-			error = new AxiosErrorLoggable(this.error, 'DELETION_3RD_PARTY_ERROR');
-		}
+		const error = isAxiosError(this.error)
+			? new AxiosErrorLoggable(this.error, 'DELETION_3RD_PARTY_ERROR')
+			: this.error;
 		const message: ErrorLogMessage = {
 			type: 'DELETION_ERROR',
 			stack: this.stack,
