91767875ac89006c36ffcb75c9f0a63eddb77ec7
Phillip <phillip.wirth@dataport.de>
2025-06-26 10:30:33 +0200

    BC-10037 remove SC_DOMAIN from globals.js (#5768)
    apparently schulconnex-group need SC_DOMAIN because they run some code
    from globals.js, SC_DOMAIN is not defined in prod config for those ...
    so simply moving that code out from globals.js since its not always
    required

--- src/services/user/hooks/userService.js
+++ src/services/user/hooks/userService.js
@@ -1,3 +1,4 @@
+const { Configuration } = require('@hpi-schul-cloud/commons');
 const { authenticate } = require('@feathersjs/authentication');
 const { keep } = require('feathers-hooks-common');
 
@@ -9,7 +10,7 @@ const { hasRoleNoHook, hasPermissionNoHook, hasPermission } = require('../../../
 const { getAge } = require('../../../utils');
 
 const constants = require('../../../utils/constants');
-const { CONSENT_WITHOUT_PARENTS_MIN_AGE_YEARS, SC_DOMAIN } = require('../../../../config/globals');
+const { CONSENT_WITHOUT_PARENTS_MIN_AGE_YEARS} = require('../../../../config/globals');
 
 /**
  *
@@ -519,7 +520,7 @@ const generateRegistrationLink = async (context) => {
 				role: data.roles[0],
 				save: true,
 				patchUser: true,
-				host: SC_DOMAIN,
+				host: Configuration.get('SC_DOMAIN'),
 				schoolId: data.schoolId,
 				toHash: data.email,
 			})
