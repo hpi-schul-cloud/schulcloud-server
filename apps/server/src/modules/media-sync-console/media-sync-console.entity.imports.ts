d40d16568c41c12e769630d9aaa99fb5d764c6f4
Gordon Nicholas <gordon.nicholas@capgemini.com>
2025-06-27 16:27:51 +0200

    N21-2500 add missing import entity

--- apps/server/src/modules/media-sync-console/media-sync-console.entity.imports.ts
+++ apps/server/src/modules/media-sync-console/media-sync-console.entity.imports.ts
@@ -2,7 +2,7 @@ import { InstanceEntity } from '@modules/instance';
 import { MediaSourceEntity } from '@modules/media-source/entity';
 import { ExternalToolEntity } from '@modules/tool/external-tool/repo';
 import { SchoolEntity, SchoolYearEntity } from '@modules/school/repo';
-import { MediaSchoolLicenseEntity } from '@modules/school-license/entity';
+import { MediaSchoolLicenseEntity, SchoolLicenseEntity } from '@modules/school-license/entity';
 import { SchoolExternalToolEntity } from '@modules/tool/school-external-tool/repo';
 import { ContextExternalToolRepo } from '@modules/tool/context-external-tool/repo';
 
@@ -12,6 +12,7 @@ export const ENTITIES = [
 	SchoolExternalToolEntity,
 	ContextExternalToolRepo,
 	InstanceEntity,
+	SchoolLicenseEntity,
 	MediaSchoolLicenseEntity,
 	SchoolEntity,
 	SchoolYearEntity,
