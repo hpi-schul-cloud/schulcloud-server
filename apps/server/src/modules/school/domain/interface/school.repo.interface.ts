e5f209f3e2675ef57d5905c4d960d4b360cae5ca
Martin Schuhmacher <55735359+MartinSchuhmacher@users.noreply.github.com>
2025-07-07 11:28:36 +0200

    BC-9948 - Performance Problem when fetching schools for external invites (#5747)
    * implement new paginated endpoint to retrieve list of schools
    * exclude expert- and tombstone-schools from this list
    * remove ownSchoolId query filter from getSchoolList
    
    ---------
    
    Co-authored-by: hoeppner.dataport <marcus.hoeppner@dataport.de>

--- apps/server/src/modules/school/domain/interface/school.repo.interface.ts
+++ apps/server/src/modules/school/domain/interface/school.repo.interface.ts
@@ -6,6 +6,11 @@ import { SchoolQuery } from '../query';
 export interface SchoolRepo {
 	getSchools(query: SchoolQuery, options?: IFindOptions<SchoolProps>): Promise<School[]>;
 
+	getSchoolList(
+		options: IFindOptions<SchoolProps>,
+		federalStateId?: EntityId
+	): Promise<{ schools: School[]; count: number }>;
+
 	getSchoolById(schoolId: EntityId): Promise<School>;
 
 	getSchoolByOfficialSchoolNumber(officialSchoolNumber: string): Promise<School | null>;
