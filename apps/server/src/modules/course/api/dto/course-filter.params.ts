e40390cee6076538781dd624289245e851b56e7d
virgilchiriac <virgil.chiriac@dataport.de>
2025-07-08 15:50:13 +0200

    fix boolean param

--- apps/server/src/modules/course/api/dto/course-filter.params.ts
+++ apps/server/src/modules/course/api/dto/course-filter.params.ts
@@ -1,15 +1,16 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsBoolean, IsEnum, IsOptional } from 'class-validator';
 import { CourseStatus } from '../../domain';
+import { StringToBoolean } from '@shared/controller/transformer';
 
 export class CourseFilterParams {
 	@IsOptional()
 	@IsEnum(CourseStatus)
 	@ApiPropertyOptional({ enum: CourseStatus, enumName: 'CourseStatus' })
-	status?: CourseStatus;
+	public status?: CourseStatus;
 
-	@IsOptional()
 	@IsBoolean()
+	@StringToBoolean()
 	@ApiPropertyOptional()
-	withoutTeachers?: boolean = false;
+	public withoutTeachers?: boolean = false;
 }
