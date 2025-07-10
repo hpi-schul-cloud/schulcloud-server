e1373c9f0211ce65dc810cc2e6b18a258af15c72
dyedwiper <maximilian.becker@dataport.de>
2025-07-10 15:22:28 +0200

    Use only a single migration

--- apps/server/src/migrations/mikro-orm/Migration20250710083440.ts
+++ apps/server/src/migrations/mikro-orm/Migration20250710083440.ts
@@ -1,5 +1,6 @@
 import { Migration } from '@mikro-orm/migrations-mongodb';
 import { ObjectId } from '@mikro-orm/mongodb';
+import { FindCursor, WithId } from '@mikro-orm/mongodb/node_modules/mongodb';
 import { AesEncryptionHelper } from '@shared/common/utils';
 import CryptoJS from 'crypto-js';
 
@@ -8,9 +9,24 @@ type StorageProvider = {
 	secretAccessKey: string;
 };
 
-// Update AES encrypted secretAccessKey of StorageProvider with new encryption function.
+type LdapSystem = {
+	_id: ObjectId;
+	ldapConfig: {
+		searchUserPassword: string;
+	};
+};
+
+type OauthSystem = {
+	_id: ObjectId;
+	oauthConfig: {
+		clientSecret: string;
+	};
+};
+
+// Update all AES encrypted secrets with new encryption function.
 export class Migration20250710083440 extends Migration {
 	public async up(): Promise<void> {
+		//  --- Update secretAccessKey of storage providers ---
 		// eslint-disable-next-line no-process-env
 		const { S3_KEY } = process.env;
 
@@ -21,25 +37,73 @@ export class Migration20250710083440 extends Migration {
 
 		const storageProviders = this.getCollection<StorageProvider>('storageproviders').find({});
 
-		let numberOfUpdatedStorageProviders = 0;
+		const numberOfUpdatedStorageProviders = await this.updateSecrets(
+			storageProviders,
+			['secretAccessKey'],
+			S3_KEY,
+			'storageproviders'
+		);
 
-		for await (const provider of storageProviders) {
-			const decrypted = CryptoJS.AES.decrypt(provider.secretAccessKey, S3_KEY).toString(CryptoJS.enc.Utf8);
-			const encrypted = AesEncryptionHelper.encrypt(decrypted, S3_KEY);
-
-			await this.getCollection('storageproviders').updateOne(
-				{ _id: provider._id },
-				{ $set: { secretAccessKey: encrypted } }
-			);
-
-			numberOfUpdatedStorageProviders += 1;
-		}
 		console.info(
 			`Updated secretAccessKey of ${numberOfUpdatedStorageProviders} storage providers with new encryption function.`
 		);
+		// --- End of update secretAccessKey of storage providers ---
+
+		// ----------------------------------------------------------------------------------
+
+		// --- Update searchUserPassword of LDAP systems ---
+		// eslint-disable-next-line no-process-env
+		const { LDAP_PASSWORD_ENCRYPTION_KEY } = process.env;
+
+		if (!LDAP_PASSWORD_ENCRYPTION_KEY) {
+			console.error('LDAP_PASSWORD_ENCRYPTION_KEY is not provided. Migration cannot proceed.');
+			return;
+		}
+
+		const ldapSystems = this.getCollection<LdapSystem>('systems').find({
+			'ldapConfig.searchUserPassword': { $ne: undefined },
+		});
+
+		const numberOfUpdatedLdapSystems = await this.updateSecrets(
+			ldapSystems,
+			['ldapConfig', 'searchUserPassword'],
+			LDAP_PASSWORD_ENCRYPTION_KEY,
+			'systems'
+		);
+
+		console.info(
+			`Updated LDAP searchUserPassword of ${numberOfUpdatedLdapSystems} systems with new encryption function.`
+		);
+		// --- End of update searchUserPassword of LDAP systems ---
+
+		// ----------------------------------------------------------------------------------
+
+		// Update clientSecret of OAuth systems ---
+		// eslint-disable-next-line no-process-env
+		const { AES_KEY } = process.env;
+
+		if (!AES_KEY) {
+			console.error('AES_KEY is not provided. Migration cannot proceed.');
+			return;
+		}
+
+		const oauthSystems = this.getCollection<OauthSystem>('systems').find({
+			'oauthConfig.clientSecret': { $ne: undefined },
+		});
+
+		const numberOfUpdatedOauthSystems = await this.updateSecrets(
+			oauthSystems,
+			['oauthConfig', 'clientSecret'],
+			AES_KEY,
+			'systems'
+		);
+
+		console.info(`Updated OAuth clientSecret of ${numberOfUpdatedOauthSystems} systems with new encryption function.`);
+		// --- End of update clientSecret of OAuth systems ---
 	}
 
 	public async down(): Promise<void> {
+		// Revert update of secretAccessKey of storage providers ---
 		// eslint-disable-next-line no-process-env
 		const { S3_KEY } = process.env;
 
@@ -50,20 +114,108 @@ export class Migration20250710083440 extends Migration {
 
 		const storageProviders = this.getCollection<StorageProvider>('storageproviders').find({});
 
-		let numberOfUpdatedStorageProviders = 0;
-
-		for await (const provider of storageProviders) {
-			const decrypted = AesEncryptionHelper.decrypt(provider.secretAccessKey, S3_KEY);
-			const encrypted = CryptoJS.AES.encrypt(decrypted, S3_KEY).toString();
-
-			await this.getCollection('storageproviders').updateOne(
-				{ _id: provider._id },
-				{ $set: { secretAccessKey: encrypted } }
-			);
-
-			numberOfUpdatedStorageProviders += 1;
-		}
+		const numberOfUpdatedStorageProviders = await this.revertUpdateOfSecrets(
+			storageProviders,
+			['secretAccessKey'],
+			S3_KEY,
+			'storageproviders'
+		);
 
 		console.info(`Reverted update of secretAccessKey of ${numberOfUpdatedStorageProviders} storage providers.`);
+		// --- End of revert update of secretAccessKey of storage providers ---
+
+		// ----------------------------------------------------------------------------------
+
+		// Revert update of searchUserPassword of LDAP systems ---
+		// eslint-disable-next-line no-process-env
+		const { LDAP_PASSWORD_ENCRYPTION_KEY } = process.env;
+
+		if (!LDAP_PASSWORD_ENCRYPTION_KEY) {
+			console.error('LDAP_PASSWORD_ENCRYPTION_KEY is not provided. Migration cannot proceed.');
+			return;
+		}
+
+		const ldapSystems = this.getCollection<LdapSystem>('systems').find({
+			'ldapConfig.searchUserPassword': { $ne: undefined },
+		});
+
+		const numberOfUpdatedLdapSystems = await this.revertUpdateOfSecrets(
+			ldapSystems,
+			['ldapConfig', 'searchUserPassword'],
+			LDAP_PASSWORD_ENCRYPTION_KEY,
+			'systems'
+		);
+
+		console.info(`Reverted update of LDAP searchUserPassword of ${numberOfUpdatedLdapSystems} systems.`);
+		// --- End of revert update of searchUserPassword of LDAP systems ---
+
+		// ----------------------------------------------------------------------------------
+
+		// --- Revert update of clientSecret of OAuth systems ---
+		// eslint-disable-next-line no-process-env
+		const { AES_KEY } = process.env;
+
+		if (!AES_KEY) {
+			console.error('AES_KEY is not provided. Migration cannot proceed.');
+			return;
+		}
+
+		const oauthSystems = this.getCollection<OauthSystem>('systems').find({
+			'oauthConfig.clientSecret': { $ne: undefined },
+		});
+
+		const numberOfUpdatedOauthSystems = await this.revertUpdateOfSecrets(
+			oauthSystems,
+			['oauthConfig', 'clientSecret'],
+			AES_KEY,
+			'systems'
+		);
+
+		console.info(`Reverted update of OAuth clientSecret of ${numberOfUpdatedOauthSystems} systems.`);
+		// --- End of revert update of clientSecret of OAuth systems ---
+	}
+
+	private async updateSecrets<T>(
+		cursor: FindCursor<WithId<T>>,
+		path: Array<string>,
+		key: string,
+		collectionName: string
+	): Promise<number> {
+		let numberOfUpdatedSecrets = 0;
+
+		for await (const item of cursor) {
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+			const secret = path.reduce((acc, part) => acc[part], item) as unknown as string;
+			const decrypted = CryptoJS.AES.decrypt(secret, key).toString(CryptoJS.enc.Utf8);
+			const encrypted = AesEncryptionHelper.encrypt(decrypted, key);
+
+			await this.getCollection(collectionName).updateOne({ _id: item._id }, { $set: { [path.join('.')]: encrypted } });
+
+			numberOfUpdatedSecrets += 1;
+		}
+
+		return numberOfUpdatedSecrets;
+	}
+
+	private async revertUpdateOfSecrets<T>(
+		cursor: FindCursor<WithId<T>>,
+		path: Array<string>,
+		key: string,
+		collectionName: string
+	): Promise<number> {
+		let numberOfUpdatedSecrets = 0;
+
+		for await (const item of cursor) {
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+			const secret = path.reduce((acc, part) => acc[part], item) as unknown as string;
+			const decrypted = AesEncryptionHelper.decrypt(secret, key);
+			const encrypted = CryptoJS.AES.encrypt(decrypted, key).toString();
+
+			await this.getCollection(collectionName).updateOne({ _id: item._id }, { $set: { [path.join('.')]: encrypted } });
+
+			numberOfUpdatedSecrets += 1;
+		}
+
+		return numberOfUpdatedSecrets;
 	}
 }
