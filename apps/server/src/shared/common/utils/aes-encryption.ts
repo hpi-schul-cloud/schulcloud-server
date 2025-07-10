ac9e2800f11afa1df43f08a62e8931399fc8c551
dyedwiper <maximilian.becker@dataport.de>
2025-07-10 10:14:59 +0200

    Use secure key derivation function

--- apps/server/src/shared/common/utils/aes-encryption.ts
+++ apps/server/src/shared/common/utils/aes-encryption.ts
@@ -1,43 +1,36 @@
 import crypto from 'node:crypto';
 
-// The functions are copied from https://github.com/RaisinTen/aes-crypto-js/blob/v0.1.0/index.js.
-
-// Refs: https://github.com/brix/crypto-js/issues/468#issuecomment-2060562277
 export const encryptAES = (plainText: string, secret: string): string => {
-	const salt = crypto.randomBytes(8);
-	const password = Buffer.concat([Buffer.from(secret), salt]);
-	const hashes: Buffer[] = [];
-	let digest = password;
-	for (let i = 0; i < 3; i++) {
-		hashes[i] = crypto.createHash('md5').update(digest).digest();
-		digest = Buffer.concat([hashes[i], password]);
-	}
-	const key = Buffer.concat([hashes[0], hashes[1]]);
-	const iv = hashes[2];
-	const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+	const salt = crypto.randomBytes(16);
 
-	return Buffer.concat([Buffer.from('Salted__', 'utf8'), salt, cipher.update(plainText), cipher.final()]).toString(
-		'base64'
-	);
+	const derivedKey = crypto.pbkdf2Sync(secret, salt, 100000, 48, 'sha256');
+	const key = derivedKey.subarray(0, 32);
+	const iv = derivedKey.subarray(32);
+
+	const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+	const cipherText = Buffer.concat([
+		Buffer.from('Salted__', 'utf8'),
+		salt,
+		cipher.update(plainText),
+		cipher.final(),
+	]).toString('base64');
+
+	console.log(cipherText.length);
+
+	return cipherText;
 };
 
-// Refs: https://github.com/brix/crypto-js/issues/468#issuecomment-1783351942
-export const decryptAES = (encryptedText: string, secret: string): string => {
-	// From https://gist.github.com/schakko/2628689?permalink_comment_id=3321113#gistcomment-3321113
-	// From https://gist.github.com/chengen/450129cb95c7159cb05001cc6bdbf6a1
-	const encryptedTextAsBuffer = Buffer.from(encryptedText, 'base64');
-	const salt = encryptedTextAsBuffer.subarray(8, 16);
-	const content = encryptedTextAsBuffer.subarray(16);
-	const password = Buffer.concat([Buffer.from(secret), salt]);
-	const hashes: Buffer[] = [];
-	let digest = password;
-	for (let i = 0; i < 3; i++) {
-		hashes[i] = crypto.createHash('md5').update(digest).digest();
-		digest = Buffer.concat([hashes[i], password]);
-	}
-	const key = Buffer.concat([hashes[0], hashes[1]]);
-	const iv = hashes[2];
-	const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+export const decryptAES = (cipherText: string, secret: string): string => {
+	const encryptedTextAsBuffer = Buffer.from(cipherText, 'base64');
+	const salt = encryptedTextAsBuffer.subarray(8, 24);
+	const content = encryptedTextAsBuffer.subarray(24);
 
-	return Buffer.concat([decipher.update(content), decipher.final()]).toString('utf8');
+	const derivedKey = crypto.pbkdf2Sync(secret, salt, 100000, 48, 'sha256');
+	const key = derivedKey.subarray(0, 32);
+	const iv = derivedKey.subarray(32);
+
+	const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+	const plainText = Buffer.concat([decipher.update(content), decipher.final()]).toString('utf8');
+
+	return plainText;
 };
