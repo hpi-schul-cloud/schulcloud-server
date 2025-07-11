4c9add30c67c80085b1c2aa94955810507b77a9e
dyedwiper <maximilian.becker@dataport.de>
2025-07-11 10:53:56 +0200

    Add test for encryption helper

--- apps/server/src/shared/common/utils/aes-encryption.spec.ts
+++ apps/server/src/shared/common/utils/aes-encryption.spec.ts
@@ -1,3 +1,13 @@
 import { AesEncryptionHelper } from './aes-encryption';
 
-describe('AesEncryptionHelper', () => {});
+describe('AesEncryptionHelper', () => {
+	it('should encrypt and decrypt a string', () => {
+		const plainText = 'foo';
+		const secret = 'bar';
+
+		const encrypted = AesEncryptionHelper.encrypt(plainText, secret);
+		const decrypted = AesEncryptionHelper.decrypt(encrypted, secret);
+
+		expect(decrypted).toBe(plainText);
+	});
+});
