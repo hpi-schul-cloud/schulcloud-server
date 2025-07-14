b11e1803d92f7c81819d01e60e677fc5d227d457
Max Bischof <maximilian.bischof@dataport.de>
2025-07-14 11:02:21 +0200

    Add access token test

---
+++ apps/server/src/infra/access-token/domain/access-token.vo.spec.ts
@@ -0,0 +1,23 @@
+import { AccessToken } from './access-token.vo';
+
+describe('AccessToken', () => {
+	describe('constructor', () => {
+		describe('when called with a valid UUID token', () => {
+			it('should assign the token property', () => {
+				const validToken = '123e4567-e89b-12d3-a456-426614174000';
+
+				const accessToken = new AccessToken(validToken);
+
+				expect(accessToken.token).toBe(validToken);
+			});
+		});
+
+		describe('when called with an invalid token', () => {
+			it('should fail class-validator validation', () => {
+				const invalidToken = 'not-a-uuid';
+
+				expect(() => new AccessToken(invalidToken)).toThrow();
+			});
+		});
+	});
+});
