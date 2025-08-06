868e0382c230cf8f5f7ff722bbec0d1ef308d3d7
MartinSchuhmacher <mschuhmacher@gmx.de>
2025-08-06 12:21:51 +0200

    cleaning up user UC

--- apps/server/src/modules/user/api/user.uc.ts
+++ apps/server/src/modules/user/api/user.uc.ts
@@ -7,22 +7,10 @@ import type { ChangeLanguageParams } from './dto';
 
 @Injectable()
 export class UserUc {
-	constructor(
-		private readonly userService: UserService,
-		private readonly configService: ConfigService<UserConfig, true>
-	) {}
-
-	private checkAvaibleLanguages(settedLanguage: LanguageType): void | Error {
-		if (!this.configService.get<string[]>('AVAILABLE_LANGUAGES').includes(settedLanguage)) {
-			throw new BadRequestException('Language is not activated.');
-		}
-	}
+	constructor(private readonly userService: UserService) {}
 
 	public async patchLanguage(userId: EntityId, params: ChangeLanguageParams): Promise<boolean> {
 		const result = await this.userService.patchLanguage(userId, params.language);
-		if (!result) {
-			throw new BadRequestException('Language could not be changed.');
-		}
 
 		return result;
 	}
