d7395596c1afd551826622edddcd6672174a0292
hoeppner.dataport <marcus.hoeppner@dataport.de>
2025-09-01 11:40:07 +0200

    add roomMember-authorizable and -rule

---
+++ apps/server/src/modules/room-membership/authorization/room-member.rule.ts
@@ -0,0 +1,69 @@
+import {
+	Action,
+	AuthorizationContext,
+	AuthorizationHelper,
+	AuthorizationInjectionService,
+	Rule,
+} from '@modules/authorization';
+import { RoleName } from '@modules/role';
+import { User } from '@modules/user/repo';
+import { Injectable } from '@nestjs/common';
+import { Permission } from '@shared/domain/interface';
+import { RoomMemberAuthorizable } from '../do/room-member-authorizable.do';
+import { RoomMembershipRule } from './room-membership.rule';
+
+@Injectable()
+export class RoomMemberRule implements Rule<RoomMemberAuthorizable> {
+	constructor(
+		private readonly roomMembershipRule: RoomMembershipRule,
+		private readonly authorizationHelper: AuthorizationHelper,
+		private readonly authorisationInjectionService: AuthorizationInjectionService
+	) {
+		this.authorisationInjectionService.injectAuthorizationRule(this);
+	}
+
+	public isApplicable(_: User, object: unknown): boolean {
+		const isMatched = object instanceof RoomMemberAuthorizable;
+
+		return isMatched;
+	}
+
+	public hasPermission(user: User, object: RoomMemberAuthorizable, context: AuthorizationContext): boolean {
+		const { requiredPermissions } = context;
+		const hasPermissions = requiredPermissions.every((permission) =>
+			this.hasRequiredPermission(user, object, permission)
+		);
+		return hasPermissions;
+	}
+
+	private hasRequiredPermission(user: User, object: RoomMemberAuthorizable, permission: Permission): boolean {
+		const hasPermission = this.hasDynamicPermission(user, object, permission);
+		const isImmutableUser = this.isImmutableObject(user, object);
+		return hasPermission && !isImmutableUser;
+	}
+
+	private hasDynamicPermission(user: User, object: RoomMemberAuthorizable, permission: Permission): boolean {
+		const hasPermission = this.roomMembershipRule.hasPermission(user, object.roomMembershipAuthorizable, {
+			action: Action.write,
+			requiredPermissions: [permission],
+		});
+
+		const isAdminOfUserSchool = this.isAdminOfUserSchool(user, object);
+		return hasPermission || isAdminOfUserSchool;
+	}
+
+	private isAdminOfUserSchool(user: User, object: RoomMemberAuthorizable): boolean {
+		const canAdministrateSchoolRooms = this.authorizationHelper.hasOneOfPermissions(user, [
+			Permission.SCHOOL_ADMINISTRATE_ROOMS,
+		]);
+		const isSameSchool = object.member.schoolId === user.school.id;
+		return canAdministrateSchoolRooms && isSameSchool;
+	}
+
+	private isImmutableObject(user: User, object: RoomMemberAuthorizable): boolean {
+		const { member } = object;
+		const isRoomOwner = member.roomRoleName == RoleName.ROOMOWNER;
+		const isCurrentUser = member.userId == user.id;
+		return isRoomOwner || isCurrentUser;
+	}
+}
