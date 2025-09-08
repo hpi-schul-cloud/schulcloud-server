391d3a06d5cd921cf1e4af477576546370359bb5
bergatco <129839305+bergatco@users.noreply.github.com>
2025-09-08 17:04:46 +0200

    BC-10071 - H5P in boards - Older h5p minor versions not installed (libraries) (#5816)

--- apps/server/src/modules/h5p-library-management/loggable/h5p-library-management-error.loggable.ts
+++ apps/server/src/modules/h5p-library-management/loggable/h5p-library-management-error.loggable.ts
@@ -1,20 +1,24 @@
 import { Loggable } from '@core/logger';
 import { ErrorLogMessage } from '@core/logger/types';
 import { InternalServerErrorException } from '@nestjs/common';
-import { Error } from 'ldapjs';
 
 export class H5PLibraryManagementErrorLoggable extends InternalServerErrorException implements Loggable {
-	constructor(private readonly library: string, private readonly error: unknown) {
+	constructor(
+		private readonly error: unknown,
+		private readonly data: Record<string, string> = {},
+		private readonly context?: string
+	) {
 		super();
 	}
 
 	// istanbul ignore next
 	public getLogMessage(): ErrorLogMessage {
+		const context = this.context || '';
+		const error = this.error instanceof Error ? this.error : new Error(`Unknown error ${context}.`);
+
 		const logMessage = {
-			error: this.error as Error,
-			data: {
-				library: this.library,
-			},
+			error,
+			data: this.data,
 			stack: this.stack,
 			type: 'Install H5P Content Type Error',
 		};
