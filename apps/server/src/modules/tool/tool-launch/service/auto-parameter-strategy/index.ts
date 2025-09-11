436e13a7a309fa7a092df564490e0a80a7dea8ac
virgilchiriac <virgil.chiriac@dataport.de>
2025-09-11 11:40:53 +0200

    BC-10466 - add auto-publisher strategy for LTI external tool

--- apps/server/src/modules/tool/tool-launch/service/auto-parameter-strategy/index.ts
+++ apps/server/src/modules/tool/tool-launch/service/auto-parameter-strategy/index.ts
@@ -5,3 +5,4 @@ export * from './auto-context-name.strategy';
 export * from './auto-school-number.strategy';
 export * from './auto-group-external-uuid-strategy.service';
 export { AutoMediumIdStrategy } from './auto-medium-id.strategy';
+export { AutoPublisherStrategy } from './auto-publlisher.strategy';
