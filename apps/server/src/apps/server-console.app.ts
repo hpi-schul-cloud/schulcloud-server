c317c14d7c2f8948700bd3237a3f6233f524055c
dyedwiper <maximilian.becker@dataport.de>
2025-09-16 10:43:41 +0200

    Remove unnecessary comments

--- apps/server/src/apps/server-console.app.ts
+++ apps/server/src/apps/server-console.app.ts
@@ -1,10 +1,5 @@
 /* istanbul ignore file */
-/* eslint-disable promise/always-return */
 import { ManagementConsoleModule } from '@modules/management/management-console.app.module';
 import { runConsoleApp } from './helpers/run-console-app';
 
-/**
- * The console is starting the application wrapped into commander.
- * This allows adding console commands to execute provider methods.
- */
 void runConsoleApp(ManagementConsoleModule);
