c7c697b49d593d2bccc4e11161dff9dc47752e69
Maximilian Kreuzkam <maximilian.kreuzkam@capgemini.com>
2025-09-19 10:24:49 +0200

    Set compression level to 3.

--- apps/server/src/modules/common-cartridge/service/common-cartridge-export.service.ts
+++ apps/server/src/modules/common-cartridge/service/common-cartridge-export.service.ts
@@ -103,7 +103,9 @@ export class CommonCartridgeExportService {
 
 	private createArchiver(): archiver.Archiver {
 		const archive = archiver('zip', {
-			store: true,
+			zlib: {
+				level: 3,
+			},
 		});
 
 		archive.on('warning', (err) => {
