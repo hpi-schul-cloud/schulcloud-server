cd91769553ac7616907df20a564a3387d0bf82b1
hoeppner.dataport <marcus.hoeppner@dataport.de>
2025-09-26 14:34:53 +0200

    check BOARD_MANAGE permission on additional board actions

--- apps/server/src/modules/board/uc/board.uc.spec.ts
+++ apps/server/src/modules/board/uc/board.uc.spec.ts
@@ -319,7 +319,7 @@ describe(BoardUc.name, () => {
 						Permission.BOARD_VIEW,
 						Permission.BOARD_EDIT,
 						Permission.BOARD_MANAGE_VIDEOCONFERENCE,
-						Permission.BOARD_MANAGE_READERS_CAN_EDIT,
+						Permission.BOARD_MANAGE,
 						Permission.BOARD_SHARE_BOARD,
 					],
 				});
