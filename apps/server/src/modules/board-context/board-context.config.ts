98b785d780fb0b482e83e2fc68670c96f738bffb
Sergej Hoffmann <97111299+SevenWaysDP@users.noreply.github.com>
2025-10-06 16:02:32 +0200

    BC-10032 - Add implementation of ConfigurationModule (#5900)

---
+++ apps/server/src/modules/board-context/board-context.config.ts
@@ -0,0 +1,18 @@
+import { ConfigProperty, Configuration } from '@infra/configuration';
+import { StringToBoolean } from '@shared/controller/transformer';
+import { IsBoolean } from 'class-validator';
+
+export const BOARD_CONTEXT_PUBLIC_API_CONFIG = 'BOARD_CONTEXT_PUBLIC_API_CONFIG';
+
+@Configuration()
+export class BoardContextPublicApiConfig {
+	@IsBoolean()
+	@StringToBoolean()
+	@ConfigProperty()
+	public FEATURE_COLUMN_BOARD_VIDEOCONFERENCE_ENABLED = true;
+
+	@IsBoolean()
+	@StringToBoolean()
+	@ConfigProperty()
+	public FEATURE_VIDEOCONFERENCE_ENABLED = true;
+}
