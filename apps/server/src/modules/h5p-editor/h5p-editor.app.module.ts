23b80ee9e747011ee5822ffd9e2806b96d0f00f8
SevenWaysDP <sergej.hoffmann@dataport.de>
2025-10-07 17:05:55 +0200

    WIP - add caching

--- apps/server/src/modules/h5p-editor/h5p-editor.app.module.ts
+++ apps/server/src/modules/h5p-editor/h5p-editor.app.module.ts
@@ -1,23 +1,43 @@
 import { CoreModule } from '@core/core.module';
 import { Logger } from '@core/logger';
 import { DB_PASSWORD, DB_URL, DB_USERNAME } from '@imports-from-feathers';
+import { AccessTokenConfig } from '@infra/access-token/access-token.config';
 import { AuthGuardModule, AuthGuardOptions } from '@infra/auth-guard';
 import { AuthorizationClientModule } from '@infra/authorization-client';
 import { RabbitMQWrapperModule } from '@infra/rabbitmq';
 import { S3ClientModule } from '@infra/s3-client';
+import { ValkeyClientModule, ValkeyConfig } from '@infra/valkey-client';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { UserModule } from '@modules/user';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { createConfigModuleOptions } from '@shared/common/config-module-options';
 import { defaultMikroOrmOptions } from '@shared/common/defaultMikroOrmOptions';
 import { H5pEditorConsumer, H5PEditorController } from './controller';
-import { authorizationClientConfig, config, s3ConfigContent, s3ConfigLibraries } from './h5p-editor.config';
+import {
+	authorizationClientConfig,
+	CACHE_TOKEN,
+	config,
+	s3ConfigContent,
+	s3ConfigLibraries,
+} from './h5p-editor.config';
 import { ENTITIES } from './h5p-editor.entity.exports';
 import { H5PAjaxEndpointProvider, H5PEditorProvider, H5PPlayerProvider } from './provider';
 import { H5PContentRepo, LibraryRepo } from './repo';
 import { ContentStorage, H5pEditorContentService, LibraryStorage, TemporaryFileStorage } from './service';
 import { H5PEditorUc } from './uc';
+
+const createValkeyModuleOptions = (configService: ConfigService<AccessTokenConfig>): ValkeyConfig => {
+	const config = {
+		MODE: configService.getOrThrow('SESSION_VALKEY__MODE', { infer: true }),
+		URI: configService.get('SESSION_VALKEY__URI', { infer: true }),
+		SENTINEL_NAME: configService.get('SESSION_VALKEY__SENTINEL_NAME', { infer: true }),
+		SENTINEL_PASSWORD: configService.get('SESSION_VALKEY__SENTINEL_PASSWORD', { infer: true }),
+		SENTINEL_SERVICE_NAME: configService.get('SESSION_VALKEY__SENTINEL_SERVICE_NAME', { infer: true }),
+	};
+
+	return config;
+};
 
 const imports = [
 	AuthorizationClientModule.register(authorizationClientConfig),
@@ -38,6 +58,11 @@ const imports = [
 	ConfigModule.forRoot(createConfigModuleOptions(config)),
 	S3ClientModule.register([s3ConfigContent, s3ConfigLibraries]),
 	AuthGuardModule.register([AuthGuardOptions.JWT]),
+	ValkeyClientModule.registerAsync({
+		injectionToken: CACHE_TOKEN,
+		useFactory: createValkeyModuleOptions,
+		inject: [ConfigService],
+	}),
 ];
 
 const controllers = [H5PEditorController];
--- apps/server/src/modules/h5p-editor/provider/h5p-editor.provider.ts
+++ apps/server/src/modules/h5p-editor/provider/h5p-editor.provider.ts
@@ -1,19 +1,36 @@
+import { ValkeyClient } from '@infra/valkey-client/clients';
+import KeyvValkey from '@keyv/valkey';
 import { H5PEditor, cacheImplementations } from '@lumieducation/h5p-server';
-
+import CachedLibraryStorage from '@lumieducation/h5p-server/build/src/implementation/cache/CachedLibraryStorage';
 import { IH5PEditorOptions, ITranslationFunction } from '@lumieducation/h5p-server/build/src/types';
+import { createCache } from 'cache-manager';
+import Keyv from 'keyv';
+import { CACHE_TOKEN } from '../h5p-editor.config';
 import { ContentStorage, LibraryStorage, TemporaryFileStorage, Translator } from '../service';
 import { h5pConfig, h5pUrlGenerator } from '../service/config/h5p-service-config';
 import EditorPermissionSystem from './editor-permission-system';
 
+
 export const H5PEditorProvider = {
 	provide: H5PEditor,
-	inject: [ContentStorage, LibraryStorage, TemporaryFileStorage],
+	inject: [ContentStorage, LibraryStorage, TemporaryFileStorage, CACHE_TOKEN],
 	async useFactory(
 		contentStorage: ContentStorage,
 		libraryStorage: LibraryStorage,
-		temporaryStorage: TemporaryFileStorage
-	) {
+		temporaryStorage: TemporaryFileStorage,
+		cacheClient: ValkeyClient
+	): Promise<H5PEditor> {
+		const cacheAdapter = createCache({
+			stores: [
+				new Keyv({
+					store: new KeyvValkey(cacheClient.valkeyInstance),
+				}),
+			],
+		});
 		const cache = new cacheImplementations.CachedKeyValueStorage('kvcache');
+		const libraryCache = new cacheImplementations.CachedLibraryStorage(
+			new CachedLibraryStorage(libraryStorage, cacheAdapter)
+		);
 
 		const permissionSystem = new EditorPermissionSystem();
 		const h5pOptions: IH5PEditorOptions = {
@@ -26,7 +43,7 @@ export const H5PEditorProvider = {
 		const h5pEditor = new H5PEditor(
 			cache,
 			h5pConfig,
-			libraryStorage,
+			libraryCache,
 			contentStorage,
 			temporaryStorage,
 			translationFunction,
