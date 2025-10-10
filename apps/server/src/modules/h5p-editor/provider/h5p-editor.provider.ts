c61766ab933dba5e37ce3f5990c9c169e49c3392
SevenWaysDP <sergej.hoffmann@dataport.de>
2025-10-10 08:38:00 +0200

    BC-10526 - Implement H5P caching

--- apps/server/src/modules/h5p-editor/provider/h5p-editor.provider.ts
+++ apps/server/src/modules/h5p-editor/provider/h5p-editor.provider.ts
@@ -1,19 +1,25 @@
-import { H5PEditor, cacheImplementations } from '@lumieducation/h5p-server';
-
+import { cacheImplementations, H5PEditor } from '@lumieducation/h5p-server';
+import CachedLibraryStorage from '@lumieducation/h5p-server/build/src/implementation/cache/CachedLibraryStorage';
 import { IH5PEditorOptions, ITranslationFunction } from '@lumieducation/h5p-server/build/src/types';
+import { Cache } from 'cache-manager';
 import { ContentStorage, LibraryStorage, TemporaryFileStorage, Translator } from '../service';
 import { h5pConfig, h5pUrlGenerator } from '../service/config/h5p-service-config';
 import EditorPermissionSystem from './editor-permission-system';
+import { H5P_CACHE_PROVIDER_TOKEN } from './h5p-cache.provider';
 
 export const H5PEditorProvider = {
 	provide: H5PEditor,
-	inject: [ContentStorage, LibraryStorage, TemporaryFileStorage],
+	inject: [ContentStorage, LibraryStorage, TemporaryFileStorage, H5P_CACHE_PROVIDER_TOKEN],
 	async useFactory(
 		contentStorage: ContentStorage,
 		libraryStorage: LibraryStorage,
-		temporaryStorage: TemporaryFileStorage
-	) {
-		const cache = new cacheImplementations.CachedKeyValueStorage('kvcache');
+		temporaryStorage: TemporaryFileStorage,
+		cacheAdapter: Cache,
+	): Promise<H5PEditor> {
+		const cache = new cacheImplementations.CachedKeyValueStorage('kvcache', cacheAdapter);
+		const cachedLibraryStorage = new cacheImplementations.CachedLibraryStorage(
+			new CachedLibraryStorage(libraryStorage, cacheAdapter)
+		);
 
 		const permissionSystem = new EditorPermissionSystem();
 		const h5pOptions: IH5PEditorOptions = {
@@ -26,7 +32,7 @@ export const H5PEditorProvider = {
 		const h5pEditor = new H5PEditor(
 			cache,
 			h5pConfig,
-			libraryStorage,
+			cachedLibraryStorage,
 			contentStorage,
 			temporaryStorage,
 			translationFunction,
