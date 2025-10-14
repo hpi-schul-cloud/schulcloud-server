a82f2a347a00fba6d802c543f1af24db009a805c
virgilchiriac <virgil.chiriac@dataport.de>
2025-10-14 17:46:24 +0200

    increase coverage

--- apps/server/src/modules/board/uc/column.uc.spec.ts
+++ apps/server/src/modules/board/uc/column.uc.spec.ts
@@ -10,6 +10,7 @@ import { BoardNodeService, ColumnBoardService } from '../service';
 import { BoardNodePermissionService } from '../service/board-node-permission.service';
 import { cardFactory, columnBoardFactory, columnFactory } from '../testing';
 import { ColumnUc } from './column.uc';
+import { CopyElementType, CopyStatus, CopyStatusEnum } from '../../copy-helper';
 
 describe(ColumnUc.name, () => {
 	let module: TestingModule;
@@ -18,6 +19,7 @@ describe(ColumnUc.name, () => {
 	let boardNodePermissionService: DeepMocked<BoardNodePermissionService>;
 	let boardNodeService: DeepMocked<BoardNodeService>;
 	let boardNodeFactory: DeepMocked<BoardNodeFactory>;
+	let columnBoardService: DeepMocked<ColumnBoardService>;
 
 	beforeAll(async () => {
 		module = await Test.createTestingModule({
@@ -50,6 +52,7 @@ describe(ColumnUc.name, () => {
 		boardNodePermissionService = module.get(BoardNodePermissionService);
 		boardNodeService = module.get(BoardNodeService);
 		boardNodeFactory = module.get(BoardNodeFactory);
+		columnBoardService = module.get(ColumnBoardService);
 		await setupEntities([User]);
 	});
 
@@ -251,6 +254,44 @@ describe(ColumnUc.name, () => {
 				await uc.moveCard(user.id, card.id, column.id, 5);
 
 				expect(boardNodeService.move).toHaveBeenCalledWith(card, column, 5);
+			});
+		});
+	});
+
+	describe('copyCard', () => {
+		describe('when something goes wrong', () => {
+			it('should throw UnprocessableEntityException when card has no parent', async () => {
+				const { user, card } = setup();
+				// card.parentId = null;
+				boardNodeService.findByClassAndId.mockResolvedValueOnce(card);
+
+				await expect(uc.copyCard(user.id, card.id, 'school-id')).rejects.toThrowError('Card has no parent column');
+			});
+
+			it('should throw UnprocessableEntityException when parent of card is not a column', async () => {
+				const { user } = setup();
+				const notAColumn = cardFactory.build(); // using a card as a non-column example
+				const card = cardFactory.build({ path: notAColumn.id }); // using a card as a non-column example
+				boardNodeService.findByClassAndId.mockResolvedValueOnce(card);
+				boardNodeService.findByClassAndId.mockResolvedValueOnce(notAColumn);
+				await expect(uc.copyCard(user.id, card.id, 'school-id')).rejects.toThrowError('Parent of card is not a column');
+			});
+
+			it('should throw InternalServerError if copyEntity is not a Card', async () => {
+				const { user, column } = setup();
+				const card = cardFactory.build({ path: column.id });
+				boardNodeService.findByClassAndId.mockResolvedValueOnce(card);
+				boardNodeService.findByClassAndId.mockResolvedValueOnce(column);
+
+				const copyStatus: CopyStatus = {
+					status: CopyStatusEnum.SUCCESS,
+					type: CopyElementType.CARD,
+					elements: [],
+					copyEntity: column, // Intentionally incorrect type to trigger the error
+				};
+				columnBoardService.copyCard.mockResolvedValueOnce(copyStatus);
+
+				await expect(uc.copyCard(user.id, card.id, 'school-id')).rejects.toThrowError('Copied entity is not a card');
 			});
 		});
 	});
