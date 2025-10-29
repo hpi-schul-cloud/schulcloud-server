068efc4ab22b326e80fefe3469e2e70ba5bd9bca
MartinSchuhmacher <mschuhmacher@gmx.de>
2025-10-29 14:53:31 +0100

    adjusting more role mappings for externals

--- apps/server/src/modules/user/repo/user.entity.ts
+++ apps/server/src/modules/user/repo/user.entity.ts
@@ -265,6 +265,8 @@ export class User extends BaseEntityWithTimestamps {
 			setOfPermissions = this.resolveSchoolPermissionsForStudent(setOfPermissions, schoolPermissions);
 		}
 
+		/* Do we need to resolve school permissions for external persons? */
+
 		return setOfPermissions;
 	}
 
