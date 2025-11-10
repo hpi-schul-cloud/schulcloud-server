c85028741fd7e336f40ea6fc3764feb9de963553
MartinSchuhmacher <mschuhmacher@gmx.de>
2025-11-10 09:59:21 +0100

    fixing linter issues

--- apps/server/src/modules/registration/api/dto/request/create-registration.body.params.ts
+++ apps/server/src/modules/registration/api/dto/request/create-registration.body.params.ts
@@ -4,48 +4,52 @@ import { LanguageType } from '@shared/domain/interface';
 import { IsArray, IsEmail, IsEnum, IsMongoId, IsString } from 'class-validator';
 
 export class CreateRegistrationBodyParams implements RegistrationCreateProps {
-    @IsEmail()
-    @ApiProperty({
-        description: 'The mail adress of the new user. Will also be used as username.',
-        required: true,
-        nullable: false,
-    })
-    email!: string;
+	@IsEmail()
+	@ApiProperty({
+		description: 'The mail adress of the new user. Will also be used as username.',
+		required: true,
+		nullable: false,
+	})
+	email!: string;
 
-    @IsString()
-    @ApiProperty({
-        description: 'The firstname of the new user.',
-        required: true,
-        nullable: false,
-    })
-    firstName!: string;
+	@IsString()
+	@ApiProperty({
+		description: 'The firstname of the new user.',
+		required: true,
+		nullable: false,
+	})
+	firstName!: string;
 
-    @IsString()
-    @ApiProperty({
-        description: 'The lastname of the new user.',
-        required: true,
-        nullable: false,
-    })
-    lastName!: string;
+	@IsString()
+	@ApiProperty({
+		description: 'The lastname of the new user.',
+		required: true,
+		nullable: false,
+	})
+	lastName!: string;
 
-    @IsArray()
-    @ApiProperty({
-        description: 'The consent given by the user.',
-        required: true,
-        isArray: true,
-        nullable: false,
-    })
-    consent!: string[];
+	@IsArray()
+	@ApiProperty({
+		description: 'The consent given by the user.',
+		required: true,
+		isArray: true,
+		nullable: false,
+	})
+	consent!: string[];
 
-    @IsEnum(LanguageType)
-    @ApiProperty({ description: 'The chosen language for the registration process.', enum: LanguageType, enumName: 'LanguageType' })
-    language!: LanguageType;
+	@IsEnum(LanguageType)
+	@ApiProperty({
+		description: 'The chosen language for the registration process.',
+		enum: LanguageType,
+		enumName: 'LanguageType',
+	})
+	language!: LanguageType;
 
-    @IsArray()
-    @IsMongoId({ each: true })
-    @ApiProperty({
-        description: 'The IDs of rooms the user is invited to.',
-        required: true,
-    })
-    roomIds!: string[];
+	@IsArray()
+	@IsMongoId({ each: true })
+	@ApiProperty({
+		description: 'The IDs of rooms the user is invited to.',
+		required: true,
+	})
+	roomIds!: string[];
 }
