3b4b62069145674badd69104824d0809bbfb5ad0
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2025-11-13 19:06:00 +0100

    increase test event bus delay

--- apps/server/src/modules/room/api/test/util/wait-for-event-bus.ts
+++ apps/server/src/modules/room/api/test/util/wait-for-event-bus.ts
@@ -1,1 +1,1 @@
-export const waitForEventBus = (ms = 10): Promise<void> => new Promise((resolve) => setTimeout(resolve, ms));
+export const waitForEventBus = (ms = 20): Promise<void> => new Promise((resolve) => setTimeout(resolve, ms));
