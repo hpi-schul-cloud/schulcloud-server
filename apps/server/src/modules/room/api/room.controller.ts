1a8d3755898b5ddf578d0669f9f8a57fa12043ca
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2025-11-19 20:15:10 +0100

    add permissions to room list response

--- apps/server/src/modules/room/api/room.controller.ts
+++ apps/server/src/modules/room/api/room.controller.ts
@@ -32,20 +32,20 @@ import { RoomPaginationParams } from './dto/request/room-pagination.params';
 import { RoomUrlParams } from './dto/request/room.url.params';
 import { UpdateRoomBodyParams } from './dto/request/update-room.body.params';
 import { RoomBoardListResponse } from './dto/response/room-board-list.response';
+import { RoomCreatedResponse } from './dto/response/room-created.response';
 import { RoomDetailsResponse } from './dto/response/room-details.response';
 import { RoomInvitationLinkListResponse } from './dto/response/room-invitation-link-list.response';
-import { RoomItemResponse } from './dto/response/room-item.response';
 import { RoomListResponse } from './dto/response/room-list.response';
 import { RoomMemberListResponse } from './dto/response/room-member-list.response';
 import { RoomRoleResponse } from './dto/response/room-role.response';
 import { RoomStatsListResponse } from './dto/response/room-stats-list.repsonse';
 import { RoomInvitationLinkMapper } from './mapper/room-invitation-link.mapper';
 import { RoomMapper } from './mapper/room.mapper';
+import { RoomArrangementUc } from './room-arrangement.uc';
+import { RoomContentUc } from './room-content.uc';
 import { RoomCopyUc } from './room-copy.uc';
 import { RoomInvitationLinkUc } from './room-invitation-link.uc';
 import { RoomUc } from './room.uc';
-import { RoomContentUc } from './room-content.uc';
-import { RoomArrangementUc } from './room-arrangement.uc';
 
 @ApiTags('Room')
 @JwtAuthentication()
@@ -113,7 +113,7 @@ export class RoomController {
 
 	@Post()
 	@ApiOperation({ summary: 'Create a new room' })
-	@ApiResponse({ status: HttpStatus.OK, description: 'Returns the details of a room', type: RoomItemResponse })
+	@ApiResponse({ status: HttpStatus.CREATED, type: RoomCreatedResponse })
 	@ApiResponse({ status: HttpStatus.BAD_REQUEST, type: ApiValidationError })
 	@ApiResponse({ status: HttpStatus.UNAUTHORIZED, type: UnauthorizedException })
 	@ApiResponse({ status: HttpStatus.FORBIDDEN, type: ForbiddenException })
@@ -121,10 +121,10 @@ export class RoomController {
 	public async createRoom(
 		@CurrentUser() currentUser: ICurrentUser,
 		@Body() createRoomParams: CreateRoomBodyParams
-	): Promise<RoomItemResponse> {
+	): Promise<RoomCreatedResponse> {
 		const room = await this.roomUc.createRoom(currentUser.userId, createRoomParams);
 
-		const response = RoomMapper.mapToRoomItemResponse({ room, isLocked: false });
+		const response = RoomMapper.mapToRoomCreatedResponse(room);
 
 		return response;
 	}
