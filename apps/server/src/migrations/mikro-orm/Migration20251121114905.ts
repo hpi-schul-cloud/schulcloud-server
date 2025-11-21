c0438a604d8a2a5a17e3700cef03b00f6528390c
Constantin Bergatt <constantin.bergatt@dataport.de>
2025-11-21 14:06:48 +0100

    BC-10968 - Update also securityCheck.reason for file records as well

--- apps/server/src/migrations/mikro-orm/Migration20251121114905.ts
+++ apps/server/src/migrations/mikro-orm/Migration20251121114905.ts
@@ -4,7 +4,7 @@ export class Migration20251121114905 extends Migration {
 	public async up(): Promise<void> {
 		// Update file records where securityCheck.reason is null and securityCheck.status is "verified"
 		// Set securityCheck.reason to "Clean"
-		const result = await this.getCollection('files').updateMany(
+		const fileRecords = await this.getCollection('filerecords').updateMany(
 			{
 				'securityCheck.reason': null,
 				'securityCheck.status': 'verified',
@@ -18,7 +18,24 @@ export class Migration20251121114905 extends Migration {
 		);
 
 		console.info(
-			`Updated securityCheck.reason from null to "Clean" for ${result.modifiedCount} of ${result.matchedCount} verified files`
+			`Updated securityCheck.reason from null to "Clean" for ${fileRecords.modifiedCount} of ${fileRecords.matchedCount} verified file records`
+		);
+
+		const files = await this.getCollection('files').updateMany(
+			{
+				'securityCheck.reason': null,
+				'securityCheck.status': 'verified',
+			},
+			{
+				$set: {
+					'securityCheck.reason': 'Clean',
+					'securityCheck.updatedAt': new Date(),
+				},
+			}
+		);
+
+		console.info(
+			`Updated securityCheck.reason from null to "Clean" for ${files.modifiedCount} of ${files.matchedCount} verified files`
 		);
 	}
 
