e11b0888e5851e0fb9241e9fa5e115e027920759
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2025-12-01 16:16:26 +0100

    ignore line in coverage

--- apps/server/src/modules/board/uc/column.uc.ts
+++ apps/server/src/modules/board/uc/column.uc.ts
@@ -94,6 +94,7 @@ export class ColumnUc {
 
 		const card = await this.boardNodeService.findByClassAndId(Card, cardId);
 		if (!card.parentId) {
+			/* istanbul ignore next */
 			throw new UnprocessableEntityException('Card has no parent column');
 		}
 
