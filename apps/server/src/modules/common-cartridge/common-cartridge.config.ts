4d4cd10c35b8b5496c5a056a0180518f557ff668
Phillip Wirth <phillip.wirth@dataport.de>
2025-12-03 10:05:48 +0100

    add COMMON_CARTRIDGE config

--- apps/server/src/modules/common-cartridge/common-cartridge.config.ts
+++ apps/server/src/modules/common-cartridge/common-cartridge.config.ts
@@ -6,6 +6,7 @@ import { CoursesClientConfig } from '@infra/courses-client';
 import { Algorithm } from 'jsonwebtoken';
 
 export interface CommonCartridgeConfig extends LoggerConfig, JwtAuthGuardConfig, CoursesClientConfig {
+	INCOMING_REQUEST_TIMEOUT: number;
 	FEATURE_COMMON_CARTRIDGE_COURSE_IMPORT_MAX_FILE_SIZE: number;
 }
 
@@ -16,6 +17,7 @@ export const authorizationClientConfig: AuthorizationClientConfig = {
 const commonCartridgeConfig: CommonCartridgeConfig = {
 	NEST_LOG_LEVEL: Configuration.get('NEST_LOG_LEVEL') as string,
 	EXIT_ON_ERROR: Configuration.get('EXIT_ON_ERROR') as boolean,
+	INCOMING_REQUEST_TIMEOUT: Configuration.get('COMMON_CARTRIDGE__INCOMING_REQUEST_TIMEOUT_MS') as number,
 	SC_DOMAIN: Configuration.get('SC_DOMAIN') as string,
 	JWT_PUBLIC_KEY: (Configuration.get('JWT_PUBLIC_KEY') as string).replace(/\\n/g, '\n'),
 	JWT_SIGNING_ALGORITHM: Configuration.get('JWT_SIGNING_ALGORITHM') as Algorithm,
