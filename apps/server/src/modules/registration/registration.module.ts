0520f163f9056619dcc2f7de812c5f150b87b57c
hoeppner.dataport <marcus.hoeppner@dataport.de>
2025-12-08 15:04:30 +0100

    chore: fix module imports

--- apps/server/src/modules/registration/registration.module.ts
+++ apps/server/src/modules/registration/registration.module.ts
@@ -1,6 +1,7 @@
 import { AccountModule } from '@modules/account';
 import { RoleService } from '@modules/role';
 import { RoleRepo } from '@modules/role/repo';
+import { RoomMembershipModule } from '@modules/room-membership';
 import { SchoolModule } from '@modules/school';
 import { ServerMailModule } from '@modules/serverDynamicModuleWrappers/server-mail.module';
 import { UserModule } from '@modules/user';
@@ -9,7 +10,7 @@ import { RegistrationService } from './domain';
 import { RegistrationRepo } from './repo';
 
 @Module({
-	imports: [ServerMailModule, UserModule, AccountModule, SchoolModule],
+	imports: [AccountModule, RoomMembershipModule, SchoolModule, ServerMailModule, UserModule],
 	providers: [RegistrationRepo, RegistrationService, RoleService, RoleRepo],
 	exports: [RegistrationService],
 })
