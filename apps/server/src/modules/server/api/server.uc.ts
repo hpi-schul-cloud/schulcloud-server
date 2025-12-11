065257ab447a6a1e35aad2f6546a14d34a1be298
Thomas Feldtkeller <thomas.feldtkeller@dataport.de>
2025-12-11 15:55:23 +0100

    primitive adding runtime config to config api

--- apps/server/src/modules/server/api/server.uc.ts
+++ apps/server/src/modules/server/api/server.uc.ts
@@ -1,3 +1,4 @@
+import { RuntimeConfigService } from '@infra/runtime-config';
 import { BOARD_CONTEXT_PUBLIC_API_CONFIG, BoardContextPublicApiConfig } from '@modules/board-context';
 import { VIDEO_CONFERENCE_PUBLIC_API_CONFIG, VideoConferencePublicApiConfig } from '@modules/video-conference';
 import { Inject, Injectable } from '@nestjs/common';
@@ -10,14 +11,20 @@ export class ServerUc {
 	constructor(
 		@Inject(SERVER_CONFIG_TOKEN) private readonly config: ServerConfig,
 		@Inject(BOARD_CONTEXT_PUBLIC_API_CONFIG) private readonly boardConfig: BoardContextPublicApiConfig,
-		@Inject(VIDEO_CONFERENCE_PUBLIC_API_CONFIG) private readonly videoConferenceConfig: VideoConferencePublicApiConfig
+		@Inject(VIDEO_CONFERENCE_PUBLIC_API_CONFIG) private readonly videoConferenceConfig: VideoConferencePublicApiConfig,
+		private readonly runtimeConfigService: RuntimeConfigService
 	) {}
 
-	public getConfig(): ConfigResponse {
+	public async getConfig(): Promise<ConfigResponse> {
 		const configDto = ConfigResponseMapper.mapToResponse({
 			...this.config,
 			...this.boardConfig,
 			...this.videoConferenceConfig,
+			// TODO: remove from boardConfig, put into seperate DTO definition
+			// TODO: performance issue: multiple calls to runtime config service
+			FEATURE_COLUMN_BOARD_SOCKET_ENABLED: await this.runtimeConfigService.getBoolean(
+				'FEATURE_COLUMN_BOARD_SOCKET_ENABLED'
+			),
 		});
 
 		return configDto;
