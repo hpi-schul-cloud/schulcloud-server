198da9e3d8afa829013fc62ccca099e90508c478
Constantin Bergatt <constantin.bergatt@dataport.de>
2025-12-16 09:02:42 +0100

    BC-10144 - Add workaround comment for tempFilePath assignment in H5PEditorUc

--- apps/server/src/modules/h5p-editor/uc/h5p.uc.ts
+++ apps/server/src/modules/h5p-editor/uc/h5p.uc.ts
@@ -46,6 +46,8 @@ import { LibraryStorage } from '../service';
 import { H5PContentParentType, H5PUploadFile, LumiUserWithContentData } from '../types';
 import { GetLibraryFile } from './dto/h5p-getLibraryFile';
 
+// Wir müssen als Workaround allen Dateien den Typ "unknown.type" als "tempFilePath" zuweisen, da die H5P-Bibliothek sonst Fehler wirft.
+// Siehe: https://github.com/Lumieducation/H5P-Nodejs-library/blob/fb84581b3aa68cfd521e84b6438c0c177d34a4be/packages/h5p-server/src/H5PEditor.ts#L659
 const UNKNOWN_TYPE = 'unknown.type';
 
 @Injectable()
