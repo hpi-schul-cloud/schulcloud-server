50628640e63721ebf0edaacd3ce9e379a39d73ab
bergatco <129839305+bergatco@users.noreply.github.com>
2025-12-18 10:10:47 +0100

    BC-10144 - SVG-Upload für h5p erlauben und Einschränkung der Dateiformate beim Upload in h5p (#5985)

--- apps/server/src/modules/h5p-editor/loggable/index.ts
+++ apps/server/src/modules/h5p-editor/loggable/index.ts
@@ -1,4 +1,6 @@
-export * from './h5p-editor-content-deletion-successful.loggable';
 export * from './h5p-editor-content-copy-successful.loggable';
+export * from './h5p-editor-content-deletion-successful.loggable';
 export * from './h5p-editor-content-invalid-id.loggable-exception';
 export * from './h5p-editor-exchange-invalid-params.loggable-exception';
+export * from './h5p-editor-uc-error.loggable';
+export * from './h5p-editor-uc.loggable';
