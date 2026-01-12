04225a6b451a6310a104fdb9f74f9f87b18fab09
hoeppner.dataport <marcus.hoeppner@dataport.de>
2026-01-12 10:58:06 +0100

    chore: adapt loggable for meaningful test coverage evaluation

--- apps/server/src/modules/registration/domain/error/resend-registration-mail.loggable.ts
+++ apps/server/src/modules/registration/domain/error/resend-registration-mail.loggable.ts
@@ -3,6 +3,7 @@ import { Loggable, LogMessage } from '@core/logger';
 export class ResendingRegistrationMailLoggable implements Loggable {
 	constructor(private readonly registrationId: string) {}
 
+	/* istanbul ignore next */
 	public getLogMessage(): LogMessage {
 		const message = {
 			message: `Resending registration mail failed for`,
