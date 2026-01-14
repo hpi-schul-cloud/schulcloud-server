f4cbeeaeaa9b6d1e9f290705b2f30fc83022952f
virgilchiriac <17074330+virgilchiriac@users.noreply.github.com>
2026-01-14 09:25:54 +0100

    add comment

--- apps/server/src/infra/bilo-client/bilo-media-client.adapter.ts
+++ apps/server/src/infra/bilo-client/bilo-media-client.adapter.ts
@@ -166,6 +166,7 @@ export class BiloMediaClientAdapter {
 
 	private validateAndCorrectCoverUrls(response: BiloMediaQueryResponse): ValidationError | void {
 		if (!isURL(response.data.coverSmall.href) && !isURL(response.data.cover.href)) {
+			// having empty cover URLs is acceptable, as this will allow the default logo to load in front-end
 			response.data.coverSmall.href = '';
 			response.data.cover.href = '';
 		} else {
