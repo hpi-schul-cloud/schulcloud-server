be773bd60b4041ea44dd0fb9b08ea00d94ac4e6c
Sebastian Mueller <sebastian.mueller@dataport.de>
2026-01-16 13:57:20 +0100

    WIP: make valkey mode config optional

--- apps/server/src/infra/auth-guard/config/jwt-auth-guard.config.ts
+++ apps/server/src/infra/auth-guard/config/jwt-auth-guard.config.ts
@@ -5,7 +5,7 @@ export interface JwtAuthGuardConfig {
 	JWT_PUBLIC_KEY: string;
 	JWT_SIGNING_ALGORITHM: Algorithm;
 	SC_DOMAIN: string;
-	SESSION_VALKEY__MODE: ValkeyMode;
+	SESSION_VALKEY__MODE?: ValkeyMode;
 	SESSION_VALKEY__URI?: string;
 	SESSION_VALKEY__SENTINEL_NAME?: string;
 	SESSION_VALKEY__SENTINEL_PASSWORD?: string;
