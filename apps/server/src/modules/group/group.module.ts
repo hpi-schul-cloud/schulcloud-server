a8a81f1a19534bcb33a6368adafb4a2f24eb6aac
Max Bischof <maximilian.bischof@dataport.de>
2026-01-16 15:09:49 +0100

    Provisioning and group module: refactor configuration handling

--- apps/server/src/modules/group/group.module.ts
+++ apps/server/src/modules/group/group.module.ts
@@ -1,15 +1,24 @@
+import { LoggerModule } from '@core/logger';
+import { ConfigurationModule } from '@infra/configuration';
+import { UserChangedSchoolGroupHandlerService } from '@modules/group/service/user-changed-school-group-handler.service';
+import { SchoolModule } from '@modules/school';
 import { forwardRef, Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
-import { SchoolModule } from '@modules/school';
 import { RoleModule } from '../role';
 import { UserModule } from '../user';
+import { GROUP_CONFIG_TOKEN, GroupConfig } from './group.config';
 import { GroupRepo } from './repo';
 import { GroupService } from './service';
-import { LoggerModule } from '@core/logger';
-import { UserChangedSchoolGroupHandlerService } from '@modules/group/service/user-changed-school-group-handler.service';
 
 @Module({
-	imports: [forwardRef(() => UserModule), RoleModule, CqrsModule, LoggerModule, SchoolModule],
+	imports: [
+		forwardRef(() => UserModule),
+		RoleModule,
+		CqrsModule,
+		LoggerModule,
+		SchoolModule,
+		ConfigurationModule.register(GROUP_CONFIG_TOKEN, GroupConfig),
+	],
 	providers: [GroupRepo, GroupService, UserChangedSchoolGroupHandlerService],
 	exports: [GroupService],
 })
--- apps/server/src/modules/provisioning/provisioning.config.ts
+++ apps/server/src/modules/provisioning/provisioning.config.ts
@@ -1,9 +1,45 @@
-export interface ProvisioningConfig {
-	FEATURE_SCHULCONNEX_COURSE_SYNC_ENABLED: boolean;
-	FEATURE_SCHULCONNEX_MEDIA_LICENSE_ENABLED: boolean;
-	PROVISIONING_SCHULCONNEX_POLICIES_INFO_URL: string;
-	PROVISIONING_SCHULCONNEX_GROUP_USERS_LIMIT?: number;
-	FEATURE_SCHULCONNEX_GROUP_PROVISIONING_ENABLED: boolean;
-	FEATURE_OTHER_GROUPUSERS_PROVISIONING_ENABLED: boolean;
-	SCHULCONNEX_COURSE_SYNC_HISTORY_EXPIRATION_SECONDS: number;
+import { ConfigProperty, Configuration } from '@infra/configuration';
+import { StringToBoolean } from '@shared/controller/transformer';
+import { IsBoolean, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export const PROVISIONING_CONFIG_TOKEN = 'PROVISIONING_CONFIG_TOKEN';
+export const PROVISIONING_PUBLIC_API_CONFIG = 'PROVISIONING_PUBLIC_API_CONFIG_TOKEN';
+
+@Configuration()
+export class ProvisioningPublicApiConfig {
+	@ConfigProperty('FEATURE_SCHULCONNEX_COURSE_SYNC_ENABLED')
+	@StringToBoolean()
+	@IsBoolean()
+	public featureSchulconnexCourseSyncEnabled = false;
+
+	@ConfigProperty('FEATURE_SCHULCONNEX_MEDIA_LICENSE_ENABLED')
+	@StringToBoolean()
+	@IsBoolean()
+	public featureSchulconnexMediaLicenseEnabled = false;
+}
+
+@Configuration()
+export class ProvisioningConfig extends ProvisioningPublicApiConfig {
+	@ConfigProperty('PROVISIONING_SCHULCONNEX_POLICIES_INFO_URL')
+	@IsString()
+	public provisioningSchulconnexPoliciesInfoUrl = '';
+
+	@ConfigProperty('PROVISIONING_SCHULCONNEX_GROUP_USERS_LIMIT')
+	@IsOptional()
+	@IsNumber()
+	public provisioningSchulconnexGroupUsersLimit?: number;
+
+	@ConfigProperty('FEATURE_SCHULCONNEX_GROUP_PROVISIONING_ENABLED')
+	@StringToBoolean()
+	@IsBoolean()
+	public featureSchulconnexGroupProvisioningEnabled = false;
+
+	@ConfigProperty('FEATURE_OTHER_GROUPUSERS_PROVISIONING_ENABLED')
+	@StringToBoolean()
+	@IsBoolean()
+	public featureOtherGroupusersProvisioningEnabled = false;
+
+	@ConfigProperty('SCHULCONNEX_COURSE_SYNC_HISTORY_EXPIRATION_SECONDS')
+	@IsNumber()
+	public schulconnexCourseSyncHistoryExpirationSeconds = 864000;
 }
