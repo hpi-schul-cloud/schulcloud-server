e6262f5a4382f93c1ee154efe1fc439636145605
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2026-01-16 16:54:31 +0100

    fix h5p test module

--- apps/server/src/modules/h5p-editor/h5p-editor-test.module.ts
+++ apps/server/src/modules/h5p-editor/h5p-editor-test.module.ts
@@ -1,16 +1,7 @@
-import { DynamicModule, Module } from '@nestjs/common';
-import { MongoDatabaseModuleOptions, MongoMemoryDatabaseModule } from '@testing/database';
+import { Module } from '@nestjs/common';
 import { H5PEditorModule } from './h5p-editor.app.module';
-import { TEST_ENTITIES } from './h5p-editor.entity.exports';
 
 @Module({
-	imports: [H5PEditorModule, MongoMemoryDatabaseModule.forRoot({ entities: TEST_ENTITIES })],
+	imports: [H5PEditorModule],
 })
-export class H5PEditorTestModule {
-	public static forRoot(options?: MongoDatabaseModuleOptions): DynamicModule {
-		return {
-			module: H5PEditorTestModule,
-			imports: [H5PEditorModule, MongoMemoryDatabaseModule.forRoot({ ...options })],
-		};
-	}
-}
+export class H5PEditorTestModule {}
