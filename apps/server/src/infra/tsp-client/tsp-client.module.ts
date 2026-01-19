9f757bc814f1851f52a85df10158b340dcebb7ae
Constantin Bergatt <constantin.bergatt@dataport.de>
2026-01-19 10:39:39 +0100

    BC-11033 - Refactor TSP client module to support new configuration structure with encryption and TSP client options

--- apps/server/src/infra/tsp-client/tsp-client.module.ts
+++ apps/server/src/infra/tsp-client/tsp-client.module.ts
@@ -1,24 +1,22 @@
 import { LoggerModule } from '@core/logger';
 import { ConfigurationModule } from '@infra/configuration';
-import { EncryptionConfig, EncryptionModule } from '@infra/encryption';
+import { EncryptionModule } from '@infra/encryption';
 import { OauthAdapterModule } from '@modules/oauth-adapter';
 import { DynamicModule, Module } from '@nestjs/common';
-import { TSP_CLIENT_CONFIG_TOKEN, TspClientConfig } from './tsp-client-config';
 import { TspClientFactory } from './tsp-client-factory';
+import { TspClientModuleOptions } from './types';
 
 @Module({})
 export class TspClientModule {
-	public static register<T extends EncryptionConfig>(
-		constructor: new () => T,
-		configInjectionToken: string
-	): DynamicModule {
+	public static register(options: TspClientModuleOptions): DynamicModule {
+		const { encryptionConfig, tspClientConfig } = options;
 		return {
 			module: TspClientModule,
 			imports: [
 				LoggerModule,
 				OauthAdapterModule,
-				EncryptionModule.register(constructor, configInjectionToken),
-				ConfigurationModule.register(TSP_CLIENT_CONFIG_TOKEN, TspClientConfig),
+				EncryptionModule.register(encryptionConfig.configConstructor, encryptionConfig.configInjectionToken),
+				ConfigurationModule.register(tspClientConfig.configInjectionToken, tspClientConfig.configConstructor),
 			],
 			providers: [TspClientFactory],
 			exports: [TspClientFactory],
