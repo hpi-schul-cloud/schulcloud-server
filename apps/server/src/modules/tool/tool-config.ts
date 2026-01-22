217e6d872973367e8d351fc2cc28648b76eff45d
Max Bischof <maximilian.bischof@dataport.de>
2026-01-22 16:06:52 +0100

    Update backend URL configurations to use PUBLIC_BACKEND_URL consistently

--- apps/server/src/modules/tool/tool-config.ts
+++ apps/server/src/modules/tool/tool-config.ts
@@ -43,7 +43,7 @@ export class ToolConfig extends ToolPublicApiConfig {
 	@IsNumber()
 	public ctlToolsExternalToolMaxLogoSizeInBytes = 300000;
 
-	@ConfigProperty('PUBLIC_BACKEND_URL')
+	@ConfigProperty('CTL_TOOLS_BACKEND_URL')
 	@IsUrl({ require_tld: false })
 	public ctlToolsBackendUrl!: string;
 
