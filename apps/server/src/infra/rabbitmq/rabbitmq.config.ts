365c44fdd16ff345b0f9e7ece8c4ccc66756b60b
SevenWaysDP <sergej.hoffmann@dataport.de>
2026-01-28 11:11:26 +0100

    refactor: remove RabbitMQ configuration from environment files and update RabbitMqConfig defaults

--- apps/server/src/infra/rabbitmq/rabbitmq.config.ts
+++ apps/server/src/infra/rabbitmq/rabbitmq.config.ts
@@ -4,17 +4,25 @@ import { IsNumber, IsUrl } from 'class-validator';
 import { InternalRabbitMqConfig } from './rabbitmq-module.options';
 
 export const RABBITMQ_CONFIG_TOKEN = 'RABBITMQ_CONFIG_TOKEN';
+
+/**
+ * This is default Configuration for the RabbitMQ.
+ * if you need to read values from different env variables, create your own config class
+ * implementing InternalRabbitMqConfig and provide it via the RabbitMqModule.register method.
+ */
 @Configuration()
 export class RabbitMqConfig implements InternalRabbitMqConfig {
+	// Please don't change the global prefetch count for the existing exchanges.
+	// If you need individual prefetch counts for each consumer, please create a separate Config of RabbitMqModule with channels for your deployment.
 	@ConfigProperty('RABBITMQ_GLOBAL_PREFETCH_COUNT')
 	@StringToNumber()
 	@IsNumber()
-	public prefetchCount!: number;
+	public prefetchCount = 5;
 
 	@IsNumber()
 	@StringToNumber()
 	@ConfigProperty('RABBITMQ_HEARTBEAT_INTERVAL_IN_SECONDS')
-	public heartBeatIntervalInSeconds!: number;
+	public heartBeatIntervalInSeconds = 20;
 
 	@IsUrl({ require_tld: false, require_valid_protocol: false })
 	@ConfigProperty('RABBITMQ_URI')
