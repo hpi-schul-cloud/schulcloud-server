4f3f3202fe20c0188ddca213463b2905121b568e
SevenWaysDP <sergej.hoffmann@dataport.de>
2026-01-28 13:03:26 +0100

    fix: update feature flag for room sharing in share token creation tests

--- apps/server/src/modules/sharing/api/test/sharing-create-token.api.spec.ts
+++ apps/server/src/modules/sharing/api/test/sharing-create-token.api.spec.ts
@@ -335,7 +335,7 @@ describe(`share token creation (api)`, () => {
 				await em.persist([school, teacherAccount, teacherUser, room, group, roomMembership, roomOwnerRole]).flush();
 				em.clear();
 
-				Configuration.set('FEATURE_COURSE_SHARE', true);
+				config.featureRoomShare = true;
 
 				const loggedInClient = await apiClient.login(teacherAccount);
 
