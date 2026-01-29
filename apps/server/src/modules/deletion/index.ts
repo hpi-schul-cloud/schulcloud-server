9c114a83b9e9e013bc000cf76a5f44ee9152ce7c
Max Bischof <106820326+bischofmax@users.noreply.github.com>
2026-01-29 12:55:23 +0100

    Add config to deletion module

--- apps/server/src/modules/deletion/index.ts
+++ apps/server/src/modules/deletion/index.ts
@@ -1,7 +1,6 @@
 export { DeletionModule } from './deletion.module';
-export { DeletionConfig } from './deletion.config';
 export { DomainDeletionReportBuilder, DomainOperationReportBuilder } from './domain/builder';
-export { DomainName, OperationType, StatusModel } from './domain/types';
+export { OperationReportHelper } from './domain/helper';
 export { DomainDeletionReport, DomainOperationReport } from './domain/interface';
 export { DeletionErrorLoggableException } from './domain/loggable-exception';
-export { OperationReportHelper } from './domain/helper';
+export { DomainName, OperationType, StatusModel } from './domain/types';
