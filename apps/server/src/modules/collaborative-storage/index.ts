b576a79b6beafb49cdfa6084d51510a41e5fcc13
Constantin Bergatt <constantin.bergatt@dataport.de>
2026-01-30 13:50:43 +0100

    BC-11033 - refactor: remove unused COLLABORATIVE_STORAGE_CONFIG_TOKEN export

--- apps/server/src/modules/collaborative-storage/index.ts
+++ apps/server/src/modules/collaborative-storage/index.ts
@@ -1,3 +1,2 @@
-export { COLLABORATIVE_STORAGE_CONFIG_TOKEN } from './collaborative-storage.config';
 export { CollaborativeStorageModule } from './collaborative-storage.module';
 export { CollaborativeStorageService, TeamDto, TeamPermissionsDto, TeamUserDto } from './services';
