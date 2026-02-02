1c8f346e0c66e00464fd086caaa9dce5f6b0cbc0
Constantin Bergatt <constantin.bergatt@dataport.de>
2026-02-02 08:40:08 +0100

    BC-11033 - refactor: update URL validation in DeletionConsoleConfig to require TLD

--- apps/server/src/modules/deletion-console/deletion-console.config.ts
+++ apps/server/src/modules/deletion-console/deletion-console.config.ts
@@ -6,7 +6,7 @@ export const DELETION_CONSOLE_CONFIG_TOKEN = 'DELETION_CONSOLE_CONFIG_TOKEN';
 @Configuration()
 export class DeletionConsoleConfig {
 	@ConfigProperty('ADMIN_API_CLIENT__BASE_URL')
-	@IsUrl()
+	@IsUrl({ require_tld: false })
 	public adminApiClientBaseUrl!: string;
 
 	@ConfigProperty('ADMIN_API_CLIENT__API_KEY')
