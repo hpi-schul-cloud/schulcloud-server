43a3908ecd364572ca280fd8e39e81a8233b19c3
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2026-02-02 14:28:02 +0100

    remove line

--- test/services/fileStorage/utils/filePermissionHelper.test.js
+++ test/services/fileStorage/utils/filePermissionHelper.test.js
@@ -30,7 +30,6 @@ describe('filePermissionHelper', () => {
 			const promises = [
 				FileModel.create(fixtures.files),
 				userModel.create(fixtures.users),
-				// RoleModel.create(fixtures.roles),
 				courseModel.create(fixtures.courses),
 			];
 
