b513ba4e7678d45707e90357d918a84ad6665047
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2026-02-02 14:06:08 +0100

    refactor H5PEditorTestModule

--- apps/server/src/modules/h5p-editor/h5p-editor-test.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { H5PEditorModule } from './h5p-editor.app.module';
-
-@Module({
-	imports: [H5PEditorModule],
-})
-export class H5PEditorTestModule {}
