3cdcdc5ca3156794d0e18869ab5218511fef01f1
Max Bischof <maximilian.bischof@dataport.de>
2026-02-03 15:27:29 +0100

    Add feature toggle for Nextcloud integration

--- apps/server/src/modules/collaborative-storage/collaborative-storage.config.ts
+++ apps/server/src/modules/collaborative-storage/collaborative-storage.config.ts
@@ -1,24 +1,34 @@
 import { InternalCollaborativeStorageAdapterConfig } from '@infra/collaborative-storage';
 import { ConfigProperty, Configuration } from '@infra/configuration';
-import { IsString, IsUrl } from 'class-validator';
+import { StringToBoolean } from '@shared/controller/transformer';
+import { IsBoolean, IsString, IsUrl, ValidateIf } from 'class-validator';
 
 export const COLLABORATIVE_STORAGE_CONFIG_TOKEN = 'COLLABORATIVE_STORAGE_CONFIG_TOKEN';
 
 @Configuration()
 export class CollaborativeStorageConfig implements InternalCollaborativeStorageAdapterConfig {
+	@ConfigProperty('FEATURE_NEXTCLOUD_TEAM_FILES_ENABLED')
+	@IsBoolean()
+	@StringToBoolean()
+	public featureNextcloudTeamFilesEnabled = false;
+
 	@ConfigProperty('NEXTCLOUD_BASE_URL')
 	@IsUrl({ require_tld: false })
+	@ValidateIf((config: CollaborativeStorageConfig) => config.featureNextcloudTeamFilesEnabled)
 	public nextcloudBaseUrl = 'http://nextcloud.localhost:9090';
 
 	@ConfigProperty('NEXTCLOUD_ADMIN_USERNAME')
 	@IsString()
+	@ValidateIf((config: CollaborativeStorageConfig) => config.featureNextcloudTeamFilesEnabled)
 	public nextcloudAdminUsername = 'admin';
 
 	@ConfigProperty('NEXTCLOUD_ADMIN_PASSWORD')
 	@IsString()
+	@ValidateIf((config: CollaborativeStorageConfig) => config.featureNextcloudTeamFilesEnabled)
 	public nextcloudAdminPassword = 'admin';
 
 	@ConfigProperty('NEXTCLOUD_SOCIALLOGIN_OIDC_INTERNAL_NAME')
 	@IsString()
+	@ValidateIf((config: CollaborativeStorageConfig) => config.featureNextcloudTeamFilesEnabled)
 	public oidcInternalName = 'SchulcloudNextcloud';
 }
