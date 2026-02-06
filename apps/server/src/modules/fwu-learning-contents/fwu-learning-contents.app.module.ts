52ed8a03edb7dc321a4dcefa79e56f26aa2d022d
Max <53796487+dyedwiper@users.noreply.github.com>
2026-02-06 16:04:01 +0100

    BC-11203 Add session handling to fwu-app (#6092)

--- apps/server/src/modules/fwu-learning-contents/fwu-learning-contents.app.module.ts
+++ apps/server/src/modules/fwu-learning-contents/fwu-learning-contents.app.module.ts
@@ -2,15 +2,29 @@ import { CoreModule } from '@core/core.module';
 import { LoggerModule } from '@core/logger';
 import { AuthGuardModule, AuthGuardOptions } from '@infra/auth-guard';
 import { S3ClientModule } from '@infra/s3-client';
+import { ValkeyClientModule, ValkeyConfig } from '@infra/valkey-client';
+import { SESSION_VALKEY_CLIENT } from '@modules/authentication/authentication-config';
 import { HttpModule } from '@nestjs/axios';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
-import { CacheModule, CacheInterceptor } from '@nestjs/cache-manager';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { createConfigModuleOptions } from '@shared/common/config-module-options';
 import { FwuLearningContentsController } from './controller/fwu-learning-contents.controller';
-import { config, s3Config } from './fwu-learning-contents.config';
+import { config, FwuLearningContentsConfig, s3Config } from './fwu-learning-contents.config';
 import { FwuLearningContentsUc } from './uc/fwu-learning-contents.uc';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+
+const createValkeyModuleOptions = (configService: ConfigService<FwuLearningContentsConfig>): ValkeyConfig => {
+	const config = {
+		MODE: configService.getOrThrow('SESSION_VALKEY__MODE', { infer: true }),
+		URI: configService.get('SESSION_VALKEY__URI', { infer: true }),
+		SENTINEL_NAME: configService.get('SESSION_VALKEY__SENTINEL_NAME', { infer: true }),
+		SENTINEL_PASSWORD: configService.get('SESSION_VALKEY__SENTINEL_PASSWORD', { infer: true }),
+		SENTINEL_SERVICE_NAME: configService.get('SESSION_VALKEY__SENTINEL_SERVICE_NAME', { infer: true }),
+	};
+
+	return config;
+};
 
 @Module({
 	imports: [
@@ -23,6 +37,11 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
 		ConfigModule.forRoot(createConfigModuleOptions(config)),
 		S3ClientModule.register([s3Config]),
 		AuthGuardModule.register([AuthGuardOptions.JWT]),
+		ValkeyClientModule.registerAsync({
+			injectionToken: SESSION_VALKEY_CLIENT,
+			useFactory: createValkeyModuleOptions,
+			inject: [ConfigService],
+		}),
 	],
 	controllers: [FwuLearningContentsController],
 	providers: [
--- apps/server/src/modules/fwu-learning-contents/fwu-learning-contents.config.ts
+++ apps/server/src/modules/fwu-learning-contents/fwu-learning-contents.config.ts
@@ -1,11 +1,17 @@
 import { Configuration } from '@hpi-schul-cloud/commons';
 import { JwtAuthGuardConfig } from '@infra/auth-guard';
 import { S3Config } from '@infra/s3-client';
+import { ValkeyMode } from '@infra/valkey-client';
 import { Algorithm } from 'jsonwebtoken';
 
 export interface FwuLearningContentsConfig extends JwtAuthGuardConfig {
 	NEST_LOG_LEVEL: string;
 	INCOMING_REQUEST_TIMEOUT: number;
+	SESSION_VALKEY__MODE?: ValkeyMode;
+	SESSION_VALKEY__URI?: string;
+	SESSION_VALKEY__SENTINEL_NAME?: string;
+	SESSION_VALKEY__SENTINEL_PASSWORD?: string;
+	SESSION_VALKEY__SENTINEL_SERVICE_NAME?: string;
 }
 
 export const FWU_CONTENT_S3_CONNECTION = 'FWU_CONTENT_S3_CONNECTION';
@@ -30,7 +36,20 @@ const jwtAuthGuardConfig: JwtAuthGuardConfig = {
 const fwuLearningContentsConfig: FwuLearningContentsConfig = {
 	NEST_LOG_LEVEL: Configuration.get('NEST_LOG_LEVEL') as string,
 	INCOMING_REQUEST_TIMEOUT: Configuration.get('FWU_CONTENT__INCOMING_REQUEST_TIMEOUT_MS') as number,
+	SESSION_VALKEY__MODE: Configuration.get('SESSION_VALKEY__MODE') as ValkeyMode,
+	SESSION_VALKEY__URI: Configuration.has('SESSION_VALKEY__URI')
+		? (Configuration.get('SESSION_VALKEY__URI') as string)
+		: undefined,
+	SESSION_VALKEY__SENTINEL_NAME: Configuration.has('SESSION_VALKEY__SENTINEL_NAME')
+		? (Configuration.get('SESSION_VALKEY__SENTINEL_NAME') as string)
+		: undefined,
+	SESSION_VALKEY__SENTINEL_PASSWORD: Configuration.has('SESSION_VALKEY__SENTINEL_PASSWORD')
+		? (Configuration.get('SESSION_VALKEY__SENTINEL_PASSWORD') as string)
+		: undefined,
+	SESSION_VALKEY__SENTINEL_SERVICE_NAME: Configuration.has('SESSION_VALKEY__SENTINEL_SERVICE_NAME')
+		? (Configuration.get('SESSION_VALKEY__SENTINEL_SERVICE_NAME') as string)
+		: undefined,
 	...jwtAuthGuardConfig,
 };
 
-export const config = () => fwuLearningContentsConfig;
+export const config = (): FwuLearningContentsConfig => fwuLearningContentsConfig;
