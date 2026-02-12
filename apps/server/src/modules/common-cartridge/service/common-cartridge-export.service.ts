63380e344c4bd6561e04579ab3d8e728611848a6
Simone Radtke <simone.radtke@capgemini.com>
2026-02-12 11:52:25 +0100

    EW-1382 Clean up

--- apps/server/src/modules/common-cartridge/service/common-cartridge-export.service.ts
+++ apps/server/src/modules/common-cartridge/service/common-cartridge-export.service.ts
@@ -297,10 +297,8 @@ export class CommonCartridgeExportService {
 			identifier: createIdentifier(card.id),
 		});
 
-		/* 		await Promise.all(
-			card.elements.map((element) => this.addCardElementToOrganization(element, version, cardOrganization))
-		); */
-
+		// INFO: for await keeps the order of files in cards in the correct order
+		// with Promise.all, the order of files would be random
 		for await (const cardElement of card.elements) {
 			await this.addCardElementToOrganization(cardElement, version, cardOrganization);
 		}
