2669a90d03f4ce22937c0a0dbfb257e9b1347cf7
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2026-02-23 12:10:56 +0100

    Cleanup

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,10 +1,8 @@
-/* eslint-disable import/no-dynamic-require */
-/* eslint-disable global-require */
 const mongoose = require('mongoose');
 const uriFormat = require('mongodb-uri');
 const { Configuration } = require('@hpi-schul-cloud/commons');
-const logger = require('../logger');
 const { ENVIRONMENTS } = require('../../config/environments');
+const logger = require('../logger');
 
 const encodeMongoURI = (urlString) => {
 	if (urlString) {
@@ -14,20 +12,13 @@ const encodeMongoURI = (urlString) => {
 	return urlString;
 };
 
-/**
- * creates the initial connection to a mongodb.
- * see https://mongoosejs.com/docs/connections.html#error-handling for error handling
- *
- * @returns {Promise} rejects on initial errors
- */
 function connect() {
 	mongoose.Promise = global.Promise;
 	logger.info('connect to database host');
 
 	const mongooseOptions = {
 		autoIndex: Configuration.get('NODE_ENV') !== ENVIRONMENTS.PRODUCTION,
-		maxPoolSize:  Configuration.get('MONGOOSE_CONNECTION_POOL_SIZE'), // https://mongoosejs.com/docs/migrating_to_6.html#mongodb-driver-40
-		// useNewUrlParser: true, @DEPRICATED in mongoose 6, now default
+		maxPoolSize:  Configuration.get('MONGOOSE_CONNECTION_POOL_SIZE'),
 	};
 
 	const auth = {};
@@ -41,21 +32,16 @@ function connect() {
 		mongooseOptions.auth = auth;
 	}
 
-	// https://mongoosejs.com/docs/6.x/docs/migrating_to_6.html#strictquery-is-removed-and-replaced-by-strict
-	// https://mongoosejs.com/docs/7.x/docs/migrating_to_7.html#strictquery
-	mongoose.set('strictQuery', false);
-
 	if (Configuration.get('FEATURE_MONGOOSE_LOGGING_ENABLED')) {
 		mongoose.set('debug', true);
-	} else {
-		logger.info('database debug log is globally disabled');
+		logger.info('database debug log is globally enabled');
 	}
 
 	return mongoose.connect(encodeMongoURI(Configuration.get('DB_URL')), mongooseOptions).then((resolved) => {
-		// handle errors that appear after connection setup
 		mongoose.connection.on('error', (err) => {
 			logger.error(err);
 		});
+
 		return resolved;
 	});
 }
