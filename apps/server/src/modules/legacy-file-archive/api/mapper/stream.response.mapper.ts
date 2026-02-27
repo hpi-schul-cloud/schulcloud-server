7a21f12d63802e71662eecf08293d2079299a340
Max Bischof <maximilian.bischof@dataport.de>
2026-02-27 08:27:23 +0100

    Refactor StreamableFileMapper to simplify disposition handling

--- apps/server/src/modules/legacy-file-archive/api/mapper/stream.response.mapper.ts
+++ apps/server/src/modules/legacy-file-archive/api/mapper/stream.response.mapper.ts
@@ -3,19 +3,11 @@ import { GetFileResponse } from '../../domain';
 
 export class StreamableFileMapper {
 	public static fromResponse(fileResponse: GetFileResponse): StreamableFile {
-		let disposition: string;
-
-		if (fileResponse.contentType === 'application/pdf') {
-			disposition = `inline;`;
-		} else {
-			disposition = `attachment;`;
-		}
-
 		const encodedFileName = encodeURIComponent(fileResponse.name);
 
 		const streamableFile = new StreamableFile(fileResponse.data, {
 			type: fileResponse.contentType,
-			disposition: `${disposition}; filename="${encodedFileName}"; filename*=UTF-8''${encodedFileName}`,
+			disposition: `attachment; filename="${encodedFileName}"; filename*=UTF-8''${encodedFileName}`,
 			length: fileResponse.contentLength,
 		});
 
