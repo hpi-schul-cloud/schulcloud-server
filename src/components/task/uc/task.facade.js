e7dad9bd27c5fca11b44f44ad5c6a9ae84ad1788
virgilchiriac <17074330+virgilchiriac@users.noreply.github.com>
2026-03-03 21:34:38 +0100

    remove obsolete v2 user deletion

--- src/components/task/uc/task.facade.js
+++
@@ -1,9 +0,0 @@
-const deleteUserUc = require('./deleteUserData.uc');
-
-const facade = {
-	deleteUserData: deleteUserUc.deleteUserRelatedData,
-};
-
-module.exports = function setupUsersFacade(app) {
-	app.registerFacade('/tasks/v2', facade);
-};
