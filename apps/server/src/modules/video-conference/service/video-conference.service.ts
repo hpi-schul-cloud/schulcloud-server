b89f5f53695e85d30178f08395dbd1cd74c74bbe
wolfganggreschus <wolfgang@greschus.de>
2026-03-06 14:00:41 +0100

    Set logoutUrl to an empty string in VideoConferenceService

--- apps/server/src/modules/video-conference/service/video-conference.service.ts
+++ apps/server/src/modules/video-conference/service/video-conference.service.ts
@@ -219,7 +219,7 @@ export class VideoConferenceService {
 				return {
 					scopeId,
 					scopeName: VideoConferenceScope.COURSE,
-					logoutUrl: `${this.hostUrl}/courses/${scopeId}?activeTab=tools`,
+					logoutUrl: ``,
 					//logoutUrl: `${this.hostUrl}/courses/${scopeId}?activeTab=tools`,
 					title: ensureMinTitleLength(course.name),
 				};
