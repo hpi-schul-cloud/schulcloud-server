e9060dddb124113030dc43f0de3b8640d1aea299
Max Bischof <maximilian.bischof@dataport.de>
2026-03-10 13:07:55 +0100

    Remove unused file-auth-context.service export from index

--- apps/server/src/modules/files/domain/index.ts
+++ apps/server/src/modules/files/domain/index.ts
@@ -1,7 +1,6 @@
 export * from './do';
 export * from './download-archive.service';
 export * from './factory';
-export * from './file-auth-context.service';
 export * from './legacy-file-storage.adapter';
 export * from './loggable';
 export * from './types';
