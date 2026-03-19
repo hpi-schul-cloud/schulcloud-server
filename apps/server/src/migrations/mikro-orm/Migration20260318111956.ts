67ac63fc306dc23544e9f012a6eb4b9c9f951ad1
dyedwiper <maximilian.becker@dataport.de>
2026-03-19 12:12:34 +0100

    Add more collections to migration

--- apps/server/src/migrations/mikro-orm/Migration20260318111956.ts
+++ apps/server/src/migrations/mikro-orm/Migration20260318111956.ts
@@ -6,13 +6,26 @@ export class Migration20260318111956 extends Migration {
 
 		await this.getCollection('activations').drop();
 		await this.getCollection('analytics').drop();
+		await this.getCollection('column-board-target').drop();
 		await this.getCollection('consents').drop();
 		await this.getCollection('consents_history').drop();
+		await this.getCollection('course_external_tools').drop();
 		await this.getCollection('dashboarddefaultreference').drop();
 		await this.getCollection('directories').drop();
+		await this.getCollection('drawings').drop();
+		await this.getCollection('external-tool-config').drop();
+		await this.getCollection('external-tool-config-entity').drop();
+		await this.getCollection('filepermissionmodels').drop();
 		await this.getCollection('keys').drop();
+		await this.getCollection('ltitools').drop();
 		await this.getCollection('newshistories').drop();
+		await this.getCollection('nexboardcontents').drop();
 		await this.getCollection('problems').drop();
+		await this.getCollection('pseudonyms').drop();
+		await this.getCollection('room-members').drop();
+		await this.getCollection('trashbins').drop();
+		await this.getCollection('user-school-embeddable').drop();
+		await this.getCollection('user-source-options-entity').drop();
 		await this.getCollection('webuntismetadatas').drop();
 
 		console.log('Finished removing unused collections');
