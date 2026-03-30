63a5c3759b131507cdb9e49395d1a5bd3229b851
jan-vcapgemini <59438728+jan-vcapgemini@users.noreply.github.com>
2026-03-30 15:54:10 +0200

    EW-1400: Fix erwin strategy tests added missing systemId to account

--- apps/server/src/modules/authentication/strategy/erwin.strategy.spec.ts
+++ apps/server/src/modules/authentication/strategy/erwin.strategy.spec.ts
@@ -84,7 +84,7 @@ describe(ErwinStrategy.name, () => {
 		schoolId: '',
 		secondarySchools: [],
 	};
-	const account = new Account({ id: accountId, username: 'username' });
+	const account = new Account({ id: accountId, username: 'username', systemId: erwinSystemId });
 	const tokenDto: {
 		systemId?: string;
 		idToken: string;
