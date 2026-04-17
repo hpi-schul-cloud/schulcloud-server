00d1627258f79ec0fd6eb65bd6107f36ab326ce6
Thomas Feldtkeller <thomas.feldtkeller@dataport.de>
2026-04-17 11:43:31 +0200

    BC-11501 - create room from team (#6242)
    * introduce new permission TEAM_EXPORT_TO_ROOM to teamowner
    
    * create new endpoint
    
    * check permissions
    
    * introduce featureflag
    
    * add user as owner
    
    * map team colors to room colors

---
+++ apps/server/src/modules/team/api/index.ts
@@ -0,0 +1,4 @@
+export { TeamController } from './team.controller';
+export { TeamUc as TeamExportUc } from './team.uc';
+export { TeamCreateRoomResponse as TeamExportRoomResponse } from './dto/response/team-export-room.response';
+export { TeamUrlParams } from './dto/request/team.url.params';
