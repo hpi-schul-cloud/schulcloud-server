522ede47ea8236a8e5dfda7278ee8d4d2c766a8d
jan-vcapgemini <59438728+jan-vcapgemini@users.noreply.github.com>
2026-04-21 17:46:43 +0200

    EW-1443: Provide COMMON_CARTRIDGE_CONFIG_TOKEN

--- apps/server/src/modules/common-cartridge/common-cartridge.module.ts
+++ apps/server/src/modules/common-cartridge/common-cartridge.module.ts
@@ -5,6 +5,7 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { API_HOST_CONFIG_TOKEN, ApiHostConfig } from './api-client.config';
+import { COMMON_CARTRIDGE_CONFIG_TOKEN, CommonCartridgeConfig } from './common-cartridge.config';
 import { CommonCartridgeImportHandler } from './handler/common-cartridge-import.handler';
 import { CommonCartridgeExportService, CommonCartridgeImportService } from './service';
 import { CommonCartridgeExportMapper } from './service/common-cartridge-export.mapper';
@@ -20,6 +21,7 @@ import { CommonCartridgeUc } from './uc/common-cartridge.uc';
 		CommonCartridgeClientsModule.register(API_HOST_CONFIG_TOKEN, ApiHostConfig),
 	],
 	providers: [
+		{ provide: COMMON_CARTRIDGE_CONFIG_TOKEN, useClass: CommonCartridgeConfig },
 		CommonCartridgeExportMapper,
 		CommonCartridgeImportMapper,
 		CommonCartridgeUc,
