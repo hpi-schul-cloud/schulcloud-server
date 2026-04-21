1cbc37d8e8383d42755b151835108ed0e1831c87
jan-vcapgemini <59438728+jan-vcapgemini@users.noreply.github.com>
2026-04-21 17:24:34 +0200

    EW-1443: Test file size limit set file size limit to 1MB

--- apps/server/src/modules/common-cartridge/common-cartridge.config.ts
+++ apps/server/src/modules/common-cartridge/common-cartridge.config.ts
@@ -6,7 +6,7 @@ export const COMMON_CARTRIDGE_PUBLIC_API_CONFIG_TOKEN = 'COMMON_CARTRIDGE_PUBLIC
 export const COMMON_CARTRIDGE_CONFIG_TOKEN = 'COMMON_CARTRIDGE_CONFIG_TOKEN';
 
 // 1GB in bytes - shared constant for both frontend and backend
-export const DEFAULT_MAX_FILE_SIZE_BYTES = 1073741824;
+export const DEFAULT_MAX_FILE_SIZE_BYTES = 1048576;
 
 @Configuration()
 export class CommonCartridgePublicApiConfig {
