4c5ef480c14a400164ebb704ea45ce73df3832f0
hoeppner.dataport <marcus.hoeppner@dataport.de>
2026-04-24 08:53:05 +0200

    chore: improve log format of loggable message

--- apps/server/src/modules/board/loggable/board-error-loggable-exception.ts
+++ apps/server/src/modules/board/loggable/board-error-loggable-exception.ts
@@ -1,6 +1,25 @@
 import { Loggable, LogMessage, ValidationErrorLogMessage } from '@core/logger';
 import { BadGatewayException } from '@nestjs/common';
 
+const SEPARATOR = '•';
+const SEPARATOR_REPLACEMENT = '-';
+
+/**
+ * Loggable exception for reported board errors
+ *
+ * This exception is used to capture and log errors related to board operations, including context such as board, school, user, and retry information.
+ *
+ * Example output of `getLogMessage()`:
+ *
+ * ```json
+ * {
+ *   "type": "board-error-report",
+ *   "message": "1.2•https://example.com/board/123•VALIDATION_ERROR•123•456•789•2•step1-step2-step3"
+ * }
+ * ```
+ * the message uses a separator-based format to encode multiple pieces of information
+ * important: the version (v) should be updated whenever the format of the log message changes to ensure proper and easy parsing in grafana and backward compatibility
+ */
 export class BoardErrorLoggableException extends BadGatewayException implements Loggable {
 	constructor(
 		public readonly message: string,
@@ -17,7 +36,7 @@ export class BoardErrorLoggableException extends BadGatewayException implements 
 
 	public getLogMessage(): LogMessage | ValidationErrorLogMessage {
 		const data = {
-			v: '1.1',
+			v: '1.2', // update this value on every format change
 			url: this.url,
 			type: this.errorType,
 			boardId: this.boardId,
@@ -28,20 +47,19 @@ export class BoardErrorLoggableException extends BadGatewayException implements 
 		};
 		return {
 			type: 'board-error-report',
-			message: this.toLogfmt(data),
+			message: this.composeMessage(data),
 		};
 	}
 
-	private toLogfmt(data: Record<string, string | number>): string {
-		const escape = (str: string): string => str.replace(/\\/g, '\\\\').replace(/"/g, '\\"');
+	private composeMessage(data: Record<string, string | number>): string {
+		const values = Object.values(data).map((value) => String(value));
+		const cleanedValues = this.cleanData(values);
+		const message = cleanedValues.join(SEPARATOR);
 
-		const escapeValue = (str: string): string => (/[=\s"]/.test(str) ? `"${escape(str)}"` : str);
+		return message;
+	}
 
-		return Object.entries(data)
-			.map(([key, value]) => {
-				const val = escapeValue(String(value));
-				return `${key}=${val}`;
-			})
-			.join(' ');
+	private cleanData(values: string[]): string[] {
+		return values.map((item) => item.replaceAll(SEPARATOR, SEPARATOR_REPLACEMENT));
 	}
 }
