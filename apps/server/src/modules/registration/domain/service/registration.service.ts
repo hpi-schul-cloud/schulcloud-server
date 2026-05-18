86f1507f11b610f95f0be8e54ede1da99f7885b0
hoeppner.dataport <marcus.hoeppner@dataport.de>
2026-05-18 16:05:18 +0200

    chore: ensure empty import hash of user entity after completing the registration

--- apps/server/src/modules/registration/domain/service/registration.service.ts
+++ apps/server/src/modules/registration/domain/service/registration.service.ts
@@ -182,7 +182,7 @@ export class RegistrationService {
 		userDo.consent = userDo.consent ?? this.createUserConsent();
 		userDo.preferences = userDo.preferences ?? { firstLogin: false };
 		userDo.language = userDo.language ?? language;
-		delete userDo.importHash;
+		userDo.importHash = undefined;
 
 		const user = await this.userService.save(userDo);
 
