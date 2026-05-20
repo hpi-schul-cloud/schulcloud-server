6dfcf8e9e9ac5ff7b940d8a23ca55451e9a2ffbb
Sebastian Mueller <sebastian.mueller@dataport.de>
2026-05-20 18:18:19 +0200

    remove canListRoomInvitations function as anyone wanting to see invitations needs to have the school permission in addition to the room permission anyway; restructure tests

--- apps/server/src/modules/room-membership/authorization/room.rule.ts
+++ apps/server/src/modules/room-membership/authorization/room.rule.ts
@@ -85,7 +85,7 @@ export class RoomRule implements Rule<RoomAuthorizable> {
 			shareRoom: canShareRoom,
 			updateRoom: canUpdateRoom,
 			createRoomInvitationLinks: canManageRoomInvitationLinks,
-			listRoomInvitationLinks: canListRoomInvitationLinks,
+			listRoomInvitationLinks: canManageRoomInvitationLinks,
 			updateRoomInvitationLinks: canManageRoomInvitationLinks,
 			deleteRoomInvitationLinks: canManageRoomInvitationLinks,
 			viewContent: canViewContent,
@@ -351,13 +351,6 @@ const canManageRoomInvitationLinks = (user: User, roomAuthorizable: RoomAuthoriz
 	const canManageRoomInvitationLinks = hasSchoolPermission && hasRoomPermission;
 
 	return canManageRoomInvitationLinks;
-};
-
-const canListRoomInvitationLinks = (user: User, roomAuthorizable: RoomAuthorizable): boolean => {
-	const { roomPermissions } = resolveUserPermissions(user, roomAuthorizable);
-	const canListRoomInvitationLinks = roomPermissions.includes(Permission.ROOM_MANAGE_INVITATIONLINKS);
-
-	return canListRoomInvitationLinks;
 };
 
 const canViewContent = (user: User, roomAuthorizable: RoomAuthorizable): boolean => {
