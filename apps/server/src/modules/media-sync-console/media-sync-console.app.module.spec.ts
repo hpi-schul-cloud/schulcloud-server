8adcfb417031fe3503e341957d719f647b4d4185
Constantin Bergatt <constantin.bergatt@dataport.de>
2026-05-22 14:34:46 +0200

    BC-11651 - Remove unused DatabaseModule override from media sync console tests

--- apps/server/src/modules/media-sync-console/media-sync-console.app.module.spec.ts
+++ apps/server/src/modules/media-sync-console/media-sync-console.app.module.spec.ts
@@ -1,15 +1,5 @@
-import { DatabaseModule } from '@infra/database';
-import { Module } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
-import { findOneOrFailHandler } from '@shared/common/database-error.handler';
-import { MongoMemoryDatabaseModule } from '@testing/database';
 import { MediaSyncConsoleAppModule, MediaSyncConsoleAppTestModule } from './media-sync-console.app.module';
-import { TEST_ENTITIES } from './media-sync-console.entity.imports';
-
-@Module({
-	imports: [MongoMemoryDatabaseModule.forRoot({ ...findOneOrFailHandler, entities: TEST_ENTITIES })],
-})
-class DatabaseModuleOverride {}
 
 // TODO remove this when definitive integration tests for this console app are implemented
 // this is for code coverage and possibly to detect missing deps for media sync
@@ -18,10 +8,7 @@ describe(MediaSyncConsoleAppModule.name, () => {
 		it('should have a defined module', async () => {
 			const module = await Test.createTestingModule({
 				imports: [MediaSyncConsoleAppTestModule],
-			})
-				.overrideModule(DatabaseModule)
-				.useModule(DatabaseModuleOverride)
-				.compile();
+			}).compile();
 
 			expect(module).toBeDefined();
 
