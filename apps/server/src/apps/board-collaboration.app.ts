f413b7feaffdb5852720c6359eb6af66575dbe0e
Max Bischof <maximilian.bischof@dataport.de>
2026-06-02 12:49:18 +0200

    Remove unused JWT whitelist and Redis client initialization from bootstrap functions

--- apps/server/src/apps/board-collaboration.app.ts
+++ apps/server/src/apps/board-collaboration.app.ts
@@ -9,7 +9,6 @@ import { install as sourceMapInstall } from 'source-map-support';
 import { createRequestLoggerMiddleware, LegacyLogger, LoggerConfig } from '@core/logger';
 import { LOGGER_CONFIG_TOKEN } from '@core/logger/logger.config';
 import { DATABASE_CONFIG_TOKEN, InternalDatabaseConfig } from '@infra/database';
-import { JWT_WHITELIST_VALKEY_CLIENT } from '@infra/jwt-whitelist';
 import { MongoIoAdapter } from '@infra/socketio';
 import { BoardCollaborationModule } from '@modules/board/board-collaboration.app.module';
 import { BOARD_CONFIG_TOKEN, BoardConfig } from '@modules/board/board.config';
@@ -18,7 +17,6 @@ import { SwaggerDocumentOptions } from '@nestjs/swagger';
 import express from 'express';
 import { enableOpenApiDocs } from './helpers';
 import { createMetricsServer } from './helpers/metrics.server';
-import legacyRedisUtils = require('../../../../src/utils/redis');
 
 async function bootstrap(): Promise<void> {
 	sourceMapInstall();
@@ -44,11 +42,6 @@ async function bootstrap(): Promise<void> {
 	enableOpenApiDocs(nestApp, 'docs', options);
 	const loggerConfig = await nestApp.resolve<LoggerConfig>(LOGGER_CONFIG_TOKEN);
 	nestApp.use(createRequestLoggerMiddleware(loggerConfig));
-	// The redisClient must be initialized in the legacy part for the session handling (whitelisting of JWTs) to work.
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-	const sessionValkeyClient = await nestApp.resolve(JWT_WHITELIST_VALKEY_CLIENT);
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-	legacyRedisUtils.initializeRedisClient(sessionValkeyClient);
 
 	await createMetricsServer(nestApp, 'Board Collaboration Server App');
 
