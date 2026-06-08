c79033e02908a178e8950040d5da6b7c023af8ab
Max Bischof <maximilian.bischof@dataport.de>
2026-06-08 10:48:09 +0200

    Refactor notification server module: remove unused Valkey client imports and session handling

--- apps/server/src/modules/notification/notification-server.app.module.ts
+++ apps/server/src/modules/notification/notification-server.app.module.ts
@@ -1,12 +1,6 @@
 import { CoreModule } from '@core/core.module';
 import { AuthGuardModule, AuthGuardOptions, JWT_AUTH_GUARD_CONFIG_TOKEN, JwtAuthGuardConfig } from '@infra/auth-guard';
 import { DATABASE_CONFIG_TOKEN, DatabaseConfig, DatabaseModule } from '@infra/database';
-import {
-	SESSION_VALKEY_CLIENT_CONFIG_TOKEN,
-	ValkeyClientModule,
-	ValkeyClientSessionConfig,
-} from '@infra/valkey-client';
-import { SESSION_VALKEY_CLIENT } from '@modules/authentication';
 import { Module } from '@nestjs/common';
 import { MongoMemoryDatabaseModule } from '@testing/database';
 import { NotificationApiModule } from './notification-api.module';
@@ -22,11 +16,6 @@ const imports = [
 			configConstructor: JwtAuthGuardConfig,
 		},
 	]),
-	ValkeyClientModule.register({
-		clientInjectionToken: SESSION_VALKEY_CLIENT,
-		configConstructor: ValkeyClientSessionConfig,
-		configInjectionToken: SESSION_VALKEY_CLIENT_CONFIG_TOKEN,
-	}),
 ];
 
 const ENTITIES = [NotificationEntity];
