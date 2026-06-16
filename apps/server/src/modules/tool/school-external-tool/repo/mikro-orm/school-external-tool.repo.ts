1d4dc37b474762407d9152ab13514bc68d941435
Sebastian Mueller <sebastian.mueller@dataport.de>
2026-06-16 12:07:42 +0200

    fix linting error, as id should always be an ObjectId in school

--- apps/server/src/modules/tool/school-external-tool/repo/mikro-orm/school-external-tool.repo.ts
+++ apps/server/src/modules/tool/school-external-tool/repo/mikro-orm/school-external-tool.repo.ts
@@ -93,7 +93,7 @@ export class SchoolExternalToolRepo {
 		const objectIds = await this.em
 			.getCollection(this.entityName)
 			.distinct('school', { tool: new ObjectId(toolId) } as Condition<SchoolExternalToolEntity>);
-		return objectIds.map((id): EntityId => (id instanceof ObjectId ? id.toHexString() : String(id)));
+		return objectIds.map((id): EntityId => (id as unknown as ObjectId).toHexString());
 	}
 
 	public async saveMany(domainObjects: SchoolExternalTool[]): Promise<void> {
