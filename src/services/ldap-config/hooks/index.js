aebb93b9238006987c6365cbcc8243f6add4e2b4
Sebastian Mueller <sebastian.mueller@dataport.de>
2026-06-24 10:41:21 +0200

    check for SYSTEM_EDIT permission, remove administrator role check

--- src/services/ldap-config/hooks/index.js
+++ src/services/ldap-config/hooks/index.js
@@ -5,33 +5,6 @@ const { populateCurrentSchool } = require('../../../hooks');
 const fillDefaultValues = require('./fillDefaultValues');
 const restrictToSchoolSystems = require('../../ldap/hooks/restrictToSchoolSystems');
 
-const hasEditPermission = async (context) => {
-	const { userId } = context.params.account;
-	const { roles } = await context.app.service('users').get(userId, { query: { $populate: 'roles' } });
-	const isAdministrator = roles.find((r) => r.name === 'administrator');
-
-	if (!isAdministrator) {
-		throw new Error('You do not have permission to change the LDAP configuration');
-	}
-};
-
-const hasCreatePermission = async (context) => {
-	hasEditPermission(context);
-};
-
-const hasPatchPermission = async (context) => {
-	hasEditPermission(context);
-};
-
-/* TODO
-	globalHooks.hasPermission auf ...
-	SCHOOL_EDIT: 'SCHOOL_EDIT', // der wird zumindest in ldap/hooks benutzt
-	SCHOOL_PERMISSION_CHANGE: 'SCHOOL_PERMISSION_CHANGE',
-	SCHOOL_SYSTEM_EDIT: 'SCHOOL_SYSTEM_EDIT',
-	SCHOOL_TOOL_ADMIN: 'SCHOOL_TOOL_ADMIN',
-	??
-	*/
-
 module.exports = {
 	before: {
 		all: [authenticate('jwt')],
@@ -39,16 +12,14 @@ module.exports = {
 		create: [
 			// only allow external calls (the service needs a user)
 			iff(!isProvider('external'), disallow()),
-			globalHooks.hasPermission('SCHOOL_EDIT'),
-			hasCreatePermission,
+			globalHooks.hasPermission('SYSTEM_EDIT'),
 			populateCurrentSchool,
 			fillDefaultValues,
 		],
 		patch: [
 			// only allow external calls (the service needs a user)
 			iff(!isProvider('external'), disallow()),
-			globalHooks.hasPermission('SCHOOL_EDIT'),
-			hasPatchPermission,
+			globalHooks.hasPermission('SYSTEM_EDIT'),
 			populateCurrentSchool,
 			restrictToSchoolSystems,
 			fillDefaultValues,
