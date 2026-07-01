7d39ca30a6cacfade413cafb1c19b66f13b79041
SevenWaysDP <sergej.hoffmann@dataport.de>
2026-07-01 11:59:50 +0200

    feat: export additional DTOs and interfaces in files-storage-amqp-client

--- apps/server/src/infra/files-storage-amqp-client/index.ts
+++ apps/server/src/infra/files-storage-amqp-client/index.ts
@@ -1,9 +1,9 @@
-export { CopyFileDto } from './dto';
+export { CopyFileDto, FileDto } from './dto';
 export {
 	FILES_STORAGE_AMQP_CLIENT_CONFIG_TOKEN,
 	FilesStorageAMQPClientConfig,
 } from './files-storage-amqp-client-config';
 export { FilesStorageAMQPClientModule } from './files-storage-amqp-client.module';
-export { FileRecordParentType, FileRequestInfo, StorageLocation } from './interfaces';
+export { FileDO, FileRecordParentType, FileRequestInfo, ScanStatus, StorageLocation } from './interfaces';
 export { CopyFilesOfParentParamBuilder } from './mapper';
 export { FilesStorageClientAdapterService } from './service';
