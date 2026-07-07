56f67cdc2f8dfd75efa8acc97fb40c8707ee1132
Max Bischof <maximilian.bischof@dataport.de>
2026-07-07 13:25:34 +0200

    Refactor Bilo OAuth config and media source factories for improved readability

--- apps/server/src/infra/bilo-client/testing/bilo-media-source.factory.ts
+++ apps/server/src/infra/bilo-client/testing/bilo-media-source.factory.ts
@@ -1,14 +1,18 @@
 import { Factory } from 'fishery';
 import { BiloMediaSource, BiloOauthConfig } from '../interface';
 
-export const biloOauthConfigFactory = Factory.define<BiloOauthConfig>(({ sequence }) => ({
-	clientId: `client-id-${sequence}`,
-	clientSecret: `client-secret-${sequence}`,
-	authEndpoint: `https://oauth-token-url.com/test-${sequence}`,
-	baseUrl: `https://oauth-base-url.com/test-${sequence}/`,
-}));
+export const biloOauthConfigFactory = Factory.define<BiloOauthConfig>(({ sequence }) => {
+	return {
+		clientId: `client-id-${sequence}`,
+		clientSecret: `client-secret-${sequence}`,
+		authEndpoint: `https://oauth-token-url.com/test-${sequence}`,
+		baseUrl: `https://oauth-base-url.com/test-${sequence}/`,
+	};
+});
 
-export const biloMediaSourceFactory = Factory.define<BiloMediaSource>(({ sequence }) => ({
-	id: `media-source-id-${sequence}`,
-	oauthConfig: biloOauthConfigFactory.build(),
-}));
+export const biloMediaSourceFactory = Factory.define<BiloMediaSource>(({ sequence }) => {
+	return {
+		id: `media-source-id-${sequence}`,
+		oauthConfig: biloOauthConfigFactory.build(),
+	};
+});
