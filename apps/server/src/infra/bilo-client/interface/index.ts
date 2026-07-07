ba53718987df686081dc9f63dc1aacafeb6e6446
Max Bischof <maximilian.bischof@dataport.de>
2026-07-07 13:17:01 +0200

    Refactor Bilo client exports and improve factory definitions for consistency

--- apps/server/src/infra/bilo-client/interface/index.ts
+++ apps/server/src/infra/bilo-client/interface/index.ts
@@ -1,3 +1,3 @@
 export { MediaQueryBadResponseReport } from './bilo-media-query-bad-response-report';
-export { BiloOauthConfig } from './bilo-oauth-config.interface';
 export { BiloMediaSource } from './bilo-media-source.interface';
+export { BiloOauthConfig } from './bilo-oauth-config.interface';
