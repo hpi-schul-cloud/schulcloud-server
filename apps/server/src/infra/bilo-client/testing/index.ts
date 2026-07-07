dc6ae5b7885a486f525efea1ea0411feb166e3d7
Max Bischof <maximilian.bischof@dataport.de>
2026-07-07 13:01:08 +0200

    Remove Bilo Modules dependencies to modules

--- apps/server/src/infra/bilo-client/testing/index.ts
+++ apps/server/src/infra/bilo-client/testing/index.ts
@@ -1,3 +1,4 @@
 export { biloMediaQueryResponseFactory } from './bilo-media-query.response.factory';
 export { biloMediaQueryDataResponseFactory } from './bilo-media-query-data.response.factory';
 export { biloMediaQueryBadResponseReportFactory } from './bilo-media-query-bad-response-report.factory';
+export { biloMediaSourceFactory, biloOauthConfigFactory } from './bilo-media-source.factory';
--- apps/server/src/modules/media-source/loggable/index.ts
+++ apps/server/src/modules/media-source/loggable/index.ts
@@ -1,2 +1,1 @@
 export { MediaSourceNotFoundLoggableException } from './media-source-not-found-loggable.exception';
-export { MediaSourceOauthConfigNotFoundLoggableException } from './media-source-oauth-config-not-found-loggable.exception';
