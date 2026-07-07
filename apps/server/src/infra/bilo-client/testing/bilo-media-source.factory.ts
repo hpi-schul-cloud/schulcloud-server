50ba12ffcd6b86ef2788c69b503180b6026f0855
SevenWaysDP <sergej.hoffmann@dataport.de>
2026-07-07 14:44:27 +0200

    Add type keyword to imports in Bilo and Vidis media source interfaces and factories

--- apps/server/src/infra/bilo-client/testing/bilo-media-source.factory.ts
+++ apps/server/src/infra/bilo-client/testing/bilo-media-source.factory.ts
@@ -1,5 +1,5 @@
 import { Factory } from 'fishery';
-import { BiloMediaSource, BiloOauthConfig } from '../interface';
+import { type BiloMediaSource, type BiloOauthConfig } from '../interface';
 
 export const biloOauthConfigFactory = Factory.define<BiloOauthConfig>(({ sequence }) => {
 	return {
