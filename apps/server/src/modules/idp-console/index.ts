81858d37cc638d6aebc632d6483531f33e7bdd2e
Constantin Bergatt <constantin.bergatt@dataport.de>
2026-07-14 12:08:29 +0200

    BC-11996 - Revert index file for `idp-console` and `deletion-console`

--- apps/server/src/modules/idp-console/index.ts
+++ apps/server/src/modules/idp-console/index.ts
@@ -1,7 +1,3 @@
-/** **********************************************************
- * This is a module facade.                                  *
- * Export only what is allowed to be used externally.        *
- * Do not use wildcard exports.                              *
- * Do not export *.app.module.ts here; import them directly. *
- *********************************************************** */
-
+/** *************************************************
+ *   Do not re-export here! File should be empty!   *
+ ************************************************** */
