5ab86f9462bd0bc64b087b78bf68beba6f08799b
Constantin Bergatt <constantin.bergatt@dataport.de>
2026-07-21 07:43:34 +0200

    BC-12022 - refactor: change CreateBoardLoadTest from interface to type alias

--- apps/server/src/modules/board/loadtest/create-board-loadtest.ts
+++ apps/server/src/modules/board/loadtest/create-board-loadtest.ts
@@ -2,10 +2,8 @@ import type { BoardLoadTest } from './board-load-test';
 import type { SocketConnectionManager } from './socket-connection-manager';
 import type { Callback, ClassDefinition } from './types';
 
-export interface CreateBoardLoadTest {
-	(
-		socketConnectionManager: SocketConnectionManager,
-		classDefinition: ClassDefinition,
-		onError: Callback
-	): BoardLoadTest;
-}
+export type CreateBoardLoadTest = (
+	socketConnectionManager: SocketConnectionManager,
+	classDefinition: ClassDefinition,
+	onError: Callback
+) => BoardLoadTest;
