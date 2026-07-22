b86090d7db1bcfc1347dd6b62969841786a286ea
Max Bischof <maximilian.bischof@dataport.de>
2026-07-22 10:59:45 +0200

    Change logger level from warning to info in InMemoryClient methods

--- apps/server/src/infra/valkey-client/clients/in-memory.client.ts
+++ apps/server/src/infra/valkey-client/clients/in-memory.client.ts
@@ -24,13 +24,13 @@ export class InMemoryClient extends EventEmitter implements StorageClient {
 			this.ttlStore[key] = { expiresAt: Date.now() + ttlSeconds * 1000, ttl: ttlSeconds };
 		}
 
-		this.logger.warning(new InMemoryLoggable(`SET - Key: ${key} - Value: ${value}`));
+		this.logger.info(new InMemoryLoggable(`SET - Key: ${key} - Value: ${value}`));
 
 		return Promise.resolve();
 	}
 
 	public get(key: string): Promise<string | null> {
-		this.logger.warning(new InMemoryLoggable(`GET - Key: ${key} - Value: ${this.store[key]}`));
+		this.logger.info(new InMemoryLoggable(`GET - Key: ${key} - Value: ${this.store[key]}`));
 
 		if (this.ttlStore[key]?.expiresAt < Date.now()) {
 			return Promise.resolve(null);
@@ -46,20 +46,20 @@ export class InMemoryClient extends EventEmitter implements StorageClient {
 			delete this.ttlStore[key];
 			length = 1;
 		}
-		this.logger.warning(new InMemoryLoggable(`DELETE - Key: ${key} - Value: ${this.store[key]}`));
+		this.logger.info(new InMemoryLoggable(`DELETE - Key: ${key} - Value: ${this.store[key]}`));
 
 		return Promise.resolve(length);
 	}
 
 	public keys(pattern: string): Promise<string[]> {
 		const regex = new RegExp(pattern.replace(/\*/g, '.*'));
-		this.logger.warning(new InMemoryLoggable(`Pattern: ${pattern}`));
+		this.logger.info(new InMemoryLoggable(`Pattern: ${pattern}`));
 
 		return Promise.resolve(Object.keys(this.store).filter((key) => regex.test(key)));
 	}
 
 	public ttl(key: string): Promise<number> {
-		this.logger.warning(new InMemoryLoggable(`TTL - Key: ${key} - TTL: ${this.ttlStore[key]?.ttl}`));
+		this.logger.info(new InMemoryLoggable(`TTL - Key: ${key} - TTL: ${this.ttlStore[key]?.ttl}`));
 
 		return Promise.resolve(this.ttlStore[key]?.ttl ?? -1);
 	}
