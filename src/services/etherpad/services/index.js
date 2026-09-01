2c324de9ffce4524ec600d18ed2b7efab2a10e22
hoeppner.dataport <marcus.hoeppner@dataport.de>
2026-09-01 10:34:34 +0200

    chore: add new class to barrel file

--- src/services/etherpad/services/index.js
+++ src/services/etherpad/services/index.js
@@ -2,10 +2,12 @@ const Pad = require('./Pad');
 const Session = require('./Session');
 const Group = require('./Group');
 const Author = require('./Author');
+const ActiveSessions = require('./ActiveSessions');
 
 module.exports = {
 	Pad,
 	Session,
 	Group,
 	Author,
+	ActiveSessions,
 };
