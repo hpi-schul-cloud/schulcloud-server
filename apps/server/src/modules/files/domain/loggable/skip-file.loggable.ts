4fc270ac2b193161d5a5ad26a49223794307b339
hoeppner-dataport <106819770+hoeppner-dataport@users.noreply.github.com>
2026-09-18 14:31:39 +0200

    SVSDEV-143 - implement pre-check logic for downloads (#6458)
    * implement pre-check logic for downloads
    * actual filesize detection on S3 using GET
    * error-logging and 500 if no file is accessible

--- apps/server/src/modules/files/domain/loggable/skip-file.loggable.ts
+++ apps/server/src/modules/files/domain/loggable/skip-file.loggable.ts
@@ -2,12 +2,15 @@ import { type Loggable, type LoggableMessage } from '@shared/common/loggable';
 import { type EntityId } from '@shared/domain/types';
 
 export class SkipFileLoggable implements Loggable {
-	constructor(private readonly fileId: EntityId) {}
+	constructor(
+		private readonly fileId: EntityId,
+		private readonly reason?: string
+	) {}
 
 	public getLogMessage(): LoggableMessage {
 		return {
 			message: 'Skipping file due to download error',
-			data: { fileId: this.fileId },
+			data: { fileId: this.fileId, reason: this.reason },
 		};
 	}
 }
--- apps/server/src/modules/files/domain/types/index.ts
+++ apps/server/src/modules/files/domain/types/index.ts
@@ -1,5 +1,6 @@
 export * from './file-owner-model.enum';
 export * from './file-permission-reference-model.enum';
+export * from './file-probe-result';
 export * from './file-security-check-status.enum';
 export * from './get-file-response';
 export * from './legacy-file.response';
